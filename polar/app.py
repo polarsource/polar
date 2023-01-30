55c6f370aee32acc366ad38a4e495d195cdcdb71
Birk Jernström <birkjernstrom@gmail.com>
2023-01-30 10:14:30 +0100

    feat: Logging with structlog

--- polar/app.py
+++ polar/app.py
@@ -1,6 +1,10 @@
+import structlog
 from fastapi import FastAPI
 
 from polar.api import router
+from polar.logging import configure as configure_logging
+
+log = structlog.get_logger()
 
 
 def create_app() -> FastAPI:
@@ -9,4 +13,6 @@ def create_app() -> FastAPI:
     return app
 
 
+configure_logging()
+log.info("Starting Polar API")
 app = create_app()
---
+++ polar/logging.py
@@ -0,0 +1,112 @@
+import logging.config
+from typing import Any, Generic, TypeVar
+
+import structlog
+
+from polar.config import settings
+
+RendererType = TypeVar("RendererType")
+
+
+class Logging(Generic[RendererType]):
+    """Hubben logging configurator of `structlog` and `logging`.
+
+    Customized implementation inspired by the following documentation:
+    https://www.structlog.org/en/stable/standard-library.html#rendering-using-structlog-based-formatters-within-logging
+
+    """
+
+    timestamper = structlog.processors.TimeStamper(fmt="iso")
+
+    @classmethod
+    def get_level(cls) -> str:
+        return settings.LOG_LEVEL
+
+    @classmethod
+    def get_processors(cls) -> list[Any]:
+        return [
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            cls.timestamper,
+            structlog.processors.UnicodeDecoder(),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ]
+
+    @classmethod
+    def get_renderer(cls) -> RendererType:
+        raise NotImplementedError()
+
+    @classmethod
+    def configure_stdlib(cls) -> None:
+        level = cls.get_level()
+        logging.config.dictConfig(
+            {
+                "version": 1,
+                "disable_existing_loggers": False,
+                "formatters": {
+                    "hubbenlog": {
+                        "()": structlog.stdlib.ProcessorFormatter,
+                        "processors": [
+                            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                            cls.get_renderer(),
+                        ],
+                        "foreign_pre_chain": [
+                            structlog.stdlib.add_log_level,
+                            structlog.stdlib.add_logger_name,
+                            structlog.stdlib.ExtraAdder(),
+                            cls.timestamper,
+                        ],
+                    },
+                },
+                "handlers": {
+                    "default": {
+                        "level": level,
+                        "class": "logging.StreamHandler",
+                        "formatter": "hubbenlog",
+                    },
+                },
+                "loggers": {
+                    "": {
+                        "handlers": ["default"],
+                        "level": level,
+                        "propagate": True,
+                    },
+                },
+            }
+        )
+
+    @classmethod
+    def configure_structlog(cls) -> None:
+        structlog.configure(
+            processors=cls.get_processors(),
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            wrapper_class=structlog.stdlib.BoundLogger,
+            cache_logger_on_first_use=True,
+        )
+
+    @classmethod
+    def configure(cls) -> None:
+        cls.configure_stdlib()
+        cls.configure_structlog()
+
+
+class Development(Logging[structlog.dev.ConsoleRenderer]):
+    @classmethod
+    def get_renderer(cls) -> structlog.dev.ConsoleRenderer:
+        return structlog.dev.ConsoleRenderer(colors=True)
+
+
+class Production(Logging[structlog.processors.JSONRenderer]):
+    @classmethod
+    def get_renderer(cls) -> structlog.processors.JSONRenderer:
+        return structlog.processors.JSONRenderer()
+
+
+def configure() -> None:
+    if settings.is_development() or settings.is_testing():
+        return Development.configure()
+    return Production.configure()

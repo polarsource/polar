593af90d6a78734204562b9b90e26903c1dafa2d
Birk Jernström <birkjernstrom@gmail.com>
2023-02-15 09:06:38 +0100

    ref: Removing some more legacy components

--- clients/apps/web/src/components/Website/Layout.tsx
+++ clients/apps/web/src/components/Website/Layout.tsx
@@ -1,14 +1,11 @@
 import Header from 'components/Shared/Header'
 import Container from 'components/Shared/Container'
-import Navigation from 'components/Website/Navigation'
 import '@stripe/stripe-js'
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Header>
-        <Navigation />
-      </Header>
+      <Header />
       <Container wide={false} className="mt-4 text-center items-start">
         <div className="grow">{children}</div>
       </Container>

2bdd962d4798b272d0ccd3a65a7bea14d01e7b34
Birk Jernström <birkjernstrom@gmail.com>
2023-02-22 20:33:54 +0100

    feat: Use Postgres xmax column to track insert/update via upserts

--- server/polar/models/base.py
+++ server/polar/models/base.py
@@ -2,11 +2,10 @@ import enum
 import uuid
 from datetime import datetime
 
-from sqlalchemy import DateTime
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
 from polar.ext.sqlalchemy import GUID, IntEnum
 from polar.models.mixins import ActiveRecordMixin, SerializeMixin
+from sqlalchemy import TEXT, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Model(DeclarativeBase, ActiveRecordMixin, SerializeMixin):
@@ -28,6 +27,24 @@ class RecordModel(TimestampedModel):
     __abstract__ = True
 
     id: Mapped[uuid.UUID] = mapped_column(GUID, primary_key=True, default=GUID.generate)
+
+    # We use upserts frequently, but would still like to know when a record was
+    # created vs. updated.
+    #
+    # Postgres has `xmax` as a system column containing the row lock in case of updates.
+    # For inserts no lock is needed so it's zero (0).
+    #
+    # https://www.cybertec-postgresql.com/en/whats-in-an-xmax/
+    # https://stackoverflow.com/questions/59579151/how-do-i-select-a-postgresql-system-column-using-sqlalchemy
+    _xmax: Mapped[str] = mapped_column("xmax", TEXT, system=True)
+
+    @property
+    def was_inserted(self) -> bool:
+        return self._xmax == 0
+
+    @property
+    def was_updated(self) -> bool:
+        return self._xmax != 0
 
 
 class StatusFlag(enum.Enum):

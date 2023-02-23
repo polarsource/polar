5e1f7658ffda06f9a9f4f88fbf72d066a3a54d81
Birk Jernström <birkjernstrom@gmail.com>
2023-02-23 22:22:24 +0100

    ref: Move polarkit code into polarkit/src/*

--- clients/packages/polarkit/index.ts
+++ clients/packages/polarkit/index.ts
@@ -1,9 +1,8 @@
-import * as React from 'react'
-export { api } from './api'
+export { api } from './src/api'
 export {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query'
 
-export * from './QueryDemo'
+export * from './src/QueryDemo'

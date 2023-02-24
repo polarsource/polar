fc0642b664c600260621d06a9e952b3b3bd58724
Gustav Westling <gustav@westling.dev>
2023-02-24 16:07:06 +0100

    fix(RepoSelection): use full width

--- clients/apps/web/src/components/Dashboard/RepoSelection.tsx
+++ clients/apps/web/src/components/Dashboard/RepoSelection.tsx
@@ -61,7 +61,7 @@ const RepoSelection = () => {
     <Listbox value={selected} onChange={setSelected}>
       {({ open }) => (
         <>
-          <div className="relative mt-1">
+          <div className="relative mt-1 w-full">
             <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
               <span className="flex items-center">
                 <img

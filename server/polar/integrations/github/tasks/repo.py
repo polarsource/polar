c3903500798421aded80998db592776e45a0d2e1
Birk Jernström <birkjernstrom@gmail.com>
2023-03-08 22:06:15 +0100

    ref: Move Github task util functions to separate module (tmp)

--- server/polar/integrations/github/tasks/repo.py
+++ server/polar/integrations/github/tasks/repo.py
@@ -2,29 +2,11 @@ import structlog
 
 from polar.integrations.github import service
 from polar.kit.extensions.sqlalchemy import GUID
-from polar.models import Organization, Repository
-from polar.postgres import AsyncSession
 from polar.worker import get_db_session, sync_worker, task
 
+from .utils import get_organization_and_repo
+
 log = structlog.get_logger()
-
-
-async def get_organization_and_repo(
-    session: AsyncSession,
-    organization_id: GUID,
-    repository_id: GUID,
-) -> tuple[Organization, Repository]:
-    organization = await service.github_organization.get(session, organization_id)
-    if not organization:
-        log.warning("no organization found", organization_id=organization_id)
-        raise ValueError("no organization found")
-
-    repository = await service.github_repository.get(session, repository_id)
-    if not repository:
-        log.warning("no repository found", repository_id=organization_id)
-        raise ValueError("no repository found")
-
-    return (organization, repository)
 
 
 @task(name="github.repo.sync.issues")

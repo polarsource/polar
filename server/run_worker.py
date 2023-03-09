2474ea497060f1ffa5509a85bd0c98bfaa4819c0
Birk Jernström <birkjernstrom@gmail.com>
2023-03-09 11:30:36 +0100

    feat: Fix Celery logging

--- server/run_worker.py
+++ server/run_worker.py
@@ -1,6 +1,19 @@
+from typing import Any
 import polar.logging
+from celery import signals
 
-polar.logging.configure()
+
+@signals.setup_logging.connect
+def setup_logging(*args: Any, **kwargs: Any) -> None:
+    polar.logging.configure()
+
+
+@signals.task_prerun.connect
+def setup_task_logging(
+    sender: Any, task_id: Any, task: Any, args: Any, kwargs: Any, **_: Any
+) -> None:
+    polar.logging.configure_celery_task(task_id, task, args, kwargs)
+
 
 from polar.tasks import *  # noqa
 from polar.worker import app  # noqa

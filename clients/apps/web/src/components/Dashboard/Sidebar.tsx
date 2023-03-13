8614ce2fb424390bcbacc3a908ff41e04c8bdf9d
Gustav Westling <gustav@polar.sh>
2023-03-13 12:33:05 +0100

    feat(dashboard): WIP design with topbar

--- clients/apps/web/src/components/Dashboard/Sidebar.tsx
+++ clients/apps/web/src/components/Dashboard/Sidebar.tsx
@@ -1,16 +1,13 @@
 import Profile from 'components/Shared/Profile'
-import RepoSelection from './RepoSelection'
 
 const Sidebar = () => {
   return (
     <>
-      <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-        <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
+      <div className="fixed mt-16 hidden min-h-full bg-[#F7F7F7] pb-16  md:inset-y-0 md:flex md:w-64 md:flex-col">
+        <div className="flex min-h-0 flex-1 flex-col border-r border-[#EDEDED]">
           <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-            <div className="flex flex-shrink-0 items-center px-4">
-              <RepoSelection />
-            </div>
-            <nav className="mt-5 flex-1 space-y-1 bg-white px-2"></nav>
+            <div className="flex flex-shrink-0 items-center px-4">[Search]</div>
+            <nav className="mt-5 flex-1 space-y-1 px-2"></nav>
           </div>
           <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
             <Profile />
--- clients/apps/web/src/components/Layout/Dashboard.tsx
+++ clients/apps/web/src/components/Layout/Dashboard.tsx
@@ -1,11 +1,13 @@
 import Sidebar from '../Dashboard/Sidebar'
+import Topbar from '../Dashboard/Topbar'
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div>
+    <div className="min-h-full bg-red-800">
+      <Topbar />
+      <div className="bg-blue-200">
         <Sidebar />
-        <div className="flex flex-1 flex-col md:pl-64">
+        <div className="flex flex-1 flex-col bg-white pt-16 md:pl-64">
           <main className="flex-1">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
@@ -15,7 +17,7 @@ const Layout = ({ children }) => {
           </main>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 export default Layout

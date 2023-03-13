9aeb93bbde730fce86efbf3a4be56af19a20f88d
Gustav Westling <gustav@polar.sh>
2023-03-13 14:25:05 +0100

    feat(topbar): design tweaks

--- clients/apps/web/src/components/Dashboard/Topbar.tsx
+++ clients/apps/web/src/components/Dashboard/Topbar.tsx
@@ -1,10 +1,6 @@
-import {
-  ArrowRightOnRectangleIcon,
-  BellIcon,
-  Cog8ToothIcon,
-} from '@heroicons/react/24/outline'
+import { BellIcon, Cog8ToothIcon, CubeIcon } from '@heroicons/react/24/outline'
 import BalanceBadge from './BalanceBadge'
-
+import Logout from './Logout'
 import RepoSelection from './RepoSelection'
 
 const Topbar = () => {
@@ -13,17 +9,23 @@ const Topbar = () => {
       <div className="flex flex-1 items-center space-x-4 ">
         <RepoSelection />
         <BalanceBadge />
-        <Cog8ToothIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-      </div>
-
-      <div className=" font-semibold text-gray-700">Polar</div>
-
-      <div className="flex flex-1 justify-end space-x-4">
-        <BellIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-        <ArrowRightOnRectangleIcon
-          className="h-6 w-6 text-gray-400"
+        <Cog8ToothIcon
+          className="h-6 w-6 cursor-pointer text-gray-400 transition-colors duration-100 hover:text-gray-800"
           aria-hidden="true"
         />
+      </div>
+
+      <div className=" inline-flex items-center space-x-2 font-semibold text-gray-700">
+        <CubeIcon className="h-6 w-6 " aria-hidden="true" />
+        <span>Polar</span>
+      </div>
+
+      <div className="flex flex-1 justify-end space-x-4">
+        <BellIcon
+          className="h-6 w-6 cursor-pointer text-gray-400 transition-colors duration-100 hover:text-gray-800"
+          aria-hidden="true"
+        />
+        <Logout />
       </div>
     </div>
   )
--- clients/apps/web/src/components/Layout/Dashboard.tsx
+++ clients/apps/web/src/components/Layout/Dashboard.tsx
@@ -3,11 +3,11 @@ import Topbar from '../Dashboard/Topbar'
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-full bg-red-800">
+    <div className="">
       <Topbar />
-      <div className="bg-blue-200">
+      <div>
         <Sidebar />
-        <div className="flex flex-1 flex-col bg-white pt-16 md:pl-80">
+        <div className="flex min-h-screen flex-1 flex-col bg-white pt-16 md:pl-80">
           <main className="flex-1">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">

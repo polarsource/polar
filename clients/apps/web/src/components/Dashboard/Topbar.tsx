74c3ff3069094c9a5511fa27f020b11573839cef
Gustav Westling <gustav@polar.sh>
2023-03-13 13:46:40 +0100

    feat(topbar): balance badge tweak

--- clients/apps/web/src/components/Dashboard/Topbar.tsx
+++ clients/apps/web/src/components/Dashboard/Topbar.tsx
@@ -1,3 +1,10 @@
+import {
+  ArrowRightOnRectangleIcon,
+  BellIcon,
+  Cog8ToothIcon,
+} from '@heroicons/react/24/outline'
+import BalanceBadge from './BalanceBadge'
+
 import RepoSelection from './RepoSelection'
 
 const Topbar = () => {
@@ -5,19 +12,18 @@ const Topbar = () => {
     <div className="fixed z-10 flex h-16 w-full items-center justify-between bg-white px-4 drop-shadow">
       <div className="flex flex-1 items-center space-x-4 ">
         <RepoSelection />
-
-        <div className="flex items-center space-x-3 rounded-full border-2 border-[#E5DEF5] bg-[#F9F7FD] py-1 pr-1.5 pl-3 text-[#7556BA]">
-          <span>$123.40</span>
-          <div className="h-[22px] w-[22px] rounded-full bg-[#9171D9]"></div>
-        </div>
-        <span className="text-xl">⚙️</span>
+        <BalanceBadge />
+        <Cog8ToothIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
       </div>
 
       <div className=" font-semibold text-gray-700">Polar</div>
 
       <div className="flex flex-1 justify-end space-x-4">
-        <span className="text-xl">🔔</span>
-        <span className="text-xl">🔥</span>
+        <BellIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+        <ArrowRightOnRectangleIcon
+          className="h-6 w-6 text-gray-400"
+          aria-hidden="true"
+        />
       </div>
     </div>
   )

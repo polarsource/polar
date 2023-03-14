223fd658830d1509e8a42f1c80ae01157efb6c02
Gustav Westling <gustav@polar.sh>
2023-03-14 10:32:09 +0100

    chore(dashboard): remove unused stripe code

--- clients/apps/web/src/dashboard/organization.tsx
+++ clients/apps/web/src/dashboard/organization.tsx
@@ -1,10 +1,5 @@
-import { api } from 'polarkit'
 import { IssueList } from 'polarkit/components'
-import {
-  requireAuth,
-  useDashboard,
-  useRepositoryPullRequests,
-} from 'polarkit/hooks'
+import { useDashboard, useRepositoryPullRequests } from 'polarkit/hooks'
 import {
   type Entry_Any_,
   type Entry_IssueRead_,
@@ -14,64 +9,9 @@ import {
 import { useParams } from 'react-router-dom'
 import { DashboardFilters } from './filters'
 
-const StripeAccountLink = ({ organization, stripeId }) => {
-  const redirect = async () => {
-    const response = await api
-      .get(
-        `/api/organizations/${organization}/account/${stripeId}/stripe_login`,
-      )
-      .then((res) => {
-        if (res.status == 200 && res.data.url) {
-          window.location = res.data.url
-        }
-      })
-  }
-
-  return (
-    <>
-      <a
-        href="#"
-        onClick={(e) => {
-          e.preventDefault()
-          redirect()
-        }}
-      >
-        Stripe Dashboard
-      </a>
-    </>
-  )
-}
-
-const createLinks = async (organization_name: string, stripe_id: string) => {
-  const response = await api
-    .post(
-      '/api/organizations/' +
-        organization_name +
-        '/account/' +
-        stripe_id +
-        '/links',
-    )
-    .then((res) => {
-      if (res.status == 200 && res.data.url) {
-        window.location = res.data.url
-      }
-    })
-}
-
-const createAccount = async (organization_name: string) => {
-  const response = await api
-    .post('/api/organizations/' + organization_name + '/account')
-    .then((res) => {
-      if (res.status == 200 && res.data?.stripe_id) {
-        createLinks(organization_name, res.data.stripe_id)
-      }
-    })
-}
-
 const Organization = (props: { filters: DashboardFilters }) => {
   const { filters } = props
 
-  const { developer } = requireAuth()
   const { orgSlug, repoSlug } = useParams()
 
   const dashboardQuery = useDashboard(orgSlug, repoSlug, filters.q)
@@ -100,43 +40,6 @@ const Organization = (props: { filters: DashboardFilters }) => {
         rewards={rewards2}
       />
     </div>
-  )
-
-  const organization = developer.getOrganizationBySlug(slug)
-
-  return (
-    <>
-      <h1>{organization.name}</h1>
-
-      {!organization.account && (
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            createAccount(organization.name)
-          }}
-        >
-          Create Stripe Account
-        </button>
-      )}
-
-      {organization.account && !organization.account.is_details_submitted && (
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            createLinks(organization.name, organization.account.stripe_id)
-          }}
-        >
-          Stripe Onboarding
-        </button>
-      )}
-
-      {organization.account && organization.account.is_details_submitted && (
-        <StripeAccountLink
-          organization={organization.name}
-          stripeId={organization.account.stripe_id}
-        />
-      )}
-    </>
   )
 }
 

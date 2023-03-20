d6d931a7fa7b7aaa6a23b3f92e3b04be1343b9ef
Gustav Westling <gustav@polar.sh>
2023-03-20 18:46:46 +0100

    feat: verify configured github app permissions and hooks on boot

---
+++ server/polar/verify_github_app.py
@@ -0,0 +1,8 @@
+from polar.config import settings
+from polar.logging import configure as configure_logging
+import polar.integrations.github.verify as github_verify
+
+
+def main() -> None:
+    configure_logging()
+    github_verify.verify_app_configuration()

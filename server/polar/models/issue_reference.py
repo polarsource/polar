23a93adc971fe3d1646a4ec2cb462c644dbe37db
Gustav Westling <gustav@polar.sh>
2023-03-22 10:21:10 +0100

    feat(references): references to pull requests in other repositories

--- server/polar/models/issue_reference.py
+++ server/polar/models/issue_reference.py
@@ -1,10 +1,12 @@
+import enum
 from uuid import UUID
 
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from polar.kit.db.models import TimestampedModel
 from polar.kit.extensions.sqlalchemy import PostgresUUID
+from polar.kit.extensions.sqlalchemy.types import StringEnum
 from polar.kit.schemas import Schema
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -30,19 +32,36 @@ class ExternalGitHubCommitReference(Schema):
     commit_id: str
 
 
+class ReferenceType(str, enum.Enum):
+    # external_id is a pull_requests.id UUID
+    PULL_REQUEST = "pull_request"
+
+    # external_id is a HREF
+    EXTERNAL_GITHUB_PULL_REQUEST = "external_github_pull_request"
+
+    # external_id is a SHA1
+    EXTERNAL_GITHUB_COMMIT = "external_github_commit"
+
+
 class IssueReference(TimestampedModel):
     __tablename__ = "issue_references"
-
-    id: Mapped[UUID] = mapped_column(
-        PostgresUUID,
-        nullable=False,
-        primary_key=True,
-    )
 
     issue_id: Mapped[UUID] = mapped_column(
         PostgresUUID,
         ForeignKey("issues.id"),
         nullable=False,
+        primary_key=True,
+    )
+
+    reference_type: Mapped[str] = mapped_column(
+        StringEnum(ReferenceType),
+        primary_key=True,
+    )
+
+    external_id: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+        primary_key=True,
     )
 
     # Either pull_request_id or external_source will be set
@@ -52,14 +71,8 @@ class IssueReference(TimestampedModel):
         nullable=True,
     )
 
-    reference_type: Mapped[str] = mapped_column(String)
-
     # If referenced by an external resource
     external_source: Mapped[
         ExternalGitHubPullRequestReference | ExternalGitHubCommitReference | None
-    ] = mapped_column(JSONB, nullable=True, default=dict)
+    ] = mapped_column(JSONB, nullable=True, default=None)
 
-    # Unstructured event data
-    # external_event_source: Mapped[JSONDict | None] = mapped_column(
-    #    JSONB, nullable=True, default=dict
-    # )

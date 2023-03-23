6bb5e1e182cc2a1f2b96c4ad5b061d391c83eac7
Gustav Westling <gustav@polar.sh>
2023-03-23 13:10:28 +0100

    feat(clients/dashboard): render external commits and prs

--- clients/packages/polarkit/src/components/IssueReference.tsx
+++ clients/packages/polarkit/src/components/IssueReference.tsx
@@ -1,9 +1,13 @@
 import {
+  ExternalGitHubCommitReference,
+  ExternalGitHubPullRequestReference,
   IssueReferenceRead,
   IssueReferenceType,
   type IssueRead,
   type PullRequestReference,
 } from '../api/client'
+import IssueReferenceExternalGitHubCommit from './IssueReferenceExternalGitHubCommit'
+import IssueReferenceExternalGitHubPullRequest from './IssueReferenceExternalGitHubPullRequest'
 import IssueReferencePullRequest from './IssueReferencePullRequest'
 
 const IssueReference = (props: {
@@ -21,12 +25,33 @@ const IssueReference = (props: {
     )
   }
 
-  return (
-    <pre>
-      <br />
-      {JSON.stringify(reference)}
-    </pre>
-  )
+  if (
+    reference &&
+    reference.type === IssueReferenceType.EXTERNAL_GITHUB_COMMIT
+  ) {
+    return (
+      <IssueReferenceExternalGitHubCommit
+        issue={issue}
+        commit={reference.payload as ExternalGitHubCommitReference}
+      />
+    )
+  }
+
+  if (
+    reference &&
+    reference.type === IssueReferenceType.EXTERNAL_GITHUB_PULL_REQUEST
+  ) {
+    return (
+      <>
+        <IssueReferenceExternalGitHubPullRequest
+          issue={issue}
+          pr={reference.payload as ExternalGitHubPullRequestReference}
+        />
+      </>
+    )
+  }
+
+  return <></>
 }
 
 export default IssueReference

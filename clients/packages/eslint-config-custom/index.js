9488f607b2b043958f3a140d5ae8cf57f5913850
Gustav Westling <gustav@polar.sh>
2023-03-27 10:45:08 +0200

    fix(eslint): fix conflicting config

--- clients/packages/eslint-config-custom/index.js
+++ clients/packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier", "next/core-web-vitals"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",

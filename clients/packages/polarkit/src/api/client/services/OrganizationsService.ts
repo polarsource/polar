1585f1bf7b2d48648b69f83278727afa9b065ce6
Gustav Westling <gustav@polar.sh>
2023-03-29 14:36:30 +0200

    clients: generated api

--- clients/packages/polarkit/src/api/client/services/OrganizationsService.ts
+++ clients/packages/polarkit/src/api/client/services/OrganizationsService.ts
@@ -3,6 +3,8 @@
 /* eslint-disable */
 import type { OrganizationRead } from '../models/OrganizationRead';
 import type { OrganizationSettingsUpdate } from '../models/OrganizationSettingsUpdate';
+import type { OrganizationSetupIntentRead } from '../models/OrganizationSetupIntentRead';
+import type { OrganizationStripeCustomerRead } from '../models/OrganizationStripeCustomerRead';
 import type { Platforms } from '../models/Platforms';
 
 import type { CancelablePromise } from '../core/CancelablePromise';
@@ -64,6 +66,90 @@ export class OrganizationsService {
       },
       body: requestBody,
       mediaType: 'application/json',
+      errors: {
+        422: `Validation Error`,
+      },
+    });
+  }
+
+  /**
+   * Get Stripe Customer
+   * @returns OrganizationStripeCustomerRead Successful Response
+   * @throws ApiError
+   */
+  public getStripeCustomer({
+    platform,
+    orgName,
+  }: {
+    platform: Platforms,
+    orgName: string,
+  }): CancelablePromise<OrganizationStripeCustomerRead> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/api/v1/{platform}/{organization_name}/stripe_customer',
+      path: {
+        'platform': platform,
+      },
+      query: {
+        'org_name': orgName,
+      },
+      errors: {
+        422: `Validation Error`,
+      },
+    });
+  }
+
+  /**
+   * Create Setup Intent
+   * @returns OrganizationSetupIntentRead Successful Response
+   * @throws ApiError
+   */
+  public createSetupIntent({
+    platform,
+    orgName,
+  }: {
+    platform: Platforms,
+    orgName: string,
+  }): CancelablePromise<OrganizationSetupIntentRead> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/api/v1/{platform}/{organization_name}/setup_intent',
+      path: {
+        'platform': platform,
+      },
+      query: {
+        'org_name': orgName,
+      },
+      errors: {
+        422: `Validation Error`,
+      },
+    });
+  }
+
+  /**
+   * Set Default Payment Method
+   * @returns OrganizationStripeCustomerRead Successful Response
+   * @throws ApiError
+   */
+  public setDefaultPaymentMethod({
+    platform,
+    orgName,
+    paymentMethodId,
+  }: {
+    platform: Platforms,
+    orgName: string,
+    paymentMethodId: string,
+  }): CancelablePromise<OrganizationStripeCustomerRead> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/api/v1/{platform}/{organization_name}/set_default_payment_method',
+      path: {
+        'platform': platform,
+      },
+      query: {
+        'org_name': orgName,
+        'payment_method_id': paymentMethodId,
+      },
       errors: {
         422: `Validation Error`,
       },

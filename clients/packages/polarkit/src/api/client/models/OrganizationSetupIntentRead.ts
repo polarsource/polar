48a5379fb75a06d6d9df3a528daa18e32627b511
Gustav Westling <gustav@polar.sh>
2023-03-29 14:36:30 +0200

    clients: generated api

---
+++ clients/packages/polarkit/src/api/client/models/OrganizationSetupIntentRead.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type OrganizationSetupIntentRead = {
+  id: string;
+  status: string;
+  client_secret: string;
+};
+
---
+++ clients/packages/polarkit/src/api/client/models/PaymentMethod.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type PaymentMethod = {
+  type: string;
+  card_last4?: string;
+  card_brand?: string;
+};
+

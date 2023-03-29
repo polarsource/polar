162a63322d732a8e5dbc3011fa290f7e1b055c58
Gustav Westling <gustav@westling.dev>
2023-03-29 17:38:16 +0200

    server/migrations: merge migrations

---
+++ server/migrations/versions/2023-03-29_merge.py
@@ -0,0 +1,27 @@
+"""merge
+
+Revision ID: 2c0195944541
+Revises: 06e9299c802a, 04b3cb443b5a
+Create Date: 2023-03-29 15:01:03.775073
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# Polar Custom Imports
+from polar.kit.extensions.sqlalchemy import PostgresUUID
+
+# revision identifiers, used by Alembic.
+revision = '2c0195944541'
+down_revision = ('06e9299c802a', '04b3cb443b5a')
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

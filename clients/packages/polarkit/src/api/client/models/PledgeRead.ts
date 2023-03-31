2494c7620f11e75e17b856fa32691101dcdfc2a3
Gustav Westling <gustav@polar.sh>
2023-03-31 11:03:26 +0200

    clients/api: generate

--- clients/packages/polarkit/src/api/client/models/PledgeRead.ts
+++ clients/packages/polarkit/src/api/client/models/PledgeRead.ts
@@ -5,15 +5,15 @@
 import type { polar__pledge__schemas__State } from './polar__pledge__schemas__State';
 
 export type PledgeRead = {
-  issue_id: string;
-  email?: string;
-  amount: number;
-  pledge_as_org?: string;
   id: string;
   created_at: string;
+  issue_id: string;
+  amount: number;
   repository_id: string;
   organization_id: string;
   state: polar__pledge__schemas__State;
+  pledger_name?: string;
+  pledger_avatar?: string;
   client_secret?: string;
 };
 

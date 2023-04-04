d64af7eafea4f2fc545cfaa3ab8466d6b6c5a85c
Gustav Westling <gustav@polar.sh>
2023-04-04 09:43:42 +0200

    clients/api: generate

--- clients/packages/polarkit/src/api/client/models/NotificationRead.ts
+++ clients/packages/polarkit/src/api/client/models/NotificationRead.ts
@@ -2,10 +2,15 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { IssueRead } from './IssueRead';
 import type { NotificationType } from './NotificationType';
+import type { PledgeRead } from './PledgeRead';
 
 export type NotificationRead = {
   id: string;
   type: NotificationType;
+  created_at: string;
+  pledge?: PledgeRead;
+  issue?: IssueRead;
 };
 

86db05734e2f1975b59a182ecf76e322ffabcc1c
Gustav Westling <gustav@polar.sh>
2023-04-04 16:08:47 +0200

    server/notifications: deduplicate notifications

--- server/polar/models/notification.py
+++ server/polar/models/notification.py
@@ -1,12 +1,13 @@
 from uuid import UUID
 
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from polar.kit.db.models import RecordModel
 from polar.kit.extensions.sqlalchemy import PostgresUUID
 from polar.models.issue import Issue
 from polar.models.pledge import Pledge
+from polar.models.pull_request import PullRequest
 
 
 class Notification(RecordModel):
@@ -23,6 +24,10 @@ class Notification(RecordModel):
         PostgresUUID, ForeignKey("pledges.id"), nullable=True
     )
 
+    pull_request_id: Mapped[UUID] = mapped_column(
+        PostgresUUID, ForeignKey("pull_requests.id"), nullable=True
+    )
+
     pledge: Mapped[Pledge | None] = relationship(
         "Pledge",
         lazy="joined",
@@ -32,3 +37,10 @@ class Notification(RecordModel):
         "Issue",
         lazy="joined",
     )
+
+    pull_request: Mapped[PullRequest | None] = relationship(
+        "PullRequest",
+        lazy="joined",
+    )
+
+    dedup_key: Mapped[str] = mapped_column(String, nullable=False, unique=True)

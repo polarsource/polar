effe1ffee4fbaf68262ddc381fb2ca80093925ad
Gustav Westling <gustav@polar.sh>
2023-04-05 14:03:11 +0200

    clients/IssueLabel: better mimick github colors

--- clients/packages/polarkit/src/components/IssueLabel.tsx
+++ clients/packages/polarkit/src/components/IssueLabel.tsx
@@ -4,15 +4,6 @@ export type LabelSchema = {
   color: string // Hex code without the #
 }
 
-const componentToHex = (c: number): string => {
-  const hex = c.toString(16)
-  return hex.length == 1 ? '0' + hex : hex
-}
-
-const rgbToHex = (r: number, g: number, b: number): string => {
-  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
-}
-
 const hexToRgb = (hex: string): { r: number; g: number; b: number } | null => {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result
@@ -22,9 +13,6 @@ const hexToRgb = (hex: string): { r: number; g: number; b: number } | null => {
         b: parseInt(result[3], 16),
       }
     : null
-}
-const clamp = (a: number, min: number, max: number): number => {
-  return a < min ? min : a > max ? max : a
 }
 
 const IssueLabel = (props: { label: LabelSchema }) => {
@@ -42,24 +30,12 @@ const IssueLabel = (props: { label: LabelSchema }) => {
     const { r, g, b } = rgb
     sum = r + g + b
 
-    if (sum < 100) {
+    if (sum < (255 * 3) / 2) {
       // Very dark, use white
       style.color = '#ffffff'
-    } else if (sum > 255 * 3 - 100) {
+    } else {
       // Very light, use black
       style.color = '#000000'
-    } else {
-      let scale = 2.4 // Lighten
-      if (sum > 400) {
-        scale = 0.4 // Darken
-      }
-
-      const newColor = rgbToHex(
-        parseInt(clamp(r * scale, 0, 255).toFixed(0)),
-        parseInt(clamp(g * scale, 0, 255).toFixed(0)),
-        parseInt(clamp(b * scale, 0, 255).toFixed(0)),
-      )
-      style.color = newColor
     }
   }
 

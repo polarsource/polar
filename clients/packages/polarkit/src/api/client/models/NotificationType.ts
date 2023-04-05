7cc57e44a9b775d3270e6e76535ad11c66c6e0de
Gustav Westling <gustav@polar.sh>
2023-04-05 11:38:07 +0200

    clients/api: generate

--- clients/packages/polarkit/src/api/client/models/NotificationType.ts
+++ clients/packages/polarkit/src/api/client/models/NotificationType.ts
@@ -10,4 +10,7 @@ export enum NotificationType {
   ISSUE_PLEDGED_BRANCH_CREATED = 'issue_pledged_branch_created',
   ISSUE_PLEDGED_PULL_REQUEST_CREATED = 'issue_pledged_pull_request_created',
   ISSUE_PLEDGED_PULL_REQUEST_MERGED = 'issue_pledged_pull_request_merged',
+  MAINTAINER_ISSUE_BRANCH_CREATED = 'maintainer_issue_branch_created',
+  MAINTAINER_ISSUE_PULL_REQUEST_CREATED = 'maintainer_issue_pull_request_created',
+  MAINTAINER_ISSUE_PULL_REQUEST_MERGED = 'maintainer_issue_pull_request_merged',
 }

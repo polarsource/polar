f7627400cb9b41466566b05b6e4eea92c51f3146
Gustav Westling <gustav@polar.sh>
2023-04-11 15:34:54 +0200

    clients/notifications: popover empty state

--- clients/apps/web/src/components/Notifications/Popover.tsx
+++ clients/apps/web/src/components/Notifications/Popover.tsx
@@ -64,6 +64,11 @@ const Popover = () => {
               <div className="mr-8 -mb-7 h-6 w-6 rotate-45 border-t-[1px] border-l-[1px] border-black/5 bg-white"></div>
               <div className="w-full max-w-md">
                 <div className="pointer-events-auto w-full  overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                  {notifs.data.length === 0 && (
+                    <div className="p-4 text-black/60">
+                      You don't have any notifications... yet!
+                    </div>
+                  )}
                   {notifs.data.map((n) => {
                     return <Notification n={n} key={n.id} />
                   })}

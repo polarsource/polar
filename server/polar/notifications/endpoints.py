2ed79847602df43116b2ecdccbf17753df1f2bc2
Gustav Westling <gustav@polar.sh>
2023-04-11 11:40:27 +0200

    server/notifications: fail gracefully if unable to parse payload

--- server/polar/notifications/endpoints.py
+++ server/polar/notifications/endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+import structlog
 
 from polar.auth.dependencies import Auth
 from polar.issue.schemas import IssueRead
@@ -14,6 +15,9 @@ from .service import notifications
 router = APIRouter(tags=["notifications"])
 
 
+log = structlog.get_logger()
+
+
 @router.get("/notifications", response_model=list[NotificationRead])
 async def get(
     auth: Auth = Depends(Auth.current_user),
@@ -21,17 +25,22 @@ async def get(
 ) -> list[NotificationRead]:
     notifs = await notifications.get_for_user(session, auth.user.id)
 
-    def decorate(n: Notification) -> NotificationRead:
-        return NotificationRead(
-            id=n.id,
-            type=NotificationType.from_str(n.type),
-            created_at=n.created_at,
-            pledge=PledgeRead.from_db(n.pledge) if n.pledge else None,
-            issue=IssueRead.from_orm(n.issue) if n.issue else None,
-            pull_request=PullRequestRead.from_orm(n.pull_request)
-            if n.pull_request
-            else None,
-            payload=notifications.parse_payload(n),
-        )
+    def decorate(n: Notification) -> NotificationRead | None:
+        try:
+            return NotificationRead(
+                id=n.id,
+                type=NotificationType.from_str(n.type),
+                created_at=n.created_at,
+                pledge=PledgeRead.from_db(n.pledge) if n.pledge else None,
+                issue=IssueRead.from_orm(n.issue) if n.issue else None,
+                pull_request=PullRequestRead.from_orm(n.pull_request)
+                if n.pull_request
+                else None,
+                payload=notifications.parse_payload(n),
+            )
+        except Exception as e:
+            log.error("failed to parse notification", e=e)
+            return None
 
-    return [decorate(n) for n in notifs]
+    res = [decorate(n) for n in notifs]
+    return [v for v in res if v is not None]

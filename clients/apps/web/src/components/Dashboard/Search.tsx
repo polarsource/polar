7b2a4cb6329793b50fea3453ac9cb97ee8b5a1fb
Gustav Westling <gustav@polar.sh>
2023-04-11 15:36:37 +0200

    clients: search empty state type fix

--- clients/apps/web/src/components/Dashboard/Search.tsx
+++ clients/apps/web/src/components/Dashboard/Search.tsx
@@ -125,7 +125,7 @@ const Search = (props: {
               className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#9171D9] sm:text-sm sm:leading-6"
               placeholder="Search issues"
               onChange={onQueryChange}
-              value={filters.q}
+              value={filters.q || ''}
             />
           </div>
         </div>

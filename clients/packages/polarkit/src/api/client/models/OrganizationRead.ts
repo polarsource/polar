a52c08ba6228365265857683de4beca00a507481
Gustav Westling <gustav@polar.sh>
2023-04-11 12:09:22 +0200

    clients/api: generate

--- clients/packages/polarkit/src/api/client/models/OrganizationRead.ts
+++ clients/packages/polarkit/src/api/client/models/OrganizationRead.ts
@@ -9,7 +9,10 @@ import type { Status } from './Status';
 export type OrganizationRead = {
   funding_badge_retroactive?: boolean;
   funding_badge_show_amount?: boolean;
-  email_notification_issue_receives_backing?: boolean;
+  email_notification_maintainer_issue_receives_backing?: boolean;
+  email_notification_maintainer_issue_branch_created?: boolean;
+  email_notification_maintainer_pull_request_created?: boolean;
+  email_notification_maintainer_pull_request_merged?: boolean;
   email_notification_backed_issue_branch_created?: boolean;
   email_notification_backed_issue_pull_request_created?: boolean;
   email_notification_backed_issue_pull_request_merged?: boolean;

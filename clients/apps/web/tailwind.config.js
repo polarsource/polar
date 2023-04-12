7ea2a5c4f04baf8815796b2d3708b964d4f4a937
Petter Nilsson <petterheterjag@gmail.com>
2023-04-12 09:29:46 +0200

    Update tailwind config with Polar branding

--- clients/apps/web/tailwind.config.js
+++ clients/apps/web/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   mode: 'jit',
@@ -9,13 +11,98 @@ module.exports = {
     '../../packages/polarkit/src/**/*.{ts,tsx}',
   ],
   theme: {
+    fontWeight: {
+      light: '300',
+      normal: '400',
+      medium: '500',
+      bold: '700',
+      display: '350'
+    },
     extend: {
+      borderColor: {
+        DEFAULT: "rgb(0 0 0 / 0.07)",
+      },
+      boxShadow: {
+        DEFAULT: '0 1px 8px rgb(0 0 0 / 0.07), 0 0.5px 2.5px rgb(0 0 0 / 0.16)',
+        lg: '0 5px 17px rgba(0 0 0 / 0.15), 0 0px 3px rgba(0 0 0 / 0.12)',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        display: ['Lexend', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        blue: {
+          50: '#f2f6fc',
+          100: '#e1ebf8',
+          200: '#caddf3',
+          300: '#a6c7ea', 
+          400: '#7ba9df',
+          500: '#5c8cd5',
+          600: '#4872c8',
+          700: '#3e60b7',
+          800: '#374e96',
+          900: '#2e4070',
+          950: '#222c49',
+        },
+        gray: {
+          50: '#fefdfb',
+          75: '#f9f8f6',
+          100: '#f6f5f3',
+          200: '#e4e3df',
+          300: '#d3d1cc',
+          400: '#a3a3a3',
+          500: '#727374',
+          600: '#505153',
+          700: '#3e3f42',
+          800: '#26282b',
+          900: '#181a1f',
+          950: '#0b0b10',
+        },
+        green: {
+          50: '#f0faf0',
+          100: '#e2f6e3',
+          200: '#c5edc6',
+          300: '#97de9a',
+          400: '#62c667',
+          500: '#3fab44',
+          600: '#2d8c31',
+          700: '#266f29',
+          800: '#235826',
+          900: '#1e4921',
+          950: '#0e2f11',
+        },
+        red: {
+          50: '#fdf3f3',
+          100: '#fde3e3',
+          200: '#fbcdcd',
+          300: '#f8a9a9',
+          400: '#f17878',
+          500: '#e64d4d',
+          600: '#d32f2f',
+          700: '#b12424',
+          800: '#922222',
+          900: '#6f1f1f',
+          950: '#420d0d',
+        }
+      }
     },
   },
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    plugin(function({ addUtilities }) {
+      const displayStyle = {
+        '.text-4xl': {
+          fontWeight: '350',
+          fontFamily: ['Lexend', defaultTheme.fontFamily.sans].toString(),
+        },
+      }
+      displayStyle['.text-5xl'] = displayStyle['.text-4xl']
+      displayStyle['.text-6xl'] = displayStyle['.text-4xl']
+      displayStyle['.text-7xl'] = displayStyle['.text-4xl']
+      displayStyle['.text-8xl'] = displayStyle['.text-4xl']
+      displayStyle['.text-9xl'] = displayStyle['.text-4xl']
+
+      addUtilities(displayStyle)
+    })
   ],
 }

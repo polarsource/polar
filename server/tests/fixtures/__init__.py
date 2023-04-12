60ae5fdf0d8bb9f9e490917c62afbcd9eb512e73
Gustav Westling <gustav@polar.sh>
2023-04-12 13:23:02 +0200

    server/tests: silence spammy faker logs

--- server/tests/fixtures/__init__.py
+++ server/tests/fixtures/__init__.py
@@ -3,3 +3,9 @@ from tests.fixtures.database import *  # noqa: F401, F403
 from tests.fixtures.webhook import *  # noqa: F401, F403
 from tests.fixtures.example_objects import *  # noqa: F401, F403
 from tests.fixtures.auth import *  # noqa: F401, F403
+
+import logging
+
+# Quiet down external libraries during testing
+logging.getLogger("faker").setLevel(logging.INFO)
+logging.getLogger("asyncio").setLevel(logging.INFO)

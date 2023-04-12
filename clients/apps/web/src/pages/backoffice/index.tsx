891d75a366cfe3c4f6befffb0b32e5e40046803a
Gustav Westling <gustav@polar.sh>
2023-04-12 16:44:47 +0200

    client/backoffice: list pledges

---
+++ clients/apps/web/src/pages/backoffice/index.tsx
@@ -0,0 +1,24 @@
+import Pledges from 'components/Backoffice/Pledges'
+import type { NextLayoutComponentType } from 'next'
+import { ReactElement } from 'react'
+import Topbar from '../../components/Shared/Topbar'
+
+const Page: NextLayoutComponentType = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Pledges</h2>
+      <Pledges />
+    </div>
+  )
+}
+
+Page.getLayout = (page: ReactElement) => {
+  return (
+    <>
+      <Topbar customLogoTitle="Backoffice"></Topbar>
+      <div className="mx-auto max-w-4xl p-4">{page}</div>
+    </>
+  )
+}
+
+export default Page

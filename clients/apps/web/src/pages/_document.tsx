e397d34b5285ead4b72bb8b0b0d3f4081fe9a865
Magnus Hult <magnus@magnushult.se>
2023-04-12 11:27:44 +0200

    Fix for cross-origin type failure

--- clients/apps/web/src/pages/_document.tsx
+++ clients/apps/web/src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin={'true'}
+          crossOrigin={''}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Lexend:wght@350&display=swap"

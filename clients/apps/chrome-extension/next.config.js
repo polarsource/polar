a9b7888f2c03b1a39f0fc312362274a2d6a52806
Magnus Hult <magnus@magnushult.se>
2023-04-13 09:07:28 +0200

    Add a commented-out section about a second entry point

--- clients/apps/chrome-extension/next.config.js
+++ clients/apps/chrome-extension/next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Trying to add another entry point, for the content script
+  // webpack: (config) => {
+  //   return {
+  //     ...config,
+  //     entry: async () => {
+  //       const entry = await config.entry()
+  //       return {
+  //         ...entry,
+  //         'content': ['./src/index.tsx'],
+  //       }
+  //     },
+  //   }
+  // },
 }
 
 module.exports = nextConfig

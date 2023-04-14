46919b7e0534dc4257130581db2cd9b2f71f6b83
Gustav Westling <gustav@polar.sh>
2023-04-14 15:54:29 +0200

    client/dashboard: tab tweaks

--- clients/apps/web/src/components/Dashboard/Tab.tsx
+++ clients/apps/web/src/components/Dashboard/Tab.tsx
@@ -11,7 +11,7 @@ const Tab = (props: {
       className={
         'w-full flex-1 cursor-pointer rounded-md py-1.5 px-3 text-center text-sm transition-all duration-100 ' +
         (props.active
-          ? 'bg-white text-gray-900 drop-shadow hover:bg-white/50 hover:text-gray-900'
+          ? 'bg-white/90 text-gray-900 drop-shadow hover:bg-white hover:text-gray-900'
           : 'bg-transparent text-gray-500 hover:bg-white/50 hover:text-gray-900')
       }
     >

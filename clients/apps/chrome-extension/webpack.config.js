420b9cd8878110ab148f679e2acdb69d57417d8e
Magnus Hult <magnus@magnushult.se>
2023-04-17 09:51:39 +0200

    Try a create-react-app chrome extension

---
+++ clients/apps/chrome-extension/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: { noEmit: false },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "content.js",
+    path: path.resolve(__dirname, "..", "extension"),
+  },
+}

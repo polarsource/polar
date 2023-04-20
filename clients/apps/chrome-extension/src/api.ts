4028234a450660df31c3b93a3e10f90d8a878373
Magnus Hult <magnus@magnushult.se>
2023-04-20 12:14:27 +0200

    Forgot a file

---
+++ clients/apps/chrome-extension/src/api.ts
@@ -0,0 +1,34 @@
+import { PolarAPI } from 'polarkit/api/client'
+
+const headers = {}
+
+// The alternative here would be to always read from storage, but since
+// token changes so seldom, that seems like a waste.
+const getHeaders = async (): Promise<Record<string, string>> => {
+  if (headers['Authorization']) {
+    return headers
+  }
+
+  return new Promise((resolve) => {
+    chrome.storage.local.get('token', (result) => {
+      headers['Authorization'] = `Bearer ${result.token}`
+      resolve(headers)
+    })
+  })
+}
+
+chrome.storage.local.onChanged.addListener(
+  (changes: { [key: string]: chrome.storage.StorageChange }) => {
+    if (changes.token && changes.token.newValue) {
+      headers['Authorization'] = `Bearer ${changes.token.newValue}`
+    }
+  },
+)
+
+const api = new PolarAPI({
+  BASE: 'http://127.0.0.1:8000', // TODO
+  WITH_CREDENTIALS: false,
+  HEADERS: getHeaders,
+})
+
+export default api

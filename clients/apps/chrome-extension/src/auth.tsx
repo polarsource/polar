65eed17d2efc6541b8384049d6dd3f8a6ca819cc
Magnus Hult <magnus@magnushult.se>
2023-04-24 15:30:45 +0200

    Close window after finding the auth token (#234)

--- clients/apps/chrome-extension/src/auth.tsx
+++ clients/apps/chrome-extension/src/auth.tsx
@@ -1,10 +1,11 @@
 window.addEventListener('load', function () {
-  const interval = setInterval(function () {
+  const interval = setInterval(async function () {
     const tokenElem = document.getElementById('polar-token')
     if (tokenElem && tokenElem.innerText) {
       alert('FOUND TOKEN ' + tokenElem.innerText)
       clearInterval(interval)
-      chrome.storage.local.set({ token: tokenElem.innerText })
+      await chrome.storage.local.set({ token: tokenElem.innerText })
+      window.close()
     }
   }, 1000)
 })

204248c3be99f683e545db229a8075ad8af7a082
Magnus Hult <magnus@magnushult.se>
2023-04-25 09:27:27 +0200

    Remove alerting the token when we find it

--- clients/apps/chrome-extension/src/auth.tsx
+++ clients/apps/chrome-extension/src/auth.tsx
@@ -2,7 +2,6 @@ window.addEventListener('load', function () {
   const interval = setInterval(async function () {
     const tokenElem = document.getElementById('polar-token')
     if (tokenElem && tokenElem.innerText) {
-      alert('FOUND TOKEN ' + tokenElem.innerText)
       clearInterval(interval)
       await chrome.storage.local.set({ token: tokenElem.innerText })
       window.close()

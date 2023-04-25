d3a7cda4926234a5289805eebfaead5b412a2483
Magnus Hult <magnus@magnushult.se>
2023-04-25 12:03:20 +0200

    Fix rendering when there is no pledge

--- clients/apps/chrome-extension/src/components/CachedIssueListItemDecoration.tsx
+++ clients/apps/chrome-extension/src/components/CachedIssueListItemDecoration.tsx
@@ -2,15 +2,8 @@ import { IssueExtensionRead } from 'polarkit/api/client'
 import { IssueListItemDecoration } from 'polarkit/components'
 import { useChromeStorageLocal } from 'use-chrome-storage'
 
-// The typing is a bit weird, so we need to provide an "empty" initial value of the correct type
-const initialValue: IssueExtensionRead = {
-  number: -1,
-  pledges: [],
-  references: [],
-}
-
 /*
- * Render the issue list item decoration from chrome.storage.
+ * Render the issue list item decoration from chrome.storage or null if there is none.
  */
 const CachedIssueListItemDecoration = ({
   orgName,
@@ -22,18 +15,18 @@ const CachedIssueListItemDecoration = ({
   number: number
 }) => {
   const [value, setValue, isPersistent, error, isInitialStateResolved] =
-    useChromeStorageLocal(
-      `issues/${orgName}/${repoName}/${number}`,
-      initialValue,
-    )
+    useChromeStorageLocal(`issues/${orgName}/${repoName}/${number}`)
 
-  if (isInitialStateResolved) {
+  // The type of value is wrong, so we need to cast the value to the correct type
+  const typedValue = value as IssueExtensionRead | undefined
+
+  if (isInitialStateResolved && typedValue) {
     return (
       <IssueListItemDecoration
         orgName={orgName}
         repoName={repoName}
-        pledges={value.pledges}
-        references={value.references}
+        pledges={typedValue.pledges}
+        references={typedValue.references}
       />
     )
   } else {

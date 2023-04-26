545bd89ab6236331a94fde9dc953d679613b4280
Gustav Westling <gustav@polar.sh>
2023-04-26 11:11:59 +0200

    client/dashboard: require invite code

--- clients/apps/web/src/pages/dashboard/[organization]/index.tsx
+++ clients/apps/web/src/pages/dashboard/[organization]/index.tsx
@@ -1,15 +1,17 @@
 import Dashboard from 'components/Dashboard/Dashboard'
+import InviteOnly from 'components/Dashboard/InviteOnly'
 import type { NextLayoutComponentType } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { ReactElement, useEffect } from 'react'
-import { useCurrentOrgAndRepoFromURL } from '../../../hooks'
+import { useCurrentOrgAndRepoFromURL, useRequireAuth } from '../../../hooks'
 
 const Page: NextLayoutComponentType = () => {
   const router = useRouter()
   const { organization } = router.query
   const key = `org-${organization}` // use key to force reload of state
   const { org, isLoaded } = useCurrentOrgAndRepoFromURL()
+  const { currentUser } = useRequireAuth()
 
   useEffect(() => {
     if (isLoaded && !org) {
@@ -17,6 +19,10 @@ const Page: NextLayoutComponentType = () => {
       return
     }
   }, [isLoaded, org])
+
+  if (currentUser && !currentUser.invite_only_approved) {
+    return <InviteOnly />
+  }
 
   if (!isLoaded) {
     return <></>
---
+++ clients/packages/polarkit/src/components/ui/atoms/index.ts
@@ -0,0 +1,4 @@
+import Input from './Input'
+import ShadowBox from './ShadowBox'
+
+export { ShadowBox, Input }

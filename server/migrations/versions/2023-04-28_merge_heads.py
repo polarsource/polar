3a59cdf6d4617c16f5848888b5634b9262a77989
Birk Jernström <birkjernstrom@gmail.com>
2023-04-28 16:41:55 +0200

    alembic: Merge heads...

---
+++ server/migrations/versions/2023-04-28_merge_heads.py
@@ -0,0 +1,27 @@
+"""merge heads
+
+Revision ID: 20743b682bf6
+Revises: 32bda2f94d52, 51d9d92d0b89
+Create Date: 2023-04-28 16:40:41.921299
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# Polar Custom Imports
+from polar.kit.extensions.sqlalchemy import PostgresUUID
+
+# revision identifiers, used by Alembic.
+revision = "20743b682bf6"
+down_revision = ("32bda2f94d52", "51d9d92d0b89")
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

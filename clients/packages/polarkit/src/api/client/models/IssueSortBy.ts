982c108f0bf47bf65ffe892cd1150ac69ca6770f
Gustav Westling <gustav@polar.sh>
2023-05-02 14:50:14 +0200

    clients/api: generate

--- clients/packages/polarkit/src/api/client/models/IssueSortBy.ts
+++ clients/packages/polarkit/src/api/client/models/IssueSortBy.ts
@@ -7,6 +7,7 @@
  */
 export enum IssueSortBy {
   NEWEST = 'newest',
+  RECENTLY_UPDATED = 'recently_updated',
   PLEDGED_AMOUNT_DESC = 'pledged_amount_desc',
   RELEVANCE = 'relevance',
   DEPENDENCIES_DEFAULT = 'dependencies_default',

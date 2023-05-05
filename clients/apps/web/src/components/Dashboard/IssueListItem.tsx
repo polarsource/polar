1c12dd188d3f436a566fafa803d5aa3d821bae9a
Gustav Westling <gustav@polar.sh>
2023-05-05 13:53:07 +0200

    clients/dispute: design dispute modal

--- clients/apps/web/src/components/Dashboard/IssueListItem.tsx
+++ clients/apps/web/src/components/Dashboard/IssueListItem.tsx
@@ -1,3 +1,4 @@
+import Modal, { ModalBox } from 'components/Shared/Modal'
 import {
   IssueDashboardRead,
   IssueReferenceRead,
@@ -11,8 +12,9 @@ import {
   IssueActivityBox,
   IssueListItemDecoration,
 } from 'polarkit/components/Issue'
-import { PolarTimeAgo } from 'polarkit/components/ui'
-import { githubIssueUrl } from 'polarkit/utils'
+import { PolarTimeAgo, PrimaryButton } from 'polarkit/components/ui'
+import { getCentsInDollarString, githubIssueUrl } from 'polarkit/utils'
+import { useState } from 'react'
 import PledgeNow from '../Pledge/PledgeNow'
 import IconCounter from './IconCounter'
 import IssueLabel, { LabelSchema } from './IssueLabel'
@@ -71,106 +73,133 @@ const IssueListItem = (props: {
 
   const markdownTitle = generateMarkdownTitle(title)
 
+  const [showDisputeModalForPledge, setShowDisputeModalForPledge] = useState<
+    PledgeRead | undefined
+  >()
+
+  const onDispute = (pledge: PledgeRead) => {
+    setShowDisputeModalForPledge(pledge)
+  }
+
+  const onDisputeModalClose = () => {
+    setShowDisputeModalForPledge(undefined)
+  }
+
   return (
-    <div>
-      <div className="hover:bg-gray-75 group flex items-center justify-between gap-4 py-4 px-2 pb-5">
-        <div className="flex flex-row items-center">
-          {isDependency && (
-            <div className="mr-3 flex-shrink-0 justify-center rounded-full bg-white p-[1px] shadow">
-              <img
-                src={props.issue.organization.avatar_url}
-                className="h-8 w-8 rounded-full"
-              />
-            </div>
-          )}
-          <div className="flex flex-col gap-1">
-            <div className="flex flex-wrap items-start gap-x-4 gap-y-2">
-              <a
-                className="text-md text-nowrap font-medium"
-                href={githubIssueUrl(props.org.name, props.repo.name, number)}
-              >
-                {markdownTitle}
-                {isDependency && (
-                  <span className="text-gray-400"> #{props.issue.number}</span>
-                )}
-              </a>
-
-              {props.issue.labels &&
-                props.issue.labels.map((label: LabelSchema) => {
-                  return <IssueLabel label={label} key={label.id} />
-                })}
-            </div>
-            {!isDependency && (
-              <div className="text-xs text-gray-500">
-                {state == 'open' && (
-                  <p>
-                    #{number} opened <PolarTimeAgo date={new Date(createdAt)} />
-                  </p>
-                )}
-                {state == 'closed' && (
-                  <p>
-                    #{number} closed <PolarTimeAgo date={new Date(closedAt)} />
-                  </p>
-                )}
-              </div>
-            )}
+    <>
+      <div>
+        <div className="hover:bg-gray-75 group flex items-center justify-between gap-4 py-4 px-2 pb-5">
+          <div className="flex flex-row items-center">
             {isDependency && (
-              <div className="text-xs text-gray-500">
-                {props.dependents?.map((dep: IssueReadWithRelations) => (
-                  <p key={dep.id}>
-                    Mentioned in{' '}
-                    <a
-                      href={githubIssueUrl(
-                        dep.organization.name,
-                        dep.repository.name,
-                        dep.number,
-                      )}
-                      className="font-medium text-blue-600"
-                    >
-                      {dep.organization.name}/{dep.repository.name}#{dep.number}{' '}
-                      - {dep.title}
-                    </a>
-                  </p>
-                ))}
+              <div className="mr-3 flex-shrink-0 justify-center rounded-full bg-white p-[1px] shadow">
+                <img
+                  src={props.issue.organization.avatar_url}
+                  className="h-8 w-8 rounded-full"
+                />
+              </div>
+            )}
+            <div className="flex flex-col gap-1">
+              <div className="flex flex-wrap items-start gap-x-4 gap-y-2">
+                <a
+                  className="text-md text-nowrap font-medium"
+                  href={githubIssueUrl(props.org.name, props.repo.name, number)}
+                >
+                  {markdownTitle}
+                  {isDependency && (
+                    <span className="text-gray-400">
+                      {' '}
+                      #{props.issue.number}
+                    </span>
+                  )}
+                </a>
+
+                {props.issue.labels &&
+                  props.issue.labels.map((label: LabelSchema) => {
+                    return <IssueLabel label={label} key={label.id} />
+                  })}
+              </div>
+              {!isDependency && (
+                <div className="text-xs text-gray-500">
+                  {state == 'open' && (
+                    <p>
+                      #{number} opened{' '}
+                      <PolarTimeAgo date={new Date(createdAt)} />
+                    </p>
+                  )}
+                  {state == 'closed' && (
+                    <p>
+                      #{number} closed{' '}
+                      <PolarTimeAgo date={new Date(closedAt)} />
+                    </p>
+                  )}
+                </div>
+              )}
+              {isDependency && (
+                <div className="text-xs text-gray-500">
+                  {props.dependents?.map((dep: IssueReadWithRelations) => (
+                    <p key={dep.id}>
+                      Mentioned in{' '}
+                      <a
+                        href={githubIssueUrl(
+                          dep.organization.name,
+                          dep.repository.name,
+                          dep.number,
+                        )}
+                        className="font-medium text-blue-600"
+                      >
+                        {dep.organization.name}/{dep.repository.name}#
+                        {dep.number} - {dep.title}
+                      </a>
+                    </p>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="flex items-center gap-6">
+              {showCommentsCount && (
+                <IconCounter icon="comments" count={comments} />
+              )}
+              {showReactionsThumbs && (
+                <IconCounter icon="thumbs_up" count={reactions.plus_one} />
+              )}
+            </div>
+
+            <IssueProgress progress={issueProgress} />
+
+            {showPledgeAction && (
+              <div className="group-hover:delay-0 -ml-6 w-0 overflow-hidden opacity-0 delay-150 duration-100 group-hover:ml-0 group-hover:w-20 group-hover:opacity-100 group-hover:transition-all group-hover:duration-200 group-hover:ease-in-out">
+                <PledgeNow
+                  issue={props.issue}
+                  org={props.org}
+                  repo={props.repo}
+                />
               </div>
             )}
           </div>
         </div>
-        <div className="flex items-center gap-6">
-          <div className="flex items-center gap-6">
-            {showCommentsCount && (
-              <IconCounter icon="comments" count={comments} />
-            )}
-            {showReactionsThumbs && (
-              <IconCounter icon="thumbs_up" count={reactions.plus_one} />
-            )}
-          </div>
 
-          <IssueProgress progress={issueProgress} />
-
-          {showPledgeAction && (
-            <div className="group-hover:delay-0 -ml-6 w-0 overflow-hidden opacity-0 delay-150 duration-100 group-hover:ml-0 group-hover:w-20 group-hover:opacity-100 group-hover:transition-all group-hover:duration-200 group-hover:ease-in-out">
-              <PledgeNow
-                issue={props.issue}
-                org={props.org}
-                repo={props.repo}
-              />
-            </div>
-          )}
-        </div>
+        {havePledgeOrReference && (
+          <IssueActivityBox>
+            <IssueListItemDecoration
+              orgName={props.org.name}
+              repoName={props.repo.name}
+              pledges={props.pledges}
+              references={props.references}
+              showDisputeAction={true}
+              onDispute={onDispute}
+            />
+          </IssueActivityBox>
+        )}
       </div>
 
-      {havePledgeOrReference && (
-        <IssueActivityBox>
-          <IssueListItemDecoration
-            orgName={props.org.name}
-            repoName={props.repo.name}
-            pledges={props.pledges}
-            references={props.references}
-          />
-        </IssueActivityBox>
+      {showDisputeModalForPledge && (
+        <Modal onClose={onDisputeModalClose}>
+          <DisputeModal pledge={showDisputeModalForPledge} />
+        </Modal>
       )}
-    </div>
+    </>
   )
 }
 
@@ -205,3 +234,69 @@ const generateMarkdownTitle = (
 }
 
 export default IssueListItem
+
+const DisputeModal = (props: { pledge: PledgeRead }) => {
+  const { pledge } = props
+  return (
+    <ModalBox>
+      <>
+        <h1 className="text-2xl font-normal">Dispute your pledge</h1>
+        <p className="text-sm text-gray-500">
+          You can dispute your pledge if you believe that you've been scammed,
+          or that the issue that you pledged towards has not been resolved in a
+          satisfacotry way.
+          <br />
+          <br />
+          Once submitted, Polar will prevent the money from being paid out to
+          the maintainer until the dispute has been resolved.
+        </p>
+        <table className="min-w-full divide-y divide-gray-300">
+          <tr>
+            <td className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+              Amount
+            </td>
+            <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+              ${getCentsInDollarString(pledge.amount)}
+            </td>
+          </tr>
+          <tr>
+            <td className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+              Pledger
+            </td>
+            <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+              {pledge.pledger_name}
+            </td>
+          </tr>
+          <tr>
+            <td className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+              Pledge ID
+            </td>
+            <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+              {pledge.id}
+            </td>
+          </tr>
+          <tr>
+            <td className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+              Issue ID
+            </td>
+            <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+              {pledge.issue_id}
+            </td>
+          </tr>
+        </table>
+        <label
+          htmlFor="dispute_description"
+          className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+        >
+          Description
+        </label>
+        <textarea
+          id="dispute_description"
+          placeholder="Explain what happened"
+          rows={8}
+        ></textarea>
+        <PrimaryButton>Submit</PrimaryButton>
+      </>
+    </ModalBox>
+  )
+}

f44b24ce6c4fe4a5a3745960a8ca8997d62a48a7
Gustav Westling <gustav@polar.sh>
2023-05-05 13:52:43 +0200

    clients: create common Modal component

--- clients/apps/web/src/components/Dashboard/SetupAccount.tsx
+++ clients/apps/web/src/components/Dashboard/SetupAccount.tsx
@@ -1,19 +1,13 @@
 import { XMarkIcon } from '@heroicons/react/24/outline'
+import Modal, { ModalBox } from 'components/Shared/Modal'
 import { api } from 'polarkit'
-import {
-  AccountType,
-  ApiError,
-  OrganizationRead,
-  Platforms,
-} from 'polarkit/api/client'
+import { AccountType, ApiError, Platforms } from 'polarkit/api/client'
 import { CountryPicker } from 'polarkit/components'
 import { PrimaryButton } from 'polarkit/components/ui'
 import { useStore } from 'polarkit/store'
 import { useState } from 'react'
 
 const SetupAccount = ({ onClose }: { onClose: () => void }) => {
-  const [selectedOrg, setSelectedOrg] = useState<OrganizationRead>()
-
   const currentOrg = useStore((store) => store.currentOrg)
 
   const [country, setCountry] = useState<string>('US')
@@ -53,73 +47,49 @@ const SetupAccount = ({ onClose }: { onClose: () => void }) => {
   }
 
   return (
-    <Background onClick={onClose}>
-      <div
-        className="h-full w-full p-8 md:h-min md:w-[400px] md:p-0"
-        onClick={(e) => {
-          e.stopPropagation()
-        }}
-      >
-        <div className="z-0 block flex h-full w-full flex-row overflow-hidden rounded-2xl bg-white shadow-2xl">
-          <div className="flex min-h-full flex-1 flex-col space-y-3 p-5 text-black/80">
-            <div className="flex w-full items-start justify-between">
-              <h1 className="text-2xl font-normal">Receive payments</h1>
-              <XMarkIcon
-                className="h-6 w-6 cursor-pointer text-black/50 hover:text-black"
-                onClick={onClose}
-              />
+    <Modal onClose={onClose}>
+      <ModalBox>
+        <>
+          <div className="flex w-full items-start justify-between">
+            <h1 className="text-2xl font-normal">Receive payments</h1>
+            <XMarkIcon
+              className="h-6 w-6 cursor-pointer text-black/50 hover:text-black"
+              onClick={onClose}
+            />
+          </div>
+
+          <form className="z-0 flex flex-col space-y-4">
+            <div className="space-y-2">
+              <label
+                htmlFor="country"
+                className="text-sm font-medium text-gray-600"
+              >
+                If this is a personal account, please select your country of
+                residence. If this is an organization or business, select the
+                country of tax residency.
+              </label>
+              <div className="space-x-4">
+                <CountryPicker onSelectCountry={onChangeCountry} />
+              </div>
             </div>
 
-            <form className="z-0 flex flex-col space-y-4">
-              <div className="space-y-2">
-                <label
-                  htmlFor="country"
-                  className="text-sm font-medium text-gray-600"
-                >
-                  If this is a personal account, please select your country of
-                  residence. If this is an organization or business, select the
-                  country of tax residency.
-                </label>
-                <div className="space-x-4">
-                  <CountryPicker onSelectCountry={onChangeCountry} />
-                </div>
-              </div>
+            {errorMessage && (
+              <p className="text-xs text-red-500">{errorMessage}</p>
+            )}
+          </form>
 
-              {errorMessage && (
-                <p className="text-xs text-red-500">{errorMessage}</p>
-              )}
-            </form>
+          <div className="md:flex-1"></div>
 
-            <div className="md:flex-1"></div>
-
-            <PrimaryButton
-              onClick={onConfirm}
-              loading={loadingStripe}
-              disabled={loadingStripe}
-            >
-              Set up account
-            </PrimaryButton>
-          </div>
-        </div>
-      </div>
-    </Background>
-  )
-}
-
-const Background = ({
-  children,
-  onClick,
-}: {
-  children: React.ReactElement
-  onClick: () => void
-}) => {
-  return (
-    <div
-      onClick={onClick}
-      className="fixed bottom-0 left-0 right-0 top-0 z-10 flex items-center justify-center bg-black/50"
-    >
-      {children}
-    </div>
+          <PrimaryButton
+            onClick={onConfirm}
+            loading={loadingStripe}
+            disabled={loadingStripe}
+          >
+            Set up account
+          </PrimaryButton>
+        </>
+      </ModalBox>
+    </Modal>
   )
 }
 
---
+++ clients/apps/web/src/components/Shared/Modal.tsx
@@ -0,0 +1,47 @@
+const Modal = ({
+  onClose,
+  children,
+}: {
+  onClose: () => void
+  children: React.ReactElement
+}) => {
+  return (
+    <Background onClick={onClose}>
+      <div
+        className="h-full w-full p-8 md:h-min md:w-[400px] md:p-0"
+        onClick={(e) => {
+          e.stopPropagation()
+        }}
+      >
+        {children}
+      </div>
+    </Background>
+  )
+}
+
+export default Modal
+
+const Background = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactElement
+  onClick: () => void
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className="fixed bottom-0 left-0 right-0 top-0 z-20 flex items-center justify-center bg-black/50"
+    >
+      {children}
+    </div>
+  )
+}
+
+export const ModalBox = ({ children }: { children: React.ReactElement }) => {
+  return (
+    <div className="z-0 block flex h-full w-full flex-col space-y-2 overflow-hidden rounded-2xl bg-white p-5 text-black/80 shadow-2xl">
+      {children}
+    </div>
+  )
+}

af680f9e78298a7b6a0cc030275de97053be2da3
Gustav Westling <gustav@polar.sh>
2023-05-17 15:15:22 +0200

    clients/badge: esm satori import

--- clients/apps/web/src/pages/[organization]/[repo]/issues/[number]/index.tsx
+++ clients/apps/web/src/pages/[organization]/[repo]/issues/[number]/index.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { CurrencyDollarIcon } from '@heroicons/react/24/outline'
-import PageNotFound from 'components/Shared/PageNotFound'
-import Pledge from 'components/Website/Pledge'
-=======
 import PageNotFound from '@/components/Shared/PageNotFound'
 import Pledge from '@/components/Website/Pledge'
->>>>>>> ddf5413 (WIP: tsconfigs)
+import { CurrencyDollarIcon } from '@heroicons/react/24/outline'
 import type {
   GetServerSideProps,
   NextLayoutComponentType,

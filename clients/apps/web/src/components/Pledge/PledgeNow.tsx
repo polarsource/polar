1b2c609cc711367f44eb9f284fbd95df10724421
Magnus Hult <magnus@magnushult.se>
2023-05-17 15:30:42 +0200

    Fix pledge overlay disappearing when mouse leaves window

--- clients/apps/web/src/components/Pledge/PledgeNow.tsx
+++ clients/apps/web/src/components/Pledge/PledgeNow.tsx
@@ -1,44 +1,21 @@
-import {
-  IssueDashboardRead,
-  OrganizationPublicRead,
-  RepositoryRead,
-} from 'polarkit/api/client'
-import { useState, type MouseEvent } from 'react'
-import Overlay from './Overlay'
-
 const PledgeNow = ({
-  issue,
-  org,
-  repo,
+  // issue,
+  // org,
+  // repo,
+  onClick,
 }: {
-  issue: IssueDashboardRead
-  org: OrganizationPublicRead
-  repo: RepositoryRead
+  // issue: IssueDashboardRead
+  // org: OrganizationPublicRead
+  // repo: RepositoryRead
+  onClick: () => void
 }) => {
-  const [open, setOpen] = useState(false)
-
-  const openOverlay = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-    setOpen(true)
-  }
-
   return (
-    <>
-      <button
-        className="w-full cursor-pointer rounded-lg bg-blue-600 px-2 py-1 text-sm font-medium text-gray-100 transition-colors duration-200 hover:bg-blue-500"
-        onClick={openOverlay}
-      >
-        Pledge
-      </button>
-      {open && (
-        <Overlay
-          onClose={() => setOpen(false)}
-          issue={issue}
-          issueOrg={org}
-          issueRepo={repo}
-        />
-      )}
-    </>
+    <button
+      className="w-full cursor-pointer rounded-lg bg-blue-600 px-2 py-1 text-sm font-medium text-gray-100 transition-colors duration-200 hover:bg-blue-500"
+      onClick={onClick}
+    >
+      Pledge
+    </button>
   )
 }
 

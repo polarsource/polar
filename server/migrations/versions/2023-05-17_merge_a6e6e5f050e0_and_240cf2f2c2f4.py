a7144d87f359b01fcb6dbba5e752806c8f797fc2
Magnus Hult <magnus@magnushult.se>
2023-05-17 10:54:32 +0200

    Merge heads

---
+++ server/migrations/versions/2023-05-17_merge_a6e6e5f050e0_and_240cf2f2c2f4.py
@@ -0,0 +1,27 @@
+"""Merge a6e6e5f050e0 and 240cf2f2c2f4
+
+Revision ID: 6243f144bd88
+Revises: a6e6e5f050e0, 240cf2f2c2f4
+Create Date: 2023-05-17 10:54:17.601278
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# Polar Custom Imports
+from polar.kit.extensions.sqlalchemy import PostgresUUID
+
+# revision identifiers, used by Alembic.
+revision = '6243f144bd88'
+down_revision = ('a6e6e5f050e0', '240cf2f2c2f4')
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

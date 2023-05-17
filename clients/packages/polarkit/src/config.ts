160a13da0ecedacb326de1b913186f448185ad9a
Birk Jernström <birkjernstrom@gmail.com>
2023-05-17 10:27:08 +0200

    stripe: Update supported countries in alpha

--- clients/packages/polarkit/src/config.ts
+++ clients/packages/polarkit/src/config.ts
@@ -6,6 +6,22 @@ const stringToNumber = (
   return parseInt(value)
 }
 
+/*
+ * Keys sorted by their countries name in English.
+ *
+ * Generated list from Stripe console settings using:
+ *
+ * let whitelist = []
+ * document.querySelectorAll('.PressableContext').forEach((d) => { if (d.checked && d.name !== '') { whitelist.push(d.name) } })
+ * whitelist.join(',')
+ *
+ *
+ * Alpha: EU, US, GB & CA.
+ * Will expand to more Stripe countries in the near future.
+ */
+const STRIPE_COUNTRIES =
+  'AT,BE,BG,CA,HR,CY,CZ,DK,EE,FI,FR,DE,GR,HU,IE,IT,LV,LT,LU,MT,NL,NO,PL,PT,RO,SK,SI,ES,SE,GB,US'
+
 let defaults = {
   BASE_URL: process?.env?.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000',
   GITHUB_APP_NAMESPACE:
@@ -20,6 +36,8 @@ let defaults = {
   MINIMUM_PLEDGE_AMOUNT:
     process?.env?.NEXT_PUBLIC_MINIMUM_PLEDGE_AMOUNT || 2000,
   SENTRY_ENABLED: process?.env?.NEXT_PUBLIC_SENTRY_ENABLED || false,
+  STRIPE_COUNTRIES_WHITELIST_CSV:
+    process?.env?.NEXT_PUBLIC_STRIPE_COUNTRIES_WHITELIST || STRIPE_COUNTRIES,
 }
 
 export const CONFIG = {

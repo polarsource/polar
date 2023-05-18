900a79a2eeea5919379168505cde6d918e7e00ae
Birk Jernström <birkjernstrom@gmail.com>
2023-05-18 20:14:02 +0200

    clients/dashboard: Prettier import of Dashboard component

--- clients/apps/web/src/pages/dashboard/index.tsx
+++ clients/apps/web/src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import Dashboard, { DefaultFilters } from 'components/Dashboard/Dashboard'
+import Dashboard, { DefaultFilters } from 'components/Dashboard'
 import Gatekeeper from 'components/Dashboard/Gatekeeper/Gatekeeper'
 import DashboardLayout from 'components/Layout/DashboardLayout'
 import OnboardingConnectReposToGetStarted from 'components/Onboarding/OnboardingConnectReposToGetStarted'

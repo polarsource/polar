a8516b87850e62b293ef5b25e43871829edf610b
Birk Jernström <birkjernstrom@gmail.com>
2023-05-19 18:06:29 +0200

    clients/extension: Redirect to login and back if unauthenticated

--- clients/apps/web/src/components/Auth/Login.tsx
+++ clients/apps/web/src/components/Auth/Login.tsx
@@ -2,7 +2,7 @@ import { LogoType70 } from 'polarkit/components/brand'
 import { ShadowBox } from 'polarkit/components/ui'
 import GithubLoginButton from '../Shared/GithubLoginButton'
 
-const Login = () => {
+const Login = ({ gotoUrl }: { gotoUrl?: string }) => {
   return (
     <div className="flex h-screen w-full grow items-center justify-center bg-[#FEFDF9]">
       <div id="polar-bg-gradient"></div>
@@ -10,7 +10,7 @@ const Login = () => {
         <ShadowBox>
           <div className="flex flex-col items-center space-y-12 px-12 py-4">
             <LogoType70 />
-            <GithubLoginButton size="large" />
+            <GithubLoginButton size="large" gotoUrl={gotoUrl} />
           </div>
         </ShadowBox>
       </div>

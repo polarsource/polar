2fb7d7910b80a7e10060f25aa3e41d2ca6a23125
Gustav Westling <gustav@polar.sh>
2023-05-19 13:37:58 +0200

    clients/stories: re-enable addons

--- clients/apps/web/.storybook/main.ts
+++ clients/apps/web/.storybook/main.ts
@@ -2,6 +2,11 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},

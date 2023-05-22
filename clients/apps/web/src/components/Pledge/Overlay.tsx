3339329c6e53deb38e8426b0cfeab6a9c16836f0
Gustav Westling <gustav@polar.sh>
2023-05-22 10:59:07 +0200

    clients/Pledge: create a pure component for the Overlay

--- clients/apps/web/src/components/Pledge/Overlay.tsx
+++ clients/apps/web/src/components/Pledge/Overlay.tsx
@@ -8,6 +8,7 @@ import {
   OrganizationStripeCustomerRead,
   PledgeMutationResponse,
   RepositoryRead,
+  UserRead,
 } from 'polarkit/api/client'
 import { IssueCard, RepositoryCard } from 'polarkit/components/pledge'
 import { GreenBanner, PrimaryButton, RedBanner } from 'polarkit/components/ui'
@@ -194,143 +195,208 @@ const Overlay = ({
     setIsDone(true)
   }
 
+  if (!currentUser || !userOrgQuery.data) {
+    return <></>
+  }
+
   return (
     <Background onClick={onClose}>
-      <div
-        className="h-full w-full p-8 md:h-min md:w-[800px] md:p-0"
-        onClick={(e) => {
-          e.stopPropagation()
-        }}
-      >
-        <div className="z-0 block flex h-full w-full flex-row overflow-hidden rounded-2xl bg-white shadow-2xl">
-          <div className="bg-gray-75 hidden flex-1 border-r border-gray-200/50 p-5 md:block">
-            <div>
-              <IssueCard
-                issue={issue}
-                className="border-0 bg-white shadow"
-                organization={issueOrg}
-                repository={issueRepo}
-              />
-            </div>
-            <RepositoryCard organization={issueOrg} repository={issueRepo} />
+      <OverlayContents
+        onClose={onClose}
+        issue={issue}
+        issueOrg={issueOrg}
+        issueRepo={issueRepo}
+        onSelectOrg={onSelectOrg}
+        selectedOrg={selectedOrg}
+        customer={customer}
+        onAmountChange={onAmountChange}
+        MINIMUM_PLEDGE={MINIMUM_PLEDGE}
+        errorMessage={errorMessage}
+        pledge={pledge}
+        isDone={isDone}
+        onClickPledge={onClickPledge}
+        isSyncing={isSyncing}
+        havePaymentMethod={havePaymentMethod}
+        currentUser={currentUser}
+        organizations={userOrgQuery.data}
+      />
+    </Background>
+  )
+}
+
+export const OverlayContents = (props: {
+  onClose: () => void
+  issue: IssueDashboardRead
+  issueOrg: OrganizationPublicRead
+  issueRepo: RepositoryRead
+  onSelectOrg: (selected: string) => void
+  selectedOrg?: OrganizationPrivateRead
+  customer?: OrganizationStripeCustomerRead
+  onAmountChange: (event: ChangeEvent<HTMLInputElement>) => void
+  MINIMUM_PLEDGE: number
+  errorMessage?: string
+  pledge: any
+  isDone: boolean
+  onClickPledge: (e: React.MouseEvent<HTMLButtonElement>) => void
+  isSyncing: boolean
+  havePaymentMethod: boolean
+  organizations: OrganizationPrivateRead[]
+  currentUser: UserRead
+}) => {
+  const {
+    onClose,
+    issue,
+    issueOrg,
+    issueRepo,
+    onSelectOrg,
+    selectedOrg,
+    customer,
+    onAmountChange,
+    MINIMUM_PLEDGE,
+    errorMessage,
+    pledge,
+    isDone,
+    onClickPledge,
+    isSyncing,
+    havePaymentMethod,
+    organizations,
+    currentUser,
+  } = props
+  return (
+    <div
+      className="h-full w-full p-8 md:h-min md:w-[800px] md:p-0"
+      onClick={(e) => {
+        e.stopPropagation()
+      }}
+    >
+      <div className="z-0 block flex h-full w-full flex-row overflow-hidden rounded-2xl bg-white shadow-2xl">
+        <div className="bg-gray-75 hidden flex-1 border-r border-gray-200/50 p-5 md:block">
+          <div>
+            <IssueCard
+              issue={issue}
+              className="border-0 bg-white shadow"
+              organization={issueOrg}
+              repository={issueRepo}
+            />
+          </div>
+          <RepositoryCard organization={issueOrg} repository={issueRepo} />
+        </div>
+
+        <div className="flex min-h-full flex-1 flex-col space-y-3 p-5 text-black/80">
+          <div className="flex w-full items-start justify-between">
+            <h1 className="text-2xl font-normal">Complete your backing</h1>
+            <XMarkIcon
+              className="h-6 w-6 cursor-pointer text-black/50 hover:text-black"
+              onClick={onClose}
+            />
           </div>
 
-          <div className="flex min-h-full flex-1 flex-col space-y-3 p-5 text-black/80">
-            <div className="flex w-full items-start justify-between">
-              <h1 className="text-2xl font-normal">Complete your backing</h1>
-              <XMarkIcon
-                className="h-6 w-6 cursor-pointer text-black/50 hover:text-black"
-                onClick={onClose}
-              />
-            </div>
-
-            <form className="z-0 flex flex-col space-y-4">
-              <div className="space-y-2">
-                <label
-                  htmlFor="pledge-as"
-                  className="text-sm font-medium text-gray-600"
-                >
-                  Pledge as
-                </label>
-                <div className="flex flex-row items-center space-x-4">
-                  <div className="relative w-full rounded-lg border border-gray-200">
-                    <RepoSelection
-                      onSelectOrg={onSelectOrg}
-                      currentOrg={selectedOrg}
-                      fullWidth={true}
-                    />
-                  </div>
-                </div>
-              </div>
-
-              {customer && (
-                <div>
-                  <PaymentMethod customer={customer} />
-                </div>
-              )}
-              <div className="space-y-2">
-                <label
-                  htmlFor="amount"
-                  className="text-sm font-medium text-gray-600"
-                >
-                  Choose amount to pledge
-                </label>
-                <div className="flex flex-row items-center space-x-4">
-                  <div className="relative w-40">
-                    <input
-                      type="number"
-                      id="amount"
-                      name="amount"
-                      className="font-display block w-full rounded-lg border-gray-200 py-2 px-4 pl-8 pr-12 text-xl shadow-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                      onChange={onAmountChange}
-                      onBlur={onAmountChange}
-                      placeholder={getCentsInDollarString(MINIMUM_PLEDGE)}
-                    />
-                    <div className="font-display pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4 text-xl">
-                      <span className="text-gray-500">$</span>
-                    </div>
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
-                      <span className="text-xs text-gray-500">USD</span>
-                    </div>
-                  </div>
-                  <p className="w-1/3 text-xs text-gray-500">
-                    Minimum is ${getCentsInDollarString(MINIMUM_PLEDGE)}
-                  </p>
-                </div>
-              </div>
-
-              {errorMessage && (
-                <p className="text-xs text-red-500">{errorMessage}</p>
-              )}
-            </form>
-
-            <div className="md:flex-1"></div>
-
-            {pledge && (
-              <>
-                <div className="mt-6 flex w-full">
-                  <div className="w-full">Pledge</div>
-                  <div className="w-full text-right">
-                    ${getCentsInDollarString(pledge.amount, true)}
-                  </div>
-                </div>
-                <div className="flex w-full">
-                  <div className="w-full">Service fee</div>
-                  <div className="w-full text-right">
-                    ${getCentsInDollarString(pledge.fee, true)}
-                  </div>
-                </div>
-                <div className="mb-6 flex w-full">
-                  <div className="w-full">Total</div>
-                  <div className="w-full text-right">
-                    ${getCentsInDollarString(pledge.amount_including_fee, true)}
-                  </div>
-                </div>
-              </>
-            )}
-
-            {!isDone && (
-              <PrimaryButton
-                onClick={onClickPledge}
-                loading={isSyncing}
-                disabled={!havePaymentMethod || !pledge || isSyncing}
+          <form className="z-0 flex flex-col space-y-4">
+            <div className="space-y-2">
+              <label
+                htmlFor="pledge-as"
+                className="text-sm font-medium text-gray-600"
               >
-                Pay $
-                {getCentsInDollarString(
-                  pledge ? pledge.amount_including_fee : MINIMUM_PLEDGE,
-                )}
-              </PrimaryButton>
-            )}
+                Pledge as
+              </label>
+              <div className="flex flex-row items-center space-x-4">
+                <div className="relative w-full rounded-lg border border-gray-200">
+                  <RepoSelection
+                    onSelectOrg={onSelectOrg}
+                    currentOrg={selectedOrg}
+                    fullWidth={true}
+                    organizations={organizations}
+                    currentUser={currentUser}
+                  />
+                </div>
+              </div>
+            </div>
 
-            {isDone && (
-              <span className="text-center text-lg">
-                Thanks for pledging! 🎉
-              </span>
+            {customer && (
+              <div>
+                <PaymentMethod customer={customer} />
+              </div>
             )}
-          </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="amount"
+                className="text-sm font-medium text-gray-600"
+              >
+                Choose amount to pledge
+              </label>
+              <div className="flex flex-row items-center space-x-4">
+                <div className="relative w-40">
+                  <input
+                    type="number"
+                    id="amount"
+                    name="amount"
+                    className="font-display block w-full rounded-lg border-gray-200 py-2 px-4 pl-8 pr-12 text-xl shadow-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                    onChange={onAmountChange}
+                    onBlur={onAmountChange}
+                    placeholder={getCentsInDollarString(MINIMUM_PLEDGE)}
+                  />
+                  <div className="font-display pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4 text-xl">
+                    <span className="text-gray-500">$</span>
+                  </div>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
+                    <span className="text-xs text-gray-500">USD</span>
+                  </div>
+                </div>
+                <p className="w-1/3 text-xs text-gray-500">
+                  Minimum is ${getCentsInDollarString(MINIMUM_PLEDGE)}
+                </p>
+              </div>
+            </div>
+
+            {errorMessage && (
+              <p className="text-xs text-red-500">{errorMessage}</p>
+            )}
+          </form>
+
+          <div className="md:flex-1"></div>
+
+          {pledge && (
+            <>
+              <div className="mt-6 flex w-full">
+                <div className="w-full">Pledge</div>
+                <div className="w-full text-right">
+                  ${getCentsInDollarString(pledge.amount, true)}
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="w-full">Service fee</div>
+                <div className="w-full text-right">
+                  ${getCentsInDollarString(pledge.fee, true)}
+                </div>
+              </div>
+              <div className="mb-6 flex w-full">
+                <div className="w-full">Total</div>
+                <div className="w-full text-right">
+                  ${getCentsInDollarString(pledge.amount_including_fee, true)}
+                </div>
+              </div>
+            </>
+          )}
+
+          {!isDone && (
+            <PrimaryButton
+              onClick={onClickPledge}
+              loading={isSyncing}
+              disabled={!havePaymentMethod || !pledge || isSyncing}
+            >
+              Pay $
+              {getCentsInDollarString(
+                pledge ? pledge.amount_including_fee : MINIMUM_PLEDGE,
+              )}
+            </PrimaryButton>
+          )}
+
+          {isDone && (
+            <span className="text-center text-lg">Thanks for pledging! 🎉</span>
+          )}
         </div>
       </div>
-    </Background>
+    </div>
   )
 }
 

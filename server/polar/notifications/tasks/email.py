46388ca9c6ed998d1246737bca96d6dc2175c203
Gustav Westling <gustav@polar.sh>
2023-05-23 11:33:34 +0200

    server/notifications: rewrite notification foundations
    - Simplify notification types, move all payloads, templates, etc to the same place
    - Remove deduplication key. If you want to send a notification, it should already be deduplicated
    - Support sending emails to anonymous pledgers
    - Very much a work in progress, pending new copy and design

--- server/polar/notifications/tasks/email.py
+++ server/polar/notifications/tasks/email.py
@@ -1,16 +1,8 @@
 from uuid import UUID
-from jinja2 import StrictUndefined
 import structlog
 from polar.models.user import User
 
 from polar.notifications.schemas import (
-    IssuePledgeCreated,
-    IssuePledgedBranchCreated,
-    IssuePledgedPullRequestCreated,
-    IssuePledgedPullRequestMerged,
-    MaintainerIssueBranchCreated,
-    MaintainerIssuePullRequestCreated,
-    MaintainerIssuePullRequestMerged,
     NotificationType,
 )
 from polar.worker import JobContext, PolarWorkerContext, task
@@ -21,10 +13,8 @@ from polar.user_organization.service import (
 )
 from polar.user.service import user as user_service
 from polar.notifications.sender import get_email_sender
-from polar.notifications.service import TNotificationPayloads, notifications
+from polar.notifications.service import notifications
 from polar.postgres import AsyncSession
-
-from jinja2.nativetypes import NativeEnvironment
 
 log = structlog.get_logger()
 
@@ -46,6 +36,8 @@ async def notifications_send(
                 log.warning("notifications.send.not_found")
                 return
 
+            # TODO: support sending to "notif.email_addr"
+
             # Get users to send to
             user = await user_service.get(session, notif.user_id)
             if not user:
@@ -59,9 +51,9 @@ async def notifications_send(
                 log.warning("notifications.send.user_no_email", user_id=user.id)
                 return
 
-            meta = notifications.parse_payload(notif)
+            notification_type = notifications.parse_payload(notif)
 
-            html = render_email(user, NotificationType.from_str(notif.type), meta)
+            html = notification_type.render(user)
             if not html:
                 log.error(
                     "notifications.send.could_not_render",
@@ -72,7 +64,7 @@ async def notifications_send(
 
             sender.send_to_user(
                 to_email_addr=user.email,
-                subject="[Polar] " + meta.issue_title,
+                subject="[Polar] " + notification_type.issue_title,
                 html_content=html,
             )
 
@@ -111,108 +103,3 @@ async def should_send(session: AsyncSession, user: User, notif: Notification) ->
             return settings.email_notification_maintainer_pull_request_merged
 
     return False
-
-
-def get_template(type: NotificationType, meta: TNotificationPayloads) -> str | None:
-    if type == NotificationType.issue_pledge_created and isinstance(
-        meta, IssuePledgeCreated
-    ):
-        return MAINTAINER_PLEDGE_CREATED
-
-    if type == NotificationType.issue_pledged_branch_created and isinstance(
-        meta, IssuePledgedBranchCreated
-    ):
-        return PLEDGER_ISSUE_BRANCH_CREATED
-
-    if type == NotificationType.issue_pledged_pull_request_created and isinstance(
-        meta, IssuePledgedPullRequestCreated
-    ):
-        return PLEDGER_ISSUE_PULL_REQUEST_CREATED
-
-    if type == NotificationType.issue_pledged_pull_request_merged and isinstance(
-        meta, IssuePledgedPullRequestMerged
-    ):
-        return PLEDGER_ISSUE_PULL_REQUEST_MERGED
-
-    if type == NotificationType.maintainer_issue_branch_created and isinstance(
-        meta, MaintainerIssueBranchCreated
-    ):
-        return MAINTAINER_ISSUE_BRANCH_CREATED
-
-    if type == NotificationType.maintainer_issue_pull_request_created and isinstance(
-        meta, MaintainerIssuePullRequestCreated
-    ):
-        return MAINTAINER_ISSUE_PULL_REQUEST_CREATED
-
-    if type == NotificationType.maintainer_issue_pull_request_merged and isinstance(
-        meta, MaintainerIssuePullRequestMerged
-    ):
-        return MAINTAINER_ISSUE_PULL_REQUEST_MERGED
-
-    return None
-
-
-def render_email(
-    user: User,
-    type: NotificationType,
-    meta: TNotificationPayloads,
-) -> str | None:
-    template = get_template(type, meta)
-    if not template:
-        log.error("email.no_template_found", typ=type)
-        return None
-
-    m: dict[str, str] = meta.dict()
-    m["username"] = user.username
-
-    env = NativeEnvironment(undefined=StrictUndefined)
-    t = env.from_string(template)
-    res = t.render(m)
-    return res
-
-
-MAINTAINER_PLEDGE_CREATED = """Hi {{username}},<br><br>
-
-{{pledger_name}} has pledged ${{pledge_amount}} to <a href="{{issue_url}}">{{issue_title}}</a>.
-"""  # noqa: E501
-
-PLEDGER_ISSUE_PULL_REQUEST_CREATED = """Hi {{username}},<br><br>
-
-{{pull_request_creator_username}} just opened a <a href="{{pull_request_url}}">pull request</a> to {{repo_owner}}/{{repo_name}} that solves
-the issue <a href="{{issue_url}}">{{issue_title}}</a> that you've backed!
-"""  # noqa: E501
-
-PLEDGER_ISSUE_PULL_REQUEST_MERGED = """Hi {{username}},<br><br>
-
-{{pull_request_creator_username}} just merged a <a href="{{pull_request_url}}">pull request</a> to {{repo_owner}}/{{repo_name}} that solves
-the issue <a href="{{issue_url}}">{{issue_title}}</a> that you've backed!<br><br>
-
-The money will soon be paid out to {{repo_owner}}.<br><br>
-
-If the issue is not solved, dispute the pledge within 14 days from the <a href="https://dashboard.polar/sh">Polar</a> dashboard, or by replying to this email.
-"""  # noqa: E501
-
-PLEDGER_ISSUE_BRANCH_CREATED = """Hi {{username}},<br><br>
-
-Polar has detected that {{branch_creator_username}} has started to work on a fix to <a href="{{issue_url}}">{{issue_title}}</a> that you've backed.
-"""  # noqa: E501
-
-
-MAINTAINER_ISSUE_PULL_REQUEST_CREATED = """Hi {{username}},<br><br>
-
-{{pull_request_creator_username}} just opened a <a href="{{pull_request_url}}">pull request</a> to {{repo_owner}}/{{repo_name}} that solves
-the issue <a href="{{issue_url}}">{{issue_title}}</a> that has been pledged on Polar.
-"""  # noqa: E501
-
-MAINTAINER_ISSUE_PULL_REQUEST_MERGED = """Hi {{username}},<br><br>
-
-{{pull_request_creator_username}} just merged a <a href="{{pull_request_url}}">pull request</a> to {{repo_owner}}/{{repo_name}} that solves
-the issue <a href="{{issue_url}}">{{issue_title}}</a> that has been pledged on Polar.<br><br>
-
-Check <a href="https://dashboard.polar.sh/">polar.sh</a> to manage your payout details.
-"""  # noqa: E501
-
-MAINTAINER_ISSUE_BRANCH_CREATED = """Hi {{username}},<br><br>
-
-Polar has detected that {{branch_creator_username}} has started to work on a fix to <a href="{{issue_url}}">{{issue_title}}</a> that has been pledged on Polar.
-"""  # noqa: E501
--- server/polar/receivers/pledges.py
+++ server/polar/receivers/pledges.py
@@ -1,24 +1,31 @@
 from typing import Any
 
 from discord_webhook import AsyncDiscordWebhook, DiscordEmbed
+import structlog
 from polar.context import PolarContext
 from polar.issue.signals import issue_updated
 from polar.models import Issue
 from polar.models.pledge import Pledge
+from polar.notifications.notification import MaintainerPledgeCreatedNotification
 from polar.notifications.schemas import NotificationType
 from polar.pledge.service import pledge as pledge_service
 from polar.postgres import AsyncSession
 from polar.notifications.service import (
     PartialNotification,
+    get_cents_in_dollar_string,
     notifications as notification_service,
 )
 from polar.issue.service import issue as issue_service
+from polar.organization.service import organization as organization_service
+from polar.repository.service import repository as repository_service
 from polar.pledge.hooks import (
     PledgeHook,
     pledge_created as pledge_created_hook,
     pledge_updated as pledge_updated_hook,
 )
 from polar.config import settings
+
+log = structlog.get_logger()
 
 
 @issue_updated.connect
@@ -32,15 +39,42 @@ async def mark_pledges_pending_on_issue_close(
 async def pledge_created_notification(pledge: Pledge, session: AsyncSession):
     issue = await issue_service.get_by_id(session, pledge.issue_id)
     if not issue:
+        log.error("pledge_created_notification.no_issue_found")
         return
+
+    org = await organization_service.get(session, issue.organization_id)
+    if not org:
+        log.error("pledge_created_notification.no_org_found")
+        return
+
+    repo = await repository_service.get(session, issue.repository_id)
+    if not repo:
+        log.error("pledge_created_notification.no_repo_found")
+        return
+
+    issue_url = f"https://github.com/{org.name}/{repo.name}/issues/{issue.number}"
+
+    # Build pledger name
+    pledger_name = None
+    if pledge.organization:
+        pledger_name = pledge.organization.name
+    elif pledge.user:
+        pledger_name = pledge.user.username
+    else:
+        pledger_name = "anonymous"
+
+    n = MaintainerPledgeCreatedNotification(
+        pledger_name=pledger_name,
+        pledge_amount=get_cents_in_dollar_string(pledge.amount),
+        issue_url=issue_url,
+        issue_title=issue.title,
+    )
 
     await notification_service.create_for_issue(
         session,
         issue,
-        NotificationType.issue_pledge_created,
         notif=PartialNotification(
-            issue_id=issue.id,
-            pledge_id=pledge.id,
+            issue_id=pledge.issue_id, pledge_id=pledge.id, payload=n
         ),
     )
 

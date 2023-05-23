d58ef9823b1bc47e8f4901e58099d5301f2e71d8
Magnus Hult <magnus@magnushult.se>
2023-05-23 10:34:28 +0200

    Reload auth script on navigation

--- clients/apps/chrome-extension/src/auth.ts
+++ clients/apps/chrome-extension/src/auth.ts
@@ -1,12 +1,10 @@
-window.addEventListener('load', function () {
-  const interval = setInterval(async function () {
-    const tokenElem = document.getElementById('polar-token')
-    if (tokenElem && tokenElem.innerText) {
-      clearInterval(interval)
-      await chrome.storage.local.set({ token: tokenElem.innerText })
-      window.close()
-    }
-  }, 1000)
-})
+const interval = setInterval(async function () {
+  const tokenElem = document.getElementById('polar-token')
+  if (tokenElem && tokenElem.innerText) {
+    clearInterval(interval)
+    await chrome.storage.local.set({ token: tokenElem.innerText })
+    window.close()
+  }
+}, 1000)
 
 export {}

e247521d6d6bd30669e230d956b577cc619b7dab
Gustav Westling <gustav@polar.sh>
2023-05-24 14:57:23 +0200

    server/notifications: update NotificationType for new notifications

--- server/polar/notifications/schemas.py
+++ server/polar/notifications/schemas.py
@@ -2,30 +2,22 @@ from datetime import datetime
 from typing import Self, Union
 from uuid import UUID
 
-from pydantic import BaseModel
 from polar.kit.schemas import Schema
 from enum import Enum
 
 from polar.notifications.notification import (
+    MaintainerPledgeCreatedNotification,
     MaintainerPledgePaidNotification,
     MaintainerPledgePendingNotification,
     PledgerPledgePendingNotification,
 )
 
 
-# TODO: keep or use?
 class NotificationType(str, Enum):
-    issue_pledge_created = "issue_pledge_created"
-
-    # To pledgers when the status changes for the pledged issue
-    issue_pledged_branch_created = "issue_pledged_branch_created"
-    issue_pledged_pull_request_created = "issue_pledged_pull_request_created"
-    issue_pledged_pull_request_merged = "issue_pledged_pull_request_merged"
-
-    # To maintainers
-    maintainer_issue_branch_created = "maintainer_issue_branch_created"
-    maintainer_issue_pull_request_created = "maintainer_issue_pull_request_created"
-    maintainer_issue_pull_request_merged = "maintainer_issue_pull_request_merged"
+    MaintainerPledgePaidNotification = "MaintainerPledgePaidNotification"
+    MaintainerPledgePendingNotification = "MaintainerPledgePendingNotification"
+    MaintainerPledgeCreatedNotification = "MaintainerPledgeCreatedNotification"
+    PledgerPledgePendingNotification = "PledgerPledgePendingNotification"
 
     @classmethod
     def from_str(cls, s: str) -> Self:
@@ -39,7 +31,7 @@ class NotificationRead(Schema):
     payload: Union[
         MaintainerPledgePaidNotification,
         MaintainerPledgePendingNotification,
-        MaintainerPledgePaidNotification,
+        MaintainerPledgeCreatedNotification,
         PledgerPledgePendingNotification,
     ]
 

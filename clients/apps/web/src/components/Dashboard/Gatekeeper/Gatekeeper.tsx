72f45fb965ddda24a302d01a2cb427601c76ce1c
Birk Jernström <birkjernstrom@gmail.com>
2023-05-30 22:16:15 +0200

    clients/auth: No longer need to reload user in Gatekeeper

--- clients/apps/web/src/components/Dashboard/Gatekeeper/Gatekeeper.tsx
+++ clients/apps/web/src/components/Dashboard/Gatekeeper/Gatekeeper.tsx
@@ -1,24 +1,19 @@
 import InviteOnly from '@/components/Dashboard/Gatekeeper/InviteOnly'
-import { useAuth } from '@/hooks/auth'
-import { useUser } from 'polarkit/hooks'
-import { useEffect } from 'react'
+import { useRequireAuth } from '@/hooks/auth'
 import AcceptTerms from './AcceptTerms'
 
 const Gatekeeper = (props: { children: React.ReactElement }) => {
-  const { currentUser, reloadUser } = useAuth()
-  const user = useUser()
+  const { currentUser } = useRequireAuth()
 
-  useEffect(() => {
-    if (user.failureReason?.status === 401) {
-      reloadUser()
-    }
-  }, [user, reloadUser])
+  if (!currentUser) {
+    return <></>
+  }
 
-  if (currentUser && !currentUser.invite_only_approved) {
+  if (!currentUser.invite_only_approved) {
     return <InviteOnly />
   }
 
-  if (currentUser && !currentUser.accepted_terms_of_service) {
+  if (!currentUser.accepted_terms_of_service) {
     return <AcceptTerms />
   }
 

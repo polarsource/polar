d56d486ddd770d1cd6230b590a6376bde4d737b5
Birk Jernström <birkjernstrom@gmail.com>
2023-05-30 22:48:57 +0200

    clients/auth: Get login path from config

--- clients/packages/polarkit/src/config.ts
+++ clients/packages/polarkit/src/config.ts
@@ -24,6 +24,7 @@ const STRIPE_COUNTRIES =
 
 let defaults = {
   BASE_URL: process?.env?.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000',
+  LOGIN_PATH: process?.env?.NEXT_PUBLIC_LOGIN_PATH || '/',
   GITHUB_APP_NAMESPACE:
     process?.env?.NEXT_PUBLIC_GITHUB_APP_NAMESPACE || 'polar-sh',
   LOCALSTORAGE_PERSIST_KEY:

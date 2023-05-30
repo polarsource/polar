3a55006b8d903a3cf55d15f7420b6fd8d8aedcac
Gustav Westling <gustav@polar.sh>
2023-05-30 15:01:33 +0200

    server/redis: type fixes

--- server/polar/redis.py
+++ server/polar/redis.py
@@ -2,6 +2,7 @@ import redis.asyncio as _async_redis
 import redis as _sync_redis
 
 from polar.config import settings
+from typing import TYPE_CHECKING
 
 
 def create_async_connection_pool() -> _async_redis.ConnectionPool:
@@ -21,17 +22,26 @@ def create_sync_connection_pool() -> _sync_redis.ConnectionPool:
 
 sync_pool = create_sync_connection_pool()
 
+# https://github.com/python/typeshed/issues/7597#issuecomment-1117551641
+# Redis is generic at type checking, but not at runtime...
+if TYPE_CHECKING:
+    Redis = _async_redis.Redis[bytes]
+    SyncRedis = _sync_redis.Redis[bytes]
+else:
+    Redis = _async_redis.Redis
+    SyncRedis = _sync_redis.Redis
 
-def get_redis() -> _async_redis.Redis: # type: ignore
+
+def get_redis() -> Redis:
     return _async_redis.Redis(connection_pool=async_pool)
 
 
-def get_sync_redis() -> _sync_redis.Redis: # type: ignore
+def get_sync_redis() -> SyncRedis:
     return _sync_redis.Redis(connection_pool=sync_pool)
 
 
 redis = get_redis()
-Redis = _async_redis.Redis
+
 sync_redis = get_sync_redis()
 
 __all__ = ["redis", "sync_redis", "Redis"]

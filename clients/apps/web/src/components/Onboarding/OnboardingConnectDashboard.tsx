f8dd71117b73a3bf860c446e322c73d7a46cced2
Petter Nilsson <petterheterjag@gmail.com>
2023-05-30 11:00:59 +0200

    Upsell card polish

--- clients/apps/web/src/components/Onboarding/OnboardingConnectDashboard.tsx
+++ clients/apps/web/src/components/Onboarding/OnboardingConnectDashboard.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
 import { CONFIG } from 'polarkit'
 import { PrimaryButton } from 'polarkit/components/ui'
@@ -34,20 +34,19 @@ const OnboardingConnectPersonalDashboard = () => {
         )}
       >
         <div className="flex-1">
-          <div className="flex h-full flex-col space-y-4 p-6">
+          <div className="flex h-full flex-col space-y-2 p-6 pt-4">
             <h2 className="text-xl text-gray-900">
-              Seamless & impactful funding
+              Connect a repo to unlock Polar's full potential
             </h2>
             <div className="flex-1 text-sm text-gray-500">
-              <p>Connect a repository to unlock the full potential of Polar.</p>
-              <p className="mt-4">
+              <p>
                 <strong className="block font-medium text-gray-900">
                   Funded & improved backlog
                 </strong>
                 Public repositories can embed the Polar badge on their Github
                 Issues.
               </p>
-              <p className="mt-4">
+              <p className="mt-3">
                 <strong className="block font-medium text-gray-900">
                   Track & back dependencies
                 </strong>
@@ -55,9 +54,10 @@ const OnboardingConnectPersonalDashboard = () => {
                 issues, their status and progress.
               </p>
             </div>
-            <div className="flex items-center justify-between gap-4 lg:justify-start">
+            <div className="flex items-center justify-between gap-4 pt-2 lg:justify-start">
               <PrimaryButton
                 color="blue"
+                classNames="pl-3.5"
                 fullWidth={false}
                 onClick={() => {
                   window.open(CONFIG.GITHUB_INSTALLATION_URL, '_blank')
@@ -68,7 +68,7 @@ const OnboardingConnectPersonalDashboard = () => {
               </PrimaryButton>
               <button
                 type="button"
-                className="text-md text-blue-600"
+                className="text-md text-blue-600 transition-colors duration-200 hover:text-blue-400"
                 onClick={hideDashboardBanner}
               >
                 Skip
@@ -76,12 +76,12 @@ const OnboardingConnectPersonalDashboard = () => {
             </div>
           </div>
         </div>
-        <div className="hidden flex-1 lg:block">
+        <div className="relative hidden flex-1 lg:block">
           <Image
             src={screenshot}
             alt="Polar dashboard screenshot"
             priority={true}
-            className="w-full"
+            className="absolute h-full w-full object-cover object-left-top"
           />
         </div>
       </div>

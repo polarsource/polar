6b1d356f32c53b2acd5d7fba0af1e452fe02fa17
Gustav Westling <gustav@polar.sh>
2023-06-02 15:28:31 +0200

    ci/render: fix image reference

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-IMG="ghcr.io/polarsource/polar@sha256:${1}"
+IMG="ghcr.io/polarsource/polar@${1}"
 
 curl -X POST \
-        "https://api.render.com/deploy/srv-chsugv9mbg57s5u31btg?key=${RENDER_DEPLOY_KEY_WORKER}&imgURL=${IMG}"
+    --silent --show-error --fail-with-body \
+    "https://api.render.com/deploy/srv-chsugv9mbg57s5u31btg?key=${RENDER_DEPLOY_KEY_WORKER}&imgURL=${IMG}"
 

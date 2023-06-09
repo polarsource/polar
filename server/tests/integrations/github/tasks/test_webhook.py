cc2997bf22d0467a384e220fa36224f8df2b6862
Gustav Westling <gustav@polar.sh>
2023-06-09 16:38:48 +0200

    server/github: mocks for new installation flow

--- server/tests/integrations/github/tasks/test_webhook.py
+++ server/tests/integrations/github/tasks/test_webhook.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+import asyncio
+import json
 
-from typing import Any
+from typing import Any, Coroutine
 from datetime import datetime
-from unittest.mock import ANY, patch
+from unittest.mock import ANY, AsyncMock, MagicMock, patch
+import githubkit
 
 import pytest
 from pytest_mock import MockerFixture
@@ -23,6 +26,9 @@ from tests.fixtures.webhook import TestWebhook, TestWebhookFactory
 from polar.integrations.github.service.repository import (
     github_repository as github_repository_service,
 )
+import httpx
+import respx
+
 
 FAKE_CTX: JobContext = {
     "redis": ArqRedis(),
@@ -224,6 +230,145 @@ async def test_webhook_installation_delete(
         await fetched.save(session)
 
 
+def hook_as_obj(
+    hook: github.webhooks.InstallationRepositoriesAdded,
+) -> github.rest.InstallationRepositoriesGetResponse200:
+    return github.rest.InstallationRepositoriesGetResponse200(
+        total_count=len(hook.repositories_added),
+        repositories=[
+            github.rest.Repository(
+                id=repo.id,
+                node_id=repo.node_id,
+                name=repo.name,
+                full_name=repo.full_name,
+                private=repo.private,
+                # dummy values
+                license=None,
+                organization=None,
+                forks=0,
+                permissions=github.rest.RepositoryPropPermissions(
+                    admin=True,
+                    pull=True,
+                    push=True,
+                    maintain=True,
+                    triage=True,
+                ),
+                owner=github.rest.SimpleUser(
+                    login="xx",
+                    id=123,
+                    node_id="what",
+                    avatar_url="xxx",
+                    gravatar_id=None,
+                    url="xxx",
+                    html_url="xxx",
+                    followers_url="xxx",
+                    following_url="xxx",
+                    gists_url="xxx",
+                    starred_url="xxx",
+                    subscriptions_url="xxx",
+                    organizations_url="xxx",
+                    repos_url="xxx",
+                    events_url="xxx",
+                    received_events_url="xxx",
+                    type="xxx",
+                    site_admin=False,
+                ),
+                html_url="xx",
+                description=None,
+                fork=False,
+                url="xx",
+                archive_url="xx",
+                assignees_url="xx",
+                blobs_url="xx",
+                branches_url="xx",
+                collaborators_url="xx",
+                comments_url="xx",
+                commits_url="xx",
+                compare_url="xx",
+                contents_url="xx",
+                contributors_url="xx",
+                deployments_url="xx",
+                downloads_url="xx",
+                events_url="xx",
+                forks_url="xx",
+                git_commits_url="xx",
+                git_refs_url="xx",
+                git_tags_url="xx",
+                git_url="xx",
+                issue_comment_url="xx",
+                issue_events_url="xx",
+                issues_url="xx",
+                keys_url="xx",
+                labels_url="xx",
+                languages_url="xx",
+                merges_url="xx",
+                milestones_url="xx",
+                notifications_url="xx",
+                pulls_url="xx",
+                releases_url="xx",
+                ssh_url="xx",
+                stargazers_url="xx",
+                statuses_url="xx",
+                subscribers_url="xx",
+                subscription_url="xx",
+                tags_url="xx",
+                teams_url="xx",
+                trees_url="xx",
+                clone_url="xx",
+                mirror_url=None,
+                hooks_url="xx",
+                svn_url="xx",
+                homepage=None,
+                language=None,
+                forks_count=0,
+                stargazers_count=0,
+                watchers_count=0,
+                size=0,
+                default_branch="main",
+                open_issues_count=20,
+                is_template=False,
+                topics=[],
+                has_issues=True,
+                has_projects=True,
+                has_wiki=True,
+                has_pages=True,
+                has_downloads=True,
+                has_discussions=True,
+                archived=False,
+                disabled=False,
+                visibility="private",
+                pushed_at=None,
+                created_at=None,
+                updated_at=None,
+                allow_rebase_merge=True,
+                template_repository=None,
+                temp_clone_token=None,
+                allow_squash_merge=True,
+                allow_auto_merge=True,
+                delete_branch_on_merge=True,
+                allow_update_branch=True,
+                use_squash_pr_title_as_default=True,
+                squash_merge_commit_title="PR_TITLE",
+                squash_merge_commit_message="PR_BODY",
+                merge_commit_title="PR_TITLE",
+                merge_commit_message="PR_BODY",
+                allow_merge_commit=True,
+                allow_forking=True,
+                web_commit_signoff_required=True,
+                subscribers_count=123,
+                network_count=123,
+                open_issues=123,
+                watchers=123,
+                master_branch="main",
+                starred_at="",
+                anonymous_access_enabled=True,
+            )
+            for repo in hook.repositories_added
+        ],
+        repository_selection="x",
+    )
+
+
 @pytest.mark.asyncio
 async def test_webhook_repositories_added(
     mocker: MockerFixture,
@@ -237,11 +382,39 @@ async def test_webhook_repositories_added(
     hook = github_webhook.create("installation_repositories.added")
     new_repo = hook["repositories_added"][0]
 
+    # def lister(*args: Any, **kwargs: Any) -> httpx.Response:
+    parsed = github.webhooks.parse_obj("installation_repositories", hook.json)
+    if not isinstance(parsed, github.webhooks.InstallationRepositoriesAdded):
+        raise Exception("wat")
+
+    # fake it
+    x = hook_as_obj(parsed)
+
+    response_mock = mocker.patch(
+        "githubkit.core.GitHubCore._arequest",
+        side_effect=[
+            httpx.Response(
+                200,
+                request=httpx.Request("POST", ""),
+                content=x.json(),
+            ),
+        ],
+    )
+
     repo = await service.github_repository.get_by_external_id(session, new_repo["id"])
     assert repo is None
 
-    await create_repositories(github_webhook)
+    await webhook_tasks.repositories_added(
+        FAKE_CTX,
+        "installation_repositories",
+        "added",
+        hook.json,
+        polar_context=PolarWorkerContext(),
+    )
+
     await assert_repository_exists(session, new_repo)
+
+    response_mock.assert_called()
 
 
 @pytest.mark.asyncio
@@ -260,6 +433,20 @@ async def test_webhook_repositories_removed(
     await create_repositories(github_webhook)
     await assert_repository_exists(session, delete_repo)
 
+    # fake it
+    response_mock = mocker.patch(
+        "githubkit.core.GitHubCore._arequest",
+        side_effect=[
+            httpx.Response(
+                200,
+                request=httpx.Request("POST", ""),
+                content=github.rest.InstallationRepositoriesGetResponse200(
+                    total_count=0, repositories=[], repository_selection="x"
+                ).json(),
+            ),
+        ],
+    )
+
     await webhook_tasks.repositories_removed(
         FAKE_CTX,
         "installation_repositories",
@@ -276,6 +463,8 @@ async def test_webhook_repositories_removed(
 
     repo_get = await service.github_repository.get(session, repo.id)
     assert repo_get is None
+
+    response_mock.assert_called()
 
 
 @pytest.mark.asyncio

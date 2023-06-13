f75bc7560013646cd01df9a082d5be9bae98300d
Magnus Hult <magnus@magnushult.se>
2023-06-13 14:03:42 +0200

    Something that at least resembles a design

--- clients/apps/web/src/pages/new.tsx
+++ clients/apps/web/src/pages/new.tsx
@@ -57,13 +57,15 @@ const NewPledgePage: NextPage = () => {
             className="flex flex-col items-stretch rounded-none p-2 text-center md:flex-row md:rounded-xl md:pr-0"
             padding={false}
           >
-            <div className="py-5 px-3 text-left md:px-6">
+            <div className="w-full py-5 px-3 text-left md:px-6">
               <form className="flex flex-col">
                 <label
                   htmlFor="email"
                   className="mt-4 mb-2 text-sm font-medium text-gray-500 dark:text-gray-400"
                 >
-                  GitHub issue URL
+                  Paste link here
+                  (https://github.com/polarsource/polar/issues/123,
+                  polarsource/polar#123)
                 </label>
                 <input
                   type="email"
@@ -73,16 +75,17 @@ const NewPledgePage: NextPage = () => {
                   value={url}
                   className="block w-full rounded-lg border-gray-200 bg-transparent py-2.5 px-3 text-sm shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40"
                 />
+
+                <div className="mt-6">
+                  <PrimaryButton
+                    disabled={false}
+                    loading={false}
+                    onClick={syncExternalIssue}
+                  >
+                    Pledge
+                  </PrimaryButton>
+                </div>
               </form>
-            </div>
-            <div className="mt-6">
-              <PrimaryButton
-                disabled={false}
-                loading={false}
-                onClick={syncExternalIssue}
-              >
-                Pledge
-              </PrimaryButton>
             </div>
           </WhiteCard>
         </div>

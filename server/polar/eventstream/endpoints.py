4e02fbd6c4ddde9a5305b3670ade74431401676d
Birk Jernström <birkjernstrom@gmail.com>
2023-06-13 14:32:40 +0200

    server/sse: Remove usage of Starlette request.is_disconnected
    Caused SSE to terminate immediately due to being an infinite loop and constantly
    calling request.is_disconnected() which in turn creates an anyio.CancelScope() to await/receive
    cancellation. Instead, SSE-Starlette will cause asyncio.CancelError to be thrown which we capture
    as a signal vs. poll.

--- server/polar/eventstream/endpoints.py
+++ server/polar/eventstream/endpoints.py
@@ -4,7 +4,6 @@ from typing import Any, AsyncGenerator
 import structlog
 from fastapi import APIRouter, Depends
 from sse_starlette.sse import EventSourceResponse
-from fastapi import Request
 
 
 from polar.enums import Platforms
@@ -24,16 +23,11 @@ log = structlog.get_logger()
 async def subscribe(
     redis: Redis,
     channels: list[str],
-    request: Request,
 ) -> AsyncGenerator[Any, Any]:
     async with redis.pubsub() as pubsub:
         await pubsub.subscribe(*channels)
 
         while True:
-            if await request.is_disconnected():
-                await pubsub.close()
-                break
-
             try:
                 message = await pubsub.get_message(
                     ignore_subscribe_messages=True,
@@ -52,24 +46,22 @@ async def subscribe(
 
 @router.get("/user/stream")
 async def user_stream(
-    request: Request,
     auth: Auth = Depends(Auth.current_user),
     redis: Redis = Depends(get_redis),
 ) -> EventSourceResponse:
     receivers = Receivers(user_id=auth.user.id)
-    return EventSourceResponse(subscribe(redis, receivers.get_channels(), request))
+    return EventSourceResponse(subscribe(redis, receivers.get_channels()))
 
 
 @router.get("/{platform}/{org_name}/stream")
 async def user_org_stream(
     platform: Platforms,
     org_name: str,
-    request: Request,
     auth: Auth = Depends(Auth.user_with_org_access),
     redis: Redis = Depends(get_redis),
 ) -> EventSourceResponse:
     receivers = Receivers(user_id=auth.user.id, organization_id=auth.organization.id)
-    return EventSourceResponse(subscribe(redis, receivers.get_channels(), request))
+    return EventSourceResponse(subscribe(redis, receivers.get_channels()))
 
 
 @router.get("/{platform}/{org_name}/{repo_name}/stream")
@@ -77,7 +69,6 @@ async def user_org_repo_stream(
     platform: Platforms,
     org_name: str,
     repo_name: str,
-    request: Request,
     auth: Auth = Depends(Auth.user_with_org_and_repo_access),
     redis: Redis = Depends(get_redis),
 ) -> EventSourceResponse:
@@ -86,4 +77,4 @@ async def user_org_repo_stream(
         organization_id=auth.organization.id,
         repository_id=auth.repository.id,
     )
-    return EventSourceResponse(subscribe(redis, receivers.get_channels(), request))
+    return EventSourceResponse(subscribe(redis, receivers.get_channels()))

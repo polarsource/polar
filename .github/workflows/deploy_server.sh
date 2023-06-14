0a5d77e90b7cc24a8b80573ba8b60ded3c74cdc9
Gustav Westling <gustav@polar.sh>
2023-06-14 14:57:20 +0200

    ci: deploy api server

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -4,7 +4,13 @@ set -euo pipefail
 
 IMG="ghcr.io/polarsource/polar@${1}"
 
+# Deploy Worker
 curl -X POST \
     --silent --show-error --fail-with-body \
     "https://api.render.com/deploy/srv-chsugv9mbg57s5u31btg?key=${RENDER_DEPLOY_KEY_WORKER}&imgURL=${IMG}"
+
+# Deploy API
+curl -X POST \
+    --silent --show-error --fail-with-body \
+    "https://api.render.com/deploy/srv-ci4r87h8g3ne0dmvvl60?key=${RENDER_DEPLOY_KEY_API}&imgURL=${IMG}"
 

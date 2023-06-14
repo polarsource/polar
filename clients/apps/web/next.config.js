52fabeaa5e41ea812532c2e192ecc69554dbf681
Gustav Westling <gustav@polar.sh>
2023-06-14 10:15:45 +0200

    vercel: proxy static pages to framer

--- clients/apps/web/next.config.js
+++ clients/apps/web/next.config.js
@@ -3,6 +3,38 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['polarkit'],
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: 'https://splendid-help-401117.framer.app/',
+      },
+      {
+        source: '/faq',
+        destination: 'https://splendid-help-401117.framer.app/faq',
+      },
+      {
+        source: '/careers',
+        destination: 'https://splendid-help-401117.framer.app/careers',
+      },
+      {
+        source: '/404',
+        destination: 'https://splendid-help-401117.framer.app/404',
+      },
+      {
+        source: '/request',
+        destination: 'https://splendid-help-401117.framer.app/request',
+      },
+      {
+        source: '/legal/privacy',
+        destination: 'https://splendid-help-401117.framer.app/legal/privacy',
+      },
+      {
+        source: '/legal/terms',
+        destination: 'https://splendid-help-401117.framer.app/legal/terms',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

ad0f79452ce231e07cf21a98eb2cd0c9f36c345f
Marc Seitz <marc@mfts.io>
2023-06-14 15:51:49 +0200

    style(chrome-extension): improve look and responsiveness

--- clients/apps/chrome-extension/src/components/AuthorizationBanner.tsx
+++ clients/apps/chrome-extension/src/components/AuthorizationBanner.tsx
@@ -2,17 +2,17 @@ import { LogoType70 } from 'polarkit/components/brand'
 import { CONFIG } from '../config'
 
 const AuthorizationBanner = () => (
-  <div className="flex items-center space-x-4">
+  <div className="flex items-center justify-between sm:justify-start sm:space-x-4">
     <LogoType70 className="h-12" />
     <a
       href={`${CONFIG.WEB_URL}/dashboard/settings/extension`}
       target="_blank"
       rel="noreferrer"
-      className="inline-flex items-center space-x-2 rounded-lg bg-blue-500 p-2 px-3 text-center text-sm font-medium text-white transition-colors duration-100 hover:bg-blue-600"
+      className="mr-2 inline-flex items-center space-x-2 rounded-lg bg-blue-500 p-2 px-3 text-center text-sm font-medium text-white transition-colors duration-100 hover:bg-blue-600 sm:mr-0"
     >
       Connect Polar
     </a>
-    <span className="text-sm text-gray-500">
+    <span className="hidden text-sm text-gray-500 sm:inline-flex">
       Connect Polar to see pledges here on GitHub
     </span>
   </div>

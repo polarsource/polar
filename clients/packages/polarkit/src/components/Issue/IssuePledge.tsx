5e65e98bdae6bef5cb822e5d9d15b5d6eb86c241
Magnus Hult <magnus@magnushult.se>
2023-06-15 14:05:54 +0200

    clients/pledge: add a button to confirm pledges for an issue

--- clients/packages/polarkit/src/components/Issue/IssuePledge.tsx
+++ clients/packages/polarkit/src/components/Issue/IssuePledge.tsx
@@ -1,27 +1,48 @@
-import { PledgeRead } from 'polarkit/api/client'
+import { api } from 'polarkit/api'
+import { Platforms, PledgeRead, PledgeState } from 'polarkit/api/client'
 import { getCentsInDollarString } from 'polarkit/utils'
+import IssueConfirmButton from './IssueConfirmButton'
 
 interface Props {
+  orgName: string
+  repoName: string
+  issueNumber: number
   pledges: PledgeRead[]
 }
 
 const IssuePledge = (props: Props) => {
-  const { pledges } = props
+  const { orgName, repoName, issueNumber, pledges } = props
 
   const totalPledgeAmount = pledges.reduce(
     (accumulator, pledge) => accumulator + pledge.amount,
     0,
   )
 
+  const confirmable = pledges.some(
+    (p) => p.state === PledgeState.CONFIRMATION_PENDING,
+  )
+
+  const confirmPledges = async () => {
+    await api.pledges.confirmPledges({
+      platform: Platforms.GITHUB,
+      orgName,
+      repoName,
+      number: issueNumber,
+    })
+  }
+
   return (
-    <div className="flex items-center gap-2">
-      <p className="space-x-1 rounded-2xl bg-blue-800 px-3 py-1 text-sm text-blue-300 dark:bg-blue-200 dark:text-blue-700">
-        ${' '}
-        <span className="text-blue-100 dark:text-blue-900">
-          {getCentsInDollarString(totalPledgeAmount)}
-        </span>
-      </p>
-    </div>
+    <>
+      <div className="flex items-center gap-2">
+        <p className="space-x-1 rounded-2xl bg-blue-800 px-3 py-1 text-sm text-blue-300 dark:bg-blue-200 dark:text-blue-700">
+          ${' '}
+          <span className="text-blue-100 dark:text-blue-900">
+            {getCentsInDollarString(totalPledgeAmount)}
+          </span>
+        </p>
+      </div>
+      {confirmable && <IssueConfirmButton onClick={confirmPledges} />}
+    </>
   )
 }
 

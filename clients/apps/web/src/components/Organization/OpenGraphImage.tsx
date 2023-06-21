a2d958b7d8a951c3fe42669e16a49438d2c74904
Gustav Westling <gustav@polar.sh>
2023-06-21 12:12:03 +0200

    clients/og: story and design tweaks

---
+++ clients/apps/web/src/components/Organization/OpenGraphImage.tsx
@@ -0,0 +1,175 @@
+import { IssuePublicRead } from 'polarkit/api/client'
+
+const OpenGraphImage = (props: {
+  org_name: string
+  issue_count: number
+  avatar: string
+  issues: IssuePublicRead[]
+}) => {
+  const showIssues = props.issues.slice(0, 2).map((i) => {
+    const now = new Date()
+    const createdAt = new Date(i.issue_created_at)
+    const days = Math.floor(
+      (now.getTime() - createdAt.getTime()) / 1000 / 60 / 60 / 24,
+    )
+    return {
+      ...i,
+      opened_since:
+        days > 0 ? `${days} ${days === 1 ? 'day' : 'days'} ago` : 'today',
+    }
+  })
+
+  return (
+    <div
+      style={{
+        height: 630,
+        width: 1200,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        background:
+          'linear-gradient(140.12deg, rgba(252, 240, 236, 0.8) 6.39%, rgba(254, 253, 249, 0.8) 61.9%)',
+        overflow: 'hidden',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '50px',
+          height: '100%',
+          width: '100%',
+          gap: '50px',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: '12px',
+            fontSize: '42px',
+            color: '#727374',
+          }}
+        >
+          <img
+            src={props.avatar}
+            style={{
+              height: 48,
+              width: 48,
+              borderRadius: 48,
+            }}
+          />
+          <div
+            style={{
+              fontWeight: 'bold',
+              color: '#181A1F',
+            }}
+          >
+            {props.org_name}
+          </div>
+          <div>{`seeks backing for ${props.issue_count} ${
+            props.issue_count === 1 ? 'issue' : 'issues'
+          }`}</div>
+        </div>
+
+        {showIssues.map((i) => (
+          <div
+            key={i.id}
+            style={{
+              background: 'white',
+              width: '1080px',
+              height: '131px',
+              display: 'flex',
+              borderRadius: '24px',
+              flexDirection: 'row',
+              padding: '25px 38px',
+              boxShadow:
+                '0px 1px 8px rgba(0, 0, 0, 0.07), 0px 0.5px 2.5px rgba(0, 0, 0, 0.16)',
+              justifyContent: 'space-between',
+              gap: '40px',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+                overflow: 'hidden',
+              }}
+            >
+              <div
+                style={{
+                  fontSize: '30px',
+                  color: '#181A1F',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                {i.title}
+              </div>
+              <div
+                style={{
+                  fontSize: '26px',
+                  color: '#727374',
+                }}
+              >
+                {`#${i.number} opened ${i.opened_since}`}
+              </div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                fontSize: '24px',
+                gap: '20px',
+                color: '#808080',
+                flexShrink: 0,
+              }}
+            >
+              {}
+              {i.reactions.plus_one > 0 && (
+                <div>{`👍 ${i.reactions.plus_one}`}</div>
+              )}
+              <div
+                style={{
+                  background: '#4667CA',
+                  color: 'white',
+                  fontSize: '24px',
+                  padding: '14px 28px',
+                  borderRadius: '8px',
+                }}
+              >
+                Pledge
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div
+        style={{
+          background:
+            'linear-gradient(180deg, rgba(254, 253, 249, 0) 0%, #FEFDF9 56.95%)',
+          height: '300px',
+          width: '100%',
+          position: 'absolute',
+          top: '305px',
+        }}
+      ></div>
+
+      <img
+        style={{
+          height: '50px',
+          position: 'absolute',
+          bottom: '50px',
+        }}
+        src="http://127.0.0.1:3000/og_logotype.png"
+      />
+    </div>
+  )
+}
+
+export default OpenGraphImage
--- clients/apps/web/src/pages/api/og/render.svg.tsx
+++ clients/apps/web/src/pages/api/og/render.svg.tsx
@@ -1,7 +1,13 @@
+import OpenGraphImage from '@/components/Organization/OpenGraphImage'
 import { promises as fs } from 'fs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path'
-import { GithubBadgeRead } from 'polarkit/api/client'
+import { api } from 'polarkit'
+import {
+  IssuePublicRead,
+  OrganizationPublicPageRead,
+  Platforms,
+} from 'polarkit/api/client'
 const { default: satori } = require('satori')
 
 const readPublicFileBuffer = async (filename: string) => {
@@ -15,157 +21,22 @@ const getFontBuffer = async (name: string) => {
   return await readPublicFileBuffer(fontPath)
 }
 
-export const renderOG = async (title: string, avatar: string) => {
+export const renderOG = async (
+  org_name: string,
+  issue_count: number,
+  avatar: string,
+  issues: IssuePublicRead[],
+) => {
   const interRegular = await getFontBuffer('Inter-Regular.ttf')
   const interMedium = await getFontBuffer('Inter-Medium.ttf')
 
   const svg = await satori(
-    <div
-      style={{
-        height: 630,
-        width: 1200,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        background:
-          'linear-gradient(140.12deg, rgba(252, 240, 236, 0.8) 6.39%, rgba(254, 253, 249, 0.8) 61.9%)',
-        fontFamily: 'Inter',
-        overflow: 'hidden',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          padding: '50px',
-          paddingBottom: '150px',
-          justifyContent: 'space-between',
-          height: '100%',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: '12px',
-          }}
-        >
-          <img
-            src={avatar}
-            style={{
-              height: 48,
-              width: 48,
-              borderRadius: 48,
-            }}
-          />
-          <div
-            style={{
-              fontSize: '42px',
-              fontWeight: 'bold',
-              color: '#181A1F',
-            }}
-          >
-            Pydantic
-          </div>
-          <div
-            style={{
-              fontSize: '42px',
-              color: '#727374',
-            }}
-          >
-            seeks backing for 24 issues
-          </div>
-        </div>
-
-        {[1, 2].map((k) => (
-          <div
-            key={k}
-            style={{
-              background: 'white',
-              width: '1080px',
-              height: '131px',
-              display: 'flex',
-              borderRadius: '24px',
-              flexDirection: 'row',
-              padding: '30px 38px',
-              boxShadow:
-                '0px 1px 8px rgba(0, 0, 0, 0.07), 0px 0.5px 2.5px rgba(0, 0, 0, 0.16)',
-              justifyContent: 'space-between',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-              }}
-            >
-              <div
-                style={{
-                  fontSize: '30px',
-                  color: '#181A1F',
-                }}
-              >
-                Wrong handling of 0 in expires_atz
-              </div>
-              <div
-                style={{
-                  fontSize: '26px',
-                  color: '#727374',
-                }}
-              >
-                #8130 opened 21 days ago
-              </div>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                fontSize: '24px',
-                gap: '20px',
-                color: '#808080',
-              }}
-            >
-              <div>👍 21</div>
-              <div
-                style={{
-                  background: '#4667CA',
-                  color: 'white',
-                  fontSize: '24px',
-                  padding: '18px 28px',
-                  borderRadius: '8px',
-                }}
-              >
-                Pledge
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <div
-        style={{
-          background:
-            'linear-gradient(180deg, rgba(254, 253, 249, 0) 0%, #FEFDF9 56.95%)',
-          height: '300px',
-          width: '100%',
-          position: 'absolute',
-          top: '305px',
-        }}
-      ></div>
-
-      <img
-        style={{
-          height: '50px',
-          position: 'absolute',
-          bottom: '50px',
-        }}
-        src="http://127.0.0.1:3000/og_logotype.png"
-      />
-    </div>,
+    <OpenGraphImage
+      org_name={org_name}
+      issue_count={issue_count}
+      avatar={avatar}
+      issues={issues}
+    />,
     {
       height: 630,
       width: 1200,
@@ -189,23 +60,31 @@ export const renderOG = async (title: string, avatar: string) => {
   return svg
 }
 
+const getData = async (
+  org: string,
+  repo: string,
+): Promise<OrganizationPublicPageRead> => {
+  const res = await api.organizations.getPublicIssues({
+    platform: Platforms.GITHUB,
+    orgName: org,
+  })
+  return res
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const isDarkmode = req.query?.darkmode ? true : false
-  const amount =
-    typeof req.query?.amount === 'string' ? parseInt(req.query?.amount) : 0
+  const data = await getData('zegloforko', '')
 
-  const badge: GithubBadgeRead = {
-    badge_type: GithubBadgeRead.badge_type.PLEDGE,
-    amount,
-  }
+  console.log(data.total_issue_count)
 
   try {
     const svg = await renderOG(
-      'zegl/kube-score have 27 issues looking for funding',
-      'https://avatars.githubusercontent.com/u/47952?v=4',
+      data.organization.name,
+      data.total_issue_count,
+      data.organization.avatar_url,
+      data.issues,
     )
     res.setHeader('Content-Type', 'image/svg+xml')
     res.setHeader('Cache-Control', 'no-cache')

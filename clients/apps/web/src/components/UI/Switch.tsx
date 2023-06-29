5aa2d4fe72165b76e2b54e64e9697af54c285675
Birk Jernström <birkjernstrom@gmail.com>
2023-06-29 14:38:47 +0200

    clients/web: Add Switch component

---
+++ clients/apps/web/src/components/UI/Switch.tsx
@@ -0,0 +1,28 @@
+import * as SwitchPrimitive from '@radix-ui/react-switch'
+import { clsx } from 'polarkit/utils'
+
+interface SwitchProps {}
+
+const Switch = (props: SwitchProps) => {
+  return (
+    <SwitchPrimitive.Root
+      className={clsx(
+        'group',
+        'radix-state-checked:bg-blue-600',
+        'radix-state-unchecked:bg-gray-200 dark:radix-state-unchecked:bg-gray-800',
+        'relative inline-flex h-[24px] w-[44px] flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out',
+        'focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75',
+      )}
+    >
+      <SwitchPrimitive.Thumb
+        className={clsx(
+          'group-radix-state-checked:translate-x-5',
+          'group-radix-state-unchecked:translate-x-0',
+          'pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white ring-0 transition duration-200 ease-in-out',
+        )}
+      />
+    </SwitchPrimitive.Root>
+  )
+}
+
+export { Switch }

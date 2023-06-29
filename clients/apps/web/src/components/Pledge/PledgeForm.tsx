44849be155bddef730ae496a003a15c36298f263
Birk Jernström <birkjernstrom@gmail.com>
2023-06-29 16:19:02 +0200

    clients/app: Move pledge amount field into standalone component

--- clients/apps/web/src/components/Pledge/PledgeForm.tsx
+++ clients/apps/web/src/components/Pledge/PledgeForm.tsx
@@ -1,3 +1,4 @@
+import MoneyInput from '@/components/UI/MoneyInput'
 import { useAuth } from '@/hooks/auth'
 import { Elements } from '@stripe/react-stripe-js'
 import { PaymentIntent } from '@stripe/stripe-js'
@@ -302,23 +303,13 @@ const PledgeForm = ({
         </label>
         <div className="mt-2 flex flex-row items-center space-x-4">
           <div className="relative w-3/5">
-            <input
-              type="text"
+            <MoneyInput
               id="amount"
               name="amount"
-              className="block w-full rounded-lg border-gray-200 bg-transparent py-2 px-4 pl-7 pr-16 text-lg placeholder-gray-400 shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40"
               onChange={onAmountChange}
               onBlur={onAmountChange}
-              placeholder={getCentsInDollarString(
-                organization.pledge_minimum_amount,
-              )}
+              placeholder={organization.pledge_minimum_amount}
             />
-            <div className="pointer-events-none absolute inset-y-0 left-0 z-20 flex items-center pl-3 text-lg">
-              <span className="text-gray-500">$</span>
-            </div>
-            <div className="pointer-events-none absolute inset-y-0 right-0 z-20 flex items-center pr-4 text-sm">
-              <span className="text-gray-500">USD</span>
-            </div>
           </div>
           <p className="w-2/5 text-xs text-gray-500 dark:text-gray-400">
             Minimum is $
---
+++ clients/apps/web/src/components/UI/MoneyInput.tsx
@@ -0,0 +1,34 @@
+import { getCentsInDollarString } from 'polarkit/money'
+import { ChangeEvent } from 'react'
+
+interface Props {
+  id: string
+  name: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  placeholder: number
+  onBlur?: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+const MoneyInput = (props: Props) => {
+  return (
+    <>
+      <input
+        type="text"
+        id={props.id}
+        name={props.name}
+        className="block w-full rounded-lg border-gray-200 bg-transparent py-2 px-4 pl-7 pr-16 text-lg placeholder-gray-400 shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40"
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        placeholder={getCentsInDollarString(props.placeholder)}
+      />
+      <div className="pointer-events-none absolute inset-y-0 left-0 z-20 flex items-center pl-3 text-lg">
+        <span className="text-gray-500">$</span>
+      </div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 z-20 flex items-center pr-4 text-sm">
+        <span className="text-gray-500">USD</span>
+      </div>
+    </>
+  )
+}
+
+export default MoneyInput

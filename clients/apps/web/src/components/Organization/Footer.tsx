558e205a5b2d987acf0548619f3cb5d6fe0e1a5e
Gustav Westling <gustav@polar.sh>
2023-07-04 11:13:48 +0200

    clients/pledge: shared footer component

---
+++ clients/apps/web/src/components/Organization/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 md:flex-row ">
+      <a className="text-blue-600 hover:text-blue-500" href="/faq">
+        Polar FAQ
+      </a>
+      <span className="text-gray-500">&copy; Polar Software Inc.</span>
+    </div>
+  )
+}
+
+export default Footer

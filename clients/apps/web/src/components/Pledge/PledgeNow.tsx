8a3d1281d5d2107dfc5be0b9ecee69330107c3b8
Birk Jernström <birkjernstrom@gmail.com>
2023-07-24 12:10:38 +0200

    clients/web: Show fund vs. pledge in dependency view

--- clients/apps/web/src/components/Pledge/PledgeNow.tsx
+++ clients/apps/web/src/components/Pledge/PledgeNow.tsx
@@ -4,7 +4,7 @@ const PledgeNow = ({ onClick }: { onClick: () => void }) => {
       className="w-full cursor-pointer rounded-lg bg-blue-600 px-2 py-1 text-sm font-medium text-gray-100 transition-colors duration-200 hover:bg-blue-500"
       onClick={onClick}
     >
-      Pledge
+      Fund
     </button>
   )
 }

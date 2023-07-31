686470b1a7c5e09351b849634d3e16a93fd38ecb
Gustav Westling <gustav@polar.sh>
2023-07-31 16:22:31 +0200

    server/api: configure OpenAPI schema
    Add custom docs and logo.
    
    When serving the OpenAPI schema in production, only show endpoints tagged with "PUBLIC".
    All endpoints are returned in development mode, to support generating clients that are using non-public endpoints.
    
    Remove "PUBLIC" and "INTERNAL" tags from schema when serving.

--- server/polar/app.py
+++ server/polar/app.py
@@ -1,14 +1,17 @@
 import structlog
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
+from starlette.routing import BaseRoute
 
 from polar import receivers  # noqa
 from polar.api import router
 from polar.config import settings
-from polar.logging import configure as configure_logging
 from polar.health.endpoints import router as health_router
+from polar.logging import configure as configure_logging
 from polar.sentry import configure_sentry
+from polar.tags.api import Tags
 
 log = structlog.get_logger()
 
@@ -38,6 +41,7 @@ def create_app() -> FastAPI:
     app.include_router(health_router)
 
     app.include_router(router)
+
     return app
 
 
@@ -46,3 +50,45 @@ configure_sentry()
 
 log.info("Starting Polar API")
 app = create_app()
+
+
+def configure_openapi() -> None:
+    show_all_routes = settings.is_development()
+
+    def show(r: BaseRoute) -> bool:
+        if show_all_routes:
+            return True
+
+        if isinstance(r, APIRoute):
+            if Tags.PUBLIC in r.tags:
+                return True
+
+        return False
+
+    def format(r: BaseRoute) -> BaseRoute:
+        if isinstance(r, APIRoute):
+            # remove public/internal from tags
+            r.tags = [
+                t for t in r.tags if t is not Tags.PUBLIC and t is not Tags.INTERNAL
+            ]
+        return r
+
+    openapi_schema = get_openapi(
+        title="Polar API",
+        version="0.1.0",
+        description="""
+Welcome to the **Polar API** for [polar.sh](https://polar.sh).
+
+The Public API is currently a [work in progress](https://github.com/polarsource/polar/issues/834) and is in active development.
+
+If you have any feedback or comments, reach out in the linked issue above, or reach out on the Polar Discord server.
+""",  # noqa: E501
+        routes=[format(r) for r in app.routes if show(r)],
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://blog.polar.sh/content/images/2023/07/Frame-647--1-.png"
+    }
+    app.openapi_schema = openapi_schema
+
+
+configure_openapi()

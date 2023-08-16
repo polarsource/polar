56191380553eecef43111ffd92af8663e3acdbaf
Birk Jernström <birkjernstrom@gmail.com>
2023-08-16 10:44:34 +0200

    web/maintainer: Update navigation tests

--- clients/apps/web/src/components/Shared/DashboardTopbar.test.ts
+++ clients/apps/web/src/components/Shared/DashboardTopbar.test.ts
@@ -1,19 +1,18 @@
 import { path } from './DashboardTopbar'
 
 test('DashboardTopbar.path', () => {
-  expect(path('/issues/[organization]/[repo]', 'org')).toBe('/issues/org')
-  expect(path('/issues/[organization]', 'org', 'repo')).toBe('/issues/org/repo')
-
-  expect(path('/dependencies/[organization]/[repo]', 'org')).toBe(
-    '/dependencies/org',
+  expect(path('/maintainer/[organization]/issues?repo=[repo]', 'org')).toBe(
+    '/maintainer/org/issues',
   )
-  expect(path('/dependencies/[organization]', 'org', 'repo')).toBe(
-    '/dependencies/org/repo',
+  expect(path('/maintainer/[organization]/issues', 'org', 'repo')).toBe(
+    '/maintainer/org/issues?repo=repo',
   )
 
-  expect(path('/finance/[organization]', 'org')).toBe('/finance/org')
+  expect(path('/maintainer/[organization]/finance', 'org')).toBe(
+    '/maintainer/org/finance',
+  )
 
-  expect(path('/finance/[organization]', 'org', 'repo')).toBe(
-    '/issues/org/repo',
+  expect(path('/maintainer/[organization]/finance', 'org', 'repo')).toBe(
+    '/maintainer/org/issues?repo=repo',
   )
 })

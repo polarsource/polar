6c6a04cf5cceaf8fe32c71158a1d36a079e88d0f
Gustav Westling <gustav@polar.sh>
2023-08-18 12:05:44 +0200

    clients/navigation: use the same cmkd repo picker as on public sites

--- clients/apps/web/src/components/Organization/RepoSelection.tsx
+++ clients/apps/web/src/components/Organization/RepoSelection.tsx
@@ -1,6 +1,6 @@
 import { Command } from 'cmdk'
 import { Organization, Repository } from 'polarkit/api/client'
-import { useOutsideClick } from 'polarkit/utils'
+import { clsx, useOutsideClick } from 'polarkit/utils'
 import React, { useEffect, useRef, useState } from 'react'
 import { Item, Left, SelectedBox, Text } from '../Dropdown'
 
@@ -10,6 +10,8 @@ export function RepoSelection(props: {
   organization: Organization
   repositories: Repository[]
   value: Repository | undefined
+  selectedClassNames: string
+  openClassNames: string
 }) {
   const [value, setValue] = React.useState('')
   const inputRef = React.useRef<HTMLInputElement | null>(null)
@@ -25,7 +27,6 @@ export function RepoSelection(props: {
 
   const resetDropdown = () => {
     setOpen(false)
-    //setDropdowndropdownSelectedOrg(undefined)
     setInputValue('')
   }
 
@@ -59,27 +60,42 @@ export function RepoSelection(props: {
 
   const width = 'min-w-[320px] max-w-[500px]'
 
+  const sortedRepositories = repositories.sort((a, b) =>
+    a.name < b.name ? -1 : 1,
+  )
+
   return (
     <div
       ref={ref}
       onClick={(e) => {
         e.stopPropagation()
       }}
+      className="h-full"
     >
       {props.value && (
         <SelectedRepository
+          classNames={props.selectedClassNames}
           repository={props.value}
           onClick={() => setOpen(true)}
         />
       )}
-      {!props.value && <SelectedEmpty onClick={() => setOpen(true)} />}
+      {!props.value && (
+        <SelectedEmpty
+          classNames={props.selectedClassNames}
+          onClick={() => setOpen(true)}
+        />
+      )}
       {open && (
         <>
-          <div className={`relative bg-red-200`}>
+          <div>
             <Command
               value={value}
               onValueChange={onValueChange}
-              className={`${width} !absolute -top-10 z-10 w-max rounded-lg bg-white shadow-lg dark:border dark:border-gray-600 dark:bg-gray-700`}
+              className={clsx(
+                width,
+                props.openClassNames,
+                '!absolute -top-10 z-10 w-max rounded-lg bg-white shadow-lg dark:border dark:border-gray-600 dark:bg-gray-700',
+              )}
             >
               <div className="flex items-center px-2">
                 <Command.Input
@@ -106,8 +122,8 @@ export function RepoSelection(props: {
                   </Left>
                 </Item>
 
-                {repositories &&
-                  repositories.map((r) => (
+                {sortedRepositories &&
+                  sortedRepositories.map((r) => (
                     <Item
                       value={`${organization.name}/${r.name}`}
                       key={r.id}
@@ -132,13 +148,16 @@ export default RepoSelection
 const SelectedRepository = ({
   repository,
   onClick,
+  classNames,
 }: {
   repository: Repository
   onClick: () => void
+  classNames: string
 }) => {
   return (
-    <SelectedBox onClick={onClick}>
+    <SelectedBox onClick={onClick} classNames={classNames}>
       <div className="flex items-center justify-between space-x-2 ">
+        <RepoIcon />
         <div className="flex items-center space-x-1 overflow-hidden ">
           <span className="overflow-hidden text-ellipsis whitespace-nowrap text-gray-900 dark:text-gray-200">
             {repository.name}
@@ -149,10 +168,46 @@ const SelectedRepository = ({
   )
 }
 
-const SelectedEmpty = ({ onClick }: { onClick: () => void }) => {
+const SelectedEmpty = ({
+  onClick,
+  classNames,
+}: {
+  onClick: () => void
+  classNames: string
+}) => {
   return (
-    <SelectedBox onClick={onClick}>
-      <div className="text-gray-500">All repositories</div>
+    <SelectedBox onClick={onClick} classNames={classNames}>
+      <div className="flex items-center gap-2 text-gray-500">
+        <RepoIcon />
+        <span>All repositories</span>
+      </div>
     </SelectedBox>
+  )
+}
+
+const RepoIcon = () => {
+  return (
+    <svg
+      width="17"
+      height="18"
+      viewBox="0 0 17 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.75 14.5208C2.75 14.0512 2.93931 13.6008 3.27629 13.2687C3.61327 12.9366 4.07031 12.75 4.54688 12.75H14.25"
+        stroke="#727374"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.54688 1.75H14.25V16.25H4.54688C4.07031 16.25 3.61327 16.059 3.27629 15.7191C2.93931 15.3792 2.75 14.9182 2.75 14.4375V3.5625C2.75 3.0818 2.93931 2.62078 3.27629 2.28087C3.61327 1.94096 4.07031 1.75 4.54688 1.75Z"
+        stroke="#727374"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   )
 }

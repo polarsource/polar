c4f98348384bacde0a26b7a98185f57b09fca471
Gustav Westling <gustav@polar.sh>
2023-08-23 10:42:05 +0200

    clients/layout: fix sticky topbar

--- clients/apps/web/src/components/Layout/BackerLayout.tsx
+++ clients/apps/web/src/components/Layout/BackerLayout.tsx
@@ -5,7 +5,7 @@ import Topbar from '../Shared/Topbar'
 const BackerLayout = (props: { children: React.ReactElement }) => {
   return (
     <div className="relative flex flex-col">
-      <Topbar />
+      <Topbar isFixed={true} />
 
       <div className="dark:bg-gray-950 flex flex-col bg-gray-50 pt-16">
         <nav className="fixed z-10 w-full border-b bg-gray-50 py-3 dark:bg-gray-800">

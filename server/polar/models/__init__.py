31175cbf5202141a62d8a27ec5a84e2dab0b617c
Gustav Westling <gustav@polar.sh>
2023-08-28 09:39:21 +0200

    server/pat: add migration

--- server/polar/models/__init__.py
+++ server/polar/models/__init__.py
@@ -1,38 +1,42 @@
 from polar.kit.db.models import Model, TimestampedModel
 
 from .account import Account
+from .invites import Invite
 from .issue import Issue
+from .issue_dependency import IssueDependency
+from .issue_reference import IssueReference
+from .issue_reward import IssueReward
+from .notification import Notification
 from .organization import Organization
-from .pull_request import PullRequest
-from .repository import Repository
+from .personal_access_token import PersonalAccessToken
 from .pledge import Pledge
 from .pledge_transaction import PledgeTransaction
+from .pull_request import PullRequest
+from .repository import Repository
 from .user import OAuthAccount, User
-from .user_organization import UserOrganization
-from .issue_reference import IssueReference
-from .issue_dependency import IssueDependency
-from .notification import Notification
-from .user_organization_settings import UserOrganizationSettings
 from .user_notification import UserNotification
-from .invites import Invite
+from .user_organization import UserOrganization
+from .user_organization_settings import UserOrganizationSettings
 
 __all__ = [
-    "Model",
-    "TimestampedModel",
-    "User",
-    "OAuthAccount",
     "Account",
-    "Organization",
-    "UserOrganization",
-    "UserOrganizationSettings",
-    "UserNotification",
-    "Repository",
+    "Invite",
     "Issue",
-    "PullRequest",
+    "IssueDependency",
+    "IssueReference",
+    "IssueReward",
+    "Model",
+    "Notification",
+    "OAuthAccount",
+    "Organization",
+    "PersonalAccessToken",
     "Pledge",
     "PledgeTransaction",
-    "IssueReference",
-    "IssueDependency",
-    "Invite",
-    "Notification",
+    "PullRequest",
+    "Repository",
+    "TimestampedModel",
+    "User",
+    "UserNotification",
+    "UserOrganization",
+    "UserOrganizationSettings",
 ]

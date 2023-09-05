c2843e3db3f10a9bf8d433d4782cedab008fb9ac
François Voron <fvoron@gmail.com>
2023-09-05 09:52:36 +0200

    clients/web/dx: revamp Sentry installation

---
+++ clients/apps/web/sentry.server.config.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+import { CONFIG } from 'polarkit'
+
+Sentry.init({
+  dsn: CONFIG.SENTRY_DSN,
+  environment: CONFIG.ENVIRONMENT,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})

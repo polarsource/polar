01d5b678eed811f222c1144ec7c4632d1339b582
François Voron <fvoron@gmail.com>
2023-09-05 16:43:35 +0200

    server/dx: implement log correlation ID on API server

---
+++ server/polar/middlewares.py
@@ -0,0 +1,25 @@
+import structlog
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
+
+from polar.logging import generate_correlation_id
+
+log = structlog.get_logger()
+
+
+class LogCorrelationIdMiddleware:
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] != "http":
+            return await self.app(scope, receive, send)
+
+        structlog.contextvars.bind_contextvars(
+            correlation_id=generate_correlation_id(),
+            method=scope["method"],
+            path=scope["path"],
+        )
+
+        await self.app(scope, receive, send)
+
+        structlog.contextvars.unbind_contextvars("correlation_id", "method", "path")

b7c41c650727a5770bab7c10911e52f847cdce6c
Gustav Westling <gustav@polar.sh>
2023-09-05 11:54:11 +0200

    clients/for-you: add feature flag test

--- clients/apps/web/src/app/(backer)/for-you/page.tsx
+++ clients/apps/web/src/app/(backer)/for-you/page.tsx
@@ -4,15 +4,25 @@ import IssueListItem from '@/components/Dashboard/IssueListItem'
 import Spinner from '@/components/Shared/Spinner'
 import { useAuth } from '@/hooks'
 import { useListForYouIssues } from 'polarkit/hooks'
+import { useFeatureFlagEnabled } from 'posthog-js/react'
 
 export default function Page() {
   const { currentUser } = useAuth()
 
   const issues = useListForYouIssues()
 
+  const enabled = useFeatureFlagEnabled('for-you-test')
+
   return (
     <div className="mt-2 space-y-5">
       {issues.isLoading && <Spinner />}
+
+      {enabled && (
+        <div className="bg-green-200">you&apos;re in the feature flag</div>
+      )}
+      {!enabled && (
+        <div className="bg-red-200">you&apos;re not in the feature flag</div>
+      )}
 
       {issues.data?.items?.map((issue) => (
         <IssueListItem

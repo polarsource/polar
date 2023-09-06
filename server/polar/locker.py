eff36107c84ef6ce214a96847857973723ae9b1f
François Voron <fvoron@gmail.com>
2023-09-06 18:04:45 +0200

    server/utils: implement distributed lock service

---
+++ server/polar/locker.py
@@ -0,0 +1,48 @@
+import contextlib
+from collections.abc import AsyncGenerator
+
+import structlog
+from fastapi import Depends
+from redis.asyncio.lock import Lock
+
+from polar.logging import Logger
+from polar.redis import Redis, get_redis
+
+log: Logger = structlog.get_logger()
+
+
+class Locker:
+    def __init__(self, redis: Redis) -> None:
+        self.redis = redis
+
+    @contextlib.asynccontextmanager
+    async def lock(
+        self,
+        name: str | bytes | memoryview,
+        timeout: float | None = None,
+        sleep: float = 0.1,
+        blocking_timeout: float | None = None,
+        thread_local: bool = True,
+    ) -> AsyncGenerator[Lock, None]:
+        lock = Lock(
+            self.redis,
+            name,
+            timeout=timeout,
+            sleep=sleep,
+            blocking=True,
+            blocking_timeout=blocking_timeout,
+            thread_local=thread_local,
+        )
+
+        log.debug("try to acquire lock", name=name)
+        await lock.acquire()
+        log.debug("acquired lock", name=name)
+
+        yield lock
+
+        await lock.release()
+        log.debug("released lock", name=name)
+
+
+async def get_locker(redis: Redis = Depends(get_redis)) -> Locker:
+    return Locker(redis)

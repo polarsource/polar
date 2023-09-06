6fb0508070f8041346a892ccfde850cfde5d7730
Gustav Westling <gustav@polar.sh>
2023-09-06 15:03:06 +0200

    clients: add global and backer routes error handling

---
+++ clients/apps/web/src/app/(backer)/error.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  return (
+    <div className="flex grow flex-col items-center justify-center p-8">
+      <h2 className="text-xl">Something went wrong!</h2>
+      <a className="text-blue-500 underline" href="/feed">
+        Go back to your feed
+      </a>
+
+      <pre className="mt-12 text-sm">{error.message}</pre>
+    </div>
+  )
+}

91ed615beebf875b554f3497216e1662d5ffed6c
Gustav Westling <gustav@polar.sh>
2023-09-07 12:32:21 +0200

    clients/installation: fix layout for installation page
    And group with similar pages

--- clients/packages/polarkit/src/components/ui/atoms/MoneyInput.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/MoneyInput.tsx
@@ -37,17 +37,17 @@ const MoneyInput = (props: Props) => {
           id={id}
           name={name}
           className={classNames(
-            'block w-full rounded-lg border-gray-200 bg-transparent py-2 px-4 pl-7 pr-16 text-lg placeholder-gray-400 shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40',
+            'block w-full rounded-lg border-gray-200 bg-transparent px-4 py-2 pl-7 pr-16 text-lg placeholder-gray-400 shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40',
             props.className ?? '',
           )}
           onChange={onChange}
           placeholder={getCentsInDollarString(props.placeholder)}
           {...other}
         />
-        <div className="pointer-events-none absolute inset-y-0 left-0 z-20 flex items-center pl-3 text-lg">
+        <div className="pointer-events-none absolute inset-y-0 left-0 z-10 flex items-center pl-3 text-lg">
           <span className="text-gray-500">$</span>
         </div>
-        <div className="pointer-events-none absolute inset-y-0 right-0 z-20 flex items-center pr-4 text-sm">
+        <div className="pointer-events-none absolute inset-y-0 right-0 z-10 flex items-center pr-4 text-sm">
           <span className="text-gray-500">USD</span>
         </div>
       </div>

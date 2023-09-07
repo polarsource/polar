cb9ee8cb214597f2052f94ce3d777f8c57eb4e64
Gustav Westling <gustav@polar.sh>
2023-09-07 10:18:19 +0200

    clients/maintainer: move more into layout
    This makes the navigation between the pages smoother

--- clients/apps/web/src/app/maintainer/[organization]/issues/ClientPage.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/issues/ClientPage.tsx
@@ -1,21 +1,36 @@
 'use client'
 
-import OrganizationIssues from '@/components/Dashboard/OrganizationIssues'
+import IssueList, { Header } from '@/components/Dashboard/IssueList'
 import {
   DashboardFilters,
   DefaultFilters,
 } from '@/components/Dashboard/filters'
+import {
+  DashboardBody,
+  DashboardHeader,
+  RepoPickerHeader,
+} from '@/components/Layout/DashboardLayout'
+import OnboardingAddBadge from '@/components/Onboarding/OnboardingAddBadge'
+import OnboardingInstallChromeExtension from '@/components/Onboarding/OnboardingInstallChromeExtension'
 import { useToast } from '@/components/Toast/use-toast'
 import { useRouter, useSearchParams } from 'next/navigation'
 import {
   IssueListType,
   IssueSortBy,
   IssueStatus,
+  Label,
   Organization,
   Repository,
 } from 'polarkit/api/client'
-import { useSSE } from 'polarkit/hooks'
-import { useEffect, useRef, useState } from 'react'
+import { useDashboard, useListRepositories, useSSE } from 'polarkit/hooks'
+import {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import { useCurrentOrgAndRepoFromURL } from '../../../../hooks'
 
 export default function ClientPage() {
@@ -161,5 +176,124 @@ const Issues = ({
       orgName={org.name}
       repoName={repo?.name}
     />
+  )
+}
+
+const OrganizationIssues = ({
+  orgName,
+  repoName,
+  filters,
+  statuses,
+  onSetFilters,
+}: {
+  orgName: string
+  repoName: string | undefined
+  filters: DashboardFilters
+  statuses: Array<IssueStatus>
+  onSetFilters: Dispatch<SetStateAction<DashboardFilters>>
+}) => {
+  const dashboardQuery = useDashboard(
+    orgName,
+    repoName,
+    filters.tab,
+    filters.q,
+    statuses,
+    filters.sort,
+    filters.onlyPledged,
+    filters.onlyBadged,
+  )
+  const dashboard = dashboardQuery.data
+  const totalCount = dashboard?.pages[0].pagination.total_count || undefined
+
+  const haveIssues = useMemo(() => {
+    return totalCount !== undefined && totalCount > 0
+  }, [totalCount])
+
+  const anyIssueHasPledgeOrBadge = useMemo(() => {
+    return dashboardQuery.data?.pages.some((p) =>
+      p.data.some(
+        (issue) =>
+          issue.attributes.labels &&
+          issue.attributes.labels.some((l: Label) => l.name === 'polar'),
+      ),
+    )
+  }, [dashboardQuery])
+
+  const isDefaultFilters = useMemo(() => {
+    return (
+      filters.statusBacklog &&
+      filters.statusTriaged &&
+      filters.statusInProgress &&
+      filters.statusPullRequest &&
+      !filters.statusClosed
+    )
+  }, [filters])
+
+  const showAddBadgeBanner = useMemo(() => {
+    return (
+      filters.tab === IssueListType.ISSUES &&
+      dashboardQuery.isLoading === false &&
+      haveIssues &&
+      anyIssueHasPledgeOrBadge === false &&
+      isDefaultFilters
+    )
+  }, [
+    filters,
+    dashboardQuery,
+    anyIssueHasPledgeOrBadge,
+    haveIssues,
+    isDefaultFilters,
+  ])
+
+  // Get current org & repo from URL
+  const { org: currentOrg, repo: currentRepo } = useCurrentOrgAndRepoFromURL()
+
+  // Get all repositories
+  const listRepositoriesQuery = useListRepositories()
+  const allRepositories = listRepositoriesQuery?.data?.items
+  if (!currentOrg || !allRepositories) {
+    return <></>
+  }
+
+  // Filter repos by current org & normalize for our select
+  const allOrgRepositories = allRepositories.filter(
+    (r) => r?.organization?.id === currentOrg.id,
+  )
+
+  return (
+    <>
+      <DashboardHeader>
+        <RepoPickerHeader
+          currentRepository={currentRepo}
+          repositories={allOrgRepositories}
+        >
+          <Header
+            totalCount={totalCount}
+            filters={filters}
+            onSetFilters={onSetFilters}
+            spinner={dashboardQuery.isInitialLoading}
+          />
+        </RepoPickerHeader>
+      </DashboardHeader>
+
+      <DashboardBody>
+        <div className="space-y-4">
+          <OnboardingInstallChromeExtension />
+          {showAddBadgeBanner && <OnboardingAddBadge />}
+
+          <IssueList
+            totalCount={totalCount}
+            loading={dashboardQuery.isLoading}
+            dashboard={dashboard}
+            filters={filters}
+            onSetFilters={onSetFilters}
+            isInitialLoading={dashboardQuery.isInitialLoading}
+            isFetchingNextPage={dashboardQuery.isFetchingNextPage}
+            hasNextPage={dashboardQuery.hasNextPage || false}
+            fetchNextPage={dashboardQuery.fetchNextPage}
+          />
+        </div>
+      </DashboardBody>
+    </>
   )
 }
--- clients/apps/web/src/components/Layout/DashboardLayout.stories.tsx
+++ clients/apps/web/src/components/Layout/DashboardLayout.stories.tsx
@@ -3,7 +3,11 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from 'polarkit'
 import BackerLayout from './BackerLayout'
-import DashboardLayout, { RepoPickerHeader } from './DashboardLayout'
+import DashboardLayout, {
+  DashboardBody,
+  DashboardHeader,
+  RepoPickerHeader,
+} from './DashboardLayout'
 
 const meta: Meta<typeof DashboardLayout> = {
   title: 'Layouts/DashboardLayout',
@@ -24,13 +28,12 @@ export const Default: Story = {
     themes: ['light'],
     padding: 'p-0 m-0',
   },
-  args: {
-    showSidebar: false,
-  },
   render: (args) => (
     <QueryClientProvider client={queryClient}>
       <DashboardLayout {...args}>
-        <div className="bg-red-200 text-black">Content</div>
+        <DashboardBody>
+          <div className="bg-red-200 text-black">Content</div>
+        </DashboardBody>
       </DashboardLayout>
     </QueryClientProvider>
   ),
@@ -44,40 +47,25 @@ export const Dark: Story = {
   },
 }
 
-export const LightSidebar: Story = {
-  ...Default,
-  args: {
-    ...Default.args,
-    showSidebar: true,
-  },
-  parameters: {
-    themes: ['light'],
-    padding: 'p-0',
-  },
-}
-
-export const DarkSidebar: Story = {
-  ...Default,
-  args: {
-    ...Default.args,
-    showSidebar: true,
-  },
-  parameters: {
-    themes: ['dark'],
-    padding: 'p-0',
-  },
-}
-
 export const Header: Story = {
   ...Default,
   args: {
     ...Default.args,
-    showSidebar: true,
-    header: <div className="bg-blue-200">Hello from header</div>,
   },
   parameters: {
     themes: ['light'],
     padding: 'p-0',
+  },
+  render: (args) => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <DashboardLayout {...args}>
+          <DashboardHeader>
+            <div className="bg-blue-200">Hello from header</div>,
+          </DashboardHeader>
+        </DashboardLayout>
+      </QueryClientProvider>
+    )
   },
 }
 
@@ -85,21 +73,28 @@ export const RepoPicker: Story = {
   ...Default,
   args: {
     ...Default.args,
-    showSidebar: true,
-    header: (
-      <RepoPickerHeader currentRepository={repo} repositories={[repo]}>
-        <input
-          type="text"
-          name="query"
-          id="query"
-          className="block w-full rounded-md border-0 bg-transparent py-2 text-gray-900  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-200 dark:ring-gray-700 sm:text-sm sm:leading-6"
-          placeholder="Example"
-        />
-      </RepoPickerHeader>
-    ),
   },
   parameters: {
     themes: ['light'],
     padding: 'p-0',
+  },
+  render: (args) => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <DashboardLayout {...args}>
+          <DashboardHeader>
+            <RepoPickerHeader currentRepository={repo} repositories={[repo]}>
+              <input
+                type="text"
+                name="query"
+                id="query"
+                className="block w-full rounded-md border-0 bg-transparent py-2 text-gray-900  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-200 dark:ring-gray-700 sm:text-sm sm:leading-6"
+                placeholder="Example"
+              />
+            </RepoPickerHeader>
+          </DashboardHeader>
+        </DashboardLayout>
+      </QueryClientProvider>
+    )
   },
 }

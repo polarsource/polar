eda77c90d632e8af69de4218ec50fa3e2b83ab2c
François Voron <fvoron@gmail.com>
2023-09-13 15:11:43 +0200

    clients/web: prevent magic link authenticate request to be triggered twice

--- clients/apps/web/src/app/login/magic-link/authenticate/page.tsx
+++ clients/apps/web/src/app/login/magic-link/authenticate/page.tsx
@@ -7,7 +7,7 @@ import { useAuth } from '@/hooks'
 import { useRouter } from 'next/navigation'
 import { api } from 'polarkit'
 import { ApiError } from 'polarkit/api'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Page({
   searchParams,
@@ -18,35 +18,44 @@ export default function Page({
   const router = useRouter()
   const session = useAuth()
   const [error, setError] = useState<string | null>(null)
+  const loading = useRef(false)
 
-  const authenticate = useCallback(async () => {
+  const authenticate = async () => {
+    if (loading.current) {
+      return
+    }
+
     if (!token) {
       router.replace('/login')
-    } else {
-      try {
-        const response = await api.magicLink.authenticateMagicLink({ token })
-        if (response.success) {
-          await session
-            .login((authenticated: boolean) => {
-              if (!authenticated) {
-                setError('Something went wrong logging in')
-              }
-            })
-            .then(() => {
-              router.push(response.goto_url || '/login/init')
-            })
-        } else {
-          setError('Invalid response')
-        }
-      } catch (err) {
-        if (err instanceof ApiError) {
-          if (err.status == 401) {
-            setError(err.body.detail)
-          }
+      return
+    }
+
+    loading.current = true
+    try {
+      const response = await api.magicLink.authenticateMagicLink({ token })
+      if (response.success) {
+        await session
+          .login((authenticated: boolean) => {
+            if (!authenticated) {
+              setError('Something went wrong logging in')
+            }
+          })
+          .then(() => {
+            router.push(response.goto_url || '/login/init')
+          })
+      } else {
+        setError('Invalid response')
+      }
+    } catch (err) {
+      if (err instanceof ApiError) {
+        if (err.status == 401) {
+          setError(err.body.detail)
         }
       }
+    } finally {
+      loading.current = false
     }
-  }, [token, router, session])
+  }
 
   useEffect(() => {
     authenticate()

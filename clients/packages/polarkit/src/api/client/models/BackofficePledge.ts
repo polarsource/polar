24b90f3b28930b792f52fdad4460bb74b5de43a5
Gustav Westling <gustav@polar.sh>
2023-09-13 15:20:58 +0200

    clients: generate api

--- clients/packages/polarkit/src/api/client/models/BackofficePledge.ts
+++ clients/packages/polarkit/src/api/client/models/BackofficePledge.ts
@@ -6,6 +6,7 @@ import type { CurrencyAmount } from './CurrencyAmount';
 import type { Issue } from './Issue';
 import type { Pledger } from './Pledger';
 import type { PledgeState } from './PledgeState';
+import type { PledgeType } from './PledgeType';
 
 export type BackofficePledge = {
   /**
@@ -25,6 +26,10 @@ export type BackofficePledge = {
    */
   state: PledgeState;
   /**
+   * Type of pledge
+   */
+  type: PledgeType;
+  /**
    * If and when the pledge was refunded to the pledger
    */
   refunded_at?: string;
@@ -40,6 +45,10 @@ export type BackofficePledge = {
    * The user or organization that made this pledge
    */
   pledger?: Pledger;
+  /**
+   * URL of invoice for this pledge
+   */
+  hosted_invoice_url?: string;
   payment_id?: string;
   dispute_reason?: string;
   disputed_by_user_id?: string;
---
+++ clients/packages/polarkit/src/api/client/models/PledgeType.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * An enumeration.
+ */
+export enum PledgeType {
+  PAY_UPFRONT = 'pay_upfront',
+  PAY_ON_COMPLETION = 'pay_on_completion',
+  PAY_FROM_MAINTAINER = 'pay_from_maintainer',
+  PAY_DIRECTLY = 'pay_directly',
+}

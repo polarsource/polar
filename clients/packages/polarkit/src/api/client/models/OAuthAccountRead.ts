249bc17feeef9fce9ac8fa1ca0efb8b5720262bc
François Voron <fvoron@gmail.com>
2023-09-15 08:42:58 +0200

    clients/kit: update OpenAPI client

--- clients/packages/polarkit/src/api/client/models/OAuthAccountRead.ts
+++ clients/packages/polarkit/src/api/client/models/OAuthAccountRead.ts
@@ -5,6 +5,8 @@
 import type { Platforms } from './Platforms';
 
 export type OAuthAccountRead = {
+  created_at: string;
+  modified_at: string;
   platform: Platforms;
   account_id: string;
   account_email: string;

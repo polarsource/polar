8903784c858718d34a340156e4ff1ed45924c596
Gustav Westling <gustav@polar.sh>
2023-09-15 10:38:36 +0200

    clients/pledge: add payment method tabs
    Feature flagged

--- clients/apps/web/src/components/Pledge/PledgeForm.tsx
+++ clients/apps/web/src/components/Pledge/PledgeForm.tsx
@@ -1,5 +1,10 @@
 import { useAuth } from '@/hooks/auth'
-import { EnvelopeIcon } from '@heroicons/react/24/outline'
+import {
+  ClockIcon,
+  CurrencyDollarIcon,
+  EnvelopeIcon,
+  GiftIcon,
+} from '@heroicons/react/24/outline'
 import { Elements } from '@stripe/react-stripe-js'
 import { PaymentIntent } from '@stripe/stripe-js'
 import { loadStripe } from '@stripe/stripe-js/pure'
@@ -20,6 +25,7 @@ import { getCentsInDollarString } from 'polarkit/money'
 import { classNames } from 'polarkit/utils'
 import posthog from 'posthog-js'
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 import {
   Select,
   SelectContent,
@@ -27,6 +33,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs'
 import PaymentForm from './PaymentForm'
 import { prettyCardName } from './payment'
 
@@ -73,6 +80,107 @@ const generateRedirectURL = (
 }
 
 const PledgeForm = ({
+  issue,
+  gotoURL,
+  onAmountChange: onAmountChangeProp,
+}: {
+  issue: Issue
+  gotoURL?: string
+  onAmountChange?: (amount: number) => void
+}) => {
+  const showFundingMethodTabs = true
+
+  return (
+    <>
+      <form className="flex flex-col">
+        {showFundingMethodTabs && (
+          <label
+            htmlFor="funding_method"
+            className="mb-2 text-sm font-medium text-gray-500 dark:text-gray-400"
+          >
+            Funding method
+          </label>
+        )}
+
+        <Tabs defaultValue="fund_today" className="">
+          <TabsList
+            className={twMerge(
+              'grid h-fit w-full grid-cols-1 grid-rows-3',
+              showFundingMethodTabs ? '' : 'hidden',
+            )}
+          >
+            <FundingMethodTab
+              value="fund_today"
+              icon={<CurrencyDollarIcon className="h-6 w-6 text-gray-600" />}
+              title="Fund today"
+              subtitle="Paid today. Held by Polar until completion."
+            />
+            <FundingMethodTab
+              value="fund_on_completion"
+              icon={<ClockIcon className="h-6 w-6 text-gray-600" />}
+              title="Fund on completion"
+              subtitle="Get an invoice when the issue is completed."
+            />
+            <FundingMethodTab
+              value="gift_today"
+              icon={<GiftIcon className="h-6 w-6 text-gray-600" />}
+              title="Gift today"
+              subtitle="No strings attached. A gift & vote in one."
+            />
+          </TabsList>
+          <TabsContent value="fund_today">
+            <FundToday
+              issue={issue}
+              gotoURL={gotoURL}
+              onAmountChange={onAmountChangeProp}
+            />
+          </TabsContent>
+          <TabsContent value="fund_on_completion">
+            fund_on_completion
+          </TabsContent>
+          <TabsContent value="gift_today">gift_today</TabsContent>
+        </Tabs>
+
+        <p className="mt-5 text-sm text-gray-600">
+          By funding this issue, you agree to our{' '}
+          <Link href="https://polar.sh/legal/terms" className="underline">
+            Terms of Service
+          </Link>{' '}
+          and understand our{' '}
+          <Link href="https://polar.sh/legal/privacy" className="underline">
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </form>
+    </>
+  )
+}
+export default PledgeForm
+
+const FundingMethodTab = ({
+  value,
+  icon,
+  title,
+  subtitle,
+}: {
+  value: string
+  icon: React.ReactElement
+  title: string
+  subtitle: string
+}) => (
+  <TabsTrigger value={value}>
+    <div className="flex w-full items-center gap-4 px-1 text-left">
+      {icon}
+      <div>
+        <div className='font-medium" text-sm text-gray-700'>{title}</div>
+        <div className="text-xs font-normal text-gray-600">{subtitle}</div>
+      </div>
+    </div>
+  </TabsTrigger>
+)
+
+const FundToday = ({
   issue,
   gotoURL,
   onAmountChange: onAmountChangeProp,
@@ -354,199 +462,179 @@ const PledgeForm = ({
   }, [savedPaymentMethods.isFetched, savedPaymentMethods.data])
 
   return (
-    <>
-      <form className="flex flex-col">
-        <label
-          htmlFor="amount"
-          className="text-sm font-medium text-gray-500 dark:text-gray-400"
-        >
-          Funding amount
-        </label>
-        <div className="mt-2 flex flex-row items-center space-x-4">
-          <MoneyInput
-            id="amount"
-            name="amount"
-            onChange={onAmountChange}
-            onBlur={onAmountChange}
-            placeholder={organization.pledge_minimum_amount}
-            value={amount}
-            onFocus={(event) => {
-              event.target.select()
-            }}
-          />
-          <p
-            className={classNames(
-              amount < organization.pledge_minimum_amount ? 'text-red-500' : '',
-              'w-2/5 text-xs text-gray-500 dark:text-gray-400',
-            )}
-          >
-            Minimum is $
-            {getCentsInDollarString(organization.pledge_minimum_amount)}
-          </p>
-        </div>
-
-        <label
-          htmlFor="email"
-          className="mb-2 mt-4 text-sm font-medium text-gray-500 dark:text-gray-400"
-        >
-          Contact details
-        </label>
-        <div className="relative">
-          <input
-            type="email"
-            id="email"
-            onChange={onEmailChange}
-            onBlur={onEmailChange}
-            value={email}
-            className="block w-full rounded-lg border-gray-200 bg-transparent px-3 py-2.5 pl-10 text-sm shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40"
-            onFocus={(event) => {
-              event.target.select()
-            }}
-          />
-          <div className="pointer-events-none absolute inset-y-0 left-0 z-20 flex items-center pl-3 text-lg">
-            <span className="text-gray-500">
-              <EnvelopeIcon className="h-6 w-6" />
-            </span>
-          </div>
-        </div>
-
-        {savedPaymentMethods.data?.items &&
-          savedPaymentMethods.data?.items?.length > 0 && (
-            <>
-              <label
-                htmlFor="payment_method"
-                className="mb-2 mt-4 text-sm font-medium text-gray-500 dark:text-gray-400"
-              >
-                Payment method
-              </label>
-
-              <Select
-                onValueChange={onPaymentMethodChange}
-                name="payment_method"
-              >
-                <SelectTrigger className="w-full">
-                  {paymentMethod ? (
-                    <SelectValue
-                      placeholder={`${prettyCardName(
-                        paymentMethod.brand,
-                      )} (****${paymentMethod.last4}) 
-                    ${paymentMethod.exp_month.toString().padStart(2, '0')}/${
-                        paymentMethod.exp_year
-                      }`}
-                    />
-                  ) : (
-                    <SelectValue placeholder="new" />
-                  )}
-                </SelectTrigger>
-
-                <SelectContent>
-                  {savedPaymentMethods.data.items.map((pm) => (
-                    <SelectItem value={pm.stripe_payment_method_id}>
-                      {prettyCardName(pm.brand)} (****{pm.last4}){' '}
-                      {pm.exp_month.toString().padStart(2, '0')}/{pm.exp_year}
-                    </SelectItem>
-                  ))}
-                  <SelectItem value="new">+ New payment method</SelectItem>
-                </SelectContent>
-              </Select>
-            </>
+    <div className="flex flex-col">
+      <label
+        htmlFor="amount"
+        className="mb-2 text-sm font-medium text-gray-500 dark:text-gray-400"
+      >
+        Funding amount
+      </label>
+      <div className="flex flex-row items-center space-x-4">
+        <MoneyInput
+          id="amount"
+          name="amount"
+          onChange={onAmountChange}
+          onBlur={onAmountChange}
+          placeholder={organization.pledge_minimum_amount}
+          value={amount}
+          onFocus={(event) => {
+            event.target.select()
+          }}
+        />
+        <p
+          className={classNames(
+            amount < organization.pledge_minimum_amount ? 'text-red-500' : '',
+            'w-2/5 text-xs text-gray-500 dark:text-gray-400',
           )}
+        >
+          Minimum is $
+          {getCentsInDollarString(organization.pledge_minimum_amount)}
+        </p>
+      </div>
 
-        {showStripeForm && polarPaymentIntent && (
-          <Elements
-            stripe={stripePromise}
-            options={{
-              clientSecret: polarPaymentIntent.client_secret,
-              appearance: {
-                rules: {
-                  '.Label': {
-                    color: resolvedTheme === 'dark' ? '#A3A3A3' : '#727374',
-                    fontWeight: '500',
-                    fontSize: '14px',
-                    marginBottom: '8px',
-                  },
-                  '.Input': {
-                    padding: '12px',
-                    backgroundColor: 'transparent',
-                    borderColor:
-                      resolvedTheme === 'dark' ? '#505153' : '#E5E5E1',
-                    color: resolvedTheme === 'dark' ? '#E5E5E1' : '#181A1F',
-                  },
-                  '.Input:focus': {
-                    borderColor:
-                      resolvedTheme === 'dark' ? '#4667CA' : '#A5C2EB',
-                  },
+      <label
+        htmlFor="email"
+        className="mb-2 mt-4 text-sm font-medium text-gray-500 dark:text-gray-400"
+      >
+        Contact details
+      </label>
+      <div className="relative">
+        <input
+          type="email"
+          id="email"
+          onChange={onEmailChange}
+          onBlur={onEmailChange}
+          value={email}
+          className="block w-full rounded-lg border-gray-200 bg-transparent px-3 py-2.5 pl-10 text-sm shadow-sm focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:border-gray-600 dark:focus:border-blue-600 dark:focus:ring-blue-700/40"
+          onFocus={(event) => {
+            event.target.select()
+          }}
+        />
+        <div className="pointer-events-none absolute inset-y-0 left-0 z-20 flex items-center pl-3 text-lg">
+          <span className="text-gray-500">
+            <EnvelopeIcon className="h-6 w-6" />
+          </span>
+        </div>
+      </div>
+
+      {savedPaymentMethods.data?.items &&
+        savedPaymentMethods.data?.items?.length > 0 && (
+          <>
+            <label
+              htmlFor="payment_method"
+              className="mb-2 mt-4 text-sm font-medium text-gray-500 dark:text-gray-400"
+            >
+              Payment method
+            </label>
+
+            <Select onValueChange={onPaymentMethodChange} name="payment_method">
+              <SelectTrigger className="w-full">
+                {paymentMethod ? (
+                  <SelectValue
+                    placeholder={`${prettyCardName(paymentMethod.brand)} (****${
+                      paymentMethod.last4
+                    }) 
+                    ${paymentMethod.exp_month.toString().padStart(2, '0')}/${
+                      paymentMethod.exp_year
+                    }`}
+                  />
+                ) : (
+                  <SelectValue placeholder="new" />
+                )}
+              </SelectTrigger>
+
+              <SelectContent>
+                {savedPaymentMethods.data.items.map((pm) => (
+                  <SelectItem value={pm.stripe_payment_method_id}>
+                    {prettyCardName(pm.brand)} (****{pm.last4}){' '}
+                    {pm.exp_month.toString().padStart(2, '0')}/{pm.exp_year}
+                  </SelectItem>
+                ))}
+                <SelectItem value="new">+ New payment method</SelectItem>
+              </SelectContent>
+            </Select>
+          </>
+        )}
+
+      {showStripeForm && polarPaymentIntent && (
+        <Elements
+          stripe={stripePromise}
+          options={{
+            clientSecret: polarPaymentIntent.client_secret,
+            appearance: {
+              rules: {
+                '.Label': {
+                  color: resolvedTheme === 'dark' ? '#A3A3A3' : '#727374',
+                  fontWeight: '500',
+                  fontSize: '14px',
+                  marginBottom: '8px',
                 },
-                variables: {
-                  borderRadius: '8px',
-                  fontFamily: '"Inter var", Inter, sans-serif',
-                  fontSizeBase: '14px',
-                  spacingGridRow: '18px',
-                  colorDanger: resolvedTheme === 'dark' ? '#F17878' : '#E64D4D',
+                '.Input': {
+                  padding: '12px',
+                  backgroundColor: 'transparent',
+                  borderColor: resolvedTheme === 'dark' ? '#505153' : '#E5E5E1',
+                  color: resolvedTheme === 'dark' ? '#E5E5E1' : '#181A1F',
+                },
+                '.Input:focus': {
+                  borderColor: resolvedTheme === 'dark' ? '#4667CA' : '#A5C2EB',
                 },
               },
-              fonts: [
-                {
-                  cssSrc:
-                    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500',
-                },
-              ],
-            }}
+              variables: {
+                borderRadius: '8px',
+                fontFamily: '"Inter var", Inter, sans-serif',
+                fontSizeBase: '14px',
+                spacingGridRow: '18px',
+                colorDanger: resolvedTheme === 'dark' ? '#F17878' : '#E64D4D',
+              },
+            },
+            fonts: [
+              {
+                cssSrc:
+                  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500',
+              },
+            ],
+          }}
+        >
+          <PaymentForm
+            paymentIntent={polarPaymentIntent}
+            issue={issue}
+            organization={organization}
+            repository={repository}
+            isSyncing={isSyncing}
+            setSyncing={setSyncing}
+            setErrorMessage={setErrorMessage}
+            onSuccess={onStripePaymentSuccess}
+            canSavePaymentMethod={currentUser !== undefined}
+            paymentMethod={paymentMethod}
+            onSavePaymentMethodChanged={onSavePaymentMethodChanged}
+            hasDetails={hasValidDetails()}
+            redirectTo={generateRedirectURL(gotoURL)}
+          />
+        </Elements>
+      )}
+
+      {/*
+       * Unfortunately, we need to have this button (disabled) by default and then
+       * remove it once Stripe is initiated. Since we cannot (in an easy/nice way)
+       * manage the submission outside of the Stripe Elements context.
+       */}
+      {!showStripeForm && (
+        <div className="mt-6">
+          <PrimaryButton
+            disabled={true}
+            loading={isSyncing}
+            onClick={() => false}
           >
-            <PaymentForm
-              paymentIntent={polarPaymentIntent}
-              issue={issue}
-              organization={organization}
-              repository={repository}
-              isSyncing={isSyncing}
-              setSyncing={setSyncing}
-              setErrorMessage={setErrorMessage}
-              onSuccess={onStripePaymentSuccess}
-              canSavePaymentMethod={currentUser !== undefined}
-              paymentMethod={paymentMethod}
-              onSavePaymentMethodChanged={onSavePaymentMethodChanged}
-              hasDetails={hasValidDetails()}
-              redirectTo={generateRedirectURL(gotoURL)}
-            />
-          </Elements>
-        )}
+            Fund this issue
+          </PrimaryButton>
+        </div>
+      )}
 
-        {/*
-         * Unfortunately, we need to have this button (disabled) by default and then
-         * remove it once Stripe is initiated. Since we cannot (in an easy/nice way)
-         * manage the submission outside of the Stripe Elements context.
-         */}
-        {!showStripeForm && (
-          <div className="mt-6">
-            <PrimaryButton
-              disabled={true}
-              loading={isSyncing}
-              onClick={() => false}
-            >
-              Fund this issue
-            </PrimaryButton>
-          </div>
-        )}
-
-        {errorMessage && (
-          <div className="mt-3.5 text-red-500 dark:text-red-400">
-            {errorMessage}
-          </div>
-        )}
-
-        <p className="mt-5 text-sm text-gray-600">
-          By funding this issue, you agree to our{' '}
-          <Link href="https://polar.sh/legal/terms" className="underline">
-            Terms of Service
-          </Link>{' '}
-          and understand our{' '}
-          <Link href="https://polar.sh/legal/privacy" className="underline">
-            Privacy Policy
-          </Link>
-          .
-        </p>
-      </form>
-    </>
+      {errorMessage && (
+        <div className="mt-3.5 text-red-500 dark:text-red-400">
+          {errorMessage}
+        </div>
+      )}
+    </div>
   )
 }
-export default PledgeForm

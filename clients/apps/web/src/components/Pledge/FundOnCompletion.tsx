6d45b9d3287b023943555d159e51909f8821e112
Gustav Westling <gustav@polar.sh>
2023-09-19 10:36:36 +0200

    clients/pledge: add fund on completion steps

--- clients/apps/web/src/components/Pledge/FundOnCompletion.tsx
+++ clients/apps/web/src/components/Pledge/FundOnCompletion.tsx
@@ -1,5 +1,9 @@
 import { useAuth } from '@/hooks/auth'
-import { BellIcon, UserCircleIcon } from '@heroicons/react/24/outline'
+import {
+  BellIcon,
+  ClockIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline'
 import { useRouter } from 'next/navigation'
 import { api } from 'polarkit/api'
 import { Issue } from 'polarkit/api/client'
@@ -9,6 +13,7 @@ import { getCentsInDollarString } from 'polarkit/money'
 import { classNames } from 'polarkit/utils'
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import GithubLoginButton from '../Shared/GithubLoginButton'
+import { Checkbox } from '../ui/checkbox'
 
 const FundOnCompletion = ({
   issue,
@@ -47,9 +52,12 @@ const FundOnCompletion = ({
 
   const [isLoading, setIsLoading] = useState(false)
 
+  const [paymentPromise, setPaymentPromise] = useState(false)
+
   const hasValidDetails =
     amount >= issue.repository.organization.pledge_minimum_amount &&
-    !!currentUser
+    !!currentUser &&
+    paymentPromise
 
   const submit = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
@@ -73,38 +81,62 @@ const FundOnCompletion = ({
   }
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-4">
       {!currentUser && <NotLoggedInBanner />}
 
-      <label
-        htmlFor="amount"
-        className="mb-2 text-sm font-medium text-gray-500 dark:text-gray-400"
-      >
-        Funding amount
-      </label>
-      <div className="flex flex-row items-center space-x-4">
-        <MoneyInput
-          id="amount"
-          name="amount"
-          onChange={onAmountChange}
-          placeholder={organization.pledge_minimum_amount}
-          value={amount}
-          onFocus={(event) => {
-            event.target.select()
-          }}
-        />
-        <p
-          className={classNames(
-            amount < organization.pledge_minimum_amount ? 'text-red-500' : '',
-            'w-2/5 text-xs text-gray-500 dark:text-gray-400',
-          )}
+      <div>
+        <label
+          htmlFor="amount"
+          className="mb-2 text-sm font-medium text-gray-500 dark:text-gray-400"
         >
-          Minimum is $
-          {getCentsInDollarString(organization.pledge_minimum_amount)}
-        </p>
+          Amount
+        </label>
+        <div className="flex flex-row items-center space-x-4">
+          <MoneyInput
+            id="amount"
+            name="amount"
+            onChange={onAmountChange}
+            placeholder={organization.pledge_minimum_amount}
+            value={amount}
+            onFocus={(event) => {
+              event.target.select()
+            }}
+          />
+          <p
+            className={classNames(
+              amount < organization.pledge_minimum_amount ? 'text-red-500' : '',
+              'w-2/5 text-xs text-gray-500 dark:text-gray-400',
+            )}
+          >
+            Minimum is $
+            {getCentsInDollarString(organization.pledge_minimum_amount)}
+          </p>
+        </div>
       </div>
 
-      <div className="mt-6">
+      <NextSteps />
+
+      <hr />
+
+      <div className="items-top flex items-center space-x-2">
+        <Checkbox
+          id="payment_promise"
+          onCheckedChange={(e) => setPaymentPromise(Boolean(e))}
+        />
+        <div className="grid gap-0 leading-none">
+          <label
+            htmlFor="payment_promise"
+            className="text-xs font-medium text-gray-500 dark:text-gray-400"
+          >
+            I promise to pay once the issue is completed
+          </label>
+          <p className="text-muted-foreground text-xs">
+            Unless I have a legitimate dispute
+          </p>
+        </div>
+      </div>
+
+      <div className="">
         <PrimaryButton
           disabled={!hasValidDetails}
           loading={isLoading}
@@ -127,12 +159,12 @@ export default FundOnCompletion
 
 const NotLoggedInBanner = () => {
   return (
-    <div className="my-4 flex flex-col gap-4 rounded-lg border border-red-200 px-4 py-3 dark:border-red-700 dark:bg-gray-900">
+    <div className="flex flex-col gap-4 rounded-lg border border-red-200 px-4 py-4 dark:border-red-700 dark:bg-gray-900">
       <div className="flex items-center justify-between">
-        <div className="text-lg font-medium text-gray-700 dark:text-gray-400">
+        <div className="text-base font-medium text-gray-700 dark:text-gray-400">
           Sign in with GitHub
         </div>
-        <div className="rounded-sm border border-red-200 bg-red-50 px-2 text-red-500 dark:border-red-700 dark:bg-red-900">
+        <div className="rounded-sm border border-red-200 bg-red-50 px-1 text-xs text-red-500 dark:border-red-700 dark:bg-red-900">
           Required
         </div>
       </div>
@@ -140,19 +172,19 @@ const NotLoggedInBanner = () => {
       <div className="flex flex-col gap-2">
         <div className="flex items-center gap-2">
           <UserCircleIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-          <div className="text-gray-600 dark:text-gray-500">
+          <div className="text-xs font-semibold text-gray-600 dark:text-gray-500">
             Show profile in connection with funding
           </div>
         </div>
         <div className="flex items-center gap-2">
           <BellIcon className="h-5 w-5 text-gray-500" />
-          <div className="text-sm text-gray-600 dark:text-gray-500">
-            + Track funding on progress
+          <div className="text-xs text-gray-600 dark:text-gray-500">
+            + Track funding on funded issue
           </div>
         </div>
         <div className="flex items-center gap-2">
           <LogoIcon className="h-5 w-5" />
-          <div className="text-sm text-gray-600 dark:text-gray-500">
+          <div className="text-xs text-gray-600 dark:text-gray-500">
             + Polar account for future funding & rewards
           </div>
         </div>
@@ -165,3 +197,43 @@ const NotLoggedInBanner = () => {
     </div>
   )
 }
+
+const NextSteps = () => (
+  <div className="flex flex-col gap-3 text-sm text-gray-600  dark:text-gray-400 ">
+    <div className="flex items-center gap-4 font-medium ">
+      <ClockIcon className="h-6 w-6 text-gray-600 dark:text-gray-400" />
+      <div>
+        <div className="text-gray-600 dark:text-gray-300">
+          Fund on completion
+        </div>
+        <div className="text-xs font-light text-gray-500 dark:text-gray-400">
+          Payment terms and how it works
+        </div>
+      </div>
+    </div>
+    <div className="flex items-center gap-4">
+      <div className="m-0.5 flex h-5 w-5 items-center justify-around rounded-full border border-gray-200 dark:border-gray-500">
+        <span className="text-sm">1</span>
+      </div>
+      <div>Pledge amount today</div>
+    </div>
+    <div className="flex items-center gap-4">
+      <div className="m-0.5 flex h-5 w-5 items-center justify-around rounded-full border border-gray-200 dark:border-gray-500">
+        <span className="text-sm">2</span>
+      </div>
+      <div>Get progress updates</div>
+    </div>
+    <div className="flex items-center gap-4">
+      <div className="m-0.5 flex h-5 w-5 items-center justify-around rounded-full border border-gray-200 dark:border-gray-500">
+        <span className="text-sm">3</span>
+      </div>
+      <div>Receive invoice once issue is completed</div>
+    </div>
+    <div className="flex items-center gap-4">
+      <div className="m-0.5 flex h-5 w-5 items-center justify-around rounded-full border border-gray-200 dark:border-gray-500">
+        <span className="text-sm">4</span>
+      </div>
+      <div>Pay invoice within 7 days</div>
+    </div>
+  </div>
+)

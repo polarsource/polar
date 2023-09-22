9dade3d0f51820ce0f596e5fdb09b19689df3ff5
Gustav Westling <gustav@polar.sh>
2023-09-22 10:14:14 +0200

    clients/badge: use pledge summary API

---
+++ clients/packages/polarkit/src/api/client/models/SummaryPledge.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { PledgeType } from './PledgeType';
+
+export type SummaryPledge = {
+  avatar_url?: string;
+  /**
+   * Type of pledge
+   */
+  type: PledgeType;
+};
+

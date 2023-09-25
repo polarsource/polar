2d16fde79154a9272993927b4807a8b65e57b27f
François Voron <fvoron@gmail.com>
2023-09-25 16:49:25 +0200

    clients/kit: update OpenAPI client

--- clients/packages/polarkit/src/api/client/models/SummaryPledge.ts
+++ clients/packages/polarkit/src/api/client/models/SummaryPledge.ts
@@ -2,13 +2,14 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { Pledger } from './Pledger';
 import type { PledgeType } from './PledgeType';
 
 export type SummaryPledge = {
-  avatar_url?: string;
   /**
    * Type of pledge
    */
   type: PledgeType;
+  pledger: Pledger;
 };
 

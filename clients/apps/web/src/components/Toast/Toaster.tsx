8f44b0513e807ef5221b245b75364d1202e1a0dd
ashharr <shaikh.ashhar6@gmail.com>
2023-09-27 19:44:01 +0530

    clients/toast: contrast fix

--- clients/apps/web/src/components/Toast/Toaster.tsx
+++ clients/apps/web/src/components/Toast/Toaster.tsx
@@ -18,7 +18,7 @@ export function Toaster() {
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
+            <div className="grid gap-1 dark:bg-gray-900 dark:text-gray-300">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>

e14356b1bb747dd3452fd0d3a9e9ba99ed35e59a
François Voron <fvoron@gmail.com>
2023-10-02 15:14:09 +0200

    server/funding: implement schema for IssueFunding

--- server/polar/funding/schemas.py
+++ server/polar/funding/schemas.py
@@ -1,7 +1,9 @@
 from pydantic import Field
 
 from polar.currency.schemas import CurrencyAmount
+from polar.issue.schemas import Issue
 from polar.kit.schemas import Schema
+from polar.pledge.schemas import Pledger
 
 
 # Public API
@@ -10,3 +12,20 @@ class Funding(Schema):
     pledges_sum: CurrencyAmount | None = Field(
         description="Sum of pledges to this isuse (including currently open pledges and pledges that have been paid out). Always in USD."  # noqa: E501
     )
+
+
+class PledgesSummary(Schema):
+    total: CurrencyAmount
+    pledgers: list[Pledger]
+
+
+class PledgesTypeSummaries(Schema):
+    pay_upfront: PledgesSummary
+    pay_on_completion: PledgesSummary
+    pay_directly: PledgesSummary
+
+
+class IssueFunding(Schema):
+    issue: Issue
+    total: CurrencyAmount
+    funding_goal: CurrencyAmount | None = None

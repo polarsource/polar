9d477536c26166c12a87cf1599f15ab1bc9a874d
Gustav Westling <gustav@polar.sh>
2023-10-03 16:50:08 +0200

    clients/feed: banner to notify about pending rewards

--- clients/apps/web/src/app/(backer)/feed/page.tsx
+++ clients/apps/web/src/app/(backer)/feed/page.tsx
@@ -8,8 +8,14 @@ import {
 import Recommended from '@/components/Feed/Recommended'
 import FundAGithubIssue from '@/components/Onboarding/FundAGithubIssue'
 import { useAuth, useGitHubAccount } from '@/hooks'
+import Link from 'next/link'
 import { IssueListType, IssueStatus } from 'polarkit/api/client'
-import { usePersonalDashboard } from 'polarkit/hooks'
+import { Banner } from 'polarkit/components/ui/molecules'
+import {
+  useListAccountsByUser,
+  useListRewardsToUser,
+  usePersonalDashboard,
+} from 'polarkit/hooks'
 
 export default function Page() {
   const { currentUser } = useAuth()
@@ -36,13 +42,38 @@ export default function Page() {
     filters.onlyPledged,
     filters.onlyBadged,
   )
+
   const dashboard = dashboardQuery.data
   const totalCount = dashboard?.pages[0].pagination.total_count ?? undefined
+
+  const rewards = useListRewardsToUser(currentUser?.id)
+  const accounts = useListAccountsByUser(currentUser?.id)
+
+  const showPendingRewardsBanner =
+    rewards.data?.items &&
+    rewards.data?.items.length > 0 &&
+    accounts.data?.items &&
+    accounts.data.items.length === 0
 
   return (
     <div className="mt-2 space-y-10">
       <div className="space-y-10 lg:px-5">
+        {showPendingRewardsBanner && (
+          <Banner color="blue">
+            <div className="flex w-full items-center justify-between">
+              <span>
+                Great news! You have pending rewards and payouts. Setup a Stripe
+                account to get paid.
+              </span>
+              <Link href="/rewards" className="whitespace-nowrap font-medium">
+                Go to Rewards
+              </Link>
+            </div>
+          </Banner>
+        )}
+
         <FundAGithubIssue />
+
         {totalCount !== undefined && totalCount > 0 && (
           <div>
             <h1 className="text-lg text-gray-900 dark:text-gray-300">

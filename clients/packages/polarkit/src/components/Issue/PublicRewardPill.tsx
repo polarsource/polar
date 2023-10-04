923c3809339bbe19f7ed971d90a9b57cd09b28f5
Gustav Westling <gustav@polar.sh>
2023-10-04 13:35:58 +0200

    clients/issue: tweak PublicRewardPill

--- clients/packages/polarkit/src/components/Issue/PublicRewardPill.tsx
+++ clients/packages/polarkit/src/components/Issue/PublicRewardPill.tsx
@@ -4,9 +4,12 @@ interface PublicRewardPillProps {
 
 const PublicRewardPill: React.FC<PublicRewardPillProps> = ({ percent }) => {
   return (
-    <div className="flex items-center gap-2 rounded-xl border border-blue-200 py-0.5 pl-2 pr-0.5 text-xs text-gray-900 dark:border-blue-800 dark:text-gray-200">
+    <div className="flex items-center gap-2 whitespace-nowrap rounded-xl border border-blue-200 py-0.5 pl-2 pr-0.5 text-xs text-gray-900 dark:border-blue-800 dark:text-gray-200">
       <div>Public Reward</div>
-      <div className="rounded-xl bg-blue-100 px-1 dark:bg-blue-700">
+      <div
+        className="rounded-xl bg-blue-100 px-1 dark:bg-blue-700"
+        style={{ fontSize: 10 }}
+      >
         {percent}%
       </div>
     </div>

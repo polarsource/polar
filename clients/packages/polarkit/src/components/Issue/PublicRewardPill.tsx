b4f136302cca65cba81848e7a6baeced085a35b6
François Voron <fvoron@gmail.com>
2023-10-04 11:05:23 +0200

    clients/kit: fix height of PublicRewardPill

--- clients/packages/polarkit/src/components/Issue/PublicRewardPill.tsx
+++ clients/packages/polarkit/src/components/Issue/PublicRewardPill.tsx
@@ -6,7 +6,7 @@ const PublicRewardPill: React.FC<PublicRewardPillProps> = ({ percent }) => {
   return (
     <div className="flex items-center gap-2 rounded-xl border border-blue-200 py-0.5 pl-2 pr-0.5 text-xs text-gray-900 dark:border-blue-800 dark:text-gray-200">
       <div>Public Reward</div>
-      <div className="rounded-xl bg-blue-100 px-1 py-0.5 dark:bg-blue-700">
+      <div className="rounded-xl bg-blue-100 px-1 dark:bg-blue-700">
         {percent}%
       </div>
     </div>

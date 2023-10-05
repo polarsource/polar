fffc401e4cf764b2dcc8888a3d76ed6acb8d17dd
Birk Jernström <birkjernstrom@gmail.com>
2023-10-05 14:49:51 +0200

    signup: Remove dashboard layout from the initial signup page

--- clients/apps/web/src/app/maintainer/page.tsx
+++ clients/apps/web/src/app/maintainer/page.tsx
@@ -1,19 +1,21 @@
 'use client'
 
 import LoadingScreen from '@/components/Dashboard/LoadingScreen'
+import { useRequireAuth } from '@/hooks'
 import { useRouter } from 'next/navigation'
 import { CONFIG } from 'polarkit/config'
 import { useListOrganizations } from 'polarkit/hooks'
 import { useEffect } from 'react'
 
 export default function Page() {
+  const { currentUser } = useRequireAuth()
   const listOrganizationsQuery = useListOrganizations()
 
   const router = useRouter()
   const orgs = listOrganizationsQuery?.data?.items
 
   useEffect(() => {
-    if (!listOrganizationsQuery.isFetched) return
+    if (!listOrganizationsQuery.isFetched || !currentUser) return
 
     // redirect to first org
     if (orgs && orgs.length > 0) {
@@ -24,13 +26,7 @@ export default function Page() {
 
     // This user have no orgs, send to GitHub for installation
     router.push(CONFIG.GITHUB_INSTALLATION_URL)
-  }, [listOrganizationsQuery, orgs, router])
+  }, [listOrganizationsQuery, orgs, router, currentUser])
 
   return <LoadingScreen animate={true}>Setting you up...</LoadingScreen>
 }
-
-// Page.getLayout = (page: ReactElement) => {
-//   return <Gatekeeper>{page}</Gatekeeper>
-// }
-
-// export default Page

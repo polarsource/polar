9172b37df5fd89fd7ef8395fa0bf7ea98de22a23
Emil Widlund <hello@emilwidlund.com>
2023-10-09 10:04:06 +0200

    clients/access-tokens: Create new Access Tokens section in settings

--- clients/apps/web/src/app/(backer)/settings/page.tsx
+++ clients/apps/web/src/app/(backer)/settings/page.tsx
@@ -1,48 +1,50 @@
 'use client'
 
+import AccessTokensSettings from '@/components/Settings/AccessTokensSettings'
 import AuthenticationSettings from '@/components/Settings/AuthenticationSettings'
 import NotificationSettings from '@/components/Settings/NotificationSettings'
 import PaymentMethodSettings from '@/components/Settings/PaymentMethodSettings'
-import { ReactElement } from 'react'
 
 export default function Page() {
   return (
     <div className="relative z-0">
       <div className="divide-y divide-gray-200 dark:divide-gray-800">
         <Section>
-          <>
-            <SectionDescription title="Payment methods" />
-            <PaymentMethodSettings />
-          </>
+          <SectionDescription title="Payment methods" />
+          <PaymentMethodSettings />
         </Section>
 
         <Section>
-          <>
-            <SectionDescription
-              title="Signin connections"
-              description="Connect external accounts for authenticating to Polar."
-            />
-            <AuthenticationSettings />
-          </>
+          <SectionDescription
+            title="Signin connections"
+            description="Connect external accounts for authenticating to Polar."
+          />
+          <AuthenticationSettings />
         </Section>
 
         <Section>
-          <>
-            <SectionDescription
-              title="Email notifications"
-              description="Polar will send emails for the notifications enabled below."
-            />
-            <NotificationSettings />
-          </>
+          <SectionDescription
+            title="Email notifications"
+            description="Polar will send emails for the notifications enabled below."
+          />
+          <NotificationSettings />
+        </Section>
+
+        <Section>
+          <SectionDescription
+            title="Access Tokens"
+            description="Manage access tokens which can be used to authenticate you with the Polar SDK."
+          />
+          <AccessTokensSettings />
         </Section>
       </div>
     </div>
   )
 }
 
-const Section = ({ children }: { children: ReactElement }) => {
+const Section = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mb-4 flex flex-col space-y-4 pt-4 xl:flex-row xl:space-y-0">
+    <div className="mb-4 flex flex-col space-x-12 space-y-4 pt-4 xl:flex-row xl:space-y-0">
       {children}
     </div>
   )
---
+++ clients/apps/web/src/components/Settings/AccessTokensSettings.tsx
@@ -0,0 +1,194 @@
+'use client'
+
+import {
+  CreatePersonalAccessTokenResponse,
+  PersonalAccessToken,
+} from 'polarkit/api/client'
+import {
+  CopyToClipboardInput,
+  Input,
+  PrimaryButton,
+  ShadowListGroup,
+  ThinButton,
+} from 'polarkit/components/ui/atoms'
+import { Banner } from 'polarkit/components/ui/molecules'
+import {
+  useCreatePersonalAccessToken,
+  useDeletePersonalAccessToken,
+  useListPersonalAccessTokens,
+  useUser,
+  useUserPreferencesMutation,
+} from 'polarkit/hooks'
+import { useEffect, useState } from 'react'
+
+export type Settings = {
+  email_newsletters_and_changelogs?: boolean
+  email_promotions_and_events?: boolean
+}
+
+const AccessTokensSettings = () => {
+  const user = useUser()
+  const mutation = useUserPreferencesMutation()
+  const [settings, setSettings] = useState<Settings>({})
+
+  useEffect(() => {
+    if (!user.data) {
+      return
+    }
+
+    setSettings({
+      email_newsletters_and_changelogs:
+        user.data?.email_newsletters_and_changelogs,
+      email_promotions_and_events: user.data?.email_promotions_and_events,
+    })
+  }, [user.data])
+
+  const [canSave, setCanSave] = useState(false)
+
+  const onUpdated = (next: Settings) => {
+    setSettings({
+      ...settings,
+      ...next,
+    })
+    setCanSave(true)
+  }
+
+  const save = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    await mutation.mutateAsync({ userUpdateSettings: settings })
+  }
+
+  if (!user.data) {
+    return <></>
+  }
+
+  return (
+    <AccessTokensBox
+      settings={settings}
+      canSave={canSave}
+      onUpdated={onUpdated}
+      isSaving={mutation.isPending}
+      save={save}
+    />
+  )
+}
+
+export default AccessTokensSettings
+
+const AccessToken = (
+  props: PersonalAccessToken & { createdTokenJWT?: string },
+) => {
+  const deleteToken = useDeletePersonalAccessToken()
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row">
+          <div className="gap-y flex flex-col">
+            <h3 className="text-md">{props.comment}</h3>
+            <p className="text-sm text-gray-500">
+              {new Date(props.created_at).toLocaleDateString()}
+            </p>
+          </div>
+        </div>{' '}
+        <div className="flex flex-row items-center space-x-4 text-gray-500 dark:text-gray-500">
+          {props.last_used_at && (
+            <span>{new Date(props.last_used_at).toLocaleDateString()}</span>
+          )}
+          <ThinButton
+            color="red"
+            onClick={async () => {
+              await deleteToken.mutateAsync({ id: props.id })
+            }}
+          >
+            <span>Revoke</span>
+          </ThinButton>
+        </div>
+      </div>
+      {props.createdTokenJWT && (
+        <>
+          <CopyToClipboardInput
+            id="access-token-jwt"
+            value={props.createdTokenJWT}
+          />
+          <Banner color="blue">
+            <span>
+              Copy the access token and save it somewhere safe. You won’t be
+              able to see it again.
+            </span>
+          </Banner>
+        </>
+      )}
+    </div>
+  )
+}
+
+export const AccessTokensBox = (props: {
+  settings: Settings
+  onUpdated: (value: Settings) => void
+  save: (event: React.MouseEvent<HTMLButtonElement>) => void
+  canSave: boolean
+  isSaving: boolean
+}) => {
+  const tokens = useListPersonalAccessTokens()
+  const createToken = useCreatePersonalAccessToken()
+  const [createdToken, setCreatedToken] =
+    useState<CreatePersonalAccessTokenResponse>()
+
+  const [accessTokenName, setAccessTokenName] = useState('')
+
+  const onCreate = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    e.stopPropagation()
+    const created = await createToken.mutateAsync({ comment: accessTokenName })
+    setCreatedToken(created)
+    setAccessTokenName('')
+  }
+
+  return (
+    <div className="flex w-full flex-col">
+      <ShadowListGroup>
+        {tokens.data?.items && tokens.data.items.length > 0 ? (
+          tokens.data?.items?.map((token) => {
+            const shouldRenderJWT = token.id === createdToken?.id
+
+            return (
+              <ShadowListGroup.Item>
+                <AccessToken
+                  {...token}
+                  createdTokenJWT={
+                    shouldRenderJWT ? createdToken?.token : undefined
+                  }
+                />
+              </ShadowListGroup.Item>
+            )
+          })
+        ) : (
+          <ShadowListGroup.Item>
+            <p className="text-gray-500 dark:text-gray-500">
+              You don&apos;t have any active Access Tokens.
+            </p>
+          </ShadowListGroup.Item>
+        )}
+        <ShadowListGroup.Item>
+          <div className="flex flex-row items-center gap-x-4">
+            <Input
+              value={accessTokenName}
+              onUpdated={setAccessTokenName}
+              id="access-token-nname"
+              name="name"
+              type="text"
+              placeholder="Name your Access Token"
+            />
+            <PrimaryButton
+              fullWidth={false}
+              onClick={onCreate}
+              disabled={accessTokenName.length < 1}
+            >
+              New Access Token
+            </PrimaryButton>
+          </div>
+        </ShadowListGroup.Item>
+      </ShadowListGroup>
+    </div>
+  )
+}

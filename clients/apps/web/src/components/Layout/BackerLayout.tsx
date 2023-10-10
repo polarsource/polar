2aa72a3fcad3398707aceaaf96109f72949aedac
Emil Widlund <hello@emilwidlund.com>
2023-10-10 10:25:50 +0200

    clients/new-layout: add topbar variant for dashboard views

--- clients/apps/web/src/components/Layout/BackerLayout.tsx
+++ clients/apps/web/src/components/Layout/BackerLayout.tsx
@@ -3,10 +3,9 @@ import { useAuth } from '@/hooks/auth'
 import { LogoType } from 'polarkit/components/brand'
 import { useStore } from 'polarkit/store'
 import { classNames } from 'polarkit/utils'
-import { useMemo } from 'react'
 import BackerConnectUpsell from '../Dashboard/BackerConnectUpsell'
 import SidebarNavigation from '../Dashboard/BackerNavigation'
-import Topbar from '../Shared/Topbar'
+import DashboardTopbar from '../Shared/DashboardTopbar'
 
 const BackerLayout = (props: {
   children: React.ReactNode
@@ -20,10 +19,6 @@ const BackerLayout = (props: {
   const showBanner =
     !isBackerConnectUpsellSkiped && !props.disableOnboardingBanner
 
-  const fixedHeight = useMemo(() => {
-    return showBanner ? 127 : 90 // BackerConnectUpsell
-  }, [showBanner])
-
   if (!hydrated) {
     return <></>
   }
@@ -33,18 +28,24 @@ const BackerLayout = (props: {
       <aside className="h-screen w-[320px] flex-shrink-0 border-r border-r-gray-100 bg-white dark:border-r-gray-800 dark:bg-gray-900">
         <a
           href="/"
-          className="mt-10 flex-shrink-0 items-center space-x-2 px-9 font-semibold text-gray-700 md:inline-flex"
+          className="mt-8 flex-shrink-0 items-center space-x-2 px-9 font-semibold text-gray-700 md:inline-flex"
         >
           <LogoType />
         </a>
         <div className="mt-8 flex px-4 py-2">
-          {currentUser && <ProfileSelection useOrgFromURL={true} />}
+          {currentUser && (
+            <ProfileSelection
+              useOrgFromURL={true}
+              className="shadow-xl"
+              narrow={false}
+            />
+          )}
         </div>
         <SidebarNavigation />
       </aside>
 
       <div className="relative flex h-screen w-full translate-x-0 flex-row bg-gray-50 dark:bg-gray-950">
-        <Topbar isFixed={true} useOrgFromURL={false} />
+        <DashboardTopbar isFixed={true} useOrgFromURL={false} />
         <nav className="fixed z-10 w-full ">
           {showBanner && <BackerConnectUpsell />}
         </nav>
@@ -55,7 +56,7 @@ const BackerLayout = (props: {
               'relative mx-auto max-w-screen-2xl px-4 sm:px-6 md:px-8',
             )}
             style={{
-              marginTop: `${fixedHeight}px`,
+              marginTop: `107px`,
             }}
           >
             {props.children}
--- clients/apps/web/src/components/Shared/DashboardTopbar.tsx
+++ clients/apps/web/src/components/Shared/DashboardTopbar.tsx
@@ -1,11 +1,97 @@
-import Topbar from './Topbar'
+'use client'
 
-const DashboardTopbar = () => {
+import { useAuth, useCurrentOrgAndRepoFromURL } from '@/hooks'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { classNames } from 'polarkit/utils'
+import { Suspense, useMemo } from 'react'
+import { Route, backerRoutes, maintainerRoutes } from '../Dashboard/navigation'
+import Popover from '../Notifications/Popover'
+
+export type LogoPosition = 'center' | 'left'
+
+const SubNav = (props: { items: (Route & { active: boolean })[] }) => {
   return (
-    <>
-      <Topbar isFixed={true} useOrgFromURL={true}></Topbar>
-    </>
+    <div className="flex flex-row items-center gap-x-2 rounded-lg bg-gray-100 p-1 dark:border-gray-800 dark:bg-gray-800">
+      {props.items.map((item) => {
+        const className = classNames(
+          item.active
+            ? 'bg-white dark:bg-gray-900 shadow-md text-gray-950 dark:text-gray-300 font-medium'
+            : 'text-gray-500 dark:gray-500 hover:text-gray-950 dark:hover:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800',
+          'dark-bg-900 flex flex-row rounded-md px-3 py-1 text-xs transition-colors',
+        )
+
+        return (
+          <Link href={item.link} className={className}>
+            {item.title}
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
+const DashboardTopbar = ({
+  hideProfile,
+  ...props
+}: {
+  useOrgFromURL: boolean
+  hideProfile?: boolean
+  isFixed?: boolean
+}) => {
+  const { org: currentOrgFromURL, isLoaded } = useCurrentOrgAndRepoFromURL()
+  const { currentUser, hydrated } = useAuth()
+
+  const useOrgFromURL = props.useOrgFromURL
+
+  const currentOrg = useMemo(() => {
+    return currentOrgFromURL && useOrgFromURL ? currentOrgFromURL : undefined
+  }, [currentOrgFromURL, useOrgFromURL])
+
+  const routes = currentOrg
+    ? maintainerRoutes(currentOrg, isLoaded)
+    : backerRoutes
+
+  const pathname = usePathname()
+
+  const [currentRoute] = routes.filter((route) =>
+    pathname?.startsWith(route.link),
+  )
+
+  const className = classNames(
+    props.isFixed !== false ? 'fixed z-20 left-0 top-0 right-0' : '',
+    'flex h-20 w-full items-center justify-between space-x-4 bg-white dark:bg-gray-900 border-b border-gray-100 dark:border-gray-800',
+  )
+
+  if (!hydrated) {
+    return <></>
+  }
+
+  return (
+    <>
+      <div className={className}>
+        <div className="relative flex w-full flex-row items-center justify-between px-4 sm:px-6 md:px-8">
+          <div className="flex flex-row items-center gap-x-24">
+            <h4 className="text-lg font-medium dark:text-gray-300">
+              {currentRoute?.title}
+            </h4>
+            {currentRoute &&
+              'subs' in currentRoute &&
+              currentRoute.subs.length > 0 && (
+                <SubNav
+                  items={currentRoute.subs.map((sub) => ({
+                    ...sub,
+                    active: sub.link === pathname,
+                  }))}
+                />
+              )}
+          </div>
+          <div className="flex flex-row items-center gap-x-6">
+            <Suspense>{currentUser && <Popover />}</Suspense>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 export default DashboardTopbar

f8a8d0fc2cb715b092e781cbda707bf79ac88b77
Emil Widlund <hello@emilwidlund.com>
2023-10-11 14:23:43 +0200

    clients/dark-mode: adjust navigation color

--- clients/apps/web/src/components/Dashboard/BackerNavigation.tsx
+++ clients/apps/web/src/components/Dashboard/BackerNavigation.tsx
@@ -27,7 +27,7 @@ const BackerNavigation = (props: { classNames?: string }) => {
               ' flex items-center gap-2 rounded-xl px-5 py-3',
               n.isActive
                 ? 'dark:bg-polar-700 bg-blue-50 text-blue-600 dark:text-blue-400'
-                : 'dark:text-polar-500 dark:hover:text-polar-200 text-gray-900 hover:text-blue-700',
+                : 'dark:text-polar-400 dark:hover:text-polar-200 text-gray-900 hover:text-blue-700',
             )}
             href={n.link}
           >

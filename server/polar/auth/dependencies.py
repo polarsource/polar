a35f683000c335d0b9f81514a589472a3bb37484
François Voron <fvoron@gmail.com>
2023-10-12 08:57:40 +0200

    server/auth: retain auth method used in Auth class

--- server/polar/auth/dependencies.py
+++ server/polar/auth/dependencies.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 from typing import Annotated, Self
 from uuid import UUID
 
@@ -13,53 +14,71 @@ from polar.postgres import AsyncSession, get_db_session
 
 from .service import AuthService
 
+
+class AuthMethod(Enum):
+    COOKIE = auto()
+    PERSONAL_ACCESS_TOKEN = auto()
+
+
 personal_access_token_scheme = HTTPBearer(
     auto_error=False,
     description="You can generate a **Personal Access Token** from your [settings](https://polar.sh/settings).",
 )
 
 
-async def get_cookie_token(request: Request) -> str | None:
+async def _get_cookie_token(request: Request) -> str | None:
     return request.cookies.get(settings.AUTH_COOKIE_KEY)
 
 
-async def current_user_optional(
-    cookie_token: str | None = Depends(get_cookie_token),
+async def _current_user_optional(
+    cookie_token: str | None = Depends(_get_cookie_token),
     personal_access_token: HTTPAuthorizationCredentials
     | None = Depends(personal_access_token_scheme),
     session: AsyncSession = Depends(get_db_session),
-) -> User | None:
+) -> tuple[User | None, AuthMethod | None]:
     if cookie_token is not None:
-        return await AuthService.get_user_from_cookie(session, cookie=cookie_token)
-    elif personal_access_token is not None:
-        return await AuthService.get_user_from_personal_access_token(
-            session, token=personal_access_token.credentials
+        return (
+            await AuthService.get_user_from_cookie(session, cookie=cookie_token),
+            AuthMethod.COOKIE,
         )
-    return None
+    elif personal_access_token is not None:
+        return (
+            await AuthService.get_user_from_personal_access_token(
+                session, token=personal_access_token.credentials
+            ),
+            AuthMethod.PERSONAL_ACCESS_TOKEN,
+        )
+    return None, None
 
 
-async def current_user_required(
-    user: User | None = Depends(current_user_optional),
-) -> User:
-    if user is None:
+async def _current_user_required(
+    user_auth_method: tuple[User | None, AuthMethod | None] = Depends(
+        _current_user_optional
+    ),
+) -> tuple[User, AuthMethod]:
+    user, auth_method = user_auth_method
+    if user is None or auth_method is None:
         raise HTTPException(status_code=401, detail="Not authenticated")
-    return user
+    return user, auth_method
 
 
 class Auth:
     subject: Subject
     user: User | None
+    auth_method: AuthMethod | None
 
     def __init__(
         self,
         *,
         subject: Subject,
         user: User | None = None,
+        auth_method: AuthMethod | None = None,
         organization: Organization | None = None,
         repository: Repository | None = None,
     ):
         self.subject = subject
         self.user = user
+        self.auth_method = auth_method
         self._organization = organization
         self._repository = repository
 
@@ -86,15 +105,22 @@ class Auth:
     ###############################################################################
 
     @classmethod
-    async def current_user(cls, user: User = Depends(current_user_required)) -> Self:
-        return cls(subject=user, user=user)
+    async def current_user(
+        cls, user_auth_method: tuple[User, AuthMethod] = Depends(_current_user_required)
+    ) -> Self:
+        user, auth_method = user_auth_method
+        return cls(subject=user, user=user, auth_method=auth_method)
 
     @classmethod
     async def optional_user(
-        cls, user: User | None = Depends(current_user_optional)
+        cls,
+        user_auth_method: tuple[User | None, AuthMethod | None] = Depends(
+            _current_user_optional
+        ),
     ) -> Self:
+        user, auth_method = user_auth_method
         if user:
-            return cls(subject=user, user=user)
+            return cls(subject=user, user=user, auth_method=auth_method)
         else:
             return cls(subject=Anonymous())
 
@@ -105,8 +131,9 @@ class Auth:
         platform: Platforms,
         org_name: str,
         session: AsyncSession = Depends(get_db_session),
-        user: User = Depends(current_user_required),
+        user_auth_method: tuple[User, AuthMethod] = Depends(_current_user_required),
     ) -> Self:
+        user, auth_method = user_auth_method
         organization = await organization_service.get_for_user(
             session,
             platform=platform,
@@ -117,7 +144,9 @@ class Auth:
             raise HTTPException(
                 status_code=404, detail="Organization not found for user"
             )
-        return cls(subject=user, user=user, organization=organization)
+        return cls(
+            subject=user, user=user, auth_method=auth_method, organization=organization
+        )
 
     @classmethod
     async def user_with_org_access_by_id(
@@ -125,8 +154,9 @@ class Auth:
         *,
         id: UUID,
         session: AsyncSession = Depends(get_db_session),
-        user: User = Depends(current_user_required),
+        user_auth_method: tuple[User, AuthMethod] = Depends(_current_user_required),
     ) -> Self:
+        user, auth_method = user_auth_method
         organization = await organization_service.get_by_id_for_user(
             session,
             org_id=id,
@@ -136,7 +166,9 @@ class Auth:
             raise HTTPException(
                 status_code=404, detail="Organization not found for user"
             )
-        return cls(subject=user, user=user, organization=organization)
+        return cls(
+            subject=user, user=user, auth_method=auth_method, organization=organization
+        )
 
     @classmethod
     async def user_with_org_and_repo_access(
@@ -146,8 +178,9 @@ class Auth:
         org_name: str,
         repo_name: str,
         session: AsyncSession = Depends(get_db_session),
-        user: User = Depends(current_user_required),
+        user_auth_method: tuple[User, AuthMethod] = Depends(_current_user_required),
     ) -> Self:
+        user, auth_method = user_auth_method
         org, repo = await organization_service.get_with_repo_for_user(
             session,
             platform=platform,
@@ -155,14 +188,21 @@ class Auth:
             repo_name=repo_name,
             user_id=user.id,
         )
-        return cls(subject=user, user=user, organization=org, repository=repo)
+        return cls(
+            subject=user,
+            user=user,
+            auth_method=auth_method,
+            organization=org,
+            repository=repo,
+        )
 
     @classmethod
     async def backoffice_user(
         cls,
         *,
-        user: User = Depends(current_user_required),
+        user_auth_method: tuple[User, AuthMethod] = Depends(_current_user_required),
     ) -> Self:
+        user, auth_method = user_auth_method
         allowed = ["zegl", "birkjernstrom", "frankie567", "emilwidlund"]
 
         if user.username not in allowed:
@@ -171,12 +211,13 @@ class Auth:
                 detail="Not Found",
             )
 
-        return cls(subject=user, user=user)
+        return cls(subject=user, user=user, auth_method=auth_method)
 
 
 class AuthRequired(Auth):
     subject: User
     user: User
+    auth_method: AuthMethod
 
 
 UserRequiredAuth = Annotated[AuthRequired, Depends(Auth.current_user)]

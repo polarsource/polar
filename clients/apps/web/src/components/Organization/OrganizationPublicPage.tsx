093fda56ac54e1f51c8f96b4c77154a7d2abfe7b
Emil Widlund <hello@emilwidlund.com>
2023-10-23 18:35:56 +0200

    clients/maintainer: polish presentational part of maintainer page

--- clients/apps/web/src/components/Organization/OrganizationPublicPage.tsx
+++ clients/apps/web/src/components/Organization/OrganizationPublicPage.tsx
@@ -1,9 +1,16 @@
 import {
+  BusinessOutlined,
+  EmailOutlined,
+  LanguageOutlined,
+  ShortTextOutlined,
+} from '@mui/icons-material'
+import {
   IssueFunding,
   Organization,
   Repository,
   SubscriptionTier,
 } from '@polar-sh/sdk'
+import { Avatar } from 'polarkit/components/ui/atoms'
 import { prettyURL } from '.'
 import HowItWorks from '../Pledge/HowItWorks'
 import PublicSubscriptionUpsell from '../Subscriptions/PublicSubscriptionUpsell'
@@ -34,44 +41,67 @@ const OrganizationPublicPage = ({
     <>
       <Header organization={organization} repositories={repositories} />
 
-      <h1 className="dark:text-polar-100 text-center text-3xl font-normal text-gray-800 md:text-3xl">
-        {organization.name} has {totalIssueCount > 0 ? totalIssueCount : 'no'}{' '}
-        {totalIssueCount === 1 ? 'issue' : 'issues'} looking for funding
-      </h1>
+      <div className="flex flex-col gap-y-8 py-12">
+        <Avatar
+          className="h-24 w-24 self-center"
+          name={organization.name}
+          avatar_url={organization.avatar_url}
+        />
 
-      {showMeta && (
-        <div className="flex flex-col items-center space-y-8">
-          {organization.bio && (
-            <div className="nowrap dark:text-polar-500 text-center text-gray-500">
-              {organization.bio}
+        <h1 className="dark:text-polar-50 text-center text-3xl font-normal text-gray-800">
+          {organization.name}
+        </h1>
+
+        {showMeta && (
+          <div className="flex flex-col items-center space-y-8">
+            {organization.bio && (
+              <div className="nowrap dark:text-polar-500 text-center text-gray-500">
+                {organization.bio}
+              </div>
+            )}
+
+            <div className="dark:text-polar-400 mt-2 flex w-full flex-wrap justify-center gap-8 text-sm text-gray-600">
+              {organization.company && (
+                <div className="flex flex-row items-center gap-x-2">
+                  <BusinessOutlined fontSize="small" />
+                  <span>{organization.company}</span>
+                </div>
+              )}
+
+              {organization.blog && (
+                <div className="flex flex-row items-center gap-x-2">
+                  <LanguageOutlined fontSize="small" />
+                  <a
+                    className="text-blue-600 hover:text-blue-700"
+                    href={organization.blog}
+                  >
+                    {prettyURL(organization.blog)}
+                  </a>
+                </div>
+              )}
+
+              {organization.email && (
+                <div className="flex flex-row items-center gap-x-2">
+                  <EmailOutlined fontSize="small" />
+                  <span>{organization.email}</span>
+                </div>
+              )}
+
+              {organization.twitter_username && (
+                <div className="flex flex-row items-center gap-x-2">
+                  <ShortTextOutlined fontSize="small" />
+                  <a
+                    className="text-blue-600 hover:text-blue-700"
+                    href={`https://twitter.com/${organization.twitter_username}`}
+                  >
+                    @{organization.twitter_username}
+                  </a>
+                </div>
+              )}
             </div>
-          )}
-
-          <div className="dark:text-polar-400 mt-2 flex w-full flex-wrap justify-center gap-4 text-sm text-gray-600">
-            {organization.company && <div>{organization.company}</div>}
-
-            {organization.blog && (
-              <a
-                className="text-blue-600 hover:text-blue-700"
-                href={organization.blog}
-              >
-                {prettyURL(organization.blog)}
-              </a>
-            )}
-
-            {organization.email && <div>{organization.email}</div>}
-
-            {organization.twitter_username && (
-              <a
-                className="text-blue-600 hover:text-blue-700"
-                href={`https://twitter.com/${organization.twitter_username}`}
-              >
-                @{organization.twitter_username}
-              </a>
-            )}
           </div>
-        </div>
-      )}
+        )}
+      </div>
 
       {subscriptionTiers.length > 0 && (
         <PublicSubscriptionUpsell
@@ -80,6 +110,11 @@ const OrganizationPublicPage = ({
           subscribePath="/subscribe"
         />
       )}
+
+      <h1 className="dark:text-polar-100 pb-8 text-center text-2xl font-normal text-gray-800">
+        {organization.name} has {totalIssueCount > 0 ? totalIssueCount : 'no'}{' '}
+        {totalIssueCount === 1 ? 'issue' : 'issues'} looking for funding
+      </h1>
 
       <IssuesLookingForFunding issuesFunding={issuesFunding} />
 

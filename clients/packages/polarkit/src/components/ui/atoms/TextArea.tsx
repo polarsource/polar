5d3bedd11ed31a1cfed9d678e1ed01593dbb478b
Emil Widlund <hello@emilwidlund.com>
2023-10-24 10:45:37 +0200

    clients/inputs: fix focusing aesthetics with inputs and textareas

--- clients/packages/polarkit/src/components/ui/atoms/TextArea.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/TextArea.tsx
@@ -7,7 +7,7 @@ export interface TextAreaProps extends TextareaProps {
 
 const TextArea = ({ resizable = true, className, ...props }: TextAreaProps) => {
   const classNames = twMerge(
-    'dark:border-polar-600 dark:placeholder:text-polar-500 min-h-[120px] rounded-lg border-gray-200 bg-transparent px-3 py-2.5 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:ring-offset-transparent dark:focus:border-blue-600 dark:focus:ring-blue-700/40',
+    'dark:border-polar-600 dark:placeholder:text-polar-500 min-h-[120px] rounded-lg border-gray-200 bg-white dark:bg-polar-800 focus-visible:ring-blue-100 p-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-300 focus:ring-[3px] focus:ring-blue-100 dark:ring-offset-transparent dark:focus:border-blue-600 dark:focus:ring-blue-700/40',
     resizable ? '' : 'resize-none',
     className,
   )

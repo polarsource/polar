75559a094f1e0985278d8d8f7b68fca44d0be5ed
François Voron <fvoron@gmail.com>
2023-10-25 17:52:18 +0200

    server/subscription: implement service for custom type benefits
    Actually, does nothing currently 🙃

--- server/polar/subscription/service/benefits/__init__.py
+++ server/polar/subscription/service/benefits/__init__.py
@@ -1,21 +1,29 @@
+from typing import Any
+
+from polar.models import SubscriptionBenefit
 from polar.models.subscription_benefit import SubscriptionBenefitType
 from polar.postgres import AsyncSession
 
 from .base import (
+    SB,
     SubscriptionBenefitGrantError,
     SubscriptionBenefitRevokeError,
     SubscriptionBenefitServiceError,
     SubscriptionBenefitServiceProtocol,
 )
+from .custom import SubscriptionBenefitCustomService
 
 _SERVICE_CLASS_MAP: dict[
-    SubscriptionBenefitType, type[SubscriptionBenefitServiceProtocol]
-] = {}
+    SubscriptionBenefitType,
+    type[SubscriptionBenefitServiceProtocol[Any]],
+] = {
+    SubscriptionBenefitType.custom: SubscriptionBenefitCustomService,
+}
 
 
 def get_subscription_benefit_service(
     type: SubscriptionBenefitType, session: AsyncSession
-) -> SubscriptionBenefitServiceProtocol:
+) -> SubscriptionBenefitServiceProtocol[SubscriptionBenefit]:
     return _SERVICE_CLASS_MAP[type](session)
 
 

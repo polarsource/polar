3f8f64b8aeadd07c2b06ab9fe1e78964d75a199c
François Voron <fvoron@gmail.com>
2023-10-30 14:58:33 +0100

    clients/web: add loading state to subscriptions overview

--- clients/apps/web/src/components/Subscriptions/OverviewPage.tsx
+++ clients/apps/web/src/components/Subscriptions/OverviewPage.tsx
@@ -4,6 +4,7 @@ import { DashboardBody } from '@/components/Layout/MaintainerLayout'
 import { ListResourceSubscriptionTier, Organization } from '@polar-sh/sdk'
 import { api } from 'polarkit'
 import { Card, CardContent, CardHeader } from 'polarkit/components/ui/card'
+import { Skeleton } from 'polarkit/components/ui/skeleton'
 import React, { useEffect, useMemo, useState } from 'react'
 import {
   MRRChart,
@@ -81,65 +82,92 @@ const OverviewPage: React.FC<OverviewPageProps> = ({
   return (
     <DashboardBody>
       <div className="flex flex-col gap-4">
-        {displayedPeriod && (
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-            <SubscribersMetric
-              data={displayedPeriod.subscribers}
-              dataDate={displayedPeriod.parsedStartDate}
-              previousData={
-                !hoveredPeriodIndex && previousPeriod
-                  ? previousPeriod.subscribers
-                  : undefined
-              }
-            />
-            <MRRMetric
-              data={displayedPeriod.mrr}
-              dataDate={displayedPeriod.parsedStartDate}
-              previousData={
-                !hoveredPeriodIndex && previousPeriod
-                  ? previousPeriod.mrr
-                  : undefined
-              }
-            />
-            <CumulativeRevenueMetric
-              data={displayedPeriod.cumulative}
-              dataDate={displayedPeriod.parsedStartDate}
-              previousData={
-                !hoveredPeriodIndex && previousPeriod
-                  ? previousPeriod.cumulative
-                  : undefined
-              }
-            />
-          </div>
-        )}
-        {summaryPeriods.length && (
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <Card>
-              <CardHeader>
-                <div className="text-xl">Subscribers</div>
-              </CardHeader>
-              <CardContent>
-                <SubscribersChart
-                  data={summaryPeriods}
-                  onDataIndexHover={setHoveredPeriodIndex}
-                  hoveredIndex={hoveredPeriodIndex}
-                />
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <div className="text-xl">Monthly recurring revenue</div>
-              </CardHeader>
-              <CardContent>
-                <MRRChart
-                  data={summaryPeriods}
-                  onDataIndexHover={setHoveredPeriodIndex}
-                  hoveredIndex={hoveredPeriodIndex}
-                />
-              </CardContent>
-            </Card>
-          </div>
-        )}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {displayedPeriod && (
+            <>
+              <SubscribersMetric
+                data={displayedPeriod.subscribers}
+                dataDate={displayedPeriod.parsedStartDate}
+                previousData={
+                  !hoveredPeriodIndex && previousPeriod
+                    ? previousPeriod.subscribers
+                    : undefined
+                }
+              />
+              <MRRMetric
+                data={displayedPeriod.mrr}
+                dataDate={displayedPeriod.parsedStartDate}
+                previousData={
+                  !hoveredPeriodIndex && previousPeriod
+                    ? previousPeriod.mrr
+                    : undefined
+                }
+              />
+              <CumulativeRevenueMetric
+                data={displayedPeriod.cumulative}
+                dataDate={displayedPeriod.parsedStartDate}
+                previousData={
+                  !hoveredPeriodIndex && previousPeriod
+                    ? previousPeriod.cumulative
+                    : undefined
+                }
+              />
+            </>
+          )}
+          {!displayedPeriod &&
+            [0, 1, 2].map((i) => (
+              <Card key={`metric-loading-${i}`}>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0">
+                  <Skeleton className="h-4 w-1/2" />
+                </CardHeader>
+                <CardContent>
+                  <Skeleton className="h-12 w-1/4" />
+                  <Skeleton className="mt-4 h-4 w-1/2" />
+                </CardContent>
+              </Card>
+            ))}
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {summaryPeriods.length > 0 && (
+            <>
+              <Card>
+                <CardHeader>
+                  <div className="text-xl">Subscribers</div>
+                </CardHeader>
+                <CardContent>
+                  <SubscribersChart
+                    data={summaryPeriods}
+                    onDataIndexHover={setHoveredPeriodIndex}
+                    hoveredIndex={hoveredPeriodIndex}
+                  />
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <div className="text-xl">Monthly recurring revenue</div>
+                </CardHeader>
+                <CardContent>
+                  <MRRChart
+                    data={summaryPeriods}
+                    onDataIndexHover={setHoveredPeriodIndex}
+                    hoveredIndex={hoveredPeriodIndex}
+                  />
+                </CardContent>
+              </Card>
+            </>
+          )}
+          {summaryPeriods.length === 0 &&
+            [0, 1].map((i) => (
+              <Card key={`chart-loading-${i}`}>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0">
+                  <Skeleton className="h-4 w-1/2" />
+                </CardHeader>
+                <CardContent>
+                  <Skeleton className="h-[300px]" />
+                </CardContent>
+              </Card>
+            ))}
+        </div>
       </div>
     </DashboardBody>
   )

c0dd81d2656a65003292cc908b4ddb6898481ce3
Emil Widlund <hello@emilwidlund.com>
2023-10-31 13:16:32 +0100

    clients/teams: make teams items in nav the same as them above

--- clients/apps/web/src/components/Dashboard/TeamsNavigation.tsx
+++ clients/apps/web/src/components/Dashboard/TeamsNavigation.tsx
@@ -2,11 +2,13 @@ import { useAuth } from '@/hooks'
 import { isFeatureEnabled } from '@/utils/feature-flags'
 import { Organization } from '@polar-sh/sdk'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { Avatar } from 'polarkit/components/ui/atoms'
 import {
   useListAllOrganizations,
   useListOrganizationMembers,
 } from 'polarkit/hooks'
+import { twMerge } from 'tailwind-merge'
 
 const TeamsNavigation = () => {
   const { currentUser, hydrated } = useAuth()
@@ -46,24 +48,33 @@ const TeamsNavigation = () => {
 export default TeamsNavigation
 
 const Team = ({ org }: { org: Organization }) => {
+  const pathname = usePathname()
   const members = useListOrganizationMembers(org.id)
+  const isActive = pathname.startsWith(`/team/${org.name.toLowerCase()}`)
 
   return (
     <Link
-      className="dark:bg-polar-700 dark:text-polar-300 dark:hover:bg-polar-800  flex items-center gap-4 rounded-xl bg-blue-50 px-4 py-2 text-sm text-gray-900 transition duration-100 hover:bg-blue-100"
+      className={twMerge(
+        'flex w-full items-center gap-x-4 rounded-xl border border-transparent px-5 py-3 text-sm transition-colors',
+        isActive
+          ? 'dark:bg-polar-800 dark:border-polar-700 bg-blue-50 text-blue-600 dark:text-blue-500'
+          : 'dark:text-polar-500 dark:hover:text-polar-200 text-gray-900 hover:text-blue-700',
+      )}
       href={`/team/${org.name}/funding`}
     >
-      <div className=" flex flex-1 items-center gap-4">
+      <div className="flex w-full min-w-0 flex-1 items-center gap-4">
         <Avatar
           avatar_url={org.avatar_url}
           name={org.name}
           className="h-8 w-8"
         />
-        <span>{org.name}</span>
+        <span className="shrink grow-0 truncate">
+          {org.name}asdnakjdsna kjnsdkajn kdajsn ka
+        </span>
       </div>
 
-      <div className=" flex flex-shrink-0 items-center -space-x-2.5">
-        {members.data?.items?.slice(0, 5).map((m) => (
+      <div className="flex flex-shrink-0 items-center -space-x-2.5">
+        {members.data?.items?.slice(0, 3).map((m) => (
           <Avatar
             avatar_url={m.avatar_url}
             name={m.name}

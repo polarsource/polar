e6fe90979ee881fc4e6cf4c9aa99634766c33636
François Voron <fvoron@gmail.com>
2023-10-31 16:20:40 +0100

    clients/kit: implement a DataTable component

---
+++ clients/packages/polarkit/src/components/ui/atoms/DataTable.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import {
+  ColumnDef,
+  OnChangeFn,
+  PaginationState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../table'
+import { DataTablePagination } from './DataTablePagination'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+  pageCount: number
+  pagination: PaginationState
+  onPaginationChange?: OnChangeFn<PaginationState>
+  sorting: SortingState
+  onSortingChange?: OnChangeFn<SortingState>
+}
+
+export type DataTableColumnDef<TData, TValue = unknown> = ColumnDef<
+  TData,
+  TValue
+>
+
+export type DataTablePaginationState = PaginationState
+export type DataTableSortingState = SortingState
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  pageCount,
+  pagination,
+  onPaginationChange,
+  sorting,
+  onSortingChange,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    manualPagination: true,
+    manualSorting: true,
+    pageCount,
+    onPaginationChange,
+    onSortingChange,
+    state: {
+      pagination,
+      sorting,
+    },
+  })
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
+    </div>
+  )
+}
---
+++ clients/packages/polarkit/src/datatable.ts
@@ -0,0 +1,69 @@
+import { PaginationState, SortingState } from '@tanstack/react-table'
+
+export type DataTablePaginationState = PaginationState
+export type DataTableSortingState = SortingState
+export type DataTableSearchParams = {
+  page?: string
+  limit?: string
+  sorting?: string[] | string
+}
+
+const sortingStateToQueryParam = (state: DataTableSortingState): string[] => {
+  return state.map(({ id, desc }) => `${desc ? '-' : ''}${id}`)
+}
+
+const sortingQueryParamToState = (param: string[]): DataTableSortingState => {
+  return param.map((id) => {
+    if (id[0] === '-') {
+      return { id: id.slice(1), desc: true }
+    }
+    return { id, desc: false }
+  })
+}
+
+export const parseSearchParams = (
+  searchParams: DataTableSearchParams,
+  defaultSorting: DataTableSortingState = [],
+  defaultPageSize: number = 20,
+): { pagination: DataTablePaginationState; sorting: DataTableSortingState } => {
+  const pageIndex = searchParams.page
+    ? Number.parseInt(searchParams.page) - 1
+    : 0
+  const pageSize = searchParams.limit
+    ? Number.parseInt(searchParams.limit)
+    : defaultPageSize
+  const sorting = searchParams.sorting
+    ? sortingQueryParamToState(
+        Array.isArray(searchParams.sorting)
+          ? searchParams.sorting
+          : [searchParams.sorting],
+      )
+    : defaultSorting
+
+  return { pagination: { pageIndex, pageSize }, sorting }
+}
+
+export const serializeSearchParams = (
+  pagination: DataTablePaginationState,
+  sorting: DataTableSortingState,
+): URLSearchParams => {
+  const searchParams = new URLSearchParams({
+    page: (pagination.pageIndex + 1).toString(),
+    limit: pagination.pageSize.toString(),
+  })
+  for (const criteria of sortingStateToQueryParam(sorting)) {
+    searchParams.append('sorting', criteria)
+  }
+  return searchParams
+}
+
+export const getAPIParams = (
+  pagination: DataTablePaginationState,
+  sorting: DataTableSortingState,
+): { page: number; limit: number; sorting: string[] } => {
+  return {
+    page: pagination.pageIndex + 1,
+    limit: pagination.pageSize,
+    sorting: sortingStateToQueryParam(sorting),
+  }
+}

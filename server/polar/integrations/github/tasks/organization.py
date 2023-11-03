86d4c927e6cbf3e0d38515dad5635d6a3dac638b
Gustav Westling <gustav@polar.sh>
2023-11-03 10:55:16 +0100

    server/organization: daily refresh metadata from github

--- server/polar/integrations/github/tasks/organization.py
+++ server/polar/integrations/github/tasks/organization.py
@@ -50,3 +50,38 @@ async def organization_refresh_members(
             )
 
             await github_organization.synchronize_members(session, org)
+
+
+@interval(
+    hour=10,
+    minute=19,
+    second=0,
+)
+async def cron_org_metadata(ctx: JobContext) -> None:
+    async with AsyncSessionMaker(ctx) as session:
+        orgs = await organization_service.list_installed(session)
+        for org in orgs:
+            await enqueue_job(
+                "github.organization.populate_org_metadata",
+                organization_id=org.id,
+            )
+
+
+@task("github.organization.populate_org_metadata")
+async def populate_org_metadata(
+    ctx: JobContext,
+    organization_id: UUID,
+    polar_context: PolarWorkerContext,
+) -> None:
+    with polar_context.to_execution_context():
+        async with AsyncSessionMaker(ctx) as session:
+            org = await github_organization.get(session, organization_id)
+            if not org:
+                return
+
+            log.info(
+                "github.organization.populate_org_metadata",
+                organization_id=organization_id,
+            )
+
+            await github_organization.populate_org_metadata(session, org)

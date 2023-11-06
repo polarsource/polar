f0732b092040e0526c6e5d5d8ff60a7c9a243748
Gustav Westling <gustav@polar.sh>
2023-11-06 11:51:14 +0100

    clients/blob: image picker design

--- clients/apps/web/src/app/(backer)/settings/blob/page.tsx
+++ clients/apps/web/src/app/(backer)/settings/blob/page.tsx
@@ -1,39 +1,71 @@
 'use client'
 
+import { SpinnerNoMargin } from '@/components/Shared/Spinner'
 import { PhotoIcon } from '@heroicons/react/24/outline'
 import { type PutBlobResult } from '@vercel/blob'
 import { upload } from '@vercel/blob/client'
-import { Button } from 'polarkit/components/ui/atoms'
 import { useRef, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export default function AvatarUploadPage() {
-  const inputFileRef = useRef<HTMLInputElement>(null)
-  const [blob, setBlob] = useState<PutBlobResult | null>(null)
-
-  // const imagePreview = useRef<HTMLImageElement>(null)
-
-  const [imagePreviewSrc, setImagePreviewSrc] = useState<string | undefined>()
+  const [url, setUrl] = useState<string | undefined>()
 
   return (
     <>
-      <h1>Wow blob uploads!</h1>
+      <h1>Test Blob Uploads</h1>
 
+      <ImageUpload onUploaded={setUrl} />
+
+      {url && (
+        <div>
+          Blob url: <a href={url}>{url}</a>
+        </div>
+      )}
+    </>
+  )
+}
+
+const ImageUpload = ({ onUploaded }: { onUploaded: (url: string) => void }) => {
+  const inputFileRef = useRef<HTMLInputElement>(null)
+  const [blob, setBlob] = useState<PutBlobResult | null>(null)
+  const [imagePreviewSrc, setImagePreviewSrc] = useState<string | undefined>()
+
+  const canUpload = imagePreviewSrc
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [errorMessage, setErrorMessage] = useState<string | undefined>()
+
+  const handleUpload = async () => {
+    if (!inputFileRef.current?.files) {
+      throw new Error('No file selected')
+    }
+
+    const file = inputFileRef.current.files[0]
+
+    setIsLoading(true)
+    setErrorMessage(undefined)
+
+    try {
+      const newBlob = await upload(file.name, file, {
+        access: 'public',
+        handleUploadUrl: '/api/blob/upload',
+      })
+      setBlob(newBlob)
+      onUploaded(newBlob.url)
+    } catch (e) {
+      setErrorMessage('Failed to upload image')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <>
       <form
         onSubmit={async (event) => {
           event.preventDefault()
-
-          if (!inputFileRef.current?.files) {
-            throw new Error('No file selected')
-          }
-
-          const file = inputFileRef.current.files[0]
-
-          const newBlob = await upload(file.name, file, {
-            access: 'public',
-            handleUploadUrl: '/api/blob/upload',
-          })
-
-          setBlob(newBlob)
+          await handleUpload()
         }}
       >
         <input
@@ -42,7 +74,8 @@ export default function AvatarUploadPage() {
           ref={inputFileRef}
           type="file"
           required
-          onChange={(e) => {
+          accept="image/*"
+          onChange={async (e) => {
             if (e.target.files && e.target.files[0]) {
               const reader = new FileReader()
               reader.onload = (readerLoad) => {
@@ -54,41 +87,49 @@ export default function AvatarUploadPage() {
                 }
               }
               reader.readAsDataURL(e.target.files[0])
+              await handleUpload()
             } else {
               setImagePreviewSrc(undefined)
             }
           }}
         />
 
-        <div className="flex gap-4">
+        <div className="flex items-end gap-4">
           {imagePreviewSrc ? (
-            <img
-              src={imagePreviewSrc}
-              className="flex h-32 w-32 cursor-pointer items-center justify-center rounded-sm border border-gray-100 bg-gray-50 hover:opacity-80"
-              onClick={(e) => {
-                inputFileRef.current?.click()
-              }}
-            />
+            <div className="relative">
+              <img
+                src={imagePreviewSrc}
+                className={twMerge(
+                  'flex h-32 w-32 cursor-pointer items-center justify-center rounded-sm border border-gray-100 bg-gray-50 hover:opacity-80',
+                  isLoading ? 'opacity-50' : '',
+                  errorMessage ? 'border-red-800' : '',
+                )}
+                onClick={(e) => {
+                  inputFileRef.current?.click()
+                }}
+              />
+              {isLoading ? (
+                <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center">
+                  <SpinnerNoMargin />
+                </div>
+              ) : null}
+            </div>
           ) : (
             <div
               onClick={(e) => {
                 inputFileRef.current?.click()
               }}
-              className="flex h-32 w-32 cursor-pointer items-center justify-center rounded-sm border border-gray-100 bg-gray-50 hover:opacity-80"
+              className="flex h-32 w-32 cursor-pointer items-center justify-center rounded-sm border border-gray-100 bg-gray-50  hover:bg-gray-100 "
             >
               <PhotoIcon className="h-6 w-6 text-gray-600" />
             </div>
           )}
 
-          <Button type="submit">Upload</Button>
+          {errorMessage ? (
+            <div className="text-red-800">{errorMessage}</div>
+          ) : null}
         </div>
       </form>
-
-      {blob && (
-        <div>
-          Blob url: <a href={blob.url}>{blob.url}</a>
-        </div>
-      )}
     </>
   )
 }

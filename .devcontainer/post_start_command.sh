c0187a8a500440c64a206d179a02bb282a02c3a4
Gustav Westling <gustav@polar.sh>
2023-11-07 09:36:01 +0000

    dev: ports and auto-create web .env

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -17,4 +17,11 @@ sudo chown 1000:1000 /workspace/clients/node_modules
 pnpm install
 echo "🐻‍❄️✅ Clients ready"
 
+
+if [ ! -f "/workspace/clients/apps/web/.env" ]; then
+    echo "🐻‍❄️ /workspace/clients/apps/web/.env does not exist, creating it"
+    cp /workspace/clients/apps/web/.env.devcontainer /workspace/clients/apps/web/.env
+fi
+
+
 echo "🐻‍❄️✅✅✅ Setup complete"

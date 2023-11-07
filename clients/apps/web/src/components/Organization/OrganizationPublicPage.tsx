0c97adf49ac1c756bc75225825aede29718d42d8
Emil Widlund <hello@emilwidlund.com>
2023-11-07 17:57:35 +0100

    clients/campaigns: remove campaigns feature flag check from server side

--- clients/apps/web/src/components/Organization/OrganizationPublicPage.tsx
+++ clients/apps/web/src/components/Organization/OrganizationPublicPage.tsx
@@ -1,4 +1,3 @@
-import { isFeatureEnabled } from '@/utils/feature-flags'
 import {
   BusinessOutlined,
   EmailOutlined,
@@ -14,11 +13,11 @@ import {
 import { LogoType } from 'polarkit/components/brand'
 import { Avatar, Tabs, TabsContent } from 'polarkit/components/ui/atoms'
 import { useMemo } from 'react'
-import { twMerge } from 'tailwind-merge'
 import { externalURL, prettyURL } from '.'
 import HowItWorks from '../Pledge/HowItWorks'
 import OrganizationSubscriptionsPublicPage from '../Subscriptions/OrganizationSubscriptionsPublicPage'
 import PublicSubscriptionUpsell from '../Subscriptions/PublicSubscriptionUpsell'
+import CampaignsSummary from './CampaignsSummary'
 import Footer from './Footer'
 import IssuesLookingForFunding from './IssuesLookingForFunding'
 import { OrganizationPublicPageNav } from './OrganizationPublicPageNav'
@@ -171,54 +170,7 @@ const OrganizationPublicPage = ({
                 </div>
               </div>
             )}
-
-            {isFeatureEnabled('subscriptions') && (
-              <div className="hidden flex-col gap-y-4 md:flex">
-                <div className="flex flex-row items-start justify-between">
-                  <h3>Campaigns</h3>
-                  <h3>3</h3>
-                </div>
-                <div className="flex flex-col flex-wrap gap-4">
-                  {Array.from({ length: 3 }).map((_, i) => (
-                    <div
-                      key={i}
-                      className={twMerge(
-                        'dark:border-polar-700 flex flex-col gap-y-4 rounded-2xl border border-gray-200 p-4',
-                        i === 0 &&
-                          'dark:bg-polar-800 border-blue-100 bg-blue-50',
-                      )}
-                    >
-                      <h4 className="text-sm font-medium">Campaign v{i + 1}</h4>
-                      <div
-                        style={{
-                          display: 'flex',
-                          borderRadius: '2px',
-                          overflow: 'hidden',
-                        }}
-                      >
-                        <div
-                          style={{
-                            width: `${80 / (i * 2 + 1)}%`,
-                            height: '4px',
-                            backgroundColor: '#4667CA', // blue-600
-                            transitionProperty: 'all',
-                            transitionDuration: '200ms',
-                          }}
-                        />
-                        <div
-                          className="dark:bg-polar-700 bg-gray-200"
-                          style={{
-                            flexGrow: '1',
-                            height: '4px',
-                          }}
-                        ></div>
-                      </div>
-                      <h4 className="text-xs text-blue-500">View Campaign</h4>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
+            <CampaignsSummary />
           </div>
           <div className="mt-12 flex h-full w-full flex-col md:mt-0">
             <TabsContent className="w-full" value="overview">

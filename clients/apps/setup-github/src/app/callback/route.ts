26d9274e5b8a9aaef5af4a4f19d14c4dc1b8fd64
Gustav Westling <gustav@polar.sh>
2023-11-08 11:00:14 +0000

    dev: ingress

--- clients/apps/setup-github/src/app/callback/route.ts
+++ clients/apps/setup-github/src/app/callback/route.ts
@@ -16,24 +16,43 @@ const addEnv = (contents: string, key: string, value: any): string => {
   }
 }
 
-const serverEnv = async (path: string, app: any) => {
-  const file = await fs.readFile(path, 'utf8')
+const serverEnv = async (
+  path: string,
+  host: string,
+  hostProto: string,
+  app: any,
+) => {
+  let file: string = ''
 
-  const host = `${process.env.NEXT_PUBLIC_CODESPACE_NAME}.${process.env.NEXT_PUBLIC_GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
-  const hostProto = `https://${host}`
+  try {
+    file = await fs.readFile(path, 'utf8')
+  } catch (e) {
+    // not exists
+  }
 
   const adds: Record<string, any> = {
-    POLAR_GITHUB_APP_IDENTIFIER: app.id,
-    POLAR_GITHUB_APP_WEBHOOK_SECRET: app.webhook_secret,
-    POLAR_GITHUB_APP_PRIVATE_KEY: app.pem,
-    POLAR_GITHUB_CLIENT_ID: app.client_id,
-    POLAR_GITHUB_CLIENT_SECRET: app.client_secret,
+    POLAR_POSTGRES_USER: 'polar',
+    POLAR_POSTGRES_PWD: 'polar',
+    POLAR_POSTGRES_DATABASE: 'polar',
+    POLAR_POSTGRES_PORT: '5432',
+    POLAR_POSTGRES_HOST: 'db',
+    POLAR_REDIS_HOST: 'redis',
+    POLAR_REDIS_PORT: '6379',
 
     POLAR_CORS_ORIGINS: `["http://127.0.0.1:3000", "http://localhost:3000", "https://github.com", "${hostProto}:3000"]`,
     POLAR_BASE_URL: `${hostProto}:8000/api/v1`,
     POLAR_FRONTEND_BASE_URL: `${hostProto}:3000`,
     POLAR_AUTH_COOKIE_DOMAIN: `${host}`,
     POLAR_GITHUB_REDIRECT_URL: `${hostProto}:3000/github/session`,
+  }
+
+  // Add GitHub Env vars
+  if (app && app.id) {
+    adds.POLAR_GITHUB_APP_IDENTIFIER = app.id
+    adds.POLAR_GITHUB_APP_WEBHOOK_SECRET = app.webhook_secret
+    adds.POLAR_GITHUB_APP_PRIVATE_KEY = app.pem
+    adds.POLAR_GITHUB_CLIENT_ID = app.client_id
+    adds.POLAR_GITHUB_CLIENT_SECRET = app.client_secret
   }
 
   let newFile = file
@@ -43,15 +62,15 @@ const serverEnv = async (path: string, app: any) => {
   return newFile
 }
 
-const webEnv = async (path: string, app: any) => {
+const webEnv = async (path: string, hostProto: string, app: any) => {
   const file = await fs.readFile(path, 'utf8')
 
-  const host = `${process.env.NEXT_PUBLIC_CODESPACE_NAME}.${process.env.NEXT_PUBLIC_GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
-  const hostProto = `https://${host}`
-
   const adds: Record<string, any> = {
-    NEXT_PUBLIC_GITHUB_APP_NAMESPACE: app.slug,
     NEXT_PUBLIC_API_URL: `${hostProto}:8000`,
+  }
+
+  if (app && app.id) {
+    adds.NEXT_PUBLIC_GITHUB_APP_NAMESPACE = app.slug
   }
 
   let newFile = file
@@ -64,38 +83,47 @@ const webEnv = async (path: string, app: any) => {
 export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url)
 
+  const host = `${process.env.NEXT_PUBLIC_CODESPACE_NAME}.${process.env.NEXT_PUBLIC_GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
+  const hostProto = `https://${host}`
+
   try {
     const code = searchParams.get('code')
 
-    const converted = await fetch(
-      `https://api.github.com/app-manifests/${code}/conversions`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    let parsed: any = undefined
+
+    if (code) {
+      const converted = await fetch(
+        `https://api.github.com/app-manifests/${code}/conversions`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    )
-
-    const parsed = await converted.json()
-
-    if (!parsed.id) {
-      throw new Error(
-        'unexpected response from github: ' + JSON.stringify(parsed),
       )
+
+      parsed = await converted.json()
+
+      if (!parsed.id) {
+        throw new Error(
+          'unexpected response from github: ' + JSON.stringify(parsed),
+        )
+      }
     }
 
     // Server .env
     const serverEnvPath = process.cwd() + '/../../../server/.env'
-    const newServerEnv = await serverEnv(serverEnvPath, parsed)
+    const newServerEnv = await serverEnv(serverEnvPath, host, hostProto, parsed)
     await fs.writeFile(serverEnvPath, newServerEnv)
 
     // Web .env
     const webEnvPath = process.cwd() + '/../../../clients/apps/web/.env'
-    const newWebEnv = await webEnv(webEnvPath, parsed)
+    const newWebEnv = await webEnv(webEnvPath, hostProto, parsed)
     await fs.writeFile(webEnvPath, newWebEnv)
 
-    return NextResponse.redirect(new URL('/done', request.url))
+    return NextResponse.json({ res: `${hostProto}:3001/done` })
+
+    //return NextResponse.redirect(new URL(`/done`, request.url))
   } catch (e) {
     console.error(e)
     if (e instanceof Error) {

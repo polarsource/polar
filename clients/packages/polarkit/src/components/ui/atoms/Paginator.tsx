68ed80fe8ae62b01974ee9e3c6fee99887e45e6c
Emil Widlund <hello@emilwidlund.com>
2023-11-08 11:57:02 +0100

    clients/paginator: avoid early return before all hooks run

--- clients/packages/polarkit/src/components/ui/atoms/Paginator.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/Paginator.tsx
@@ -27,11 +27,6 @@ const Paginator = ({
     siblingCount,
   })
 
-  // If there are less than 2 times in pagination range we shall not render the component
-  if (currentPage === 0 || (paginationRange?.length ?? 0) < 2) {
-    return null
-  }
-
   const onNext = useCallback(() => {
     onPageChange(currentPage + 1)
   }, [onPageChange, currentPage])
@@ -39,6 +34,11 @@ const Paginator = ({
   const onPrevious = useCallback(() => {
     onPageChange(currentPage - 1)
   }, [onPageChange, currentPage])
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || (paginationRange?.length ?? 0) < 2) {
+    return null
+  }
 
   const lastPage = paginationRange?.[paginationRange.length - 1]
 

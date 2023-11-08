8b14e497c4faa4bfb7fc782494fcb920b8e7d57d
Emil Widlund <hello@emilwidlund.com>
2023-11-08 15:15:16 +0100

    clients/pagination: implement a shared pagination component with hook

---
+++ clients/apps/web/src/components/Shared/Pagination.tsx
@@ -0,0 +1,57 @@
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Paginator } from 'polarkit/components/ui/atoms'
+import {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+
+interface PaginationProps extends PropsWithChildren {
+  totalCount: number
+  pageSize: number
+  currentPage: number
+  siblingCount?: number
+  onPageChange: (page: number) => void
+  className?: string
+}
+
+const Pagination = ({ children, ...paginatorProps }: PaginationProps) => {
+  return (
+    <div className="flex flex-col gap-y-12">
+      <div className="flex flex-col">{children}</div>
+      <Paginator {...paginatorProps} />
+    </div>
+  )
+}
+
+export default Pagination
+
+export const usePagination = () => {
+  const router = useRouter()
+  const search = useSearchParams()
+  const initialPage = useMemo(() => Number(search.get('page') || '1'), [])
+  const [currentPage, setCurrentPage] = useState<number>(initialPage)
+
+  useEffect(() => {
+    if (!search.has('page')) {
+      setCurrentPage(1)
+    }
+  }, [search])
+
+  const handleSetCurrentPage = useCallback(
+    (page: number) => {
+      setCurrentPage(page)
+      const params = new URLSearchParams(search)
+      params.set('page', page.toString())
+
+      const url = new URL(window.location.href)
+      const newPath = `${url.pathname}?${params.toString()}`
+      router.replace(newPath)
+    },
+    [router, search],
+  )
+
+  return { currentPage, setCurrentPage: handleSetCurrentPage }
+}

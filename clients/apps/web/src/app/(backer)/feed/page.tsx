34c4a9e2fe1ddea9048c19bc633bb35b8b2d88d7
Emil Widlund <hello@emilwidlund.com>
2023-11-13 22:26:01 +0100

    clients/feed: more feed polish

--- clients/apps/web/src/app/(backer)/feed/page.tsx
+++ clients/apps/web/src/app/(backer)/feed/page.tsx
@@ -1,11 +1,34 @@
 'use client'
 
 import { Feed } from '@/components/Feed/Feed'
+import { LanguageOutlined } from '@mui/icons-material'
+import { Button, Input } from 'polarkit/components/ui/atoms'
 
 export default function Page() {
   return (
-    <div className="mx-auto mb-24 mt-2 max-w-xl space-y-10">
-      <Feed />
+    <div className="relative mx-auto mb-24 mt-2 flex max-w-5xl flex-row items-start">
+      <div className="absolute left-0 flex max-w-xl flex-col gap-y-4">
+        <div className="flex flex-col gap-y-5">
+          <h3 className="text-lg">Feed</h3>
+          <Input placeholder="Let your community know what you're up to..." />
+          <div className="flex flex-row items-center justify-between">
+            <div className="dark:text-polar-400 dark:bg-polar-800 dark:border-polar-700 flex flex-row items-center gap-x-2 self-start rounded-full border border-gray-100 bg-white px-3 py-1 text-sm text-gray-400 shadow-lg">
+              <LanguageOutlined className="text-blue-500" fontSize="inherit" />
+              <span>Public</span>
+            </div>
+            <Button size="sm" className="self-end">
+              Create Post
+            </Button>
+          </div>
+        </div>
+        <Feed />
+      </div>
+      <div className="absolute right-0 flex w-[320px] flex-col gap-y-6">
+        <h3 className="dark:text-polar-50 text-lg text-gray-950">
+          Maintainers you may know
+        </h3>
+        <div className="dark:bg-polar-800 dark:border-polar-700 h-[160px] w-full rounded-xl border border-gray-100 bg-white" />
+      </div>
     </div>
   )
 }
--- clients/apps/web/src/components/Feed/Post.tsx
+++ clients/apps/web/src/components/Feed/Post.tsx
@@ -2,11 +2,21 @@ import {
   BookmarkBorderOutlined,
   ChatBubbleOutline,
   FavoriteBorderOutlined,
+  LanguageOutlined,
   MoreVertOutlined,
+  PlayArrow,
 } from '@mui/icons-material'
 import Link from 'next/link'
 import { Avatar, Button, PolarTimeAgo } from 'polarkit/components/ui/atoms'
-import { CodePost, Post as FeedPost, PostType } from './data'
+import { twMerge } from 'tailwind-merge'
+import SubscriptionGroupIcon from '../Subscriptions/SubscriptionGroupIcon'
+import {
+  CodePost,
+  Post as FeedPost,
+  PollPost,
+  PostType,
+  VideoPost,
+} from './data'
 
 export const Post = (props: FeedPost) => {
   return (
@@ -16,7 +26,7 @@ export const Post = (props: FeedPost) => {
         avatar_url={props.author.avatar_url}
         name={props.author.username}
       />
-      <div className="flex min-w-0 flex-col">
+      <div className="flex w-full min-w-0 flex-col">
         <PostHeader {...props} />
         <PostBody {...props} />
         <PostFooter {...props} />
@@ -38,9 +48,22 @@ const PostHeader = (props: FeedPost) => {
             <PolarTimeAgo date={props.createdAt} />
           </div>
           &middot;
-          <Button className="px-0" variant="link" size="sm">
-            Subscribe
-          </Button>
+          {props.visibility === 'public' && (
+            <>
+              <div className="flex flex-row items-center gap-x-1">
+                <span className="flex items-center text-blue-500">
+                  <LanguageOutlined fontSize="inherit" />
+                </span>
+                <span className="text-xs">Public</span>
+              </div>
+              &middot;
+            </>
+          )}
+          <Link href={`/${props.author.username}?tab=subscriptions`}>
+            <Button className="px-0" variant="link" size="sm">
+              Subscribe
+            </Button>
+          </Link>
         </div>
       </div>
       <div className="dark:text-polar-400 text-base">
@@ -61,27 +84,40 @@ const PostBody = (props: FeedPost) => {
 
 const PostFooter = (props: FeedPost) => {
   return (
-    <div className="dark:text-polar-400 flex flex-row items-center gap-x-12 text-sm text-gray-400">
-      <div className="flex flex-row items-center gap-x-2">
-        <FavoriteBorderOutlined fontSize="small" />
-        <span>{props.likes.length}</span>
+    <div className="flex flex-row items-center gap-x-4">
+      <div className="dark:text-polar-400 dark:bg-polar-800 dark:border-polar-700 flex flex-row items-center gap-x-8 self-start rounded-full border border-gray-100 bg-white px-4 py-1.5 text-sm text-gray-400 shadow-lg">
+        <div className="flex flex-row items-center gap-x-2">
+          <FavoriteBorderOutlined fontSize="inherit" />
+          <span>{props.likes.length}</span>
+        </div>
+        <div className="flex flex-row items-center gap-x-2">
+          <ChatBubbleOutline fontSize="inherit" />
+          <span>{props.likes.length}</span>
+        </div>
+        <div className="flex flex-row items-center gap-x-2">
+          <BookmarkBorderOutlined fontSize="inherit" />
+          <span>{props.likes.length}</span>
+        </div>
       </div>
-      <div className="flex flex-row items-center gap-x-2">
-        <ChatBubbleOutline fontSize="small" />
-        <span>{props.likes.length}</span>
-      </div>
-      <div className="flex flex-row items-center gap-x-2">
-        <BookmarkBorderOutlined fontSize="small" />
-        <span>{props.likes.length}</span>
-      </div>
+
+      {props.visibility !== 'public' ? (
+        <div className="dark:text-polar-400 dark:bg-polar-800 dark:border-polar-700 flex flex-row items-center gap-x-1.5 self-start rounded-full border border-gray-100 bg-white px-3 py-1.5 text-sm text-gray-400 shadow-lg">
+          <SubscriptionGroupIcon type={props.visibility} />
+          <span className="capitalize">{props.visibility}</span>
+        </div>
+      ) : null}
     </div>
   )
 }
 
 const metaResolver = (post: FeedPost) => {
   switch (post.type) {
+    case PostType.Video:
+      return <PostMetaVideo {...(post as VideoPost)} />
     case PostType.Code:
       return <PostMetaCode {...(post as CodePost)} />
+    case PostType.Poll:
+      return <PostMetaPoll {...(post as PollPost)} />
     default:
       return null
   }
@@ -91,10 +127,37 @@ const PostMeta = (post: FeedPost) => {
   const children = metaResolver(post)
 
   return children ? (
-    <div className="dark:border-polar-700 dark:bg-polar-800 mb-2 flex w-full flex-col overflow-auto rounded-lg border border-gray-100 bg-white text-sm">
+    <div className="dark:border-polar-700 dark:bg-polar-800 mb-2 flex w-full flex-col overflow-hidden rounded-2xl border border-gray-100 bg-white text-sm shadow-xl">
       {children}
     </div>
   ) : null
+}
+
+const PostMetaVideo = (post: VideoPost) => {
+  return (
+    <div className="flex w-full flex-col">
+      <div
+        className="relative flex h-[260px] w-full flex-col items-center justify-center bg-cover bg-center text-white"
+        style={{ backgroundImage: `url(${post.video.thumbnailUrl})` }}
+      >
+        <span className="z-10 text-5xl">
+          <PlayArrow fontSize="inherit" />
+        </span>
+        <div className="absolute inset-0 bg-[rgba(0_0_0_/_.8)]" />
+      </div>
+
+      <div className="flex flex-col gap-y-3 p-4">
+        <div className="flex flex-col gap-y-1">
+          <h4 className="dark:text-polar-50 font-medium text-gray-950">
+            {post.video.title}
+          </h4>
+          <p className="dark:text-polar-500 truncate text-gray-400">
+            {post.video.description}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const PostMetaCode = (post: CodePost) => {
@@ -102,5 +165,56 @@ const PostMetaCode = (post: CodePost) => {
     <pre className="max-h-[280px] w-full overflow-auto p-4 text-xs">
       {post.code.code}
     </pre>
+  )
+}
+
+const PostMetaPoll = (post: PollPost) => {
+  const winningOption = post.poll.options.reduce(
+    (acc, option, index) => {
+      if (option.votes > acc.votes) {
+        return { index, votes: option.votes }
+      }
+      return acc
+    },
+    {
+      index: 0,
+      votes: post.poll.options[0].votes,
+    },
+  )
+
+  return (
+    <div className="flex w-full flex-col">
+      <div className="dark:border-polar-800 flex flex-col gap-y-1 border-b border-gray-100 px-6 py-4">
+        <h4 className="dark:text-polar-50 font-medium">{post.poll.question}</h4>
+        <span className="text-xs">{post.poll.totalVotes} votes</span>
+      </div>
+      <div className="flex flex-col gap-y-2 p-6">
+        {post.poll.options.map((option, index) => (
+          <div key={option.text} className="relative flex flex-row">
+            <div
+              className={twMerge(
+                'h-8 rounded-md',
+                winningOption.index === index
+                  ? 'bg-blue-600'
+                  : 'dark:bg-polar-600 bg-gray-300',
+              )}
+              style={{
+                width: `${(option.votes / post.poll.totalVotes) * 100}%`,
+              }}
+            />
+            <span
+              className={twMerge(
+                'absolute inset-x-3 inset-y-2 w-full text-xs',
+                winningOption.index === index && 'text-white',
+              )}
+            >
+              {`${Math.round((option.votes / post.poll.totalVotes) * 100)}% ${
+                option.text
+              }`}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }

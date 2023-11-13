81a4a737e5204e1636d1440e92c5a0e59b163ba4
François Voron <fvoron@gmail.com>
2023-11-13 16:48:28 +0100

    clients/web: handle subscribe session creation error a bit more gracefully

--- clients/apps/web/src/app/(public)/subscribe/route.ts
+++ clients/apps/web/src/app/(public)/subscribe/route.ts
@@ -1,7 +1,9 @@
-import { Configuration, PolarAPI } from '@polar-sh/sdk'
+import { Configuration, PolarAPI, ResponseError } from '@polar-sh/sdk'
 import { cookies } from 'next/headers'
 import { NextRequest } from 'next/server'
 import { getServerURL } from 'polarkit/api'
+
+export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
   const cookieStore = cookies()
@@ -18,15 +20,23 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const subscriptionTierId = searchParams.get('tier') as string
 
-  const { url } = await api.subscriptions.createSubscribeSession({
-    subscribeSessionCreate: {
-      tier_id: subscriptionTierId,
-      success_url: `${request.nextUrl.protocol}//${request.nextUrl.host}/subscribe/success?session_id={CHECKOUT_SESSION_ID}`,
-    },
-  })
-
-  return new Response(undefined, {
-    status: 303,
-    headers: { Location: url as string },
-  })
+  try {
+    const { url } = await api.subscriptions.createSubscribeSession({
+      subscribeSessionCreate: {
+        tier_id: subscriptionTierId,
+        success_url: `${request.nextUrl.protocol}//${request.nextUrl.host}/subscribe/success?session_id={CHECKOUT_SESSION_ID}`,
+      },
+    })
+    return new Response(undefined, {
+      status: 303,
+      headers: { Location: url as string },
+    })
+  } catch (err) {
+    if (err instanceof ResponseError) {
+      const response = err.response
+      const data = await response.json()
+      return new Response(data.detail, { status: response.status })
+    }
+    throw err
+  }
 }

83716bfdfc0c0c6e8fae161878de32403704cd40
Emil Widlund <hello@emilwidlund.com>
2023-11-15 17:24:49 +0100

    clear

--- clients/apps/web/src/components/Feed/Editor.tsx
+++ clients/apps/web/src/components/Feed/Editor.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { evaluate } from '@mdx-js/mdx'
+import { evaluate, UseMdxComponents } from '@mdx-js/mdx'
+import { Root } from 'mdast'
 import {
   Tabs,
   TabsContent,
@@ -10,6 +11,8 @@ import {
 } from 'polarkit/components/ui/atoms'
 import { ChangeEventHandler, useCallback, useEffect, useState } from 'react'
 import * as runtime from 'react/jsx-runtime'
+import { Plugin } from 'unified'
+import { visit } from 'unist-util-visit'
 
 interface EditorProps {
   value: string
@@ -22,53 +25,10 @@ const Editor = ({ value, onChange }: EditorProps) => {
 
   useEffect(() => {
     const asyncEvaluate = async () => {
-      const { default: MDXContent } = await evaluate(value, runtime)
+      const MDXContent = await evalMDX(value)
       setMDXContent(
         MDXContent({
-          components: {
-            wrapper: (props) => <div className="my-4 my-4" {...props} />,
-            h1: (props) => (
-              <h1 className="my-4 text-3xl font-bold" {...props} />
-            ),
-            h2: (props) => (
-              <h2 className="my-4 text-2xl font-bold" {...props} />
-            ),
-            h3: (props) => <h3 className="my-4 text-xl font-bold" {...props} />,
-            h4: (props) => <h4 className="my-4 text-lg font-bold" {...props} />,
-            h5: (props) => (
-              <h5 className="my-4 text-base font-bold" {...props} />
-            ),
-            h6: (props) => <h6 className="my-4 text-sm font-bold" {...props} />,
-            p: (props) => <p className="my-4 text-base" {...props} />,
-            a: (props) => (
-              <a className="my-4 text-base text-blue-500" {...props} />
-            ),
-            ul: (props) => <ul className="my-4 text-base" {...props} />,
-            ol: (props) => <ol className="my-4 text-base" {...props} />,
-            li: (props) => <li className="my-4 text-base" {...props} />,
-            blockquote: (props) => (
-              <blockquote className="my-4 text-base" {...props} />
-            ),
-            pre: (props) => <pre className="my-4 text-base" {...props} />,
-            code: (props) => <code className="my-4 text-base" {...props} />,
-            inlineCode: (props) => (
-              <code className="my-4 text-base" {...props} />
-            ),
-            hr: (props) => <hr className="my-4 text-base" {...props} />,
-            table: (props) => <table className="my-4 text-base" {...props} />,
-            thead: (props) => <thead className="my-4 text-base" {...props} />,
-            tbody: (props) => <tbody className="my-4 text-base" {...props} />,
-            tr: (props) => <tr className="my-4 text-base" {...props} />,
-            th: (props) => <th className="my-4 text-base" {...props} />,
-            td: (props) => <td className="my-4 text-base" {...props} />,
-            strong: (props) => <strong className="my-4 text-base" {...props} />,
-            em: (props) => <em className="my-4 text-base" {...props} />,
-            del: (props) => <del className="my-4 text-base" {...props} />,
-            img: (props) => <img className="my-4 text-base" {...props} />,
-            iframe: (props) => <iframe className="my-4 text-base" {...props} />,
-            span: (props) => <span className="my-4 text-base" {...props} />,
-            sup: (props) => <sup className="my-4 text-base" {...props} />,
-          },
+          components: COMPONENTS,
         }),
       )
     }
@@ -85,12 +45,16 @@ const Editor = ({ value, onChange }: EditorProps) => {
 
   return (
     <Tabs className="flex flex-col gap-y-6" defaultValue="edit">
-      <TabsList>
+      <TabsList className="dark:border-polar-700 dark:border">
         <TabsTrigger value="edit">Markdown</TabsTrigger>
         <TabsTrigger value="preview">Preview</TabsTrigger>
       </TabsList>
       <TabsContent value="edit">
-        <TextArea className="h-[400px]" value={value} onChange={handleChange} />
+        <TextArea
+          className=" dark:bg-polar-800 dark:border-polar-700 text-md h-[400px] min-h-[400px] rounded-3xl border border-gray-100 bg-white p-8 shadow-xl"
+          value={value}
+          onChange={handleChange}
+        />
       </TabsContent>
       <TabsContent value="preview">{MDXContent}</TabsContent>
     </Tabs>
@@ -98,3 +62,63 @@ const Editor = ({ value, onChange }: EditorProps) => {
 }
 
 export default Editor
+
+const rehypePlugin: Plugin<[], Root> = () => (ast) => {
+  visit(ast, 'text', (node, index, parent) => {
+    if ('value' in node && /(#\d+)/.test(node?.value)) {
+      if (parent) {
+        parent.tagName = 'issue'
+      }
+    }
+  })
+}
+
+const evalMDX = async (value: string) => {
+  // @ts-ignore
+  const { default: MDXContent } = await evaluate(value, {
+    ...runtime,
+    /** 
+     * Experimental plugin which can be used to inject custom elements into the MDX AST.
+    rehypePlugins: [rehypePlugin],
+    */
+  })
+
+  return MDXContent
+}
+
+const COMPONENTS: ReturnType<UseMdxComponents> = {
+  wrapper: (props) => (
+    <div
+      className="dark:bg-polar-800 dark:border-polar-700 min-h-[400px] rounded-3xl border border-gray-100 bg-white px-8 pb-8 pt-2 shadow-xl"
+      {...props}
+    />
+  ),
+  h1: (props) => <h1 className="my-6 text-3xl font-bold" {...props} />,
+  h2: (props) => <h2 className="my-6 text-2xl font-bold" {...props} />,
+  h3: (props) => <h3 className="my-6 text-xl font-bold" {...props} />,
+  h4: (props) => <h4 className="my-6 text-lg font-bold" {...props} />,
+  h5: (props) => <h5 className="my-6 text-base font-bold" {...props} />,
+  h6: (props) => <h6 className="my-6 text-sm font-bold" {...props} />,
+  p: (props) => <p className="my-6 text-base" {...props} />,
+  a: (props) => <a className="my-6 text-base text-blue-500" {...props} />,
+  ul: (props) => <ul className="my-6 text-base" {...props} />,
+  ol: (props) => <ol className="my-6 text-base" {...props} />,
+  li: (props) => <li className="my-6 text-base" {...props} />,
+  blockquote: (props) => <blockquote className="my-6 text-base" {...props} />,
+  pre: (props) => <pre className="my-6 text-base" {...props} />,
+  code: (props) => <code className="my-6 text-base" {...props} />,
+  inlineCode: (props) => <code className="my-6 text-base" {...props} />,
+  hr: (props) => <hr className="my-6 text-base" {...props} />,
+  table: (props) => <table className="my-6 text-base" {...props} />,
+  thead: (props) => <thead className="my-6 text-base" {...props} />,
+  tbody: (props) => <tbody className="my-6 text-base" {...props} />,
+  tr: (props) => <tr className="my-6 text-base" {...props} />,
+  th: (props) => <th className="my-6 text-base" {...props} />,
+  td: (props) => <td className="my-6 text-base" {...props} />,
+  strong: (props) => <strong className="my-6 text-base" {...props} />,
+  em: (props) => <em className="my-6 text-base" {...props} />,
+  del: (props) => <del className="my-6 text-base" {...props} />,
+  img: (props) => <img className="my-6 text-base" {...props} />,
+  iframe: (props) => <iframe className="my-6 text-base" {...props} />,
+  span: (props) => <span className="my-6 text-base" {...props} />,
+}

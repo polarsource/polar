263073519e87431fa40f504af04922b25b0e858b
Emil Widlund <hello@emilwidlund.com>
2023-11-20 13:11:01 +0100

    clients/posts: remove meta components

--- clients/apps/web/src/components/Feed/Posts/meta/Code.tsx
+++
@@ -1,9 +0,0 @@
-import { CodePost } from '../../data'
-
-export const CodeMeta = (post: CodePost) => {
-  return (
-    <pre className="max-h-[280px] w-full overflow-auto p-4 text-xs">
-      {post.code.code}
-    </pre>
-  )
-}

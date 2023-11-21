4281714a4279bf9943dcb1a54bb2bf148f9228db
Emil Widlund <hello@emilwidlund.com>
2023-11-21 10:02:51 +0100

    clients/maintainer: move maintainer content with feature flags to client

--- clients/apps/web/src/components/Organization/OrganizationPublicPageNav.tsx
+++ clients/apps/web/src/components/Organization/OrganizationPublicPageNav.tsx
@@ -7,10 +7,22 @@ import {
   HiveOutlined,
   HowToVoteOutlined,
 } from '@mui/icons-material'
+import { Organization, Repository, SubscriptionTier } from '@polar-sh/sdk'
+import Link from 'next/link'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { TabsList, TabsTrigger } from 'polarkit/components/ui/atoms'
+import {
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from 'polarkit/components/ui/atoms'
 import { useCallback } from 'react'
+import { Post as PostComponent } from '../Feed/Posts/Post'
+import { Post } from '../Feed/data'
 import ProfileSelection from '../Shared/ProfileSelection'
+import OrganizationSubscriptionsPublicPage from '../Subscriptions/OrganizationSubscriptionsPublicPage'
+import PublicSubscriptionUpsell from '../Subscriptions/PublicSubscriptionUpsell'
+import IssuesLookingForFunding from './IssuesLookingForFunding'
+import { RepositoriesOverivew } from './RepositoriesOverview'
 
 interface OrganizationPublicPageNavProps {
   shouldRenderSubscriptionsTab: boolean
@@ -30,7 +42,7 @@ export const OrganizationPublicPageNav = ({
       params.set('tab', value)
       router.push(`${pathname}?${params.toString()}`)
     },
-    [search, pathname],
+    [search, router, pathname],
   )
 
   return (
@@ -89,6 +101,67 @@ export const OrganizationPublicPageNav = ({
           className="border border-gray-100 shadow-sm"
         />
       </div>
+    </div>
+  )
+}
+
+export const OrganizationPublicPageContent = ({
+  posts,
+  organization,
+  repositories,
+  subscriptionTiers,
+}: {
+  posts: Post[]
+  organization: Organization
+  repositories: Repository[]
+  subscriptionTiers: SubscriptionTier[]
+}) => {
+  return (
+    <div className="mt-12 flex h-full w-full flex-col md:mt-0">
+      {isFeatureEnabled('feed') && (
+        <TabsContent className="w-full" value="overview">
+          <div className="flex max-w-xl flex-col gap-y-6">
+            {posts.map((post) => (
+              <Link href={`/${organization.name}/posts/${post.id}`}>
+                <PostComponent {...post} />
+              </Link>
+            ))}
+          </div>
+        </TabsContent>
+      )}
+      <TabsContent
+        className="w-full"
+        value={isFeatureEnabled('feed') ? 'issues' : 'overview'}
+      >
+        <div className="flex w-full flex-col gap-y-8">
+          {subscriptionTiers.length > 0 && (
+            <PublicSubscriptionUpsell
+              organization={organization}
+              subscriptionTiers={subscriptionTiers}
+              subscribePath="/subscribe"
+            />
+          )}
+
+          <div className="flex flex-row items-start justify-between">
+            <h2 className="text-lg">Issues looking for funding</h2>
+          </div>
+          <IssuesLookingForFunding organization={organization} />
+        </div>
+      </TabsContent>
+      <TabsContent className="w-full" value="repositories">
+        <RepositoriesOverivew
+          organization={organization}
+          repositories={repositories}
+        />
+      </TabsContent>
+      {subscriptionTiers.length > 0 && (
+        <TabsContent className="w-full" value="subscriptions">
+          <OrganizationSubscriptionsPublicPage
+            organization={organization}
+            subscriptionTiers={subscriptionTiers}
+          />
+        </TabsContent>
+      )}
     </div>
   )
 }

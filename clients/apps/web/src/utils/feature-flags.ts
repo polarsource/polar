e8f43634995f3303b4fff6bcac3d4ca90b8b6a22
Emil Widlund <hello@emilwidlund.com>
2023-11-24 09:53:10 +0100

    clients/backoffice: remove isPolarEmployee func

--- clients/apps/web/src/utils/feature-flags.ts
+++ clients/apps/web/src/utils/feature-flags.ts
@@ -8,11 +8,3 @@ export const isFeatureEnabled = (key: string): boolean => {
 
   return posthog.isFeatureEnabled(key) || false
 }
-
-export const isPolarEmployee = (): boolean => {
-  if (CONFIG.ENVIRONMENT == 'development') {
-    return true
-  }
-
-  return posthog.get_property('is_polar_employee') ?? false
-}

eb8b9a48529fefbaaf26e5d540cc6b7e4e3f1d60
Emil Widlund <hello@emilwidlund.com>
2023-11-27 17:05:21 +0100

    clients/footer: polish footer

--- clients/apps/web/src/components/Organization/UpsellFooter.tsx
+++ clients/apps/web/src/components/Organization/UpsellFooter.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { useAuth } from '@/hooks'
+import { ArrowUpRightIcon } from '@heroicons/react/20/solid'
 import {
   ArrowForward,
   FavoriteBorderOutlined,
   HiveOutlined,
 } from '@mui/icons-material'
 import Link from 'next/link'
+import { LogoIcon } from 'polarkit/components/brand'
 import { Separator } from 'polarkit/components/ui/separator'
 import { twMerge } from 'tailwind-merge'
 
@@ -21,25 +23,36 @@ export const UpsellFooter = ({ wide }: { wide?: boolean }) => {
     <>
       <div
         className={twMerge(
-          'flex w-full flex-col items-start gap-x-24 gap-y-12 px-8 md:flex-row md:justify-between',
+          'flex w-full flex-col items-start gap-x-32 gap-y-12 px-8 md:flex-row md:justify-between',
           wide ? 'max-w-7xl' : 'max-w-[970px]',
         )}
       >
-        <div className="flex w-full flex-col md:w-1/2 md:flex-row">
-          <h1 className="text-4xl leading-normal">
+        <div className="flex w-full flex-col gap-y-6 md:w-1/2">
+          <LogoIcon className="h-10 w-10 text-blue-500 dark:text-blue-400" />
+          <h1 className="text-4xl !font-light leading-normal">
             Your open source projects deserve more than just recognition
           </h1>
+          <a
+            className="flex flex-row items-center text-blue-500 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
+            href="https://blog.polar.sh/polar-v1-0-lets-fix-open-source-funding/"
+            target="_blank"
+          >
+            Learn more about our mission
+            <ArrowUpRightIcon className="ml-2 h-5 w-5" fontSize="inherit" />
+          </a>
         </div>
-        <div className="flex w-full flex-col gap-16 md:w-1/2 md:flex-row">
-          <div className="flex flex-col gap-y-4 text-sm">
+        <div className="mt-7 flex w-full flex-col gap-16 md:w-1/2 md:flex-row">
+          <div className="flex flex-col gap-y-6 text-sm">
             <HiveOutlined className="text-blue-500 dark:text-blue-400" />
-            <h3 className="dark:text-polar-50 text-base font-medium text-gray-950">
-              Maintainer
-            </h3>
-            <p className="dark:text-polar-400 leading-relaxed text-gray-500">
-              Connect your GitHub repositories & embed the Polar funding-badge
-              on your issues
-            </p>
+            <div className="flex flex-col gap-y-4">
+              <h3 className="dark:text-polar-50 text-base font-medium text-gray-950">
+                Maintainer
+              </h3>
+              <p className="dark:text-polar-400 leading-relaxed text-gray-500">
+                Connect your GitHub repositories & embed the Polar funding-badge
+                on your issues
+              </p>
+            </div>
             <Link
               className="text-blue-500 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
               href="/signup/maintainer"
@@ -48,15 +61,17 @@ export const UpsellFooter = ({ wide }: { wide?: boolean }) => {
               <ArrowForward className="ml-2" fontSize="inherit" />
             </Link>
           </div>
-          <div className="flex flex-col gap-y-4 text-sm">
+          <div className="flex flex-col gap-y-6 text-sm">
             <FavoriteBorderOutlined className="text-blue-500 dark:text-blue-400" />
-            <h3 className="dark:text-polar-50 text-base font-medium text-gray-950">
-              Backer
-            </h3>
-            <p className="dark:text-polar-400 leading-relaxed text-gray-500">
-              Fund issues in open source projects or receive rewards for your
-              contributions
-            </p>
+            <div className="flex flex-col gap-y-4">
+              <h3 className="dark:text-polar-50 text-base font-medium text-gray-950">
+                Backer
+              </h3>
+              <p className="dark:text-polar-400 leading-relaxed text-gray-500">
+                Fund issues in open source projects or receive rewards for your
+                contributions
+              </p>
+            </div>
             <Link
               className="text-blue-500 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
               href="/login"
@@ -67,7 +82,7 @@ export const UpsellFooter = ({ wide }: { wide?: boolean }) => {
           </div>
         </div>
       </div>
-      <Separator />
+      <Separator className="dark:bg-polar-700 bg-gray-100" />
     </>
   )
 }

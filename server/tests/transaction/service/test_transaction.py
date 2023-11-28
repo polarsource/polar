0c84a8ae2824c99dbd7d4f2abf97635d9fe506c3
François Voron <fvoron@gmail.com>
2023-11-28 16:22:56 +0100

    server/transaction: revamp API to query for all transactions accessible by the user

--- server/tests/transaction/service/test_transaction.py
+++ server/tests/transaction/service/test_transaction.py
@@ -5,7 +5,7 @@ import pytest
 from polar.authz.service import Authz
 from polar.exceptions import NotPermitted, ResourceNotFound
 from polar.kit.pagination import PaginationParams
-from polar.models import Account, Pledge, Transaction, User, UserOrganization
+from polar.models import Account, Organization, Transaction, User, UserOrganization
 from polar.models.transaction import TransactionType
 from polar.postgres import AsyncSession
 from polar.transaction.service.transaction import transaction as transaction_service
@@ -18,34 +18,42 @@ def authz(session: AsyncSession) -> Authz:
 
 @pytest.mark.asyncio
 class TestSearch:
-    async def test_account_not_permitted(
-        self, session: AsyncSession, account: Account, user_second: User, authz: Authz
-    ) -> None:
-        with pytest.raises(NotPermitted):
-            await transaction_service.search(
-                session, user_second, account, authz, pagination=PaginationParams(1, 10)
-            )
-
-    async def test_valid(
+    async def test_no_access(
         self,
         session: AsyncSession,
-        account: Account,
-        user: User,
-        user_organization: UserOrganization,
-        pledge: Pledge,
-        account_transactions: list[Transaction],
-        authz: Authz,
+        user_second: User,
+        all_transactions: list[Transaction],
     ) -> None:
         session.expunge_all()
 
         results, count = await transaction_service.search(
-            session, user, account, authz, pagination=PaginationParams(1, 10)
+            session, user_second, pagination=PaginationParams(1, 10)
         )
 
-        assert count == len(account_transactions)
-        assert len(results) == len(account_transactions)
+        assert count == 0
+        assert len(results) == 0
+
+    async def test_no_filter(
+        self,
+        session: AsyncSession,
+        user: User,
+        user_organization: UserOrganization,
+        readable_user_transactions: list[Transaction],
+        all_transactions: list[Transaction],
+    ) -> None:
+        session.expunge_all()
+
+        results, count = await transaction_service.search(
+            session, user, pagination=PaginationParams(1, 10)
+        )
+
+        assert count == len(readable_user_transactions)
+        assert len(results) == len(readable_user_transactions)
+
+        readable_user_transactions_id = [t.id for t in readable_user_transactions]
 
         for result in results:
+            assert result.id in readable_user_transactions_id
             # Check that relationships are eagerly loaded
             result.pledge
             if result.pledge is not None:
@@ -54,6 +62,105 @@ class TestSearch:
             result.subscription
             if result.subscription is not None:
                 result.subscription.subscription_tier
+
+    async def test_filter_type(
+        self,
+        session: AsyncSession,
+        account: Account,
+        user: User,
+        user_organization: UserOrganization,
+        readable_user_transactions: list[Transaction],
+        all_transactions: list[Transaction],
+    ) -> None:
+        session.expunge_all()
+
+        results, count = await transaction_service.search(
+            session,
+            user,
+            type=TransactionType.payout,
+            pagination=PaginationParams(1, 10),
+        )
+
+        payout_transactions_id = [
+            t.id for t in readable_user_transactions if t.type == TransactionType.payout
+        ]
+
+        assert count == len(payout_transactions_id)
+        assert len(results) == len(payout_transactions_id)
+
+        for result in results:
+            assert result.id in payout_transactions_id
+
+    async def test_filter_account(
+        self,
+        session: AsyncSession,
+        user: User,
+        account: Account,
+        user_organization: UserOrganization,
+        account_transactions: list[Transaction],
+        all_transactions: list[Transaction],
+    ) -> None:
+        session.expunge_all()
+
+        results, count = await transaction_service.search(
+            session, user, account_id=account.id, pagination=PaginationParams(1, 10)
+        )
+
+        assert count == len(account_transactions)
+        assert len(results) == len(account_transactions)
+
+        account_transactions_id = [t.id for t in account_transactions]
+
+        for result in results:
+            assert result.id in account_transactions_id
+
+    async def test_filter_payment_user(
+        self,
+        session: AsyncSession,
+        user: User,
+        user_organization: UserOrganization,
+        user_transactions: list[Transaction],
+        all_transactions: list[Transaction],
+    ) -> None:
+        session.expunge_all()
+
+        results, count = await transaction_service.search(
+            session, user, payment_user_id=user.id, pagination=PaginationParams(1, 10)
+        )
+
+        assert count == len(user_transactions)
+        assert len(results) == len(user_transactions)
+
+        user_transactions_id = [t.id for t in user_transactions]
+
+        for result in results:
+            assert result.id in user_transactions_id
+
+    async def test_filter_payment_organization(
+        self,
+        session: AsyncSession,
+        user: User,
+        organization: Organization,
+        user_organization: UserOrganization,
+        organization_transactions: list[Transaction],
+        all_transactions: list[Transaction],
+    ) -> None:
+        session.expunge_all()
+
+        results, count = await transaction_service.search(
+            session,
+            user,
+            payment_organization_id=organization.id,
+            pagination=PaginationParams(1, 10),
+        )
+
+        assert count == len(organization_transactions)
+        assert len(results) == len(organization_transactions)
+
+        organization_transactions_id = [t.id for t in organization_transactions]
+
+        for result in results:
+            assert result.id in organization_transactions_id
 
 
 @pytest.mark.asyncio
@@ -96,40 +203,35 @@ class TestGetSummary:
 
 @pytest.mark.asyncio
 class TestLookup:
-    async def test_not_existing(
-        self, session: AsyncSession, user_second: User, authz: Authz
-    ) -> None:
+    async def test_not_existing(self, session: AsyncSession, user_second: User) -> None:
         with pytest.raises(ResourceNotFound):
-            await transaction_service.lookup(session, uuid.uuid4(), user_second, authz)
+            await transaction_service.lookup(session, uuid.uuid4(), user_second)
 
-    async def test_account_not_permitted(
+    async def test_user_not_accessible(
         self,
         session: AsyncSession,
-        account: Account,
         user_second: User,
-        authz: Authz,
-        account_transactions: list[Transaction],
+        readable_user_transactions: list[Transaction],
+        all_transactions: list[Transaction],
     ) -> None:
         with pytest.raises(ResourceNotFound):
             await transaction_service.lookup(
-                session, account_transactions[0].id, user_second, authz
+                session, readable_user_transactions[0].id, user_second
             )
 
     async def test_valid(
         self,
         session: AsyncSession,
         user: User,
-        authz: Authz,
-        account_transactions: list[Transaction],
-        account: Account,
         user_organization: UserOrganization,
+        readable_user_transactions: list[Transaction],
+        all_transactions: list[Transaction],
     ) -> None:
         transaction = await transaction_service.lookup(
-            session, account_transactions[0].id, user, authz
+            session, readable_user_transactions[0].id, user
         )
 
-        assert transaction.id == account_transactions[0].id
-        assert transaction.account is not None
+        assert transaction.id == readable_user_transactions[0].id
         # Check that relationships are eagerly loaded
         transaction.pledge
         transaction.issue_reward

5205fdf9b3901b81e54106b123b09bf4f032ac0b
Emil Widlund <hello@emilwidlund.com>
2023-11-28 16:45:37 +0100

    clients/finance: more transaction logic

--- clients/apps/web/src/app/(dashboard)/finance/outgoing/ClientPage.tsx
+++ clients/apps/web/src/app/(dashboard)/finance/outgoing/ClientPage.tsx
@@ -1,26 +1,61 @@
 'use client'
 
-import { useToast } from '@/components/Toast/use-toast'
-import { useCurrentOrgAndRepoFromURL } from '@/hooks'
+import { usePagination } from '@/components/Shared/Pagination'
+import TransactionsList from '@/components/Transactions/TransactionsList'
+import { useAuth } from '@/hooks'
+import { TransactionType } from '@polar-sh/sdk'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { Separator } from 'polarkit/components/ui/separator'
 import {
   useListAccountsByOrganization,
-  useListPledgesForOrganization,
-  useListRewards,
+  useListAccountsByUser,
+  useListAdminOrganizations,
+  useSearchTransactions,
 } from 'polarkit/hooks'
+import { useCallback } from 'react'
 
 export default function ClientPage() {
   const router = useRouter()
-  const { org, isLoaded } = useCurrentOrgAndRepoFromURL()
-
   const params = useSearchParams()
-  const status = params?.get('status')
+  const { currentUser } = useAuth()
+  const organizations = useListAdminOrganizations()
+  const personalOrganization = organizations.data?.items?.find(
+    (org) => org.name === currentUser?.username,
+  )
+  const { currentPage, setCurrentPage } = usePagination()
 
-  const { toast } = useToast()
+  const setActiveTab = useCallback((value: string) => {
+    router.replace(`/finance/incoming?type=${value}`)
+  }, [])
 
-  const pledges = useListPledgesForOrganization(org?.platform, org?.name)
-  const rewards = useListRewards(org?.id)
-  const accounts = useListAccountsByOrganization(org?.id)
+  const accounts = useListAccountsByUser(currentUser?.id ?? '')
+  const organizationAccounts = useListAccountsByOrganization(
+    personalOrganization?.id,
+  )
+  const [account] = accounts.data?.items ?? []
+  const [organizationAccount] = organizationAccounts.data?.items ?? []
 
-  return <></>
+  const transactions = useSearchTransactions({
+    accountId: account.id,
+    page: currentPage,
+    type: TransactionType.PAYMENT,
+  })
+
+  return (
+    <div className="dark:bg-polar-900 dark:border-polar-800 min-h-[480px] rounded-3xl border border-gray-100 bg-white p-12">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h2 className="text-lg font-medium capitalize">Transactions</h2>
+        </div>
+      </div>
+      <Separator className="my-8" />
+      {account && (
+        <TransactionsList
+          accountId={account.id}
+          type={TransactionType.PAYMENT}
+          pageSize={20}
+        />
+      )}
+    </div>
+  )
 }

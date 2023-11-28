83821c3c741093159f9866f065cacd5bb0ac07a4
François Voron <fvoron@gmail.com>
2023-11-28 11:34:42 +0100

    server/transaction: output related data in search/lookup endpoints

--- server/tests/transaction/conftest.py
+++ server/tests/transaction/conftest.py
@@ -3,15 +3,19 @@ import pytest_asyncio
 from polar.enums import AccountType
 from polar.models import (
     Account,
+    Issue,
     IssueReward,
     Organization,
     Pledge,
+    Repository,
     Subscription,
     Transaction,
     User,
 )
 from polar.models.transaction import PaymentProcessor, TransactionType
 from polar.postgres import AsyncSession
+from tests.fixtures.random_objects import create_pledge
+from tests.subscription.conftest import create_subscription, create_subscription_tier
 
 
 async def create_transaction(
@@ -66,12 +70,56 @@ async def account(
 
 
 @pytest_asyncio.fixture
+async def transaction_pledge(
+    session: AsyncSession,
+    organization: Organization,
+    repository: Repository,
+    issue: Issue,
+) -> Pledge:
+    return await create_pledge(session, organization, repository, issue, organization)
+
+
+@pytest_asyncio.fixture
+async def transaction_issue_reward(session: AsyncSession, issue: Issue) -> IssueReward:
+    issue_reward = IssueReward(
+        issue_id=issue.id,
+        share_thousands=100,
+    )
+    session.add(issue_reward)
+    await session.commit()
+    return issue_reward
+
+
+@pytest_asyncio.fixture
+async def transaction_subscription(
+    session: AsyncSession, organization: Organization, user: User
+) -> Subscription:
+    subscription_tier = await create_subscription_tier(
+        session, organization=organization
+    )
+    return await create_subscription(
+        session, subscription_tier=subscription_tier, user=user
+    )
+
+
+@pytest_asyncio.fixture
 async def account_transactions(
-    session: AsyncSession, account: Account
+    session: AsyncSession,
+    account: Account,
+    transaction_pledge: Pledge,
+    transaction_issue_reward: IssueReward,
+    transaction_subscription: Subscription,
 ) -> list[Transaction]:
     return [
-        await create_transaction(session, account=account),
-        await create_transaction(session, account=account),
+        await create_transaction(
+            session,
+            account=account,
+            pledge=transaction_pledge,
+            issue_reward=transaction_issue_reward,
+        ),
+        await create_transaction(
+            session, account=account, subscription=transaction_subscription
+        ),
         await create_transaction(session, account=account),
         await create_transaction(
             session, account=account, type=TransactionType.payout, amount=-3000

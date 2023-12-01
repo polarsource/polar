9f937b4dbda9f532b3adb730219a74c533d8b0d8
Gustav Westling <gustav@polar.sh>
2023-12-01 10:03:25 +0100

    clients/Poll: improve parser

---
+++ clients/apps/web/src/components/Feed/Posts/Poll.tsx
@@ -0,0 +1,67 @@
+const InvalidPoll = () => (
+  <div className="my-2 bg-gray-100 p-8 text-gray-400">
+    This <code>&lt;Poll&gt;</code> is not configured correctly.
+  </div>
+)
+
+const Poll = (props: { children?: React.ReactNode }) => {
+  if (!props.children || typeof props.children !== 'object') {
+    return <InvalidPoll />
+  }
+
+  let child = props.children
+  if (Array.isArray(child)) {
+    if (child.length > 0) {
+      child = child[0]
+    }
+  }
+
+  if (!('type' in child)) {
+    return <InvalidPoll />
+  }
+
+  let options: string[] = []
+
+  const getStr = (c: any): string | undefined => {
+    if (typeof c === 'string') {
+      return c
+    }
+
+    if (typeof c === 'object' && Array.isArray(c) && c.length === 1) {
+      return getStr(c[0])
+    }
+
+    return undefined
+  }
+
+  if (child.type === 'ol') {
+    options = child.props.children
+      .map((c: React.ReactHTMLElement<HTMLLIElement>) => c.props.children)
+      .map(getStr)
+      .filter((s: any) => typeof s === 'string')
+  } else if (child.type == 'ul') {
+    options = child.props.children
+      .map((c: React.ReactHTMLElement<HTMLLIElement>) => c.props.children)
+      .map(getStr)
+      .filter((s: any) => typeof s === 'string')
+  } else {
+    return <InvalidPoll />
+  }
+
+  if (options.length === 0) {
+    return <InvalidPoll />
+  }
+
+  return (
+    <div className="my-2 flex flex-col space-y-2 bg-blue-300 p-8">
+      {options.map((s) => (
+        <div className="item-center flex justify-between bg-black/20 p-2">
+          <div>{s}</div>
+          <div>123 votes (10%)</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Poll

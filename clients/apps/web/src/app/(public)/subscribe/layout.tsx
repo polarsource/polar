60d8b86bf8c08de9d271a6cbcda72b46399b1b43
François Voron <fvoron@gmail.com>
2023-12-11 17:20:30 +0100

    clients/web: upgrade subscribe success page

--- clients/apps/web/src/app/(public)/subscribe/layout.tsx
+++ clients/apps/web/src/app/(public)/subscribe/layout.tsx
@@ -1,12 +1,9 @@
 import PublicLayout from '@/components/Layout/PublicLayout'
-import TopbarLayout from '@/components/Layout/TopbarLayout'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <TopbarLayout logoPosition="center" isFixed={false} hideProfile={true}>
-      <PublicLayout>
-        <>{children}</>
-      </PublicLayout>
-    </TopbarLayout>
+    <PublicLayout>
+      <>{children}</>
+    </PublicLayout>
   )
 }

a49b6c62d5b5b27ee2d892e6ab01f6b8ed79523b
Emil Widlund <hello@emilwidlund.com>
2023-12-12 17:10:36 +0100

    clients/syntax: fix theming

--- clients/apps/web/src/components/Feed/Posts/BrowserRender.tsx
+++ clients/apps/web/src/components/Feed/Posts/BrowserRender.tsx
@@ -1,15 +1,12 @@
-import { ContentPasteOutlined } from '@mui/icons-material'
 // @ts-ignore
 import Markdown from 'markdown-to-jsx'
-import { Button } from 'polarkit/components/ui/atoms'
-import SyntaxHighlighter from 'react-syntax-highlighter'
 import Embed from './BrowserEmbed'
 import Iframe from './BrowserIframe'
 import BrowserPoll from './BrowserPoll'
 import Paywall from './Paywall'
 import Poll from './Poll'
 import SubscribeNow from './SubscribeNow'
-import { polarStyle } from './SyntaxHighlighter'
+import { SyntaxHighlighter } from './SyntaxHighlighter'
 import {
   RenderArticle,
   markdownOpts,
@@ -27,40 +24,7 @@ export const opts = {
     SubscribeNow: (args: any) => <SubscribeNow {...args} />,
     embed: (args: any) => <Embed {...args} />,
     iframe: (args: any) => <Iframe {...args} />,
-    code: (args: any) => {
-      // Language gets passed in as a className
-      const language = args.className?.replace('lang-', '')
-
-      // Copy the code contents to the clipboard
-      const handleCopy = () => {
-        navigator.clipboard.writeText(args.children)
-      }
-
-      return (
-        <div className="relative w-full">
-          <SyntaxHighlighter
-            language={language}
-            style={polarStyle}
-            lineNumberStyle={{
-              paddingRight: '1.5rem',
-              opacity: '.2',
-              fontSize: '.7rem',
-            }}
-            showLineNumbers
-          >
-            {args.children}
-          </SyntaxHighlighter>
-          <Button
-            size="icon"
-            variant="secondary"
-            className="absolute right-4 top-4 h-8 w-8 rounded-full text-sm"
-            onClick={handleCopy}
-          >
-            <ContentPasteOutlined fontSize="inherit" />
-          </Button>
-        </div>
-      )
-    },
+    code: (args: any) => <SyntaxHighlighter {...args} />,
   },
 } as const
 

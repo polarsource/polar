c93de2f58f4d76da305ababbaf13fc9e042f74e8
Gustav Westling <gustav@polar.sh>
2023-12-12 14:47:10 +0100

    clients/subscriptions: make SubscriptionTierCard responsive

--- clients/apps/web/src/components/Subscriptions/SubscriptionGroupPublic.tsx
+++ clients/apps/web/src/components/Subscriptions/SubscriptionGroupPublic.tsx
@@ -40,10 +40,10 @@ const SubscriptionGroupPublic = ({
         <p className="dark:text-polar-500 mt-2 text-gray-500">{description}</p>
       </div>
 
-      <div className="-mx-10 flex h-fit flex-row gap-6 overflow-x-auto px-10 py-6 md:mx-0 md:grid md:grid-cols-3 md:overflow-x-visible md:px-0 md:py-2">
+      <div className="-mx-10 flex h-fit flex-row flex-wrap gap-6 overflow-x-auto px-10 py-6 lg:mx-0 lg:overflow-x-visible lg:px-0 lg:py-2">
         {tiers.map((tier) => (
           <SubscriptionTierCard
-            className="self-stretch md:h-full"
+            className="w-full self-stretch lg:max-w-[250px]"
             key={tier.id}
             subscriptionTier={tier}
             variant="small"

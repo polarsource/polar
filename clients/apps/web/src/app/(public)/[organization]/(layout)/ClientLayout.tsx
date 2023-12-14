81237bcc449105f8ea8a294a6f0c6334a79ad5d3
Emil Widlund <hello@emilwidlund.com>
2023-12-14 13:24:05 +0100

    clients/maintainer: fix navbar state

--- clients/apps/web/src/app/(public)/[organization]/(layout)/ClientLayout.tsx
+++ clients/apps/web/src/app/(public)/[organization]/(layout)/ClientLayout.tsx
@@ -3,16 +3,33 @@
 import { OrganizationPublicPageNav } from '@/components/Organization/OrganizationPublicPageNav'
 import { OrganizationPublicSidebar } from '@/components/Organization/OrganizationPublicSidebar'
 import { Organization } from '@polar-sh/sdk'
+import { usePathname } from 'next/navigation'
 import { LogoType } from 'polarkit/components/brand'
 import { Tabs } from 'polarkit/components/ui/atoms'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useMemo } from 'react'
 
 const ClientLayout = ({
   organization,
   children,
 }: PropsWithChildren<{ organization: Organization }>) => {
+  const pathname = usePathname()
+  const currentTab = useMemo(() => {
+    const tabs = ['overview', 'issues', 'repositories', 'subscriptions']
+
+    const pathParts = pathname.split('/')
+
+    if (pathParts.includes('posts')) {
+      return 'overview'
+    } else {
+      return tabs.find((tab) => pathParts.includes(tab)) ?? 'overview'
+    }
+  }, [pathname])
+
   return (
-    <Tabs className="flex min-h-screen flex-col justify-between">
+    <Tabs
+      className="flex min-h-screen flex-col justify-between"
+      value={currentTab}
+    >
       <div className="flex flex-col px-4 md:px-8">
         <div className="relative flex w-full flex-row items-center justify-between gap-x-24 md:justify-normal">
           <div className="shrink-0 md:w-64">

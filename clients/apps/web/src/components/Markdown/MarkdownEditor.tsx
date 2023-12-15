8a1d273d271012c8be4da6e728c5d500adb467a5
Emil Widlund <hello@emilwidlund.com>
2023-12-15 09:55:26 +0100

    clients/markdowneditor: extract helpers out to a separate hook

--- clients/apps/web/src/components/Markdown/MarkdownEditor.tsx
+++ clients/apps/web/src/components/Markdown/MarkdownEditor.tsx
@@ -1,15 +1,7 @@
-import { upload } from '@vercel/blob/client'
 import { TextArea } from 'polarkit/components/ui/atoms'
-import {
-  ChangeEventHandler,
-  ClipboardEvent,
-  DragEventHandler,
-  KeyboardEventHandler,
-  useCallback,
-  useEffect,
-  useRef,
-} from 'react'
+import { useEffect } from 'react'
 import { twMerge } from 'tailwind-merge'
+import { useEditorHelpers } from './useEditorHelpers'
 
 const uploadingText = 'Uploading...'
 
@@ -26,122 +18,21 @@ export const MarkdownEditor = ({
   onChange,
   autoFocus,
 }: MarkdownEditorProps) => {
-  const ref = useRef<HTMLTextAreaElement>(null)
-  const insertTextAtCursor = useCallback(
-    (text: string, element: HTMLTextAreaElement) => {
-      const cursorPosition = element.selectionStart
-
-      const textBeforeCursorPosition = element.value.substring(
-        0,
-        cursorPosition,
-      )
-      const textAfterCursorPosition = element.value.substring(
-        cursorPosition,
-        element.value.length,
-      )
-
-      element.value = textBeforeCursorPosition + text + textAfterCursorPosition
-
-      element.selectionStart = cursorPosition + text.length
-      element.selectionEnd = cursorPosition + text.length
-    },
-    [],
-  )
+  const {
+    ref,
+    handleChange,
+    handleDrag,
+    handleDragOver,
+    handleDrop,
+    handleKeyDown,
+    handlePaste,
+  } = useEditorHelpers(onChange)
 
   useEffect(() => {
     if (ref.current) {
       ref.current.style.height = ref.current.scrollHeight + 'px'
     }
   }, [value])
-
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback(
-    (e) => {
-      onChange?.(e.target.value)
-    },
-    [onChange],
-  )
-
-  const handleDrag: DragEventHandler<HTMLTextAreaElement> = useCallback((e) => {
-    e.preventDefault()
-    e.stopPropagation()
-  }, [])
-
-  const handleDrop: DragEventHandler<HTMLTextAreaElement> = useCallback(
-    async (e) => {
-      if (e.target instanceof HTMLTextAreaElement) {
-        e.preventDefault()
-        e.stopPropagation()
-
-        for (const file of e.dataTransfer.files) {
-          try {
-            insertTextAtCursor(uploadingText, e.target)
-
-            const newBlob = await upload(file.name, file, {
-              access: 'public',
-              handleUploadUrl: '/api/blob/upload',
-            })
-
-            const textToInsert = `![${newBlob.pathname}](${newBlob.url})`
-
-            e.target.value = e.target.value.replace(uploadingText, textToInsert)
-          } catch (err) {
-            e.target.value = e.target.value.replace(uploadingText, '')
-          } finally {
-            onChange?.(e.target.value)
-          }
-        }
-      }
-    },
-    [onChange, insertTextAtCursor],
-  )
-
-  const handlePaste = useCallback(
-    async (e: ClipboardEvent<HTMLTextAreaElement>) => {
-      if (e.target instanceof HTMLTextAreaElement) {
-        if (e.clipboardData.files.length > 0) {
-          e.preventDefault()
-          e.stopPropagation()
-        }
-
-        for (const file of e.clipboardData.files) {
-          try {
-            insertTextAtCursor(uploadingText, e.target)
-
-            const newBlob = await upload(file.name, file, {
-              access: 'public',
-              handleUploadUrl: '/api/blob/upload',
-            })
-
-            const textToInsert = `![${newBlob.pathname}](${newBlob.url})`
-
-            e.target.value = e.target.value.replace(uploadingText, textToInsert)
-          } catch (err) {
-            e.target.value = e.target.value.replace(uploadingText, '')
-          } finally {
-            onChange?.(e.target.value)
-          }
-        }
-      }
-    },
-    [onChange, insertTextAtCursor],
-  )
-
-  const handleKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = useCallback(
-    (e) => {
-      /** Handle tab presses */
-      if (e.key == 'Tab' && e.target instanceof HTMLTextAreaElement) {
-        e.preventDefault()
-        insertTextAtCursor('\t', e.target)
-        onChange?.(e.target.value)
-      }
-    },
-    [onChange, insertTextAtCursor],
-  )
-
-  const allow: DragEventHandler<HTMLTextAreaElement> = useCallback((e) => {
-    e.preventDefault()
-    e.stopPropagation()
-  }, [])
 
   return (
     <TextArea
@@ -156,7 +47,7 @@ export const MarkdownEditor = ({
       onChange={handleChange}
       onDrop={handleDrop}
       onDrag={handleDrag}
-      onDragOver={allow}
+      onDragOver={handleDragOver}
       onPaste={handlePaste}
       onKeyDown={handleKeyDown}
       autoFocus={autoFocus}

6f284d20b02c57a7ffa797ee9a25ef01d3293c51
Emil Widlund <hello@emilwidlund.com>
2023-12-18 14:34:25 +0100

    clients/products: add file upload manager

--- clients/packages/polarkit/src/components/ui/atoms/ShadowBoxOnMd.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/ShadowBoxOnMd.tsx
@@ -1,18 +1,19 @@
-import { PropsWithChildren } from 'react'
+import { DetailedHTMLProps } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-const ShadowBoxOnMd = (
-  props: PropsWithChildren<{
-    className?: string
-  }>,
-) => (
+const ShadowBoxOnMd = ({
+  className,
+  children,
+  ...props
+}: DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => (
   <div
     className={twMerge(
       'md:dark:bg-polar-900 md:dark:ring-polar-800 w-full md:rounded-xl md:bg-white md:p-8 md:shadow-lg md:dark:ring-1 lg:rounded-3xl',
-      props.className,
+      className,
     )}
+    {...props}
   >
-    {props.children}
+    {children}
   </div>
 )
 

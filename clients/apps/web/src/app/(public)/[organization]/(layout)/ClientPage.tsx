f2c62e89b6175cc6fe0c7fa34a9bc819d79b7bd6
Gustav Westling <gustav@polar.sh>
2023-12-19 12:44:38 +0100

    clients/posts: show RSS button with automatic auth for logged in users

--- clients/apps/web/src/app/(public)/[organization]/(layout)/ClientPage.tsx
+++ clients/apps/web/src/app/(public)/[organization]/(layout)/ClientPage.tsx
@@ -1,11 +1,19 @@
 'use client'
 
 import { Post as PostComponent } from '@/components/Feed/Posts/Post'
+import { Modal, ModalHeader } from '@/components/Modal'
+import { useModal } from '@/components/Modal/useModal'
 import IssuesLookingForFunding from '@/components/Organization/IssuesLookingForFunding'
+import Spinner from '@/components/Shared/Spinner'
 import { StaggerReveal } from '@/components/Shared/StaggerReveal'
+import { useAuth } from '@/hooks'
 import { isFeatureEnabled } from '@/utils/feature-flags'
-import { Article, Organization } from '@polar-sh/sdk'
-import { ShadowBoxOnMd } from 'polarkit/components/ui/atoms'
+import { RssIcon } from '@heroicons/react/24/outline'
+import { Article, CreatePersonalAccessTokenResponse, Organization } from '@polar-sh/sdk'
+import Link from 'next/link'
+import { api } from 'polarkit/api'
+import { Button, CopyToClipboardInput, ShadowBoxOnMd } from 'polarkit/components/ui/atoms'
+import { useEffect, useRef, useState } from 'react'
 
 const ClientPage = ({
   organization,
@@ -14,14 +22,39 @@ const ClientPage = ({
   organization: Organization
   posts: Article[]
 }) => {
+
+  const {
+    isShown: rssModalIsShown,
+    hide: hideRssModal,
+    show: showRssModal,
+  } = useModal() 
+
   return isFeatureEnabled('feed') ? (
+    <>
     <StaggerReveal className="flex max-w-xl flex-col gap-y-6">
       {posts.map((post) => (
         <StaggerReveal.Child key={post.id}>
           <PostComponent article={post} />
         </StaggerReveal.Child>
       ))}
+      <div>
+      <Button fullWidth={false} onClick={showRssModal} variant={"outline"}>
+        <RssIcon className='h-4 w-4 mr-2' />
+        <span>RSS</span></Button>
+      </div>
     </StaggerReveal>
+
+    <Modal
+        isShown={rssModalIsShown}
+        hide={hideRssModal}
+        modalContent={
+          <RssModal
+            hide={hideRssModal}
+            organization={organization}
+          />
+        }
+      />
+    </>
   ) : (
     <ShadowBoxOnMd>
       <div className="p-4">
@@ -35,3 +68,74 @@ const ClientPage = ({
 }
 
 export default ClientPage
+
+
+const RssModal = ({
+  hide,
+  organization,
+}: {
+  hide: () => void
+  organization: Organization
+}) => {
+
+  const {currentUser} = useAuth()
+  const [token, setToken] = useState<string>()
+  const auth = token ? `?auth=${token}` : ''
+  const url = `https://polar.sh/${organization.name}/rss${auth}`
+
+  useEffect(() => {
+    if (!currentUser) {
+      return
+    }
+
+    let active = true
+
+    api.personalAccessToken.create({
+      createPersonalAccessToken: {
+        comment: `RSS for ${organization.name}`,
+        scopes: ["articles:read"],
+      }
+    }).then((res: CreatePersonalAccessTokenResponse) => {
+      if (active) {
+        setToken(res.token)
+      }
+    })
+
+    return () => {
+      active = false
+    }
+  }, [currentUser])
+
+  return (
+    <>
+      <ModalHeader className="px-8 py-4" hide={hide}>
+        <h3 className="dark:text-polar-50 text-lg font-medium text-gray-950">
+          Subscribe to {organization.pretty_name || organization.name} via RSS
+        </h3>
+      </ModalHeader>
+      <div className="p-8">
+        <div className="flex flex-col gap-y-4">
+          
+          <div className="flex flex-col gap-y-2">
+            <span className="font-medium">{currentUser ? 'Your feed URL' : 'Feed URL'}</span>
+            {currentUser ? 
+            <p className="text-polar-500 dark:text-polar-500 text-sm">
+              This URL is personal, keep it safe.
+            </p> : null}
+          </div>
+
+
+
+        {url ? <div className='flex items-center gap-2'>
+          <CopyToClipboardInput value={url} id={'rssurl'} />
+            <Link href={`feed:${url}`}>
+          <Button asChild>Open</Button>
+          </Link>
+          </div> : <Spinner />}
+
+          
+        </div>
+      </div>
+    </>
+  )
+}
--- clients/packages/sdk/src/client/apis/UsersApi.ts
+++ clients/packages/sdk/src/client/apis/UsersApi.ts
@@ -19,6 +19,7 @@ import type {
   LoginResponse,
   LogoutResponse,
   UserRead,
+  UserScopes,
   UserSetAccount,
   UserStripePortalSession,
   UserUpdateSettings,
@@ -170,6 +171,40 @@ export class UsersApi extends runtime.BaseAPI {
      */
     async logout(initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<LogoutResponse> {
         const response = await this.logoutRaw(initOverrides);
+        return await response.value();
+    }
+
+    /**
+     * Scopes
+     */
+    async scopesRaw(initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<UserScopes>> {
+        const queryParameters: any = {};
+
+        const headerParameters: runtime.HTTPHeaders = {};
+
+        if (this.configuration && this.configuration.accessToken) {
+            const token = this.configuration.accessToken;
+            const tokenString = await token("HTTPBearer", []);
+
+            if (tokenString) {
+                headerParameters["Authorization"] = `Bearer ${tokenString}`;
+            }
+        }
+        const response = await this.request({
+            path: `/api/v1/users/me/scopes`,
+            method: 'GET',
+            headers: headerParameters,
+            query: queryParameters,
+        }, initOverrides);
+
+        return new runtime.JSONApiResponse(response);
+    }
+
+    /**
+     * Scopes
+     */
+    async scopes(initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<UserScopes> {
+        const response = await this.scopesRaw(initOverrides);
         return await response.value();
     }
 

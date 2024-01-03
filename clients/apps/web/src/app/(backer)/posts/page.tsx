654de0725bff3287a73bdf653c320a149e89276c
Emil Widlund <hello@emilwidlund.com>
2024-01-03 11:58:00 +0100

    clients/feed: remove maintainers to follow module

--- clients/apps/web/src/app/(backer)/posts/page.tsx
+++ clients/apps/web/src/app/(backer)/posts/page.tsx
@@ -6,12 +6,6 @@ export default async function Page() {
       <div className="flex w-full max-w-xl flex-col gap-y-8 pb-12">
         <Feed />
       </div>
-      <div className="hidden w-full max-w-sm flex-col gap-y-6 pt-6 md:flex">
-        <h3 className="dark:text-polar-50 text-md text-gray-950">
-          Maintainers you may know
-        </h3>
-        <div className="dark:bg-polar-900 dark:border-polar-800 h-[160px] w-full rounded-2xl border border-gray-100 bg-white" />
-      </div>
     </div>
   )
 }

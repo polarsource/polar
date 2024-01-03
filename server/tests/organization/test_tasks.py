7b8ad9b1ea02640f94e1e911bae6a92c5d82d83b
Gustav Westling <gustav@polar.sh>
2024-01-03 12:16:26 +0100

    server/organization: expunge after setup

--- server/tests/organization/test_tasks.py
+++ server/tests/organization/test_tasks.py
@@ -3,24 +3,35 @@ import uuid
 import pytest
 from pytest_mock import MockerFixture
 
+from polar.kit.db.postgres import AsyncSession
 from polar.models import Organization, SubscriptionBenefit
 from polar.models.subscription_benefit import SubscriptionBenefitType
-from polar.organization.tasks import (  # type: ignore[attr-defined]
+from polar.organization.tasks import (
     OrganizationDoesNotExist,
     organization_post_install,
-    subscription_benefit_service,
-    subscription_tier_service,
 )
 from polar.subscription.service.subscription_benefit import SubscriptionBenefitService
+from polar.subscription.service.subscription_benefit import (
+    subscription_benefit as subscription_benefit_service,
+)
 from polar.subscription.service.subscription_tier import SubscriptionTierService
+from polar.subscription.service.subscription_tier import (
+    subscription_tier as subscription_tier_service,
+)
 from polar.worker import JobContext, PolarWorkerContext
 
 
 @pytest.mark.asyncio
 class TestOrganizationPostInstall:
     async def test_not_existing_organization(
-        self, job_context: JobContext, polar_worker_context: PolarWorkerContext
+        self,
+        job_context: JobContext,
+        polar_worker_context: PolarWorkerContext,
+        session: AsyncSession,
     ) -> None:
+        # then
+        session.expunge_all()
+
         with pytest.raises(OrganizationDoesNotExist):
             await organization_post_install(
                 job_context, uuid.uuid4(), polar_worker_context
@@ -32,6 +43,7 @@ class TestOrganizationPostInstall:
         job_context: JobContext,
         polar_worker_context: PolarWorkerContext,
         organization: Organization,
+        session: AsyncSession,
     ) -> None:
         subscription_benefit = SubscriptionBenefit(
             type=SubscriptionBenefitType.articles,
@@ -54,6 +66,9 @@ class TestOrganizationPostInstall:
             "create_free",
             spec=SubscriptionTierService.create_free,
         )
+
+        # then
+        session.expunge_all()
 
         await organization_post_install(
             job_context, organization.id, polar_worker_context

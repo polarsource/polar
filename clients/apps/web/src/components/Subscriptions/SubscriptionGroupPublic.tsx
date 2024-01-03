d4a9142681edaf164418813bb771f526ce3bb312
Emil Widlund <hello@emilwidlund.com>
2024-01-03 09:45:31 +0100

    clients/tiers: rework the tier layout

--- clients/apps/web/src/components/Subscriptions/SubscriptionGroupPublic.tsx
+++ clients/apps/web/src/components/Subscriptions/SubscriptionGroupPublic.tsx
@@ -30,21 +30,21 @@ const SubscriptionGroupPublic = ({
   }
 
   return (
-    <div className="flex flex-col gap-6">
-      <div>
+    <div className="flex flex-row gap-12">
+      <div className="w-64 flex-shrink-0">
         <div className="flex items-center justify-between">
           <h2 className="flex flex-row items-center text-lg font-medium">
             <SubscriptionGroupIcon type={type} className="!h-6 !w-6" />
-            <span className="dark:text-polar-50 ml-2">{title}</span>
+            <span className="dark:text-polar-50 ml-1">{title}</span>
           </h2>
         </div>
         <p className="dark:text-polar-500 mt-2 text-gray-500">{description}</p>
       </div>
 
-      <div className="-mx-10 flex h-fit flex-row flex-wrap gap-6 overflow-x-auto px-10 py-6 lg:mx-0 lg:overflow-x-visible lg:px-0 lg:py-2">
+      <div className="flex h-fit flex-row flex-wrap gap-6 overflow-x-auto px-10 py-6 lg:mx-0 lg:overflow-x-visible lg:px-0 lg:py-2">
         {tiers.map((tier) => (
           <SubscriptionTierCard
-            className="w-full self-stretch lg:max-w-[286px]"
+            className="w-full self-stretch lg:w-[286px]"
             key={tier.id}
             subscriptionTier={tier}
             variant="small"

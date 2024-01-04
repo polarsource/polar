70589d5e431aa7268966a4a5959ce0b5b26eff7d
Gustav Westling <gustav@polar.sh>
2024-01-04 17:35:58 +0100

    clients: wrap skeleton in DashboardBody

--- clients/apps/web/src/app/maintainer/[organization]/(topbar)/loading.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/(topbar)/loading.tsx
@@ -1,13 +1,16 @@
+import { DashboardBody } from '@/components/Layout/DashboardLayout'
 import { Skeleton } from 'polarkit/components/ui/skeleton'
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-2 p-8">
-      <Skeleton className="h-20 w-full" />
-      <Skeleton className="h-20 w-full" />
-      <Skeleton className="h-20 w-full" />
-      <Skeleton className="h-20 w-full" />
-      <Skeleton className="h-20 w-full" />
-    </div>
+    <DashboardBody>
+      <div className="flex flex-col gap-2 p-8">
+        <Skeleton className="h-20 w-full" />
+        <Skeleton className="h-20 w-full" />
+        <Skeleton className="h-20 w-full" />
+        <Skeleton className="h-20 w-full" />
+        <Skeleton className="h-20 w-full" />
+      </div>
+    </DashboardBody>
   )
 }

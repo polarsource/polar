a969890efc8b9e88a58b064cd0747d737e45ea12
Gustav Westling <gustav@polar.sh>
2024-01-04 11:38:24 +0100

    clients/backoffice/rewards: restructure data

--- clients/apps/web/src/components/Backoffice/RewardsPending.tsx
+++ clients/apps/web/src/components/Backoffice/RewardsPending.tsx
@@ -16,7 +16,7 @@ import {
   useBackofficeRewardsPending,
 } from 'polarkit/hooks'
 import { getCentsInDollarString } from 'polarkit/money'
-import { useMemo } from 'react'
+import { Fragment, useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 const Pledges = () => {
@@ -72,6 +72,16 @@ const Pledges = () => {
     await createInvoice.mutateAsync({ pledgeId })
   }
 
+  const pledgeRewardsGroupBorder = (idx: number): string => {
+    const c = [
+      'border-emerald-500',
+      'border-violet-500',
+      'border-purple-500',
+      'border-orange-500',
+    ]
+    return c[idx % c.length]
+  }
+
   return (
     <div className="space-y-4">
       {issueList.map((i) => (
@@ -111,143 +121,191 @@ const Pledges = () => {
             <a
               href={`https://github.com/${i[0][0].pledge.issue.repository.organization.name}/${i[0][0].pledge.issue.repository.name}/issues/${i[0][0].pledge.issue.number}`}
             >
-              <Button size="sm">
+              <Button size="sm" variant={'outline'}>
                 <span>GitHub</span>
-                <ArrowTopRightOnSquareIcon />
+                <ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
               </Button>
             </a>
           </div>
-          <div className="flex flex-col gap-2 p-4">
-            {i.map((p) => (
-              <div
-                className="flex flex-col gap-2"
-                key={p[0].issue_reward_id + p[0].pledge.id}
-              >
-                <div className="flex items-center gap-2">
-                  <ArrowLeftIcon className="h-4 w-4 text-green-600" />
-                  <div>
-                    $
-                    {getCentsInDollarString(
-                      p[0].pledge.amount.amount,
-                      true,
-                      true,
-                    )}{' '}
-                    from{' '}
-                  </div>
-                  {p[0].pledge.pledger?.avatar_url && (
-                    <img
-                      className="h-6 w-6"
-                      src={p[0].pledge.pledger.avatar_url}
-                    />
+          <table className="w-full table-auto p-4">
+            <tr>
+              <th>From/To</th>
+              <th>State</th>
+              <th>Actions</th>
+              <th>Scheduled At</th>
+            </tr>
+            {i.map((p, idx) => (
+              <Fragment key={p[0].issue_reward_id + p[0].pledge.id}>
+                <tr
+                  className={twMerge(
+                    'border-l-4 p-2',
+                    pledgeRewardsGroupBorder(idx),
                   )}
-                  <div className="underline">
-                    {p[0].pledge.pledger?.github_username ||
-                      p[0].pledge.pledger?.name ||
-                      'Anonymous'}
-                  </div>
-                  <div>{p[0].pledger_email}</div>
-                  <div
-                    className={twMerge(
-                      'flex items-center rounded-full px-2 text-sm text-white',
-                      p[0].pledge.state === PledgeState.CHARGE_DISPUTED ||
-                        p[0].pledge.state === PledgeState.DISPUTED
-                        ? 'bg-red-700'
-                        : '',
-                      p[0].pledge.state === PledgeState.PENDING
-                        ? 'bg-green-700'
-                        : '',
-                      p[0].pledge.state === PledgeState.CREATED
-                        ? 'bg-blue-300'
-                        : '',
-                    )}
-                  >
-                    state={p[0].pledge.state}
-                  </div>
-                  <div
-                    className={twMerge(
-                      'flex items-center rounded-full px-2 text-sm text-white',
-                      p[0].pledge.type === PledgeType.UPFRONT
-                        ? 'bg-green-700'
-                        : '',
-                      p[0].pledge.type === PledgeType.ON_COMPLETION
-                        ? 'bg-red-700'
-                        : '',
-                    )}
-                  >
-                    type={p[0].pledge.type}
-                  </div>
-
-                  {p[0].pledge.type === PledgeType.ON_COMPLETION && (
-                    <>
-                      {p[0].pledge.hosted_invoice_url ? (
-                        <a
-                          href={p[0].pledge.hosted_invoice_url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <Button size="sm">
-                            <span>Open Invoice</span>
-                            <ArrowTopRightOnSquareIcon />
-                          </Button>
-                        </a>
-                      ) : (
-                        <Button
-                          color="gray"
-                          size="sm"
-                          onClick={() => onClickCreateInvoice(p[0].pledge.id)}
-                        >
-                          <span>Send Invoice</span>
-                          <BanknotesIcon />
-                        </Button>
-                      )}
-                    </>
-                  )}
-
-                  <a
-                    href={`https://dashboard.stripe.com/payments/${p[0].pledge_payment_id}`}
-                  >
-                    <Button>
-                      <span>Payment</span>
-                      <ArrowTopRightOnSquareIcon />
-                    </Button>
-                  </a>
-
-                  <div>
-                    scheduled_payout_at ={' '}
-                    {p[0].pledge.scheduled_payout_at?.toString()}
-                  </div>
-                </div>
-
-                {p.map((r) => (
-                  <div
-                    className="flex items-center gap-2"
-                    key={r.issue_reward_id}
-                  >
-                    <ArrowRightIcon className="h-4 w-4 text-blue-500" />
-                    <div>
-                      ${getCentsInDollarString(r.amount.amount, true, true)} to
-                    </div>
-                    {r.user && (
-                      <>
-                        <img className="h-6 w-6" src={r.user.avatar_url} />
-                        <div className="underline">{r.user.username}</div>
-                      </>
-                    )}
-                    {r.organization && (
-                      <>
+                >
+                  <td className="py-2">
+                    <div className="flex items-center gap-2">
+                      <ArrowLeftIcon className="h-4 w-4 text-green-600" />
+                      <div>
+                        $
+                        {getCentsInDollarString(
+                          p[0].pledge.amount.amount,
+                          true,
+                          true,
+                        )}{' '}
+                        from{' '}
+                      </div>
+                      {p[0].pledge.pledger?.avatar_url && (
                         <img
                           className="h-6 w-6"
-                          src={r.organization.avatar_url}
+                          src={p[0].pledge.pledger.avatar_url}
                         />
-                        <div className="underline">{r.organization.name}</div>
-                      </>
+                      )}
+                      <div className="underline">
+                        {p[0].pledge.pledger?.github_username ||
+                          p[0].pledge.pledger?.name ||
+                          'Anonymous'}
+                      </div>
+                      <div>{p[0].pledger_email}</div>
+                    </div>
+                  </td>
+
+                  {/* State */}
+                  <td>
+                    <div className="flex items-center gap-2">
+                      <div
+                        className={twMerge(
+                          'inline-flex items-center rounded-full px-2 text-sm text-white',
+                          p[0].pledge.state === PledgeState.CHARGE_DISPUTED ||
+                            p[0].pledge.state === PledgeState.DISPUTED
+                            ? 'bg-red-700'
+                            : '',
+                          p[0].pledge.state === PledgeState.PENDING
+                            ? 'bg-green-700'
+                            : '',
+                          p[0].pledge.state === PledgeState.CREATED
+                            ? 'bg-blue-300'
+                            : '',
+                        )}
+                      >
+                        state={p[0].pledge.state}
+                      </div>
+
+                      <div
+                        className={twMerge(
+                          'inline-flex items-center rounded-full px-2 text-sm text-white',
+                          p[0].pledge.type === PledgeType.UPFRONT
+                            ? 'bg-green-700'
+                            : '',
+                          p[0].pledge.type === PledgeType.ON_COMPLETION
+                            ? 'bg-red-700'
+                            : '',
+                        )}
+                      >
+                        type={p[0].pledge.type}
+                      </div>
+                    </div>
+                  </td>
+
+                  {/* Actions */}
+                  <td>
+                    <div className="flex gap-2">
+                      {p[0].pledge.type === PledgeType.ON_COMPLETION && (
+                        <>
+                          {p[0].pledge.hosted_invoice_url ? (
+                            <a
+                              href={p[0].pledge.hosted_invoice_url}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <Button size="sm" variant={'outline'}>
+                                <span className="whitespace-nowrap">
+                                  Invoice
+                                </span>
+                                <ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
+                              </Button>
+                            </a>
+                          ) : (
+                            <Button
+                              color="gray"
+                              size="sm"
+                              onClick={() =>
+                                onClickCreateInvoice(p[0].pledge.id)
+                              }
+                            >
+                              <span className="whitespace-nowrap">
+                                Send Invoice
+                              </span>
+                              <BanknotesIcon className="ml-2 h-5 w-5" />
+                            </Button>
+                          )}
+                        </>
+                      )}
+
+                      <a
+                        href={`https://dashboard.stripe.com/payments/${p[0].pledge_payment_id}`}
+                      >
+                        <Button size="sm" variant={'outline'}>
+                          <span>Payment</span>
+                          <ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
+                        </Button>
+                      </a>
+                    </div>
+                  </td>
+
+                  <td>{p[0].pledge.scheduled_payout_at?.toString()}</td>
+                </tr>
+
+                {p.map((r) => (
+                  <tr
+                    key={r.issue_reward_id}
+                    className={twMerge(
+                      'border-l-4 bg-gray-200 p-2',
+                      pledgeRewardsGroupBorder(idx),
                     )}
-                    {!r.paid_at && (
-                      <>
-                        <div className="flex items-center rounded-full bg-green-400 px-2 text-sm text-white">
+                  >
+                    <td>
+                      <div className="flex items-center gap-2">
+                        <ArrowRightIcon className="h-4 w-4 text-blue-500" />
+                        <div>
+                          ${getCentsInDollarString(r.amount.amount, true, true)}{' '}
+                          to
+                        </div>
+                        {r.user && (
+                          <>
+                            <img className="h-6 w-6" src={r.user.avatar_url} />
+                            <div className="underline">{r.user.username}</div>
+                          </>
+                        )}
+                        {r.organization && (
+                          <>
+                            <img
+                              className="h-6 w-6"
+                              src={r.organization.avatar_url}
+                            />
+                            <div className="underline">
+                              {r.organization.name}
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    </td>
+
+                    {/* State */}
+                    <td>
+                      {r.paid_at ? (
+                        <div className="inline-flex items-center rounded-full bg-green-400 px-2 text-sm text-white">
+                          Paid {r.paid_at.toString()}
+                        </div>
+                      ) : (
+                        <div className="inline-flex items-center rounded-full bg-green-400 px-2 text-sm text-white">
                           not paid out
                         </div>
+                      )}
+                    </td>
 
+                    {/* Actions */}
+                    <td>
+                      {!r.paid_at && (
                         <Button
                           size="sm"
                           onClick={async () => {
@@ -260,22 +318,20 @@ const Pledges = () => {
                             alert('paid!')
                           }}
                         >
-                          <span>Create transfer</span>
-                          <CurrencyDollarIcon />
+                          <span className="whitespace-nowrap">
+                            Create transfer
+                          </span>
+                          <CurrencyDollarIcon className="ml-2 h-4 w-4 " />
                         </Button>
-                      </>
-                    )}
+                      )}
+                    </td>
 
-                    {r.paid_at && (
-                      <div className="flex items-center rounded-full bg-green-400 px-2 text-sm text-white">
-                        Paid {r.paid_at.toString()}
-                      </div>
-                    )}
-                  </div>
+                    <td>&nbsp;</td>
+                  </tr>
                 ))}
-              </div>
+              </Fragment>
             ))}
-          </div>
+          </table>
         </div>
       ))}
     </div>

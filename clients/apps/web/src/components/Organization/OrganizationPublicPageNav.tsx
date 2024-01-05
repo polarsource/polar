05f18fad0b3ef4b8da1d817cc65bb453b81d3fc1
Emil Widlund <hello@emilwidlund.com>
2024-01-05 13:42:52 +0100

    clients/maintainer: add navigation to maintainer pages on mobile

--- clients/apps/web/src/components/Organization/OrganizationPublicPageNav.tsx
+++ clients/apps/web/src/components/Organization/OrganizationPublicPageNav.tsx
@@ -1,25 +1,21 @@
 'use client'
 
-import { useAuth } from '@/hooks'
 import { isFeatureEnabled } from '@/utils/feature-flags'
-import { Organization, UserSignupType } from '@polar-sh/sdk'
+import { Organization } from '@polar-sh/sdk'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { TabsList, TabsTrigger } from 'polarkit/components/ui/atoms'
 import { useSubscriptionTiers } from 'polarkit/hooks'
-import GithubLoginButton from '../Shared/GithubLoginButton'
-import { ProfileMenu } from '../Shared/ProfileSelection'
+import { twMerge } from 'tailwind-merge'
 
 interface OrganizationPublicPageNavProps {
+  className?: string
   organization: Organization
 }
 
 export const OrganizationPublicPageNav = ({
   organization,
+  className,
 }: OrganizationPublicPageNavProps) => {
-  const { currentUser } = useAuth()
-  const pathname = usePathname()
-
   const { data: { items: subscriptionTiers } = { items: [] } } =
     useSubscriptionTiers(organization.name, 100)
 
@@ -27,45 +23,33 @@ export const OrganizationPublicPageNav = ({
   const shouldRenderSubscriptionsTab = (paidTiers?.length ?? 0) > 0
 
   return (
-    <div className="flex flex-row items-center justify-between md:w-full">
-      <TabsList className="dark:border-polar-700 hidden dark:border md:flex">
-        <Link href={`/${organization.name}`}>
-          <TabsTrigger value="overview" size="small">
-            Overview
+    <TabsList
+      className={twMerge('dark:border-polar-700 flex dark:border', className)}
+    >
+      <Link href={`/${organization.name}`}>
+        <TabsTrigger value="overview" size="small">
+          Overview
+        </TabsTrigger>
+      </Link>
+      {isFeatureEnabled('subscriptions') && shouldRenderSubscriptionsTab && (
+        <Link href={`/${organization.name}/subscriptions`}>
+          <TabsTrigger value="subscriptions" size="small">
+            Subscriptions
           </TabsTrigger>
         </Link>
-        {isFeatureEnabled('subscriptions') && shouldRenderSubscriptionsTab && (
-          <Link href={`/${organization.name}/subscriptions`}>
-            <TabsTrigger value="subscriptions" size="small">
-              Subscriptions
-            </TabsTrigger>
-          </Link>
-        )}
-        {isFeatureEnabled('feed') && (
-          <Link href={`/${organization.name}/issues`}>
-            <TabsTrigger value="issues" size="small">
-              Issues
-            </TabsTrigger>
-          </Link>
-        )}
-        <Link href={`/${organization.name}/repositories`}>
-          <TabsTrigger value="repositories" size="small">
-            Repositories
-          </TabsTrigger>
-        </Link>
-      </TabsList>
-      {currentUser ? (
-        <ProfileMenu className="z-50" />
-      ) : (
-        <GithubLoginButton
-          userSignupType={UserSignupType.BACKER}
-          posthogProps={{
-            view: 'Maintainer Page',
-          }}
-          text="Sign in with GitHub"
-          returnTo={pathname || '/feed'}
-        />
       )}
-    </div>
+      {isFeatureEnabled('feed') && (
+        <Link href={`/${organization.name}/issues`}>
+          <TabsTrigger value="issues" size="small">
+            Issues
+          </TabsTrigger>
+        </Link>
+      )}
+      <Link href={`/${organization.name}/repositories`}>
+        <TabsTrigger value="repositories" size="small">
+          Repositories
+        </TabsTrigger>
+      </Link>
+    </TabsList>
   )
 }

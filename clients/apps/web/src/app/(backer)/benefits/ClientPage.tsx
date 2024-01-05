20e28d51ea0facc12bc5fac534887c16cfa45ab9
Gustav Westling <gustav@polar.sh>
2024-01-05 13:16:22 +0100

    clients/benefits: make Buttons asChild
    Makes buttons behave as links

--- clients/apps/web/src/app/(backer)/benefits/ClientPage.tsx
+++ clients/apps/web/src/app/(backer)/benefits/ClientPage.tsx
@@ -85,12 +85,14 @@ const Subscription = ({
         </div>
         <div className="flex flex-row items-center gap-x-2">
           <Link href={`/settings`}>
-            <Button size="sm" variant="secondary">
+            <Button size="sm" variant="secondary" asChild>
               Manage
             </Button>
           </Link>
           <Link href={`/${org?.name}/subscriptions`}>
-            <Button size="sm">Upgrade</Button>
+            <Button size="sm" asChild>
+              Upgrade
+            </Button>
           </Link>
         </div>
       </div>

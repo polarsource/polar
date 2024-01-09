ed7d9955a1e447dbf58103187a58346b85191918
Emil Widlund <hello@emilwidlund.com>
2024-01-09 14:27:41 +0100

    clients/temp: force action rerun

--- clients/apps/web/src/components/Feed/LongformPost.tsx
+++ clients/apps/web/src/components/Feed/LongformPost.tsx
@@ -158,7 +158,7 @@ export default function LongformPost({
           transition={revealTransition}
           variants={animationVariants}
         >
-          <div className="dark:bg-polar-800 flex flex-col items-center gap-y-6 rounded-3xl bg-gray-100 p-8 py-12 md:px-16">
+          <div className="dark:bg-polar-800 flex flex-col items-center gap-y-6 rounded-3xl bg-gray-100 p-8 py-12 md:px-16 ">
             <Avatar
               className="h-12 w-12"
               avatar_url={article.organization.avatar_url}

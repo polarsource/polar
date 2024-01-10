9c4cb2a7f6a240cad09c82399a232c90bff90728
François Voron <fvoron@gmail.com>
2024-01-10 15:58:36 +0100

    server/subscription: tweak email detection in CSV import

--- server/polar/kit/csv.py
+++ server/polar/kit/csv.py
@@ -1,21 +1,37 @@
 import csv
+from collections.abc import Iterable
+from typing import BinaryIO
+
+from email_validator import EmailNotValidError, validate_email
 
 
-def get_emails_from_csv(contents: str) -> list[str]:
-    reader = csv.DictReader(contents.splitlines())
+def get_iterable_from_binary_io(file: BinaryIO) -> Iterable[str]:
+    for line in file:
+        yield line.decode("utf-8")
 
-    res: list[str] = [
-        e
-        for e in [
-            # get with fallback
-            row.get("email", None)
-            or row.get("EMAIL", None)
-            or row.get("Email", None)
-            # Mailchimp uses "Email Address"
-            or row.get("Email Address", None)
-            or None
-            for row in reader
-        ]
-        if e and "@" in e
-    ]
-    return res
+
+def get_emails_from_csv(lines: Iterable[str]) -> set[str]:
+    emails: set[str] = set()
+
+    reader = csv.DictReader(lines)
+    if reader.fieldnames is None:
+        return emails
+
+    try:
+        email_field = next(
+            field for field in reader.fieldnames if "email" in field.lower()
+        )
+    except StopIteration:
+        return emails
+
+    for row in reader:
+        email = row.get(email_field)
+        if email is not None:
+            try:
+                validate_email(email, check_deliverability=False)
+            except EmailNotValidError:
+                continue
+            else:
+                emails.add(email)
+
+    return emails
--- server/tests/kit/test_csv.py
+++ server/tests/kit/test_csv.py
@@ -6,36 +6,44 @@ from polar.kit.csv import get_emails_from_csv
 @pytest.mark.asyncio
 async def test_get_emails_from_csv() -> None:
     assert get_emails_from_csv(
-        """name,email
-hello world,foo@example.com
-froop
-bar,bar@example.com
-baz,bazexample.com"""
-    ) == ["foo@example.com", "bar@example.com"]
+        [
+            "name,email",
+            "hello world,foo@example.com",
+            "froop",
+            "bar,bar@example.com",
+            "baz,bazexample.com",
+        ]
+    ) == {"foo@example.com", "bar@example.com"}
 
     assert (
         get_emails_from_csv(
-            """name,not-email
-hello world,foo@example.com
-froop
-bar,bar@example.com
-baz,bazexample.com"""
+            [
+                "name,foo",
+                "hello world,foo@example.com",
+                "froop",
+                "bar,bar@example.com",
+                "baz,bazexample.com",
+            ]
         )
-        == []
+        == set()
     )
 
     assert get_emails_from_csv(
-        """name,EMAIL
-hello world,foo@example.com
-froop
-bar,bar@example.com
-baz,bazexample.com"""
-    ) == ["foo@example.com", "bar@example.com"]
+        [
+            "name,EMAIL",
+            "hello world,foo@example.com",
+            "froop",
+            "bar,bar@example.com",
+            "baz,bazexample.com",
+        ]
+    ) == {"foo@example.com", "bar@example.com"}
 
     assert get_emails_from_csv(
-        """name,Email
-hello world,foo@example.com
-froop
-bar,bar@example.com
-baz,bazexample.com"""
-    ) == ["foo@example.com", "bar@example.com"]
+        [
+            "name,Email",
+            "hello world,foo@example.com",
+            "froop",
+            "bar,bar@example.com",
+            "baz,bazexample.com",
+        ]
+    ) == {"foo@example.com", "bar@example.com"}

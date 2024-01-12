24e7a28ddaabfbf3db2daf0cd3625f1c47c31739
François Voron <fvoron@gmail.com>
2024-01-12 10:46:04 +0100

    clients/web: show custom note in Benefits page

--- clients/apps/web/src/app/(backer)/benefits/page.tsx
+++ clients/apps/web/src/app/(backer)/benefits/page.tsx
@@ -4,26 +4,11 @@ import ClientPage from './ClientPage'
 
 export default async function Page() {
   const api = getServerSideAPI()
-
-  const user = await api.users.getAuthenticated()
-
   const [subscriptions] = await Promise.all([
-    api.subscriptions.searchSubscriptions({
-      organizationName: undefined,
-      subscriberUserId: user.id,
-      active: true,
+    api.subscriptions.searchSubscribedSubscriptions({
       limit: 100,
       platform: Platforms.GITHUB,
     }),
   ])
-
-  const subscriptionTiers = await Promise.all(
-    subscriptions.items?.map((subscription) =>
-      api.subscriptions.lookupSubscriptionTier({
-        subscriptionTierId: subscription.subscription_tier_id,
-      }),
-    ) ?? [],
-  )
-
-  return <ClientPage subscriptionTiers={subscriptionTiers} />
+  return <ClientPage subscriptions={subscriptions.items || []} />
 }

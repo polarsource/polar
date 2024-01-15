2f715deac10646c214b984ac4ae6cd4f38dc18d7
Gustav Westling <gustav@polar.sh>
2024-01-15 16:35:24 +0100

    clients/public: prevent content shifts on navigation
    Make all public pages look have the same layout, even if the content height differs

--- clients/apps/web/src/app/(public)/[organization]/(layout)/layout.tsx
+++ clients/apps/web/src/app/(public)/[organization]/(layout)/layout.tsx
@@ -55,7 +55,7 @@ export default async function Layout({
   return (
     <EmptyLayout>
       <div className="flex min-h-screen flex-col justify-between">
-        <div className="flex flex-col">
+        <div className="flex shrink-0 flex-col">
           <div className="dark:bg-polar-900 sticky top-0 z-50 bg-white px-4 py-4 shadow-sm md:px-8">
             <div className="relative mx-auto flex w-full max-w-7xl flex-row items-center justify-between gap-x-24 md:justify-normal md:space-y-0 lg:px-0">
               <div className="shrink-0 md:w-64">
@@ -74,8 +74,8 @@ export default async function Layout({
             </div>
           </div>
 
-          <div className="mx-auto mb-16 mt-4 flex w-full max-w-7xl flex-col space-y-8 px-4 lg:px-0">
-            <div className="flex w-full flex-col gap-8 py-6 md:flex-row md:gap-24">
+          <div className="mx-auto mb-16 mt-4 flex w-full max-w-7xl shrink-0 flex-col space-y-8 px-4 lg:px-0">
+            <div className="flex w-full shrink-0 flex-col gap-8 py-6 md:min-h-screen md:flex-row md:gap-24">
               <OrganizationPublicSidebar organization={organization} />
               <div className="-mx-4 flex flex-row overflow-x-auto px-4 pb-4 md:hidden">
                 <OrganizationPublicPageNav

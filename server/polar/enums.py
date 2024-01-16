023ada9f9059107afc8d86ba2846abfc2ac89441
Gustav Westling <gustav@polar.sh>
2024-01-16 09:23:36 +0100

    server: remove __get_pydantic_json_schema__ workaround

--- server/polar/enums.py
+++ server/polar/enums.py
@@ -1,22 +1,8 @@
 from enum import StrEnum
 
-from pydantic import GetJsonSchemaHandler
-from pydantic.json_schema import JsonSchemaValue
-from pydantic_core import CoreSchema
-
 
 class Platforms(StrEnum):
     github = "github"
-
-    @classmethod
-    def __get_pydantic_json_schema__(
-        cls, core_schema: CoreSchema, handler: GetJsonSchemaHandler
-    ) -> JsonSchemaValue:
-        """
-        Workaround to force Pydantic to generate an enum schema
-        even if there is only one item.
-        """
-        return {"enum": ["github"], "type": "string", "title": "Platforms"}
 
 
 class UserSignupType(StrEnum):

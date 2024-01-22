d0c242a540014f9053067836c7841a65ce2e1c3f
François Voron <fvoron@gmail.com>
2024-01-22 10:47:36 +0100

    server/user: prepare OAuthAccount to receive other platforms

--- server/polar/user/oauth_service.py
+++ server/polar/user/oauth_service.py
@@ -2,10 +2,10 @@ from uuid import UUID
 
 import structlog
 
-from polar.enums import Platforms
 from polar.kit.services import ResourceServiceReader
 from polar.logging import Logger
 from polar.models import OAuthAccount
+from polar.models.user import OAuthPlatform
 from polar.postgres import AsyncSession
 
 log: Logger = structlog.get_logger()
@@ -13,12 +13,12 @@ log: Logger = structlog.get_logger()
 
 class OAuthAccountService(ResourceServiceReader[OAuthAccount]):
     async def get_by_platform_and_account_id(
-        self, session: AsyncSession, platform: Platforms, account_id: str
+        self, session: AsyncSession, platform: OAuthPlatform, account_id: str
     ) -> OAuthAccount | None:
         return await self.get_by(session, platform=platform, account_id=account_id)
 
     async def get_by_platform_and_user_id(
-        self, session: AsyncSession, platform: Platforms, user_id: UUID
+        self, session: AsyncSession, platform: OAuthPlatform, user_id: UUID
     ) -> OAuthAccount | None:
         return await self.get_by(session, platform=platform, user_id=user_id)
 

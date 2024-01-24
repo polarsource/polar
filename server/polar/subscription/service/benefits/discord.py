7a0a1aefb72d1aef483bfab2b13a508bdf44ccaa
François Voron <fvoron@gmail.com>
2024-01-24 14:21:23 +0100

    server/subscriptions: implement Discord benefit precondition error notification

--- server/polar/subscription/service/benefits/discord.py
+++ server/polar/subscription/service/benefits/discord.py
@@ -3,6 +3,7 @@ from typing import Any
 import httpx
 import structlog
 
+from polar.config import settings
 from polar.integrations.discord.service import DiscordAccountNotConnected
 from polar.integrations.discord.service import discord_bot as discord_bot_service
 from polar.integrations.discord.service import discord_user as discord_user_service
@@ -12,6 +13,9 @@ from polar.models.subscription_benefit import (
     SubscriptionBenefitDiscord,
     SubscriptionBenefitDiscordProperties,
 )
+from polar.notifications.notification import (
+    SubscriptionBenefitPreconditionErrorNotificationContextualPayload,
+)
 
 from .base import (
     SubscriptionBenefitPreconditionError,
@@ -20,6 +24,42 @@ from .base import (
 )
 
 log: Logger = structlog.get_logger()
+
+precondition_error_subject_template = (
+    "Action required: get access to {organization_name}'s Discord server"
+)
+precondition_error_body_template = """
+<h1>Hi,</h1>
+<p>You just subscribed to {organization_name}'s plan, <strong>{subscription_tier_name}</strong>. Thank you!</p>
+<p>As you may now, it includes an access to a private Discord server. To grant you access, we need you to link your Discord account on Polar.</p>
+<p>Once done, you'll automatically be added to {organization_name}'s Discord server.</p>
+<!-- Action -->
+<table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td align="center">
+            <!-- Border based button
+https://litmus.com/blog/a-guide-to-bulletproof-buttons-in-email-design -->
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation">
+                <tr>
+                    <td align="center">
+                        <a href="{extra_context[url]}" class="f-fallback button">Sign in to Polar</a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<!-- Sub copy -->
+<table class="body-sub" role="presentation">
+    <tr>
+        <td>
+            <p class="f-fallback sub">If you're having trouble with the button above, copy and paste the URL below into
+                your web browser.</p>
+            <p class="f-fallback sub"><a href="{extra_context[url]}">{extra_context[url]}</a></p>
+        </td>
+    </tr>
+</table>
+"""
 
 
 class SubscriptionBenefitDiscordService(
@@ -63,9 +103,13 @@ class SubscriptionBenefitDiscordService(
         try:
             account = await discord_user_service.get_oauth_account(self.session, user)
         except DiscordAccountNotConnected as e:
-            # TODO: Notify user & delay retry
             raise SubscriptionBenefitPreconditionError(
-                "Discord account not linked"
+                "Discord account not linked",
+                payload=SubscriptionBenefitPreconditionErrorNotificationContextualPayload(
+                    subject_template=precondition_error_subject_template,
+                    body_template=precondition_error_body_template,
+                    extra_context={"url": settings.generate_frontend_url("/settings")},
+                ),
             ) from e
 
         try:

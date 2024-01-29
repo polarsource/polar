567f1145b6b16346a25ca9252a56d8527e4730e6
François Voron <fvoron@gmail.com>
2024-01-29 10:01:54 +0100

    devcontainer: tweak initial setup

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,26 +1,22 @@
-#!/bin/bash
+#! /bin/bash
 
 set -euo pipefail
 set -x
 
 # Server setup
-# 1000:1000 is vscode:vscode
-sudo chown 1000:1000 /workspace/server/.venv/
 cd /workspace/server
 poetry install
 echo "🐻‍❄️✅ Server ready"
 
 # Clients setup
 cd /workspace/clients
-sudo chown 1000:1000 /workspace/clients/node_modules
 pnpm install
 echo "🐻‍❄️✅ Clients ready"
 
-
+# Environment file
 if [ ! -f "/workspace/clients/apps/web/.env" ]; then
     echo "🐻‍❄️ /workspace/clients/apps/web/.env does not exist, creating it"
     cp /workspace/clients/apps/web/.env.devcontainer /workspace/clients/apps/web/.env
 fi
-
 
 echo "🐻‍❄️✅✅✅ Setup complete"

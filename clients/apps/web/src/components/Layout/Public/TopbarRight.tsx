c0e632814f853eca849507072f2646c1d7570173
Gustav Westling <gustav@polar.sh>
2024-01-30 17:45:31 +0100

    clients/topbar: reactive return paths

--- clients/apps/web/src/components/Layout/Public/TopbarRight.tsx
+++ clients/apps/web/src/components/Layout/Public/TopbarRight.tsx
@@ -1,21 +1,24 @@
+'use client'
+
 import GithubLoginButton from '@/components/Shared/GithubLoginButton'
 import { ProfileMenu } from '@/components/Shared/ProfileSelection'
 import { UserRead } from '@polar-sh/sdk'
+import { usePathname } from 'next/navigation'
 
 const TopbarRight = ({
   authenticatedUser,
 }: {
   authenticatedUser?: UserRead
 }) => {
+  const pathname = usePathname()
+  const returnTo = pathname ?? '/feed'
+
   return (
     <>
       {authenticatedUser ? (
         <ProfileMenu authenticatedUser={authenticatedUser} />
       ) : (
-        <GithubLoginButton
-          text="Continue with GitHub"
-          returnTo={window?.location?.href ?? '/feed'}
-        />
+        <GithubLoginButton text="Continue with GitHub" returnTo={returnTo} />
       )}
     </>
   )

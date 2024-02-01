2381841149410e61bea556453eba9c77f32d875a
Emil Widlund <hello@emilwidlund.com>
2024-02-01 11:48:31 +0100

    clients/shadowbox: adjust ring

--- clients/packages/polarkit/src/components/ui/atoms/ShadowBoxOnMd.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/ShadowBoxOnMd.tsx
@@ -8,7 +8,7 @@ const ShadowBoxOnMd = ({
 }: DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => (
   <div
     className={twMerge(
-      'md:dark:bg-polar-900 md:dark:ring-polar-800 w-full md:rounded-xl md:bg-white md:p-8 md:shadow-sm md:ring-1 md:ring-gray-100 md:dark:ring-1 lg:rounded-3xl',
+      'md:dark:bg-polar-900 md:dark:ring-polar-800 md:ring-gray-75 w-full md:rounded-xl md:bg-white md:p-8 md:shadow-sm md:ring-1 md:dark:ring-1 lg:rounded-3xl',
       className,
     )}
     {...props}

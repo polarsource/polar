92dda211b23b1fe1ac5fcb827fbc828c43630938
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-03 13:43:03 +0000

    fix: fix PostToolbar overlaying ProfileMenu

--- clients/apps/web/src/components/Feed/Toolbar/PostToolbar.tsx
+++ clients/apps/web/src/components/Feed/Toolbar/PostToolbar.tsx
@@ -21,13 +21,13 @@ export const PostToolbar = ({
 }: PostToolbarProps) => {
   const isPublished = Boolean(
     article &&
-      article.published_at &&
-      new Date(article.published_at) <= new Date() &&
-      article.visibility === ArticleVisibilityEnum.PUBLIC,
+    article.published_at &&
+    new Date(article.published_at) <= new Date() &&
+    article.visibility === ArticleVisibilityEnum.PUBLIC,
   )
 
   return (
-    <div className="dark:border-polar-800 dark:bg-polar-900 sticky top-0 z-20 flex w-full flex-col border-b border-gray-100 bg-white">
+    <div className="dark:border-polar-800 dark:bg-polar-900 sticky top-0 z-10 flex w-full flex-col border-b border-gray-100 bg-white">
       <div className="relative mx-auto flex w-full min-w-0 max-w-screen-xl flex-row items-center justify-between gap-x-4 px-4 py-4 sm:px-6 md:px-8">
         <TabsList className="relative flex-row dark:border md:flex-row">
           <TabsTrigger value="edit" size="small">

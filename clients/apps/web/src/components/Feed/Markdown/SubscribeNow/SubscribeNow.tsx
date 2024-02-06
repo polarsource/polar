7098ad0f019ec247debe7a26cd24a40a67d8ea95
Gustav Westling <gustav@polar.sh>
2024-02-06 09:44:24 +0100

    clients/posts: show <SubscribeNow> for subscribed users

--- clients/apps/web/src/components/Feed/Markdown/SubscribeNow/SubscribeNow.tsx
+++ clients/apps/web/src/components/Feed/Markdown/SubscribeNow/SubscribeNow.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from '@heroicons/react/24/outline'
 import { Article } from '@polar-sh/sdk'
 import { useRouter } from 'next/navigation'
 import { Button } from 'polarkit/components/ui/atoms'
@@ -5,9 +6,14 @@ import { Button } from 'polarkit/components/ui/atoms'
 const SubscribeNow = (props: { article: Article; isSubscriber: boolean }) => {
   const router = useRouter()
 
-  // User is already subscribed, hide button.
   if (props.isSubscriber) {
-    return <></>
+    return (
+      <div className="flex flex-col items-center py-1">
+        <Button disabled={true}>
+          <CheckIcon className="-ml-1 mr-2 h-4 w-4" /> Subscribed
+        </Button>
+      </div>
+    )
   }
 
   return (

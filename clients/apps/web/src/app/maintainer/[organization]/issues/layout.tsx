2fc06d35900f53d93171bbed8cdc34509e4c6770
Gustav Westling <gustav@polar.sh>
2024-02-06 14:19:09 +0100

    clients/maintainer: check user org access on all pages

--- clients/apps/web/src/app/maintainer/[organization]/issues/layout.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/issues/layout.tsx
@@ -1,12 +1,9 @@
-import Gatekeeper from '@/components/Dashboard/Gatekeeper/Gatekeeper'
 import DashboardLayout from '@/components/Layout/DashboardLayout'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Gatekeeper>
-      <DashboardLayout>
-        <>{children}</>
-      </DashboardLayout>
-    </Gatekeeper>
+    <DashboardLayout>
+      <>{children}</>
+    </DashboardLayout>
   )
 }

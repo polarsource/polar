fafe18198ba91bf51b7138bb93cb1b90d741bd80
François Voron <fvoron@gmail.com>
2024-02-08 14:29:33 +0100

    clients/web: handle errors in magic link login form

--- clients/apps/web/src/components/Shared/MagicLinkLoginForm.tsx
+++ clients/apps/web/src/components/Shared/MagicLinkLoginForm.tsx
@@ -1,9 +1,19 @@
 'use client'
 
+import { FormControl } from '@mui/material'
+import { ResponseError, ValidationError } from '@polar-sh/sdk'
 import { useRouter } from 'next/navigation'
 import { api } from 'polarkit'
+import { setValidationErrors } from 'polarkit/api/errors'
 import { Button, Input } from 'polarkit/components/ui/atoms'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormMessage,
+} from 'polarkit/components/ui/form'
 import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 interface MagicLinkLoginFormProps {
   returnTo?: string
@@ -12,46 +22,70 @@ interface MagicLinkLoginFormProps {
 const MagicLinkLoginForm: React.FC<MagicLinkLoginFormProps> = ({
   returnTo,
 }) => {
-  const [email, setEmail] = useState<string>('')
+  const form = useForm<{ email: string }>()
+  const { control, handleSubmit, setError } = form
   const [loading, setLoading] = useState(false)
   const router = useRouter()
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const onSubmit: SubmitHandler<{ email: string }> = async ({ email }) => {
     setLoading(true)
-
     try {
       await api.magicLink.magicLinkRequest({
         magicLinkRequest: { email, return_to: returnTo },
       })
-      const searchParams = new URLSearchParams({ email: email })
+      const searchParams = new URLSearchParams({ email })
       router.push(`/login/magic-link/request?${searchParams}`)
-    } catch (err) {
-      // TODO: error handling
+    } catch (e) {
+      if (e instanceof ResponseError) {
+        const body = await e.response.json()
+        if (e.response.status === 422) {
+          const validationErrors = body['detail'] as ValidationError[]
+          setValidationErrors(validationErrors, setError)
+        } else if (body['detail']) {
+          setError('email', { message: body['detail'] })
+        }
+      }
     } finally {
       setLoading(false)
     }
   }
 
   return (
-    <form className="relative w-full" onSubmit={onSubmit}>
-      <Input
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        id="magic-link-email"
-        name="magic-link-email"
-        type="email"
-        placeholder="Email"
-        required
-        className="w-full"
-        data-1p-ignore
-      />
-      <div className="absolute inset-y-0 right-0 z-50 flex items-center pr-2">
-        <Button type="submit" size="sm" loading={loading} disabled={loading}>
-          Sign in
-        </Button>
-      </div>
-    </form>
+    <Form {...form}>
+      <form className="flex w-full flex-col" onSubmit={handleSubmit(onSubmit)}>
+        <FormField
+          control={control}
+          name="email"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl className="w-full">
+                  <Input
+                    type="email"
+                    required
+                    placeholder="Email"
+                    autoComplete="off"
+                    data-1p-ignore
+                    {...field}
+                  />
+                  <div className="absolute inset-y-0 right-0 z-50 flex items-center pr-2">
+                    <Button
+                      type="submit"
+                      size="sm"
+                      loading={loading}
+                      disabled={loading}
+                    >
+                      Sign in
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )
+          }}
+        />
+      </form>
+    </Form>
   )
 }
 

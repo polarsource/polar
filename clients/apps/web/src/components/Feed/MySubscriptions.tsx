7f5f79d35f2c31d60a0c08ddf18a63084a71be6f
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-11 12:08:46 +0000

    clients/subscriptions: add cursor-pointer to unsubscribe dropdown menus

--- clients/apps/web/src/components/Feed/MySubscriptions.tsx
+++ clients/apps/web/src/components/Feed/MySubscriptions.tsx
@@ -89,7 +89,7 @@ const SubscriptionOrganizationItem = ({
               align="end"
               className="dark:bg-polar-800 bg-gray-50 shadow-lg"
             >
-              <DropdownMenuItem onClick={() => setShowCancelModal(true)}>
+              <DropdownMenuItem onClick={() => setShowCancelModal(true)} className="cursor-pointer">
                 Unsubscribe
               </DropdownMenuItem>
             </DropdownMenuContent>

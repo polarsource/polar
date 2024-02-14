d765ce9511d79cf6f34827424ced67ccbb75f24f
Gustav Westling <gustav@polar.sh>
2024-02-14 15:43:50 +0100

    clients/posts: do not highlight pinned posts on /feed

--- clients/apps/web/src/components/Feed/Posts/Post.tsx
+++ clients/apps/web/src/components/Feed/Posts/Post.tsx
@@ -12,7 +12,10 @@ import { useHoverDirty } from 'react-use'
 import { twMerge } from 'tailwind-merge'
 import { AbbreviatedBrowserRender } from '../Markdown/BrowserRender'
 
-type FeedPost = { article: Article }
+type FeedPost = {
+  article: Article
+  highlightPinned?: boolean
+}
 
 const articleHref = (art: Article): string => {
   return `/${art.organization.name}/posts/${art.slug}`
@@ -33,7 +36,8 @@ export const Post = (props: FeedPost) => {
         'dark:border-polar-800 hover:dark:bg-polar-800/60 dark:bg-polar-900 flex w-full flex-col justify-start gap-4 rounded-3xl bg-white px-6 pb-6 pt-8 shadow-sm transition-all duration-100 dark:border md:flex-row',
         props.article.paid_subscribers_only &&
           'border border-blue-50 bg-gradient-to-b from-blue-50/80 to-transparent hover:from-blue-100 dark:from-blue-800/20 dark:hover:from-blue-800/30',
-        props.article.is_pinned &&
+        props.highlightPinned &&
+          props.article.is_pinned &&
           'border border-green-100 bg-gradient-to-b from-green-100/80 to-transparent hover:from-green-200 dark:from-green-700/20 dark:hover:from-green-600/30',
       )}
       ref={ref}
@@ -108,7 +112,7 @@ const PostHeader = (props: FeedPost & { isHovered: boolean }) => {
             </>
           ) : null}
 
-          {props.article.is_pinned ? (
+          {props.highlightPinned && props.article.is_pinned ? (
             <>
               &middot;
               <div className="flex flex-row items-center rounded-full bg-green-100 bg-gradient-to-l px-2 dark:bg-green-950">

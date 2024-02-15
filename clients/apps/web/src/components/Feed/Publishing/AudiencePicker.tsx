0fa1ee7b29ac295159b7069711f8df78d9cc1603
Gustav Westling <gustav@polar.sh>
2024-02-15 12:02:37 +0100

    clients/PublishSettigs: add FormMessage to all fields

--- clients/apps/web/src/components/Feed/Publishing/AudiencePicker.tsx
+++ clients/apps/web/src/components/Feed/Publishing/AudiencePicker.tsx
@@ -1,5 +1,5 @@
 import { ArticleUpdate } from '@polar-sh/sdk'
-import { FormField } from 'polarkit/components/ui/form'
+import { FormField, FormItem, FormMessage } from 'polarkit/components/ui/form'
 import { Label } from 'polarkit/components/ui/label'
 import { RadioGroup, RadioGroupItem } from 'polarkit/components/ui/radio-group'
 import { useFormContext } from 'react-hook-form'
@@ -16,40 +16,48 @@ export const AudiencePicker = () => {
         const audience = field.value ? 'premium-subscribers' : 'all-subscribers'
 
         return (
-          <div className="flex flex-col gap-y-4">
-            <div className="flex flex-col gap-y-2">
-              <span className="font-medium">Audience</span>
-              <p className="text-polar-500 dark:text-polar-500 text-sm">
-                Pick which audience you want to publish this post to
-              </p>
+          <FormItem>
+            <div className="flex flex-col gap-y-4">
+              <div className="flex flex-col gap-y-2">
+                <span className="font-medium">Audience</span>
+                <p className="text-polar-500 dark:text-polar-500 text-sm">
+                  Pick which audience you want to publish this post to
+                </p>
+              </div>
+              <RadioGroup
+                value={audience}
+                onValueChange={(v) =>
+                  field.onChange(v === 'premium-subscribers')
+                }
+              >
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem
+                    value="all-subscribers"
+                    id="all-subscribers"
+                  />
+                  <Label
+                    className={twMerge('capitalize')}
+                    htmlFor="all-subscribers"
+                  >
+                    All Subscribers
+                  </Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem
+                    value="premium-subscribers"
+                    id="premium-subscribers"
+                  />
+                  <Label
+                    className={twMerge('capitalize')}
+                    htmlFor="premium-subscribers"
+                  >
+                    Premium Subscribers
+                  </Label>
+                </div>
+              </RadioGroup>
             </div>
-            <RadioGroup
-              value={audience}
-              onValueChange={(v) => field.onChange(v === 'premium-subscribers')}
-            >
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="all-subscribers" id="all-subscribers" />
-                <Label
-                  className={twMerge('capitalize')}
-                  htmlFor="all-subscribers"
-                >
-                  All Subscribers
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="premium-subscribers"
-                  id="premium-subscribers"
-                />
-                <Label
-                  className={twMerge('capitalize')}
-                  htmlFor="premium-subscribers"
-                >
-                  Premium Subscribers
-                </Label>
-              </div>
-            </RadioGroup>
-          </div>
+            <FormMessage />
+          </FormItem>
         )
       }}
     />
--- clients/apps/web/src/components/Feed/Publishing/PublishingTimePicker.tsx
+++ clients/apps/web/src/components/Feed/Publishing/PublishingTimePicker.tsx
@@ -1,5 +1,5 @@
 import { ArticleUpdate } from '@polar-sh/sdk'
-import { FormField } from 'polarkit/components/ui/form'
+import { FormField, FormItem, FormMessage } from 'polarkit/components/ui/form'
 import { Label } from 'polarkit/components/ui/label'
 import { RadioGroup, RadioGroupItem } from 'polarkit/components/ui/radio-group'
 import { useFormContext } from 'react-hook-form'
@@ -13,53 +13,56 @@ export const PublishingTimePicker = () => {
     <FormField
       control={control}
       name="published_at"
-      rules={{
-        required: 'This field is required',
-        minLength: 1,
-      }}
       render={({ field }) => {
         const publish = field.value ? 'schedule' : 'publish-now'
 
         return (
-          <div className="flex flex-col gap-y-4">
-            <div className="flex items-start justify-between gap-y-2">
-              <span className="font-medium">Publishing</span>
-            </div>
-
-            <RadioGroup
-              value={publish}
-              onValueChange={(value) => {
-                const v = value === 'publish-now' ? null : new Date()
-                field.onChange(v)
-              }}
-            >
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="publish-now" id="publish-now" />
-                <Label className={twMerge('capitalize')} htmlFor="publish-now">
-                  Publish Now
-                </Label>
+          <FormItem>
+            <div className="flex flex-col gap-y-4">
+              <div className="flex items-start justify-between gap-y-2">
+                <span className="font-medium">Publishing</span>
               </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="schedule" id="schedule" />
-                <Label className={twMerge('capitalize')} htmlFor="schedule">
-                  Schedule
-                </Label>
-              </div>
-            </RadioGroup>
 
-            {publish === 'schedule' ? (
-              <div className="flex flex-col gap-y-4">
-                <div className="flex flex-row justify-between">
-                  <DateTimePicker
-                    date={field.value ? new Date(field.value) : new Date()}
-                    canSelectFuture={true}
-                    canSelectPast={true}
-                    onChange={field.onChange}
-                  />
+              <RadioGroup
+                value={publish}
+                onValueChange={(value) => {
+                  const v = value === 'publish-now' ? null : new Date()
+                  field.onChange(v)
+                }}
+              >
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="publish-now" id="publish-now" />
+                  <Label
+                    className={twMerge('capitalize')}
+                    htmlFor="publish-now"
+                  >
+                    Publish Now
+                  </Label>
                 </div>
-              </div>
-            ) : null}
-          </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="schedule" id="schedule" />
+                  <Label className={twMerge('capitalize')} htmlFor="schedule">
+                    Schedule
+                  </Label>
+                </div>
+              </RadioGroup>
+
+              {publish === 'schedule' ? (
+                <div className="flex flex-col gap-y-4">
+                  <div className="flex flex-row justify-between">
+                    <DateTimePicker
+                      date={field.value ? new Date(field.value) : new Date()}
+                      canSelectFuture={true}
+                      canSelectPast={true}
+                      onChange={field.onChange}
+                    />
+                  </div>
+                </div>
+              ) : null}
+
+              <FormMessage />
+            </div>
+          </FormItem>
         )
       }}
     />

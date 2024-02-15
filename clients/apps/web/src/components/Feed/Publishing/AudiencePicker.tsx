6874682a6779671a6148797061d4d7b5ea3253ac
Gustav Westling <gustav@polar.sh>
2024-02-15 10:50:36 +0100

    clients/posts: rewrite publish settings with react-hook-form

--- clients/apps/web/src/components/Feed/Publishing/AudiencePicker.tsx
+++ clients/apps/web/src/components/Feed/Publishing/AudiencePicker.tsx
@@ -1,57 +1,57 @@
+import { ArticleUpdate } from '@polar-sh/sdk'
+import { FormField } from 'polarkit/components/ui/form'
 import { Label } from 'polarkit/components/ui/label'
 import { RadioGroup, RadioGroupItem } from 'polarkit/components/ui/radio-group'
-import { useCallback, useMemo } from 'react'
+import { useFormContext } from 'react-hook-form'
 import { twMerge } from 'tailwind-merge'
 
-interface AudiencePickerProps {
-  paidSubscribersOnly: boolean
-  onChange: (paidSubscribersOnly: boolean) => void
-}
-
-export const AudiencePicker = ({
-  paidSubscribersOnly,
-  onChange,
-}: AudiencePickerProps) => {
-  const handleAudienceChange = useCallback(
-    (audience: string) => {
-      onChange(audience === 'premium-subscribers')
-    },
-    [onChange],
-  )
-
-  const audience = useMemo(
-    () => (paidSubscribersOnly ? 'premium-subscribers' : 'all-subscribers'),
-    [paidSubscribersOnly],
-  )
+export const AudiencePicker = () => {
+  const { control } = useFormContext<ArticleUpdate>()
 
   return (
-    <div className="flex flex-col gap-y-4">
-      <div className="flex flex-col gap-y-2">
-        <span className="font-medium">Audience</span>
-        <p className="text-polar-500 dark:text-polar-500 text-sm">
-          Pick which audience you want to publish this post to
-        </p>
-      </div>
-      <RadioGroup value={audience} onValueChange={handleAudienceChange}>
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem value="all-subscribers" id="all-subscribers" />
-          <Label className={twMerge('capitalize')} htmlFor="all-subscribers">
-            All Subscribers
-          </Label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem
-            value="premium-subscribers"
-            id="premium-subscribers"
-          />
-          <Label
-            className={twMerge('capitalize')}
-            htmlFor="premium-subscribers"
-          >
-            Premium Subscribers
-          </Label>
-        </div>
-      </RadioGroup>
-    </div>
+    <FormField
+      control={control}
+      name="paid_subscribers_only"
+      render={({ field }) => {
+        const audience = field.value ? 'premium-subscribers' : 'all-subscribers'
+
+        return (
+          <div className="flex flex-col gap-y-4">
+            <div className="flex flex-col gap-y-2">
+              <span className="font-medium">Audience</span>
+              <p className="text-polar-500 dark:text-polar-500 text-sm">
+                Pick which audience you want to publish this post to
+              </p>
+            </div>
+            <RadioGroup
+              value={audience}
+              onValueChange={(v) => field.onChange(v === 'premium-subscribers')}
+            >
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="all-subscribers" id="all-subscribers" />
+                <Label
+                  className={twMerge('capitalize')}
+                  htmlFor="all-subscribers"
+                >
+                  All Subscribers
+                </Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value="premium-subscribers"
+                  id="premium-subscribers"
+                />
+                <Label
+                  className={twMerge('capitalize')}
+                  htmlFor="premium-subscribers"
+                >
+                  Premium Subscribers
+                </Label>
+              </div>
+            </RadioGroup>
+          </div>
+        )
+      }}
+    />
   )
 }

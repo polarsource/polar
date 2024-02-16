2aa8ae9fd8f14f9e46c47a7bf5b0565e6fd05d42
Gustav Westling <gustav@polar.sh>
2024-02-16 13:56:52 +0100

    server/metrics: add grafana agent sidecar

---
+++ server/bin/run_render.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Dynamically create grafana agent configuration
+cat << EOF > ./agent-config.yaml
+metrics:
+  global:
+    scrape_interval: 30s
+  configs:
+  - name: hosted-prometheus
+    scrape_configs:
+      - job_name: node
+        static_configs:
+        - targets: ['127.0.0.1:8000']
+          labels:
+            instance_id: "${RENDER_INSTANCE_ID}"
+            service_name: "${RENDER_SERVICE_NAME}"
+        basic_auth:
+            username: "metrics"
+            password: "${PROMETHEUS_EXPORTER_HTTP_PASSWORD}"
+
+    remote_write:
+      - url: https://prometheus-prod-13-prod-us-east-0.grafana.net/api/prom/push
+        basic_auth:
+          username: "${GRAFANA_AGENT_USERNAME}"
+          password: "${GRAFANA_AGENT_PASSWORD}"
+EOF
+
+# Run grafana agent in the background
+/usr/bin/grafana-agent-linux-amd64 --config.file=agent-config.yaml &
+
+poetry run uvicorn polar.app:app --host 0.0.0.0 --port 10000

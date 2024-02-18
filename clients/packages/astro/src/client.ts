13946b251eb9c3d9ce37c541677334844efaf9f6
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-18 15:58:45 +0000

    feat(astro): improve typing and documentation; format

--- clients/packages/astro/src/client.ts
+++ clients/packages/astro/src/client.ts
@@ -1,7 +1,7 @@
 import type { ConfigurationParameters } from '@polar-sh/sdk'
 import { Configuration, PolarAPI } from '@polar-sh/sdk'
-import { PolarUploadBuilder, type UploadOptions } from './upload'
 import type { AstroCollectionEntry } from './types'
+import { PolarUploadBuilder, type UploadOptions } from './upload'
 
 export class Polar {
   public client: PolarAPI
@@ -10,7 +10,10 @@ export class Polar {
     this.client = new PolarAPI(new Configuration(config))
   }
 
-  upload<TEntry extends AstroCollectionEntry>(entries: TEntry[], options: UploadOptions) {
+  upload<TEntry extends AstroCollectionEntry>(
+    entries: TEntry[],
+    options: UploadOptions,
+  ) {
     return new PolarUploadBuilder(this.client, entries, options)
   }
 }
--- clients/packages/astro/src/types.ts
+++ clients/packages/astro/src/types.ts
@@ -1,13 +1,15 @@
 /**
  * A PolarResult contains either the data or an error from a Polar API request.
  */
-export type PolarResult<T, E extends Error = Error> = {
-  data: T
-  error: null
-} | {
-  data: null
-  error: E
-}
+export type PolarResult<T, E extends Error = Error> =
+  | {
+      data: T
+      error: null
+    }
+  | {
+      data: null
+      error: E
+    }
 
 /**
  * An ErrorGroup is a collection of errors that are related to a single
@@ -26,6 +28,28 @@ export class ErrorGroup<E extends Error = Error> extends Error {
 /**
  * A PolarUploadError is an error that occurs when uploading an article
  * to the Polar API.
+ *
+ * @example
+ * ```typescript
+ * import { getCollection } from 'astro:content'
+ * import { Polar } from '@polar-sh/astro'
+ *
+ * const polar = new Polar({ accessToken: 'my-access-token' })
+ * const posts = await getCollection('posts')
+ *
+ * const uploadResult = await polar
+ *   .upload(posts, {
+ *     organizationName: 'my-organization',
+ *     organizationId: 'my-organization-id',
+ *   })
+ *   // Filter out any articles that are drafts
+ *   .filter(({ entry }) => !!entry.publishedAt)
+ *   // Add a custom image to the article
+ *   .transform(({ article, entry }) => ({
+ *     ...article,
+ *     body: `![${entry.title}](${entry.featuredImage})\n\n${article.body}`,
+ *   }))
+ * ```
  */
 export class PolarUploadError extends Error {
   name = 'PolarUploadError'

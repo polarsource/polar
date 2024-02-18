4f9514d86ce50d8768c40e4f8c3ae7c1eceab34c
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-18 15:45:08 +0000

    feat(astro): implement uploading

--- clients/packages/astro/src/types.ts
+++ clients/packages/astro/src/types.ts
@@ -1,26 +1,44 @@
 /**
  * A PolarResult contains either the data or an error from a Polar API request.
  */
-type PolarResult<T, E extends Error = Error> =
-  | {
-      data: T
-      error: null
-    }
-  | {
-      data: null
-      error: E
-    }
+export type PolarResult<T, E extends Error = Error> = {
+  data: T
+  error: null
+} | {
+  data: null
+  error: E
+}
 
 /**
  * An ErrorGroup is a collection of errors that are related to a single
  * operation.
  */
-class ErrorGroup<E extends Error = Error> extends Error {
+export class ErrorGroup<E extends Error = Error> extends Error {
   name = 'ErrorGroup'
   errors: E[]
 
-  constructor(errors: E[]) {
-    super(`${errors.length} errors occurred.`)
+  constructor(errors: E[], options?: ErrorOptions) {
+    super(`${errors.length} errors occurred.`, options)
     this.errors = errors
   }
+}
+
+/**
+ * A PolarUploadError is an error that occurs when uploading an article
+ * to the Polar API.
+ */
+export class PolarUploadError extends Error {
+  name = 'PolarUploadError'
+  status: number
+
+  constructor(message: string, status: number, options?: ErrorOptions) {
+    super(message, options)
+    this.status = status
+  }
+}
+
+export interface AstroCollectionEntry {
+  title: string
+  slug: string
+  body: string
 }

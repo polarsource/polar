2f9618ebb2c75460ffeee57e6513ff1b63e7cb05
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-18 16:13:45 +0000

    feat(astro): exports

--- clients/packages/astro/index.ts
+++ clients/packages/astro/index.ts
@@ -1,6 +1,2 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
-
-import MyComponent from './src/MyComponent.astro'
-
-export default MyComponent
+export { Polar } from './src/client'
+export { ErrorGroup, PolarUploadError } from './src/types'

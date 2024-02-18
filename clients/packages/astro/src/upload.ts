1ea898980f0252d23fda967c00e3280121d1999c
Isaac Harris-Holt <isaac@harris-holt.com>
2024-02-18 15:58:45 +0000

    feat(astro): improve typing and documentation; format

--- clients/packages/astro/src/upload.ts
+++ clients/packages/astro/src/upload.ts
@@ -2,9 +2,35 @@
  * @module upload
  * The PolarUpload class is used to upload Astro articles to Polar via
  * the Polar SDK.
+ *
+ * Essentially, we generate a list of Polar articles from the Astro articles,
+ * and then allow the user to apply a series of functions to the list before
+ * uploading them to Polar.
+ *
+ * The upload happens in the `then` method, which is called by using `await`
+ * on the PolarUploadBuilder instance.
+ *
+ * This allows for a fluent API that is easy to use and understand:
+ *
+ * ```typescript
+ * const result = await polar.upload(articles, options).filter(...).transform(...)
+ * ```
+ *
+ * This is inspired by the [Supabase Client](https://supabase.com/docs/reference/javascript/select),
+ * which uses a similar pattern for building queries.
  */
-import type { Article, ArticleUpdate, ArticleCreate, PolarAPI } from '@polar-sh/sdk'
-import { type AstroCollectionEntry, PolarUploadError, ErrorGroup, type PolarResult } from './types'
+import type {
+  Article,
+  ArticleCreate,
+  ArticleUpdate,
+  PolarAPI,
+} from '@polar-sh/sdk'
+import {
+  ErrorGroup,
+  PolarUploadError,
+  type AstroCollectionEntry,
+  type PolarResult,
+} from './types'
 
 /**
  * A PolarArticle contains all the details required for creating or updating
@@ -12,38 +38,72 @@ import { type AstroCollectionEntry, PolarUploadError, ErrorGroup, type PolarResu
  */
 export type PolarArticle = ArticleCreate & ArticleUpdate
 
-type UploadBuilderFunctionData<TEntry extends AstroCollectionEntry, TArticle extends PolarArticle> = {
+/**
+ * Information about the current article in the upload pipeline.
+ * `entry` is the original Astro article, `article` is the Polar article,
+ * and `exists` is a boolean indicating whether the article already exists
+ * on Polar.
+ */
+type UploadBuilderFunctionData<
+  TEntry extends AstroCollectionEntry,
+  TArticle extends PolarArticle,
+> = {
   entry: TEntry
   article: TArticle
   exists: boolean
 }
 
-type UploadFilterFunction<TEntry extends AstroCollectionEntry, TArticle extends PolarArticle> = (
-  data: UploadBuilderFunctionData<TEntry, TArticle>,
-) => boolean
+/**
+ * @private
+ * A function that filters out articles from the list of entries.
+ */
+type UploadFilterFunction<
+  TEntry extends AstroCollectionEntry,
+  TArticle extends PolarArticle,
+> = (data: UploadBuilderFunctionData<TEntry, TArticle>) => boolean
 
+/**
+ * @private
+ * A function that transforms an article from the list of entries.
+ * This can be used to modify the article before it is uploaded to Polar.
+ */
 type UploadTransformFunction<
   TEntry extends AstroCollectionEntry,
   TArticle extends PolarArticle,
   NewTArticle extends PolarArticle,
 > = (data: UploadBuilderFunctionData<TEntry, TArticle>) => NewTArticle
 
-type _UploadBuilderFunctionDefinition<TEntry extends AstroCollectionEntry> =
+/**
+ * @private
+ * A definition for a function that can be used in the upload pipeline.
+ */
+type UploadBuilderFunctionDefinition<TEntry extends AstroCollectionEntry> =
   | {
-    type: 'filter'
-    function: UploadFilterFunction<TEntry, PolarArticle>
-  }
+      type: 'filter'
+      function: UploadFilterFunction<TEntry, PolarArticle>
+    }
   | {
-    type: 'transform'
-    // Use any here as we don't need to know the return type of the transform function
-    function: UploadTransformFunction<TEntry, PolarArticle, any>
-  }
+      type: 'transform'
+      // Use any here as we don't need to know the return type of the transform function
+      function: UploadTransformFunction<TEntry, PolarArticle, any>
+    }
 
-type UploadResult = PolarResult<{
-  created: Article[]
-  updated: Article[]
-}, PolarUploadError | ErrorGroup<PolarUploadError>>
+/**
+ * The result from uploading articles to Polar.
+ * This contains either lists of created and updated articles, or an error
+ * if something went wrong.
+ */
+export type PolarUploadResult = PolarResult<
+  {
+    created: Article[]
+    updated: Article[]
+  },
+  PolarUploadError | ErrorGroup<PolarUploadError>
+>
 
+/**
+ * Options for uploading articles to Polar.
+ */
 export type UploadOptions = {
   organizationId: string
   organizationName: string
@@ -51,19 +111,27 @@ export type UploadOptions = {
   showUnpublished?: boolean
 }
 
+/**
+ * The PolarUploadBuilder class is used to build a list of Astro articles
+ * and upload them to the Polar API.
+ */
 export class PolarUploadBuilder<
   TEntry extends AstroCollectionEntry,
   TArticle extends PolarArticle = PolarArticle,
-> implements PromiseLike<UploadResult> {
+> implements PromiseLike<PolarUploadResult>
+{
   constructor(
     private client: PolarAPI,
     private entries: TEntry[],
     private options: UploadOptions,
-    private pipeline: _UploadBuilderFunctionDefinition<TEntry>[] = [],
-  ) { }
+    private pipeline: UploadBuilderFunctionDefinition<TEntry>[] = [],
+  ) {}
 
+  /**
+   * Create a new PolarUploadBuilder with a new function added to the pipeline.
+   */
   private withFunction<NewTArticle extends PolarArticle>(
-    func: _UploadBuilderFunctionDefinition<TEntry>,
+    func: UploadBuilderFunctionDefinition<TEntry>,
   ): PolarUploadBuilder<TEntry, NewTArticle> {
     return new PolarUploadBuilder(this.client, this.entries, this.options, [
       ...this.pipeline,
@@ -71,6 +139,11 @@ export class PolarUploadBuilder<
     ])
   }
 
+  /**
+   * Filter out articles from the list of entries.
+   * This function is called for each article in the list, and if it returns
+   * `false`, the article will not be uploaded.
+   */
   public filter(func: UploadFilterFunction<TEntry, TArticle>) {
     return this.withFunction({
       type: 'filter',
@@ -78,42 +151,11 @@ export class PolarUploadBuilder<
     })
   }
 
-  private async getArticles(): Promise<PolarResult<Article[], PolarUploadError>> {
-    const articles: Article[] = []
-    let page = 1
-
-    while (true) {
-      try {
-        const response = await this.client.articles.search({
-          organizationName: this.options.organizationName,
-          showUnpublished: this.options.showUnpublished ?? true,
-          platform: this.options.platform ?? 'github',
-          page,
-          limit: 100,
-        })
-        articles.push(...response.items!)
-        if (response.pagination?.max_page && page < response.pagination.max_page) {
-          page++
-        } else {
-          break
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          return {
-            data: null,
-            error: new PolarUploadError(error.message, 500, { cause: error })
-          }
-        } else {
-          return {
-            data: null,
-            error: new PolarUploadError('An unknown error occurred.', 500, { cause: error })
-          }
-        }
-      }
-    }
-    return { data: articles, error: null }
-  }
-
+  /**
+   * Transform articles before uploading them to Polar.
+   * This function is called for each article in the list, and allows you to
+   * modify the article before it is uploaded to Polar.
+   */
   public transform<NewTArticle extends PolarArticle>(
     func: UploadTransformFunction<TEntry, TArticle, NewTArticle>,
   ) {
@@ -127,7 +169,61 @@ export class PolarUploadBuilder<
     })
   }
 
-  private processEntry(data: UploadBuilderFunctionData<TEntry, PolarArticle>): UploadBuilderFunctionData<TEntry, PolarArticle> | null {
+  /**
+   * Fetch articles from the Polar API. Automatically paginates through
+   * all the articles in the organization.
+   */
+  private async getArticles(): Promise<
+    PolarResult<Article[], PolarUploadError>
+  > {
+    const articles: Article[] = []
+    let page = 1
+
+    while (true) {
+      try {
+        const response = await this.client.articles.search({
+          organizationName: this.options.organizationName,
+          showUnpublished: this.options.showUnpublished ?? true,
+          platform: this.options.platform ?? 'github',
+          page,
+          limit: 100,
+        })
+        articles.push(...response.items!)
+        if (
+          response.pagination?.max_page &&
+          page < response.pagination.max_page
+        ) {
+          page++
+        } else {
+          break
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          return {
+            data: null,
+            error: new PolarUploadError(error.message, 500, { cause: error }),
+          }
+        } else {
+          return {
+            data: null,
+            error: new PolarUploadError('An unknown error occurred.', 500, {
+              cause: error,
+            }),
+          }
+        }
+      }
+    }
+    return { data: articles, error: null }
+  }
+
+  /**
+   * Process an entry in the upload pipeline.
+   * This applies all the functions in the pipeline to the entry, and returns
+   * the result. If the entry is filtered out, it will return `null`.
+   */
+  private processEntry(
+    data: UploadBuilderFunctionData<TEntry, PolarArticle>,
+  ): UploadBuilderFunctionData<TEntry, PolarArticle> | null {
     let result = data
     for (const func of this.pipeline) {
       if (func.type === 'filter') {
@@ -144,35 +240,78 @@ export class PolarUploadBuilder<
     return result
   }
 
-  private async createArticle(article: ArticleCreate): Promise<PolarResult<Article, PolarUploadError>> {
+  /**
+   * Create a new article on Polar.
+   */
+  private async createArticle(
+    article: ArticleCreate,
+  ): Promise<PolarResult<Article, PolarUploadError>> {
     try {
-      const response = await this.client.articles.create({ articleCreate: article })
+      const response = await this.client.articles.create({
+        articleCreate: article,
+      })
       return { data: response, error: null }
     } catch (error) {
       if (error instanceof Error) {
-        return { data: null, error: new PolarUploadError(error.message, 500, { cause: error }) }
+        return {
+          data: null,
+          error: new PolarUploadError(error.message, 500, { cause: error }),
+        }
       } else {
-        return { data: null, error: new PolarUploadError('An unknown error occurred.', 500, { cause: error }) }
+        return {
+          data: null,
+          error: new PolarUploadError('An unknown error occurred.', 500, {
+            cause: error,
+          }),
+        }
       }
     }
   }
 
-  private async updateArticle(articleId: string, article: ArticleUpdate): Promise<PolarResult<Article, PolarUploadError>> {
+  /**
+   * Update an existing article on Polar.
+   */
+  private async updateArticle(
+    articleId: string,
+    article: ArticleUpdate,
+  ): Promise<PolarResult<Article, PolarUploadError>> {
     try {
-      const response = await this.client.articles.update({ id: articleId, articleUpdate: article })
+      const response = await this.client.articles.update({
+        id: articleId,
+        articleUpdate: article,
+      })
       return { data: response, error: null }
     } catch (error) {
       if (error instanceof Error) {
-        return { data: null, error: new PolarUploadError(error.message, 500, { cause: error }) }
+        return {
+          data: null,
+          error: new PolarUploadError(error.message, 500, { cause: error }),
+        }
       } else {
-        return { data: null, error: new PolarUploadError('An unknown error occurred.', 500, { cause: error }) }
+        return {
+          data: null,
+          error: new PolarUploadError('An unknown error occurred.', 500, {
+            cause: error,
+          }),
+        }
       }
     }
   }
 
-  public async then<TResult1 = UploadResult, TResult2 = never>(
-    onfulfilled?: ((value: UploadResult) => TResult1 | PromiseLike<TResult1>) | null | undefined,
-    onrejected?: ((reason: UploadResult) => TResult2 | PromiseLike<TResult2>) | null | undefined,
+  /**
+   * Upload all the articles to Polar.
+   * This is called by using `await` on the PolarUploadBuilder instance, and
+   * should not be called directly.
+   */
+  public async then<TResult1 = PolarUploadResult, TResult2 = never>(
+    onfulfilled?:
+      | ((value: PolarUploadResult) => TResult1 | PromiseLike<TResult1>)
+      | null
+      | undefined,
+    onrejected?:
+      | ((reason: PolarUploadResult) => TResult2 | PromiseLike<TResult2>)
+      | null
+      | undefined,
   ): Promise<TResult1 | TResult2> {
     // Fetch articles
     const articlesResult = await this.getArticles()
@@ -184,31 +323,42 @@ export class PolarUploadBuilder<
     }
     const articles = articlesResult.data
 
-    const toProcess: UploadBuilderFunctionData<TEntry, PolarArticle>[] = this.entries.map((entry) => {
-      return {
-        entry,
-        exists: !!articles.find((article) => article.slug === entry.slug),
-        article: {
-          title: entry.title,
-          slug: entry.slug,
-          body: entry.body,
-          organization_id: this.options.organizationId,
-        },
-      }
-    })
+    const toProcess: UploadBuilderFunctionData<TEntry, PolarArticle>[] =
+      this.entries.map((entry) => {
+        return {
+          entry,
+          exists: !!articles.find((article) => article.slug === entry.slug),
+          article: {
+            title: entry.title,
+            slug: entry.slug,
+            body: entry.body,
+            organization_id: this.options.organizationId,
+          },
+        }
+      })
 
     // Process entries
-    const processed: TArticle[] = toProcess.map((data) => this.processEntry(data)?.article).filter(Boolean) as TArticle[]
+    const processed: TArticle[] = toProcess
+      .map((data) => this.processEntry(data)?.article)
+      .filter(Boolean) as TArticle[]
 
-    const toCreate = processed.filter((article) => !articles.find((a) => a.slug === article.slug))
-    const toUpdate = processed.map((article) => {
-      const existing = articles.find((a) => a.slug === article.slug)
-      return existing ? { article, id: existing.id } : null
-    }).filter(Boolean) as { article: TArticle, id: string }[]
+    const toCreate = processed.filter(
+      (article) => !articles.find((a) => a.slug === article.slug),
+    )
+    const toUpdate = processed
+      .map((article) => {
+        const existing = articles.find((a) => a.slug === article.slug)
+        return existing ? { article, id: existing.id } : null
+      })
+      .filter(Boolean) as { article: TArticle; id: string }[]
 
     // Create/Update articles
-    const createResults = await Promise.all(toCreate.map((article) => this.createArticle(article)))
-    const updateResults = await Promise.all(toUpdate.map(({ article, id }) => this.updateArticle(id, article)))
+    const createResults = await Promise.all(
+      toCreate.map((article) => this.createArticle(article)),
+    )
+    const updateResults = await Promise.all(
+      toUpdate.map(({ article, id }) => this.updateArticle(id, article)),
+    )
 
     const errors: PolarUploadError[] = []
     for (const result of createResults.concat(updateResults)) {

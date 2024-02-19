6dd6b2b271f29f558d76a9eac8a4ca88c7abaffb
Gustav Westling <gustav@polar.sh>
2024-02-19 13:47:28 +0100

    server/traffic: add tracking API

---
+++ server/polar/traffic/endpoints.py
@@ -0,0 +1,36 @@
+import datetime
+
+import structlog
+from fastapi import APIRouter, Depends
+
+from polar.postgres import AsyncSession, get_db_session
+from polar.tags.api import Tags
+
+from .schemas import (
+    TrackPageView,
+    TrackPageViewResponse,
+)
+from .service import traffic_service
+
+log = structlog.get_logger()
+
+router = APIRouter(prefix="", tags=["traffic"])
+
+
+@router.post(
+    "/traffic/track_page_view",
+    response_model=TrackPageViewResponse,
+    tags=[Tags.PUBLIC],
+)
+async def track_page_view(
+    track: TrackPageView,
+    session: AsyncSession = Depends(get_db_session),
+) -> TrackPageViewResponse:
+    if track.article_id:
+        await traffic_service.add(
+            session,
+            article_id=track.article_id,
+            date=datetime.date.today(),
+        )
+
+    return TrackPageViewResponse(ok=True)

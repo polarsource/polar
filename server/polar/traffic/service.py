9b1dba2060170a8af2fdbd2c120f3d35d1e85222
Gustav Westling <gustav@polar.sh>
2024-02-20 11:30:00 +0100

    server/traffic: extend traffic endpoint
    Using the same interface and behaviour as the subscriptions statistics endpoints

--- server/polar/traffic/service.py
+++ server/polar/traffic/service.py
@@ -1,9 +1,11 @@
 import datetime
 from collections.abc import Sequence
+from typing import Literal
 from uuid import UUID
 
-from sqlalchemy import func
+from sqlalchemy import and_, func, null, text
 
+from polar.kit.utils import utc_now
 from polar.models.traffic import Traffic
 from polar.postgres import AsyncSession, sql
 from polar.traffic.schemas import TrafficStatisticsPeriod
@@ -40,30 +42,62 @@ class TrafficService:
         article_ids: list[UUID],
         start_date: datetime.date,
         end_date: datetime.date,
+        interval: Literal["month", "week", "day"],
+        group_by_article: bool,
+        start_of_last_period: datetime.date | None = None,
     ) -> Sequence[TrafficStatisticsPeriod]:
+        interval_txt = {
+            "month": "interval 'P1M'",
+            "week": "interval 'P1W'",
+            "day": "interval 'P1D'",
+        }[interval]
+
+        sql_interval = text(interval_txt)
+
+        start_date_column = func.generate_series(
+            start_date, end_date, sql_interval
+        ).column_valued("start_date")
+        end_date_column = start_date_column + sql_interval
+
+        start_of_last_period = start_of_last_period or utc_now().date().replace(day=1)
+
         stmt = (
             sql.select(
-                Traffic.article_id, func.sum(Traffic.views).label("period_views")
+                start_date_column,
             )
-            .where(
-                Traffic.article_id.in_(article_ids),
-                Traffic.date >= start_date,
-                Traffic.date <= end_date,
+            .add_columns(
+                end_date_column,
+                Traffic.article_id if group_by_article else null(),
+                # func.sum(Traffic.views).label("period_views"),
+                func.coalesce(
+                    func.sum(Traffic.views).filter(
+                        Traffic.date >= start_date_column,
+                        Traffic.date < end_date_column,
+                    ),
+                    0,
+                ),
             )
-            .group_by(Traffic.article_id, Traffic.date)
-            .order_by(Traffic.date)
+            .join(
+                Traffic,
+                onclause=and_(
+                    Traffic.article_id.in_(article_ids),
+                ),
+            )
+            .where(start_date_column <= start_of_last_period)
+            .group_by(start_date_column)
+            .order_by(start_date_column)
         )
 
         res = await session.execute(stmt)
 
         return [
             TrafficStatisticsPeriod(
-                start_date=start_date,
-                end_date=end_date,
+                start_date=row_start_date,
+                end_date=row_end_date,
                 article_id=article_id,
                 views=views,
             )
-            for (article_id, views) in res.tuples().all()
+            for (row_start_date, row_end_date, article_id, views) in res.tuples().all()
         ]
 
 

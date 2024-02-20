73135bd5fb60168032329f51987a207891a5a902
Emil Widlund <hello@emilwidlund.com>
2024-02-20 14:57:57 +0100

    examples/nextjs: polish example

--- clients/examples/next-js-example/src/app/page.tsx
+++ clients/examples/next-js-example/src/app/page.tsx
@@ -35,20 +35,21 @@ export default async function Home() {
         </div>
       </div>
       <div className="flex w-full max-w-2xl flex-col gap-y-6">
-        <div className="flex flex-col gap-y-6 rounded-3xl bg-white p-8">
+        <div className="flex flex-col gap-y-6 rounded-3xl bg-slate-100 p-8">
           <span className="font-mono text-xs text-slate-400">README.md</span>
           <p className="text-pretty text-sm leading-normal text-slate-500">
             I'm Emil, a Creative Technologist who loves to play around with
             WebGL and reactive frameworks like RxJS. If you like what I'm up to,
-            please consider subscribing to my Newsletter.
+            please consider subscribing to my Newsletter, or the paid
+            subscriptions down below.
           </p>
           <div className="flex flex-row gap-x-4">
             <input
-              className="w-full rounded-full bg-slate-100 px-4 text-sm placeholder-slate-400"
+              className="w-full rounded-full bg-slate-200 px-4 text-sm placeholder-slate-400 focus:outline-none"
               type="email"
               placeholder="Enter your email..."
             />
-            <button className="rounded-full border-none bg-blue-600 px-4 py-2 text-sm text-white">
+            <button className="rounded-full border-none bg-blue-600 px-4 py-2 text-xs text-white transition-colors hover:bg-blue-500">
               Subscribe
             </button>
           </div>
@@ -60,7 +61,7 @@ export default async function Home() {
             .map((tier) => (
               <div
                 key={tier.id}
-                className="flex w-full flex-col gap-y-6 rounded-3xl bg-white p-6"
+                className="flex w-full flex-col gap-y-4 rounded-3xl bg-slate-100 p-6"
               >
                 <div className="flex flex-row items-baseline justify-between">
                   <h3 className="font-semibold">{tier.name}</h3>
@@ -68,10 +69,10 @@ export default async function Home() {
                     ${tier.price_amount / 100}
                   </h3>
                 </div>
-                <p className="h-full text-sm text-slate-500">
+                <p className="h-full text-sm text-slate-400">
                   {tier.description}
                 </p>
-                <button className="self-start rounded-full border-none bg-blue-600 px-3 py-1.5 text-xs text-white">
+                <button className="self-start rounded-full border-none bg-blue-600 px-3 py-1.5 text-xs text-white transition-colors hover:bg-blue-500">
                   Subscribe
                 </button>
               </div>
@@ -83,7 +84,7 @@ export default async function Home() {
           <a
             key={article.id}
             href={`/${article.slug}`}
-            className="flex w-full max-w-2xl flex-col gap-y-4"
+            className="flex w-full max-w-2xl flex-col gap-y-4 transition-opacity duration-200 ease-in-out hover:opacity-60"
           >
             <h3 className="text-balance text-4xl font-bold leading-normal">
               {article.title}
@@ -98,7 +99,7 @@ export default async function Home() {
                 },
               )}
             </p>
-            <Markdown className="prose prose-a:text-blue-600 prose-a:no-underline prose-img:rounded-3xl prose-headings:leading-normal">
+            <Markdown className="prose prose-a:text-blue-600 prose-a:no-underline prose-img:rounded-3xl prose-headings:leading-normal w-full">
               {article.body.split('\n\n')[0]}
             </Markdown>
           </a>

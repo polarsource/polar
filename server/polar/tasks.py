87677db006bc84ecaabfee82b31ffb0c7e03293b
François Voron <fvoron@gmail.com>
2024-02-20 17:24:53 +0100

    server/transaction: add a task to sync Stripe fees

--- server/polar/tasks.py
+++ server/polar/tasks.py
@@ -7,6 +7,7 @@ from polar.magic_link import tasks as magic_link
 from polar.notifications import tasks as notifications
 from polar.organization import tasks as organization
 from polar.subscription import tasks as subscription
+from polar.transaction import tasks as transaction
 from polar.user import tasks as user
 
 __all__ = [
@@ -19,5 +20,6 @@ __all__ = [
     "notifications",
     "organization",
     "subscription",
+    "transaction",
     "user",
 ]

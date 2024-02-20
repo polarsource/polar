6a1c4ab60e767fcc14fae03f5ee42a9033390cc5
François Voron <fvoron@gmail.com>
2024-02-20 17:21:33 +0100

    stripe/transaction: implement fee service

---
+++ server/polar/transaction/service/fee.py
@@ -0,0 +1,214 @@
+import math
+from datetime import UTC, datetime
+from typing import Literal
+
+from polar.integrations.stripe.service import stripe as stripe_service
+from polar.integrations.stripe.utils import get_expandable_id
+from polar.models import Transaction
+from polar.models.transaction import PaymentProcessor, ProcessorFeeType, TransactionType
+from polar.postgres import AsyncSession
+
+from .base import BaseTransactionService, BaseTransactionServiceError
+
+
+class FeeTransactionError(BaseTransactionServiceError):
+    ...
+
+
+class UnsupportedStripeFeeType(FeeTransactionError):
+    def __init__(self, description: str) -> None:
+        self.description = description
+        message = f"Unsupported Stripe fee type: {description}"
+        super().__init__(message)
+
+
+def _round_stripe(amount: float) -> int:
+    return math.ceil(amount) if amount - int(amount) >= 0.5 else math.floor(amount)
+
+
+def _get_stripe_subscription_fee(amount: int) -> int:
+    return _round_stripe(amount * 0.005)
+
+
+def _get_stripe_tax_fee(amount: int) -> int:
+    return _round_stripe(amount * 0.005)
+
+
+def _get_stripe_processor_fee_type(description: str) -> ProcessorFeeType:
+    description = description.lower()
+    if "payout fee" in description or "account volume" in description:
+        return ProcessorFeeType.payout
+    if "cross-border transfers" in description:
+        return ProcessorFeeType.cross_border_transfer
+    if "active account" in description:
+        return ProcessorFeeType.account
+    if "subscriptions" in description:
+        return ProcessorFeeType.subscription
+    if "automatic tax" in description:
+        return ProcessorFeeType.tax
+    if "invoicing" in description:
+        return ProcessorFeeType.invoice
+    raise UnsupportedStripeFeeType(description)
+
+
+class FeeTransactionService(BaseTransactionService):
+    async def create_payment_fees(
+        self, session: AsyncSession, *, payment_transaction: Transaction
+    ) -> list[Transaction]:
+        fee_transactions: list[Transaction] = []
+
+        if payment_transaction.processor != PaymentProcessor.stripe:
+            return fee_transactions
+
+        if payment_transaction.charge_id is None:
+            return fee_transactions
+
+        charge = stripe_service.get_charge(payment_transaction.charge_id)
+
+        # Payment fee
+        if charge.balance_transaction:
+            stripe_balance_transaction = stripe_service.get_balance_transaction(
+                get_expandable_id(charge.balance_transaction)
+            )
+            payment_fee_transaction = Transaction(
+                type=TransactionType.processor_fee,
+                processor=PaymentProcessor.stripe,
+                processor_fee_type=ProcessorFeeType.payment,
+                currency=payment_transaction.currency,
+                amount=-stripe_balance_transaction.fee,
+                account_currency=payment_transaction.currency,
+                account_amount=-stripe_balance_transaction.fee,
+                tax_amount=0,
+                incurred_by_transaction_id=payment_transaction.id,
+            )
+            session.add(payment_fee_transaction)
+            fee_transactions.append(payment_fee_transaction)
+
+        await session.commit()
+
+        return fee_transactions
+
+    async def create_refund_fees(
+        self,
+        session: AsyncSession,
+        *,
+        refund_transaction: Transaction,
+    ) -> list[Transaction]:
+        fee_transactions: list[Transaction] = []
+
+        if refund_transaction.processor != PaymentProcessor.stripe:
+            return fee_transactions
+
+        if refund_transaction.refund_id is None:
+            return fee_transactions
+
+        refund = stripe_service.get_refund(refund_transaction.refund_id)
+
+        if refund.balance_transaction is None:
+            return fee_transactions
+
+        balance_transaction = stripe_service.get_balance_transaction(
+            get_expandable_id(refund.balance_transaction)
+        )
+
+        refund_fee_transaction = Transaction(
+            type=TransactionType.processor_fee,
+            processor=PaymentProcessor.stripe,
+            processor_fee_type=ProcessorFeeType.refund,
+            currency=refund_transaction.currency,
+            amount=-balance_transaction.fee,
+            account_currency=refund_transaction.currency,
+            account_amount=-balance_transaction.fee,
+            tax_amount=0,
+            incurred_by_transaction_id=refund_transaction.id,
+        )
+
+        session.add(refund_fee_transaction)
+        fee_transactions.append(refund_fee_transaction)
+
+        await session.commit()
+
+        return fee_transactions
+
+    async def create_dispute_fees(
+        self,
+        session: AsyncSession,
+        *,
+        dispute_transaction: Transaction,
+        category: Literal["dispute", "dispute_reversal"],
+    ) -> list[Transaction]:
+        fee_transactions: list[Transaction] = []
+
+        if dispute_transaction.processor != PaymentProcessor.stripe:
+            return fee_transactions
+
+        if dispute_transaction.dispute_id is None:
+            return fee_transactions
+
+        dispute = stripe_service.get_dispute(dispute_transaction.dispute_id)
+        balance_transaction = next(
+            bt
+            for bt in dispute.balance_transactions
+            if bt.reporting_category == category
+        )
+
+        dispute_fee_transaction = Transaction(
+            type=TransactionType.processor_fee,
+            processor=PaymentProcessor.stripe,
+            processor_fee_type=ProcessorFeeType.dispute,
+            currency=dispute_transaction.currency,
+            amount=-balance_transaction.fee,
+            account_currency=dispute_transaction.currency,
+            account_amount=-balance_transaction.fee,
+            tax_amount=0,
+            incurred_by_transaction_id=dispute_transaction.id,
+        )
+
+        session.add(dispute_fee_transaction)
+        fee_transactions.append(dispute_fee_transaction)
+
+        await session.commit()
+
+        return fee_transactions
+
+    async def sync_stripe_fees(self, session: AsyncSession) -> list[Transaction]:
+        transactions: list[Transaction] = []
+
+        for balance_transaction in stripe_service.list_balance_transactions(
+            type="stripe_fee"
+        ):
+            transaction = await self.get_by(
+                session, fee_balance_transaction_id=balance_transaction.id
+            )
+
+            # We reached the point where we have already synced all the fees
+            if transaction is not None:
+                break
+
+            if balance_transaction.description is None:
+                continue
+
+            processor_fee_type = _get_stripe_processor_fee_type(
+                balance_transaction.description
+            )
+            transaction = Transaction(
+                created_at=datetime.fromtimestamp(balance_transaction.created, tz=UTC),
+                type=TransactionType.processor_fee,
+                processor=PaymentProcessor.stripe,
+                processor_fee_type=processor_fee_type,
+                currency=balance_transaction.currency,
+                amount=balance_transaction.net,
+                account_currency=balance_transaction.currency,
+                account_amount=balance_transaction.net,
+                tax_amount=0,
+                fee_balance_transaction_id=balance_transaction.id,
+            )
+            session.add(transaction)
+            transactions.append(transaction)
+
+        await session.commit()
+
+        return transactions
+
+
+fee_transaction = FeeTransactionService(Transaction)
---
+++ server/tests/transaction/service/test_fee.py
@@ -0,0 +1,498 @@
+import datetime
+from typing import Literal
+from unittest.mock import MagicMock
+
+import pytest
+import stripe as stripe_lib
+from pytest_mock import MockerFixture
+
+from polar.integrations.stripe.service import StripeService
+from polar.models import IssueReward, Pledge, Subscription, Transaction
+from polar.models.transaction import PaymentProcessor, ProcessorFeeType, TransactionType
+from polar.postgres import AsyncSession
+from polar.transaction.service.fee import (
+    fee_transaction as fee_transaction_service,
+)
+
+
+@pytest.fixture(autouse=True)
+def stripe_service_mock(mocker: MockerFixture) -> MagicMock:
+    mock = MagicMock(spec=StripeService)
+    mocker.patch("polar.transaction.service.fee.stripe_service", new=mock)
+    return mock
+
+
+async def create_payment_transaction(
+    session: AsyncSession,
+    *,
+    processor: PaymentProcessor = PaymentProcessor.stripe,
+    currency: str = "usd",
+    amount: int = 1000,
+    charge_id: str | None = "STRIPE_CHARGE_ID",
+    pledge: Pledge | None = None,
+    subscription: Subscription | None = None,
+    issue_reward: IssueReward | None = None,
+) -> Transaction:
+    transaction = Transaction(
+        type=TransactionType.payment,
+        processor=processor,
+        currency=currency,
+        amount=amount,
+        account_currency=currency,
+        account_amount=amount,
+        tax_amount=0,
+        charge_id=charge_id,
+        pledge=pledge,
+        subscription=subscription,
+        issue_reward=issue_reward,
+    )
+    session.add(transaction)
+    await session.commit()
+    return transaction
+
+
+async def create_refund_transaction(
+    session: AsyncSession,
+    *,
+    processor: PaymentProcessor = PaymentProcessor.stripe,
+    currency: str = "usd",
+    amount: int = 1000,
+    charge_id: str | None = "STRIPE_CHARGE_ID",
+    refund_id: str | None = "STRIPE_REFUND_ID",
+    pledge: Pledge | None = None,
+    subscription: Subscription | None = None,
+    issue_reward: IssueReward | None = None,
+) -> Transaction:
+    transaction = Transaction(
+        type=TransactionType.dispute,
+        processor=processor,
+        currency=currency,
+        amount=amount,
+        account_currency=currency,
+        account_amount=amount,
+        tax_amount=0,
+        charge_id=charge_id,
+        refund_id=refund_id,
+        pledge=pledge,
+        subscription=subscription,
+        issue_reward=issue_reward,
+    )
+    session.add(transaction)
+    await session.commit()
+    return transaction
+
+
+async def create_dispute_transaction(
+    session: AsyncSession,
+    *,
+    processor: PaymentProcessor = PaymentProcessor.stripe,
+    currency: str = "usd",
+    amount: int = 1000,
+    charge_id: str | None = "STRIPE_CHARGE_ID",
+    dispute_id: str | None = "STRIPE_DISPUTE_ID",
+    pledge: Pledge | None = None,
+    subscription: Subscription | None = None,
+    issue_reward: IssueReward | None = None,
+) -> Transaction:
+    transaction = Transaction(
+        type=TransactionType.dispute,
+        processor=processor,
+        currency=currency,
+        amount=amount,
+        account_currency=currency,
+        account_amount=amount,
+        tax_amount=0,
+        charge_id=charge_id,
+        dispute_id=dispute_id,
+        pledge=pledge,
+        subscription=subscription,
+        issue_reward=issue_reward,
+    )
+    session.add(transaction)
+    await session.commit()
+    return transaction
+
+
+@pytest.mark.asyncio
+class TestCreatePaymentFees:
+    async def test_not_stripe(self, session: AsyncSession) -> None:
+        payment_transaction = await create_payment_transaction(
+            session, processor=PaymentProcessor.open_collective
+        )
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_payment_fees(
+            session, payment_transaction=payment_transaction
+        )
+        assert len(fee_transactions) == 0
+
+    async def test_stripe_no_charge_id(self, session: AsyncSession) -> None:
+        payment_transaction = await create_payment_transaction(session, charge_id=None)
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_payment_fees(
+            session, payment_transaction=payment_transaction
+        )
+        assert len(fee_transactions) == 0
+
+    async def test_stripe_subscription(
+        self, session: AsyncSession, stripe_service_mock: MagicMock
+    ) -> None:
+        payment_transaction = await create_payment_transaction(session)
+
+        stripe_service_mock.get_charge.return_value = stripe_lib.Charge.construct_from(
+            {
+                "id": "STRIPE_CHARGE_ID",
+                "balance_transaction": "STRIPE_BALANCE_TRANSACTION_ID",
+                "invoice": "STRIPE_INVOICE_ID",
+            },
+            None,
+        )
+        stripe_service_mock.get_balance_transaction.return_value = (
+            stripe_lib.BalanceTransaction.construct_from(
+                {"id": "STRIPE_BALANCE_TRANSACTION_ID", "fee": 100}, None
+            )
+        )
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_payment_fees(
+            session, payment_transaction=payment_transaction
+        )
+        assert len(fee_transactions) == 1
+
+        payment_fee_transaction = fee_transactions[0]
+
+        assert payment_fee_transaction.type == TransactionType.processor_fee
+        assert payment_fee_transaction.processor == PaymentProcessor.stripe
+        assert payment_fee_transaction.processor_fee_type == ProcessorFeeType.payment
+        assert payment_fee_transaction.amount == -100
+        assert (
+            payment_fee_transaction.incurred_by_transaction_id == payment_transaction.id
+        )
+
+
+@pytest.mark.asyncio
+class TestCreateRefundFees:
+    async def test_not_stripe(self, session: AsyncSession) -> None:
+        refund_transaction = await create_refund_transaction(
+            session, processor=PaymentProcessor.open_collective
+        )
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_refund_fees(
+            session, refund_transaction=refund_transaction
+        )
+        assert len(fee_transactions) == 0
+
+    async def test_stripe_no_refund_id(self, session: AsyncSession) -> None:
+        refund_transaction = await create_refund_transaction(session, refund_id=None)
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_refund_fees(
+            session, refund_transaction=refund_transaction
+        )
+        assert len(fee_transactions) == 0
+
+    async def test_stripe_refund(
+        self,
+        session: AsyncSession,
+        stripe_service_mock: MagicMock,
+    ) -> None:
+        refund_transaction = await create_refund_transaction(session)
+
+        stripe_service_mock.get_refund.return_value = stripe_lib.Refund.construct_from(
+            {
+                "id": "STRIPE_REFUND_ID",
+                "charge": "STRIPE_CHARGE_ID",
+                "currency": "usd",
+                "amount": 100,
+                "balance_transaction": "STRIPE_BALANCE_TRANSACTION_ID",
+            },
+            None,
+        )
+        stripe_service_mock.get_balance_transaction.return_value = (
+            stripe_lib.BalanceTransaction.construct_from(
+                {"id": "STRIPE_BALANCE_TRANSACTION_ID", "fee": 100}, None
+            )
+        )
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_refund_fees(
+            session, refund_transaction=refund_transaction
+        )
+        assert len(fee_transactions) == 1
+
+        refund_fee_transaction = fee_transactions[0]
+
+        assert refund_fee_transaction.type == TransactionType.processor_fee
+        assert refund_fee_transaction.processor == PaymentProcessor.stripe
+        assert refund_fee_transaction.processor_fee_type == ProcessorFeeType.refund
+        assert refund_fee_transaction.amount == -100
+        assert (
+            refund_fee_transaction.incurred_by_transaction_id == refund_transaction.id
+        )
+
+
+@pytest.mark.asyncio
+class TestCreateDisputeFees:
+    async def test_not_stripe(self, session: AsyncSession) -> None:
+        dispute_transaction = await create_dispute_transaction(
+            session, processor=PaymentProcessor.open_collective
+        )
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_dispute_fees(
+            session, dispute_transaction=dispute_transaction, category="dispute"
+        )
+        assert len(fee_transactions) == 0
+
+    async def test_stripe_no_dispute_id(self, session: AsyncSession) -> None:
+        dispute_transaction = await create_dispute_transaction(session, dispute_id=None)
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_dispute_fees(
+            session, dispute_transaction=dispute_transaction, category="dispute"
+        )
+        assert len(fee_transactions) == 0
+
+    @pytest.mark.parametrize("category", ["dispute", "dispute_reversal"])
+    async def test_stripe_dispute(
+        self,
+        category: Literal["dispute", "dispute_reversal"],
+        session: AsyncSession,
+        stripe_service_mock: MagicMock,
+    ) -> None:
+        dispute_transaction = await create_dispute_transaction(session)
+
+        stripe_service_mock.get_dispute.return_value = (
+            stripe_lib.Dispute.construct_from(
+                {
+                    "id": "STRIPE_DISPUTE_ID",
+                    "charge": "STRIPE_CHARGE_ID",
+                    "currency": "usd",
+                    "amount": 100,
+                    "balance_transactions": [
+                        {
+                            "id": "STRIPE_BALANCE_TRANSACTION_ID",
+                            "fee": 100,
+                            "reporting_category": category,
+                        }
+                    ],
+                },
+                None,
+            )
+        )
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.create_dispute_fees(
+            session, dispute_transaction=dispute_transaction, category=category
+        )
+        assert len(fee_transactions) == 1
+
+        dispute_fee_transaction = fee_transactions[0]
+
+        assert dispute_fee_transaction.type == TransactionType.processor_fee
+        assert dispute_fee_transaction.processor == PaymentProcessor.stripe
+        assert dispute_fee_transaction.processor_fee_type == ProcessorFeeType.dispute
+        assert dispute_fee_transaction.amount == -100
+        assert (
+            dispute_fee_transaction.incurred_by_transaction_id == dispute_transaction.id
+        )
+
+
+@pytest.mark.asyncio
+class TestSyncStripeFees:
+    async def test_sync_stripe_fees(
+        self,
+        session: AsyncSession,
+        stripe_service_mock: MagicMock,
+    ) -> None:
+        now_timestamp = int(datetime.datetime.now().timestamp())
+        balance_transactions = [
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_1",
+                    "net": -100,
+                    "currency": "usd",
+                    "description": "Connect (2024-01-01 - 2024-01-31): Payout Fee",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_2",
+                    "net": -200,
+                    "currency": "usd",
+                    "description": "Connect (2024-01-01 - 2024-01-31): Account Volume Billing",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_3",
+                    "net": -300,
+                    "currency": "usd",
+                    "description": "Connect (2024-01-01 - 2024-01-31): Active Account Billing",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_4",
+                    "net": -400,
+                    "currency": "usd",
+                    "description": "Connect (2024-01-01 - 2024-01-31): Cross-Border Transfers",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_5",
+                    "net": -500,
+                    "currency": "usd",
+                    "description": "Billing (2024-01-03): Subscriptions",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_6",
+                    "net": -600,
+                    "currency": "usd",
+                    "description": "Automatic Taxes (2024-01-03): Automatic tax",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_7",
+                    "net": -700,
+                    "currency": "usd",
+                    "description": "Invoicing (2024-01-03): Invoicing Plus",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_8",
+                    "net": -100,
+                    "currency": "usd",
+                    "description": "Connect (2024-01-01 - 2024-01-31): Payout Fee",
+                },
+                None,
+            ),
+            stripe_lib.BalanceTransaction.construct_from(
+                {
+                    "created": now_timestamp,
+                    "id": "STRIPE_BALANCE_TRANSACTION_ID_9",
+                    "net": -200,
+                    "currency": "usd",
+                    "description": "Connect (2024-01-01 - 2024-01-31): Account Volume Billing",
+                },
+                None,
+            ),
+        ]
+
+        stripe_service_mock.list_balance_transactions.return_value = (
+            balance_transactions
+        )
+
+        fee_transaction_9 = Transaction(
+            type=TransactionType.processor_fee,
+            processor=PaymentProcessor.stripe,
+            processor_fee_type=ProcessorFeeType.payout,
+            currency="usd",
+            amount=-200,
+            account_currency="usd",
+            account_amount=-200,
+            tax_amount=0,
+            fee_balance_transaction_id="STRIPE_BALANCE_TRANSACTION_ID_9",
+        )
+        fee_transaction_8 = Transaction(
+            type=TransactionType.processor_fee,
+            processor=PaymentProcessor.stripe,
+            processor_fee_type=ProcessorFeeType.payout,
+            currency="usd",
+            amount=-100,
+            account_currency="usd",
+            account_amount=-100,
+            tax_amount=0,
+            fee_balance_transaction_id="STRIPE_BALANCE_TRANSACTION_ID_8",
+        )
+        session.add(fee_transaction_9)
+        session.add(fee_transaction_8)
+        await session.commit()
+
+        # then
+        session.expunge_all()
+
+        fee_transactions = await fee_transaction_service.sync_stripe_fees(session)
+
+        assert len(fee_transactions) == 7
+
+        (
+            fee_transaction_1,
+            fee_transaction_2,
+            fee_transaction_3,
+            fee_transaction_4,
+            fee_transaction_5,
+            fee_transaction_6,
+            fee_transaction_7,
+        ) = fee_transactions
+
+        assert fee_transaction_1.type == TransactionType.processor_fee
+        assert fee_transaction_1.processor_fee_type == ProcessorFeeType.payout
+        assert fee_transaction_1.amount == -100
+
+        assert fee_transaction_2.type == TransactionType.processor_fee
+        assert fee_transaction_2.processor_fee_type == ProcessorFeeType.payout
+        assert fee_transaction_2.amount == -200
+
+        assert fee_transaction_3.type == TransactionType.processor_fee
+        assert fee_transaction_3.processor_fee_type == ProcessorFeeType.account
+        assert fee_transaction_3.amount == -300
+
+        assert fee_transaction_4.type == TransactionType.processor_fee
+        assert (
+            fee_transaction_4.processor_fee_type
+            == ProcessorFeeType.cross_border_transfer
+        )
+        assert fee_transaction_4.amount == -400
+
+        assert fee_transaction_5.type == TransactionType.processor_fee
+        assert fee_transaction_5.processor_fee_type == ProcessorFeeType.subscription
+        assert fee_transaction_5.amount == -500
+
+        assert fee_transaction_6.type == TransactionType.processor_fee
+        assert fee_transaction_6.processor_fee_type == ProcessorFeeType.tax
+        assert fee_transaction_6.amount == -600
+
+        assert fee_transaction_7.type == TransactionType.processor_fee
+        assert fee_transaction_7.processor_fee_type == ProcessorFeeType.invoice
+        assert fee_transaction_7.amount == -700

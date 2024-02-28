52687671c22b2a5a2969defaa130978dfb34b03f
Emil Widlund <hello@emilwidlund.com>
2024-02-28 15:31:08 +0100

    clients/profile: refactor org layout

--- clients/apps/web/src/app/[organization]/ClientPage.tsx
+++ clients/apps/web/src/app/[organization]/ClientPage.tsx
@@ -60,73 +60,7 @@ const ClientPage = ({
 
   return (
     <div className="flex w-full flex-col gap-y-6">
-      <div className="flex w-full flex-col gap-y-12">
-        {(posts.length ?? 0) > 0 ? (
-          <div className="flex flex-col gap-y-8">
-            <div className="flex flex-row justify-between">
-              <h2 className="text-lg">Pinned & Latest Posts</h2>
-              <Link
-                className="text-sm text-blue-500 dark:text-blue-400"
-                href={`/${organization.name}/posts`}
-              >
-                <span>View all posts</span>
-                <ArrowForwardOutlined className="ml-2" fontSize="inherit" />
-              </Link>
-            </div>
-            <div className="flex w-full flex-col gap-y-6">
-              {posts.map((post) => (
-                <PostComponent article={post} key={post.id} highlightPinned />
-              ))}
-            </div>
-          </div>
-        ) : null}
-        {highlightedTiers.length > 1 && (
-          <div className="flex flex-col items-center gap-y-12 md:py-12">
-            <div className="flex flex-col items-center gap-y-4">
-              <BoltOutlined
-                className="text-blue-500 dark:text-blue-400"
-                fontSize="large"
-              />
-              <h2 className="text-xl">Subscriptions</h2>
-              <p className="dark:text-polar-500 text-center text-gray-500 [text-wrap:balance]">
-                Support {organization.name} with a subscription & receive unique
-                benefits in return
-              </p>
-            </div>
-            <div className="flex w-fit flex-row flex-wrap gap-8">
-              {highlightedTiers.map((tier) => (
-                <SubscriptionTierCard
-                  variant="small"
-                  className="w-full self-stretch md:w-[276px]"
-                  key={tier.id}
-                  subscriptionTier={tier}
-                >
-                  {shouldRenderSubscribeButton &&
-                    (tier.type === 'free' ? (
-                      <FreeTierSubscribe
-                        subscriptionTier={tier}
-                        organization={organization}
-                      />
-                    ) : (
-                      <SubscriptionTierSubscribeButton
-                        organization={organization}
-                        subscriptionTier={tier}
-                        subscribePath="/subscribe"
-                      />
-                    ))}
-                </SubscriptionTierCard>
-              ))}
-            </div>
-            <Link
-              className="text-sm text-blue-500 dark:text-blue-400"
-              href={`/${organization.name}/subscriptions`}
-            >
-              <span>View all tiers</span>
-              <ArrowForwardOutlined className="ml-2" fontSize="inherit" />
-            </Link>
-          </div>
-        )}
-
+      <div className="flex w-full flex-col gap-y-24">
         {repositories.length > 0 && (
           <div className="flex flex-col gap-y-8">
             <div className="flex flex-row items-center justify-between">
@@ -185,6 +119,71 @@ const ClientPage = ({
                 </Link>
               ))}
             </div>
+          </div>
+        )}
+        {(posts.length ?? 0) > 0 ? (
+          <div className="flex flex-col gap-y-8">
+            <div className="flex flex-row justify-between">
+              <h2 className="text-lg">Pinned & Latest Posts</h2>
+              <Link
+                className="text-sm text-blue-500 dark:text-blue-400"
+                href={`/${organization.name}/posts`}
+              >
+                <span>View all posts</span>
+                <ArrowForwardOutlined className="ml-2" fontSize="inherit" />
+              </Link>
+            </div>
+            <div className="flex w-full flex-col gap-y-6">
+              {posts.map((post) => (
+                <PostComponent article={post} key={post.id} highlightPinned />
+              ))}
+            </div>
+          </div>
+        ) : null}
+        {highlightedTiers.length > 1 && (
+          <div className="flex flex-col items-center gap-y-12">
+            <div className="flex flex-col items-center gap-y-4">
+              <BoltOutlined
+                className="text-blue-500 dark:text-blue-400"
+                fontSize="large"
+              />
+              <h2 className="text-xl">Subscriptions</h2>
+              <p className="dark:text-polar-500 text-center text-gray-500 [text-wrap:balance]">
+                Support {organization.name} with a subscription & receive unique
+                benefits in return
+              </p>
+            </div>
+            <div className="flex w-fit flex-row flex-nowrap gap-8">
+              {highlightedTiers.map((tier) => (
+                <SubscriptionTierCard
+                  variant="small"
+                  className="w-full self-stretch md:w-[276px]"
+                  key={tier.id}
+                  subscriptionTier={tier}
+                >
+                  {shouldRenderSubscribeButton &&
+                    (tier.type === 'free' ? (
+                      <FreeTierSubscribe
+                        subscriptionTier={tier}
+                        organization={organization}
+                      />
+                    ) : (
+                      <SubscriptionTierSubscribeButton
+                        organization={organization}
+                        subscriptionTier={tier}
+                        subscribePath="/subscribe"
+                      />
+                    ))}
+                </SubscriptionTierCard>
+              ))}
+            </div>
+            <Link
+              className="text-sm text-blue-500 dark:text-blue-400"
+              href={`/${organization.name}/subscriptions`}
+            >
+              <span>View all tiers</span>
+              <ArrowForwardOutlined className="ml-2" fontSize="inherit" />
+            </Link>
           </div>
         )}
 
---
+++ clients/apps/web/src/app/[organization]/LayoutPolarMenu.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { ProfileMenu } from '@/components/Shared/ProfileSelection'
+import { UserRead } from '@polar-sh/sdk'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { getGitHubAuthorizeURL } from 'polarkit/auth'
+import { LogoIcon } from 'polarkit/components/brand'
+import { Button } from 'polarkit/components/ui/atoms'
+
+export const PolarMenu = ({
+  authenticatedUser,
+}: {
+  authenticatedUser?: UserRead
+}) => {
+  const pathname = usePathname()
+  const returnTo = pathname ?? '/feed'
+
+  return (
+    <div className="flex flex-row items-center gap-x-4">
+      <a href="/">
+        <LogoIcon className="text-blue-500 dark:text-blue-400" size={40} />
+      </a>
+      {authenticatedUser ? (
+        <div>
+          <div className="relative flex w-max flex-shrink-0 flex-row items-center justify-between gap-x-6">
+            <ProfileMenu
+              authenticatedUser={authenticatedUser}
+              className="flex-shrink-0"
+            />
+          </div>
+        </div>
+      ) : (
+        <>
+          <CreateWithPolar returnTo={returnTo} />
+        </>
+      )}
+    </div>
+  )
+}
+
+const CreateWithPolar = ({ returnTo }: { returnTo: string }) => {
+  const search = useSearchParams()
+  const authorizeURL = getGitHubAuthorizeURL({
+    paymentIntentId: search?.get('payment_intent_id') ?? undefined,
+    returnTo: returnTo,
+  })
+
+  return (
+    <a href={authorizeURL}>
+      <Button variant="secondary" asChild>
+        Create with Polar
+      </Button>
+    </a>
+  )
+}

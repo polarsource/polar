f5cb508918c047bdbd1dfd7945a1b43e494fbe57
Gustav Westling <gustav@polar.sh>
2024-03-04 09:46:24 +0100

    clients/astro: run prettier format

--- clients/packages/astro/src/schemas.ts
+++ clients/packages/astro/src/schemas.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 /**
  * A useful Zod schema for adding Polar article metadata to your
@@ -15,31 +15,32 @@ import { z } from 'zod'
  *    })),
  * })
  */
-export const polarArticleSchema = z.object({
-  /**
-   * The title of the article on Polar
-   */
-  title: z.string(),
-  /**
-   * Whether the article is public, hidden, or private
-   */
-  visibility: z.enum(['public', 'hidden', 'private']),
-  /**
-   * Whether the article is for paid subscribers only
-   */
-  paid_subscribers_only: z.boolean(),
-  /**
-   * The date the article was published. Set to a future date to schedule
-   * the article to be published at a later time
-   */
-  published_at: z.coerce.date(),
-  /**
-   * Whether to notify subscribers of the new article
-   */
-  notify_subscribers: z.boolean(),
-  /**
-   * Whether the article is pinned to the top of your profile
-   */
-  is_pinned: z.boolean(),
-}).partial()
-
+export const polarArticleSchema = z
+	.object({
+		/**
+		 * The title of the article on Polar
+		 */
+		title: z.string(),
+		/**
+		 * Whether the article is public, hidden, or private
+		 */
+		visibility: z.enum(['public', 'hidden', 'private']),
+		/**
+		 * Whether the article is for paid subscribers only
+		 */
+		paid_subscribers_only: z.boolean(),
+		/**
+		 * The date the article was published. Set to a future date to schedule
+		 * the article to be published at a later time
+		 */
+		published_at: z.coerce.date(),
+		/**
+		 * Whether to notify subscribers of the new article
+		 */
+		notify_subscribers: z.boolean(),
+		/**
+		 * Whether the article is pinned to the top of your profile
+		 */
+		is_pinned: z.boolean(),
+	})
+	.partial();

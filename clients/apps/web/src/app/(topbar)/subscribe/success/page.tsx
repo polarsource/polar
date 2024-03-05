d755f85fb6c02d0e7f329ac99a64d65092517266
Gustav Westling <gustav@polar.sh>
2024-03-05 11:49:06 +0100

    clients/subscribe: login post subscribe handle custom domains

--- clients/apps/web/src/app/(topbar)/subscribe/success/page.tsx
+++ clients/apps/web/src/app/(topbar)/subscribe/success/page.tsx
@@ -1,6 +1,7 @@
 import SubscriptionSuccess from '@/components/Subscriptions/SubscriptionSuccess'
+import { PublicPageOrganizationContextProvider } from '@/providers/organization'
 import { getServerSideAPI } from '@/utils/api'
-import { ResponseError } from '@polar-sh/sdk'
+import { Organization, ResponseError, SubscribeSession } from '@polar-sh/sdk'
 import { notFound } from 'next/navigation'
 
 export default async function Page({
@@ -9,14 +10,49 @@ export default async function Page({
   searchParams: { session_id: string }
 }) {
   const api = getServerSideAPI()
+
+  let subscribeSession: SubscribeSession | undefined
+
   try {
-    const subscribeSession = await api.subscriptions.getSubscribeSession({
+    subscribeSession = await api.subscriptions.getSubscribeSession({
       id: searchParams.session_id,
     })
-    return <SubscriptionSuccess subscribeSession={subscribeSession} />
   } catch (e) {
     if (e instanceof ResponseError && e.response.status === 404) {
       notFound()
+    } else {
+      throw e
     }
   }
+
+  if (!subscribeSession) {
+    notFound()
+  }
+
+  if (!subscribeSession.subscription_tier.organization_id) {
+    notFound()
+  }
+
+  let organization: Organization | undefined
+  try {
+    organization = await api.organizations.get({
+      id: subscribeSession.subscription_tier.organization_id,
+    })
+  } catch (e) {
+    if (e instanceof ResponseError && e.response.status === 404) {
+      notFound()
+    } else {
+      throw e
+    }
+  }
+
+  if (!organization) {
+    notFound()
+  }
+
+  return (
+    <PublicPageOrganizationContextProvider organization={organization}>
+      <SubscriptionSuccess subscribeSession={subscribeSession} />
+    </PublicPageOrganizationContextProvider>
+  )
 }

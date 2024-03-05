f8d5aaeb04cda35f9825da233adce84af4553c04
Emil Widlund <hello@emilwidlund.com>
2024-03-05 14:12:41 +0100

    clients/org-page: implement better looking post upsell

--- clients/apps/web/src/app/[organization]/(sidebar)/ClientPage.tsx
+++ clients/apps/web/src/app/[organization]/(sidebar)/ClientPage.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { HighlightedPost } from '@/components/Feed/Posts/HighlightedPost'
 import { Post as PostComponent } from '@/components/Feed/Posts/Post'
 import { FreeTierSubscribe } from '@/components/Organization/FreeTierSubscribe'
 import { OrganizationIssueSummaryList } from '@/components/Organization/OrganizationIssueSummaryList'
@@ -68,24 +69,34 @@ const ClientPage = ({
     [subscriptionsSummary],
   )
 
+  const [highlightedPost, ...restPosts] = posts
+
   return (
     <div className="flex w-full flex-col gap-y-6">
       <div className="flex w-full flex-col gap-y-24">
-        {(posts.length ?? 0) > 0 ? (
+        {highlightedPost || (restPosts.length ?? 0) > 0 ? (
+          <div className="flex flex-col gap-y-2 md:flex-row md:justify-between">
+            <h2 className="text-lg">Pinned & Latest Posts</h2>
+            <Link
+              className="text-sm text-blue-500 dark:text-blue-400"
+              href={organizationPageLink(organization, 'posts')}
+            >
+              <span>View all posts</span>
+              <ArrowForwardOutlined className="ml-2" fontSize="inherit" />
+            </Link>
+          </div>
+        ) : null}
+        {highlightedPost && <HighlightedPost post={highlightedPost} />}
+        {(restPosts.length ?? 0) > 0 ? (
           <div className="flex flex-col gap-y-8">
-            <div className="flex flex-col gap-y-2 md:flex-row md:justify-between">
-              <h2 className="text-lg">Pinned & Latest Posts</h2>
-              <Link
-                className="text-sm text-blue-500 dark:text-blue-400"
-                href={organizationPageLink(organization, 'posts')}
-              >
-                <span>View all posts</span>
-                <ArrowForwardOutlined className="ml-2" fontSize="inherit" />
-              </Link>
-            </div>
-            <div className="flex w-full flex-col gap-y-6">
-              {posts.map((post) => (
-                <PostComponent article={post} key={post.id} highlightPinned />
+            <div className="grid grid-cols-2 gap-6">
+              {restPosts.map((post) => (
+                <PostComponent
+                  className={restPosts.length === 1 ? 'col-span-2' : ''}
+                  article={post}
+                  key={post.id}
+                  highlightPinned
+                />
               ))}
             </div>
           </div>

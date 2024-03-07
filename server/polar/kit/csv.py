c164c7dfd8116ca623d2a04a8ed9d0b20780200f
François Voron <fvoron@gmail.com>
2024-03-07 10:04:33 +0100

    server/transaction: implement CSV payout receipts

--- server/polar/kit/csv.py
+++ server/polar/kit/csv.py
@@ -1,6 +1,10 @@
+import collections
 import csv
 from collections.abc import Iterable
-from typing import BinaryIO
+from typing import TYPE_CHECKING, Any, BinaryIO
+
+if TYPE_CHECKING:
+    import _csv
 
 from .email import EmailNotValidError, validate_email
 
@@ -35,3 +39,51 @@ def get_emails_from_csv(lines: Iterable[str]) -> set[str]:
                 emails.add(email)
 
     return emails
+
+
+class IterableCSVWriter:
+    """
+    Utility class wrapping the built-in csv.writer allowing
+    to generate CSV rows as strings.
+
+    It's useful to generate CSV with StreamingResponse, for example.
+    """
+
+    writer: "_csv._writer"
+
+    def __init__(
+        self,
+        dialect: "_csv._DialectLike" = "excel",
+        *,
+        delimiter: str = ",",
+        quotechar: str | None = '"',
+        escapechar: str | None = None,
+        doublequote: bool = True,
+        skipinitialspace: bool = False,
+        lineterminator: str = "\r\n",
+        quoting: "_csv._QuotingType" = 0,
+        strict: bool = False,
+    ) -> None:
+        self._lines: collections.deque[str] = collections.deque()
+        self.writer = csv.writer(
+            self,
+            dialect=dialect,
+            delimiter=delimiter,
+            quotechar=quotechar,
+            escapechar=escapechar,
+            doublequote=doublequote,
+            skipinitialspace=skipinitialspace,
+            lineterminator=lineterminator,
+            quoting=quoting,
+            strict=strict,
+        )
+
+    def getrow(self, row: Iterable[Any]) -> str:
+        self.writer.writerow(row)
+        return self.read()
+
+    def write(self, line: str) -> None:
+        self._lines.append(line)
+
+    def read(self) -> str:
+        return self._lines.popleft()

6ed0e474c208da326b51c881fcd312ee4a0d4d3e
Gustav Westling <gustav@polar.sh>
2024-03-08 16:55:23 +0100

    clients/PolarTimeAgo: allow server side rendering

--- clients/packages/polarkit/src/components/ui/atoms/PolarTimeAgo.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/PolarTimeAgo.tsx
@@ -1,22 +1,9 @@
-'use client'
-
-import { useEffect, useState } from 'react'
 import TimeAgo from 'react-timeago'
 
 const PolarTimeAgo = (props: { date: Date; suffix?: string }) => {
-  const [rendered, setRendered] = useState(false)
-
-  useEffect(() => {
-    setRendered(true)
-  }, [])
-
-  // hide during hydration
-  if (!rendered) {
-    return <></>
-  }
-
   return (
     <TimeAgo
+      suppressHydrationWarning
       date={props.date}
       formatter={(value, unit, suffix) => {
         if (unit === 'second') {

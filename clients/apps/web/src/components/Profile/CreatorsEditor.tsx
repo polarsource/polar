3f3860122cb44706671ad33562a31b3b79f090d3
Emil Widlund <hello@emilwidlund.com>
2024-03-08 12:03:38 +0100

    clients/org-profile: polish dnd behaviour and empty state

--- clients/apps/web/src/components/Profile/CreatorsEditor.tsx
+++ clients/apps/web/src/components/Profile/CreatorsEditor.tsx
@@ -13,113 +13,15 @@ import {
   SortableContext,
   arrayMove,
   rectSortingStrategy,
-  useSortable,
 } from '@dnd-kit/sortable'
-import { CSS } from '@dnd-kit/utilities'
-import { DragIndicatorOutlined } from '@mui/icons-material'
+import { FaceOutlined, TuneOutlined } from '@mui/icons-material'
 import { Organization, OrganizationProfileSettings } from '@polar-sh/sdk'
-import Link from 'next/link'
-import Avatar from 'polarkit/components/ui/atoms/avatar'
-import Button from 'polarkit/components/ui/atoms/button'
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from 'polarkit/components/ui/atoms/card'
-import { useGetOrganization, useUpdateOrganization } from 'polarkit/hooks'
-import { organizationPageLink } from 'polarkit/utils/nav'
+import { useUpdateOrganization } from 'polarkit/hooks'
 import { useState } from 'react'
-import { twMerge } from 'tailwind-merge'
 import { Modal } from '../Modal'
 import { useModal } from '../Modal/useModal'
+import { CreatorCard, DraggableCreatorCard } from './CreatorCard'
 import { CreatorsModal } from './CreatorsModal'
-
-const CreatorCard = ({
-  organizationId,
-  disabled,
-}: {
-  organizationId: string
-  disabled?: boolean
-}) => {
-  const sortable = useSortable({ id: organizationId })
-  const {
-    attributes,
-    listeners,
-    isDragging,
-    setNodeRef,
-    setDraggableNodeRef,
-    transform,
-    transition,
-  } = sortable
-
-  const organization = useGetOrganization(organizationId).data
-
-  if (!organization) return null
-
-  return (
-    <Card
-      ref={setNodeRef}
-      style={{
-        transform: CSS.Transform.toString(transform),
-        transition,
-      }}
-      className={twMerge(
-        'dark:hover:bg-polar-800 dark:text-polar-500 dark:hover:text-polar-300 transition-color flex h-full flex-col gap-y-2 rounded-3xl text-gray-500 duration-100 hover:bg-gray-50 hover:text-gray-600',
-      )}
-      {...attributes}
-      {...listeners}
-    >
-      <Link className="h-full" href={organizationPageLink(organization)}>
-        <CardHeader className="flex flex-row justify-between p-6">
-          <Avatar
-            className="h-16 w-16"
-            avatar_url={organization.avatar_url}
-            name={organization.name}
-          />
-          {!disabled && (
-            <span ref={setDraggableNodeRef} className="cursor-grab">
-              <DragIndicatorOutlined
-                className={twMerge('dark:text-polar-600 text-gray-400')}
-                fontSize="small"
-              />
-            </span>
-          )}
-        </CardHeader>
-        <CardContent className="flex h-full grow flex-col flex-wrap gap-y-4 px-6 py-0">
-          <div className="flex flex-row items-baseline gap-x-3">
-            <h3 className="dark:text-polar-50 text-lg text-gray-950">
-              {organization.pretty_name || organization.name}
-            </h3>
-            {organization.pretty_name && (
-              <h3 className="text-blue-500 dark:text-blue-400">
-                @{organization.name}
-              </h3>
-            )}
-          </div>
-          {organization.bio && (
-            <p className="[text-wrap:pretty]">{organization.bio}</p>
-          )}
-        </CardContent>
-      </Link>
-      <CardFooter className="flex flex-row items-center justify-between gap-x-4 p-6">
-        <div className="flex-items flex items-center gap-x-2">
-          <Link href={organizationPageLink(organization, 'subscriptions')}>
-            <Button size="sm">Subscribe</Button>
-          </Link>
-          <Link
-            href={`https://github.com/${organization.name}`}
-            target="_blank"
-          >
-            <Button size="sm" variant="ghost">
-              GitHub Profile
-            </Button>
-          </Link>
-        </div>
-      </CardFooter>
-    </Card>
-  )
-}
 
 export interface CreatorsEditorProps {
   organization: Organization
@@ -180,7 +82,44 @@ export const CreatorsEditor = ({
     setActiveId(null)
   }
 
-  return (
+  const EditorEmptyState = () => {
+    return (
+      <div className="flex flex-col gap-y-8">
+        <div className="dark:border-polar-800 dark:bg-polar-900 flex flex-col items-center gap-y-4 rounded-3xl border-gray-100 bg-white py-16 shadow-sm">
+          <FaceOutlined
+            fontSize="large"
+            className="text-blue-500 dark:text-blue-400"
+          />
+          <h3 className="text-center text-lg">Featured Developers</h3>
+          <p
+            className="cursor-pointer text-center text-blue-500 dark:text-blue-400"
+            onClick={show}
+          >
+            Add featured developers to your profile
+          </p>
+        </div>
+        <Modal
+          className="w-full md:max-w-lg lg:max-w-lg"
+          isShown={isShown}
+          hide={hide}
+          modalContent={
+            <CreatorsModal
+              creators={featuredCreators.map((c) => ({ id: c.id }))}
+              organization={organization}
+              setCreators={updateFeaturedCreators}
+              hideModal={hide}
+            />
+          }
+        />
+      </div>
+    )
+  }
+
+  if (featuredCreators.length === 0 && !disabled) {
+    return <EditorEmptyState />
+  }
+
+  return featuredCreators.length > 0 ? (
     <DndContext
       sensors={sensors}
       collisionDetection={closestCenter}
@@ -190,17 +129,21 @@ export const CreatorsEditor = ({
     >
       <SortableContext items={featuredCreators} strategy={rectSortingStrategy}>
         <div className="flex flex-col gap-y-8">
-          <div className="flex flex-col gap-y-2 md:flex-row md:justify-between">
-            <h3 className="text-lg">Featured Creators</h3>
+          <div className="flex flex-col items-start gap-y-2 md:flex-row md:justify-between">
+            <h3 className="text-lg">Featured Developers</h3>
             {!disabled && (
-              <Button variant="ghost" onClick={show}>
-                Configure Creators
-              </Button>
+              <div
+                className="flex cursor-pointer flex-row items-center gap-x-2 text-sm text-blue-500 dark:text-blue-400"
+                onClick={show}
+              >
+                <TuneOutlined fontSize="small" />
+                <span>Configure</span>
+              </div>
             )}
           </div>
           <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
             {featuredCreators.map((creator) => (
-              <CreatorCard
+              <DraggableCreatorCard
                 key={creator.id}
                 organizationId={creator.id}
                 disabled={disabled}
@@ -219,7 +162,7 @@ export const CreatorsEditor = ({
           hide={hide}
           modalContent={
             <CreatorsModal
-              creators={profile.featured_organizations.map((id) => ({ id }))}
+              creators={featuredCreators.map((c) => ({ id: c.id }))}
               organization={organization}
               setCreators={updateFeaturedCreators}
               hideModal={hide}
@@ -228,5 +171,5 @@ export const CreatorsEditor = ({
         />
       </SortableContext>
     </DndContext>
-  )
+  ) : null
 }
---
+++ clients/apps/web/src/components/Profile/ProjectCard.tsx
@@ -0,0 +1,125 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { StarIcon } from '@heroicons/react/20/solid'
+import { StarIcon as StarIconOutlined } from '@heroicons/react/24/outline'
+import { DragIndicatorOutlined, HiveOutlined } from '@mui/icons-material'
+import { Repository } from '@polar-sh/sdk'
+import Link from 'next/link'
+import { Pill } from 'polarkit/components/ui/atoms'
+import Button from 'polarkit/components/ui/atoms/button'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from 'polarkit/components/ui/atoms/card'
+import { formatStarsNumber } from 'polarkit/utils'
+import { organizationPageLink } from 'polarkit/utils/nav'
+import { twMerge } from 'tailwind-merge'
+
+export const ProjectCard = ({
+  repository,
+  disabled,
+  sortable,
+}: {
+  repository: Repository
+  disabled?: boolean
+  sortable?: ReturnType<typeof useSortable>
+}) => {
+  return (
+    <Card
+      ref={sortable ? sortable.setNodeRef : undefined}
+      style={
+        sortable
+          ? {
+              transform: CSS.Transform.toString(sortable.transform),
+              transition: sortable.transition,
+            }
+          : {}
+      }
+      className={twMerge(
+        'dark:text-polar-500 transition-color dark:hover:text-polar-300 dark:hover:bg-polar-800 transition-color flex h-full flex-col gap-y-2 rounded-3xl text-gray-500 hover:bg-gray-50 hover:text-gray-600',
+        sortable?.isDragging && 'opacity-30',
+      )}
+    >
+      <Link
+        className="h-full"
+        href={organizationPageLink(repository.organization, repository.name)}
+      >
+        <CardHeader className="flex flex-row justify-between p-6">
+          <div className="flex flex-row items-baseline gap-x-3">
+            <span className="text-[20px] text-blue-500">
+              <HiveOutlined fontSize="inherit" />
+            </span>
+            <h3 className="dark:text-polar-50 text-lg text-gray-950">
+              {repository.name}
+            </h3>
+          </div>
+          {!disabled && (
+            <span
+              ref={
+                disabled || !sortable ? undefined : sortable.setDraggableNodeRef
+              }
+              className="cursor-grab"
+              {...sortable?.attributes}
+              {...sortable?.listeners}
+            >
+              <DragIndicatorOutlined
+                className={twMerge('dark:text-polar-600 text-gray-400')}
+                fontSize="small"
+              />
+            </span>
+          )}
+        </CardHeader>
+        <CardContent className="flex h-full grow flex-col flex-wrap px-6 py-0">
+          {repository.description && <p>{repository.description}</p>}
+        </CardContent>
+      </Link>
+      <CardFooter className="flex flex-row items-center justify-between gap-x-4 p-6">
+        <div className="flex-items flex items-center gap-x-4">
+          {repository.license && (
+            <Pill className="px-3 py-1.5" color="blue">
+              {repository.license}
+            </Pill>
+          )}
+          <span className="flex flex-row items-center gap-x-1 text-sm">
+            <StarIcon className="h-4 w-4" />
+            <span className="pt-.5">
+              {formatStarsNumber(repository.stars ?? 0)}
+            </span>
+          </span>
+        </div>
+        <Link
+          href={`https://github.com/${repository.organization.name}/${repository.name}`}
+          target="_blank"
+          onClick={(e) => {
+            e.stopPropagation()
+          }}
+        >
+          <Button className="rounded-lg px-2.5" variant="secondary" size="sm">
+            <StarIconOutlined className="mr-2 h-4 w-4" />
+            <span>Star on GitHub</span>
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export const DraggableProjectCard = ({
+  repository,
+  disabled,
+}: {
+  repository: Repository
+  disabled?: boolean
+}) => {
+  const sortable = useSortable({ id: repository.id })
+
+  return (
+    <ProjectCard
+      sortable={sortable}
+      repository={repository}
+      disabled={disabled}
+    />
+  )
+}

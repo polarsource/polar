9400720ab6344391bb1b21fd3b1d8c251946da19
Emil Widlund <hello@emilwidlund.com>
2024-03-11 14:12:39 +0100

    clients/profile: fallback to flex-col on creator cards on mobile

--- clients/apps/web/src/components/Profile/CreatorCard.tsx
+++ clients/apps/web/src/components/Profile/CreatorCard.tsx
@@ -63,7 +63,7 @@ export const CreatorCard = ({
           )}
         </CardHeader>
         <CardContent className="flex h-full grow flex-col flex-wrap gap-y-4 px-6 py-0">
-          <div className="flex flex-row items-baseline gap-x-3">
+          <div className="flex flex-col items-baseline gap-x-3 md:flex-row">
             <h3 className="dark:text-polar-50 text-lg text-gray-950">
               {organization.pretty_name || organization.name}
             </h3>

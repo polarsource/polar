3b7ad37e9f96adf35eef55317d8c5adf9c28775b
Emil Widlund <hello@emilwidlund.com>
2024-03-15 10:10:01 +0100

    clients/description-editor: replace textarea with contenteditable paragraph

--- clients/apps/web/src/components/Profile/DescriptionEditor/DescriptionEditor.tsx
+++ clients/apps/web/src/components/Profile/DescriptionEditor/DescriptionEditor.tsx
@@ -1,12 +1,5 @@
 import { SpinnerNoMargin } from '@/components/Shared/Spinner'
-import {
-  ChangeEventHandler,
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react'
+import { FormEventHandler, useCallback, useRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export interface DescriptionEditorProps {
@@ -27,34 +20,14 @@ export const DescriptionEditor = ({
   size = 'default',
 }: DescriptionEditorProps) => {
   const paragraphRef = useRef<HTMLParagraphElement>(null)
-  const inputRef = useRef<HTMLTextAreaElement>(null)
-  const [internalDescription, setInternalDescription] = useState(description)
 
-  const resizeTextarea = useCallback(() => {
-    if (inputRef?.current) {
-      inputRef.current.style.height = ''
-      inputRef.current.style.height = inputRef.current.scrollHeight + 'px'
-    }
-  }, [])
-
-  useLayoutEffect(() => {
-    resizeTextarea()
-  }, [internalDescription, resizeTextarea])
-
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback(
+  const handleChange: FormEventHandler<HTMLParagraphElement> = useCallback(
     (e) => {
-      setInternalDescription(e.target.value ?? '')
-      onChange(e.target.value ?? '')
+      if (!paragraphRef.current) return
+      onChange((e.target as HTMLParagraphElement).innerText ?? '')
     },
     [onChange],
   )
-
-  useEffect(() => {
-    window.addEventListener('resize', resizeTextarea)
-    return () => {
-      window.removeEventListener('resize', resizeTextarea)
-    }
-  }, [resizeTextarea])
 
   return (
     <div
@@ -66,28 +39,25 @@ export const DescriptionEditor = ({
         size === 'default' ? '-m-6 p-6' : '-m-4 p-4',
       )}
     >
-      <textarea
-        ref={inputRef}
-        className={twMerge(
-          'dark:text-polar-50 hidden h-[2.8rem] w-full resize-none border-none bg-transparent p-0 text-3xl !font-normal leading-normal text-gray-950 outline-none focus:ring-0 md:block',
-          className,
-        )}
-        value={internalDescription}
-        onChange={handleChange}
-        disabled={disabled}
-      />
-
       <p
         ref={paragraphRef}
         className={twMerge(
-          'dark:text-polar-50 block w-full text-3xl !font-normal leading-normal text-gray-950 md:hidden',
+          'dark:text-polar-50 w-full text-3xl !font-normal leading-normal text-gray-950 [text-wrap:pretty] focus-visible:outline-0',
           className,
         )}
+        contentEditable={!disabled}
+        onBlur={handleChange}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault()
+          }
+        }}
       >
-        {internalDescription}
+        {description}
       </p>
+
       {loading && (
-        <div className="absolute bottom-4 right-4">
+        <div className="absolute right-2 top-2">
           <SpinnerNoMargin
             className={twMerge('dark:text-polar-50 h-4 w-4 text-gray-950')}
           />

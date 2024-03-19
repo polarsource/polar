6d6222282ce755a4308ddafb170ce2db60dfd94a
Gustav Westling <gustav@polar.sh>
2024-03-19 10:42:34 +0100

    clients/sb: chromatic integration in storybook

--- clients/apps/web/.storybook/main.ts
+++ clients/apps/web/.storybook/main.ts
@@ -1,5 +1,5 @@
-import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/nextjs'
+import { dirname, join } from 'path'
 
 const config: StorybookConfig = {
   stories: [
@@ -8,12 +8,13 @@ const config: StorybookConfig = {
     '../../../packages/polarkit/src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@chromatic-com/storybook'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
+    name: getAbsolutePath('@storybook/nextjs'),
     options: {},
   },
   docs: {
@@ -27,5 +28,5 @@ const config: StorybookConfig = {
 export default config
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')))
 }

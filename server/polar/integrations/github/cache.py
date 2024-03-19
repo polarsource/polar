55df591720cf22fcfa79a8ad35e107e58f200c44
Gustav Westling <gustav@polar.sh>
2024-03-19 17:27:55 +0100

    server/github: separate cache keyspaces per app

--- server/polar/integrations/github/cache.py
+++ server/polar/integrations/github/cache.py
@@ -8,17 +8,17 @@ from polar.redis import redis
 class RedisCache(BaseCache):
     """Redis Backed Cache"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, app: str) -> None:
+        self.app = app
 
     def get(self, key: str) -> str | None:
         raise NotImplementedError()
 
     async def aget(self, key: str) -> str | None:
-        return await redis.get("githubkit:" + key)
+        return await redis.get(f"githubkit:{self.app}:{key}")
 
     def set(self, key: str, value: str, ex: datetime.timedelta) -> None:
         raise NotImplementedError()
 
     async def aset(self, key: str, value: str, ex: datetime.timedelta) -> None:
-        await redis.setex("githubkit:" + key, time=ex, value=value)
+        await redis.setex(f"githubkit:{self.app}:{key}", time=ex, value=value)

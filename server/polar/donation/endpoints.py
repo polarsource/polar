715832486055754a667965a2dc54215d0f33e559
Gustav Westling <gustav@polar.sh>
2024-03-21 16:58:11 +0100

    server/donations: update payment intent API

--- server/polar/donation/endpoints.py
+++ server/polar/donation/endpoints.py
@@ -9,6 +9,7 @@ from polar.currency.schemas import CurrencyAmount
 from polar.donation.schemas import (
     DonationCreateStripePaymentIntent,
     DonationStripePaymentIntentMutationResponse,
+    DonationUpdateStripePaymentIntent,
 )
 from polar.enums import Platforms
 from polar.exceptions import BadRequest, ResourceNotFound, Unauthorized
@@ -78,5 +79,52 @@ async def create_payment_intent(
         amount=intent.amount,
         fee=CurrencyAmount(currency="USD", amount=0),
         amount_including_fee=intent.amount,
+        client_secret=pi.client_secret,
+    )
+
+
+@router.patch(
+    "/donations/payment_intent/{id}",
+    response_model=DonationStripePaymentIntentMutationResponse,
+)
+async def update_payment_intent(
+    id: str,
+    updates: DonationUpdateStripePaymentIntent,
+    session: AsyncSession = Depends(get_db_session),
+    auth: Auth = Depends(Auth.optional_user),
+) -> DonationStripePaymentIntentMutationResponse:
+    # If on behalf of org, check that user is member of this org.
+    on_behalf_of_organization: Organization | None = None
+    if updates.on_behalf_of_organization_id:
+        if not auth.user:
+            raise Unauthorized()
+        member = await user_organization_service.get_by_user_and_org(
+            session, auth.user.id, updates.on_behalf_of_organization_id
+        )
+        if not member:
+            raise Unauthorized()
+
+        # get org
+        on_behalf_of_organization = await organization_service.get(
+            session, updates.on_behalf_of_organization_id
+        )
+        if not on_behalf_of_organization:
+            raise ResourceNotFound()
+
+    pi = await donation_service.update_payment_intent(
+        session=session,
+        payment_intent_id=id,
+        user=auth.user,
+        amount=updates.amount,
+        receipt_email=updates.email,
+        on_behalf_of_organization=on_behalf_of_organization,
+        setup_future_usage=updates.setup_future_usage,
+    )
+
+    return DonationStripePaymentIntentMutationResponse(
+        payment_intent_id=pi.id,
+        amount=updates.amount,
+        fee=CurrencyAmount(currency="USD", amount=0),
+        amount_including_fee=updates.amount,
         client_secret=pi.client_secret,
     )

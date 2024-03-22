66c2f6999cd0d117595022221c3da081f541d421
Emil Widlund <hello@emilwidlund.com>
2024-03-22 11:36:21 +0100

    clients/repo: implement sidebar to repo pages

--- clients/apps/web/src/app/[organization]/[repo]/ClientPage.tsx
+++ clients/apps/web/src/app/[organization]/[repo]/ClientPage.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import revalidate from '@/app/actions'
+import { FreeTierSubscribe } from '@/components/Organization/FreeTierSubscribe'
 import IssuesLookingForFunding from '@/components/Organization/IssuesLookingForFunding'
 import { CoverEditor } from '@/components/Profile/CoverEditor/CoverEditor'
 import { CreatorsEditor } from '@/components/Profile/CreatorEditor/CreatorsEditor'
@@ -21,6 +22,7 @@ import {
   Repository,
   RepositoryProfileSettingsUpdate,
   SubscriptionTier,
+  SubscriptionTierType,
 } from '@polar-sh/sdk'
 import Link from 'next/link'
 import { OgObject } from 'open-graph-scraper-lite/dist/lib/types'
@@ -100,10 +102,14 @@ const ClientPage = ({
     updateProfile({ links: links.map((l) => l.url) })
   }
 
+  const freeSubscriptionTier = subscriptionTiers.find(
+    (tier) => tier.type === SubscriptionTierType.FREE,
+  )
+
   return (
     <div className="flex w-full flex-col gap-y-12">
       <div className="flex w-full flex-col gap-16">
-        <div className="flex flex-col gap-16 md:flex-row md:gap-24">
+        <div className="flex flex-col gap-16 md:flex-row">
           <div className="flex w-full min-w-0 flex-shrink flex-col gap-y-16">
             {repository.description && (
               <DescriptionEditor
@@ -151,78 +157,118 @@ const ClientPage = ({
             )}
           </div>
 
-          <div className="flex w-full flex-col gap-16 md:max-w-52 lg:max-w-72">
-            <div className="grid grid-cols-2 flex-col gap-12 md:flex md:gap-4">
-              <div className="flex flex-col gap-y-1">
-                <span className="dark:text-polar-400 text-gray-600">
-                  Creator
-                </span>
-                <Link href={organizationPageLink(organization)}>
-                  {repository.organization.pretty_name}
-                </Link>
-              </div>
-              <div className="flex flex-col gap-y-1">
-                <span className="dark:text-polar-400 text-gray-600">Stars</span>
-                <span>{formatStarsNumber(repository.stars ?? 0)}</span>
-              </div>
-              <div className="flex flex-col gap-y-1">
-                <span className="dark:text-polar-400 text-gray-600">
-                  License
-                </span>
-                <span>{repository.license ?? 'Unlicensed'}</span>
-              </div>
-              <div className="flex flex-col gap-y-1">
-                <span className="dark:text-polar-400 text-gray-600">
-                  Repository
-                </span>
-                <Link
-                  className="flex flex-row items-center gap-x-2"
-                  href={`https://github.com/${repository.organization.name}/${repository.name}`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {'GitHub'}
-                  <ArrowUpRightIcon className="h-5 w-5" />
-                </Link>
-              </div>
-              {repository.homepage && (
-                <div className="flex flex-col gap-y-1">
-                  <span className="dark:text-polar-400 text-gray-600">
-                    Website
+          <div className="flex w-full flex-col gap-12 md:max-w-52 lg:max-w-72">
+            <div className="flex flex-col gap-6">
+              <ShadowBoxOnMd className="flex flex-col gap-6 md:p-6">
+                <div className="flex flex-col gap-4">
+                  <Avatar
+                    className="h-12 w-12"
+                    avatar_url={organization.avatar_url}
+                    name={organization.name}
+                  />
+                  <span className="flex flex-row flex-wrap gap-2">
+                    <Link
+                      className="dark:text-polar-500 text-wrap text-gray-500 transition-colors hover:text-blue-500 dark:hover:text-blue-400"
+                      href={organizationPageLink(organization)}
+                    >
+                      {repository.organization.name}
+                    </Link>
+                    <span className="dark:text-polar-600 text-gray-400">/</span>
+                    <Link
+                      className="text-wrap transition-colors hover:text-blue-500 dark:hover:text-blue-400"
+                      href={organizationPageLink(organization, repository.name)}
+                    >
+                      {repository.name}
+                    </Link>
                   </span>
-                  <Link
-                    className="flex flex-row items-center gap-x-2"
-                    href={repository.homepage}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    {new URL(repository.homepage).hostname}
-                    <ArrowUpRightIcon className="h-5 w-5" />
-                  </Link>
                 </div>
-              )}
+                <div className="flex flex-col gap-1.5 text-sm">
+                  <div className="flex flex-row justify-between gap-x-4">
+                    <span className="dark:text-polar-400 text-gray-600">
+                      Creator
+                    </span>
+                    <Link
+                      className="truncate text-right"
+                      href={organizationPageLink(organization)}
+                    >
+                      {repository.organization.pretty_name}
+                    </Link>
+                  </div>
+                  <div className="flex flex-row justify-between gap-x-4">
+                    <span className="dark:text-polar-400 text-gray-600">
+                      Stars
+                    </span>
+                    <span className="truncate text-right">
+                      {formatStarsNumber(repository.stars ?? 0)}
+                    </span>
+                  </div>
+                  <div className="flex flex-row justify-between gap-x-4">
+                    <span className="dark:text-polar-400 text-gray-600">
+                      License
+                    </span>
+                    <span className="truncate text-right">
+                      {repository.license ?? 'Unlicensed'}
+                    </span>
+                  </div>
+                  <div className="flex flex-row justify-between gap-x-4">
+                    <span className="dark:text-polar-400 text-gray-600">
+                      Repository
+                    </span>
+                    <Link
+                      className="flex flex-row items-center gap-x-2 truncate text-right"
+                      href={`https://github.com/${repository.organization.name}/${repository.name}`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      {'GitHub'}
+                      <ArrowUpRightIcon className="h-4 w-4" />
+                    </Link>
+                  </div>
+                  {repository.homepage && (
+                    <div className="flex flex-row justify-between gap-x-4">
+                      <span className="dark:text-polar-400 text-gray-600">
+                        Website
+                      </span>
+                      <Link
+                        className="flex flex-row items-center gap-x-2 truncate text-right"
+                        href={repository.homepage}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {new URL(repository.homepage).hostname}
+                        <ArrowUpRightIcon className="h-4 w-4" />
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </ShadowBoxOnMd>
+
+              {freeSubscriptionTier && !isAdmin ? (
+                <>
+                  <FreeTierSubscribe
+                    subscriptionTier={freeSubscriptionTier}
+                    organization={organization}
+                    upsellSubscriptions
+                  />
+                </>
+              ) : null}
             </div>
 
-            <div className="flex flex-col gap-8">
-              <div className="flex flex-row items-center gap-4">
-                <h3>Posts from {organization.name}</h3>
-              </div>
-              <div className="flex w-full flex-col gap-4">
-                {posts.map((post) => (
-                  <Link
-                    key={post.id}
-                    href={organizationPageLink(
-                      organization,
-                      `posts/${post.slug}`,
-                    )}
-                    className="flex w-full flex-row items-start gap-4 transition-opacity hover:opacity-70"
-                  >
-                    <Avatar
-                      className="h-8 w-8"
-                      avatar_url={post.byline.avatar_url}
-                      name={post.byline.name}
-                    />
-                    <div className="flex w-full flex-col gap-y-1">
+            {posts.length > 0 && (
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-row items-center gap-4">
+                  <h3>Posts from the creator</h3>
+                </div>
+                <ShadowBoxOnMd className="flex w-full flex-col gap-6 md:p-6">
+                  {posts.map((post) => (
+                    <Link
+                      key={post.id}
+                      href={organizationPageLink(
+                        organization,
+                        `posts/${post.slug}`,
+                      )}
+                      className="flex w-full flex-col gap-1 transition-opacity hover:opacity-70"
+                    >
                       <h3 className="line-clamp-2">{post.title}</h3>
                       <span className="dark:text-polar-500 text-sm text-gray-500">
                         {new Date(post.published_at ?? 0).toLocaleDateString(
@@ -234,18 +280,18 @@ const ClientPage = ({
                           },
                         )}
                       </span>
-                    </div>
+                    </Link>
+                  ))}
+                  <Link
+                    className="flex flex-row items-center gap-2 text-sm text-blue-500 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
+                    href={organizationPageLink(organization, 'posts')}
+                  >
+                    <span>View all</span>
+                    <ArrowForward fontSize="inherit" />
                   </Link>
-                ))}
+                </ShadowBoxOnMd>
               </div>
-              <Link
-                className="flex flex-row items-center gap-2 text-sm text-blue-500 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
-                href={organizationPageLink(organization, 'posts')}
-              >
-                <span>View all</span>
-                <ArrowForward fontSize="inherit" />
-              </Link>
-            </div>
+            )}
 
             <LinksEditor
               organization={organization}

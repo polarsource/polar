d9f0d74270a33e099f14eea78a5af837f0aa3f76
Gustav Westling <gustav@polar.sh>
2024-03-26 13:58:06 +0100

    server/donations: feature flag for donations

--- clients/apps/web/src/app/[organization]/(sidebar)/donate/ClientPage.tsx
+++ clients/apps/web/src/app/[organization]/(sidebar)/donate/ClientPage.tsx
@@ -1,11 +1,21 @@
 'use client'
 
+import { isFeatureEnabled } from '@/utils/feature-flags'
 import { useTrafficRecordPageView } from '@/utils/traffic'
 import { Organization } from '@polar-sh/sdk'
 import Checkout from './Checkout'
 
 const ClientPage = ({ organization }: { organization: Organization }) => {
   useTrafficRecordPageView({ organization: organization })
+
+  if (!isFeatureEnabled('donations')) {
+    return (
+      <div className="w-full pt-8 text-center text-gray-500">
+        You've found an upcoming feature. Please come back later. 😎
+      </div>
+    )
+  }
+
   return (
     <div className="max-w-[500px]">
       <h2>Donate to {organization.pretty_name ?? organization.name}</h2>

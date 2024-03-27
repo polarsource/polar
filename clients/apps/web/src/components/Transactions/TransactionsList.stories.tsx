6e61e7eca99551273d46608e253351eedfacb57e
Gustav Westling <gustav@polar.sh>
2024-03-27 15:04:24 +0100

    clients/transactions: add story for transactions list

---
+++ clients/apps/web/src/components/Transactions/TransactionsList.stories.tsx
@@ -0,0 +1,171 @@
+import {
+  Platforms,
+  Transaction,
+  TransactionDonation,
+  TransactionIssue,
+  TransactionIssueReward,
+  TransactionOrganization,
+  TransactionPledge,
+  TransactionRepository,
+  TransactionSubscription,
+  TransactionSubscriptionPrice,
+  TransactionSubscriptionTier,
+} from '@polar-sh/sdk'
+import type { Meta, StoryObj } from '@storybook/react'
+import TransactionsList from './TransactionsList'
+
+const meta: Meta<typeof TransactionsList> = {
+  title: 'Organisms/TransactionsList',
+  component: TransactionsList,
+}
+
+export default meta
+
+type Story = StoryObj<typeof TransactionsList>
+
+const tx: Transaction = {
+  created_at: '2024-03-27',
+  id: '',
+  type: 'payment',
+  currency: 'USD',
+  amount: 12300,
+  account_currency: 'USD',
+  account_amount: 12300,
+  account_incurred_transactions: [],
+  incurred_amount: 300,
+  gross_amount: 12300,
+  net_amount: 12000,
+}
+
+const donation: TransactionDonation = {
+  created_at: '2024-03-27',
+  id: '',
+  to_organization: {
+    id: '',
+    name: 'hello',
+    is_personal: false,
+    avatar_url: '',
+    created_at: '2024-03-27',
+    platform: Platforms.GITHUB,
+  },
+  donor: null,
+  message: undefined,
+}
+
+const txDonation: Transaction = {
+  ...tx,
+  donation,
+}
+
+const txDonationWithMessage: Transaction = {
+  ...tx,
+  donation: {
+    ...donation,
+    message: "You're the best!",
+  },
+}
+
+const txDonationWithMessageFromDonor: Transaction = {
+  ...tx,
+  donation: {
+    ...donation,
+    message: "You're the best!",
+    donor: {
+      id: '',
+      avatar_url: 'https://avatars.githubusercontent.com/u/1144727?s=60&v=4',
+      public_name: 'frankie567',
+    },
+  },
+}
+
+const issue_reward: TransactionIssueReward = {
+  created_at: '',
+  id: '',
+  issue_id: '',
+  share_thousands: 800,
+}
+
+const organization: TransactionOrganization = {
+  created_at: '',
+  id: '',
+  platform: 'github',
+  name: 'OrgName',
+  avatar_url: 'https://avatars.githubusercontent.com/u/1144727?s=60&v=4',
+  is_personal: false,
+}
+
+const repository: TransactionRepository = {
+  created_at: '',
+  id: '',
+  platform: 'github',
+  organization_id: '',
+  name: 'reponame',
+}
+
+const issue: TransactionIssue = {
+  created_at: '',
+  id: '',
+  platform: 'github',
+  organization_id: '',
+  repository_id: '',
+  number: 0,
+  title: '',
+  organization,
+  repository,
+}
+
+const pledge: TransactionPledge = {
+  created_at: '',
+  id: '',
+  state: 'initiated',
+  issue,
+}
+
+const txIssueReward: Transaction = {
+  ...tx,
+  issue_reward,
+  pledge,
+}
+
+const subscription_tier: TransactionSubscriptionTier = {
+  created_at: '',
+  id: '',
+  type: 'free',
+  name: 'TierName',
+  organization,
+}
+
+const subscription: TransactionSubscription = {
+  created_at: '',
+  id: '',
+  status: 'incomplete',
+  subscription_tier,
+}
+
+const subscription_tier_price: TransactionSubscriptionPrice = {
+  created_at: '',
+  id: '',
+  recurring_interval: 'month',
+  price_amount: 4000,
+  price_currency: 'USD',
+  is_archived: false,
+}
+
+const txSubscription: Transaction = {
+  ...tx,
+  subscription,
+  subscription_tier_price,
+}
+
+export const Default: Story = {
+  args: {
+    transactions: [
+      tx,
+      txDonation,
+      txDonationWithMessage,
+      txDonationWithMessageFromDonor,
+      txIssueReward,
+      txSubscription,
+    ],
+  },
+}

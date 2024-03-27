47721fae4c3e0baa9a48a55facfe1c2d78d16315
François Voron <fvoron@gmail.com>
2024-03-27 11:48:31 +0100

    server/oauth2: add ./well-known/openid-configuration

--- server/polar/oauth2/constants.py
+++ server/polar/oauth2/constants.py
@@ -2,12 +2,9 @@ AUTHORIZATION_CODE_PREFIX = "polar_ac_"
 ACCESS_TOKEN_PREFIX = "polar_at_"
 REFRESH_TOKEN_PREFIX = "polar_rt_"
 
+ISSUER = "https://polar.sh"
 SCOPES_SUPPORTED = ["openid", "profile", "email"]
-
-
-__all__ = [
-    "AUTHORIZATION_CODE_PREFIX",
-    "ACCESS_TOKEN_PREFIX",
-    "REFRESH_TOKEN_PREFIX",
-    "SCOPES_SUPPORTED",
-]
+SERVICE_DOCUMENTATION = "https://docs.polar.sh"
+SUBJECT_TYPES_SUPPORTED = ["public"]
+ID_TOKEN_SIGNING_ALG_VALUES_SUPPORTED = ["RS256"]
+CLAIMS_SUPPORTED = ["sub", "name", "email", "email_verified"]

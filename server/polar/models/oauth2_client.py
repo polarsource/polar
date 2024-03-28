4ba66926d3a8ea8289639531baef7f136bb9a6d5
François Voron <fvoron@gmail.com>
2024-03-28 14:51:58 +0100

    server/oauth2: implement OAuth2Grant model

--- server/polar/models/oauth2_client.py
+++ server/polar/models/oauth2_client.py
@@ -1,7 +1,9 @@
 from authlib.integrations.sqla_oauth2 import OAuth2ClientMixin
+from sqlalchemy import UniqueConstraint
 
 from polar.kit.db.models import RecordModel
 
 
 class OAuth2Client(RecordModel, OAuth2ClientMixin):
     __tablename__ = "oauth2_clients"
+    __table_args__ = (UniqueConstraint("client_id"),)

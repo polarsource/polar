3881310be309956b06933300e3dda7f1176e1199
Gustav Westling <gustav@polar.sh>
2024-04-02 09:41:41 +0200

    clients: remove chrome-extension

--- clients/apps/chrome-extension/src/config.ts
+++
@@ -1,4 +0,0 @@
-export const CONFIG = {
-  API_URL: process.env.API_URL || 'http://127.0.0.1:8000',
-  WEB_URL: process.env.WEB_URL || 'http://127.0.0.1:3000',
-}
--- clients/apps/chrome-extension/tailwind.config.js
+++
@@ -1,1 +0,0 @@
-../web/tailwind.config.js

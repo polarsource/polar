4a6baf609422be2016238328a251bcd6a652ccdc
Gustav Westling <gustav@polar.sh>
2024-04-03 10:04:32 +0200

    clients/finance: remove unused navigation logic

--- clients/apps/web/src/app/maintainer/[organization]/(topbar)/finance/layout.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/(topbar)/finance/layout.tsx
@@ -1,39 +1,5 @@
-'use client'
-
-import { useDashboardRoutes } from '@/components/Dashboard/navigation'
 import { DashboardBody } from '@/components/Layout/DashboardLayout'
-import { SubNav } from '@/components/Navigation/DashboardTopbar'
-import {
-  useCurrentOrgAndRepoFromURL,
-  useIsOrganizationAdmin,
-  usePersonalOrganization,
-} from '@/hooks'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { org: currentOrgFromURL } = useCurrentOrgAndRepoFromURL()
-  const personalOrg = usePersonalOrganization()
-  const isOrgAdmin = useIsOrganizationAdmin(currentOrgFromURL)
-  const isPersonal = currentOrgFromURL?.name === personalOrg?.name
-
-  const routes = useDashboardRoutes(
-    currentOrgFromURL,
-    currentOrgFromURL ? isPersonal : true,
-    isOrgAdmin ?? false,
-  )
-
-  const currentRoute = routes.find((r) => r.isActive)
-
-  return (
-    <>
-      <DashboardBody>
-        {isPersonal &&
-          currentRoute &&
-          'subs' in currentRoute &&
-          (currentRoute.subs?.length ?? 0) > 0 && (
-            <SubNav items={currentRoute.subs ?? []} />
-          )}
-        {children}
-      </DashboardBody>
-    </>
-  )
+  return <DashboardBody>{children}</DashboardBody>
 }

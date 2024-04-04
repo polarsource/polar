4d344d158eb9c896e1e6636513646722a7bd6834
Emil Widlund <hello@emilwidlund.com>
2024-04-04 09:28:41 +0200

    clients/public-pages: adjust max-width for public pages

--- clients/apps/web/src/app/[organization]/(sidebar)/layout.tsx
+++ clients/apps/web/src/app/[organization]/(sidebar)/layout.tsx
@@ -102,7 +102,7 @@ export default async function Layout({
   return (
     <PublicPageOrganizationContextProvider organization={organization}>
       <div className="flex flex-col">
-        <div className="mx-auto flex w-full max-w-[1580px] flex-col items-start px-4 md:h-full md:flex-row md:gap-8 md:space-y-0 xl:gap-24">
+        <div className="mx-auto flex w-full max-w-[1440px] flex-col items-start px-4 md:h-full md:flex-row md:gap-8 md:space-y-0 xl:gap-24">
           <div className="dark:bg-polar-950 sticky top-0 z-20 flex w-full flex-row items-center justify-between bg-white py-4 md:relative md:hidden">
             <a href="/">
               <LogoIcon

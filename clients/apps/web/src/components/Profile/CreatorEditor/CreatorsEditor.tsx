a3fc671c30ed4255213b81f726947acf77868e52
Emil Widlund <hello@emilwidlund.com>
2024-04-08 17:40:58 +0200

    clients/creators: fix overflow bug on creator grid

--- clients/apps/web/src/components/Profile/CreatorEditor/CreatorsEditor.tsx
+++ clients/apps/web/src/components/Profile/CreatorEditor/CreatorsEditor.tsx
@@ -91,7 +91,7 @@ export const CreatorsEditor = ({
               </div>
             )}
           </div>
-          <div className="-mx-4 flex flex-row justify-start gap-6 overflow-x-auto px-4 pb-4 md:mx-0 md:grid md:flex-col md:gap-6 md:p-0 xl:grid-cols-2">
+          <div className="-mx-4 flex flex-row justify-start gap-6 overflow-x-auto px-4 pb-4 md:mx-0 md:grid md:flex-col md:gap-6 md:overflow-x-visible md:p-0 xl:grid-cols-2">
             {featuredCreators.map((creator) => (
               <DraggableCreatorCard
                 className="w-full max-w-[80%] shrink-0 md:max-w-full"

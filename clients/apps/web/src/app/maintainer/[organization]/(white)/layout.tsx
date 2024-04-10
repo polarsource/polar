75e24002828a125ab50df36a63038deeb6dc8401
Emil Widlund <hello@emilwidlund.com>
2024-04-10 10:37:42 +0200

    clients/posts: adjust dark mode post editor bg

--- clients/apps/web/src/app/maintainer/[organization]/(white)/layout.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/(white)/layout.tsx
@@ -2,7 +2,7 @@ import DashboardLayout from '@/components/Layout/DashboardLayout'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <DashboardLayout className="dark:bg-polar-950 bg-white">
+    <DashboardLayout className="dark:bg-polar-900 bg-white">
       <>{children}</>
     </DashboardLayout>
   )

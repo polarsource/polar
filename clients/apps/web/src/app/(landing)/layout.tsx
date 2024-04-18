c2542bf17faa55d7c98537ab75fad8b87c5a61fd
Emil Widlund <hello@emilwidlund.com>
2024-04-18 21:58:24 +0200

    clients/layout: reset scroll on public pages

--- clients/apps/web/src/app/(landing)/layout.tsx
+++ clients/apps/web/src/app/(landing)/layout.tsx
@@ -1,6 +1,10 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect } from 'react'
 import ClientLayout from './ClientLayout'
 
 export default function Layout({ children }: PropsWithChildren) {
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [])
+
   return <ClientLayout>{children}</ClientLayout>
 }

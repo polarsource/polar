9b7275ab35902b3ad1093cdb4d7413fe54904d3d
Emil Widlund <hello@emilwidlund.com>
2024-04-18 23:13:35 +0200

    clients/landing: add useEffect in client layout

--- clients/apps/web/src/app/(landing)/layout.tsx
+++ clients/apps/web/src/app/(landing)/layout.tsx
@@ -1,10 +1,6 @@
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren } from 'react'
 import ClientLayout from './ClientLayout'
 
 export default function Layout({ children }: PropsWithChildren) {
-  useEffect(() => {
-    window.scroll(0, 0)
-  }, [])
-
   return <ClientLayout>{children}</ClientLayout>
 }

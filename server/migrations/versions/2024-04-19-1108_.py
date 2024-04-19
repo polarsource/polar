de56778d06085bf00c14c1a872b7ca242b260e77
Gustav Westling <gustav@polar.sh>
2024-04-19 11:08:23 +0200

    server/webhooks: merge migrations

--- server/migrations/versions/2024-04-19-1108_.py
+++ server/migrations/versions/2024-04-19-1108_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 1121c6536d71
-Revises: 59b002a11fe8
-Create Date: 2024-04-19 10:56:03.540853
+Revision ID: 65aaa49b6c56
+Revises: 9c60487b49ab, 59b002a11fe8
+Create Date: 2024-04-19 11:08:04.077924
 
 """
 
@@ -13,8 +13,8 @@ from alembic import op
 from polar.kit.extensions.sqlalchemy import PostgresUUID
 
 # revision identifiers, used by Alembic.
-revision = "1121c6536d71"
-down_revision = "59b002a11fe8"
+revision = "65aaa49b6c56"
+down_revision = ("9c60487b49ab", "59b002a11fe8")
 branch_labels: tuple[str] | None = None
 depends_on: tuple[str] | None = None
 

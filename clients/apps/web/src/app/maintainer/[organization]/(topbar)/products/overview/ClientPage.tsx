e53736dadbbbc72cec0dc7388318cbd7dba45e0a
Emil Widlund <hello@emilwidlund.com>
2024-04-22 18:06:09 +0200

    clients/products: init nav

---
+++ clients/apps/web/src/app/maintainer/[organization]/(topbar)/products/overview/ClientPage.tsx
@@ -0,0 +1,9 @@
+import { DashboardBody } from '@/components/Layout/DashboardLayout'
+
+export default function ClientPage() {
+  return (
+    <DashboardBody>
+      <h1 className="text-lg">Overview</h1>
+    </DashboardBody>
+  )
+}

13529c18202f68229348c15a79574a69d3dc2092
Emil Widlund <hello@emilwidlund.com>
2024-04-23 17:30:23 +0200

    clients/products: darkmode adjustments

--- clients/apps/web/src/app/maintainer/[organization]/(topbar)/products/overview/ClientPage.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/(topbar)/products/overview/ClientPage.tsx
@@ -93,7 +93,7 @@ const ProductTile = ({ product }: ProductTileProps) => {
 
   return (
     <Link
-      className="flex w-full flex-col gap-8 rounded-3xl bg-white p-6 shadow-sm transition-colors hover:bg-gray-50"
+      className="dark:bg-polar-800 dark:border-polar-700 dark:hover:bg-polar-700 flex w-full flex-col gap-8 rounded-3xl border border-transparent bg-white p-6 shadow-sm transition-colors hover:bg-gray-50"
       href={`/maintainer/${org?.name}/products/overview?product=${product.id}`}
     >
       {product.media ? (
@@ -105,8 +105,8 @@ const ProductTile = ({ product }: ProductTileProps) => {
           src={product.media ?? ''}
         />
       ) : (
-        <div className="flex aspect-square w-full flex-col items-center justify-center rounded-2xl bg-gray-100">
-          <PanoramaOutlined className="text-gray-500" />
+        <div className="dark:bg-polar-900 flex aspect-square w-full flex-col items-center justify-center rounded-2xl bg-gray-100">
+          <PanoramaOutlined className="dark:text-polar-500 text-gray-500" />
         </div>
       )}
       <div className="flex flex-grow flex-col gap-2">
@@ -118,7 +118,7 @@ const ProductTile = ({ product }: ProductTileProps) => {
         </p>
       </div>
       <div className="flex flex-row items-center justify-between">
-        <h3 className="font-medium leading-snug text-blue-500">
+        <h3 className="font-medium leading-snug text-blue-500 dark:text-blue-400">
           ${getCentsInDollarString(product.price)}
         </h3>
         <Pill className="px-2.5 py-1" color="blue">

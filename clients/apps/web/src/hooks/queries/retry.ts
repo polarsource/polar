992a0ea91aa63ebb57352d58d890f1c09c051da3
Gustav Westling <gustav@polar.sh>
2024-04-24 11:43:12 +0200

    clients/queries: null checks in defaultRetry

--- clients/apps/web/src/hooks/queries/retry.ts
+++ clients/apps/web/src/hooks/queries/retry.ts
@@ -3,9 +3,9 @@ import { ResponseError } from '@polar-sh/sdk'
 
 export const authenticatingRetry = (
   failureCount: number,
-  error: ResponseError,
+  error: ResponseError, // TODO: This type is not correct
 ): boolean => {
-  if (error.response.status === 401) {
+  if (error?.response?.status === 401) {
     // Empty Zustand store on unauthenticated errors
     const state = useStore.getState()
     if (state.resetState) {
@@ -14,12 +14,14 @@ export const authenticatingRetry = (
     return false
   }
 
-  if (error.response.status === 404) {
+  if (error?.response?.status === 404) {
     return false
   }
+
   if (failureCount > 2) {
     return false
   }
+
   return true
 }
 
@@ -27,13 +29,13 @@ export const defaultRetry = authenticatingRetry
 
 export const serverErrorRetry = (
   failureCount: number,
-  error: ResponseError,
+  error: ResponseError, // TODO: This type is not correct
 ): boolean => {
-  if (error.response.status === 401) {
+  if (error?.response?.status === 401) {
     return false
   }
 
-  if (error.response.status === 404) {
+  if (error?.response?.status === 404) {
     return false
   }
 

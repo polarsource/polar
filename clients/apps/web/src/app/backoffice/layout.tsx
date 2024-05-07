6ecda3158a0caa4bd0b9b515fc0120b34fe4c73a
Emil Widlund <hello@emilwidlund.com>
2024-05-07 09:40:58 +0200

    clients/backoffice: remove hideProfile prop

--- clients/apps/web/src/app/backoffice/layout.tsx
+++ clients/apps/web/src/app/backoffice/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <DashboardLayout>
-        <DashboardTopbar useOrgFromURL={false} hideProfile={false} />
+        <DashboardTopbar useOrgFromURL={false} />
         <DashboardBody>{children}</DashboardBody>
       </DashboardLayout>
     </>

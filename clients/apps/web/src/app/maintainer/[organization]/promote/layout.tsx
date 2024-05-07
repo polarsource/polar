23b641d3b149b3e407e93ff4f69bbf015f6f6f32
Emil Widlund <hello@emilwidlund.com>
2024-05-07 09:29:23 +0200

    clients/dashboard: adjust topbar

---
+++ clients/apps/web/src/app/maintainer/[organization]/promote/layout.tsx
@@ -0,0 +1,5 @@
+import DashboardLayout from '@/components/Layout/DashboardLayout'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <DashboardLayout>{children}</DashboardLayout>
+}

42d7920a32ef3c7be08cbf9594f8b6ae702df6ba
Emil Widlund <hello@emilwidlund.com>
2024-05-15 15:55:51 +0200

    clients/docs: improve api docs

--- clients/apps/web/src/app/(landing)/(mdx)/layout.tsx
+++ clients/apps/web/src/app/(landing)/(mdx)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="px-6 pb-16 md:px-12">
       <AnimatedSeparator />
-      <div className="prose dark:prose-invert prose-headings:mt-16 prose-headings:font-semibold prose-headings:text-black prose-h1:text-blue-500 prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-md dark:prose-headings:text-white dark:text-polar-200 max-w-4xl text-gray-800">
+      <div className="prose dark:prose-invert dark:prose-headings:leading-normal prose-headings:leading-normal prose-headings:mt-16 prose-headings:font-semibold prose-headings:text-black prose-h1:text-blue-500 prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-md dark:prose-headings:text-white dark:text-polar-200 max-w-4xl text-gray-800">
         {children}
       </div>
     </div>

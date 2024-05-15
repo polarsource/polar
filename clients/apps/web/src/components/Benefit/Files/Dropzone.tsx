37074bcb0bcebd637f642bc9d75c196046e9f611
Birk Jernström <birkjernstrom@gmail.com>
2024-05-15 14:17:56 +0200

    server: Store SHA-256 base64 & hex for uploaded files

--- clients/apps/web/src/components/Benefit/Files/Dropzone.tsx
+++ clients/apps/web/src/components/Benefit/Files/Dropzone.tsx
@@ -1,9 +1,31 @@
 'use client'
 
+import { api } from '@/utils/api'
 import { FileRead, Organization } from '@polar-sh/sdk'
 
-import { api } from '@/utils/api'
-import { useRef } from 'react'
+// Credit: https://codepen.io/dulldrums/pen/RqVrRr
+const hex = (buffer: ArrayBuffer) => {
+  var hexCodes = []
+  var view = new DataView(buffer)
+  for (var i = 0; i < view.byteLength; i += 4) {
+    // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+    var value = view.getUint32(i)
+    // toString(16) will give the hex representation of the number without padding
+    var stringValue = value.toString(16)
+    // We use concatenation and slice for padding
+    var padding = '00000000'
+    var paddedValue = (padding + stringValue).slice(-padding.length)
+    hexCodes.push(paddedValue)
+  }
+
+  // Join all the hex strings into one
+  return hexCodes.join('')
+}
+
+const getSha256Hash = async (file: ArrayBuffer) => {
+  const hash = await crypto.subtle.digest('SHA-256', file)
+  return hash
+}
 
 const Dropzone = ({
   organization,
@@ -12,36 +34,28 @@ const Dropzone = ({
   organization: Organization
   onUploaded: (file: FileRead) => void
 }) => {
-  const inputFileRef = useRef<HTMLInputElement>(null)
+  const handleUpload = async (file: File, buffer: ArrayBuffer) => {
+    const sha256hash = await getSha256Hash(buffer)
+    const sha256hex = hex(sha256hash)
+    const base64hash = btoa(String.fromCharCode(...new Uint8Array(sha256hash)))
 
-  const handleUpload = async (image: string) => {
-    if (!inputFileRef.current?.files) {
-      throw new Error('No file selected')
-    }
-
-    if (image === undefined) {
-      throw new Error('No image')
-    }
-
-    const file = inputFileRef.current.files[0]
     const params = {
       organization_id: organization.id,
       name: file.name,
       size: file.size,
       mime_type: file.type,
+      sha256: {
+        base64: base64hash,
+        hex: sha256hex,
+      },
       version: null,
     }
     const response = await api.files.createFile({
       fileCreate: params,
     })
 
-    const binary = atob(image.split(',')[1])
-    const array = []
-    for (var i = 0; i < binary.length; i++) {
-      array.push(binary.charCodeAt(i))
-    }
+    let blob = new Blob([buffer], { type: file.type })
 
-    let blob = new Blob([new Uint8Array(array)], { type: file.type })
     const result = await fetch(response.url, {
       method: 'PUT',
       headers: response.headers,
@@ -63,24 +77,20 @@ const Dropzone = ({
     <>
       <input
         name="file"
-        ref={inputFileRef}
         type="file"
         required
+        multiple={true}
         tabIndex={-1}
         onChange={async (e) => {
-          if (e.target.files && e.target.files[0]) {
+          const files = e.target.files
+          Array.from(files).forEach((file) => {
             const reader = new FileReader()
-            reader.onload = async (readerLoad) => {
-              if (
-                readerLoad.target &&
-                typeof readerLoad.target.result === 'string'
-              ) {
-                console.log('imageSet', readerLoad.target.result)
-                await handleUpload(readerLoad.target.result)
-              }
+            reader.onload = async () => {
+              const result = reader.result
+              await handleUpload(file, result)
             }
-            reader.readAsDataURL(e.target.files[0])
-          }
+            reader.readAsArrayBuffer(file)
+          })
         }}
       />
     </>
--- server/polar/file/schemas.py
+++ server/polar/file/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Self
+from typing import Any, Self
 
 from pydantic import UUID4
 
@@ -12,11 +12,17 @@ def get_disposition(file_name: str):
     return f'attachment; filename="{file_name}"'
 
 
+class SHA256Checksums(Schema):
+    base64: str | None
+    hex: str | None
+
+
 class FileCreate(Schema):
     organization_id: UUID4
     name: str
     size: int
     mime_type: str
+    sha256: SHA256Checksums | None
     version: str | None = None
 
 
@@ -29,12 +35,38 @@ class FileRead(Schema):
     version: str | None = None
     mime_type: str
     size: int
+    sha256: SHA256Checksums | None
 
     status: str
 
     uploaded_at: datetime | None = None
     created_at: datetime
     modified_at: datetime | None = None
+
+    @classmethod
+    def prepare_dict_from_db(cls, record: File) -> dict[str, Any]:
+        return dict(
+            id=record.id,
+            organization_id=record.organization_id,
+            name=record.name,
+            extension=record.extension,
+            version=record.version,
+            mime_type=record.mime_type,
+            size=record.size,
+            status=record.status,
+            sha256=SHA256Checksums(
+                base64=record.sha256_base64,
+                hex=record.sha256_hex,
+            ),
+            uploaded_at=record.uploaded_at,
+            created_at=record.created_at,
+            modified_at=record.modified_at,
+        )
+
+    @classmethod
+    def from_db(cls, record: File) -> Self:
+        params = cls.prepare_dict_from_db(record)
+        return cls(**params)
 
 
 class FilePresignedRead(FileRead):
@@ -45,25 +77,20 @@ class FilePresignedRead(FileRead):
 
     @classmethod
     def from_presign(cls, record: File, url: str, expires_at: datetime) -> Self:
-        return cls(
-            id=record.id,
-            organization_id=record.organization_id,
-            name=record.name,
-            extension=record.extension,
-            version=record.version,
-            mime_type=record.mime_type,
-            size=record.size,
-            status=record.status,
-            url=url,
-            url_expires_at=expires_at,
-            uploaded_at=record.uploaded_at,
-            created_at=record.created_at,
-            modified_at=record.modified_at,
-            headers={
-                "Content-Disposition": get_disposition(record.name),
-                "Content-Type": record.mime_type,
-            },
+        params = cls.prepare_dict_from_db(record)
+        params.update(
+            dict(
+                url=url,
+                url_expires_at=expires_at,
+                headers={
+                    "Content-Disposition": get_disposition(record.name),
+                    "Content-Type": record.mime_type,
+                    "x-amz-checksum-sha256": record.sha256_base64,
+                    "x-amz-sdk-checksum-algorithm": "SHA256",
+                },
+            )
         )
+        return cls(**params)
 
 
 class FileUpdate(Schema):

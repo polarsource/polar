e788c5b9d97acefab885e95aec06647634aea4b2
François Voron <fvoron@gmail.com>
2024-05-16 15:32:11 +0200

    server/sale: add a list endpoint

---
+++ server/polar/sale/auth.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from polar.auth.dependencies import Authenticator
+from polar.auth.models import AuthSubject, Organization, User
+from polar.auth.scope import Scope
+
+_SalesRead = Authenticator(
+    required_scopes={Scope.web_default, Scope.creator_sales_read},
+    allowed_subjects={User, Organization},
+)
+SalesRead = Annotated[AuthSubject[User | Organization], Depends(_SalesRead)]

7335d29dc59ae7a0311c974466f2da9eaec7166c
François Voron <fvoron@gmail.com>
2024-05-17 11:33:04 +0200

    server/sales: implement API to retrieve single sale and invoice

--- server/polar/sale/endpoints.py
+++ server/polar/sale/endpoints.py
@@ -1,17 +1,25 @@
-from fastapi import Depends, Query
+from typing import Annotated
+
+from fastapi import Depends, Path, Query
 from pydantic import UUID4
 
+from polar.exceptions import ResourceNotFound
 from polar.kit.pagination import ListResource, PaginationParamsQuery
 from polar.kit.routing import APIRouter
+from polar.models import Sale
 from polar.postgres import AsyncSession, get_db_session
 from polar.tags.api import Tags
 
 from . import auth
 from .schemas import Sale as SaleSchema
-from .schemas import SalesStatistics
+from .schemas import SaleInvoice, SalesStatistics
 from .service import sale as sale_service
 
 router = APIRouter(prefix="/sales", tags=["sales"])
+
+
+SaleID = Annotated[UUID4, Path(description="The sale ID.")]
+SaleNotFound = {"description": "Sale not found.", "model": ResourceNotFound.schema()}
 
 
 @router.get("/", response_model=ListResource[SaleSchema], tags=[Tags.PUBLIC])
@@ -55,3 +63,45 @@ async def get_sales_statistics(
         product_id=product_id,
     )
     return SalesStatistics(periods=periods)
+
+
+@router.get(
+    "/{id}",
+    response_model=SaleSchema,
+    tags=[Tags.PUBLIC],
+    responses={404: SaleNotFound},
+)
+async def get_sale(
+    id: SaleID,
+    auth_subject: auth.SalesRead,
+    session: AsyncSession = Depends(get_db_session),
+) -> Sale:
+    """Get a sale by ID."""
+    sale = await sale_service.get_by_id(session, auth_subject, id)
+
+    if sale is None:
+        raise ResourceNotFound()
+
+    return sale
+
+
+@router.get(
+    "/{id}/invoice",
+    response_model=SaleInvoice,
+    tags=[Tags.PUBLIC],
+    responses={404: SaleNotFound},
+)
+async def get_sale_invoice(
+    id: SaleID,
+    auth_subject: auth.SalesRead,
+    session: AsyncSession = Depends(get_db_session),
+) -> SaleInvoice:
+    """Get a sale's invoice data."""
+    sale = await sale_service.get_by_id(session, auth_subject, id)
+
+    if sale is None:
+        raise ResourceNotFound()
+
+    invoice_url = await sale_service.get_sale_invoice_url(sale)
+
+    return SaleInvoice(url=invoice_url)

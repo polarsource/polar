410d9f259fe3988f96fcdf4b3ae92ae6f3712cd0
Emil Widlund <hello@emilwidlund.com>
2024-05-20 15:21:28 +0200

    clients/docs: attempt to fix some type issues

--- clients/apps/web/src/app/docs/APINavigation.tsx
+++ clients/apps/web/src/app/docs/APINavigation.tsx
@@ -1,6 +1,22 @@
 import openapiSchema from '@polar-sh/sdk/openapi'
 import { OpenAPIV3_1 } from 'openapi-types'
 
+export type SchemaPaths = (typeof openapiSchema)['paths']
+export type SchemaPathKey = keyof SchemaPaths
+export type SchemaPathMethods<T extends SchemaPathKey> = SchemaPaths[T]
+export type SchemaPathMethod<T extends SchemaPathKey> =
+  keyof SchemaPathMethods<T>
+
+export type FindMatchingPath<
+  A extends string,
+  B extends SchemaPathKey = SchemaPathKey,
+  // @ts-ignore
+> = B extends `${infer X}${A}${infer Y}`
+  ? A extends string
+    ? SchemaPathMethods<B>[SchemaPathMethod<B>]
+    : never
+  : never
+
 export type APIMethod = 'get' | 'post' | 'put' | 'patch' | 'delete'
 
 export interface APIParameter {
@@ -31,58 +47,59 @@ const extractEntries = <T extends {}>(obj: T) => {
 }
 
 const buildSections = (): Section[] => {
-  const sections = extractEntries(
-    (openapiSchema as OpenAPIV3_1.Document).paths ?? {},
-  ).reduce<Section[]>((acc, [path, endpoints]) => {
-    if (!endpoints) return acc
+  const sections = extractEntries(openapiSchema.paths ?? {}).reduce<Section[]>(
+    (acc, [path, endpoints]) => {
+      const [ancestor] = path.replace('/api/v1/', '').split('/').filter(Boolean)
 
-    const [ancestor] = path.replace('/api/v1/', '').split('/').filter(Boolean)
-
-    switch (ancestor) {
-      case 'readyz':
-      case 'healthz':
-      case 'backoffice':
-      case '{platform}':
-        return acc
-    }
-
-    for (const [method, endpoint] of extractEntries(endpoints)) {
-      const matchingAncestor = acc.find(
-        (section) => section.name === ancestor.replaceAll('_', ' '),
-      )
-
-      if (
-        !endpoint ||
-        !(
-          typeof endpoint === 'object' &&
-          'summary' in endpoint &&
-          typeof endpoint.summary === 'string'
-        )
-      )
-        continue
-
-      if (!matchingAncestor) {
-        acc.push({
-          name: ancestor.replaceAll('_', ' '),
-          endpoints: [
-            {
-              name: endpoint.summary,
-              path: path,
-              method: method as APIMethod,
-            },
-          ],
-        })
-      } else {
-        matchingAncestor.endpoints.push({
-          name: endpoint.summary,
-          path: path,
-          method: method as APIMethod,
-        })
+      switch (ancestor) {
+        case 'readyz':
+        case 'healthz':
+        case 'backoffice':
+        case '{platform}':
+          return acc
       }
-    }
 
-    return acc
-  }, [])
+      for (const [method, endpoint] of extractEntries(
+        endpoints as OpenAPIV3_1.PathsObject,
+      )) {
+        const matchingAncestor = acc.find(
+          (section) => section.name === ancestor.replaceAll('_', ' '),
+        )
+
+        if (
+          !endpoint ||
+          !(
+            typeof endpoint === 'object' &&
+            'summary' in endpoint &&
+            typeof endpoint.summary === 'string'
+          )
+        )
+          continue
+
+        if (!matchingAncestor) {
+          acc.push({
+            name: ancestor.replaceAll('_', ' '),
+            endpoints: [
+              {
+                name: endpoint.summary,
+                path: path,
+                method: method as APIMethod,
+              },
+            ],
+          })
+        } else {
+          matchingAncestor.endpoints.push({
+            name: endpoint.summary,
+            path: path,
+            method: method as APIMethod,
+          })
+        }
+      }
+
+      return acc
+    },
+    [],
+  )
 
   return sections.sort((a, b) => a.name.localeCompare(b.name))
 }

234ca0096766e1cd4ce22e42d12167d936728edc
Emil Widlund <hello@emilwidlund.com>
2024-05-23 13:51:57 +0200

    clients/docs: tweak modal animation speed

--- clients/apps/web/src/components/Modal/index.tsx
+++ clients/apps/web/src/components/Modal/index.tsx
@@ -72,7 +72,7 @@ export const Modal: FunctionComponent<ModalProps> = ({
               )}
               initial={{ opacity: 0, scale: 0.99 }}
               animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.2, ease: 'easeInOut' }}
+              transition={{ duration: 0.1, ease: 'easeInOut' }}
               onClick={onInnerClick}
             >
               {modalContent}

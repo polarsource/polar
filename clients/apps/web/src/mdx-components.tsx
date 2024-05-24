7ed6bd7127964c4f651ecbce1f46cfc8e3f11da3
Emil Widlund <hello@emilwidlund.com>
2024-05-24 16:07:07 +0200

    clients/docs: prepare interaction observer logic

--- clients/apps/web/src/mdx-components.tsx
+++ clients/apps/web/src/mdx-components.tsx
@@ -1,7 +1,14 @@
 import type { MDXComponents } from 'mdx/types'
+import { HeadingObserver } from './components/Documentation/HeadingObserver'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    h1: (props) => <HeadingObserver type="h1" {...props} />,
+    h2: (props) => <HeadingObserver type="h2" {...props} />,
+    h3: (props) => <HeadingObserver type="h3" {...props} />,
+    h4: (props) => <HeadingObserver type="h4" {...props} />,
+    h5: (props) => <HeadingObserver type="h5" {...props} />,
+    h6: (props) => <HeadingObserver type="h6" {...props} />,
   }
 }

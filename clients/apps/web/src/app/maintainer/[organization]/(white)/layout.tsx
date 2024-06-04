b70b54cbddd5bac780589a40484a426e9c32ad05
Emil Widlund <hello@emilwidlund.com>
2024-06-04 17:26:02 +0200

    clients/posts: fix dark mode

--- clients/apps/web/src/app/maintainer/[organization]/(white)/layout.tsx
+++ clients/apps/web/src/app/maintainer/[organization]/(white)/layout.tsx
@@ -2,7 +2,7 @@ import DashboardLayout from '@/components/Layout/DashboardLayout'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <DashboardLayout className="dark:bg-polar-900 bg-white">
+    <DashboardLayout className="bg-white dark:bg-transparent">
       <>{children}</>
     </DashboardLayout>
   )

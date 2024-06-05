8284d4a94eeab4ec24a00968fd09001007a07054
François Voron <fvoron@gmail.com>
2024-06-05 16:52:59 +0200

    server/metrics: implement endpoint

---
+++ server/polar/metrics/auth.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from polar.auth.dependencies import Authenticator
+from polar.auth.models import AuthSubject, Organization, User
+from polar.auth.scope import Scope
+
+_MetricsRead = Authenticator(
+    required_scopes={Scope.web_default, Scope.creator_metrics_read},
+    allowed_subjects={User, Organization},
+)
+MetricsRead = Annotated[AuthSubject[User | Organization], Depends(_MetricsRead)]

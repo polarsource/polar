3376dbdbe84053148d8989b547d6c9077aba86c9
Birk Jernström <birkjernstrom@gmail.com>
2024-06-05 11:52:00 +0200

    github: Attempt to install mc & configure

--- server/.minio/configure.sh
+++ server/.minio/configure.sh
@@ -10,7 +10,7 @@ mc alias set polar http://$MINIO_HOST:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 # Setup user & acccess policy
 mc admin user add polar $ACCESS_KEY $SECRET_ACCESS_KEY
-mc admin policy create polar polar-development /tmp/config/policy.json
+mc admin policy create polar polar-development $POLICY_FILE
 mc admin policy attach polar polar-development --user $ACCESS_KEY
 
 # Create development & testing buckets

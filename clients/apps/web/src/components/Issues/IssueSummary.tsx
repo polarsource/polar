eb2227b820f4f297f38e1131d688a7a58790b292
Emil Widlund <hello@emilwidlund.com>
2024-06-05 13:19:56 +0200

    clients/issues: fix styling of issue activity

--- clients/apps/web/src/components/Issues/IssueSummary.tsx
+++ clients/apps/web/src/components/Issues/IssueSummary.tsx
@@ -63,7 +63,7 @@ const IssueSummary: React.FC<IssueSummaryProps> = ({
   linkToFunding = linkToFunding !== undefined ? linkToFunding : true
 
   return (
-    <div className="dark:md:hover:bg-polar-800 duration-50 dark:text-polar-50 md:hover:bg-gray-75 group flex flex-col items-start justify-between gap-4 overflow-hidden px-6 py-4 pb-5 md:flex-row md:items-center md:rounded-2xl">
+    <div className="dark:md:hover:bg-polar-800 duration-50 dark:text-polar-50 group flex flex-col items-start justify-between gap-4 overflow-hidden px-6 py-4 pb-5 md:flex-row md:items-center md:rounded-2xl md:hover:bg-gray-50">
       <div className="flex flex-row items-center">
         {showLogo && (
           <div className="mr-4 flex-shrink-0 justify-center rounded-full bg-white p-[1px] shadow">

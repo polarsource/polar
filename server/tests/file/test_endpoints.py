7beb40fed45fb8b0a35c001b7d3cf17265dfe9fa
Birk Jernström <birkjernstrom@gmail.com>
2024-06-06 20:04:30 +0200

    server/tests: Move upload logic to fixture

--- server/tests/file/test_endpoints.py
+++ server/tests/file/test_endpoints.py
@@ -1,4 +1,3 @@
-import base64
 from urllib.parse import parse_qs, urlencode, urlparse
 from uuid import UUID
 
@@ -6,16 +5,10 @@ import pytest
 from httpx import AsyncClient, ReadError, Response
 
 from polar.auth.models import AuthSubject
-from polar.config import settings
-from polar.file.schemas import FileRead, FileUpload, FileUploadCompleted
+from polar.file.schemas import FileUpload
 from polar.file.service import file as file_service
 from polar.file.service import s3_service
 from polar.integrations.aws.s3.exceptions import S3FileError
-from polar.integrations.aws.s3.schemas import (
-    S3FileUploadCompleted,
-    S3FileUploadCompletedPart,
-    S3FileUploadPart,
-)
 from polar.models import Organization, User, UserOrganization
 from polar.postgres import AsyncSession
 from tests.fixtures.auth import AuthSubjectFixture
@@ -41,55 +34,6 @@ class TestEndpoints:
         assert len(valid.upload.parts) == parts
         return valid
 
-    async def upload_part_and_test(
-        self,
-        minio_client: AsyncClient,
-        upload_id: str,
-        part: S3FileUploadPart,
-        file: TestFile,
-    ) -> S3FileUploadCompletedPart:
-        upload_url = part.url
-        url = urlparse(upload_url)
-        params = parse_qs(url.query)
-
-        def p(name: str) -> str:
-            return params[name][0]
-
-        assert int(p("partNumber")) == part.number
-        assert p("uploadId") == upload_id
-        assert p("X-Amz-Signature")
-        assert int(p("X-Amz-Expires")) == settings.S3_FILES_PRESIGN_TTL
-
-        chunk = file.get_chunk(part)
-
-        upload = await minio_client.put(upload_url, content=chunk, headers=part.headers)
-
-        assert upload.status_code == 200
-        etag = upload.headers.get("ETag")
-        assert etag
-        return S3FileUploadCompletedPart(
-            number=part.number,
-            checksum_etag=etag,
-            checksum_sha256_base64=part.checksum_sha256_base64,
-        )
-
-    async def upload_multiparts_and_test(
-        self,
-        created: FileUpload,
-        file: TestFile,
-    ) -> list[S3FileUploadCompletedPart]:
-        completed = []
-        async with AsyncClient() as minio_client:
-            for part in created.upload.parts:
-                uploaded = await self.upload_part_and_test(
-                    minio_client,
-                    created.upload.id,
-                    part=part,
-                    file=file,
-                )
-                completed.append(uploaded)
-        return completed
-
     async def test_anonymous_create_401(
         self, client: AsyncClient, organization: Organization
     ) -> None:
@@ -103,7 +47,7 @@ class TestEndpoints:
     ) -> None:
         response = await client.post(
             "/api/v1/files/",
-            json=logo_png.build_create_payload(organization.id),
+            json=logo_png.build_create_json(organization.id),
         )
 
         assert response.status_code == 403
@@ -121,14 +65,7 @@ class TestEndpoints:
         logo_png: TestFile,
     ) -> None:
         organization_id = user_organization_admin.organization_id
-
-        response = await client.post(
-            "/api/v1/files/",
-            json=logo_png.build_create_payload(organization_id),
-        )
-        created = self.ensure_expected_create_response(
-            response, organization_id, file=logo_png
-        )
+        await logo_png.create(client, organization_id)
 
     @pytest.mark.http_auto_expunge
     @pytest.mark.auth(
@@ -143,16 +80,9 @@ class TestEndpoints:
         logo_jpg: TestFile,
     ) -> None:
         organization_id = user_organization_admin.organization_id
+        created = await logo_jpg.create(client, organization_id)
 
-        response = await client.post(
-            "/api/v1/files/",
-            json=logo_jpg.build_create_payload(organization_id),
-        )
-        created = self.ensure_expected_create_response(
-            response, organization_id, file=logo_jpg
-        )
-
-        await self.upload_multiparts_and_test(created, file=logo_jpg)
+        await logo_jpg.upload(created)
 
         # S3 object is not available until we fully complete it
         with pytest.raises(S3FileError):
@@ -176,13 +106,7 @@ class TestEndpoints:
     ) -> None:
         organization_id = user_organization_admin.organization_id
 
-        response = await client.post(
-            "/api/v1/files/",
-            json=logo_jpg.build_create_payload(organization_id),
-        )
-        created = self.ensure_expected_create_response(
-            response, organization_id, file=logo_jpg
-        )
+        created = await logo_jpg.create(client, organization_id)
 
         part = created.upload.parts[0]
 
@@ -204,20 +128,19 @@ class TestEndpoints:
         )
         tampered_url = f"{url.scheme}://{url.netloc}{url.path}?{tampered_query}"
 
-        async with AsyncClient() as minio_client:
-            try:
-                failed = False
-                res = await minio_client.put(
-                    tampered_url, content=logo_jpg.data, headers=part.headers
-                )
-                # TODO
-                # Investigate this issue with httpx raising ReadError instead
-                # of handling the denied request (sometimes)
-                failed = res.status_code != 200
-            except ReadError:
-                failed = True
+        try:
+            failed = False
+            res = await logo_jpg.put_upload(
+                tampered_url, logo_jpg.data, headers=part.headers
+            )
+            # TODO
+            # Investigate this issue with httpx raising ReadError instead
+            # of handling the denied request (sometimes)
+            failed = res.status_code != 200
+        except ReadError:
+            failed = True
 
-            assert failed
+        assert failed
 
         # S3 object is definitely not available
         with pytest.raises(S3FileError):
@@ -240,55 +163,9 @@ class TestEndpoints:
         logo_jpg: TestFile,
     ) -> None:
         organization_id = user_organization_admin.organization_id
-
-        response = await client.post(
-            "/api/v1/files/",
-            json=logo_jpg.build_create_payload(organization_id),
-        )
-        created = self.ensure_expected_create_response(
-            response, organization_id, file=logo_jpg
-        )
-
-        uploaded_parts = await self.upload_multiparts_and_test(created, file=logo_jpg)
-
-        payload = FileUploadCompleted(
-            id=created.upload.id, path=created.path, parts=uploaded_parts
-        )
-        payload_json = payload.model_dump(mode="json")
-
-        completed = await client.post(
-            f"/api/v1/files/{created.id}/uploaded",
-            json=payload_json,
-        )
-
-        assert completed.status_code == 200
-        upload_data = completed.json()
-        uploaded = FileRead(**upload_data)
-
-        assert uploaded.id == created.id
-        assert uploaded.is_uploaded is True
-        s3_object = s3_service.get_object_or_raise(uploaded.path)
-        metadata = s3_object["Metadata"]
-
-        assert s3_object["ETag"] == uploaded.checksum_etag
-        assert metadata["polar-id"] == str(uploaded.id)
-        assert metadata["polar-organization-id"] == str(uploaded.organization_id)
-        assert metadata["polar-name"] == uploaded.name
-        assert metadata["polar-size"] == str(logo_jpg.size)
-        assert s3_object["ContentLength"] == logo_jpg.size
-        assert s3_object["ContentType"] == logo_jpg.mime_type
-
-        digests = []
-        for part in uploaded_parts:
-            checksum_sha256_base64 = part.checksum_sha256_base64
-            if checksum_sha256_base64:
-                digests.append(base64.b64decode(checksum_sha256_base64))
-
-        valid_s3_checksum = S3FileUploadCompleted.generate_base64_multipart_checksum(
-            digests
-        )
-        # S3 stores checksums as <Checksum>-<PartCount>
-        assert s3_object["ChecksumSHA256"].split("-")[0] == valid_s3_checksum
+        created = await logo_jpg.create(client, organization_id)
+        uploaded = await logo_jpg.upload(created)
+        await logo_jpg.complete(client, created, uploaded)
 
         record = await file_service.get(session, created.id, allow_deleted=True)
         assert record

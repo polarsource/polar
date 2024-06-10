d3001e8c78573049793872239dc3b1a26d69e18f
Emil Widlund <hello@emilwidlund.com>
2024-06-10 16:42:06 +0200

    clients/landing: add testimonials

--- clients/apps/web/src/app/(landing)/ClientPage.tsx
+++ clients/apps/web/src/app/(landing)/ClientPage.tsx
@@ -65,6 +65,9 @@ export default function Page() {
         <HeroSection />
       </Box>
       <Box>
+        <Testamonials />
+      </Box>
+      <Box>
         <BenefitsUpsell />
       </Box>
       <Box>
@@ -110,7 +113,7 @@ const HeroSection = () => {
       >
         <div className="flex flex-col gap-y-8">
           <h1 className="text-pretty text-4xl leading-tight text-gray-950 dark:text-white">
-            From a first donation to IPO
+            Funding & Monetization tools for Developers
           </h1>
           <p className="text-xl leading-relaxed text-gray-500">
             The all-in-one funding & monetization platform for open source- and
@@ -146,7 +149,7 @@ const HeroSection = () => {
         className="flex-grow md:hidden"
         orientation="horizontal"
       />
-      <div className="dark:bg-polar-800 flex flex-col items-center justify-center bg-gray-50 p-12 md:w-3/5">
+      <div className="flex h-full w-full flex-col items-center justify-center p-12 md:w-3/5">
         <Image
           className="block dark:hidden"
           src="/assets/landing/subscriptions_view.webp"
@@ -595,6 +598,146 @@ const SignUpBanner = () => {
         userSignupType={UserSignupType.MAINTAINER}
         returnTo="/maintainer"
       />
+    </motion.div>
+  )
+}
+
+const testamonials = [
+  {
+    link: 'https://x.com/mitchellh/status/1775925951668552005',
+    name: 'Mitchell Hashimoto',
+    company: 'Ghostty',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1141762999838842880/64_Y4_XB_400x400.jpg',
+    text: (
+      <>
+        <p>I&apos;ve joined Polar as an advisor!</p>
+        <p className="dark:text-polar-200 text-gray-500">
+          I think it benefits everyone for devs to have more options to get paid
+          to work on their passions, to support upstreams, and for users to have
+          more confidence/transparency in the software they&apos;re
+          supporting/purchasing.
+        </p>
+      </>
+    ),
+  },
+  {
+    link: 'https://x.com/awesomekling/status/1794769509305528625',
+    name: 'Andreas Kling',
+    company: 'SerenityOS & Ladybird',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1743699387165925376/-Zd5Bwsi_400x400.jpg',
+    text: (
+      <>
+        <p className="dark:text-polar-200 text-gray-500">
+          I just used Polar to sponsor someone to improve Polar in Ladybird
+          Browser!
+        </p>
+        <p>
+          It&apos;s honestly such a comfy way to spread the love and share some
+          of my funding with more of our developers!
+        </p>
+      </>
+    ),
+  },
+  {
+    link: 'https://x.com/cpojer/status/1795905984977576017',
+    name: 'Christoph Nakazawa',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1189537722286952449/OrscO0bD_400x400.jpg',
+    company: 'Athena Crisis',
+    text: (
+      <>
+        <p className="dark:text-polar-200 text-gray-500">
+          It&apos;s only been two weeks but Polar has been extremely good for
+          funding contributions for Athena Crisis.
+        </p>
+        <p>Frictionless, fast, easy.</p>
+      </>
+    ),
+  },
+  {
+    link: 'https://x.com/samuel_colvin/status/1676167205715582978',
+    name: 'Samuel Colvin',
+    company: 'Pydantic',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1678332260569710594/of0Ed11O_400x400.jpg',
+    text: (
+      <>
+        <p className="dark:text-polar-200 text-gray-500">
+          Amazing! Really excited to seeing how this turns out.
+        </p>
+        <p>
+          Polar is the cutting edge of how open source might be financed in the
+          future.
+        </p>
+      </>
+    ),
+  },
+]
+
+const Testamonials = () => {
+  return (
+    <motion.div
+      className="flex flex-col divide-y"
+      initial="initial"
+      variants={{ initial: { opacity: 0 }, animate: { opacity: 1 } }}
+      transition={{ delay: 0.2, duration: 0.5, ease: 'easeInOut' }}
+      whileInView="animate"
+      viewport={{ once: true }}
+    >
+      <Link
+        className="hover:bg-gray-75 dark:hover:bg-polar-900 flex flex-col gap-y-6 p-8 transition-colors"
+        href={testamonials[0].link}
+        target="_blank"
+      >
+        <div className="flex flex-col gap-y-4 text-lg md:w-2/3">
+          {testamonials[0].text}
+        </div>
+        <div className="flex flex-row items-center gap-x-4">
+          <Avatar
+            className="h-10 w-10"
+            avatar_url={testamonials[0].avatar}
+            name={testamonials[0].name}
+          />
+
+          <div className="flex flex-col text-sm">
+            <span>{testamonials[0].name}</span>
+            <span className="dark:text-polar-500 text-gray-500">
+              {testamonials[0].company}
+            </span>
+          </div>
+        </div>
+      </Link>
+      <div className="flex flex-col items-center divide-y md:flex-row md:divide-x md:divide-y-0">
+        {testamonials.slice(1).map((testamonial) => (
+          <Link
+            key={testamonial.name}
+            className="flex h-full flex-col md:w-1/3"
+            href={testamonial.link}
+            target="_blank"
+          >
+            <div className="hover:bg-gray-75 dark:hover:bg-polar-900 group relative flex h-full w-full flex-col gap-y-8 rounded-none border-none p-8 transition-colors">
+              <div className="flex h-full flex-col gap-y-4 leading-relaxed">
+                {testamonial.text}
+              </div>
+              <div className="flex flex-row items-center gap-x-4 space-y-0">
+                <Avatar
+                  className="h-10 w-10"
+                  avatar_url={testamonial.avatar}
+                  name={testamonial.name}
+                />
+                <div className="flex flex-col text-sm">
+                  <span>{testamonial.name}</span>
+                  <span className="dark:text-polar-500 text-gray-500">
+                    {testamonial.company}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </motion.div>
   )
 }

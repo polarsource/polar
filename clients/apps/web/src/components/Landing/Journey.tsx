31364765a725663c296fb1b7ec7be170d36509bb
Emil Widlund <hello@emilwidlund.com>
2024-06-10 15:39:27 +0200

    clients/landing: first initial draft

---
+++ clients/apps/web/src/components/Landing/Journey.tsx
@@ -0,0 +1,112 @@
+import { ArrowForward } from '@mui/icons-material'
+import { List, ListItem } from 'polarkit/components/ui/atoms/list'
+import { PropsWithChildren, useState } from 'react'
+import { Section } from './Section'
+
+const codeExample = `curl -X POST \
+
+https://api.polar.sh/api/v1/issues/123/add_badge \
+
+-H "Content-type: application/json" \
+-H "Accept: application/json" \
+-H "Authorization: Bearer <token>" \
+`
+
+const items = [
+  {
+    title: 'From Idea to Funding',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet in urna ut porta. Cras at libero vitae erat cursus dapibus. Sed quis volutpat lectus.',
+  },
+  {
+    title: 'Build a Community from Day 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet in urna ut porta. Cras at libero vitae erat cursus dapibus. Sed quis volutpat lectus.',
+  },
+  {
+    title: 'Start a Business',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet in urna ut porta. Cras at libero vitae erat cursus dapibus. Sed quis volutpat lectus.',
+  },
+  {
+    title: 'We handle your taxes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet in urna ut porta. Cras at libero vitae erat cursus dapibus. Sed quis volutpat lectus.',
+  },
+  {
+    title: 'Deeply integrated with GitHub',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet in urna ut porta. Cras at libero vitae erat cursus dapibus. Sed quis volutpat lectus.',
+  },
+  {
+    title: 'Built entirely Open Source',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet in urna ut porta. Cras at libero vitae erat cursus dapibus. Sed quis volutpat lectus.',
+  },
+]
+
+export const Journey = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  return (
+    <Section className="gap-y-24">
+      <h3 className="text-5xl font-light leading-relaxed">
+        Focus on your passion —<br />
+        while we build infrastructure to get you paid
+      </h3>
+      <div className="flex flex-row gap-x-24">
+        <div className="flex w-1/2 flex-col">
+          <List>
+            {items.map((item, index) => (
+              <FeatureItem
+                key={item.title}
+                title={item.title}
+                description={item.description}
+                active={selectedIndex === index}
+                onClick={() => setSelectedIndex(index)}
+              />
+            ))}
+          </List>
+        </div>
+        <div className="flex w-1/2 flex-col items-center justify-center">
+          <div className="flex flex-col rounded-3xl border p-8 font-mono">
+            <pre className="select-text overflow-auto text-wrap p-4 font-mono text-xs leading-loose text-gray-900 dark:text-white">
+              {codeExample}
+            </pre>
+          </div>
+        </div>
+      </div>
+    </Section>
+  )
+}
+
+const FeatureItem = ({
+  title,
+  description,
+  children,
+  active,
+  onClick,
+}: PropsWithChildren<{
+  title: string
+  description: string
+  active: boolean
+  onClick: () => void
+}>) => {
+  return (
+    <ListItem
+      className="hover:bg-gray-75 dark:hover:bg-polar-900 flex flex-col items-start gap-y-4 p-6 dark:bg-transparent"
+      selected={active}
+      onSelect={onClick}
+    >
+      <div className="flex flex-row items-center gap-x-4">
+        <ArrowForward className="text-black dark:text-white" fontSize="small" />
+        <h3 className="text-lg">{title}</h3>
+      </div>
+      {active && (
+        <div className="dark:text-polar-200 flex flex-col text-sm leading-relaxed text-gray-500">
+          {description}
+        </div>
+      )}
+    </ListItem>
+  )
+}

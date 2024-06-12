8a3537ab60112640ac7f38c9fa3fc2042c2bcae9
François Voron <fvoron@gmail.com>
2024-06-12 16:03:59 +0200

    clients/web: add required/optional badge for query parameters

---
+++ clients/apps/web/src/components/Documentation/OptionalBadge.tsx
@@ -0,0 +1,8 @@
+const OptionalBadge = () => {
+  return (
+    <span className="text-xxs rounded-md bg-green-50 px-2 py-1 font-mono font-normal capitalize text-green-500 dark:bg-green-950/50">
+      Optional
+    </span>
+  )
+}
+export default OptionalBadge

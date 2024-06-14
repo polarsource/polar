718d346c2cc5dd8488b81c042a97c5fa4e9f55f9
Emil Widlund <hello@emilwidlund.com>
2024-06-14 10:05:15 +0200

    clients/landing: adjust grid

--- clients/apps/web/src/components/Landing/Journey.tsx
+++ clients/apps/web/src/components/Landing/Journey.tsx
@@ -17,7 +17,6 @@ import {
   CardFooter,
   CardHeader,
 } from 'polarkit/components/ui/atoms/card'
-import { List } from 'polarkit/components/ui/atoms/list'
 import React, { PropsWithChildren } from 'react'
 import { twMerge } from 'tailwind-merge'
 import GitHubIcon from '../Icons/GitHubIcon'
@@ -48,7 +47,7 @@ const FeatureItem = ({
       <Card className="hover:bg-gray-75 dark:hover:to-polar-900 dark:border-polar-800 dark:from-polar-900 dark:to-polar-800 flex h-full flex-col bg-gradient-to-tr from-gray-50 to-blue-50 p-1 transition-colors">
         <CardHeader className="flex flex-row items-center gap-x-3 space-y-0 pb-4">
           {icon ? (
-            <span className="dark:bg-polar-700 dark flex h-10 w-10 flex-col items-center justify-center rounded-full bg-gray-200 text-xl transition-colors group-hover:bg-blue-500 group-hover:text-white dark:group-hover:bg-white dark:group-hover:text-black">
+            <span className="dark:bg-polar-700 dark flex h-10 w-10 flex-col items-center justify-center rounded-full bg-white text-xl transition-colors group-hover:bg-blue-500 group-hover:text-white dark:group-hover:bg-white dark:group-hover:text-black">
               {React.cloneElement(icon, { fontSize: 'inherit' })}
             </span>
           ) : (
@@ -60,7 +59,7 @@ const FeatureItem = ({
           <p className="dark:text-polar-200 h-full leading-relaxed text-gray-500 group-hover:text-black dark:group-hover:text-white">
             {description}
           </p>
-          <div className="dark:text-polar-200 flex flex-row items-center gap-x-2 text-sm">
+          <div className="dark:text-polar-200 flex flex-row items-center gap-x-2 text-sm transition-colors group-hover:text-blue-500 dark:group-hover:text-white">
             <span>Learn More</span>
             <ArrowForwardOutlined fontSize="inherit" />
           </div>
@@ -121,11 +120,10 @@ const items = [
             link="/docs/overview/donations"
           />
           <FeatureItem
-            className="md:col-span-1"
-            icon={<AccountBalanceOutlined />}
-            title="Payouts"
-            description="Withdraw your earnings with ease. Supporting Stripe & Open Collective."
-            link="#"
+            icon={<HiveOutlined />}
+            title="Benefits Engine"
+            description="Give supporters value for their money with private GitHub repository access, Discord server invites & file downloads."
+            link="#benefits"
           />
         </div>
       </div>
@@ -136,32 +134,68 @@ const items = [
     description:
       'Polar helps you take a leap of faith & turn your passion project into a thriving business.',
     content: (
-      <div className="flex flex-col gap-y-12">
+      <div className="flex flex-col gap-y-24">
         <div className="flex flex-col gap-y-4">
-          <h2 className="text-2xl">From passion to business</h2>
-          <p className="dark:text-polar-200 text-gray-500">
-            Unlock the full potential of your project by selling digital
-            products.
+          <h2 className="text-4xl">From passion to business</h2>
+          <p className="dark:text-polar-200 text-xl text-gray-500">
+            Unlock the full potential of your project with insights to your
+            sales
           </p>
         </div>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <FeatureItem
-            icon={<DiamondOutlined />}
-            title="Products"
-            description="Sell licenses, access to private repositories, or any other digital product you can think of."
-            link="/docs/overview/issue-funding/reward-contributors"
-          />
-          <FeatureItem
+            className="md:col-span-2 md:row-span-2"
             icon={<TrendingUpOutlined />}
             title="Sales Metrics"
             description="We aggregate all your sales data into one place, so you can focus on what matters."
             link="/docs/overview/issue-funding/reward-contributors"
+          >
+            <picture>
+              <source
+                media="(prefers-color-scheme: dark)"
+                srcSet={`/assets/landing/sales_dark.png`}
+              />
+              <img
+                className="dark:border-polar-700 rounded-2xl border border-gray-100"
+                srcSet={`/assets/landing/sales.png`}
+              />
+            </picture>
+          </FeatureItem>
+          <FeatureItem
+            icon={<Language />}
+            title="Public Page"
+            description="A home for your profile on Polar. Showcase your projects, crowdfunded issues, and more."
+            link="#"
+          />
+          <FeatureItem
+            className="md:row-span-2"
+            icon={<ReceiptLongOutlined />}
+            title="Newsletters"
+            description="Reach your community with insightful newsletter posts - using an MDX-powered editor."
+            link="/docs/overview/issue-funding/reward-contributors"
+          >
+            <picture>
+              <source
+                media="(prefers-color-scheme: dark)"
+                srcSet={`/assets/landing/newsletter_dark.png`}
+              />
+              <img
+                className="dark:border-polar-700 rounded-2xl border border-gray-100"
+                srcSet={`/assets/landing/newsletter.png`}
+              />
+            </picture>
+          </FeatureItem>
+          <FeatureItem
+            icon={<AccountBalanceOutlined />}
+            title="Payouts"
+            description="Withdraw your earnings with ease. Supporting Stripe & Open Collective."
+            link="#"
           />
           <FeatureItem
             icon={<ReceiptLongOutlined />}
-            title="Newsletters"
-            description="Reach your community with insightful newsletter posts."
-            link="/docs/overview/issue-funding/reward-contributors"
+            title="Merchant of Record"
+            description="No more VAT headache - we handle the necessary taxes for you."
+            link="/docs/overview/payments-taxes#taxes"
           />
         </div>
       </div>
@@ -175,44 +209,17 @@ export const Journey = () => {
       <div className="flex flex-col gap-32 gap-y-32">
         <div className="flex flex-col gap-y-2 text-5xl leading-normal">
           <h3>From first donation to IPO</h3>
-          <h3 className="dark:text-polar-500 text-gray-500">
+          <h3 className="dark:text-polar-600 text-gray-500">
             With a wide array of funding tools for your project
           </h3>
         </div>
-        <div className="flex flex-col gap-24">
+        <div className="flex flex-col gap-32">
           {items.map((item) => (
             <div className="flex w-full flex-col" key={item.title}>
               {item.content}
             </div>
           ))}
         </div>
-      </div>
-
-      <div className="flex flex-col gap-y-16">
-        <h3 className="text-4xl leading-relaxed">The Polar Foundation</h3>
-        <List className="md:flex-row md:divide-x md:divide-y-0">
-          <FeatureItem
-            className="h-auto md:w-1/3"
-            icon={<HiveOutlined />}
-            title="Benefits Engine"
-            description="Give supporters value for their money with private GitHub repository access, Discord server invites & file downloads, to name a few."
-            link="#benefits"
-          />
-          <FeatureItem
-            className="h-auto md:w-1/3"
-            icon={<Language />}
-            title="Public Page"
-            description="A home for your profile on Polar. Showcase your projects, crowdfunded issues, and more."
-            link="#"
-          />
-          <FeatureItem
-            className="h-auto md:w-1/3"
-            icon={<ReceiptLongOutlined />}
-            title="Merchant of Record"
-            description="No more VAT headache - we handle the necessary taxes for you."
-            link="/docs/overview/payments-taxes#taxes"
-          />
-        </List>
       </div>
     </Section>
   )

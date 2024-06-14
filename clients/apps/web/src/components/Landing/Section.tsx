e0f73de2d43ffe7dfcabc40a6fb6caa42dc94495
Emil Widlund <hello@emilwidlund.com>
2024-06-14 16:52:01 +0200

    clients/landing: more polish

--- clients/apps/web/src/components/Landing/Section.tsx
+++ clients/apps/web/src/components/Landing/Section.tsx
@@ -21,7 +21,7 @@ export const Section = ({
     >
       <div
         className={twMerge(
-          'flex w-full max-w-[100vw] flex-col px-6 py-32 md:max-w-7xl md:px-12',
+          'flex w-full max-w-[100vw] flex-col px-4 py-24 md:max-w-7xl md:px-12 md:py-32',
           className,
         )}
       >

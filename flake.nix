147b0aa22b82175cd7f0dc132578ca4451da2529
Birk Jernstrom <birkjernstrom@gmail.com>
2024-06-18 10:15:22 +0200

    meta: Initial flake.nix file

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          nodejs_20
+          nodePackages.pnpm
+
+          python312
+        ];
+      };
+    };
+}

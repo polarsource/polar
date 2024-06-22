c1f4a094b1aff710f870ef95549c7acc1cf47dca
François Voron <fvoron@gmail.com>
2024-06-22 14:23:57 +0200

    clients/web: fix unit tests when using Shiki

--- clients/apps/web/jest.setup.ts
+++ clients/apps/web/jest.setup.ts
@@ -2,4 +2,7 @@ import '@testing-library/jest-dom'
 
 import { TextDecoder, TextEncoder } from 'util'
 
+jest.mock('shiki', () => ({}))
+jest.mock('shiki/bundle/web', () => ({}))
+
 Object.assign(global, { TextDecoder, TextEncoder })

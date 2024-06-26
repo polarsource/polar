fd110af1879132b4a2297ba728164cde16952dcc
François Voron <fvoron@gmail.com>
2024-06-26 14:58:40 +0200

    server/checkout: tweak API definition for prettier SDK generation

--- server/polar/checkout/endpoints.py
+++ server/polar/checkout/endpoints.py
@@ -13,17 +13,17 @@ log = structlog.get_logger()
 router = APIRouter(prefix="/checkouts", tags=["checkouts"])
 
 
-@router.post("/", response_model=Checkout, status_code=201)
-async def create_checkout(
+@router.post("/", summary="Create Checkout", response_model=Checkout, status_code=201)
+async def create(
     checkout_create: CheckoutCreate,
     auth_subject: auth.Checkout,
     session: AsyncSession = Depends(get_db_session),
 ) -> Checkout:
+    """Create a checkout session."""
     return await checkout_service.create(session, checkout_create, auth_subject)
 
 
-@router.get("/{id}", response_model=Checkout)
-async def get_checkout(
-    id: str, session: AsyncSession = Depends(get_db_session)
-) -> Checkout:
+@router.get("/{id}", summary="Get Checkout", response_model=Checkout)
+async def get(id: str, session: AsyncSession = Depends(get_db_session)) -> Checkout:
+    """Get an active checkout session by ID."""
     return await checkout_service.get_by_id(session, id)

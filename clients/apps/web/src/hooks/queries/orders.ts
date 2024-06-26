48276ffeb19652701a3751de85d1ad3317cfb35d
François Voron <fvoron@gmail.com>
2024-06-26 14:05:15 +0200

    clients/web: fix Orders calls with updated SDK

--- clients/apps/web/src/hooks/queries/orders.ts
+++ clients/apps/web/src/hooks/queries/orders.ts
@@ -1,16 +1,16 @@
 import { api } from '@/utils/api'
-import { OrdersApiListOrdersRequest } from '@polar-sh/sdk'
+import { OrdersApiListRequest } from '@polar-sh/sdk'
 import { useQuery } from '@tanstack/react-query'
 import { defaultRetry } from './retry'
 
 export const useOrders = (
   organizationId?: string,
-  parameters?: Omit<OrdersApiListOrdersRequest, 'organization_id'>,
+  parameters?: Omit<OrdersApiListRequest, 'organization_id'>,
 ) =>
   useQuery({
     queryKey: ['orders', { organizationId, ...(parameters || {}) }],
     queryFn: () =>
-      api.orders.listOrders({
+      api.orders.list({
         organizationId: organizationId ?? '',
         ...(parameters || {}),
       }),

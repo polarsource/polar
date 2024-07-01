2abef2d9dfd06ceedc50815f81683b3afd855c44
Emil Widlund <hello@emilwidlund.com>
2024-07-01 19:35:34 +0200

    clients/chrome: adjust contrasts

--- clients/apps/web/src/components/Subscriptions/SubscriptionTierCard.tsx
+++ clients/apps/web/src/components/Subscriptions/SubscriptionTierCard.tsx
@@ -127,7 +127,7 @@ const SubscriptionTierCard: React.FC<SubscriptionTierCardProps> = ({
       }}
       id={subscriptionTier.name}
       className={twMerge(
-        'dark:border-polar-700 relative flex flex-col gap-y-6 overflow-hidden rounded-3xl border-gray-100 bg-white bg-gradient-to-tr from-white to-blue-50/50 dark:from-transparent dark:to-transparent',
+        'dark:bg-polar-900 relative flex flex-col gap-y-6 overflow-hidden rounded-3xl bg-white hover:bg-gray-50',
         draggable?.isDragging && 'opacity-30',
         variantStyles[variant]['card'],
         className,

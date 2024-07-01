eda2d0512b571626ecfe288f553b27babfb80a04
Emil Widlund <hello@emilwidlund.com>
2024-07-01 11:15:59 +0200

    clients/landing: add asset to MOR section

--- clients/apps/web/src/components/Landing/MOR.tsx
+++ clients/apps/web/src/components/Landing/MOR.tsx
@@ -5,40 +5,46 @@ import {
   ReceiptLongOutlined,
   TrendingUpOutlined,
 } from '@mui/icons-material'
+import { motion } from 'framer-motion'
 import FeatureItem from './molecules/FeatureItem'
 
 export const MerchantOfRecord = () => {
   return (
     <div className="flex flex-col gap-y-24">
-      <div className="flex flex-col items-center gap-y-4 text-center md:gap-y-8">
-        <h2 className="text-4xl leading-snug md:text-5xl">
-          Increase sales, not overhead
-        </h2>
-        <h3 className="dark:text-polar-600 text-4xl leading-snug text-gray-500">
-          Polar handles VAT, sales tax and billing so you don&apos;t have to
-        </h3>
-      </div>
+      <motion.div
+        className="relative flex flex-col gap-y-4"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, transition: { duration: 2 } }}
+      >
+        <picture>
+          <source
+            media="(prefers-color-scheme: dark)"
+            srcSet={`/assets/landing/metrics_dark.png`}
+          />
+          <img
+            className="dark:border-polar-700 rounded-3xl border border-gray-200"
+            srcSet={`/assets/landing/metrics.png`}
+            alt="Metrics dashboard in Polar"
+          />
+        </picture>
+        <div className="dark:from-polar-950 absolute inset-0 flex flex-col items-center justify-end gap-y-2 bg-gradient-to-t from-white from-30% to-transparent p-6 text-center md:gap-y-8 md:p-12 dark:to-transparent">
+          <h2 className="text-2xl leading-snug md:text-5xl">
+            Increase sales, not overhead
+          </h2>
+          <h3 className="dark:text-polar-600 text-xl leading-snug text-gray-300 md:text-4xl">
+            Polar handles VAT, sales tax and billing so you don&apos;t have to
+          </h3>
+        </div>
+      </motion.div>
+
       <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
         <FeatureItem
-          className="md:col-span-2 md:row-span-2"
           icon={<TrendingUpOutlined />}
           title="Sales Metrics"
           description="Professional funding, sales and subscription metrics. Your dashboard can be plotted down to each hour - let's aim high, together."
           link="#"
           showLink={false}
-        >
-          <picture>
-            <source
-              media="(prefers-color-scheme: dark)"
-              srcSet={`/assets/landing/sales_dark.png`}
-            />
-            <img
-              className="dark:border-polar-700 rounded-2xl border border-gray-100"
-              srcSet={`/assets/landing/sales.png`}
-              alt="Showing metric dashboard within Polar"
-            />
-          </picture>
-        </FeatureItem>
+        />
         <FeatureItem
           icon={<ReceiptLongOutlined />}
           title="Merchant of Record"

4bad5828f280f6eb5f0b323f1416442f944be19d
François Voron <fvoron@gmail.com>
2024-07-02 21:04:40 +0200

    clients/web/docs: fix new layouts

--- clients/apps/web/src/app/docs/support/layout.tsx
+++ clients/apps/web/src/app/docs/support/layout.tsx
@@ -1,10 +1,8 @@
-import { MDXContentWrapper } from '@/components/Documentation/MDXContentWrapper'
 import {
   DocumentationPageSidebar,
   MainNavigation,
   MobileNav,
 } from '@/components/Documentation/Navigation'
-import { TableOfContents } from '@/components/Documentation/TableOfContents'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,15 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="hidden md:block">
         <DocumentationPageSidebar activeSection="support" />
       </div>
-      <div
-        id="mdx-wrapper"
-        className="flex w-full max-w-3xl flex-shrink flex-col"
-      >
-        <MDXContentWrapper>{children}</MDXContentWrapper>
-      </div>
-      <div className="flex w-full flex-shrink-0 flex-col md:sticky md:top-12 md:w-64">
-        <TableOfContents />
-      </div>
+      {children}
     </>
   )
 }

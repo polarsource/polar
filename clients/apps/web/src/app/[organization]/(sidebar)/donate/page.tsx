b61d55be84af8085f99dcea4aeb90e9df57d1ab7
François Voron <fvoron@gmail.com>
2024-07-09 10:34:51 +0200

    clients/web: use scalar organization_id filter on products queries
    This reverts commit 04339abeb962e08f32bd98ecd01c9e3031f941f6.

--- clients/apps/web/src/app/[organization]/(sidebar)/donate/page.tsx
+++ clients/apps/web/src/app/[organization]/(sidebar)/donate/page.tsx
@@ -115,7 +115,7 @@ export default async function Page({
 
         api.products.list(
           {
-            organizationId: [organization.id],
+            organizationId: organization.id,
             isRecurring: true,
           },
           {

2a2123f62bbe5901cf1da0570dfc8a5e90ae14fb
François Voron <fvoron@gmail.com>
2024-07-09 15:35:56 +0200

    clients/web: use full bundle for client-side Shiki
    Attempt to fix #3622

--- clients/apps/web/jest.setup.ts
+++ clients/apps/web/jest.setup.ts
@@ -3,6 +3,6 @@ import '@testing-library/jest-dom'
 import { TextDecoder, TextEncoder } from 'util'
 
 jest.mock('shiki', () => ({}))
-jest.mock('shiki/bundle/web', () => ({}))
+jest.mock('shiki/bundle/full', () => ({}))
 
 Object.assign(global, { TextDecoder, TextEncoder })

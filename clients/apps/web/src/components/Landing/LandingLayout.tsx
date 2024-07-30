f45117376853915e712b9b56e681f3cf9221f2d6
Emil Widlund <hello@emilwidlund.com>
2024-07-30 09:56:14 +0200

    clients/web: adjust github button

--- clients/apps/web/src/components/Landing/LandingLayout.tsx
+++ clients/apps/web/src/components/Landing/LandingLayout.tsx
@@ -45,15 +45,12 @@ const LandingPageTopbar = () => {
         logoVariant="logotype"
         size={100}
       />
-      <div className="flex flex-row items-center gap-x-6">
+      <div className="flex flex-row items-center gap-x-4">
         <a
           href="https://github.com/polarsource/polar"
-          className="dark:border-polar-700 dark:text-polar-200 dark:hover:bg-polar-800 transition-color hidden flex-row items-center gap-x-2 rounded-full border border-gray-200 py-1.5 pl-3 pr-1.5 text-sm hover:bg-gray-100 md:flex"
+          className="dark:text-polar-200 dark:bg-polar-800 dark:hover:bg-polar-700 transition-color hidden flex-row items-center gap-x-2 rounded-full bg-gray-100 px-4 py-1.5 text-sm hover:bg-gray-200 md:flex"
         >
           Star on GitHub
-          <span className="dark:bg-polar-700 rounded-full bg-gray-100 px-2 py-0.5 text-black dark:text-white">
-            1.5k
-          </span>
         </a>
         <Link href="/login">
           <Button>Login</Button>

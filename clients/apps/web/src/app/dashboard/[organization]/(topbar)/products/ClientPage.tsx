f15b1c30be3b4904d5f0735b43c1b61ec5d069b7
Birk Jernstrom <birkjernstrom@gmail.com>
2024-08-08 22:20:14 +0200

    clients: Show products & subscription tiers in one table

--- clients/apps/web/src/app/dashboard/[organization]/(topbar)/products/ClientPage.tsx
+++ clients/apps/web/src/app/dashboard/[organization]/(topbar)/products/ClientPage.tsx
@@ -4,7 +4,6 @@ import { DashboardBody } from '@/components/Layout/DashboardLayout'
 import { EnableProductsView } from '@/components/Products/EnableProductsView'
 import ProductPriceLabel from '@/components/Products/ProductPriceLabel'
 import ProductPrices from '@/components/Products/ProductPrices'
-import SubscriptionGroupIcon from '@/components/Subscriptions/SubscriptionGroupIcon'
 import { useProducts } from '@/hooks/queries/products'
 import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
 import { AddOutlined } from '@mui/icons-material'
@@ -18,13 +17,7 @@ import { useContext } from 'react'
 export default function ClientPage() {
   const { organization: org } = useContext(MaintainerOrganizationContext)
 
-  const subscriptions = useProducts(org.id, {
-    isRecurring: true,
-  })
-
-  const oneTime = useProducts(org.id, {
-    isRecurring: false,
-  })
+  const products = useProducts(org.id)
 
   if (org && !org.feature_settings?.subscriptions_enabled) {
     return <EnableProductsView organization={org} />
@@ -34,7 +27,7 @@ export default function ClientPage() {
     <DashboardBody className="flex flex-col gap-16">
       <div className="flex flex-col gap-y-8">
         <div className="flex flex-row items-center justify-between">
-          <h1 className="text-lg font-medium">Subscriptions</h1>
+          <h1 className="text-lg font-medium">Overview</h1>
           <div className="flex w-1/3 flex-row items-center justify-end gap-6 md:w-1/5">
             <Link href={`/dashboard/${org.slug}/products/new`}>
               <Button size="icon" role="link">
@@ -43,33 +36,9 @@ export default function ClientPage() {
             </Link>
           </div>
         </div>
-        {(subscriptions.data?.items?.length ?? 0) > 0 && (
+        {(products.data?.items?.length ?? 0) > 0 && (
           <List>
-            {subscriptions.data?.items?.map((product) => (
-              <ProductListItem
-                key={product.id}
-                organization={org}
-                product={product}
-              />
-            ))}
-          </List>
-        )}
-      </div>
-
-      <div className="flex flex-col gap-y-8">
-        <div className="flex flex-row items-center justify-between">
-          <h1 className="text-lg font-medium">One-Time Purchasables</h1>
-          <div className="flex w-1/3 flex-row items-center justify-end gap-6 md:w-1/5">
-            <Link href={`/dashboard/${org.slug}/products/new`}>
-              <Button size="icon" role="link">
-                <AddOutlined className="h-4 w-4" />
-              </Button>
-            </Link>
-          </div>
-        </div>
-        {(oneTime.data?.items?.length ?? 0) > 0 && (
-          <List>
-            {oneTime.data?.items?.map((product) => (
+            {products.data?.items?.map((product) => (
               <ProductListItem
                 key={product.id}
                 organization={org}
@@ -96,9 +65,6 @@ const ProductListItem = ({ product, organization }: ProductListItemProps) => {
     >
       <ListItem className="dark:hover:bg-polar-800 dark:bg-polar-900 flex flex-row items-center justify-between bg-white">
         <div className="flex flex-row items-center gap-x-4">
-          {product.is_recurring && (
-            <SubscriptionGroupIcon className="text-lg" type={product.type} />
-          )}
           <span>{product.name}</span>
         </div>
         <div className="flex flex-row items-center gap-x-4">
@@ -120,6 +86,9 @@ const ProductListItem = ({ product, organization }: ProductListItemProps) => {
                 : `${product.benefits.length} Benefits`}
             </Pill>
           )}
+          <Pill className="px-2.5 py-1" color="gray">
+            {product.is_recurring ? 'Subscription' : 'Product'}
+          </Pill>
         </div>
       </ListItem>
     </Link>

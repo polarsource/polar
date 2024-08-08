93271f8c80300e7364315ff2f07ae23b4f4b5975
Birk Jernstrom <birkjernstrom@gmail.com>
2024-08-08 22:45:03 +0200

    server: Fix tests

--- server/tests/kit/test_http.py
+++ server/tests/kit/test_http.py
@@ -11,8 +11,6 @@ async def test_get_safe_return_url() -> None:
         get_safe_return_url("http://127.0.0.1:3000/foo") == "http://127.0.0.1:3000/foo"
     )
 
-    assert get_safe_return_url("") == "http://127.0.0.1:3000/feed"
+    assert get_safe_return_url("") == "http://127.0.0.1:3000/"
 
-    assert (
-        get_safe_return_url("https://whatever.com/hey") == "http://127.0.0.1:3000/feed"
-    )
+    assert get_safe_return_url("https://whatever.com/hey") == "http://127.0.0.1:3000/"

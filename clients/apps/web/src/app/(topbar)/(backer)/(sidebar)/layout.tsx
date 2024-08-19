fc8eaa039e81acbd9c7517850ac72a4f99b2eb27
Emil Widlund <hello@emilwidlund.com>
2024-08-19 11:28:48 +0200

    clients/web: remove github app upsell

--- clients/apps/web/src/app/(topbar)/(backer)/(sidebar)/layout.tsx
+++ clients/apps/web/src/app/(topbar)/(backer)/(sidebar)/layout.tsx
@@ -1,19 +1,17 @@
 'use client'
 
 import {
-  GitHubAuthUpsell,
   MaintainerUpsell,
   SetupProductsUpsell,
 } from '@/components/Dashboard/Upsell'
 import PurchaseSidebar from '@/components/Purchases/PurchasesSidebar'
-import { useAuth, useGitHubAccount } from '@/hooks'
+import { useAuth } from '@/hooks'
 import { useProducts } from '@/hooks/queries'
 import { PropsWithChildren } from 'react'
 
 export default function Layout({ children }: PropsWithChildren) {
   const { authenticated, userOrganizations: organizations } = useAuth()
   const products = useProducts(organizations.map((o) => o.id))
-  const githubAccount = useGitHubAccount()
 
   const shouldShowMaintainerUpsell = authenticated && organizations.length === 0
 
@@ -21,13 +19,10 @@ export default function Layout({ children }: PropsWithChildren) {
     !products.isLoading &&
     (products?.data?.items?.filter((p) => p.type !== 'free').length ?? 0) < 1
 
-  const shouldShowGitHubAuthUpsell = authenticated && !githubAccount
-
   return (
     <div className="flex h-full flex-col gap-12 md:flex-row">
       <div className="flex h-full w-full flex-shrink-0 flex-col gap-y-6 self-stretch md:sticky md:top-[3rem] md:max-w-xs">
         <PurchaseSidebar />
-        {shouldShowGitHubAuthUpsell && <GitHubAuthUpsell />}
         {shouldShowMaintainerUpsell ? (
           <MaintainerUpsell />
         ) : shouldShowProductsUpsell ? (

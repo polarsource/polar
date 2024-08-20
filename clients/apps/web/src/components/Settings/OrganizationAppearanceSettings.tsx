8e8771c2811c93168e492923412abde4a78c454e
François Voron <fvoron@gmail.com>
2024-08-20 14:37:32 +0200

    clients/web: allow to upload organization avatar

--- clients/apps/web/src/components/Settings/OrganizationAppearanceSettings.tsx
+++ clients/apps/web/src/components/Settings/OrganizationAppearanceSettings.tsx
@@ -1,6 +1,13 @@
 import { useUpdateOrganization } from '@/hooks/queries'
 import { setValidationErrors } from '@/utils/api/errors'
-import { Organization, ResponseError, ValidationError } from '@polar-sh/sdk'
+import { AddPhotoAlternateOutlined } from '@mui/icons-material'
+import {
+  FileServiceTypes,
+  Organization,
+  OrganizationAvatarFileRead,
+  ResponseError,
+  ValidationError,
+} from '@polar-sh/sdk'
 import Avatar from 'polarkit/components/ui/atoms/avatar'
 import Button from 'polarkit/components/ui/atoms/button'
 import Input from 'polarkit/components/ui/atoms/input'
@@ -12,8 +19,11 @@ import {
   FormItem,
   FormMessage,
 } from 'polarkit/components/ui/form'
-import React from 'react'
+import React, { useCallback } from 'react'
+import { FileRejection } from 'react-dropzone'
 import { useForm } from 'react-hook-form'
+import { twMerge } from 'tailwind-merge'
+import { FileObject, useFileUpload } from '../FileUpload'
 
 interface OrganizationAppearanceSettingsProps {
   organization: Organization
@@ -25,9 +35,43 @@ const OrganizationAppearanceSettings: React.FC<
   const form = useForm<{ name: string; avatar_url: string }>({
     defaultValues: organization,
   })
-  const { control, handleSubmit, watch, setError } = form
+  const { control, handleSubmit, watch, setError, setValue } = form
   const name = watch('name')
   const avatarURL = watch('avatar_url')
+
+  const onFilesUpdated = useCallback(
+    (files: FileObject<OrganizationAvatarFileRead>[]) => {
+      if (files.length === 0) {
+        return
+      }
+      const lastFile = files[files.length - 1]
+      setValue('avatar_url', lastFile.public_url)
+    },
+    [setValue],
+  )
+  const onFilesRejected = useCallback(
+    (rejections: FileRejection[]) => {
+      rejections.forEach((rejection) => {
+        setError('avatar_url', { message: rejection.errors[0].message })
+      })
+    },
+    [setError],
+  )
+  const { getRootProps, getInputProps, isDragActive } = useFileUpload({
+    organization: organization,
+    service: FileServiceTypes.ORGANIZATION_AVATAR,
+    accept: {
+      'image/jpeg': [],
+      'image/png': [],
+      'image/gif': [],
+      'image/webp': [],
+      'image/svg+xml': [],
+    },
+    maxSize: 1 * 1024 * 1024,
+    onFilesUpdated,
+    onFilesRejected,
+    initialFiles: [],
+  })
 
   const updateOrganization = useUpdateOrganization()
   const onSubmit = async (body: { name: string; avatar_url: string }) => {
@@ -52,7 +96,7 @@ const OrganizationAppearanceSettings: React.FC<
   return (
     <Form {...form}>
       <form
-        className="dark:divide-polar-700 flex w-full flex-col gap-2"
+        className="dark:divide-polar-700 flex w-full flex-col gap-y-6"
         onSubmit={handleSubmit(onSubmit)}
       >
         <FormField
@@ -76,17 +120,40 @@ const OrganizationAppearanceSettings: React.FC<
           control={control}
           name="avatar_url"
           render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between gap-4">
-              <FormControl>
-                <Input {...field} placeholder="Logo URL" />
-              </FormControl>
-              <Avatar
-                avatar_url={avatarURL}
-                name={name}
-                className="h-16 w-16"
-              />
-              <FormMessage />
-            </FormItem>
+            <div className="flex flex-row items-center gap-4">
+              <FormItem className="grow">
+                <FormControl>
+                  <Input {...field} placeholder="Logo URL" />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+              <div
+                {...getRootProps()}
+                className={twMerge(
+                  'group relative',
+                  isDragActive && 'opacity-50',
+                )}
+              >
+                <input {...getInputProps()} />
+                <Avatar
+                  avatar_url={avatarURL}
+                  name={name}
+                  className={twMerge(
+                    'h-16 w-16 group-hover:opacity-50',
+                    isDragActive && 'opacity-50',
+                  )}
+                />
+                <div
+                  className={twMerge(
+                    'absolute left-0 top-0 h-16 w-16 cursor-pointer items-center justify-center group-hover:flex',
+                    isDragActive ? 'flex' : 'hidden',
+                  )}
+                >
+                  <AddPhotoAlternateOutlined />
+                </div>
+              </div>
+            </div>
           )}
         />
         <div>

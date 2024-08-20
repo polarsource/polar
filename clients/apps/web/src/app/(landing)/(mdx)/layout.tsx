909f0a020b42bb17b54d1e5814a8a7207d53f24f
François Voron <fvoron@gmail.com>
2024-08-20 11:44:05 +0200

    clients/web: implement legal pages

--- clients/apps/web/src/app/(landing)/(mdx)/layout.tsx
+++ clients/apps/web/src/app/(landing)/(mdx)/layout.tsx
@@ -1,12 +1,7 @@
-import { Section } from '@/components/Landing/Section'
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // Create any shared layout or styles here
   return (
-    <Section className="md:py-0">
-      <div className="prose dark:prose-invert dark:prose-headings:leading-normal prose-headings:leading-normal prose-headings:mt-16 prose-headings:font-semibold prose-headings:text-black prose-h1:text-blue-500 prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-md dark:prose-headings:text-white dark:text-polar-200 max-w-4xl text-gray-800">
-        {children}
-      </div>
-    </Section>
+    <div className="px-4py-4 flex w-full max-w-[100vw] flex-col gap-x-16 gap-y-16 pb-24 pt-16 md:max-w-7xl md:flex-row md:items-start md:justify-between md:px-12 md:py-8 [&>_#toc]:top-28">
+      {children}
+    </div>
   )
 }
--- clients/apps/web/src/components/Documentation/TableOfContents.tsx
+++ clients/apps/web/src/components/Documentation/TableOfContents.tsx
@@ -12,7 +12,10 @@ export const TableOfContents = ({
   if (!items || items.length === 0) return null
 
   return (
-    <div className="flex w-full flex-shrink-0 flex-col md:sticky md:top-12 md:w-64">
+    <div
+      id="toc"
+      className="flex w-full flex-shrink-0 flex-col md:sticky md:top-12 md:w-64"
+    >
       <nav className="hidden w-full flex-col gap-y-4 md:flex">
         <h3 className="text-black dark:text-white">On this page</h3>
         <ul className="flex flex-col gap-y-2.5 text-sm">

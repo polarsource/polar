d7afa85ea0fcefe94d7a2ca3b28b884429db9288
Birk Jernstrom <birkjernstrom@gmail.com>
2024-08-22 09:54:38 +0200

    alembic: Merge heads

---
+++ server/migrations/versions/2024-08-22-0954_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 225d352dfa66
+Revises: 806a7aac6884, 4ef14889c3d9
+Create Date: 2024-08-22 09:54:13.655024
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# Polar Custom Imports
+
+# revision identifiers, used by Alembic.
+revision = "225d352dfa66"
+down_revision = ("806a7aac6884", "4ef14889c3d9")
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

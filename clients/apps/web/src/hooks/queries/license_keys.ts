b886c56aa2136aa4c8e3247a32c0b081a533444c
Birk Jernstrom <birkjernstrom@gmail.com>
2024-08-23 09:10:01 +0200

    clients: Remove unused hook

--- clients/apps/web/src/hooks/queries/license_keys.ts
+++ clients/apps/web/src/hooks/queries/license_keys.ts
@@ -3,17 +3,6 @@ import { useQuery } from '@tanstack/react-query'
 import { api } from '@/utils/api'
 import { defaultRetry } from './retry'
 
-export const useLicenseKeys = (organizationId: string, benefitId: string) =>
-  useQuery({
-    queryKey: ['user', 'license_keys', organizationId, benefitId],
-    queryFn: () =>
-      api.users.listLicenseKeys({
-        organizationId: organizationId,
-        benefitId: benefitId,
-      }),
-    retry: defaultRetry,
-  })
-
 export const useLicenseKey = (licenseKeyId: string) =>
   useQuery({
     queryKey: ['user', 'license_key', licenseKeyId],

5c2a3f192602ada682ec6f0c1e4af37b82b4e690
Birk Jernstrom <birkjernstrom@gmail.com>
2024-09-04 00:39:21 +0200

    website: Updated funding section copy

--- clients/apps/web/src/components/Landing/Monetization.tsx
+++ clients/apps/web/src/components/Landing/Monetization.tsx
@@ -14,33 +14,45 @@ export const Monetization = () => {
       <div className="flex flex-col gap-y-24">
         <div className="relative flex flex-col items-center gap-y-2 text-center md:gap-y-4">
           <h2 className="text-3xl leading-snug md:text-5xl">
-            Tap into additional monetization tools
+            Offer Community Funding & Rewards
           </h2>
-          <h3 className="dark:text-polar-400 text-lg leading-snug text-gray-400 md:text-2xl">
-            With vertically integrated GitHub support
-          </h3>
         </div>
 
         <div className="grid grid-cols-1 gap-x-16 gap-y-16 md:grid-cols-3 md:gap-y-24">
           <div className="flex flex-col justify-between gap-y-12">
-            <picture>
-              <source
-                media="(prefers-color-scheme: dark)"
-                srcSet={`/assets/landing/fund_dark.svg`}
-              />
-              <img
-                className="dark:border-polar-700 rounded-2xl border border-gray-100 md:mt-4"
-                srcSet={`/assets/landing/fund_dark.svg`}
-                alt="Polar crowdfunding badge embedded on a GitHub issue"
-              />
-            </picture>
+            <div className="dark:bg-polar-950 dark:border-polar-700 flex h-full w-full flex-col gap-y-6 rounded-2xl border p-6 text-sm">
+              <div className="flex flex-row items-center gap-x-1.5">
+                <div className="h-2.5 w-2.5 rounded-full bg-red-500" />
+                <div className="h-2.5 w-2.5 rounded-full bg-yellow-500" />
+                <div className="h-2.5 w-2.5 rounded-full bg-green-500" />
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <span className="dark:text-polar-500 font-mono">
+                  // FUNDING.yaml
+                </span>
+                <pre>
+                  <code className="language-yaml">
+                    polar:{' '}
+                    <TypewriterText
+                      delay={1}
+                      texts={[
+                        'capawesome-team',
+                        'serenityos',
+                        'HDInnovations',
+                        'emilwidlund',
+                      ]}
+                    />
+                  </code>
+                </pre>
+              </div>
+            </div>
             <div className="flex flex-col gap-y-2">
               <h3 className="text-lg font-medium leading-snug md:text-xl">
-                Issue Funding & Rewards
+                Official GitHub Support
               </h3>
               <p className="dark:text-polar-200 h-full leading-relaxed text-gray-500">
-                Crowdfunded backlog or community bounties with support to split
-                funds with contributors.
+                Your Polar page can be displayed as an official funding option
+                across your GitHub repositories.
               </p>
             </div>
           </div>
@@ -95,46 +107,29 @@ export const Monetization = () => {
                 Donations
               </h3>
               <p className="dark:text-polar-200 h-full leading-relaxed text-gray-500">
-                Make it easy for your community to support your work with
-                Donations.
+                Get one-time donations of support from your community with ease.
               </p>
             </div>
           </div>
-
           <div className="flex flex-col justify-between gap-y-12">
-            <div className="dark:bg-polar-950 dark:border-polar-700 flex h-full w-full flex-col gap-y-6 rounded-2xl border p-6 text-sm">
-              <div className="flex flex-row items-center gap-x-1.5">
-                <div className="h-2.5 w-2.5 rounded-full bg-red-500" />
-                <div className="h-2.5 w-2.5 rounded-full bg-yellow-500" />
-                <div className="h-2.5 w-2.5 rounded-full bg-green-500" />
-              </div>
-              <div className="flex flex-col gap-y-2">
-                <span className="dark:text-polar-500 font-mono">
-                  // FUNDING.yaml
-                </span>
-                <pre>
-                  <code className="language-yaml">
-                    polar:{' '}
-                    <TypewriterText
-                      delay={1}
-                      texts={[
-                        'capawesome-team',
-                        'serenityos',
-                        'HDInnovations',
-                        'emilwidlund',
-                      ]}
-                    />
-                  </code>
-                </pre>
-              </div>
-            </div>
+            <picture>
+              <source
+                media="(prefers-color-scheme: dark)"
+                srcSet={`/assets/landing/fund_dark.svg`}
+              />
+              <img
+                className="dark:border-polar-700 rounded-2xl border border-gray-100 md:mt-4"
+                srcSet={`/assets/landing/fund_dark.svg`}
+                alt="Polar crowdfunding badge embedded on a GitHub issue"
+              />
+            </picture>
             <div className="flex flex-col gap-y-2">
               <h3 className="text-lg font-medium leading-snug md:text-xl">
-                Official GitHub Funding Option
+                Issue Funding & Rewards
               </h3>
               <p className="dark:text-polar-200 h-full leading-relaxed text-gray-500">
-                Promote your Polar page on GitHub with a badge that links to
-                your funding page.
+                Turn issues into a crowdfunded backlog and share the funding
+                with your contributors.
               </p>
             </div>
           </div>

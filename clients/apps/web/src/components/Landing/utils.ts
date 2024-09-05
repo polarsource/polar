4808b76566f0bc07dab8b8d995f4893df336400c
Birk Jernstrom <birkjernstrom@gmail.com>
2024-09-05 18:09:20 +0200

    website: Clarify example tier

--- clients/apps/web/src/components/Landing/utils.ts
+++ clients/apps/web/src/components/Landing/utils.ts
@@ -358,3 +358,67 @@ export const MOCKED_SUBSCRIPTIONS: (Partial<Product> & {
     created_at: new Date().toDateString(),
   },
 ]
+
+export const MOCKED_WEBSITE_SUBSCRIPTION: Partial<Product> & {
+  type: SubscriptionTierType
+} = {
+  name: 'Just an Example',
+  type: 'individual',
+  description:
+    'Polar has no monthly fees. This tier is only and example of what you could offer your audience in minutes using Polar.',
+  prices: [
+    {
+      id: '123',
+      created_at: new Date().toDateString(),
+      modified_at: null,
+      price_amount: 900,
+      price_currency: 'usd',
+      type: 'recurring',
+      recurring_interval: 'month',
+      is_archived: false,
+    },
+  ],
+  benefits: [
+    {
+      id: '456',
+      description: 'Private GitHub repository',
+      type: 'github_repository',
+      created_at: new Date().toDateString(),
+      modified_at: null,
+      selectable: false,
+      deletable: false,
+      organization_id: '123',
+    },
+    {
+      id: '789',
+      description: 'Discord Support Channel',
+      type: 'discord',
+      created_at: new Date().toDateString(),
+      modified_at: null,
+      selectable: false,
+      deletable: false,
+      organization_id: '123',
+    },
+    {
+      id: '852',
+      description: 'License Key',
+      type: 'license_keys',
+      created_at: new Date().toDateString(),
+      modified_at: null,
+      selectable: false,
+      deletable: false,
+      organization_id: '123',
+    },
+    {
+      id: '123',
+      description: 'Binary Downloads',
+      type: 'downloadables',
+      created_at: new Date().toDateString(),
+      modified_at: null,
+      selectable: false,
+      deletable: false,
+      organization_id: '123',
+    },
+  ],
+  created_at: new Date().toDateString(),
+}

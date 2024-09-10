a02c0b0148a37b88eb47f39df176fb87e8a6222b
Emil Widlund <hello@emilwidlund.com>
2024-09-10 07:54:28 +0200

    clients/customize: always show editing capability

--- clients/apps/web/src/app/dashboard/[organization]/customize/ClientLayout.tsx
+++ clients/apps/web/src/app/dashboard/[organization]/customize/ClientLayout.tsx
@@ -20,7 +20,7 @@ export default function ClientLayout({
         (search.get('mode') as CustomizationContextMode) ?? undefined
       }
     >
-      <div className="ml-8 flex h-full flex-grow flex-row gap-x-8">
+      <div className="ml-4 flex h-full flex-grow flex-row gap-x-4">
         {children}
         <CustomizationSidebar />
       </div>

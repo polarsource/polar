a0726ef5afea98730bc9f48ac3a160a7f8b4aed3
François Voron <fvoron@gmail.com>
2024-09-18 14:59:59 +0200

    clients/web: add banner in sandbox mode

---
+++ clients/apps/web/src/components/Sandbox/SandboxBanner.tsx
@@ -0,0 +1,23 @@
+import { CONFIG } from '@/utils/config'
+
+const SandboxBanner = () => {
+  if (!CONFIG.IS_SANDBOX) {
+    return null
+  }
+  return (
+    <div className="sticky top-0 z-50 flex flex-row items-center justify-between bg-yellow-100 px-8 py-2 text-sm text-yellow-500 dark:bg-yellow-950">
+      <div></div>
+      <div className="hidden md:block">
+        Changes you make here don&apos;t affect your live account • Payments are
+        not processed
+      </div>
+      <div>
+        <a href="https://polar.sh/start" className="font-bold hover:opacity-50">
+          Exit sandbox
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default SandboxBanner

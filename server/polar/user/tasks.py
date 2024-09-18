95f99ec228f11d5e1cfac54ae62e92fc3b5934b3
Emil Widlund <hello@emilwidlund.com>
2024-09-18 15:11:03 +0200

    server/user: keep user takss

--- server/polar/user/tasks.py
+++ server/polar/user/tasks.py
@@ -1,6 +1,9 @@
 import uuid
 
 from polar.exceptions import PolarTaskError
+from polar.worker import AsyncSessionMaker, JobContext, PolarWorkerContext, task
+
+from .service.user import user as user_service
 
 
 class UserTaskError(PolarTaskError): ...
@@ -11,3 +14,16 @@ class UserDoesNotExist(UserTaskError):
         self.user_id = user_id
         message = f"The user with id {user_id} does not exist."
         super().__init__(message)
+
+
+@task("user.on_after_signup")
+async def user_on_after_signup(
+    ctx: JobContext,
+    user_id: uuid.UUID,
+    polar_context: PolarWorkerContext,
+) -> None:
+    async with AsyncSessionMaker(ctx) as session:
+        user = await user_service.get(session, user_id)
+
+        if user is None:
+            raise UserDoesNotExist(user_id)

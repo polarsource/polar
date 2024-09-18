bf7871e2de237efe8f76cd927157dd34463ea298
Emil Widlund <hello@emilwidlund.com>
2024-09-18 23:42:48 +0200

    clients/subs: cleanup subscriptions view

--- clients/apps/web/src/components/Purchases/PurchasesSidebar.tsx
+++ clients/apps/web/src/components/Purchases/PurchasesSidebar.tsx
@@ -4,11 +4,9 @@ import { useUserOrders, useUserSubscriptions } from '@/hooks/queries'
 import { ProductPriceType } from '@polar-sh/sdk'
 import Link, { LinkProps } from 'next/link'
 import { usePathname } from 'next/navigation'
-import Input from 'polarkit/components/ui/atoms/input'
 import ShadowBox from 'polarkit/components/ui/atoms/shadowbox'
-import { PropsWithChildren, useCallback, useContext, useMemo } from 'react'
+import { PropsWithChildren, useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
-import { PurchasesQueryParametersContext } from './PurchasesQueryParametersContext'
 
 const PurchaseLink = ({ ...props }: PropsWithChildren<LinkProps>) => {
   const pathname = usePathname()
@@ -32,12 +30,6 @@ const PurchaseLink = ({ ...props }: PropsWithChildren<LinkProps>) => {
 const PurchaseSidebar: React.FC<React.PropsWithChildren<{}>> = ({
   children,
 }) => {
-  const [purchaseParameters, setPurchaseParameters] = useContext(
-    PurchasesQueryParametersContext,
-  )
-
-  const pathname = usePathname()
-
   const { data: orders } = useUserOrders({
     limit: 1,
     productPriceType: ProductPriceType.ONE_TIME,
@@ -46,21 +38,6 @@ const PurchaseSidebar: React.FC<React.PropsWithChildren<{}>> = ({
     limit: 1,
     active: true,
   })
-
-  const handleSearch = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setPurchaseParameters((prev) => ({
-        ...prev,
-        query: e.target.value,
-      }))
-    },
-    [setPurchaseParameters],
-  )
-
-  const renderSearch = useMemo(
-    () => pathname.startsWith('/purchases'),
-    [pathname],
-  )
 
   return (
     <ShadowBox className="flex w-full flex-shrink-0 flex-col gap-6">
@@ -89,19 +66,6 @@ const PurchaseSidebar: React.FC<React.PropsWithChildren<{}>> = ({
           </PurchaseLink>
         </div>
       </div>
-      {renderSearch ? (
-        <div className="flex flex-col gap-y-3">
-          <div className="w-full">
-            <Input
-              placeholder="Search products or creators"
-              onChange={handleSearch}
-              value={purchaseParameters.query}
-            />
-          </div>
-        </div>
-      ) : (
-        <></>
-      )}
       <>{children}</>
     </ShadowBox>
   )

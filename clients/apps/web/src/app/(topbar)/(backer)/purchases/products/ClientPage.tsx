f4e083252a993cf411bdac7d646ec5eaaf5e7288
Emil Widlund <hello@emilwidlund.com>
2024-09-19 10:45:29 +0200

    clients/purchases: add better component for displaying purchases

--- clients/apps/web/src/app/(topbar)/(backer)/purchases/products/ClientPage.tsx
+++ clients/apps/web/src/app/(topbar)/(backer)/purchases/products/ClientPage.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import LogoIcon from '@/components/Brand/LogoIcon'
 import Pagination from '@/components/Pagination/Pagination'
-import { ProductCard } from '@/components/Products/ProductCard'
 import { PurchasesQueryParametersContext } from '@/components/Purchases/PurchasesQueryParametersContext'
 import PurchaseSidebar from '@/components/Purchases/PurchasesSidebar'
-import { useUserOrders } from '@/hooks/queries'
+import AmountLabel from '@/components/Shared/AmountLabel'
+import { useOrganization, useUserOrders } from '@/hooks/queries'
 import { DiamondOutlined } from '@mui/icons-material'
 import { ProductPriceType, UserOrder } from '@polar-sh/sdk'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import Button from 'polarkit/components/ui/atoms/button'
+import ShadowBox from 'polarkit/components/ui/atoms/shadowbox'
 import { useCallback, useContext } from 'react'
 
 export default function ClientPage() {
@@ -50,12 +53,16 @@ export default function ClientPage() {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col">
-          <div className="grid h-full grid-cols-1 gap-6 md:grid-cols-3">
-            {orders?.items.map((order) => (
+        <div className="flex w-full max-w-2xl flex-col gap-y-6">
+          {orders?.items.map((order) => (
+            <Link
+              key={order.id}
+              className="flex w-full flex-row items-center justify-between"
+              href={`/purchases/products/${order.id}`}
+            >
               <OrderItem key={order.id} order={order} />
-            ))}
-          </div>
+            </Link>
+          ))}
           <Pagination
             currentPage={purchaseParameters.page}
             totalCount={orders?.pagination.total_count || 0}
@@ -70,14 +77,77 @@ export default function ClientPage() {
 }
 
 const OrderItem = ({ order }: { order: UserOrder }) => {
+  const { data: organization } = useOrganization(order.product.organization_id)
+
+  if (!organization) {
+    return null
+  }
+
   return (
-    <Link href={`/purchases/products/${order.id}`}>
-      <ProductCard
-        key={order.id}
-        product={order.product}
-        price={order.product_price}
-        showOrganization
-      />
-    </Link>
+    <ShadowBox className="flex w-full flex-col gap-y-6">
+      <div className="flex flex-row items-start justify-between">
+        <div className="flex flex-row items-center gap-x-4">
+          {order.product.medias.length > 0 ? (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              className="dark:bg-polar-950 aspect-square h-16 rounded-2xl bg-gray-100 object-cover"
+              alt={order.product.medias[0].name}
+              width={600}
+              height={600}
+              src={order.product.medias[0].public_url}
+            />
+          ) : (
+            <div className="dark:from-polar-900 dark:via-polar-800 dark:to-polar-900 flex aspect-square h-16 flex-col items-center justify-center rounded-2xl bg-gradient-to-tr from-white via-blue-50 to-white">
+              <div className="flex flex-col items-center justify-center text-4xl text-blue-500 dark:text-white">
+                <LogoIcon className="dark:text-polar-600 h-12 w-12 text-white/50" />
+              </div>
+            </div>
+          )}
+          <div className="flex flex-col">
+            <h3 className="truncate text-2xl">{order.product.name}</h3>
+            <p className="dark:text-polar-500 text-sm text-gray-500">
+              {organization.name}
+            </p>
+          </div>
+        </div>
+        <Link href={`/purchases/products/${order.id}`}>
+          <Button size="sm">View Purchase</Button>
+        </Link>
+      </div>
+      <div className="dark:divide-polar-700 flex flex-col divide-y divide-gray-100 text-sm">
+        <div className="flex flex-row items-center justify-between py-2">
+          <span>Amount</span>
+          {order.amount && order.currency ? (
+            <AmountLabel amount={order.amount} currency={order.currency} />
+          ) : (
+            'Free'
+          )}
+        </div>
+        {order.created_at && (
+          <div className="flex flex-row items-center justify-between py-3">
+            <span>Purchase Date</span>
+            <span>
+              {new Date(order.created_at).toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </span>
+          </div>
+        )}
+        {order.product.benefits.length > 0 && (
+          <div className="flex flex-row items-center justify-between py-3">
+            <span>Benefits</span>
+            <span>
+              <Link href={`/purchases/products/${order.id}`}>
+                <Button size="sm" variant="secondary">
+                  View Benefits
+                </Button>
+              </Link>
+            </span>
+          </div>
+        )}
+      </div>
+    </ShadowBox>
   )
 }

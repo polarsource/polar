929d0066518f93207dc301df106344d1be4970f5
Emil Widlund <hello@emilwidlund.com>
2024-09-23 15:22:48 +0200

    clients/checkout: improve checkout

---
+++ clients/apps/web/src/components/Checkout/CheckoutInfo.tsx
@@ -0,0 +1,49 @@
+import { markdownOpts } from '@/components/Feed/Markdown/markdown'
+import { Slideshow } from '@/components/Products/Slideshow'
+import { Organization, Product } from '@polar-sh/sdk'
+import Markdown from 'markdown-to-jsx'
+import Avatar from 'polarkit/components/ui/atoms/avatar'
+import { CheckoutCard } from './CheckoutCard'
+
+export interface CheckoutInfoProps {
+  organization: Organization
+  product: Product
+}
+
+export const CheckoutInfo = ({ organization, product }: CheckoutInfoProps) => {
+  return (
+    <div className="flex w-1/2 flex-col gap-y-12 p-20">
+      <Avatar
+        className="md:h-16 md:w-16"
+        avatar_url={organization.avatar_url}
+        name={organization.name}
+      />
+      <h1 className="text-3xl">{product.name}</h1>
+      {product.medias.length > 0 && (
+        <Slideshow
+          images={product.medias.map(({ public_url }) => public_url)}
+        />
+      )}
+      {product.description ? (
+        <div className="prose dark:prose-invert prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-h4:text-lg prose-h5:text-md prose-h6:text-sm dark:prose-headings:text-polar-50 dark:text-polar-300 max-w-4xl text-gray-800">
+          <Markdown
+            options={{
+              ...markdownOpts,
+              overrides: {
+                ...markdownOpts.overrides,
+                a: (props) => (
+                  <a {...props} rel="noopener noreferrer nofollow" />
+                ),
+              },
+            }}
+          >
+            {product.description}
+          </Markdown>
+        </div>
+      ) : (
+        <></>
+      )}
+      <CheckoutCard organization={organization} product={product} />
+    </div>
+  )
+}

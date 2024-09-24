2896120b658149fef212bdeff13016a01737a88f
Birk Jernstrom <birkjernstrom@gmail.com>
2024-09-24 17:12:07 +0200

    clients: Fix gready redirect of /purchases/subscriptions
    Avoid overwriting a real route by moving the redirect to the storefront
    /:slug/subscriptions app directory

--- clients/apps/web/next.config.mjs
+++ clients/apps/web/next.config.mjs
@@ -194,15 +194,6 @@ const nextConfig = {
         permanent: true,
       },
 
-      // Redirect /:slug/subscriptions to /:slug
-      // Now we surface subscriptions on landing page vs. below subpage
-      {
-        source: '/:slug/subscriptions',
-        destination: '/:slug',
-        permanent: true,
-      },
-
-
       // Redirect /docs/overview/:path to /docs/:path
       {
         source: '/docs/overview/:path*',

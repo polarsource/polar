a772ed35aa2cf15e69a8235caf907dec1d590250
Emil Widlund <hello@emilwidlund.com>
2024-10-10 19:57:02 +0200

    clients/products: implement product context view

--- clients/apps/web/src/components/Checkout/CheckoutInfo.tsx
+++ clients/apps/web/src/components/Checkout/CheckoutInfo.tsx
@@ -3,6 +3,7 @@ import {
   CheckoutUpdatePublic,
   Organization,
 } from '@polar-sh/sdk'
+import { twMerge } from 'tailwind-merge'
 import { CheckoutCard } from './CheckoutCard'
 import CheckoutProductInfo from './CheckoutProductInfo'
 
@@ -10,16 +11,20 @@ export interface CheckoutInfoProps {
   organization: Organization
   checkout: CheckoutPublic
   onCheckoutUpdate?: (body: CheckoutUpdatePublic) => Promise<CheckoutPublic>
+  className?: string
 }
 
 export const CheckoutInfo = ({
   organization,
   checkout,
   onCheckoutUpdate,
+  className,
 }: CheckoutInfoProps) => {
   const { product } = checkout
   return (
-    <div className="flex flex-col gap-y-12 md:w-1/2 md:p-20">
+    <div
+      className={twMerge('flex flex-col gap-y-12 md:w-1/2 md:p-20', className)}
+    >
       <CheckoutProductInfo organization={organization} product={product} />
       <CheckoutCard checkout={checkout} onCheckoutUpdate={onCheckoutUpdate} />
     </div>

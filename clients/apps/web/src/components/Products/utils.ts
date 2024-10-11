7df903d529e5a48f3ea27ddcb255c59407cbe539
Emil Widlund <hello@emilwidlund.com>
2024-10-11 10:19:00 +0200

    clients/products: add liveview to product modification

---
+++ clients/apps/web/src/components/Products/utils.ts
@@ -0,0 +1,56 @@
+import {
+  BenefitPublicInner,
+  Product,
+  ProductCreate,
+  ProductPrice,
+  ProductUpdate,
+} from '@polar-sh/sdk'
+import { ProductFullMediasMixin } from './ProductForm/ProductForm'
+
+export const productUpdateToProduct = (
+  productUpdate: ProductUpdate & ProductFullMediasMixin,
+  benefits: BenefitPublicInner[],
+  product: Product,
+): Product => {
+  const { full_medias, ...productUpdateRest } = productUpdate
+  return {
+    ...product,
+    name: productUpdateRest.name ?? '',
+    description: productUpdateRest.description ?? '',
+    prices:
+      productUpdateRest.prices?.filter(
+        (price): price is ProductPrice => 'type' in price,
+      ) ?? [],
+    medias: full_medias,
+    benefits,
+  }
+}
+
+export const productCreateToProduct = (
+  organizationId: string,
+  productCreate: ProductCreate & ProductFullMediasMixin,
+  benefits: BenefitPublicInner[],
+): Product => {
+  const { full_medias, ...productCreateRest } = productCreate
+  return {
+    id: organizationId + productCreate.name,
+    ...productCreate,
+    organization_id: organizationId,
+    name: productCreateRest.name ?? '',
+    description: productCreateRest.description ?? '',
+    prices:
+      productCreate.prices
+        ?.map((price) => ({
+          ...price,
+          type: price.type ?? 'one_time',
+        }))
+        .filter((price): price is ProductPrice => 'type' in price) ?? [],
+    medias: full_medias,
+    benefits,
+    created_at: new Date().toDateString(),
+    modified_at: new Date().toDateString(),
+    is_recurring:
+      productCreate.prices.some((price) => price.type === 'recurring') ?? false,
+    is_archived: false,
+  }
+}

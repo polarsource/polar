059101e12cda6a18c392490c2b55c698d9e7add9
Emil Widlund <hello@emilwidlund.com>
2024-10-11 15:05:42 +0200

    clients/checkout: add metadata

--- clients/apps/web/src/app/checkout/[clientSecret]/page.tsx
+++ clients/apps/web/src/app/checkout/[clientSecret]/page.tsx
@@ -3,7 +3,48 @@ import { getServerSideAPI } from '@/utils/api/serverside'
 import { getCheckoutByClientSecret } from '@/utils/checkout'
 import { getOrganizationById } from '@/utils/organization'
 import { CheckoutStatus } from '@polar-sh/sdk'
-import { redirect } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
+
+export async function generateMetadata({
+  params: { clientSecret },
+}: {
+  params: { clientSecret: string }
+}) {
+  const api = getServerSideAPI()
+
+  const checkout = await getCheckoutByClientSecret(api, clientSecret)
+
+  if (!checkout) {
+    notFound()
+  }
+
+  return {
+    title: `Checkout | ${checkout.product.name}`, // " | Polar is added by the template"
+    openGraph: {
+      title: `Checkout | ${checkout.product.name}`,
+      siteName: 'Polar',
+      images: [
+        {
+          url: checkout.product.medias[0]?.public_url,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    twitter: {
+      images: [
+        {
+          url: checkout.product.medias[0]?.public_url,
+          width: 1200,
+          height: 630,
+          alt: `${checkout.product.name}`,
+        },
+      ],
+      card: 'summary_large_image',
+      title: `Checkout | ${checkout.product.name}`,
+    },
+  }
+}
 
 export default async function Page({
   params: { clientSecret },

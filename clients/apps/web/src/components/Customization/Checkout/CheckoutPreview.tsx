db112aeff7cb056a1cf4a62ea972dcfae04cf3a2
Emil Widlund <hello@emilwidlund.com>
2024-10-15 14:31:13 +0200

    clients/storefront: remove topbar from disabled storefront

--- clients/apps/web/src/components/Customization/Checkout/CheckoutPreview.tsx
+++ clients/apps/web/src/components/Customization/Checkout/CheckoutPreview.tsx
@@ -23,16 +23,18 @@ export const CheckoutPreview = ({ product }: CheckoutPreviewProps) => {
   return (
     <ShadowBox className="dark:bg-polar-950 flex h-full w-full flex-col items-center overflow-y-auto bg-gray-50">
       <div className="flex w-full max-w-7xl flex-col items-center gap-y-12">
-        <div className="relative flex w-full flex-row items-center justify-end gap-x-6">
-          <BrandingMenu
-            className="absolute left-1/2 -translate-x-1/2"
-            size={50}
-          />
-
-          <TopbarRight authenticatedUser={currentUser} />
-        </div>
         {org.profile_settings?.enabled && (
-          <StorefrontHeader organization={org} />
+          <>
+            <div className="relative flex w-full flex-row items-center justify-end gap-x-6">
+              <BrandingMenu
+                className="absolute left-1/2 -translate-x-1/2"
+                size={50}
+              />
+
+              <TopbarRight authenticatedUser={currentUser} />
+            </div>
+            <StorefrontHeader organization={org} />
+          </>
         )}
         <Checkout
           organization={org}

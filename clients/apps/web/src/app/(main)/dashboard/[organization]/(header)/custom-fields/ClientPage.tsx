ec3fe17080a07b0b3220b9d2da79f4de9e7799dd
François Voron <fvoron@gmail.com>
2024-10-30 15:50:58 +0100

    clients/web: implement create/update forms for CustomField

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/custom-fields/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/custom-fields/ClientPage.tsx
@@ -1,23 +1,34 @@
 'use client'
 
 import CopyToClipboardButton from '@/components/CopyToClipboardButton/CopyToClipboardButton'
+import CreateCustomFieldModalContent from '@/components/CustomFields/CreateCustomFieldModalContent'
 import CustomFieldTypeLabel from '@/components/CustomFields/CustomFieldTypeLabel'
+import UpdateCustomFieldModalContent from '@/components/CustomFields/UpdateCustomFieldModalContent'
 import { DashboardBody } from '@/components/Layout/DashboardLayout'
-import { useCustomFields } from '@/hooks/queries'
+import { InlineModal } from '@/components/Modal/InlineModal'
+import { useCustomFields, useDeleteCustomField } from '@/hooks/queries'
 import {
   DataTablePaginationState,
   DataTableSortingState,
   getAPIParams,
   serializeSearchParams,
 } from '@/utils/datatable'
+import { AddOutlined, MoreVertOutlined } from '@mui/icons-material'
 import { CustomField, CustomFieldType, Organization } from '@polar-sh/sdk'
 import { useRouter } from 'next/navigation'
+import Button from 'polarkit/components/ui/atoms/button'
 import {
   DataTable,
   DataTableColumnDef,
   DataTableColumnHeader,
 } from 'polarkit/components/ui/atoms/datatable'
-import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from 'polarkit/components/ui/dropdown-menu'
+import React, { useCallback, useState } from 'react'
 
 interface ClientPageProps {
   organization: Organization
@@ -32,6 +43,8 @@ const ClientPage: React.FC<ClientPageProps> = ({
   sorting,
   type,
 }) => {
+  const router = useRouter()
+
   const getSearchParams = (
     pagination: DataTablePaginationState,
     sorting: DataTableSortingState,
@@ -45,8 +58,6 @@ const ClientPage: React.FC<ClientPageProps> = ({
 
     return params
   }
-
-  const router = useRouter()
 
   const setPagination = (
     updaterOrValue:
@@ -131,11 +142,62 @@ const ClientPage: React.FC<ClientPageProps> = ({
         return <CustomFieldTypeLabel type={getValue() as CustomFieldType} />
       },
     },
+    {
+      id: 'actions',
+      enableSorting: false,
+      cell: ({ row: { original: customField } }) => (
+        <div className="flex flex-row justify-end">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="focus:outline-none" asChild>
+              <Button
+                className={
+                  'border-none bg-transparent text-[16px] opacity-50 transition-opacity hover:opacity-100 dark:bg-transparent'
+                }
+                size="icon"
+                variant="secondary"
+              >
+                <MoreVertOutlined fontSize="inherit" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align="end"
+              className="dark:bg-polar-800 bg-gray-50 shadow-lg"
+            >
+              <DropdownMenuItem
+                onClick={() => onCustomFieldSelected(customField)}
+              >
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => deleteCustomField.mutate(customField)}
+              >
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      ),
+    },
   ]
+
+  const [showNewModal, setShowNewModal] = useState(false)
+  const [showUpdateModal, setShowUpdateModal] = useState(false)
+  const [selectedCustomField, setSelectedCustomField] = useState<CustomField>()
+  const onCustomFieldSelected = useCallback((customField: CustomField) => {
+    setSelectedCustomField(customField)
+    setShowUpdateModal(true)
+  }, [])
+  const deleteCustomField = useDeleteCustomField()
 
   return (
     <DashboardBody>
       <div className="flex flex-col gap-8">
+        <div className="flex items-center justify-end gap-2">
+          <Button type="button" onClick={() => setShowNewModal(true)}>
+            <AddOutlined className="h-4 w-4" />
+            <span>New Custom Field</span>
+          </Button>
+        </div>
         {customFields && pageCount !== undefined && (
           <DataTable
             columns={columns}
@@ -149,6 +211,32 @@ const ClientPage: React.FC<ClientPageProps> = ({
           />
         )}
       </div>
+      <InlineModal
+        isShown={showNewModal}
+        hide={() => setShowNewModal(false)}
+        modalContent={
+          <CreateCustomFieldModalContent
+            organization={organization}
+            onCustomFieldCreated={() => setShowNewModal(false)}
+            hideModal={() => setShowNewModal(false)}
+          />
+        }
+      />
+      <InlineModal
+        isShown={showUpdateModal}
+        hide={() => setShowUpdateModal(false)}
+        modalContent={
+          selectedCustomField ? (
+            <UpdateCustomFieldModalContent
+              customField={selectedCustomField}
+              onCustomFieldUpdated={() => setShowUpdateModal(false)}
+              hideModal={() => setShowUpdateModal(false)}
+            />
+          ) : (
+            <></>
+          )
+        }
+      />
     </DashboardBody>
   )
 }
---
+++ clients/apps/web/src/components/CustomFields/CreateCustomFieldModalContent.tsx
@@ -0,0 +1,108 @@
+import { useCreateCustomField } from '@/hooks/queries'
+import { setValidationErrors } from '@/utils/api/errors'
+import {
+  CustomField,
+  CustomFieldCreate,
+  CustomFieldType,
+  Organization,
+  ResponseError,
+  ValidationError,
+} from '@polar-sh/sdk'
+import Button from 'polarkit/components/ui/atoms/button'
+import { Form } from 'polarkit/components/ui/form'
+import { useCallback, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import CustomFieldForm from './CustomFieldForm'
+
+interface CreateCustomFieldModalContentProps {
+  organization: Organization
+  onCustomFieldCreated: (customField: CustomField) => void
+  hideModal: () => void
+}
+
+const CreateCustomFieldModalContent = ({
+  organization,
+  onCustomFieldCreated,
+  hideModal,
+}: CreateCustomFieldModalContentProps) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const createCustomField = useCreateCustomField(organization.id)
+
+  const form = useForm<CustomFieldCreate>({
+    defaultValues: {
+      organization_id: organization.id,
+      type: CustomFieldType.TEXT,
+      properties: {},
+    },
+  })
+
+  const {
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = form
+
+  const onSubmit = useCallback(
+    async (customFieldCreate: CustomFieldCreate) => {
+      try {
+        setIsLoading(true)
+        const customField =
+          await createCustomField.mutateAsync(customFieldCreate)
+        onCustomFieldCreated(customField)
+      } catch (e) {
+        if (e instanceof ResponseError) {
+          const body = await e.response.json()
+          if (e.response.status === 422) {
+            const validationErrors = body['detail'] as ValidationError[]
+            setValidationErrors(validationErrors, setError, 1)
+          }
+        }
+      } finally {
+        setIsLoading(false)
+      }
+    },
+    [createCustomField, onCustomFieldCreated, setError],
+  )
+
+  return (
+    <div className="flex flex-col gap-y-6 overflow-y-auto px-8 py-10">
+      <div>
+        <h2 className="text-lg">Create Custom Field</h2>
+        <p className="dark:text-polar-500 mt-2 text-sm text-gray-500">
+          Custom Fields allow you to ask additional information from your
+          customers at checkout, and will be available for use in all products
+          of your organization.
+        </p>
+      </div>
+      <div className="flex flex-col gap-y-6">
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <CustomFieldForm update={false} />
+            {errors.root && (
+              <p className="text-destructive-foreground text-sm">
+                {errors.root.message}
+              </p>
+            )}
+            <div className="mt-4 flex flex-row items-center gap-x-4">
+              <Button className="self-start" type="submit" loading={isLoading}>
+                Create
+              </Button>
+              <Button
+                variant="ghost"
+                className="self-start"
+                onClick={hideModal}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default CreateCustomFieldModalContent

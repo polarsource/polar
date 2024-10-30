ec891fbdc2b1e270c138e5e125c868f17db39b41
Emil Widlund <hello@emilwidlund.com>
2024-10-30 17:28:34 +0100

    blog tweaks

--- clients/apps/web/src/app/(main)/(landing)/(mdx)/blog/page.tsx
+++ clients/apps/web/src/app/(main)/(landing)/(mdx)/blog/page.tsx
@@ -1,12 +1,8 @@
 import { firstImageUrlFromMarkdown } from '@/utils/markdown'
 import Image from 'next/image'
 import Link from 'next/link'
-import {
-  Card,
-  CardContent,
-  CardHeader,
-} from 'polarkit/components/ui/atoms/card'
-import { CardFooter } from 'polarkit/components/ui/card'
+import path from 'path'
+import { Card, CardContent } from 'polarkit/components/ui/atoms/card'
 import { getBlogPosts } from './utils'
 
 export default async function BlogPage() {
@@ -15,9 +11,8 @@ export default async function BlogPage() {
   return (
     <div className="flex w-full flex-col gap-y-16">
       <h1 className="text-2xl md:text-5xl">Blog</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8">
-        {posts.map((post) => {
-          console.log(post.content)
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
+        {posts.map(async (post) => {
           const firstImageUrl = firstImageUrlFromMarkdown(post.content)
 
           return (
@@ -27,33 +22,29 @@ export default async function BlogPage() {
               key={post.slug}
             >
               <Card className="flex h-full flex-col justify-between">
-                <CardHeader>
-                  <Image
-                    className="aspect-video object-cover"
-                    src={firstImageUrl ?? ''}
-                    alt={post.metadata.title}
-                    width={640}
-                    height={360}
-                  />
-                </CardHeader>
+                <Image
+                  className="aspect-video object-cover"
+                  src={path.join('/_next/static/media', firstImageUrl ?? '')}
+                  alt={post.metadata.title}
+                  width={640}
+                  height={360}
+                />
                 <CardContent className="flex flex-col gap-y-4">
                   <h2 className="text-xl font-medium">{post.metadata.title}</h2>
                   <p className="dark:text-polar-500 line-clamp-2 text-gray-500">
                     {post.metadata.description}
                   </p>
-                </CardContent>
-                <CardFooter>
                   <p className="dark:text-polar-500 text-gray-500">
                     {new Date(post.metadata.created_at).toLocaleDateString(
                       'en-US',
                       {
                         year: 'numeric',
-                        month: 'long',
+                        month: 'short',
                         day: 'numeric',
                       },
                     )}
                   </p>
-                </CardFooter>
+                </CardContent>
               </Card>
             </Link>
           )

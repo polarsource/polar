0b64e7db9550b8f10232f5c081cf5707d4b19309
Birk Jernstrom <birkjernstrom@gmail.com>
2024-11-01 11:27:58 +0100

    clients: Move GitHub Funding.yaml embed to storefront

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/promote/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/promote/ClientPage.tsx
@@ -3,7 +3,6 @@
 import { DashboardBody } from '@/components/Layout/DashboardLayout'
 import Spinner from '@/components/Shared/Spinner'
 import { useListRepositories } from '@/hooks/queries'
-import { Skeleton } from '@mui/material'
 import { Organization } from '@polar-sh/sdk'
 import { useSearchParams } from 'next/navigation'
 import CopyToClipboardInput from 'polarkit/components/ui/atoms/copytoclipboardinput'
@@ -42,8 +41,6 @@ export default function ClientPage({
     ? `org=${organization.slug}&repo=${currentRepo.name}`
     : `org=${organization.slug}`
 
-  const fundingYAML = `polar: ${organization.slug}`
-
   const [currentEmbedTab, setCurrentEmbedTab] = useState('Issues')
 
   const embeds: Record<string, Embeddable> = {
@@ -62,33 +59,6 @@ export default function ClientPage({
   return (
     <DashboardBody>
       <div className="flex flex-col gap-y-8">
-        <ShadowBox className="flex flex-col gap-y-8">
-          <div className="flex flex-col gap-y-2">
-            <h2 className="text-lg font-medium">GitHub Sponsors</h2>
-            <p className="dark:text-polar-400 text-sm text-gray-500">
-              Make sure to link to your public funding page from GitHub&apos;s
-              Sponsor section.
-            </p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <h3 className="dark:text-polar-200 font-medium text-gray-500">
-              Link to your Polar funding page
-            </h3>
-            <div className="max-w-[600px]">
-              <CopyToClipboardInput value={fundingYAML} />
-            </div>
-            <div className="rounded-md border border-blue-100 bg-blue-50 px-4 py-2 text-sm text-blue-500 dark:border-blue-500 dark:bg-blue-700 dark:text-blue-300">
-              Follow the instructions{' '}
-              <a
-                className="font-bold text-blue-500 dark:text-blue-200"
-                href="https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/displaying-a-sponsor-button-in-your-repository"
-              >
-                here
-              </a>{' '}
-              and paste the above in your FUNDING.yml
-            </div>
-          </div>
-        </ShadowBox>
         <ShadowBox className="flex flex-col gap-y-8">
           <div className="flex flex-col gap-y-2">
             <h2 className="text-lg font-medium">README Embeds</h2>

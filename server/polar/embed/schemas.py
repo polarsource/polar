69cac5a2b7c7d2695f42c670a81c03957bf7b560
Birk Jernstrom <birkjernstrom@gmail.com>
2024-11-06 14:44:59 +0100

    server: Fix import path for product embed cover

--- server/polar/embed/schemas.py
+++ server/polar/embed/schemas.py
@@ -1,7 +1,8 @@
 from pydantic import UUID4
 
+from polar.file.schemas import ProductMediaFileRead
 from polar.kit.schemas import Schema
-from polar.product.schemas import BenefitList, ProductMediaFileRead, ProductPrice
+from polar.product.schemas import BenefitList, ProductPrice
 
 
 class ProductEmbed(Schema):

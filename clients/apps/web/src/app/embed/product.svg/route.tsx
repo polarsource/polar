a5329ef1cc9330277018220cfbabc9457282a05f
Birk Jernstrom <birkjernstrom@gmail.com>
2024-11-06 13:21:40 +0100

    clients: Proxy server cache responses for product embed

--- clients/apps/web/src/app/embed/product.svg/route.tsx
+++ clients/apps/web/src/app/embed/product.svg/route.tsx
@@ -7,23 +7,58 @@ const { default: satori } = require('satori')
 
 export const runtime = 'edge'
 
+const generate304Response = () => {
+  return new Response(
+    null,
+    {
+      status: 304,
+    },
+  )
+}
+
+const generate404Response = () => {
+  return new Response(
+    '<svg width="1" height="1" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>',
+    {
+      headers: {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-cache',
+      },
+      status: 400,
+    },
+  )
+}
+
 const getEmbed = async (
   productId: string,
   productPriceId?: string,
-): Promise<ProductEmbed | undefined> => {
+  cachedEtag?: string,
+): Promise<ProductEmbed | Response> => {
   let path = `/v1/embed/product/${productId}`
   if (productPriceId) {
     path += `?price_id=${productPriceId}`
   }
   let url = getServerURL(path)
+
+  let headers: Record<string, string> = {}
+  if (cachedEtag) {
+    headers['If-None-Match'] = cachedEtag
+  }
+
   const response = await fetch(url, {
     method: 'GET',
+    headers: headers
   })
 
-  if (!response.ok) {
+  const useCache = response.status === 304
+  if (useCache) {
+    return generate304Response()
+  }
+
+  if (!useCache && !response.ok) {
     const body = await response.text()
     console.error(`HTTP ${response.status} /v1/embed/product: ${body}`)
-    return
+    return generate404Response()
   }
 
   return (await response.json()) as ProductEmbed
@@ -67,34 +102,26 @@ const render = async (
   )
 }
 
-const generate404Response = () => {
-  return new Response(
-    '<svg width="1" height="1" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>',
-    {
-      headers: {
-        'Content-Type': 'image/svg+xml',
-        'Cache-Control': 'no-cache',
-      },
-      status: 400,
-    },
-  )
-}
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
-
   const productId = searchParams.get('productId')
   if (!productId) {
     return generate404Response()
   }
 
+  const cachedEtag = request.headers.get('If-None-Match') ?? undefined
   const productPriceId = searchParams.get('productPriceId') ?? undefined
   const darkmode = searchParams.has('darkmode')
   const cta = searchParams.get('cta') ?? undefined
 
   try {
-    const embed = await getEmbed(productId, productPriceId)
-    if (!embed) {
+    const embed = await getEmbed(productId, productPriceId, cachedEtag)
+    if (embed instanceof Response) {
+      return embed
+    }
+
+    // Sanity check before render
+    if (!embed.price) {
       return generate404Response()
     }
 
@@ -104,10 +131,12 @@ export async function GET(request: Request) {
       darkmode,
     )
 
+    const cacheTTL = 60 * 60 * 24 * 100  // 100 days
     return new Response(svg, {
       headers: {
         'Content-Type': 'image/svg+xml',
-        'Cache-Control': 'no-cache',
+        'Cache-Control': `public, max-age=${cacheTTL}`,
+        'ETag': embed.etag,
       },
       status: 200,
     })

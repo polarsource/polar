521717036484934959ab29f347fcec4d8ada3ddb
Emil Widlund <hello@emilwidlund.com>
2024-11-10 11:58:44 +0100

    clients/storefront: fix mobile layout

--- clients/apps/web/src/components/Profile/ProductsGrid.tsx
+++ clients/apps/web/src/components/Profile/ProductsGrid.tsx
@@ -1,4 +1,11 @@
 import { Organization, SubscriptionRecurringInterval } from '@polar-sh/sdk'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from 'polarkit/components/ui/atoms/select'
 import { twMerge } from 'tailwind-merge'
 import SubscriptionTierRecurringIntervalSwitch from '../Subscriptions/SubscriptionTierRecurringIntervalSwitch'
 
@@ -23,15 +30,35 @@ export const ProductsGrid = ({
 }: ProductsGridProps) => {
   return (
     <div className={twMerge('flex flex-grow flex-col gap-y-8', className)}>
-      <div className="flex flex-row items-center justify-between gap-x-8">
+      <div className="flex flex-col gap-8 md:flex-row md:items-center md:justify-between">
         {title && <h2 className="text-2xl font-medium">{title}</h2>}
         {hasBothIntervals && recurringInterval && setRecurringInterval && (
-          <div className="flex justify-center">
-            <SubscriptionTierRecurringIntervalSwitch
-              value={recurringInterval}
-              onChange={setRecurringInterval}
-            />
-          </div>
+          <>
+            <div className="hidden justify-center md:flex">
+              <SubscriptionTierRecurringIntervalSwitch
+                value={recurringInterval}
+                onChange={setRecurringInterval}
+              />
+            </div>
+            <div className="flex md:hidden">
+              <Select
+                onValueChange={setRecurringInterval}
+                value={recurringInterval}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select interval" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value={SubscriptionRecurringInterval.MONTH}>
+                    <span className="whitespace-nowrap">Monthly</span>
+                  </SelectItem>
+                  <SelectItem value={SubscriptionRecurringInterval.YEAR}>
+                    <span className="whitespace-nowrap">Yearly</span>
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </>
         )}
       </div>
       <div

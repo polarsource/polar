58799e4ceff6b723c950c6e5787641fdf5691fb8
Emil Widlund <hello@emilwidlund.com>
2024-11-12 23:42:13 +0100

    clients/licensekey: add activations

--- clients/apps/web/src/hooks/queries/license_keys.ts
+++ clients/apps/web/src/hooks/queries/license_keys.ts
@@ -33,6 +33,27 @@ export const useLicenseKeyUpdate = (organizationId: string) =>
     },
   })
 
+export const useLicenseKeyDeactivation = (licenseKeyId: string) =>
+  useMutation({
+    mutationFn: (opts: {
+      key: string
+      organizationId: string
+      activationId: string
+    }) =>
+      api.usersLicenseKeys.deactivate({
+        body: {
+          key: opts.key,
+          organization_id: opts.organizationId,
+          activation_id: opts.activationId,
+        },
+      }),
+    onSuccess: async (_result, _variables, _ctx) => {
+      queryClient.invalidateQueries({
+        queryKey: ['user', 'license_key', licenseKeyId],
+      })
+    },
+  })
+
 export const useOrganizationLicenseKeys = ({
   organizationId,
   page,

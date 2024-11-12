714c0e68880310e74137f981f771b40ca6fa01ad
Emil Widlund <hello@emilwidlund.com>
2024-11-12 23:51:53 +0100

    clients/input: adjust clipboardinput bg

--- clients/packages/polarkit/src/components/ui/atoms/CopyToClipboardInput.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/CopyToClipboardInput.tsx
@@ -9,7 +9,7 @@ const CopyToClipboardInput = ({
   onCopy,
   buttonLabel,
   disabled = false,
-  className = "",
+  className = '',
 }: {
   value: string
   onCopy?: () => void
@@ -34,10 +34,12 @@ const CopyToClipboardInput = ({
   }
 
   return (
-    <div className={twMerge(
-      "dark:border-polar-700 dark:bg-polar-800 flex w-full flex-row items-center overflow-hidden rounded-full border bg-gray-50",
-      className
-    )}>
+    <div
+      className={twMerge(
+        'dark:border-polar-700 dark:bg-polar-800 flex w-full flex-row items-center overflow-hidden rounded-full border bg-white',
+        className,
+      )}
+    >
       <Input
         className="dark:text-polar-400 !focus:border-transparent !focus:ring-transparent !dark:focus:border-transparent !dark:focus:ring-transparent w-full grow border-none bg-transparent font-mono text-xs text-gray-600 focus-visible:ring-transparent dark:bg-transparent dark:focus-visible:ring-transparent"
         value={value}

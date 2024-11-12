331b521747159efa833b1c4a5a7cc9829e6f2b48
François Voron <fvoron@gmail.com>
2024-11-12 13:00:36 +0100

    ops: disable Sentry tracing and replay
    We don't really use them, so better to save a bit of money for the time being

--- clients/apps/web/sentry.client.config.ts
+++ clients/apps/web/sentry.client.config.ts
@@ -10,7 +10,9 @@ Sentry.init({
   environment: CONFIG.ENVIRONMENT,
 
   // Add optional integrations for additional features
-  integrations: [Sentry.httpClientIntegration(), Sentry.replayIntegration()],
+  integrations: [Sentry.httpClientIntegration()],
+
+  enableTracing: false,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 0.1,
@@ -18,10 +20,10 @@ Sentry.init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,

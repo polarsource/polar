e99f93bb00b8735899c37d61dd5b5f03c4117272
François Voron <fvoron@gmail.com>
2024-11-21 15:08:32 +0100

    clients/web: fix typing issue following schema update

--- clients/apps/web/src/utils/discount.ts
+++ clients/apps/web/src/utils/discount.ts
@@ -1,15 +1,15 @@
 import {
+  CheckoutDiscount,
   CheckoutDiscountFixedOnceForeverDuration,
   CheckoutDiscountFixedRepeatDuration,
   CheckoutDiscountPercentageOnceForeverDuration,
   CheckoutDiscountPercentageRepeatDuration,
-  CheckoutPublicDiscount,
   DiscountType,
 } from '@polar-sh/sdk'
 import { formatCurrencyAndAmount } from '@polarkit/lib/money'
 
 const isDiscountFixed = (
-  discount: CheckoutPublicDiscount,
+  discount: CheckoutDiscount,
 ): discount is
   | CheckoutDiscountFixedOnceForeverDuration
   | CheckoutDiscountFixedRepeatDuration => {
@@ -17,7 +17,7 @@ const isDiscountFixed = (
 }
 
 const isDiscountPercentage = (
-  discount: CheckoutPublicDiscount,
+  discount: CheckoutDiscount,
 ): discount is
   | CheckoutDiscountPercentageOnceForeverDuration
   | CheckoutDiscountPercentageRepeatDuration => {
@@ -29,9 +29,7 @@ const percentageFormatter = new Intl.NumberFormat('en-US', {
   maximumFractionDigits: 2,
 })
 
-export const getDiscountDisplay = (
-  discount: CheckoutPublicDiscount,
-): string => {
+export const getDiscountDisplay = (discount: CheckoutDiscount): string => {
   if (isDiscountPercentage(discount)) {
     return percentageFormatter.format(-discount.basis_points / 10000)
   }

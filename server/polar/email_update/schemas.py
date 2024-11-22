fd7552801df69aeb12813c8c2a0d14d2ffb4b5a6
apsinghdev <ajeetpratap517@gmail.com>
2024-11-22 23:38:16 +0530

    chore: add EmailStr for email

--- server/polar/email_update/schemas.py
+++ server/polar/email_update/schemas.py
@@ -1,12 +1,12 @@
 import datetime
 
-from pydantic import field_validator
+from pydantic import field_validator, EmailStr
 from polar.kit.http import get_safe_return_url
 from polar.kit.schemas import Schema, EmailStrDNS, UUID4
 from polar.user.schemas.user import UserSignupAttribution
 
 class EmailUpdateCreate(Schema):
-    email: EmailStrDNS
+    email: EmailStr
     token_hash: str
     expires_at: datetime.datetime | None = None
     user_id: UUID4

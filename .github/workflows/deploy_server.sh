f06407dfa15d6d5b77e1f836c81b9a2a410210cd
François Voron <fvoron@gmail.com>
2024-11-22 09:43:22 +0100

    ops: fully configure new GitHub workers

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -6,8 +6,10 @@ IMG="ghcr.io/polarsource/polar@${1}"
 declare -A servers=(
   ["srv-ci4r87h8g3ne0dmvvl60"]="${RENDER_DEPLOY_KEY_API}"
   ["srv-csth45d6l47c73ekphf0"]="${RENDER_DEPLOY_KEY_WORKER}"
+  ["srv-ct0460ggph6c738go8i0"]="${RENDER_DEPLOY_KEY_WORKER_GITHUB}"
   ["srv-crkocgbtq21c73ddsdbg"]="${RENDER_DEPLOY_KEY_API_SANDBOX}"
   ["srv-csth45d6l47c73ekphdg"]="${RENDER_DEPLOY_KEY_WORKER_SANDBOX}"
+  ["srv-ct0460ggph6c738go8d0"]="${RENDER_DEPLOY_KEY_WORKER_GITHUB_SANDBOX}"
 )
 
 for server_id in "${!servers[@]}"; do

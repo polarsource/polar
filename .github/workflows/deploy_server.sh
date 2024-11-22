339b7a222a94a23cfb8da3a142b232c3e1027aa3
François Voron <fvoron@gmail.com>
2024-11-22 09:30:44 +0100

    ops: create dedicated servers for GitHub workers
    Environment groups are commented in a first step, because Render doesn't support environments in blueprints.
    
    So we need to create the services first, move them manually from the UI to the right environement and *then* attach their environment groups.

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -3,23 +3,16 @@
 set -euo pipefail
 
 IMG="ghcr.io/polarsource/polar@${1}"
+declare -A servers=(
+  ["srv-ci4r87h8g3ne0dmvvl60"]="${RENDER_DEPLOY_KEY_API}"
+  ["srv-csth45d6l47c73ekphf0"]="${RENDER_DEPLOY_KEY_WORKER}"
+  ["srv-crkocgbtq21c73ddsdbg"]="${RENDER_DEPLOY_KEY_API_SANDBOX}"
+  ["srv-csth45d6l47c73ekphdg"]="${RENDER_DEPLOY_KEY_WORKER_SANDBOX}"
+)
 
-# Deploy api
-curl -X POST \
+for server_id in "${!servers[@]}"; do
+  deploy_key=${servers[$server_id]}
+  curl -X POST \
     --silent --show-error --fail-with-body \
-    "https://api.render.com/deploy/srv-ci4r87h8g3ne0dmvvl60?key=${RENDER_DEPLOY_KEY_API}&imgURL=${IMG}"
-
-# Deploy worker
-curl -X POST \
-    --silent --show-error --fail-with-body \
-    "https://api.render.com/deploy/srv-csth45d6l47c73ekphf0?key=${RENDER_DEPLOY_KEY_WORKER}&imgURL=${IMG}"
-
-# Deploy api-sandbox
-curl -X POST \
-    --silent --show-error --fail-with-body \
-    "https://api.render.com/deploy/srv-crkocgbtq21c73ddsdbg?key=${RENDER_DEPLOY_KEY_API_SANDBOX}&imgURL=${IMG}"
-
-# Deploy worker-sandbox
-curl -X POST \
-    --silent --show-error --fail-with-body \
-    "https://api.render.com/deploy/srv-csth45d6l47c73ekphdg?key=${RENDER_DEPLOY_KEY_WORKER_SANDBOX}&imgURL=${IMG}"
+    "https://api.render.com/deploy/${server_id}?key=${deploy_key}&imgURL=${IMG}"
+done

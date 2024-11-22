d1df3751a224ffa87e6e008ccf403df2ffd8f645
Emil Widlund <hello@emilwidlund.com>
2024-11-22 21:41:04 +0100

    clients/orders: add order modal view

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/ClientPage.tsx
@@ -1,10 +1,15 @@
 'use client'
 
+import { CheckoutCard } from '@/components/Checkout/CheckoutCard'
 import CustomFieldValue from '@/components/CustomFields/CustomFieldValue'
+import { createCheckoutPreview } from '@/components/Customization/utils'
 import { DashboardBody } from '@/components/Layout/DashboardLayout'
+import { InlineModal } from '@/components/Modal/InlineModal'
+import { useModal } from '@/components/Modal/useModal'
 import ProductSelect from '@/components/Products/ProductSelect'
-import { useCustomFields } from '@/hooks/queries'
+import { useCustomFields, useProduct } from '@/hooks/queries'
 import { useOrders } from '@/hooks/queries/orders'
+import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
 import {
   DataTablePaginationState,
   DataTableSortingState,
@@ -12,6 +17,7 @@ import {
   serializeSearchParams,
 } from '@/utils/datatable'
 import { Order, OrderUser, Organization, Product } from '@polar-sh/sdk'
+import { RowSelectionState } from '@tanstack/react-table'
 import { useRouter } from 'next/navigation'
 import { FormattedDateTime } from 'polarkit/components/ui/atoms'
 import Avatar from 'polarkit/components/ui/atoms/avatar'
@@ -21,7 +27,7 @@ import {
   DataTableColumnHeader,
 } from 'polarkit/components/ui/atoms/datatable'
 import { formatCurrencyAndAmount } from 'polarkit/lib/money'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 interface ClientPageProps {
   organization: Organization
@@ -36,6 +42,10 @@ const ClientPage: React.FC<ClientPageProps> = ({
   sorting,
   productId,
 }) => {
+  const [selectedOrderState, setSelectedOrderState] =
+    useState<RowSelectionState>({})
+  const { hide: hideModal, show: showModal, isShown: isModalShown } = useModal()
+
   const getSearchParams = (
     pagination: DataTablePaginationState,
     sorting: DataTableSortingState,
@@ -108,8 +118,6 @@ const ClientPage: React.FC<ClientPageProps> = ({
   const orders = ordersHook.data?.items || []
   const pageCount = ordersHook.data?.pagination.max_page ?? 1
 
-  const { data: customFields } = useCustomFields(organization.id)
-
   const columns: DataTableColumnDef<Order>[] = [
     {
       accessorKey: 'user',
@@ -121,11 +129,14 @@ const ClientPage: React.FC<ClientPageProps> = ({
         const user = props.getValue() as OrderUser
         return (
           <div className="flex flex-row items-center gap-2">
-            <Avatar avatar_url={user.avatar_url} name={user.public_name} />
+            <Avatar
+              className="h-8 w-8"
+              avatar_url={user.avatar_url}
+              name={user.public_name}
+            />
             {user.github_username ? (
-              <div className="fw-medium">@{user.github_username}</div>
+              <div className="fw-medium">{user.public_name}</div>
             ) : null}
-            <div className="fw-medium">{user.email}</div>
           </div>
         )
       },
@@ -170,34 +181,17 @@ const ClientPage: React.FC<ClientPageProps> = ({
         <>{formatCurrencyAndAmount(order.amount, order.currency)}</>
       ),
     },
-    {
-      accessorKey: 'discount',
-      enableSorting: true,
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Discount" />
-      ),
-      cell: ({ row: { original: order } }) => (
-        <>{order.discount ? order.discount.name : '—'}</>
-      ),
-    },
-    ...(customFields
-      ? customFields.items.map<DataTableColumnDef<Order>>((field) => ({
-          accessorKey: `custom_field_data.${field.slug}`,
-          enableSorting: false,
-          header: ({ column }) => (
-            <DataTableColumnHeader column={column} title={field.name} />
-          ),
-          cell: (props) => (
-            <div className="max-w-48">
-              <CustomFieldValue
-                field={field}
-                value={props.getValue() as string | number | boolean}
-              />
-            </div>
-          ),
-        }))
-      : []),
   ]
+
+  const selectedOrder = orders.find((order) => selectedOrderState[order.id])
+
+  useEffect(() => {
+    if (selectedOrder) {
+      showModal()
+    } else {
+      hideModal()
+    }
+  }, [selectedOrder, showModal, hideModal])
 
   return (
     <DashboardBody>
@@ -222,10 +216,111 @@ const ClientPage: React.FC<ClientPageProps> = ({
             sorting={sorting}
             onSortingChange={setSorting}
             isLoading={ordersHook.isLoading}
+            onRowSelectionChange={(row) => {
+              setSelectedOrderState(row)
+            }}
+            rowSelection={selectedOrderState}
+            getRowId={(row) => row.id.toString()}
+            enableRowSelection
           />
         )}
       </div>
+      <InlineModal
+        modalContent={<OrderModal order={selectedOrder} />}
+        isShown={isModalShown}
+        hide={() => {
+          setSelectedOrderState({})
+          hideModal()
+        }}
+      />
     </DashboardBody>
+  )
+}
+
+interface OrderModalProps {
+  order?: Order
+}
+
+const OrderModal = ({ order }: OrderModalProps) => {
+  const { organization } = useContext(MaintainerOrganizationContext)
+  const { data: product } = useProduct(order?.product_id)
+  const { data: customFields } = useCustomFields(organization.id)
+
+  if (!order) return null
+
+  return (
+    <div className="flex flex-col gap-8 overflow-y-auto px-8 py-12">
+      <h2 className="mb-4 text-2xl">Order Details</h2>
+      <div className="flex flex-row items-center gap-4">
+        <Avatar
+          avatar_url={order.user.avatar_url}
+          name={order.user.public_name}
+          className="h-16 w-16"
+        />
+        <div className="flex flex-col gap-1">
+          <p className="text-xl">{order.user.public_name}</p>
+          <p className="dark:text-polar-500 text-gray-500">
+            {order.user.email}
+          </p>
+        </div>
+      </div>
+      <h2 className="text-2xl">{order.product.name}</h2>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <span className="dark:text-polar-500 text-gray-500">Order Date</span>
+          <span>
+            <FormattedDateTime datetime={order.created_at} />
+          </span>
+        </div>
+        <div className="flex justify-between">
+          <span className="dark:text-polar-500 text-gray-500">Tax</span>
+          <span>
+            {formatCurrencyAndAmount(order.tax_amount, order.currency)}
+          </span>
+        </div>
+        <div className="flex justify-between">
+          <span className="dark:text-polar-500 text-gray-500">Discount</span>
+          <span>{order.discount ? order.discount.code : '—'}</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="dark:text-polar-500 text-gray-500">Amount</span>
+          <span>{formatCurrencyAndAmount(order.amount, order.currency)}</span>
+        </div>
+      </div>
+      {customFields && (
+        <div className="flex flex-col gap-4">
+          <h3 className="text-lg">Custom Fields</h3>
+          <div className="flex flex-col gap-2">
+            {customFields.items.map((field) => (
+              <div
+                key={field.slug}
+                className="flex flex-row items-center justify-between"
+              >
+                <span className="dark:text-polar-500 text-gray-500">
+                  {field.name}
+                </span>
+                <CustomFieldValue
+                  field={field}
+                  value={
+                    order.custom_field_data
+                      ? order.custom_field_data[
+                          field.slug as keyof typeof order.custom_field_data
+                        ]
+                      : undefined
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {product && (
+        <CheckoutCard
+          checkout={createCheckoutPreview(product, order.product_price)}
+          disabled
+        />
+      )}
+    </div>
   )
 }
 

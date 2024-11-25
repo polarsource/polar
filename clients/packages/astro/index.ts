c198aa74292d348c8775d005780e5fd4119d52ec
François Voron <fvoron@gmail.com>
2024-11-25 15:52:57 +0100

    clients: remove unused packages and bump dependencies

--- clients/packages/astro/index.ts
+++
@@ -1,3 +0,0 @@
-export { Polar } from './src/client';
-export { ErrorGroup, PolarUploadError } from './src/types';
-export { polarArticleSchema } from './src/schemas';
--- clients/packages/eslint-plugin-polar-rules/eslint-plugin-polar-rules.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const enforceGitHubCapitalization = require('./enforce-github-capitalization');
-const plugin = { rules: { 'enforce-github-capitalization': enforceGitHubCapitalization } };
-module.exports = plugin;

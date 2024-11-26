9ad323fff5ebc8daff13e13004025398e55b6b90
François Voron <fvoron@gmail.com>
2024-11-26 09:30:59 +0100

    clients/web: fix link to webhook docs in webhook settings

--- clients/apps/web/src/components/Settings/Webhook/WebhookForm.tsx
+++ clients/apps/web/src/components/Settings/Webhook/WebhookForm.tsx
@@ -195,7 +195,7 @@ export const FieldEvents = () => {
           control={form.control}
           name="events"
           render={({ field }) => {
-            const href = `https://docs.polar.sh/api/webhooks/events#${event}`
+            const href = `https://docs.polar.sh/developers/webhooks/events/${event}`
 
             return (
               <FormItem className="flex flex-row items-center space-x-3 space-y-0">

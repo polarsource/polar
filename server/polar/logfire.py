6ed1dcdd9336abe2ea5640a0c983610ecdcbed82
François Voron <fvoron@gmail.com>
2024-11-29 10:28:27 +0100

    server: filter out noise in Logfire

--- server/polar/logfire.py
+++ server/polar/logfire.py
@@ -1,14 +1,77 @@
 import os
-from typing import Literal
+from collections.abc import Callable, Sequence
+from typing import TYPE_CHECKING, Literal
 
 import httpx
 import logfire
 from fastapi import FastAPI
 from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+from opentelemetry.sdk.trace.sampling import (
+    ALWAYS_OFF,
+    ALWAYS_ON,
+    ParentBased,
+    Sampler,
+    SamplingResult,
+)
+
+if TYPE_CHECKING:
+    from opentelemetry.context import Context
+    from opentelemetry.trace import Link, SpanKind
+    from opentelemetry.trace.span import TraceState
+    from opentelemetry.util.types import Attributes
 
 from polar.config import settings
 from polar.kit.db.postgres import Engine
+
+Matcher = Callable[[str, "Attributes | None"], bool]
+
+
+class IgnoreSampler(Sampler):
+    def __init__(self, matchers: Sequence[Matcher]) -> None:
+        super().__init__()
+        self.matchers = matchers
+
+    def should_sample(
+        self,
+        parent_context: "Context | None",
+        trace_id: int,
+        name: str,
+        kind: "SpanKind | None" = None,
+        attributes: "Attributes | None" = None,
+        links: Sequence["Link"] | None = None,
+        trace_state: "TraceState | None" = None,
+    ) -> SamplingResult:
+        sampler = ALWAYS_ON
+
+        for matcher in self.matchers:
+            if matcher(name, attributes):
+                sampler = ALWAYS_OFF
+                break
+
+        return sampler.should_sample(
+            parent_context,
+            trace_id,
+            name,
+            kind,
+            attributes,
+            links,
+            trace_state,
+        )
+
+    def get_description(self) -> str:
+        return "IgnoreSampler"
+
+
+def _healthz_matcher(name: str, attributes: "Attributes | None") -> bool:
+    return attributes is not None and attributes.get("http.route") == "/healthz"
+
+
+def _worker_health_matcher(name: str, attributes: "Attributes | None") -> bool:
+    lower_name = name.lower()
+    return lower_name.startswith("recording health:") or lower_name.startswith(
+        "health check successful"
+    )
 
 
 def configure_logfire(service_name: Literal["server", "worker"]) -> None:
@@ -21,6 +84,9 @@ def configure_logfire(service_name: Literal["server", "worker"]) -> None:
         service_name=service_name,
         service_version=os.environ.get("RELEASE_VERSION", "development"),
         console=False,
+        sampling=logfire.SamplingOptions(
+            head=ParentBased(IgnoreSampler((_healthz_matcher, _worker_health_matcher))),
+        ),
     )
 
 
@@ -38,10 +104,7 @@ def instrument_fastapi(app: FastAPI) -> None:
     if settings.is_testing():
         return
 
-    logfire.instrument_fastapi(
-        app,
-        excluded_urls=("/healthz$",),
-    )
+    logfire.instrument_fastapi(app)
 
 
 def instrument_sqlalchemy(engine: Engine) -> None:

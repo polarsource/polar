a4307dd922360d34e9be763a4205618efa7beb03
Emil Widlund <hello@emilwidlund.com>
2024-12-09 09:37:14 +0100

    clients: adjust datatable header

--- clients/packages/polarkit/src/components/ui/atoms/datatable/DataTable.tsx
+++ clients/packages/polarkit/src/components/ui/atoms/datatable/DataTable.tsx
@@ -118,7 +118,10 @@ export function DataTable<TData, TValue>({
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
+              <TableRow
+                key={headerGroup.id}
+                className="dark:bg-polar-800 bg-gray-50"
+              >
                 {headerGroup.headers.map((header) => {
                   return (
                     <TableHead key={header.id}>

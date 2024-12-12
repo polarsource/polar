0f726bda83584a16b540ceb48277c1f7d59e00d4
François Voron <fvoron@gmail.com>
2024-12-12 10:54:58 +0100

    clients/web: implement GitHub/Discord benefit grant handling

--- clients/apps/web/src/components/Benefit/BenefitGrant.tsx
+++ clients/apps/web/src/components/Benefit/BenefitGrant.tsx
@@ -1,11 +1,27 @@
 import {
+  useCustomerBenefitGrantUpdate,
+  useCustomerPortalCustomer,
+} from '@/hooks/queries'
+import {
   CustomerBenefitGrant,
   CustomerBenefitGrantCustom,
+  CustomerBenefitGrantDiscord,
   CustomerBenefitGrantDownloadables,
+  CustomerBenefitGrantGitHubRepository,
   CustomerBenefitGrantLicenseKeys,
   PolarAPI,
 } from '@polar-sh/sdk'
+import { usePathname } from 'next/navigation'
+import Button from 'polarkit/components/ui/atoms/button'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from 'polarkit/components/ui/atoms/select'
 import ShadowBox from 'polarkit/components/ui/atoms/shadowbox'
+import { useCallback, useMemo, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import DownloadablesBenefitGrant from './Downloadables/DownloadablesBenefitGrant'
 import { LicenseKeyBenefitGrant } from './LicenseKeys/LicenseKeyBenefitGrant'
@@ -33,6 +49,182 @@ const BenefitGrantCustom = ({
     <ShadowBox className="dark:bg-polar-800 bg-white p-4 text-sm lg:rounded-3xl">
       <p className="whitespace-pre-line">{note}</p>
     </ShadowBox>
+  )
+}
+
+const BenefitGrantOAuth = ({
+  api,
+  benefitGrant,
+  platform,
+  connectButtonText,
+  openButtonUrl,
+  openButtonText,
+  selectPlaceholder,
+}: {
+  api: PolarAPI
+  benefitGrant:
+    | CustomerBenefitGrantGitHubRepository
+    | CustomerBenefitGrantDiscord
+  platform: 'github' | 'discord'
+  openButtonText: string
+  openButtonUrl: string
+  connectButtonText: string
+  selectPlaceholder: string
+}) => {
+  const pathname = usePathname()
+  const {
+    customer_id,
+    properties: { account_id },
+    benefit: { type: benefitType },
+  } = benefitGrant
+  const { data: customer } = useCustomerPortalCustomer(api, customer_id)
+  const accounts = useMemo(
+    () =>
+      customer
+        ? Object.keys(customer.oauth_accounts || {})
+            .filter((key) => key.startsWith(platform))
+            .map((key) => customer.oauth_accounts[key])
+        : [],
+    [customer, platform],
+  )
+
+  const authorize = useCallback(async () => {
+    const { url } =
+      await api.customerPortalOauthAccounts.customerPortalOauthAccountsAuthorize(
+        {
+          platform,
+          returnTo: pathname,
+          customerId: customer_id,
+        },
+      )
+    window.location.href = url
+  }, [customer_id, api, pathname, platform])
+
+  const updateBenefitGrant = useCustomerBenefitGrantUpdate(api)
+  const [selectedAccountKey, setSelectedAccountKey] = useState<
+    string | undefined
+  >(undefined)
+  const onAccountSubmit = useCallback(async () => {
+    if (!selectedAccountKey) {
+      return
+    }
+    await updateBenefitGrant.mutateAsync({
+      id: benefitGrant.id,
+      body: {
+        benefit_type: benefitType,
+        properties: {
+          account_id: selectedAccountKey,
+        },
+      },
+    })
+  }, [updateBenefitGrant, selectedAccountKey, benefitGrant.id, benefitType])
+
+  return (
+    <ShadowBox className="dark:bg-polar-800 bg-white p-4 text-sm lg:rounded-3xl">
+      <div className="flex flex-row gap-2">
+        {account_id && (
+          <a href={openButtonUrl} target="_blank" rel="noopener noreferrer">
+            <Button asChild size="lg">
+              {openButtonText}
+            </Button>
+          </a>
+        )}
+        {!account_id && (
+          <>
+            {accounts.length === 0 ? (
+              <Button
+                type="button"
+                onClick={authorize}
+                size="lg"
+                className="w-full"
+              >
+                {connectButtonText}
+              </Button>
+            ) : (
+              <>
+                <Select
+                  onValueChange={(value) => {
+                    if (value === 'add') {
+                      authorize()
+                    } else {
+                      setSelectedAccountKey(value)
+                    }
+                  }}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder={selectPlaceholder} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {accounts.map((account) => (
+                      <SelectItem
+                        key={account.account_id}
+                        value={account.account_id}
+                      >
+                        {account.account_username}
+                      </SelectItem>
+                    ))}
+                    <SelectItem value="add">Connect new account</SelectItem>
+                  </SelectContent>
+                </Select>
+                <Button type="button" onClick={onAccountSubmit} size="lg">
+                  Request my invite
+                </Button>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </ShadowBox>
+  )
+}
+
+const BenefitGrantGitHubRepository = ({
+  api,
+  benefitGrant,
+}: {
+  api: PolarAPI
+  benefitGrant: CustomerBenefitGrantGitHubRepository
+}) => {
+  const {
+    benefit: {
+      properties: { repository_owner, repository_name },
+    },
+  } = benefitGrant
+  return (
+    <BenefitGrantOAuth
+      api={api}
+      benefitGrant={benefitGrant}
+      platform="github"
+      connectButtonText="Connect GitHub account"
+      openButtonText={`Go to ${repository_owner}/${repository_name}`}
+      openButtonUrl={`https://github.com/${repository_owner}/${repository_name}/invitations`}
+      selectPlaceholder="Select a GitHub account"
+    />
+  )
+}
+
+const BenefitGrantDiscord = ({
+  api,
+  benefitGrant,
+}: {
+  api: PolarAPI
+  benefitGrant: CustomerBenefitGrantDiscord
+}) => {
+  const {
+    benefit: {
+      properties: { guild_id },
+    },
+  } = benefitGrant
+  return (
+    <BenefitGrantOAuth
+      api={api}
+      benefitGrant={benefitGrant}
+      platform="discord"
+      connectButtonText="Connect Discord account"
+      openButtonText="Open Discord"
+      openButtonUrl={`https://www.discord.com/channels/${guild_id}`}
+      selectPlaceholder="Select a Discord account"
+    />
   )
 }
 
@@ -71,6 +263,18 @@ export const BenefitGrant = ({ api, benefitGrant }: BenefitGrantProps) => {
         <LicenseKeyBenefitGrant
           api={api}
           benefitGrant={benefitGrant as CustomerBenefitGrantLicenseKeys}
+        />
+      )}
+      {benefit.type === 'github_repository' && (
+        <BenefitGrantGitHubRepository
+          api={api}
+          benefitGrant={benefitGrant as CustomerBenefitGrantGitHubRepository}
+        />
+      )}
+      {benefit.type === 'discord' && (
+        <BenefitGrantDiscord
+          api={api}
+          benefitGrant={benefitGrant as CustomerBenefitGrantDiscord}
         />
       )}
     </div>

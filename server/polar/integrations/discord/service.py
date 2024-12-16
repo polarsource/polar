fa3531987091a03828d74987ad9b713dae189279
François Voron <fvoron@gmail.com>
2024-12-16 11:05:18 +0100

    server/integrations/discord: remove Discord authentication for users

--- server/polar/integrations/discord/service.py
+++ server/polar/integrations/discord/service.py
@@ -1,15 +1,11 @@
 import structlog
-from httpx_oauth.oauth2 import OAuth2Token
 
 from polar.config import settings
 from polar.exceptions import PolarError
 from polar.logging import Logger
-from polar.models import Customer, OAuthAccount, User
-from polar.models.user import OAuthPlatform
-from polar.postgres import AsyncSession
+from polar.models import Customer, User
 
-from . import oauth
-from .client import DiscordClient, bot_client
+from .client import bot_client
 from .schemas import DiscordGuild, DiscordGuildRole
 
 log: Logger = structlog.get_logger()
@@ -49,80 +45,6 @@ class DiscordCustomerExpiredAccessToken(DiscordError):
         self.account_id = account_id
         message = "The access token is expired and no refresh token is available."
         super().__init__(message, 401)
-
-
-class DiscordUserService:
-    async def create_oauth_account(
-        self, session: AsyncSession, user: User, oauth2_token_data: OAuth2Token
-    ) -> OAuthAccount:
-        access_token = oauth2_token_data["access_token"]
-
-        client = DiscordClient("Bearer", access_token)
-        data = await client.get_me()
-
-        account_id = data["id"]
-        account_email = data["email"]
-        account_username = data["username"]
-
-        oauth_account = OAuthAccount(
-            platform=OAuthPlatform.discord,
-            access_token=access_token,
-            expires_at=oauth2_token_data["expires_at"],
-            refresh_token=oauth2_token_data["refresh_token"],
-            account_id=account_id,
-            account_email=account_email,
-            account_username=account_username,
-            user=user,
-        )
-        session.add(oauth_account)
-        await session.commit()
-
-        return oauth_account
-
-    async def update_user_info(
-        self,
-        session: AsyncSession,
-        oauth_account: OAuthAccount,
-    ) -> OAuthAccount:
-        client = DiscordClient("Bearer", oauth_account.access_token)
-        data = await client.get_me()
-
-        account_email = data["email"]
-        account_username = data["username"]
-
-        oauth_account.account_email = account_email
-        oauth_account.account_username = account_username
-
-        session.add(oauth_account)
-
-        return oauth_account
-
-    async def get_oauth_account(
-        self, session: AsyncSession, user: User
-    ) -> OAuthAccount:
-        account = user.get_oauth_account(OAuthPlatform.discord)
-        if account is None:
-            raise DiscordAccountNotConnected(user)
-
-        if account.is_access_token_expired():
-            if account.refresh_token is None:
-                raise DiscordExpiredAccessToken(user)
-
-            log.debug(
-                "Refresh Discord access token",
-                oauth_account_id=str(account.id),
-                user_id=str(user.id),
-            )
-            refreshed_token_data = await oauth.user_client.refresh_token(
-                account.refresh_token
-            )
-            account.access_token = refreshed_token_data["access_token"]
-            account.expires_at = refreshed_token_data["expires_at"]
-            account.refresh_token = refreshed_token_data["refresh_token"]
-            session.add(account)
-            await session.commit()
-
-        return account
 
 
 class DiscordBotService:
@@ -184,5 +106,4 @@ class DiscordBotService:
         raise DiscordError("Roles not found")
 
 
-discord_user = DiscordUserService()
 discord_bot = DiscordBotService()

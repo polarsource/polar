df57b7c4d8fd64a7b15e6edeac4bb9bdb8344a7a
Birk Jernstrom <birkjernstrom@gmail.com>
2024-12-18 09:34:42 +0100

    server: Add `subscription.ended` bool in API

--- server/polar/subscription/schemas.py
+++ server/polar/subscription/schemas.py
@@ -58,6 +58,7 @@ class SubscriptionBase(IDSchema, TimestampedSchema):
     started_at: datetime | None
     ends_at: datetime | None
     ended_at: datetime | None
+    ended: bool
 
     customer_id: UUID4
     product_id: UUID4

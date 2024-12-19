f6fc68362a24cafbab92d6e1645ffb32c558d74f
François Voron <fvoron@gmail.com>
2024-12-19 13:49:42 +0100

    clients/web: allow to create customer session from customers modal

--- clients/apps/web/src/components/Customer/CustomerModal.tsx
+++ clients/apps/web/src/components/Customer/CustomerModal.tsx
@@ -1,27 +1,30 @@
+import AmountLabel from '@/components/Shared/AmountLabel'
+import { SubscriptionStatusLabel } from '@/components/Subscriptions/utils'
+import { useListSubscriptions } from '@/hooks/queries'
+import { useOrders } from '@/hooks/queries/orders'
+import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
+import { api } from '@/utils/api'
+import { CONFIG } from '@/utils/config'
+import { Customer, CustomerSession } from '@polar-sh/sdk'
+import Link from 'next/link'
+import { FormattedDateTime } from 'polarkit/components/ui/atoms'
+import Avatar from 'polarkit/components/ui/atoms/avatar'
+import Button from 'polarkit/components/ui/atoms/button'
+import CopyToClipboardInput from 'polarkit/components/ui/atoms/copytoclipboardinput'
+import { List, ListItem } from 'polarkit/components/ui/atoms/list'
+import { PropsWithChildren, useCallback, useContext, useState } from 'react'
 
-import AmountLabel from "@/components/Shared/AmountLabel"
-import { SubscriptionStatusLabel } from "@/components/Subscriptions/utils"
-import { useListSubscriptions } from "@/hooks/queries"
-import { useOrders } from "@/hooks/queries/orders"
-import { MaintainerOrganizationContext } from "@/providers/maintainerOrganization"
-import { Customer } from "@polar-sh/sdk"
-import Link from "next/link"
-import { FormattedDateTime } from "polarkit/components/ui/atoms"
-import Avatar from "polarkit/components/ui/atoms/avatar"
-import Button from "polarkit/components/ui/atoms/button"
-import { List, ListItem } from "polarkit/components/ui/atoms/list"
-import { PropsWithChildren, useContext } from "react"
-
-
-const CustomerStatBox = ({ title, children }: PropsWithChildren<{title: string}>) => {
+const CustomerStatBox = ({
+  title,
+  children,
+}: PropsWithChildren<{ title: string }>) => {
   return (
-    <div className="flex text-sm flex-col bg-gray-100 dark:bg-polar-800 flex-1 rounded-lg px-4 py-3 gap-1">
-      <span className="text-gray-500 dark:text-polar-500">{title}</span>
+    <div className="dark:bg-polar-800 flex flex-1 flex-col gap-1 rounded-lg bg-gray-100 px-4 py-3 text-sm">
+      <span className="dark:text-polar-500 text-gray-500">{title}</span>
       {children}
     </div>
   )
 }
-
 
 interface CustomerModalProps {
   customer: Customer
@@ -45,6 +48,28 @@ export const CustomerModal = ({ customer }: CustomerModalProps) => {
 
   const { organization } = useContext(MaintainerOrganizationContext)
 
+  const [customerSessionLoading, setCustomerSessionLoading] = useState(false)
+  const [customerSessionError, setCustomerSessionError] = useState<
+    string | null
+  >(null)
+  const [customerSession, setCustomerSession] =
+    useState<CustomerSession | null>(null)
+  const createCustomerSession = useCallback(async () => {
+    setCustomerSessionLoading(true)
+    try {
+      const session = await api.customerSessions.create({
+        body: { customer_id: customer.id },
+      })
+      setCustomerSession(session)
+    } catch {
+      setCustomerSessionError(
+        'An error occurred while creating the customer portal link. Please try again later.',
+      )
+    } finally {
+      setCustomerSessionLoading(false)
+    }
+  }, [customer])
+
   return (
     <div className="flex flex-col gap-8 overflow-y-auto px-8 py-12">
       <h2 className="text-xl">Customer Details</h2>
@@ -56,31 +81,63 @@ export const CustomerModal = ({ customer }: CustomerModalProps) => {
         />
         <div className="flex flex-col gap-1">
           <p className="text-lg">{customer.email}</p>
-          <div className="flex flex-row items-center text-gray-500 dark:text-polar-500 gap-1 font-mono text-xs">
+          <div className="dark:text-polar-500 flex flex-row items-center gap-1 font-mono text-xs text-gray-500">
             {customer.id}
           </div>
         </div>
       </div>
       <div className="flex flex-row justify-between gap-4">
         <CustomerStatBox title="Name">
-          <span className="text-sm flex-wrap">{customer.name ?? '—'}</span>
+          <span className="flex-wrap text-sm">{customer.name ?? '—'}</span>
         </CustomerStatBox>
         <CustomerStatBox title="First Seen">
           <FormattedDateTime datetime={customer.created_at} />
         </CustomerStatBox>
       </div>
-      <a href={`mailto:${customer.email}`} className="text-blue-500 dark:text-blue-400">
-        <Button className="w-full" size='lg'>Send Email</Button>
-      </a>
+      <div className="flex flex-col gap-4">
+        <a
+          href={`mailto:${customer.email}`}
+          className="text-blue-500 dark:text-blue-400"
+        >
+          <Button className="w-full" size="lg">
+            Send Email
+          </Button>
+        </a>
+        {customerSession ? (
+          <CopyToClipboardInput
+            value={`${CONFIG.FRONTEND_BASE_URL}/${organization.slug}/portal?customer_session_token=${customerSession.token}`}
+            buttonLabel="Copy"
+            className="bg-white"
+          />
+        ) : (
+          <Button
+            className="w-full"
+            size="lg"
+            variant="secondary"
+            loading={customerSessionLoading}
+            onClick={createCustomerSession}
+          >
+            Generate Customer Portal Link
+          </Button>
+        )}
+        {customerSessionError && (
+          <p className="text-destructive-foreground text-sm">
+            {customerSessionError}
+          </p>
+        )}
+      </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-lg">Subscriptions</h3>
         {subscriptions && subscriptions.items.length > 0 ? (
           <List size="small">
             {subscriptions.items.map((subscription) => (
               <ListItem key={subscription.id} className="text-sm" size="small">
-                <div className='flex flex-col gap-y-1'>
+                <div className="flex flex-col gap-y-1">
                   <span>{subscription.product.name}</span>
-                  <SubscriptionStatusLabel className="text-xs" subscription={subscription} />
+                  <SubscriptionStatusLabel
+                    className="text-xs"
+                    subscription={subscription}
+                  />
                 </div>
                 {subscription.amount && subscription.currency && (
                   <span>
@@ -95,7 +152,9 @@ export const CustomerModal = ({ customer }: CustomerModalProps) => {
             ))}
           </List>
         ) : (
-          <span className="text-gray-500 dark:text-polar-500">No subscriptions found</span>
+          <span className="dark:text-polar-500 text-gray-500">
+            No subscriptions found
+          </span>
         )}
       </div>
       <div className="flex flex-col gap-4">
@@ -103,11 +162,16 @@ export const CustomerModal = ({ customer }: CustomerModalProps) => {
         {orders && orders.items.length > 0 ? (
           <List size="small">
             {orders.items.map((order) => (
-              <Link href={`/dashboard/${organization?.slug}/sales/${order.id}`} key={order.id}>
+              <Link
+                href={`/dashboard/${organization?.slug}/sales/${order.id}`}
+                key={order.id}
+              >
                 <ListItem className="text-sm" size="small">
-                  <div className='flex flex-col gap-y-1'>
+                  <div className="flex flex-col gap-y-1">
                     <span>{order.product.name}</span>
-                    <span className='text-xs text-gray-500 dark:text-polar-500'><FormattedDateTime datetime={order.created_at} /></span>
+                    <span className="dark:text-polar-500 text-xs text-gray-500">
+                      <FormattedDateTime datetime={order.created_at} />
+                    </span>
                   </div>
                   <span>
                     <AmountLabel
@@ -120,7 +184,9 @@ export const CustomerModal = ({ customer }: CustomerModalProps) => {
             ))}
           </List>
         ) : (
-          <span className="text-gray-500 dark:text-polar-500">No orders found</span>
+          <span className="dark:text-polar-500 text-gray-500">
+            No orders found
+          </span>
         )}
       </div>
     </div>

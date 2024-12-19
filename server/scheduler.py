bc9a75c4c55ffcb4cc63d73d2b56269a6b75bc37
François Voron <fvoron@gmail.com>
2024-12-19 15:43:05 +0100

    ops: use Supervisor to run worker and scheduler

--- server/scheduler.py
+++ server/scheduler.py
@@ -2,10 +2,17 @@ from polar import tasks
 from polar.logfire import configure_logfire
 from polar.logging import configure as configure_logging
 from polar.sentry import configure_sentry
-from polar.worker import WorkerSettings, WorkerSettingsGitHubCrawl
+from polar.worker import CronTasksScheduler
 
 configure_sentry()
 configure_logfire("worker")
 configure_logging(logfire=True)
 
-__all__ = ["WorkerSettings", "WorkerSettingsGitHubCrawl", "tasks"]
+__all__ = ["tasks"]
+
+if __name__ == "__main__":
+    scheduler = CronTasksScheduler()
+    try:
+        scheduler.run()
+    except KeyboardInterrupt:
+        pass

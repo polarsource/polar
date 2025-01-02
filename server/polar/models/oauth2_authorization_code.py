dfb2f144e5d06033deea4f3a840dc21a94647783
François Voron <fvoron@gmail.com>
2025-01-02 15:46:57 +0100

    server/oauth2: fix OAuth2AuthorizationCode.client_id column length

--- server/polar/models/oauth2_authorization_code.py
+++ server/polar/models/oauth2_authorization_code.py
@@ -1,4 +1,8 @@
-from authlib.integrations.sqla_oauth2 import OAuth2AuthorizationCodeMixin
+from authlib.integrations.sqla_oauth2 import (
+    OAuth2AuthorizationCodeMixin,
+)
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from polar.kit.db.models import RecordModel
 from polar.oauth2.sub_type import SubTypeModelMixin
@@ -8,3 +12,5 @@ class OAuth2AuthorizationCode(
     RecordModel, SubTypeModelMixin, OAuth2AuthorizationCodeMixin
 ):
     __tablename__ = "oauth2_authorization_codes"
+
+    client_id: Mapped[str] = mapped_column(String(52), nullable=False)

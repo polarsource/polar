6ca4fa44a478d952f8d059b056d361038c912dd9
François Voron <fvoron@gmail.com>
2025-01-06 11:52:58 +0100

    server/integrations/plain: support multiple organizations/customers in cards

--- server/polar/integrations/plain/service.py
+++ server/polar/integrations/plain/service.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Any
 
 import pycountry
 import pycountry.db
@@ -53,79 +54,72 @@ class PlainService:
             )
         )
         result = await session.execute(statement)
-        organization = result.unique().scalar_one_or_none()
+        organizations = result.unique().scalars().all()
 
-        if organization is None:
+        if len(organizations) == 0:
             return None
+
+        def _get_organization_container(organization: Organization) -> dict[str, Any]:
+            return {
+                "componentContainer": {
+                    "containerContent": [
+                        {"componentText": {"text": organization.name}},
+                        {
+                            "componentText": {
+                                "text": organization.slug,
+                                "textColor": "MUTED",
+                            }
+                        },
+                        {"componentDivider": {"dividerSpacingSize": "M"}},
+                        {
+                            "componentRow": {
+                                "rowMainContent": [
+                                    {
+                                        "componentText": {
+                                            "text": "ID",
+                                            "textSize": "S",
+                                            "textColor": "MUTED",
+                                        }
+                                    },
+                                    {"componentText": {"text": organization.id}},
+                                ],
+                                "rowAsideContent": [
+                                    {
+                                        "componentCopyButton": {
+                                            "copyButtonValue": organization.id,
+                                            "copyButtonTooltipLabel": "Copy Organization ID",
+                                        }
+                                    }
+                                ],
+                            }
+                        },
+                        {"componentSpacer": {"spacerSize": "M"}},
+                        {
+                            "componentText": {
+                                "text": "Created At",
+                                "textSize": "S",
+                                "textColor": "MUTED",
+                            }
+                        },
+                        {
+                            "componentText": {
+                                "text": organization.created_at.date().isoformat()
+                            }
+                        },
+                    ]
+                }
+            }
+
+        components = []
+        for i, organization in enumerate(organizations):
+            components.append(_get_organization_container(organization))
+            if i < len(organizations) - 1:
+                components.append({"componentDivider": {"dividerSpacingSize": "M"}})
 
         return CustomerCard(
             key=CustomerCardKey.organization,
             timeToLiveSeconds=86400,
-            components=[
-                {
-                    "componentContainer": {
-                        "containerContent": [
-                            {
-                                "componentRow": {
-                                    "rowMainContent": [
-                                        {"componentText": {"text": organization.name}},
-                                        {
-                                            "componentText": {
-                                                "text": organization.slug,
-                                                "textColor": "MUTED",
-                                            }
-                                        },
-                                    ],
-                                    "rowAsideContent": [
-                                        {
-                                            "componentLinkButton": {
-                                                "linkButtonLabel": "Omni ↗",
-                                                "linkButtonUrl": "https://example.com",
-                                            }
-                                        }
-                                    ],
-                                }
-                            },
-                            {"componentDivider": {"dividerSpacingSize": "M"}},
-                            {
-                                "componentRow": {
-                                    "rowMainContent": [
-                                        {
-                                            "componentText": {
-                                                "text": "ID",
-                                                "textSize": "S",
-                                                "textColor": "MUTED",
-                                            }
-                                        },
-                                        {"componentText": {"text": organization.id}},
-                                    ],
-                                    "rowAsideContent": [
-                                        {
-                                            "componentCopyButton": {
-                                                "copyButtonValue": organization.id,
-                                                "copyButtonTooltipLabel": "Copy Organization ID",
-                                            }
-                                        }
-                                    ],
-                                }
-                            },
-                            {"componentSpacer": {"spacerSize": "M"}},
-                            {
-                                "componentText": {
-                                    "text": "Created At",
-                                    "textSize": "S",
-                                    "textColor": "MUTED",
-                                }
-                            },
-                            {
-                                "componentText": {
-                                    "text": organization.created_at.date().isoformat()
-                                }
-                            },
-                        ]
-                    }
-                }
-            ],
+            components=components,
         )
 
     async def get_customer_card(
@@ -135,136 +129,126 @@ class PlainService:
 
         statement = select(Customer).where(func.lower(Customer.email) == email.lower())
         result = await session.execute(statement)
-        customer = result.unique().scalar_one_or_none()
+        customers = result.unique().scalars().all()
 
-        if customer is None:
+        if len(customers) == 0:
             return None
 
-        country: pycountry.db.Country | None = None
-        if customer.billing_address and customer.billing_address.country:
-            country = pycountry.countries.get(alpha_2=customer.billing_address.country)
+        def _get_customer_container(customer: Customer) -> dict[str, Any]:
+            country: pycountry.db.Country | None = None
+            if customer.billing_address and customer.billing_address.country:
+                country = pycountry.countries.get(
+                    alpha_2=customer.billing_address.country
+                )
+            return {
+                "componentContainer": {
+                    "containerContent": [
+                        {"componentText": {"text": customer.name}},
+                        {"componentDivider": {"dividerSpacingSize": "M"}},
+                        {
+                            "componentRow": {
+                                "rowMainContent": [
+                                    {
+                                        "componentText": {
+                                            "text": "ID",
+                                            "textSize": "S",
+                                            "textColor": "MUTED",
+                                        }
+                                    },
+                                    {"componentText": {"text": customer.id}},
+                                ],
+                                "rowAsideContent": [
+                                    {
+                                        "componentCopyButton": {
+                                            "copyButtonValue": customer.id,
+                                            "copyButtonTooltipLabel": "Copy Customer ID",
+                                        }
+                                    }
+                                ],
+                            }
+                        },
+                        {"componentSpacer": {"spacerSize": "M"}},
+                        {
+                            "componentText": {
+                                "text": "Created At",
+                                "textSize": "S",
+                                "textColor": "MUTED",
+                            }
+                        },
+                        {
+                            "componentText": {
+                                "text": customer.created_at.date().isoformat()
+                            }
+                        },
+                        *(
+                            [
+                                {"componentSpacer": {"spacerSize": "M"}},
+                                {
+                                    "componentRow": {
+                                        "rowMainContent": [
+                                            {
+                                                "componentText": {
+                                                    "text": "Country",
+                                                    "textSize": "S",
+                                                    "textColor": "MUTED",
+                                                }
+                                            },
+                                            {
+                                                "componentText": {
+                                                    "text": country.name,
+                                                }
+                                            },
+                                        ],
+                                        "rowAsideContent": [
+                                            {"componentText": {"text": country.flag}}
+                                        ],
+                                    }
+                                },
+                            ]
+                            if country
+                            else []
+                        ),
+                        {"componentSpacer": {"spacerSize": "M"}},
+                        {
+                            "componentRow": {
+                                "rowMainContent": [
+                                    {
+                                        "componentText": {
+                                            "text": "Stripe Customer ID",
+                                            "textSize": "S",
+                                            "textColor": "MUTED",
+                                        }
+                                    },
+                                    {
+                                        "componentText": {
+                                            "text": customer.stripe_customer_id,
+                                        }
+                                    },
+                                ],
+                                "rowAsideContent": [
+                                    {
+                                        "componentLinkButton": {
+                                            "linkButtonLabel": "Stripe ↗",
+                                            "linkButtonUrl": f"https://dashboard.stripe.com/customers/{customer.stripe_customer_id}",
+                                        }
+                                    }
+                                ],
+                            }
+                        },
+                    ]
+                }
+            }
+
+        components = []
+        for i, customer in enumerate(customers):
+            components.append(_get_customer_container(customer))
+            if i < len(customers) - 1:
+                components.append({"componentDivider": {"dividerSpacingSize": "M"}})
 
         return CustomerCard(
             key=CustomerCardKey.customer,
             timeToLiveSeconds=86400,
-            components=[
-                {
-                    "componentContainer": {
-                        "containerContent": [
-                            {
-                                "componentRow": {
-                                    "rowMainContent": [
-                                        {"componentText": {"text": customer.name}},
-                                    ],
-                                    "rowAsideContent": [
-                                        {
-                                            "componentLinkButton": {
-                                                "linkButtonLabel": "Omni ↗",
-                                                "linkButtonUrl": "https://example.com",
-                                            }
-                                        }
-                                    ],
-                                }
-                            },
-                            {"componentDivider": {"dividerSpacingSize": "M"}},
-                            {
-                                "componentRow": {
-                                    "rowMainContent": [
-                                        {
-                                            "componentText": {
-                                                "text": "ID",
-                                                "textSize": "S",
-                                                "textColor": "MUTED",
-                                            }
-                                        },
-                                        {"componentText": {"text": customer.id}},
-                                    ],
-                                    "rowAsideContent": [
-                                        {
-                                            "componentCopyButton": {
-                                                "copyButtonValue": customer.id,
-                                                "copyButtonTooltipLabel": "Copy Customer ID",
-                                            }
-                                        }
-                                    ],
-                                }
-                            },
-                            {"componentSpacer": {"spacerSize": "M"}},
-                            {
-                                "componentText": {
-                                    "text": "Created At",
-                                    "textSize": "S",
-                                    "textColor": "MUTED",
-                                }
-                            },
-                            {
-                                "componentText": {
-                                    "text": customer.created_at.date().isoformat()
-                                }
-                            },
-                            *(
-                                [
-                                    {"componentSpacer": {"spacerSize": "M"}},
-                                    {
-                                        "componentRow": {
-                                            "rowMainContent": [
-                                                {
-                                                    "componentText": {
-                                                        "text": "Country",
-                                                        "textSize": "S",
-                                                        "textColor": "MUTED",
-                                                    }
-                                                },
-                                                {
-                                                    "componentText": {
-                                                        "text": country.name,
-                                                    }
-                                                },
-                                            ],
-                                            "rowAsideContent": [
-                                                {
-                                                    "componentText": {
-                                                        "text": country.flag
-                                                    }
-                                                }
-                                            ],
-                                        }
-                                    },
-                                ]
-                                if country
-                                else []
-                            ),
-                            {"componentSpacer": {"spacerSize": "M"}},
-                            {
-                                "componentRow": {
-                                    "rowMainContent": [
-                                        {
-                                            "componentText": {
-                                                "text": "Stripe Customer ID",
-                                                "textSize": "S",
-                                                "textColor": "MUTED",
-                                            }
-                                        },
-                                        {
-                                            "componentText": {
-                                                "text": customer.stripe_customer_id,
-                                            }
-                                        },
-                                    ],
-                                    "rowAsideContent": [
-                                        {
-                                            "componentLinkButton": {
-                                                "linkButtonLabel": "Stripe ↗",
-                                                "linkButtonUrl": f"https://dashboard.stripe.com/customers/{customer.stripe_customer_id}",
-                                            }
-                                        }
-                                    ],
-                                }
-                            },
-                        ]
-                    }
-                }
-            ],
+            components=components,
         )
 
 

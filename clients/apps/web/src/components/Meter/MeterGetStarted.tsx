4cc5e30be95408e64c28690dd8c5dc7ad9615b24
Emil Widlund <hello@emilwidlund.com>
2025-01-10 16:36:18 +0100

    adjhust metering

--- clients/apps/web/src/components/Meter/MeterGetStarted.tsx
+++ clients/apps/web/src/components/Meter/MeterGetStarted.tsx
@@ -1,4 +1,8 @@
 import { Meter } from '@/app/api/meters/data'
+import {
+  SyntaxHighlighterClient,
+  SyntaxHighlighterProvider,
+} from '../SyntaxHighlighterShiki/SyntaxHighlighterClient'
 
 export interface MeterGetStartedProps {
   meter: Meter
@@ -7,14 +11,27 @@ export interface MeterGetStartedProps {
 export const MeterGetStarted = ({ meter }: MeterGetStartedProps) => {
   return (
     <div className="dark:bg-polar-800 dark:border-polar-700 flex flex-col gap-y-4 rounded-2xl border border-gray-200 bg-gray-100 p-6">
-      <h2 className="text-lg font-semibold">Get started with metering</h2>
-      <p>Meter usage by sending meter events to the Polar API.</p>
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-xl">Get started with metering</h2>
+        <p className="dark:text-polar-500 text-gray-500">
+          Meter usage by sending meter events to the Polar API. Use our handy
+          NextJS Usage utility if you're using Next.js.
+        </p>
+      </div>
       <pre className="dark:bg-polar-900 rounded-lg bg-white p-4 font-mono text-sm">
-        <code>{`curl https://api.polar.sh/v1/billing/meter_events \\
--u "sk_test_...:gHzA" \\
--d slug=${meter.slug} \\
--d "payload[customer_id]"="{{ CUSTOMER_ID }}" \\
--d "payload[value]"=1`}</code>
+        <SyntaxHighlighterProvider>
+          <SyntaxHighlighterClient
+            lang="typescript"
+            code={`import { Usage } from "@polar-sh/nextjs";
+              
+export const POST = Usage()
+  .model(openai("gpt-4o-mini"))
+  .customer(async (req) => req.headers.get("X-Polar-Customer-Id") ?? "")
+  .increment("${meter.slug}", (ctx) => ctx.usage.promptTokens)
+  .handler(async (req, res, model) => /** Handle request and model as usual */);
+`}
+          />
+        </SyntaxHighlighterProvider>
       </pre>
     </div>
   )

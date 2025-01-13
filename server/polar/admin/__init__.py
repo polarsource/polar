8823d36e599bbff7b7afbc519e808609f2dd19fb
François Voron <fvoron@gmail.com>
2025-01-13 09:15:30 +0100

    wip

--- server/polar/admin/__init__.py
+++ server/polar/admin/__init__.py
@@ -1,10 +1,12 @@
 from collections.abc import AsyncIterator
+from operator import attrgetter
 
 from asgi_admin.admin import AdminBase
 from asgi_admin.integrations.sqlalchemy import RepositoryBase
 from asgi_admin.views import ModelView
 from sqlalchemy import Select, select
 from starlette.requests import Request
+from wtforms import StringField, validators
 
 from polar.models import Organization
 
@@ -18,14 +20,30 @@ class OrganizationRepository(RepositoryBase[Organization]):
 
 class OrganizationView(ModelView[Organization]):
     model = Organization
+    model_id_getter = attrgetter("id")
+
     list_fields = ["id", "name", "slug", "created_at"]
     list_query_fields = ["id", "name", "slug"]
+
+    edit_fields = (
+        (
+            "name",
+            StringField(
+                "Name",
+                validators=[validators.InputRequired(), validators.Length(min=3)],
+            ),
+        ),
+    )
 
     async def get_repository(
         self, request: Request
     ) -> AsyncIterator[OrganizationRepository]:
         async with request.state.async_sessionmaker() as session:
             yield OrganizationRepository(session)
+            await session.commit()
+
+    async def get_item_title(self, request: Request, item: Organization) -> str:
+        return item.name
 
 
 class Admin(AdminBase):

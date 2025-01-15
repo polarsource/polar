ed6dfa5f8672e5543c706c0b40f601a85c5e906a
Emil Widlund <hello@emilwidlund.com>
2025-01-15 23:37:49 +0100

    adjust context view gap

--- clients/apps/web/src/components/Layout/DashboardLayout.tsx
+++ clients/apps/web/src/components/Layout/DashboardLayout.tsx
@@ -257,7 +257,7 @@ export const DashboardBody = ({
   )
 
   return (
-    <div className={twMerge('flex h-full w-full flex-row gap-x-2')}>
+    <div className={twMerge('flex h-full w-full flex-row gap-x-3')}>
       <div
         onScroll={handleScroll}
         className={twMerge(

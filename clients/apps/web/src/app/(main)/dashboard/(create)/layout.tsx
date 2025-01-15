f26e05513dbfc8bc1b18b668c754fd0bb17e98da
Emil Widlund <hello@emilwidlund.com>
2025-01-15 23:31:12 +0100

    revert create org bg

--- clients/apps/web/src/app/(main)/dashboard/(create)/layout.tsx
+++ clients/apps/web/src/app/(main)/dashboard/(create)/layout.tsx
@@ -6,7 +6,7 @@ export default async function Layout({
   children: React.ReactNode
 }) {
   return (
-    <div className="dark:bg-polar-900 flex flex-col items-center px-4 py-12">
+    <div className="flex flex-col items-center px-4 py-12">
       <LogoIcon className="text-black dark:text-white" size={50} />
       <div className="relative flex min-h-screen w-full flex-col items-center md:py-0">
         {children}

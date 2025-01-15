877352cf72df5ace19ec0d3338157085bb96ed33
Emil Widlund <hello@emilwidlund.com>
2025-01-15 16:13:16 +0100

    clients: add edit capability in UI for customers

--- clients/apps/web/src/hooks/queries/customers.ts
+++ clients/apps/web/src/hooks/queries/customers.ts
@@ -1,6 +1,6 @@
-import { api } from '@/utils/api'
-import { CustomersApiListRequest } from '@polar-sh/sdk'
-import { useQuery } from '@tanstack/react-query'
+import { api, queryClient } from '@/utils/api'
+import { CustomersApiListRequest, CustomerUpdate } from '@polar-sh/sdk'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { defaultRetry } from './retry'
 
 export const useCustomers = (
@@ -8,11 +8,22 @@ export const useCustomers = (
   parameters?: Omit<CustomersApiListRequest, 'organization_id'>,
 ) =>
   useQuery({
-    queryKey: ['customers', { organizationId, ...(parameters || {}) }],
+    queryKey: ['customers', organizationId, parameters],
     queryFn: () =>
       api.customers.list({
         organizationId: organizationId ?? '',
         ...(parameters || {}),
       }),
     retry: defaultRetry,
+  })
+
+export const useUpdateCustomer = (customerId: string, organizationId: string) =>
+  useMutation({
+    mutationFn: (data: CustomerUpdate) =>
+      api.customers.update({ id: customerId, body: data }),
+    onSuccess: async (_result, _variables, _ctx) => {
+      queryClient.invalidateQueries({
+        queryKey: ['customers', organizationId],
+      })
+    },
   })

ad3f4e05ba9ad54ee7cdefa64cce7617a77b2307
Emil Widlund <hello@emilwidlund.com>
2025-01-22 09:28:10 +0100

    fix section scroll on mobile

--- clients/apps/web/src/components/Vision/Section.tsx
+++ clients/apps/web/src/components/Vision/Section.tsx
@@ -11,14 +11,23 @@ export interface SectionProps {
   context?: React.ReactNode
 }
 
-export const Section = ({ active, header, title, children, context }: SectionProps) => {
-  let desktopClasses = "md:hidden"
+export const Section = ({
+  active,
+  header,
+  title,
+  children,
+  context,
+}: SectionProps) => {
+  let desktopClasses = 'md:hidden'
   if (active) {
-    desktopClasses = "md:flex-row md:gap-x-32"
+    desktopClasses = 'md:flex-row md:gap-x-32'
   }
 
   return (
-    <div className={twMerge(desktopClasses, "flex flex-col gap-y-16 mb-16")}>
+    <div
+      id={header.index}
+      className={twMerge(desktopClasses, 'mb-16 flex flex-col gap-y-16')}
+    >
       <div className="flex max-w-lg flex-col gap-y-8">
         <div className="flex flex-row items-center gap-x-4">
           <span className="bg-polar-200 px-1 py-0.5 text-sm leading-none text-black">

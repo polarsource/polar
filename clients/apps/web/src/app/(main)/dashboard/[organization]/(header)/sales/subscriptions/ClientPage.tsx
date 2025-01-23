a2db6fa1a01f7f8de2f9f06f882f9fc525d22a02
François Voron <fvoron@gmail.com>
2025-01-23 11:04:20 +0100

    clients/web: fix imports following merge conflict

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/subscriptions/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/subscriptions/ClientPage.tsx
@@ -7,7 +7,6 @@ import { SubscriptionModal } from '@/components/Subscriptions/SubscriptionModal'
 import { SubscriptionStatus as SubscriptionStatusComponent } from '@/components/Subscriptions/SubscriptionStatus'
 import SubscriptionStatusSelect from '@/components/Subscriptions/SubscriptionStatusSelect'
 import SubscriptionTiersSelect from '@/components/Subscriptions/SubscriptionTiersSelect'
-import { subscriptionStatusDisplayNames } from '@/components/Subscriptions/utils'
 import { useListSubscriptions, useProducts } from '@/hooks/queries'
 import { getServerURL } from '@/utils/api'
 import {
@@ -16,11 +15,7 @@ import {
   getAPIParams,
   serializeSearchParams,
 } from '@/utils/datatable'
-import {
-  AccessTimeOutlined,
-  CancelOutlined,
-  FileDownloadOutlined,
-} from '@mui/icons-material'
+import { FileDownloadOutlined } from '@mui/icons-material'
 import {
   Organization,
   Product,
@@ -35,108 +30,9 @@ import {
   DataTableColumnHeader,
 } from '@polar-sh/ui/components/atoms/DataTable'
 import FormattedDateTime from '@polar-sh/ui/components/atoms/FormattedDateTime'
-import Pill from '@polar-sh/ui/components/atoms/Pill'
 import { RowSelectionState } from '@tanstack/react-table'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
-import { twMerge } from 'tailwind-merge'
-
-const CANCELLATION_REASONS: {
-  [key: string]: string
-} = {
-  unused: 'Unused',
-  too_expensive: 'Too Expensive',
-  missing_features: 'Missing Features',
-  switched_service: 'Switched Service',
-  customer_service: 'Customer Service',
-  low_quality: 'Low Quality',
-  too_complex: 'Too Complicated',
-  other: 'Other',
-}
-
-const getHumanCancellationReason = (key: string | null) => {
-  if (key && key in CANCELLATION_REASONS) {
-    return CANCELLATION_REASONS[key]
-  }
-  return null
-}
-
-const StatusLabel = ({
-  color,
-  dt,
-  icon,
-  children,
-}: {
-  color: string
-  dt?: string | null
-  icon?: React.ReactNode
-  children: React.ReactNode
-}) => {
-  let prettyEventDate = null
-  if (dt) {
-    const event = new Date(dt)
-    const now = new Date()
-    if (event.getFullYear() != now.getFullYear()) {
-      prettyEventDate = event.toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'short',
-        year: 'numeric',
-      })
-    } else {
-      prettyEventDate = event.toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'short',
-      })
-    }
-  }
-
-  return (
-    <div className={`flex flex-row items-center gap-x-2`}>
-      <span className={twMerge('h-2 w-2 rounded-full border-2', color)} />
-      <span className="capitalize">{children}</span>
-      {prettyEventDate && (
-        <Pill color="gray" className="flex flex-row">
-          {icon}
-          <span className="!ml-1">{prettyEventDate}</span>
-        </Pill>
-      )}
-    </div>
-  )
-}
-
-const Status = ({ subscription }: { subscription: Subscription }) => {
-  switch (subscription.status) {
-    case 'active':
-      if (!subscription.ends_at) {
-        return <StatusLabel color="border-emerald-500">Active</StatusLabel>
-      }
-      return (
-        <StatusLabel
-          color="border-yellow-500"
-          dt={subscription.ends_at}
-          icon={<AccessTimeOutlined fontSize="inherit" />}
-        >
-          Ending
-        </StatusLabel>
-      )
-    case 'canceled':
-      return (
-        <StatusLabel
-          color="border-red-500"
-          dt={subscription.ended_at}
-          icon={<CancelOutlined fontSize="inherit" />}
-        >
-          Canceled
-        </StatusLabel>
-      )
-    default:
-      return (
-        <StatusLabel color="border-red-500">
-          {subscriptionStatusDisplayNames[subscription.status]}
-        </StatusLabel>
-      )
-  }
-}
 
 interface ClientPageProps {
   organization: Organization

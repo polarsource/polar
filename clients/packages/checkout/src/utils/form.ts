a37a481ea586b9d785ca105e8f48f66f14b7cad0
François Voron <fvoron@gmail.com>
2025-01-27 11:13:00 +0100

    clients/packages/checkout: transform error loc to camelCase to match SDK style

--- clients/packages/checkout/src/utils/form.ts
+++ clients/packages/checkout/src/utils/form.ts
@@ -12,6 +12,15 @@ export const setValidationErrors = <TFieldValues extends FieldValues>(
     if (discriminators && discriminators.includes(loc[0] as string)) {
       loc = loc.slice(1)
     }
+
+    // Transform each loc to camelCase, since the schema from our SDK converts everythng to camelCase
+    loc = loc.map((part) => {
+      if (Number.isInteger(part)) {
+        return part
+      }
+      return (part as string).replace(/[-_](.)/g, (_, c) => c.toUpperCase())
+    })
+
     setError(loc.join('.') as FieldPath<TFieldValues>, {
       type: error.type,
       message: error.msg,

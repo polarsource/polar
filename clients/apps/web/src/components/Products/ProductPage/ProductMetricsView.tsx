603aa434e209318ce0ebef0207622b52b7c3ec6b
Emil Widlund <hello@emilwidlund.com>
2025-02-03 23:26:26 +0100

    clients/metrics: fix metric interval

--- clients/apps/web/src/components/Products/ProductPage/ProductMetricsView.tsx
+++ clients/apps/web/src/components/Products/ProductPage/ProductMetricsView.tsx
@@ -1,30 +1,33 @@
-import MetricChartBox from "@/components/Metrics/MetricChartBox";
-import { ParsedMetricPeriod } from "@/hooks/queries";
-import { Metrics } from "@polar-sh/api";
+import MetricChartBox from '@/components/Metrics/MetricChartBox'
+import { ParsedMetricPeriod } from '@/hooks/queries'
+import { dateToInterval } from '@/utils/metrics'
+import { Metrics } from '@polar-sh/api'
 
 export interface ProductMetricsViewProps {
-	metrics?: Metrics;
-	periods?: ParsedMetricPeriod[];
-	loading: boolean;
+  metrics?: Metrics
+  periods?: ParsedMetricPeriod[]
+  loading: boolean
 }
 
 export const ProductMetricsView = ({
-	metrics,
-	loading,
-	periods,
+  metrics,
+  loading,
+  periods,
 }: ProductMetricsViewProps) => {
-	return (
-		<div className="flex flex-col gap-y-12">
-			<MetricChartBox
-				data={periods ?? []}
-				loading={loading}
-				metric={metrics?.orders}
-			/>
-			<MetricChartBox
-				data={periods ?? []}
-				loading={loading}
-				metric={metrics?.revenue}
-			/>
-		</div>
-	);
-};
+  return (
+    <div className="flex flex-col gap-y-12">
+      <MetricChartBox
+        data={periods ?? []}
+        loading={loading}
+        metric={metrics?.orders}
+        interval={dateToInterval(periods?.[0]?.timestamp ?? new Date())}
+      />
+      <MetricChartBox
+        data={periods ?? []}
+        loading={loading}
+        metric={metrics?.revenue}
+        interval={dateToInterval(periods?.[0]?.timestamp ?? new Date())}
+      />
+    </div>
+  )
+}

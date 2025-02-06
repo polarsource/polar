9b838d095018987bf32b9864dc8e5ca58905c750
François Voron <fvoron@gmail.com>
2025-02-06 10:19:38 +0100

    clients: update apps/web dependencies

--- clients/apps/web/src/components/Shared/PageNotFound.tsx
+++ clients/apps/web/src/components/Shared/PageNotFound.tsx
@@ -2,28 +2,18 @@
 
 import Button from '@polar-sh/ui/components/atoms/Button'
 import Link from 'next/link'
-import { StaggerReveal } from './StaggerReveal'
 
 const PageNotFound = () => {
   return (
-    <StaggerReveal
-      className="flex h-screen w-full flex-col items-center justify-center gap-y-16 px-12"
-      transition={{ staggerChildren: 0.2 }}
-    >
-      <StaggerReveal.Child transition={{ duration: 1 }}>
-        <h1 className="text-4xl text-blue-500 dark:text-blue-400">404</h1>
-      </StaggerReveal.Child>
-      <StaggerReveal.Child transition={{ duration: 1 }}>
-        <h1 className="max-w-xl text-center text-4xl leading-normal">
-          We couldn&apos;t find the page you were looking for
-        </h1>
-      </StaggerReveal.Child>
-      <StaggerReveal.Child transition={{ duration: 1 }}>
-        <Link href={`/`}>
-          <Button>Take me home</Button>
-        </Link>
-      </StaggerReveal.Child>
-    </StaggerReveal>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-y-16 px-12">
+      <h1 className="text-4xl text-blue-500 dark:text-blue-400">404</h1>
+      <h1 className="max-w-xl text-center text-4xl leading-normal">
+        We couldn&apos;t find the page you were looking for
+      </h1>
+      <Link href={`/`}>
+        <Button>Take me home</Button>
+      </Link>
+    </div>
   )
 }
 

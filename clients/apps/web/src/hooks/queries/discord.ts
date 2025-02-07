3650fdbcefb9d23eb395c4d8072d4c2f5dacd79f
François Voron <fvoron@gmail.com>
2025-02-07 15:42:23 +0100

    clients/web: move discord queries to new client

--- clients/apps/web/src/hooks/queries/discord.ts
+++ clients/apps/web/src/hooks/queries/discord.ts
@@ -1,18 +1,25 @@
-import { api } from '@/utils/api'
-import { DiscordGuild } from '@polar-sh/api'
+import { api } from '@/utils/client'
+import { components, unwrap } from '@polar-sh/client'
 import { UseQueryResult, useQuery } from '@tanstack/react-query'
 import { defaultRetry } from './retry'
 
 export const useDiscordGuild: (
   guildToken?: string,
-) => UseQueryResult<DiscordGuild, Error> = (guildToken?: string) =>
+) => UseQueryResult<components['schemas']['DiscordGuild'], Error> = (
+  guildToken?: string,
+) =>
   useQuery({
     queryKey: ['discord', 'guild', guildToken],
     queryFn: () =>
-      api.integrationsDiscord.discordGuildLookup({
-        guildToken: guildToken || '',
-      }),
-
+      unwrap(
+        api.GET('/v1/integrations/discord/guild/lookup', {
+          params: {
+            query: {
+              guild_token: guildToken ?? '',
+            },
+          },
+        }),
+      ),
     retry: defaultRetry,
     enabled: !!guildToken,
   })

0b99a2d99feb71c03d4341c053aec3f54bef9a5b
Emil Widlund <hello@emilwidlund.com>
2025-02-08 00:57:00 +0100

    clients/issues: fix 404 on issue pages

---
+++ clients/apps/web/src/app/(main)/[organization]/[repo]/issues/status/page.tsx
@@ -0,0 +1,58 @@
+import Status from '@/components/Pledge/Status'
+import { api } from '@/utils/api'
+import { resolveRepositoryPath } from '@/utils/repository'
+import { Pledge, ResponseError } from '@polar-sh/api'
+import { notFound } from 'next/navigation'
+
+const cacheConfig = {
+  cache: 'no-store',
+} as const
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { organization: string; repo: string; number: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const resolvedRepositoryOrganization = await resolveRepositoryPath(
+    api,
+    params.organization,
+    params.repo,
+    cacheConfig,
+  )
+
+  if (!resolvedRepositoryOrganization) {
+    notFound()
+  }
+
+  const [, organization] = resolvedRepositoryOrganization
+
+  const paymentIntentId = searchParams['payment_intent_id']
+  if (typeof paymentIntentId !== 'string') {
+    notFound()
+  }
+
+  let pledge: Pledge
+
+  try {
+    pledge = await api.pledges.create({
+      body: {
+        payment_intent_id: paymentIntentId,
+      },
+    })
+  } catch (e) {
+    if (e instanceof ResponseError) {
+      if (e.response.status === 404) {
+        notFound()
+      }
+    }
+    throw e
+  }
+
+  const email = searchParams['email'] as string | undefined
+
+  // TODO: Handle different statuses than success... #happy-path-alpha-programming
+
+  return <Status pledge={pledge} organization={organization} email={email} />
+}

aaf7fe9a698ceb84df08cb786c35199524cd230a
François Voron <fvoron@gmail.com>
2025-02-13 15:46:46 +0100

    clients/web: mock Geist font in Storybook config to avoid build error
    Ref:
    
    * https://github.com/vercel/geist-font/issues/59
    * https://github.com/vercel/geist-font/issues/13
    * https://storybook.js.org/docs/get-started/frameworks/nextjs#with-module-aliases

---
+++ clients/apps/web/.storybook/geist-mono.mock.ts
@@ -0,0 +1,5 @@
+export const GeistMono = {
+  style: {
+    fontFamily: 'GeistMono',
+  },
+}
--- clients/apps/web/.storybook/main.ts
+++ clients/apps/web/.storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs'
-import { dirname, join } from 'path'
+import { dirname, join, resolve } from 'path'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -19,6 +19,16 @@ const config: StorybookConfig = {
   ${head}
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
   `,
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'geist/font/mono': resolve(__dirname, './geist-mono.mock.ts'),
+        'geist/font/sans': resolve(__dirname, './geist-sans.mock.ts'),
+      }
+    }
+    return config
+  },
 }
 export default config
 

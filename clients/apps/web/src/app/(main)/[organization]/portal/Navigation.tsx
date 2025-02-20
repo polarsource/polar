66c2064feba0e1ab12b1ca7bee14305987df7821
Emil Widlund <hello@emilwidlund.com>
2025-02-20 13:45:18 +0100

    improve customer portal

--- clients/apps/web/src/app/(main)/[organization]/portal/Navigation.tsx
+++ clients/apps/web/src/app/(main)/[organization]/portal/Navigation.tsx
@@ -5,11 +5,14 @@ import Link from 'next/link'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { twMerge } from 'tailwind-merge'
 
-const links = [
-  { href: '/', label: 'Overview' },
-  { href: '/subscriptions', label: 'Subscriptions' },
-  { href: '/usage', label: 'Usage' },
-  { href: '/orders', label: 'Orders' },
+const links = (organization: schemas['Organization']) => [
+  { href: `/${organization.slug}/portal`, label: 'Overview' },
+  {
+    href: `/${organization.slug}/portal/subscriptions`,
+    label: 'Subscriptions',
+  },
+  { href: `/${organization.slug}/portal/orders`, label: 'Orders' },
+  { href: `/${organization.slug}/portal/usage`, label: 'Usage' },
 ]
 
 export const Navigation = ({
@@ -22,7 +25,7 @@ export const Navigation = ({
 
   const buildPath = (path: string) => {
     const url = new URL(window.location.origin + currentPath)
-    url.pathname = `/${organization.slug}/portal${path}`
+    url.pathname = path
 
     for (const [key, value] of searchParams.entries()) {
       url.searchParams.set(key, value)
@@ -33,13 +36,13 @@ export const Navigation = ({
 
   return (
     <nav className="flex w-64 flex-col gap-y-2 py-12 pr-12">
-      {links.map((link) => (
+      {links(organization).map((link) => (
         <Link
           key={link.href}
           href={buildPath(link.href)}
           className={twMerge(
             'dark:text-polar-500 text-gray-500',
-            currentPath.includes(link.href) && 'text-blue-500 dark:text-white',
+            currentPath === link.href && 'text-blue-500 dark:text-white',
           )}
         >
           {link.label}

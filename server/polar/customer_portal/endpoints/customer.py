8f7d5c33ae2e379706c62b71fe4de618b26c9f31
François Voron <fvoron@gmail.com>
2025-02-21 14:11:11 +0100

    server/customer_portal: add endpoints to list, add and delete payment methods

--- server/polar/customer_portal/endpoints/customer.py
+++ server/polar/customer_portal/endpoints/customer.py
@@ -1,12 +1,10 @@
-from typing import Annotated
-
-from fastapi import Depends, Path, Request
-from pydantic import UUID4
+from fastapi import Depends, Request
 from sse_starlette import EventSourceResponse
 
 from polar.eventstream.endpoints import subscribe
 from polar.eventstream.service import Receivers
 from polar.exceptions import ResourceNotFound
+from polar.kit.pagination import ListResource
 from polar.models import Customer
 from polar.openapi import APITag
 from polar.postgres import AsyncSession, get_db_session
@@ -14,16 +12,15 @@ from polar.redis import Redis, get_redis
 from polar.routing import APIRouter
 
 from .. import auth
-from ..schemas.customer import CustomerPortalCustomer, CustomerPortalCustomerUpdate
+from ..schemas.customer import (
+    CustomerPaymentMethod,
+    CustomerPaymentMethodCreate,
+    CustomerPortalCustomer,
+    CustomerPortalCustomerUpdate,
+)
 from ..service.customer import customer as customer_service
 
 router = APIRouter(prefix="/customers", tags=["customers", APITag.documented])
-
-CustomerID = Annotated[UUID4, Path(description="The customer ID.")]
-CustomerNotFound = {
-    "description": "Customer not found.",
-    "model": ResourceNotFound.schema(),
-}
 
 
 @router.get("/stream", include_in_schema=False)
@@ -59,3 +56,52 @@ async def update(
 ) -> Customer:
     """Update authenticated customer."""
     return await customer_service.update(session, auth_subject.subject, customer_update)
+
+
+@router.get(
+    "/me/payment-methods",
+    summary="Get Customer Payment Methods",
+    response_model=ListResource[CustomerPaymentMethod],
+)
+async def get_payment_methods(
+    auth_subject: auth.CustomerPortalRead,
+) -> ListResource[CustomerPaymentMethod]:
+    """Get saved payment methods of the authenticated customer."""
+    return await customer_service.list_payment_methods(auth_subject.subject)
+
+
+@router.post(
+    "/me/payment-methods",
+    summary="Add Customer Payment Method",
+    status_code=201,
+    responses={
+        201: {"description": "Payment method created."},
+    },
+    response_model=CustomerPaymentMethod,
+)
+async def add_payment_method(
+    auth_subject: auth.CustomerPortalRead,
+    payment_method_create: CustomerPaymentMethodCreate,
+    session: AsyncSession = Depends(get_db_session),
+) -> CustomerPaymentMethod:
+    """Add a payment method to the authenticated customer."""
+    return await customer_service.add_payment_method(
+        session, auth_subject.subject, payment_method_create
+    )
+
+
+@router.delete(
+    "/me/payment-methods/{id}",
+    summary="Delete Customer Payment Method",
+    status_code=204,
+    responses={
+        204: {"description": "Payment method deleted."},
+        404: {
+            "description": "Payment method not found.",
+            "model": ResourceNotFound.schema(),
+        },
+    },
+)
+async def delete_payment_method(id: str, auth_subject: auth.CustomerPortalRead) -> None:
+    """Delete a payment method from the authenticated customer."""
+    return await customer_service.delete_payment_method(auth_subject.subject, id)
--- server/polar/customer_portal/schemas/customer.py
+++ server/polar/customer_portal/schemas/customer.py
@@ -1,7 +1,11 @@
-from typing import Annotated
+from typing import Annotated, Literal
+
+from pydantic import AfterValidator, AliasPath, Field, TypeAdapter
 
 from polar.kit.address import Address
+from polar.kit.http import get_safe_return_url
 from polar.kit.schemas import (
+    EmailStrDNS,
     EmptyStrToNoneValidator,
     IDSchema,
     Schema,
@@ -25,6 +29,40 @@ class CustomerPortalCustomer(IDSchema, TimestampedSchema):
 
 
 class CustomerPortalCustomerUpdate(Schema):
+    email: Annotated[EmailStrDNS | None, EmptyStrToNoneValidator] = None
     name: Annotated[str | None, EmptyStrToNoneValidator] = None
     billing_address: Address | None = None
     tax_id: Annotated[str | None, EmptyStrToNoneValidator] = None
+
+
+class PaymentMethodGeneric(Schema):
+    id: str
+    type: str
+
+
+class PaymentMethodCardData(Schema):
+    brand: str
+    last4: str
+    exp_month: int
+    exp_year: int
+    wallet: str | None = Field(
+        default=None, validation_alias=AliasPath("wallet", "type")
+    )
+
+
+class PaymentMethodCard(PaymentMethodGeneric):
+    type: Literal["card"]
+    card: PaymentMethodCardData
+
+
+CustomerPaymentMethod = PaymentMethodCard | PaymentMethodGeneric
+
+CustomerPaymentMethodTypeAdapter: TypeAdapter[CustomerPaymentMethod] = TypeAdapter(
+    CustomerPaymentMethod
+)
+
+
+class CustomerPaymentMethodCreate(Schema):
+    confirmation_token_id: str
+    set_default: bool
+    return_url: Annotated[str, AfterValidator(get_safe_return_url)]

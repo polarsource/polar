e62cdf0e3cf63f4f62cb765bc432def319b3d38a
François Voron <fvoron@gmail.com>
2025-02-21 10:27:47 +0100

    docs: tweak OpenAPI update script

--- docs/update-schema.sh
+++ docs/update-schema.sh
@@ -2,16 +2,26 @@
 
 set -e
 
+openapi_file=openapi.yaml
+config_file=docs.json
+
 # Check if jq is installed
 if ! command -v jq &> /dev/null; then
   echo "jq is not installed. Please install jq to use this script."
   exit 1
 fi
 
-openapi_file=openapi.yaml
-config_file=docs.json
+# Check if there is exactly one positional argument
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <openapi_schema_url>"
+  exit 1
+fi
 
-# Create a new MDX file for each webhook
+# Download latest OpenAPI schema
+curl -s -o openapi.yaml "$1"
+echo "Downloaded OpenAPI schema from $1"
+
+# Create a new MDX file for each webhook, if it doesn't exist
 file="$1"
 jq -r '.webhooks | to_entries[] | "\(.key) \(.value.post.requestBody.content["application/json"].schema["$ref"])"' "$openapi_file" | while read -r key ref; do
   schema=${ref##*/}
@@ -25,9 +35,11 @@ title: $key
 openapi-schema: $schema
 ---
 EOL
-  # Add the $schema_file path to the $config_file JSON file
+  # Add the $schema_file path to the docs navigation
   jq --arg schema_page "$schema_page" \
   '(.navigation.anchors[] | select(.anchor == "API Reference") | .groups[] | select(.group == "Webhooks") | .pages) += [$schema_page]' \
   "$config_file" > tmp.$$.json && mv tmp.$$.json "$config_file"
+  else
+    echo "$schema_file already exists, skipping"
   fi
 done

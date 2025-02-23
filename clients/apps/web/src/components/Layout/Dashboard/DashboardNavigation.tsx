e31138a159ec9bf9ed0c586627b90d69e54819e4
Emil Widlund <hello@emilwidlund.com>
2025-02-23 16:19:10 +0100

    clients: refactor dashboard sidebar

---
+++ clients/apps/web/src/components/Layout/Dashboard/DashboardNavigation.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from '@polar-sh/ui/components/atoms/Sidebar'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { twMerge } from 'tailwind-merge'
+import {
+  useFundingRoutes,
+  useGeneralRoutes,
+  useOrganizationRoutes,
+} from '../../Dashboard/navigation'
+
+const MaintainerNavigation = () => {
+  const orgContext = useContext(MaintainerOrganizationContext)
+  const org = orgContext?.organization
+
+  const generalRoutesList = useGeneralRoutes(org)
+  const fundingRoutes = useFundingRoutes(org)
+  const organizationRoutes = useOrganizationRoutes(org)
+
+  const { state } = useSidebar()
+
+  const isCollapsed = state === 'collapsed'
+
+  const dashboardRoutes = [
+    ...generalRoutesList,
+    ...fundingRoutes,
+    ...organizationRoutes,
+  ]
+
+  if (!org) {
+    return <></>
+  }
+
+  return (
+    <SidebarMenu>
+      {dashboardRoutes.map((route) => (
+        <SidebarMenuItem key={route.link}>
+          <SidebarMenuButton asChild>
+            <Link
+              key={route.link}
+              className={twMerge(
+                'flex flex-row items-center rounded-lg border border-transparent px-2 transition-colors dark:border-transparent',
+                route.isActive
+                  ? 'dark:bg-polar-900 dark:border-polar-800 border-gray-200 bg-white text-black shadow-sm dark:text-white'
+                  : 'dark:text-polar-500 dark:hover:text-polar-200 text-gray-500 hover:text-black',
+                isCollapsed && '!dark:text-polar-600',
+              )}
+              href={route.link}
+            >
+              {'icon' in route && route.icon ? (
+                <span
+                  className={twMerge(
+                    'flex flex-col items-center justify-center overflow-visible rounded-full bg-transparent text-[15px]',
+                    route.isActive
+                      ? 'text-blue-500 dark:text-white'
+                      : 'bg-transparent',
+                  )}
+                >
+                  {route.icon}
+                </span>
+              ) : undefined}
+              <span className="ml-2 text-sm font-medium">{route.title}</span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </SidebarMenu>
+  )
+}
+
+export default MaintainerNavigation
---
+++ clients/apps/web/src/components/Layout/Dashboard/DashboardSidebar.tsx
@@ -0,0 +1,196 @@
+import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
+import { CONFIG } from '@/utils/config'
+import { schemas } from '@polar-sh/client'
+import Avatar from '@polar-sh/ui/components/atoms/Avatar'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+  useSidebar,
+} from '@polar-sh/ui/components/atoms/Sidebar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@polar-sh/ui/components/ui/dropdown-menu'
+import { ChevronDown } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useContext } from 'react'
+import { BrandingMenu } from '../Public/BrandingMenu'
+import MaintainerNavigation from './DashboardNavigation'
+
+export const DashboardSidebar = () => {
+  const router = useRouter()
+  const { state } = useSidebar()
+
+  const { organization, organizations } = useContext(
+    MaintainerOrganizationContext,
+  )
+
+  const isCollapsed = state === 'collapsed'
+
+  const navigateToOrganization = (org: schemas['Organization']) => {
+    router.push(`/dashboard/${org.slug}`)
+  }
+
+  return (
+    <Sidebar variant="inset" collapsible="icon">
+      <SidebarHeader className="flex flex-row items-center justify-between">
+        <BrandingMenu size={32} />
+        {!isCollapsed && <SidebarTrigger />}
+      </SidebarHeader>
+      <SidebarContent className="gap-4 px-2 py-4">
+        <MaintainerNavigation />
+      </SidebarContent>
+
+      <SidebarFooter>
+        {isCollapsed ? (
+          <SidebarTrigger />
+        ) : (
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <Avatar
+                      name={organization?.name}
+                      avatar_url={organization?.avatar_url}
+                      className="h-6 w-6"
+                    />
+                    {organization?.name}
+                    <ChevronDown className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  {organizations.map((org) => (
+                    <DropdownMenuItem
+                      key={org.id}
+                      className="flex flex-row items-center gap-x-2"
+                      onClick={() => navigateToOrganization(org)}
+                    >
+                      <Avatar
+                        name={org.name}
+                        avatar_url={org.avatar_url}
+                        className="h-6 w-6"
+                      />
+                      {org.name}
+                    </DropdownMenuItem>
+                  ))}
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    onClick={() => router.push('/dashboard/create')}
+                  >
+                    New Organization
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => router.push('/settings')}>
+                    Account Settings
+                  </DropdownMenuItem>
+                  {!CONFIG.IS_SANDBOX && (
+                    <DropdownMenuItem
+                      onClick={() =>
+                        router.push('https://sandbox.polar.sh/start')
+                      }
+                    >
+                      Go to Sandbox
+                    </DropdownMenuItem>
+                  )}
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    onClick={() =>
+                      router.push(`${CONFIG.BASE_URL}/v1/auth/logout`)
+                    }
+                  >
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        )}
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
+
+/* const DashboardSidebar = () => {
+  const [scrollTop, setScrollTop] = useState(0)
+  const { currentUser } = useAuth()
+
+  const handleScroll: UIEventHandler<HTMLDivElement> = useCallback((e) => {
+    setScrollTop(e.currentTarget.scrollTop)
+  }, [])
+
+  const shouldRenderUpperBorder = useMemo(() => scrollTop > 0, [scrollTop])
+
+  const upperScrollClassName = shouldRenderUpperBorder
+    ? 'border-b dark:border-b-polar-700 border-b-gray-200'
+    : ''
+
+  if (!currentUser) {
+    return <></>
+  }
+
+  return (
+    <aside
+      className={twMerge(
+        'flex h-full w-full flex-shrink-0 flex-col justify-between gap-y-4 overflow-y-auto md:w-[220px] md:overflow-y-visible',
+      )}
+    >
+      <div className="flex h-full flex-col gap-y-6">
+        <div className="hidden md:flex">
+          <BrandingMenu />
+        </div>
+
+        <div className={upperScrollClassName}>
+          <DashboardProfileDropdown />
+        </div>
+
+        <div
+          className="flex w-full flex-grow flex-col gap-y-12 md:h-full md:justify-between md:overflow-y-auto"
+          onScroll={handleScroll}
+        >
+          <div className="flex flex-col gap-y-12">
+            <MaintainerNavigation />
+          </div>
+        </div>
+      </div>
+      <DashboardBottom authenticatedUser={currentUser} />
+    </aside>
+  )
+}
+
+const DashboardBottom = ({
+  authenticatedUser,
+}: {
+  authenticatedUser: schemas['UserRead']
+}) => {
+  return (
+    <div className="flex flex-row items-center justify-between gap-x-5">
+      <div className="flex w-full min-w-0 flex-grow-0 flex-row items-center gap-x-2">
+        <PublicProfileDropdown
+          authenticatedUser={authenticatedUser}
+          anchor="bottombar"
+        />
+        <div className="flex w-full min-w-0 flex-col">
+          <span className="w-full truncate text-xs">
+            {authenticatedUser.email}
+          </span>
+          <span className="dark:text-polar-500 text-xs text-gray-500">
+            User Account
+          </span>
+        </div>
+      </div>
+      <NotificationsPopover />
+    </div>
+  )
+} */

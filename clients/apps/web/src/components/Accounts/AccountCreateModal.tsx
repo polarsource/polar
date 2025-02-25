0da6ddbf472c57234fd5160781e669d849a3d52a
Birk Jernstrom <birkjernstrom@gmail.com>
2025-02-25 22:45:03 +0100

    clients: Refactor Account Setup to React Form

--- clients/apps/web/src/components/Accounts/AccountCreateModal.tsx
+++ clients/apps/web/src/components/Accounts/AccountCreateModal.tsx
@@ -2,11 +2,26 @@ import { getValidationErrorsMap } from '@/utils/api/errors'
 import { api } from '@/utils/client'
 import { CONFIG } from '@/utils/config'
 import { isValidationError, schemas } from '@polar-sh/client'
+import { setValidationErrors } from '@/utils/api/errors'
 import Button from '@polar-sh/ui/components/atoms/Button'
 import CountryPicker from '@polar-sh/ui/components/atoms/CountryPicker'
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
+import { useForm, useFormContext } from 'react-hook-form'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@polar-sh/ui/components/ui/form'
 
 const stripeConnectWhitelist = CONFIG.STRIPE_COUNTRIES_WHITELIST_CSV.split(',')
+
+interface AccountForm {
+  country: string
+}
 
 const AccountCreateModal = ({
   forOrganizationId,
@@ -17,58 +32,19 @@ const AccountCreateModal = ({
   forUserId?: string
   returnPath: string
 }) => {
-  const [country, setCountry] = useState<string>('US')
-  const [validationErrors, setValidationErrors] = useState<
-    Record<string, string[]>
-  >({})
-  const [errorMessage, setErrorMessage] = useState<string | undefined>()
+  const form = useForm<AccountForm>({
+    defaultValues: {
+      country: 'US',
+    }
+  })
+
+  const {
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = form
 
   const [loading, setLoading] = useState(false)
-
-  const resetErrors = () => {
-    setErrorMessage(undefined)
-    setValidationErrors({})
-  }
-
-  const onChangeCountry = (countryCode: string) => {
-    resetErrors()
-    setCountry(countryCode)
-  }
-
-  const onConfirm = async () => {
-    setLoading(true)
-
-    const { data: account, error } = await api.POST('/v1/accounts', {
-      body: {
-        account_type: 'stripe',
-        country,
-      },
-    })
-
-    if (error) {
-      if (isValidationError(error.detail)) {
-        setValidationErrors(getValidationErrorsMap(error.detail))
-      } else {
-        setErrorMessage(error.detail)
-      }
-      return
-    }
-
-    if (forOrganizationId) {
-      await api.PATCH('/v1/organizations/{id}/account', {
-        params: { path: { id: forOrganizationId } },
-        body: { account_id: account.id },
-      })
-    }
-    if (forUserId) {
-      await api.PATCH('/v1/users/me/account', {
-        body: { account_id: account.id },
-      })
-    }
-
-    setLoading(false)
-    await goToOnboarding(account)
-  }
 
   const goToOnboarding = async (account: schemas['Account']) => {
     setLoading(true)
@@ -91,45 +67,106 @@ const AccountCreateModal = ({
     window.location.href = data.url
   }
 
+  const onSubmit = useCallback(
+    async (data: AccountForm) => {
+      setLoading(true)
+
+      const { data: account, error } = await api.POST('/v1/accounts', {
+        body: {
+          account_type: 'stripe',
+          country: data.country,
+        },
+      })
+
+      if (error) {
+        if (isValidationError(error.detail)) {
+          setValidationErrors(error.detail, setError)
+        } else {
+          setError('root', { message: error.detail })
+        }
+        return
+      }
+
+      if (forOrganizationId) {
+        await api.PATCH('/v1/organizations/{id}/account', {
+          params: { path: { id: forOrganizationId } },
+          body: { account_id: account.id },
+        })
+      }
+      if (forUserId) {
+        await api.PATCH('/v1/users/me/account', {
+          body: { account_id: account.id },
+        })
+      }
+
+      setLoading(false)
+      await goToOnboarding(account)
+    },
+    [setLoading, forOrganizationId, forUserId, goToOnboarding, setError]
+  )
+
   return (
     <>
       <div className="flex flex-col gap-y-6 overflow-auto p-8">
         <h2>Setup payout account</h2>
-        <form className="flex flex-col gap-y-4">
-          <CountryPicker
-            onChange={onChangeCountry}
-            value={country}
-            allowedCountries={stripeConnectWhitelist}
-          />
-          <p className="dark:text-polar-500 mt-2 text-justify text-xs text-gray-500">
-            If this is a personal account, please select your country of
-            residence. If this is an organization or business, select the
-            country of tax residency.
-          </p>
-          {validationErrors.country?.map((error) => (
-            <p key={error} className="mt-2 text-xs text-red-500">
-              {error}
-            </p>
-          ))}
 
-          {errorMessage && (
-            <p className="text-xs text-red-500">{errorMessage}</p>
-          )}
-          {validationErrors.__root__?.map((error) => (
-            <p key={error} className="mt-2 text-xs text-red-500">
-              {error}
-            </p>
-          ))}
-          <Button
-            className="self-start"
-            onClick={onConfirm}
-            loading={loading}
-            disabled={loading}
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-y-4"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            Set up account
-          </Button>
-        </form>
+            <AccountCountry />
+            {errors.root && (
+              <p className="text-destructive-foreground text-sm">
+                {errors.root.message}
+              </p>
+            )}
+            <Button
+              className="self-start"
+              type="submit"
+              loading={loading}
+              disabled={loading}
+            >
+              Set up account
+            </Button>
+          </form>
+        </Form>
       </div>
+    </>
+  )
+}
+
+import React from 'react'
+
+export const AccountCountry = () => {
+  const { control } = useFormContext<AccountForm>()
+
+  return (
+    <>
+      <FormField
+        control={control}
+        name="country"
+        render={({ field }) => {
+          return (
+            <FormItem>
+              <FormLabel>Country</FormLabel>
+              <FormControl>
+                <CountryPicker
+                  value={field.value || undefined}
+                  onChange={field.onChange}
+                  allowedCountries={stripeConnectWhitelist}
+                />
+              </FormControl>
+              <FormMessage />
+              <FormDescription>
+                If this is a personal account, please select your country of
+                residence. If this is an organization or business, select the
+                country of tax residency.
+              </FormDescription>
+            </FormItem>
+          )
+        }}
+      />
     </>
   )
 }

dac6f641a063a9d40245f21ff69b75d1eef357e5
François Voron <fvoron@gmail.com>
2025-02-27 14:47:07 +0100

    server/auth: tweak logic that adds scope docstring to not ignore explicitly set description

--- server/polar/auth/routing.py
+++ server/polar/auth/routing.py
@@ -18,39 +18,46 @@ class DocumentedAuthSubjectAPIRoute(APIRoute):
     def __init__(
         self, path: str, endpoint: Callable[..., typing.Any], **kwargs: typing.Any
     ) -> None:
-        for param in typing.get_type_hints(endpoint, include_extras=True).values():
-            if typing.get_origin(param) is not typing.Annotated:
-                continue
+        openapi_extra = kwargs.get("openapi_extra") or {}
+        # Check we haven't already added the allowed subjects
+        if "x-polar-allowed-subjects" not in openapi_extra:
+            for param in typing.get_type_hints(endpoint, include_extras=True).values():
+                if typing.get_origin(param) is not typing.Annotated:
+                    continue
 
-            metadata = param.__metadata__
-            if len(metadata) == 0 or not isinstance(metadata[0], Depends):
-                continue
+                metadata = param.__metadata__
+                if len(metadata) == 0 or not isinstance(metadata[0], Depends):
+                    continue
 
-            dependency = metadata[0].dependency
-            if not isinstance(dependency, _Authenticator):
-                continue
+                dependency = metadata[0].dependency
+                if not isinstance(dependency, _Authenticator):
+                    continue
 
-            allowed_subjects = dependency.allowed_subjects
-            required_scopes = dependency.required_scopes
+                allowed_subjects = dependency.allowed_subjects
+                required_scopes = dependency.required_scopes
 
-            allowed_subjects_names = sorted(
-                [allowed_subject.__name__ for allowed_subject in allowed_subjects]
-            )
-            openapi_extra = kwargs.get("openapi_extra") or {}
-            kwargs["openapi_extra"] = {
-                "x-polar-allowed-subjects": allowed_subjects_names,
-                **openapi_extra,
-            }
+                allowed_subjects_names = sorted(
+                    [allowed_subject.__name__ for allowed_subject in allowed_subjects]
+                )
 
-            description = inspect.cleandoc(endpoint.__doc__ or "")
-            if required_scopes:
-                scopes_list = " ".join(
+                kwargs["openapi_extra"] = {
+                    "x-polar-allowed-subjects": allowed_subjects_names,
+                    **openapi_extra,
+                }
+
+                description = kwargs["description"] or inspect.cleandoc(
+                    endpoint.__doc__ or ""
+                )
+                scopes_list = [
                     f"`{s}`"
                     for s in sorted(required_scopes or [])
                     if s not in RESERVED_SCOPES
-                )
-                description += f"\n\n**Scopes**: {scopes_list}"
-            kwargs["description"] = description
+                ]
+                if scopes_list:
+                    description += f"\n\n**Scopes**: {' '.join(scopes_list)}"
+                kwargs["description"] = description
+
+                break
 
         super().__init__(path, endpoint, **kwargs)
 

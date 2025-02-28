0d26d3f19c485eba96ff14348cbec08c09bf4387
Emil Widlund <hello@emilwidlund.com>
2025-02-28 15:17:42 +0100

    fix mobile nav

--- clients/apps/web/src/app/(main)/[organization]/portal/Navigation.tsx
+++ clients/apps/web/src/app/(main)/[organization]/portal/Navigation.tsx
@@ -10,7 +10,7 @@ import {
   SelectValue,
 } from '@polar-sh/ui/components/atoms/Select'
 import Link from 'next/link'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { twMerge } from 'tailwind-merge'
 
 const links = (organization: schemas['Organization']) => [
@@ -24,6 +24,7 @@ export const Navigation = ({
 }: {
   organization: schemas['Organization']
 }) => {
+  const router = useRouter()
   const currentPath = usePathname()
   const searchParams = useSearchParams()
   const { isFeatureEnabled } = usePostHog()
@@ -43,41 +44,49 @@ export const Navigation = ({
     return url.toString()
   }
 
+  const filteredLinks = links(organization).filter(({ label }) =>
+    label === 'Usage' ? isFeatureEnabled('usage_based_billing') : true,
+  )
+
   return (
     <>
       <nav className="hidden w-64 flex-col gap-y-1 py-12 md:flex">
-        {links(organization)
-          .filter(({ label }) =>
-            label === 'Usage' ? isFeatureEnabled('usage_based_billing') : true,
-          )
-          .map((link) => (
-            <Link
-              key={link.href}
-              href={buildPath(link.href)}
-              className={twMerge(
-                'dark:text-polar-500 dark:hover:bg-polar-800 rounded-xl border border-transparent px-4 py-2 font-medium text-gray-500 transition-colors duration-75 hover:bg-gray-100',
-                currentPath === link.href &&
-                  'dark:bg-polar-800 dark:border-polar-700 bg-gray-100 text-black dark:text-white',
-              )}
-            >
-              {link.label}
-            </Link>
-          ))}
+        {filteredLinks.map((link) => (
+          <Link
+            key={link.href}
+            href={buildPath(link.href)}
+            className={twMerge(
+              'dark:text-polar-500 dark:hover:bg-polar-800 rounded-xl border border-transparent px-4 py-2 font-medium text-gray-500 transition-colors duration-75 hover:bg-gray-100',
+              currentPath === link.href &&
+                'dark:bg-polar-800 dark:border-polar-700 bg-gray-100 text-black dark:text-white',
+            )}
+          >
+            {link.label}
+          </Link>
+        ))}
       </nav>
-      <Select defaultValue={currentPath}>
+      <Select
+        defaultValue={
+          filteredLinks.find(({ href }) => href === currentPath)?.label
+        }
+        onValueChange={(value) => {
+          router.push(
+            buildPath(
+              filteredLinks.find(({ label }) => label === value)?.href ?? '',
+            ),
+          )
+        }}
+      >
         <SelectTrigger className="md:hidden">
           <SelectValue>
-            {
-              links(organization).find(({ href }) => href === currentPath)
-                ?.label
-            }
+            {filteredLinks.find(({ href }) => href === currentPath)?.label}
           </SelectValue>
         </SelectTrigger>
         <SelectContent>
-          {links(organization).map((link) => (
-            <Link key={link.href} href={buildPath(link.href)}>
-              <SelectItem value={link.href}>{link.label}</SelectItem>
-            </Link>
+          {filteredLinks.map((link) => (
+            <SelectItem key={link.href} value={link.label}>
+              {link.label}
+            </SelectItem>
           ))}
         </SelectContent>
       </Select>

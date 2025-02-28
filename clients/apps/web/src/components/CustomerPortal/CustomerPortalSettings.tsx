5c4ac37730c311ffcb730faecd47bf1f37a65dec
Emil Widlund <hello@emilwidlund.com>
2025-02-28 14:25:45 +0100

    add billing address

--- clients/apps/web/src/components/CustomerPortal/CustomerPortalSettings.tsx
+++ clients/apps/web/src/components/CustomerPortal/CustomerPortalSettings.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import revalidate from '@/app/actions'
-import { useCustomerPaymentMethods } from '@/hooks/queries'
+import { useCustomer, useCustomerPaymentMethods } from '@/hooks/queries'
 import { createClientSideAPI } from '@/utils/client'
 import { schemas } from '@polar-sh/client'
 import Button from '@polar-sh/ui/components/atoms/Button'
@@ -10,6 +10,7 @@ import { Modal } from '../Modal'
 import { useModal } from '../Modal/useModal'
 import { Well, WellContent, WellHeader } from '../Shared/Well'
 import { AddPaymentMethodModal } from './AddPaymentMethodModal'
+import EditBillingDetails from './EditBillingDetails'
 import PaymentMethod from './PaymentMethod'
 
 interface CustomerPortalSettingsProps {
@@ -28,14 +29,18 @@ export const CustomerPortalSettings = ({
     hide: hideAddPaymentMethodModal,
     show: showAddPaymentMethodModal,
   } = useModal()
-
+  const { data: customer } = useCustomer(api)
   const { data: paymentMethods } = useCustomerPaymentMethods(api)
+
+  if (!customer) {
+    return null
+  }
 
   return (
     <div className="flex flex-col gap-y-8">
       <h3 className="text-2xl">Settings</h3>
       <Well className="dark:bg-polar-900 flex flex-col gap-y-6 bg-gray-50">
-        <WellHeader className="flex-row items-center justify-between">
+        <WellHeader className="flex-row items-start justify-between">
           <div className="flex flex-col gap-y-2">
             <h3 className="text-xl">Payment Methods</h3>
             <p className="dark:text-polar-500 text-gray-500">
@@ -51,6 +56,26 @@ export const CustomerPortalSettings = ({
           {paymentMethods?.items.map((pm) => (
             <PaymentMethod key={pm.id} paymentMethod={pm} api={api} />
           ))}
+        </WellContent>
+      </Well>
+      <Well className="dark:bg-polar-900 flex flex-col gap-y-6 bg-gray-50">
+        <WellHeader className="flex-row items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h3 className="text-xl">Billing Details</h3>
+            <p className="dark:text-polar-500 text-gray-500">
+              Update your billing details
+            </p>
+          </div>
+        </WellHeader>
+        <Separator className="dark:bg-polar-700" />
+        <WellContent>
+          <EditBillingDetails
+            api={api}
+            customer={customer}
+            onSuccess={() => {
+              revalidate(`customer_portal`)
+            }}
+          />
         </WellContent>
       </Well>
 

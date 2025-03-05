24fafbbe22ea34ceee7e06dd91003fdc9f1e76b8
Emil Widlund <hello@emilwidlund.com>
2025-03-05 09:38:54 +0100

    checkout: add image slideshow component to CHeckout

--- clients/apps/web/src/components/Checkout/CheckoutProductInfo.tsx
+++ clients/apps/web/src/components/Checkout/CheckoutProductInfo.tsx
@@ -4,8 +4,8 @@ import type { CheckoutProduct } from '@polar-sh/sdk/models/components/checkoutpr
 import type { Organization } from '@polar-sh/sdk/models/components/organization'
 import Avatar from '@polar-sh/ui/components/atoms/Avatar'
 import Markdown from 'markdown-to-jsx'
-import Image from 'next/image'
 import Link from 'next/link'
+import { Slideshow } from '../Products/Slideshow'
 
 interface CheckoutProductInfoProps {
   organization: Organization
@@ -37,14 +37,7 @@ const CheckoutProductInfo = ({
         <h1 className="text-3xl">{product.name}</h1>
       </div>
       {product.medias.length > 0 && (
-        <div className="dark:bg-polar-900 dark:border-polar-700 relative flex aspect-video w-full items-center justify-center overflow-hidden rounded-xl bg-gray-100 bg-cover bg-center lg:rounded-3xl dark:border">
-          <Image
-            alt={product.medias[0].name}
-            src={product.medias[0].publicUrl}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
+        <Slideshow images={product.medias.map((m) => m.publicUrl)} />
       )}
       {product.description ? (
         <div className="prose dark:prose-invert prose-headings:mt-8 prose-headings:font-medium prose-headings:text-black prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-md prose-h5:text-sm prose-h6:text-sm dark:prose-headings:text-white dark:text-polar-300 max-w-4xl text-gray-800">

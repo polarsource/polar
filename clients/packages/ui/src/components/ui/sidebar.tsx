f1bba57ac42c180d87679cfff506d1ca01828c4c
Florian Klampfer <mail@qwtel.com>
2025-03-06 14:01:01 +0100

    clients/packages/ui: fix double scrollbar

--- clients/packages/ui/src/components/ui/sidebar.tsx
+++ clients/packages/ui/src/components/ui/sidebar.tsx
@@ -141,7 +141,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              "group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar",
+              "group/sidebar-wrapper flex min-h-svh lg:overflow-clip w-full has-[[data-variant=inset]]:bg-sidebar",
               className
             )}
             ref={ref}

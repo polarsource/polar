e5a099ee00f0dc85908d2308615961b9bfcb7007
François Voron <fvoron@gmail.com>
2025-03-10 11:03:27 +0100

    clients/web: disable login button on magic link page
    Avoids double click and such provoking an already token used error

--- clients/apps/web/src/app/(main)/login/magic-link/authenticate/page.tsx
+++ clients/apps/web/src/app/(main)/login/magic-link/authenticate/page.tsx
@@ -1,38 +1,14 @@
-import LogoIcon from '@/components/Brand/LogoIcon'
-import { CONFIG } from '@/utils/config'
-import Button from '@polar-sh/ui/components/atoms/Button'
 import { Metadata } from 'next'
+import ClientPage from './ClientPage'
 
 export const metadata: Metadata = {
   title: 'Magic link confirmation',
 }
 
 export default function Page({
-  searchParams: { token, return_to },
+  searchParams,
 }: {
   searchParams: { token: string; return_to?: string }
 }) {
-  const urlSearchParams = new URLSearchParams({
-    ...(return_to && { return_to }),
-  })
-  return (
-    <form
-      className="dark:bg-polar-950 flex h-screen w-full grow items-center justify-center bg-gray-50"
-      method="post"
-      action={`${CONFIG.BASE_URL}/v1/magic_link/authenticate?${urlSearchParams.toString()}`}
-    >
-      <div id="polar-bg-gradient"></div>
-      <div className="flex w-80 flex-col items-center gap-4">
-        <LogoIcon size={60} className="mb-6 text-blue-500 dark:text-blue-400" />
-        <div className="dark:text-polar-400 text-center text-gray-500">
-          To complete the login verification process, please click the button
-          below:
-        </div>
-        <input type="hidden" name="token" value={token} />
-        <Button fullWidth size="lg" type="submit">
-          Log in
-        </Button>
-      </div>
-    </form>
-  )
+  return <ClientPage searchParams={searchParams} />
 }

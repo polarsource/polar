d2e0707f4f45f5d3ebcc55ee0f4fc723d501f113
Emil Widlund <hello@emilwidlund.com>
2025-03-27 01:42:53 +0800

    polish setting forms

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/settings/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/settings/ClientPage.tsx
@@ -1,52 +1,43 @@
-'use client'
+"use client";
 
-import { DashboardBody } from '@/components/Layout/DashboardLayout'
-import FeatureSettings from '@/components/Settings/FeatureSettings'
-import OrganizationAccessTokensSettings from '@/components/Settings/OrganizationAccessTokensSettings'
-import OrganizationProfileSettings from '@/components/Settings/OrganizationProfileSettings'
-import OrganizationSubscriptionSettings from '@/components/Settings/OrganizationSubscriptionSettings'
-import { Section, SectionDescription } from '@/components/Settings/Section'
-import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
-import { useContext } from 'react'
+import { DashboardBody } from "@/components/Layout/DashboardLayout";
+import OrganizationAccessTokensSettings from "@/components/Settings/OrganizationAccessTokensSettings";
+import OrganizationProfileSettings from "@/components/Settings/OrganizationProfileSettings";
+import OrganizationSubscriptionSettings from "@/components/Settings/OrganizationSubscriptionSettings";
+import { Section, SectionDescription } from "@/components/Settings/Section";
+import { MaintainerOrganizationContext } from "@/providers/maintainerOrganization";
+import { useContext } from "react";
 
 export default function ClientPage() {
-  const { organization: org } = useContext(MaintainerOrganizationContext)
+	const { organization: org } = useContext(MaintainerOrganizationContext);
 
-  return (
-    <DashboardBody>
-      <div className="flex flex-col gap-y-12">
-        <Section id="organization">
-          <SectionDescription
-            title="Organization"
-            description="Configure your organization settings"
-          />
-          <OrganizationProfileSettings organization={org} />
-        </Section>
+	return (
+		<DashboardBody>
+			<div className="flex flex-col gap-y-12">
+				<Section id="organization">
+					<SectionDescription
+						title="Organization"
+						description="Configure your organization settings"
+					/>
+					<OrganizationProfileSettings organization={org} />
+				</Section>
 
-        <Section id="subscriptions">
-          <SectionDescription
-            title="Subscriptions"
-            description="Configure how subscriptions are managed"
-          />
-          <OrganizationSubscriptionSettings organization={org} />
-        </Section>
+				<Section id="subscriptions">
+					<SectionDescription
+						title="Subscriptions"
+						description="Configure how subscriptions are managed"
+					/>
+					<OrganizationSubscriptionSettings organization={org} />
+				</Section>
 
-        <Section id="developers">
-          <SectionDescription
-            title="Developers"
-            description="Manage access tokens to authenticate with the Polar API"
-          />
-          <OrganizationAccessTokensSettings organization={org} />
-        </Section>
-
-        <Section id="features">
-          <SectionDescription
-            title="Additional Features"
-            description={`Legacy, experimental or early-access features.`}
-          />
-          <FeatureSettings organization={org} />
-        </Section>
-      </div>
-    </DashboardBody>
-  )
+				<Section id="developers">
+					<SectionDescription
+						title="Developers"
+						description="Manage access tokens to authenticate with the Polar API"
+					/>
+					<OrganizationAccessTokensSettings organization={org} />
+				</Section>
+			</div>
+		</DashboardBody>
+	);
 }

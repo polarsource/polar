2df660bd840f426ff5cd758adcded3ce080cbf25
Emil Widlund <hello@emilwidlund.com>
2025-03-27 12:52:55 +0400

    remove unused import

--- clients/apps/web/src/app/(main)/dashboard/(create)/layout.tsx
+++ clients/apps/web/src/app/(main)/dashboard/(create)/layout.tsx
@@ -1,15 +1,13 @@
-import LogoIcon from "@/components/Brand/LogoIcon";
-
 export default async function Layout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode
 }) {
-	return (
-		<div className="flex flex-col items-center px-4 py-12">
-			<div className="relative flex min-h-screen max-w-md w-full flex-col items-center md:py-0">
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center px-4 py-12">
+      <div className="relative flex min-h-screen w-full max-w-md flex-col items-center md:py-0">
+        {children}
+      </div>
+    </div>
+  )
 }

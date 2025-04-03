180a07d9405034e3a736589d7cbf9fc925f870cd
Emil Widlund <hello@emilwidlund.com>
2025-04-03 14:26:10 +0200

    improve event table

--- clients/apps/web/src/components/Events/Events.tsx
+++ clients/apps/web/src/components/Events/Events.tsx
@@ -1,10 +1,15 @@
+import { MaintainerOrganizationContext } from '@/providers/maintainerOrganization'
 import { schemas } from '@polar-sh/client'
 import Avatar from '@polar-sh/ui/components/atoms/Avatar'
-import { List, ListItem } from '@polar-sh/ui/components/atoms/List'
-import { useState } from 'react'
+import { List } from '@polar-sh/ui/components/atoms/List'
+import Link from 'next/link'
+import { useContext, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 const EventRow = ({ event }: { event: schemas['Event'] }) => {
   const [isExpanded, setIsExpanded] = useState(false)
+
+  const { organization } = useContext(MaintainerOrganizationContext)
 
   const handleToggleExpand = () => {
     setIsExpanded(!isExpanded)
@@ -22,71 +27,96 @@ const EventRow = ({ event }: { event: schemas['Event'] }) => {
   )
 
   return (
-    <ListItem
-      className="flex flex-col items-start justify-start gap-y-4 font-mono text-xs"
-      onSelect={handleToggleExpand}
-    >
-      <div className="flex flex-row items-center justify-start gap-x-8">
-        <div className="flex w-full max-w-[180px] flex-shrink-0 flex-col items-start">
-          <span className="font-mono text-xs capitalize">
-            {formattedTimestamp}
-          </span>
-        </div>
-        <div className="flex w-full max-w-[120px] flex-shrink-0 flex-col items-start">
-          <span>{event.name}</span>
-        </div>
-        <div className="flex w-full max-w-[180px] flex-shrink-0 flex-row items-center gap-x-2">
-          <Avatar
-            className="dark:bg-polar-900 text-xxs h-8 w-8 bg-white"
-            name={event.customer?.name ?? event.customer?.email ?? '—'}
-            avatar_url={event.customer?.avatar_url ?? null}
-          />
-          <div className="flex flex-col">
-            <span className="text-xs">
-              {event.customer
-                ? (event.customer.name ?? '—')
-                : (event.external_customer_id ?? '—')}
-            </span>
-            <span className="dark:text-polar-500 text-xxs text-xs text-gray-500">
-              {event.customer?.email ?? '—'}
+    <>
+      <tr
+        onClick={handleToggleExpand}
+        className={twMerge(
+          'dark:hover:bg-polar-800 cursor-pointer hover:bg-gray-50',
+          isExpanded && 'dark:bg-polar-800 bg-gray-50',
+        )}
+      >
+        <td className="px-4 py-2">
+          <div className="flex w-[180px] flex-shrink-0">
+            <span className="font-mono text-xs capitalize">
+              {formattedTimestamp}
             </span>
           </div>
-        </div>
-        <div className="flex w-full flex-col items-start">
+        </td>
+        <td className="px-4 py-2">
+          <div className="w-[120px] flex-shrink-0 font-mono text-sm">
+            <span>{event.name}</span>
+          </div>
+        </td>
+        <td className="px-4 py-2">
+          <Link
+            href={`/dashboard/${organization.slug}/customers?customerId=${event.customer?.id}`}
+            className="flex w-[180px] flex-shrink-0 items-center gap-x-3"
+            onClick={(e) => {
+              e.stopPropagation()
+            }}
+          >
+            <Avatar
+              className="dark:bg-polar-900 text-xxs h-8 w-8 bg-white"
+              name={event.customer?.name ?? event.customer?.email ?? '—'}
+              avatar_url={event.customer?.avatar_url ?? null}
+            />
+            <div className="flex flex-col">
+              <span className="text-xs">
+                {event.customer
+                  ? (event.customer.name ?? '—')
+                  : (event.external_customer_id ?? '—')}
+              </span>
+              <span className="dark:text-polar-500 text-xxs text-gray-500">
+                {event.customer?.email ?? '—'}
+              </span>
+            </div>
+          </Link>
+        </td>
+        <td className="px-4 py-2">
           <pre className="w-full min-w-0 truncate font-mono text-xs">
             {JSON.stringify(event.metadata, null, 2)}
           </pre>
-        </div>
-      </div>
+        </td>
+      </tr>
       {isExpanded && (
-        <pre className="dark:bg-polar-800 w-full rounded-md bg-gray-100 p-4 font-mono text-xs">
-          {JSON.stringify(event.metadata, null, 2)}
-        </pre>
+        <tr>
+          <td className="px-2 pb-2" colSpan={4}>
+            <pre className="dark:bg-polar-800 mt-2 w-full rounded-md bg-gray-100 p-4 font-mono text-xs">
+              {JSON.stringify(event.metadata, null, 2)}
+            </pre>
+          </td>
+        </tr>
       )}
-    </ListItem>
+    </>
   )
 }
 
 export const Events = ({ events }: { events: schemas['Event'][] }) => {
   return (
     <List className="flex flex-col" size="small">
-      <ListItem className="flex flex-row items-center justify-start gap-x-8 font-mono text-xs">
-        <div className="flex w-full max-w-[180px] flex-shrink-0 flex-col items-start">
-          <span className="font-mono text-xs capitalize">Timestamp</span>
-        </div>
-        <div className="flex w-full max-w-[120px] flex-shrink-0 flex-col items-start">
-          <span className="font-mono text-xs capitalize">Event</span>
-        </div>
-        <div className="flex w-full max-w-[180px] flex-shrink-0 flex-col items-start">
-          <span className="font-mono text-xs capitalize">Customer</span>
-        </div>
-        <div className="flex w-full max-w-[120px] flex-shrink-0 flex-col items-start">
-          <span className="font-mono text-xs capitalize">Metadata</span>
-        </div>
-      </ListItem>
-      {events.map((event) => (
-        <EventRow key={event.id} event={event} />
-      ))}
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="px-4 py-2 text-left">
+              <span className="font-mono text-xs capitalize">Timestamp</span>
+            </th>
+            <th className="px-4 py-2 text-left">
+              <span className="font-mono text-xs capitalize">Event</span>
+            </th>
+            <th className="px-4 py-2 text-left">
+              <span className="font-mono text-xs capitalize">Customer</span>
+            </th>
+            <th className="px-4 py-2 text-left">
+              <span className="font-mono text-xs capitalize">Metadata</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody className="">
+          {events.map((event) => (
+            <EventRow key={event.id} event={event} />
+          ))}
+        </tbody>
+      </table>
     </List>
   )
 }

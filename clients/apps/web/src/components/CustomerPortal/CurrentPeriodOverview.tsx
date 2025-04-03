44bf1f4e4e57b2f34ce2a58d75612d03bcfe3b8b
Emil Widlund <hello@emilwidlund.com>
2025-04-03 16:59:04 +0200

    more adjustments

--- clients/apps/web/src/components/CustomerPortal/CurrentPeriodOverview.tsx
+++ clients/apps/web/src/components/CustomerPortal/CurrentPeriodOverview.tsx
@@ -1,5 +1,6 @@
 import { schemas } from '@polar-sh/client'
 import ProductPriceLabel from '../Products/ProductPriceLabel'
+import AmountLabel from '../Shared/AmountLabel'
 
 interface CurrentPeriodOverviewProps {
   subscription: schemas['CustomerSubscription']
@@ -12,7 +13,18 @@ export const CurrentPeriodOverview = ({
     return null
   }
 
-  console.log(subscription)
+  const basePrice = subscription.prices.find(
+    (price) => price.amount_type === 'fixed',
+  )
+
+  const meteredPrices = subscription.prices.filter(
+    (price) => price.amount_type === 'metered_unit',
+  )
+
+  const totalAmount = meteredPrices.reduce(
+    (acc, price) => acc + price.unit_amount,
+    basePrice?.price_amount || 0,
+  )
 
   return (
     <div className="dark:border-polar-700 flex flex-col gap-4 rounded-3xl border border-gray-200 p-6">
@@ -24,7 +36,7 @@ export const CurrentPeriodOverview = ({
             ? new Date(subscription.current_period_end).toLocaleDateString(
                 'en-US',
                 {
-                  dateStyle: 'short',
+                  dateStyle: 'medium',
                 },
               )
             : 'N/A'}
@@ -41,25 +53,39 @@ export const CurrentPeriodOverview = ({
           </span>
         </div>
 
-        <div className="flex items-center justify-between">
-          <span className="text-gray-600 dark:text-gray-400">
-            Usage Charges
-          </span>
-          <span className="font-medium">$6.00</span>
-        </div>
+        <span className="font-medium">Metered Charges</span>
+
+        {meteredPrices.map((price) => (
+          <div key={price.id} className="flex items-center justify-between">
+            <span className="text-gray-600 dark:text-gray-400">
+              {price.meter.name}
+            </span>
+            <span className="font-medium">
+              <AmountLabel
+                amount={price.unit_amount}
+                currency={price.price_currency}
+              />
+            </span>
+          </div>
+        ))}
 
         <div className="dark:border-polar-700 mt-2 border-t border-gray-200 pt-2">
           <div className="flex items-center justify-between">
             <span className="font-medium">Estimated Total</span>
-            <span className="text-lg font-semibold">$16.00</span>
+            <span className="text-lg font-semibold">
+              <AmountLabel
+                amount={totalAmount}
+                currency={subscription.currency}
+              />
+            </span>
           </div>
+
+          <p className="text-xs text-gray-500">
+            Final charges may vary based on usage until the end of the billing
+            period
+          </p>
         </div>
       </div>
-
-      <p className="text-xs text-gray-500">
-        * Final charges may vary based on usage until the end of the billing
-        period
-      </p>
     </div>
   )
 }

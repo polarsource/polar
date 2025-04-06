d9e4fb8ee5c24ded4751c36d4dae15f7f4e09f02
Birk Jernstrom <birkjernstrom@gmail.com>
2025-04-06 23:23:17 +0200

    chore: alembic merge heads

---
+++ server/migrations/versions/2025-04-06-2322_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: ec6d1080d2c9
+Revises: d2efdf94bfa9, 4015480760bd
+Create Date: 2025-04-06 23:22:53.115359
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# Polar Custom Imports
+
+# revision identifiers, used by Alembic.
+revision = "ec6d1080d2c9"
+down_revision = ("d2efdf94bfa9", "4015480760bd")
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

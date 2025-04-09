b3d633d7c279ca72f16543011b7c3d3cd1fb6565
Emil Widlund <hello@emilwidlund.com>
2025-04-09 12:55:16 +0200

    add dither to landing page

--- clients/apps/web/src/components/Landing/LandingPage.tsx
+++ clients/apps/web/src/components/Landing/LandingPage.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { Hero } from '@/components/Landing/Hero/Hero'
 import { MerchantOfRecord } from '@/components/Landing/MOR'
 import { Testimonials } from '@/components/Landing/Testimonials'
+import Dither from '@/src/components/Dither/Dither'
+import { useTheme } from 'next-themes'
 import { Benefits } from './Benefits'
 import { Checkout } from './Checkout'
 import Features from './Features'
@@ -14,20 +18,39 @@ import { Upsell } from './Upsell'
 
 export default function Page() {
   return (
-    <div className="flex w-full flex-col items-center pt-24 md:pt-0">
+    <div className="flex w-full flex-col items-center">
       <PageContent />
     </div>
   )
 }
 
 export const PageContent = () => {
+  const { resolvedTheme } = useTheme()
+
   return (
     <>
+      <div className="relative w-screen py-16">
+        <div className="absolute inset-0">
+          <Dither
+            waveAmplitude={0}
+            waveFrequency={0}
+            waveColor={
+              resolvedTheme === 'dark' ? [0.35, 0.35, 0.35] : [0.8, 0.8, 0.8]
+            }
+            enableMouseInteraction={false}
+            invert={resolvedTheme === 'light'}
+          />
+          <div className="dark:from-polar-950 absolute inset-0 bg-gradient-to-t from-gray-100 to-transparent" />
+        </div>
+        <Hero className="relative z-10" />
+      </div>
       <Section className="flex flex-col gap-y-24">
-        <Hero />
         <Features />
         <Intro />
         <SDKs />
+      </Section>
+      <Testimonials />
+      <Section className="flex flex-col gap-y-24">
         <Benefits />
         <MerchantOfRecord />
         <Checkout />
@@ -36,7 +59,6 @@ export const PageContent = () => {
       <div className="flex w-full flex-col items-center justify-center px-4 py-16 md:max-w-[1440px] md:px-8">
         <Video src="/assets/landing/Polar2024.webm" />
       </div>
-      <Testimonials />
       <Section className="flex flex-col gap-y-24">
         <Tools />
         <Pricing />

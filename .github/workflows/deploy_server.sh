2d182b498420363847600558f1c3993e707528c8
François Voron <fvoron@gmail.com>
2025-04-14 14:28:55 +0200

    ops: remove GitHub workers from deploy script

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -6,10 +6,8 @@ IMG="ghcr.io/polarsource/polar@${1}"
 declare -A servers=(
   ["srv-ci4r87h8g3ne0dmvvl60"]="${RENDER_DEPLOY_KEY_API}"
   ["srv-ctilidt2ng1s73bctvp0"]="${RENDER_DEPLOY_KEY_WORKER}"
-  ["srv-ctilidt2ng1s73bctvg0"]="${RENDER_DEPLOY_KEY_WORKER_GITHUB}"
   ["srv-crkocgbtq21c73ddsdbg"]="${RENDER_DEPLOY_KEY_API_SANDBOX}"
   ["srv-ctilidt2ng1s73bctv8g"]="${RENDER_DEPLOY_KEY_WORKER_SANDBOX}"
-  ["srv-ctilidt2ng1s73bctvbg"]="${RENDER_DEPLOY_KEY_WORKER_GITHUB_SANDBOX}"
 )
 
 for server_id in "${!servers[@]}"; do

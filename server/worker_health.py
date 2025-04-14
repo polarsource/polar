a3cc8690f51ea9aa4f07c8fe479eb71854d552db
François Voron <fvoron@gmail.com>
2025-04-14 14:03:15 +0200

    server: remove last bits of GitHub worker and client

--- server/worker_health.py
+++ server/worker_health.py
@@ -1,4 +1,3 @@
-import structlog
 from arq.worker import async_check_health
 from starlette.applications import Starlette
 from starlette.convertors import StringConvertor, register_url_convertor
@@ -6,23 +5,19 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Route
 
-from polar.logging import Logger
 from polar.logging import configure as configure_logging
-from polar.worker import WorkerSettings, WorkerSettingsGitHubCrawl
+from polar.worker import WorkerSettings
 
 configure_logging()
-logger: Logger = structlog.get_logger()
 
 
-async def arq_health_check(
-    settings_cls: type[WorkerSettings] | type[WorkerSettingsGitHubCrawl],
-) -> bool:
+async def arq_health_check(settings_cls: type[WorkerSettings]) -> bool:
     exit_code = await async_check_health(settings_cls.redis_settings)
     return exit_code == 0
 
 
 class WorkerParamConvertor(StringConvertor):
-    regex = "main|github"
+    regex = "main"
 
 
 register_url_convertor("worker", WorkerParamConvertor())
@@ -30,8 +25,7 @@ register_url_convertor("worker", WorkerParamConvertor())
 
 async def healthz(request: Request) -> Response:
     worker = request.path_params["worker"]
-    worker_class = WorkerSettingsGitHubCrawl if worker == "github" else WorkerSettings
-    if await arq_health_check(worker_class):
+    if await arq_health_check(WorkerSettings):
         return Response(status_code=200)
     else:
         return Response(status_code=503)

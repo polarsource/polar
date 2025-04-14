12253f0f5b156cae1d38760713957c593b979b45
Emil Widlund <hello@emilwidlund.com>
2025-04-14 15:31:57 +0200

    make sure to pass organization in portal

--- clients/apps/web/src/app/(main)/[organization]/portal/orders/[id]/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/[organization]/portal/orders/[id]/ClientPage.tsx
@@ -3,8 +3,10 @@
 import CustomerPortalOrder from '@/components/CustomerPortal/CustomerPortalOrder'
 import { createClientSideAPI } from '@/utils/client'
 import { schemas } from '@polar-sh/client'
+import { useThemePreset } from '@polar-sh/ui/hooks/theming'
 
 const ClientPage = ({
+  organization,
   order,
   customerSessionToken,
 }: {
@@ -12,8 +14,19 @@ const ClientPage = ({
   order: schemas['CustomerOrder']
   customerSessionToken?: string
 }) => {
+  const themingPreset = useThemePreset(
+    organization.slug === 'midday' ? 'midday' : 'polar',
+  )
   const api = createClientSideAPI(customerSessionToken)
-  return <CustomerPortalOrder api={api} order={order} />
+
+  return (
+    <CustomerPortalOrder
+      api={api}
+      order={order}
+      themingPreset={themingPreset}
+      organization={organization}
+    />
+  )
 }
 
 export default ClientPage

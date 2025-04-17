016ed99d9b511d224e5addfe62f5acc3496edd10
François Voron <fvoron@gmail.com>
2025-04-17 16:37:40 +0200

    clients: upgrade dependencies

--- clients/apps/web/sentry.server.config.ts
+++ clients/apps/web/sentry.server.config.ts
@@ -10,8 +10,7 @@ Sentry.init({
   environment: CONFIG.ENVIRONMENT,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  enableTracing: false,
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,

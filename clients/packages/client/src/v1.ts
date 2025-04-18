d0c3e5a898c8bd40d24b2aa502017c73622c5929
François Voron <fvoron@gmail.com>
2025-04-18 09:25:20 +0200

    clients/packages/client: update OpenAPI client

--- clients/packages/client/src/v1.ts
+++ clients/packages/client/src/v1.ts
@@ -14186,18 +14186,6 @@ export interface components {
         };
         /** SubscriptionCancel */
         SubscriptionCancel: {
-            /**
-             * Cancel At Period End
-             * @description Cancel an active subscription once the current period ends.
-             *
-             *     Or uncancel a subscription currently set to be revoked at period end.
-             */
-            cancel_at_period_end?: boolean | null;
-            /**
-             * Revoke
-             * @description Cancel and revoke an active subscription immediately
-             */
-            revoke?: true | null;
             /** @description Customer reason for cancellation.
              *
              *     Helpful to monitor reasons behind churn for future improvements.
@@ -14229,6 +14217,13 @@ export interface components {
              *     conversation, i.e support.
              */
             customer_cancellation_comment?: string | null;
+            /**
+             * Cancel At Period End
+             * @description Cancel an active subscription once the current period ends.
+             *
+             *     Or uncancel a subscription currently set to be revoked at period end.
+             */
+            cancel_at_period_end: boolean;
         };
         /** SubscriptionCustomer */
         SubscriptionCustomer: {
@@ -14349,6 +14344,46 @@ export interface components {
          * @enum {string}
          */
         SubscriptionRecurringInterval: "month" | "year";
+        /** SubscriptionRevoke */
+        SubscriptionRevoke: {
+            /** @description Customer reason for cancellation.
+             *
+             *     Helpful to monitor reasons behind churn for future improvements.
+             *
+             *     Only set this in case your own service is requesting the reason from the
+             *     customer. Or you know based on direct conversations, i.e support, with
+             *     the customer.
+             *
+             *     * `too_expensive`: Too expensive for the customer.
+             *     * `missing_features`: Customer is missing certain features.
+             *     * `switched_service`: Customer switched to another service.
+             *     * `unused`: Customer is not using it enough.
+             *     * `customer_service`: Customer is not satisfied with the customer service.
+             *     * `low_quality`: Customer is unhappy with the quality.
+             *     * `too_complex`: Customer considers the service too complicated.
+             *     * `other`: Other reason(s). */
+            customer_cancellation_reason?: components["schemas"]["CustomerCancellationReason"] | null;
+            /**
+             * Customer Cancellation Comment
+             * @description Customer feedback and why they decided to cancel.
+             *
+             *     **IMPORTANT:**
+             *     Do not use this to store internal notes! It's intended to be input
+             *     from the customer and is therefore also available in their Polar
+             *     purchases library.
+             *
+             *     Only set this in case your own service is requesting the reason from the
+             *     customer. Or you copy a message directly from a customer
+             *     conversation, i.e support.
+             */
+            customer_cancellation_comment?: string | null;
+            /**
+             * Revoke
+             * @description Cancel and revoke an active subscription immediately
+             * @constant
+             */
+            revoke: true;
+        };
         /**
          * SubscriptionSortProperty
          * @enum {string}
@@ -14359,7 +14394,7 @@ export interface components {
          * @enum {string}
          */
         SubscriptionStatus: "incomplete" | "incomplete_expired" | "trialing" | "active" | "past_due" | "canceled" | "unpaid";
-        SubscriptionUpdate: components["schemas"]["SubscriptionUpdateProduct"] | components["schemas"]["SubscriptionCancel"];
+        SubscriptionUpdate: components["schemas"]["SubscriptionUpdateProduct"] | components["schemas"]["SubscriptionCancel"] | components["schemas"]["SubscriptionRevoke"];
         /** SubscriptionUpdateProduct */
         SubscriptionUpdateProduct: {
             /**

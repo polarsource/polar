c98ae0a50418f7d74f5f6e3b947d7fc7245be731
Emil Widlund <hello@emilwidlund.com>
2025-04-28 16:42:15 +0200

    improve customer portal experience

---
+++ clients/apps/web/src/components/CustomerPortal/CustomerPortalSubscriptions.tsx
@@ -0,0 +1,210 @@
+import revalidate from '@/app/actions'
+import { Client, schemas } from '@polar-sh/client'
+import Button from '@polar-sh/ui/components/atoms/Button'
+import { DataTable } from '@polar-sh/ui/components/atoms/DataTable'
+import FormattedDateTime from '@polar-sh/ui/components/atoms/FormattedDateTime'
+import { useThemePreset } from '@polar-sh/ui/hooks/theming'
+import { useCallback, useState } from 'react'
+import { InlineModal } from '../Modal/InlineModal'
+import { useModal } from '../Modal/useModal'
+import CustomerSubscriptionDetails from '../Subscriptions/CustomerSubscriptionDetails'
+import { CurrentPeriodOverview } from './CurrentPeriodOverview'
+import CustomerPortalSubscription from './CustomerPortalSubscription'
+
+interface CustomerPortalSubscriptionsProps {
+  organization: schemas['Organization']
+  subscriptions: schemas['CustomerSubscription'][]
+  products: schemas['CustomerProduct'][]
+  api: Client
+  customerSessionToken?: string
+}
+
+export const CustomerPortalSubscriptions = ({
+  organization,
+  subscriptions,
+  products,
+  api,
+  customerSessionToken,
+}: CustomerPortalSubscriptionsProps) => {
+  const activeSubscriptions = subscriptions.filter((s) => s.status === 'active')
+  const pastSubscriptions = subscriptions.filter((s) => s.status !== 'active')
+
+  return (
+    <div className="flex flex-col gap-y-8">
+      {activeSubscriptions.map((s) => (
+        <CurrentPeriodOverview key={s.id} subscription={s} />
+      ))}
+
+      <ActiveSubscriptionsOverview
+        customerSessionToken={customerSessionToken}
+        api={api}
+        organization={organization}
+        subscriptions={activeSubscriptions}
+        products={products}
+      />
+
+      <InactiveSubscriptionsOverview
+        customerSessionToken={customerSessionToken}
+        api={api}
+        organization={organization}
+        subscriptions={pastSubscriptions}
+        products={products}
+      />
+    </div>
+  )
+}
+
+interface SubscriptionsOverviewProps {
+  organization: schemas['Organization']
+  subscriptions: schemas['CustomerSubscription'][]
+  products: schemas['CustomerProduct'][]
+  api: Client
+  customerSessionToken?: string
+}
+
+const ActiveSubscriptionsOverview = ({
+  subscriptions,
+  products,
+  api,
+  customerSessionToken,
+}: SubscriptionsOverviewProps) => {
+  const onSubscriptionUpdate = useCallback(async () => {
+    await revalidate(`customer_portal`)
+  }, [])
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h3 className="text-2xl">Subscriptions</h3>
+      <div className="flex flex-col gap-y-4">
+        {subscriptions.length > 0 ? (
+          subscriptions.map((s) => (
+            <CustomerSubscriptionDetails
+              key={s.id}
+              api={api}
+              subscription={s}
+              products={products}
+              onUserSubscriptionUpdate={onSubscriptionUpdate}
+              customerSessionToken={customerSessionToken}
+            />
+          ))
+        ) : (
+          <div className="dark:border-polar-700 flex flex-col items-center justify-center rounded-2xl border border-gray-200 p-12 text-gray-500">
+            <p>No Subscriptions Found</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+interface SubscriptionsOverviewProps {
+  organization: schemas['Organization']
+  subscriptions: schemas['CustomerSubscription'][]
+}
+
+const InactiveSubscriptionsOverview = ({
+  organization,
+  subscriptions,
+  api,
+}: SubscriptionsOverviewProps) => {
+  const themingPreset = useThemePreset(
+    organization.slug === 'midday' ? 'midday' : 'polar',
+  )
+
+  const [selectedSubscription, setSelectedSubscription] = useState<
+    schemas['CustomerSubscription'] | null
+  >(null)
+
+  const {
+    isShown: isSubscriptionModalOpen,
+    hide: _hideSubscriptionModal,
+    show: showSubscriptionModal,
+  } = useModal()
+
+  const openSubscriptionModal = useCallback(
+    (subscription: schemas['CustomerSubscription']) => {
+      setSelectedSubscription(subscription)
+      showSubscriptionModal()
+    },
+    [showSubscriptionModal],
+  )
+
+  const hideSubscriptionModal = useCallback(() => {
+    setSelectedSubscription(null)
+    _hideSubscriptionModal()
+  }, [_hideSubscriptionModal])
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex flex-row items-center justify-between">
+        <h3 className="text-2xl">Inactive Subscriptions</h3>
+      </div>
+      <DataTable
+        wrapperClassName={themingPreset.polar.table}
+        headerClassName={themingPreset.polar.tableHeader}
+        data={subscriptions ?? []}
+        isLoading={false}
+        columns={[
+          {
+            accessorKey: 'product.name',
+            header: 'Product',
+            cell: ({ row }) => row.original.product.name,
+          },
+          {
+            accessorKey: 'status',
+            header: 'Status',
+            cell: ({ row }) => (
+              <span className="capitalize">
+                {row.original.status.split('_').join(' ')}
+              </span>
+            ),
+          },
+          {
+            accessorKey: 'ended_at',
+            header: 'Ended At',
+            cell: ({ row }) => (
+              <FormattedDateTime
+                datetime={row.original.ended_at ?? '—'}
+                dateStyle="medium"
+                resolution="day"
+              />
+            ),
+          },
+          {
+            accessorKey: 'id',
+            header: '',
+            cell: ({ row }) => (
+              <span className="flex justify-end">
+                <Button
+                  variant="secondary"
+                  size="sm"
+                  onClick={() => openSubscriptionModal(row.original)}
+                >
+                  View Subscription
+                </Button>
+              </span>
+            ),
+          },
+        ]}
+      />
+
+      <InlineModal
+        isShown={isSubscriptionModalOpen}
+        hide={hideSubscriptionModal}
+        modalContent={
+          selectedSubscription ? (
+            <div className="flex flex-col overflow-y-auto p-8">
+              <CustomerPortalSubscription
+                api={api}
+                subscription={selectedSubscription}
+                themingPreset={themingPreset}
+              />
+            </div>
+          ) : (
+            <></>
+          )
+        }
+      />
+    </div>
+  )
+}

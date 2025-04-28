4d3a289192a1efd026573dfa48f6b1d7cf100a14
François Voron <fvoron@gmail.com>
2025-04-28 20:57:17 +0200

    ops: create new Render service to deploy Dramatiq worker

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -5,9 +5,7 @@ set -euo pipefail
 IMG="ghcr.io/polarsource/polar@${1}"
 declare -A servers=(
   ["srv-ci4r87h8g3ne0dmvvl60"]="${RENDER_DEPLOY_KEY_API}"
-  ["srv-ctilidt2ng1s73bctvp0"]="${RENDER_DEPLOY_KEY_WORKER}"
   ["srv-crkocgbtq21c73ddsdbg"]="${RENDER_DEPLOY_KEY_API_SANDBOX}"
-  ["srv-ctilidt2ng1s73bctv8g"]="${RENDER_DEPLOY_KEY_WORKER_SANDBOX}"
 )
 
 for server_id in "${!servers[@]}"; do

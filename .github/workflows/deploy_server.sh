344417af2fb67b5a6b5657b3edae30a71ae74abf
François Voron <fvoron@gmail.com>
2025-04-29 12:03:14 +0200

    ops: reestablish server deploy now that Dramatiq workers are properly up

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 IMG="ghcr.io/polarsource/polar@${1}"
 declare -A servers=(
   ["srv-ci4r87h8g3ne0dmvvl60"]="${RENDER_DEPLOY_KEY_API}"
+  ["srv-d089jj7diees73934ka0"]="${RENDER_DEPLOY_KEY_WORKER}"
   ["srv-crkocgbtq21c73ddsdbg"]="${RENDER_DEPLOY_KEY_API_SANDBOX}"
+  ["srv-d089jj7diees73934kgg"]="${RENDER_DEPLOY_KEY_WORKER_SANDBOX}"
 )
 
 for server_id in "${!servers[@]}"; do

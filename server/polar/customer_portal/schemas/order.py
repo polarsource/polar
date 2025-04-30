c1327ab0fadb65b1af0fd6afa7daa4e3d3765771
François Voron <fvoron@gmail.com>
2025-04-30 15:51:46 +0200

    server/order: factorize schema between customer portal and dashboard

--- server/polar/customer_portal/schemas/order.py
+++ server/polar/customer_portal/schemas/order.py
@@ -1,9 +1,8 @@
 from pydantic import UUID4, AliasChoices, AliasPath, Field
 from pydantic.json_schema import SkipJsonSchema
 
-from polar.kit.schemas import Schema, TimestampedSchema
-from polar.models.order import OrderStatus
-from polar.order.schemas import OrderItemSchema
+from polar.kit.schemas import Schema
+from polar.order.schemas import OrderBase, OrderItemSchema
 from polar.organization.schemas import Organization
 from polar.product.schemas import (
     BenefitPublicList,
@@ -13,47 +12,6 @@ from polar.product.schemas import (
     ProductPriceList,
 )
 from polar.subscription.schemas import SubscriptionBase
-
-
-class CustomerOrderBase(TimestampedSchema):
-    id: UUID4
-    status: OrderStatus = Field(examples=["paid"])
-    paid: bool = Field(
-        description="Whether the order has been paid for.", examples=[True]
-    )
-    subtotal_amount: int = Field(
-        description="Amount in cents, before discounts and taxes."
-    )
-    discount_amount: int = Field(description="Discount amount in cents.")
-    net_amount: int = Field(
-        description="Amount in cents, after discounts but before taxes."
-    )
-    amount: SkipJsonSchema[int] = Field(
-        description="Amount in cents, after discounts but before taxes.",
-        deprecated=(
-            "Use `net_amount`. "
-            "It has the same value and meaning, but the name is more descriptive."
-        ),
-        validation_alias="net_amount",
-    )
-    tax_amount: int = Field(description="Sales tax amount in cents.")
-    total_amount: int = Field(description="Amount in cents, after discounts and taxes.")
-    refunded_amount: int = Field(description="Amount refunded in cents.")
-    refunded_tax_amount: int = Field(description="Sales tax refunded in cents.")
-    currency: str
-
-    customer_id: UUID4
-    product_id: UUID4
-    product_price_id: SkipJsonSchema[UUID4] = Field(
-        deprecated="Use `items` instead.",
-        validation_alias=AliasChoices(
-            # Validate from stored webhook payload
-            "product_price_id",
-            # Validate from ORM model
-            AliasPath("legacy_product_price", "id"),
-        ),
-    )
-    subscription_id: UUID4 | None
 
 
 class CustomerOrderProduct(ProductBase):
@@ -66,7 +24,7 @@ class CustomerOrderProduct(ProductBase):
 class CustomerOrderSubscription(SubscriptionBase): ...
 
 
-class CustomerOrder(CustomerOrderBase):
+class CustomerOrder(OrderBase):
     user_id: UUID4 = Field(
         validation_alias=AliasChoices(
             # Validate from stored webhook payload

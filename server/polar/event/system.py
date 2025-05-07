8d7bc3785920da4fa387472ba9a55f4652a6dbf1
François Voron <fvoron@gmail.com>
2025-05-07 13:53:26 +0200

    server/event: introduce meter.reset event

--- server/polar/event/system.py
+++ server/polar/event/system.py
@@ -1,5 +1,5 @@
 from enum import StrEnum
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Any, Literal, overload
 
 from sqlalchemy.orm import Mapped
 from sqlalchemy.util.typing import TypedDict
@@ -9,13 +9,14 @@ from polar.models import Customer, Event, Organization
 from polar.models.event import EventSource
 
 
+class SystemEvent(StrEnum):
+    meter_credited = "meter.credited"
+    meter_reset = "meter.reset"
+
+
 class MeterCreditedMetadata(TypedDict):
     meter_id: str
     units: int
-
-
-class SystemEvent(StrEnum):
-    meter_credited = "meter.credited"
 
 
 class MeterCreditedEvent(Event):
@@ -25,12 +26,40 @@ class MeterCreditedEvent(Event):
         user_metadata: Mapped[MeterCreditedMetadata]  # type: ignore[assignment]
 
 
-# TODO: add @typing.overload when we have more than one event
+class MeterResetMetadata(TypedDict):
+    meter_id: str
+
+
+class MeterResetEvent(Event):
+    if TYPE_CHECKING:
+        source: Mapped[Literal[EventSource.system]]
+        name: Mapped[Literal[SystemEvent.meter_reset]]
+        user_metadata: Mapped[MeterResetMetadata]  # type: ignore[assignment]
+
+
+@overload
 def build_system_event(
     name: Literal[SystemEvent.meter_credited],
     customer: Customer,
     organization: Organization,
     metadata: MeterCreditedMetadata,
+) -> Event: ...
+
+
+@overload
+def build_system_event(
+    name: Literal[SystemEvent.meter_reset],
+    customer: Customer,
+    organization: Organization,
+    metadata: MeterResetMetadata,
+) -> Event: ...
+
+
+def build_system_event(
+    name: SystemEvent,
+    customer: Customer,
+    organization: Organization,
+    metadata: Any,
 ) -> Event:
     return Event(
         name=name,

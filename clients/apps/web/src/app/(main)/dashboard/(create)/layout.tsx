a169878239ecc28eb7f3fa6b63e4fde1c900fae7
Emil Widlund <hello@emilwidlund.com>
2025-05-21 10:52:57 +0200

    init onboarding view

--- clients/apps/web/src/app/(main)/dashboard/(create)/layout.tsx
+++ clients/apps/web/src/app/(main)/dashboard/(create)/layout.tsx
@@ -3,11 +3,5 @@ export default async function Layout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <div className="flex flex-col items-center px-4 py-12">
-      <div className="relative flex min-h-screen w-full max-w-md flex-col items-center md:py-0">
-        {children}
-      </div>
-    </div>
-  )
+  return <div className="flex h-full flex-col">{children}</div>
 }

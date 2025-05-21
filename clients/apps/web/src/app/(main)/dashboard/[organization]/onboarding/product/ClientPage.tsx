7d856c0a0eeac6e231e290433f7d7a419099ba95
Emil Widlund <hello@emilwidlund.com>
2025-05-21 18:44:02 +0200

    onboarding polish

---
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/onboarding/product/ClientPage.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import ProductStep from '@/components/Onboarding/ProductStep'
+
+export default function ClientPage() {
+  return <ProductStep />
+}
--- clients/apps/web/src/components/Checkout/CheckoutProductInfo.tsx
+++ clients/apps/web/src/components/Checkout/CheckoutProductInfo.tsx
@@ -22,17 +22,25 @@ const CheckoutProductInfo = ({
           avatar_url={organization.avatarUrl}
           name={organization.name}
         />
-        <h1 className="text-3xl">{product.name}</h1>
+        {product.name ? (
+          <h1 className="text-3xl">{product.name}</h1>
+        ) : (
+          <div className="dark:bg-polar-700 h-6 w-48 animate-pulse rounded-md bg-gray-200" />
+        )}
       </div>
       {product.medias.length > 0 && (
         <Slideshow images={product.medias.map((m) => m.publicUrl)} />
       )}
       {product.description ? (
-        <div className="prose dark:prose-invert prose-headings:mt-8 prose-headings:font-medium prose-headings:text-black prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-md prose-h5:text-sm prose-h6:text-sm dark:prose-headings:text-white dark:text-polar-300 max-w-4xl text-gray-800">
+        <div className="prose dark:prose-invert prose-headings:mt-8 prose-headings:font-medium prose-headings:text-black prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-md prose-h5:text-sm prose-h6:text-sm dark:prose-headings:text-white dark:text-polar-300 max-w-4xl leading-normal text-gray-800">
           <Markdown options={markdownOptions}>{product.description}</Markdown>
         </div>
       ) : (
-        <></>
+        <div className="flex flex-col gap-y-2">
+          <div className="dark:bg-polar-700 h-4 w-full animate-pulse rounded-sm bg-gray-200" />
+          <div className="dark:bg-polar-700 h-4 w-full animate-pulse rounded-sm bg-gray-200" />
+          <div className="dark:bg-polar-700 h-4 w-full animate-pulse rounded-sm bg-gray-200" />
+        </div>
       )}
     </>
   )

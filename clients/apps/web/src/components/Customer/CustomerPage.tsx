44f30ca857dd021572a47a922175b568d0472755
Emil Widlund <hello@emilwidlund.com>
2025-06-09 23:44:12 +0200

    fix testflight banner bug

--- clients/apps/web/src/components/Customer/CustomerPage.tsx
+++ clients/apps/web/src/components/Customer/CustomerPage.tsx
@@ -209,7 +209,7 @@ export const CustomerPage: React.FC<CustomerPageProps> = ({
           />
         </div>
 
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-2">
           <ShadowBox className="flex flex-col gap-8">
             <h2 className="text-xl">Customer Details</h2>
             <div className="flex flex-col">
--- clients/apps/web/src/components/Layout/Dashboard/DashboardSidebar.tsx
+++ clients/apps/web/src/components/Layout/Dashboard/DashboardSidebar.tsx
@@ -22,7 +22,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@polar-sh/ui/components/ui/dropdown-menu'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { ChevronDown } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
@@ -81,20 +81,32 @@ export const DashboardSidebar = ({
         </motion.div>
       </SidebarContent>
       <SidebarFooter>
-        <SidebarContent>
-          <div className="dark:bg-polar-900 dark:border-polar-800 flex flex-col gap-3 rounded-lg border border-gray-200 bg-white p-3 text-sm">
-            <LogoIcon size={24} />
-            <span>Polar for iOS is now available on TestFlight!</span>
-            <Link
-              href="https://testflight.apple.com/join/CwVdc1Jt"
-              target="_blank"
-              className="dark:bg-polar-800 dark:hover:bg-polar-700 self-start rounded-sm bg-gray-100 p-1 text-xs transition-colors hover:bg-gray-200"
+        {!isCollapsed && (
+          <AnimatePresence>
+            <motion.div
+              key={isCollapsed ? 'nav-collapsed' : 'nav-expanded'}
+              className={`flex ${isCollapsed ? 'flex-col' : 'flex-row'} items-center gap-2`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
             >
-              <span>Join Beta</span>
-              <ArrowOutwardOutlined className="ml-2" fontSize="inherit" />
-            </Link>
-          </div>
-        </SidebarContent>
+              <SidebarContent>
+                <div className="dark:bg-polar-900 dark:border-polar-800 flex flex-col gap-3 rounded-lg border border-gray-200 bg-white p-3 text-sm">
+                  <LogoIcon size={24} />
+                  <span>Polar for iOS is now available on TestFlight!</span>
+                  <Link
+                    href="https://testflight.apple.com/join/CwVdc1Jt"
+                    target="_blank"
+                    className="dark:bg-polar-800 dark:hover:bg-polar-700 self-start rounded-sm bg-gray-100 p-1 text-xs transition-colors hover:bg-gray-200"
+                  >
+                    <span>Join Beta</span>
+                    <ArrowOutwardOutlined className="ml-2" fontSize="inherit" />
+                  </Link>
+                </div>
+              </SidebarContent>
+            </motion.div>
+          </AnimatePresence>
+        )}
         <SidebarMenu>
           <SidebarMenuItem>
             <DropdownMenu>

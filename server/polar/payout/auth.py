13ba1fa9c5cf0ab7fd195effdaef3d99ae099669
Emil Widlund <hello@emilwidlund.com>
2025-06-12 10:40:36 +0200

    Add payout scopes

---
+++ server/polar/payout/auth.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from polar.auth.dependencies import Authenticator
+from polar.auth.models import AuthSubject, User
+from polar.auth.scope import Scope
+
+_PayoutsRead = Authenticator(
+    required_scopes={
+        Scope.web_default,
+        Scope.payouts_read,
+    },
+    allowed_subjects={User},
+)
+PayoutsRead = Annotated[AuthSubject[User], Depends(_PayoutsRead)]
+
+_PayoutsWrite = Authenticator(
+    required_scopes={Scope.web_default, Scope.payouts_write},
+    allowed_subjects={User},
+)
+PayoutsWrite = Annotated[AuthSubject[User], Depends(_PayoutsWrite)]

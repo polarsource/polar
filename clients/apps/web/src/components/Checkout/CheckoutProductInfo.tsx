15820c7be0d7d7bddf31c85bc978c82c0857d619
Aaryan Mishra <aaryan.mshra@gmail.com>
2025-06-18 01:36:07 +0530

    Removed skeletons when there is no product descrpition

--- clients/apps/web/src/components/Checkout/CheckoutProductInfo.tsx
+++ clients/apps/web/src/components/Checkout/CheckoutProductInfo.tsx
@@ -31,15 +31,9 @@ const CheckoutProductInfo = ({
       {product.medias.length > 0 && (
         <Slideshow images={product.medias.map((m) => m.publicUrl)} />
       )}
-      {product.description ? (
+      {product.description && (
         <div className="prose dark:prose-invert prose-headings:mt-8 prose-headings:font-medium prose-headings:text-black prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-md prose-h5:text-sm prose-h6:text-sm dark:prose-headings:text-white dark:text-polar-300 max-w-4xl leading-normal text-gray-800">
           <Markdown options={markdownOptions}>{product.description}</Markdown>
-        </div>
-      ) : (
-        <div className="flex flex-col gap-y-2">
-          <div className="dark:bg-polar-700 h-4 w-full animate-pulse rounded-sm bg-gray-200" />
-          <div className="dark:bg-polar-700 h-4 w-full animate-pulse rounded-sm bg-gray-200" />
-          <div className="dark:bg-polar-700 h-4 w-full animate-pulse rounded-sm bg-gray-200" />
         </div>
       )}
     </>

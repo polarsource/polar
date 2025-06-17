c6d08793f53a974289e45d5b086af6467bb4ad42
François Voron <fvoron@gmail.com>
2025-06-17 09:59:14 +0200

    server/worker: add Redis instrumentation to Logfire

--- server/run_worker.py
+++ server/run_worker.py
@@ -1,5 +1,5 @@
 from polar import tasks
-from polar.logfire import configure_logfire
+from polar.logfire import configure_logfire, instrument_redis
 from polar.logging import configure as configure_logging
 from polar.sentry import configure_sentry
 from polar.worker import broker
@@ -7,5 +7,6 @@ from polar.worker import broker
 configure_sentry()
 configure_logfire("worker")
 configure_logging(logfire=True)
+instrument_redis()
 
 __all__ = ["tasks", "broker"]

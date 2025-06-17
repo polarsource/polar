237ff5a3e5d531855e84589c3a5d47e5a6a007cc
François Voron <fvoron@gmail.com>
2025-06-17 09:59:14 +0200

    server/worker: add Redis instrumentation to Logfire

--- server/polar/logfire.py
+++ server/polar/logfire.py
@@ -21,6 +21,7 @@ if TYPE_CHECKING:
     from opentelemetry.trace.span import TraceState
     from opentelemetry.util.types import Attributes
 
+
 from polar.config import settings
 from polar.kit.db.postgres import Engine
 
@@ -114,8 +115,16 @@ def instrument_sqlalchemy(engine: Engine) -> None:
     SQLAlchemyInstrumentor().instrument(engine=engine)
 
 
+def instrument_redis() -> None:
+    if settings.is_testing():
+        return
+
+    logfire.instrument_redis()
+
+
 __all__ = [
     "configure_logfire",
     "instrument_fastapi",
     "instrument_sqlalchemy",
+    "instrument_redis",
 ]

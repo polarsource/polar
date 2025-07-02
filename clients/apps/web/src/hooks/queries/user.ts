eb3b42d0f075f3f656a0c9222d36412c37de98cd
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2025-07-02 16:19:58 +0200

    Remove `/personal_access_tokens`-API
    This API is unused.
    
    We can't remove the models/tables as they might still be in use (i.e.
    our auth module uses `personal_access_token/dependencies.py::get_optional_personal_access_token()`).

--- clients/apps/web/src/hooks/queries/user.ts
+++ clients/apps/web/src/hooks/queries/user.ts
@@ -1,34 +1,5 @@
-import { queryClient } from '@/utils/api/query'
 import { api } from '@/utils/client'
-import { unwrap } from '@polar-sh/client'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { defaultRetry } from './retry'
-
-export const usePersonalAccessTokens = () =>
-  useQuery({
-    queryKey: ['personalAccessTokens'],
-    queryFn: () => unwrap(api.GET('/v1/personal_access_tokens/')),
-    retry: defaultRetry,
-  })
-
-export const useDeletePersonalAccessToken = () =>
-  useMutation({
-    mutationFn: (variables: { id: string }) => {
-      return api.DELETE('/v1/personal_access_tokens/{id}', {
-        params: {
-          path: {
-            id: variables.id,
-          },
-        },
-      })
-    },
-    onSuccess: (result, _variables, _ctx) => {
-      if (result.error) {
-        return
-      }
-      queryClient.invalidateQueries({ queryKey: ['personalAccessTokens'] })
-    },
-  })
+import { useMutation } from '@tanstack/react-query'
 
 export const useCreateIdentityVerification = () =>
   useMutation({

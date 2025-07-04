9d871e4ceadc6f5fc2e719e45d1be287e24debde
Emil Widlund <hello@emilwidlund.com>
2025-07-04 11:54:11 +0200

    ui: adjust button border

--- clients/apps/web/src/components/Customer/CustomerContextView.tsx
+++ clients/apps/web/src/components/Customer/CustomerContextView.tsx
@@ -72,8 +72,8 @@ export const CustomerContextView = ({
   })
 
   return (
-    <div className="flex flex-col gap-2 overflow-y-auto">
-      <ShadowBox className="dark:border-polar-800 flex flex-col gap-6 p-6 lg:rounded-2xl">
+    <div className="flex h-full flex-col gap-2 overflow-y-auto">
+      <ShadowBox className="dark:border-polar-800 flex flex-col gap-6 border-gray-200 bg-white p-6 md:shadow-sm lg:rounded-2xl">
         <Link
           href={`/dashboard/${organization.slug}/customers?customerId=${customer.id}&query=${customer.email}`}
           className="flex flex-row items-center gap-4"
@@ -162,7 +162,7 @@ export const CustomerContextView = ({
           </div>
         )}
       </ShadowBox>
-      <ShadowBox className="dark:border-polar-800 flex flex-col gap-4 p-6 md:gap-0 lg:rounded-2xl">
+      <ShadowBox className="dark:border-polar-800 flex flex-col gap-4 border-gray-200 bg-white p-6 md:gap-0 md:shadow-sm lg:rounded-2xl">
         {!customer.deleted_at && <DetailRow label="ID" value={customer.id} />}
         <DetailRow label="Email" value={customer.email} />
         <DetailRow label="Name" value={customer.name} />
@@ -172,7 +172,7 @@ export const CustomerContextView = ({
           value={<FormattedDateTime datetime={customer.created_at} />}
         />
       </ShadowBox>
-      <ShadowBox className="dark:border-polar-800 flex flex-col gap-4 p-6 lg:rounded-2xl">
+      <ShadowBox className="dark:border-polar-800 flex flex-col gap-4 border-gray-200 bg-white p-6 md:shadow-sm lg:rounded-2xl">
         <h4 className="text-lg">Billing Address</h4>
         <div className="flex flex-col gap-4 md:gap-0">
           <DetailRow label="Line 1" value={customer.billing_address?.line1} />
@@ -190,7 +190,7 @@ export const CustomerContextView = ({
         </div>
       </ShadowBox>
       {!customer.deleted_at && (
-        <ShadowBox className="dark:border-polar-800 flex flex-col gap-4 p-6 lg:rounded-2xl">
+        <ShadowBox className="dark:border-polar-800 flex flex-col gap-4 border-gray-200 bg-white p-6 md:shadow-sm lg:rounded-2xl">
           <div className="flex flex-row items-center justify-between gap-2">
             <h3 className="text-lg">Metadata</h3>
             <Button className="h-8 w-8" variant="secondary" onClick={showModal}>

ffc70d2100451e9c3f0cc9f0f4d3395978d56146
François Voron <fvoron@gmail.com>
2025-07-21 15:21:45 +0200

    server/processor_transaction: handle colliding transactions timestamp while syncing

--- server/polar/processor_transaction/repository.py
+++ server/polar/processor_transaction/repository.py
@@ -1,3 +1,5 @@
+from collections.abc import Sequence
+
 from polar.kit.repository.base import RepositoryBase
 from polar.models import ProcessorTransaction
 from polar.models.processor_transaction import Processor
@@ -8,11 +10,20 @@ class ProcessorTransactionRepository(RepositoryBase[ProcessorTransaction]):
 
     async def get_latest_by_processor(
         self, processor: Processor
-    ) -> ProcessorTransaction | None:
-        statement = (
+    ) -> Sequence[ProcessorTransaction]:
+        latest_timestamp_statement = (
             self.get_base_statement()
             .where(ProcessorTransaction.processor == processor)
             .order_by(ProcessorTransaction.timestamp.desc())
             .limit(1)
         )
-        return await self.get_one_or_none(statement)
+        latest_transaction = await self.get_one_or_none(latest_timestamp_statement)
+
+        if latest_transaction is None:
+            return []
+
+        statement = self.get_base_statement().where(
+            ProcessorTransaction.processor == processor,
+            ProcessorTransaction.timestamp == latest_transaction.timestamp,
+        )
+        return await self.get_all(statement)

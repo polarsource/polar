7156e5e3556eab1ef1ba30dc0162fb34b7c6c763
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2025-07-21 16:22:27 +0200

    Orders table: Stop emails from overflowing into amount column

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/ClientPage.tsx
@@ -138,7 +138,9 @@ const ClientPage: React.FC<ClientPageProps> = ({
               avatar_url={customer.avatar_url}
               name={customer.name || customer.email}
             />
-            <div className="fw-medium">{customer.email}</div>
+            <div className="fw-medium hover:line-clamp-x overflow-hidden text-ellipsis">
+              {customer.email}
+            </div>
           </div>
         )
       },

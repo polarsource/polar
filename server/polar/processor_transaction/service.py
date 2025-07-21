e8aa06bded1c206fa209cceb8792632234148d90
François Voron <fvoron@gmail.com>
2025-07-21 15:21:45 +0200

    server/processor_transaction: handle colliding transactions timestamp while syncing

--- server/polar/processor_transaction/service.py
+++ server/polar/processor_transaction/service.py
@@ -9,11 +9,13 @@ from .repository import ProcessorTransactionRepository
 class ProcessorTransactionService:
     async def sync_stripe(self, session: AsyncSession) -> None:
         repository = ProcessorTransactionRepository.from_session(session)
-        latest = await repository.get_latest_by_processor(Processor.stripe)
+        latest_transactions = await repository.get_latest_by_processor(Processor.stripe)
+        latest_processor_ids = {tx.processor_id for tx in latest_transactions}
+
         balance_transactions = await stripe_service.list_balance_transactions()
         async for balance_transaction in balance_transactions:
             # We reached the point where we have already synced all the fees
-            if latest is not None and latest.processor_id == balance_transaction.id:
+            if balance_transaction.id in latest_processor_ids:
                 break
             await repository.create(
                 ProcessorTransaction.from_stripe(balance_transaction)

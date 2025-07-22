4d6fd9a9f8163dde93d74cc0549ea0c63e1852cc
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2025-07-22 10:42:42 +0200

    DetailRow: Allow adding CSS classes to the label

--- clients/apps/web/src/components/Shared/DetailRow.tsx
+++ clients/apps/web/src/components/Shared/DetailRow.tsx
@@ -5,17 +5,26 @@ export interface DetailRowProps {
   value: React.ReactNode
   action?: React.ReactNode
   valueClassName?: string
+  labelClassName?: string
 }
 
 export const DetailRow = ({
   label,
+  labelClassName = '',
   value,
   valueClassName = '',
   action,
 }: DetailRowProps) => {
   return (
     <div className="flex flex-col text-sm md:flex-row md:items-baseline md:justify-between md:gap-4">
-      <h3 className="dark:text-polar-500 flex-1 text-gray-500">{label}</h3>
+      <h3
+        className={twMerge(
+          'dark:text-polar-500 flex-1 text-gray-500',
+          labelClassName,
+        )}
+      >
+        {label}
+      </h3>
       <span
         className={twMerge(
           'dark:md:hover:bg-polar-800 group flex flex-1 flex-row flex-nowrap items-center justify-between gap-x-2 rounded-md transition-colors duration-75 md:px-2.5 md:py-1 hover:md:bg-gray-100',

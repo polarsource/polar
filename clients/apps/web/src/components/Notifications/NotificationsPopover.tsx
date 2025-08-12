b36679bb8cdf65207575f84a03bde075491bd206
Petru Rares Sincraian <psincraian@outlook.com>
2025-08-12 10:10:02 +0200

    revert: Incorrect payout account links in notifications (#6375) (#6395)
    This reverts commit 04fff8c2d55d9a805ef763ca3c07a3346d61feed.

--- clients/apps/web/src/components/Notifications/NotificationsPopover.tsx
+++ clients/apps/web/src/components/Notifications/NotificationsPopover.tsx
@@ -166,16 +166,13 @@ const MaintainerAccountUnderReview = ({
 }: {
   n: schemas['MaintainerAccountUnderReviewNotification']
 }) => {
-  const { payload } = n
   return (
     <Item n={n} iconClasses="bg-yellow-200 text-yellow-500">
       {{
         text: (
           <>
             Your{' '}
-            <InternalLink
-              href={`/dashboard/${payload.organization_name}/finance/account`}
-            >
+            <InternalLink href="/finance/account">
               <>payout account</>
             </InternalLink>{' '}
             is under review. Transfers are paused until we complete the review
@@ -193,16 +190,13 @@ const MaintainerAccountReviewed = ({
 }: {
   n: schemas['MaintainerAccountReviewedNotification']
 }) => {
-  const { payload } = n
   return (
     <Item n={n} iconClasses="bg-green-200 text-green-500">
       {{
         text: (
           <>
             Your{' '}
-            <InternalLink
-              href={`/dashboard/${payload.organization_name}/finance/account`}
-            >
+            <InternalLink href="/finance/account">
               <>payout account</>
             </InternalLink>{' '}
             has been reviewed successfully. Transfers are resumed.

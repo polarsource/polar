9a3062cb1b90f196a6258aeddade64dc35c55291
Konrad Kalemba <konrad_kal@hotmail.com>
2025-08-13 15:52:19 +0200

    add wildcard org redirect url

---
+++ clients/apps/web/src/app/(main)/dashboard/*/[[...route]]/page.tsx
@@ -0,0 +1,28 @@
+import { getServerSideAPI } from '@/utils/client/serverside'
+import { getLastVisitedOrg } from '@/utils/cookies'
+import { getUserOrganizations } from '@/utils/user'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ route: string[] }>
+}) {
+  const { route } = await params
+
+  const api = getServerSideAPI()
+  const userOrganizations = await getUserOrganizations(api)
+
+  if (userOrganizations.length === 0) {
+    redirect('/dashboard/create')
+  }
+
+  const org = userOrganizations.find(
+    (org) => org.slug === getLastVisitedOrg(cookies()),
+  )
+
+  const targetOrg = org?.slug ?? userOrganizations[0].slug
+
+  redirect(`/dashboard/${targetOrg}/${route.join('/')}`)
+}

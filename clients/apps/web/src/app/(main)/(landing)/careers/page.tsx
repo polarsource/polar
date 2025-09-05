b22b543467119c2d482ae1959f89cbfec15bca37
Birk Jernstrom <birkjernstrom@gmail.com>
2025-09-05 15:50:01 +0200

    website: Updated careers page and openings

--- clients/apps/web/src/app/(main)/(landing)/careers/page.tsx
+++ clients/apps/web/src/app/(main)/(landing)/careers/page.tsx
@@ -44,51 +44,97 @@ export default function CareersPage() {
       <h1 className="text-3xl md:text-5xl">Careers</h1>
       <div className="flex flex-col gap-12">
         <JobSection
-          title="Operations "
+          title="Product & Engineering"
           jobs={[
             {
-              role: 'Chief of Staff',
+              role: 'Staff Frontend Engineer',
               description:
-                'Help us scale from an early-stage startup to scale-up.',
+                'Come and build the Shadcn of Billing and shape our frontend architecture, standards, and user experience across both our dashboard and SDKs.',
+              location: 'Remote — Europe',
+              link: 'https://jobs.ashbyhq.com/polar/f47dae43-497f-4ad2-8123-f6e5d23485fb',
+            },
+            {
+              role: 'Staff Infrastructure Engineer',
+              description: 'Own the end-to-end architecture and implementation of our infrastructure to ensure world-class uptime and latency.',
+              location: 'Remote — Europe',
+              link: 'https://jobs.ashbyhq.com/polar/e610cfb0-a883-4138-aef0-f826f82958cb',
+            },
+            {
+              role: 'Senior Product Engineer',
+              description: 'Ship features, APIs and SDKs that empowers the next generation of developers to build businesses.',
+              location: 'Remote — Europe',
+              link: 'https://jobs.ashbyhq.com/polar/955c6935-6d03-46e5-b649-a8b958a52962',
+            },
+            {
+              role: 'Product Engineer',
+              description: 'Ship features, APIs and SDKs that empowers the next generation of developers to build businesses.',
+              location: 'Remote — Europe',
+              link: 'https://jobs.ashbyhq.com/polar/b2b48ba9-0b10-41a2-81b6-145c06fc5dcd',
+            },
+            {
+              role: 'Senior Growth Engineer',
+              description: 'Design and ship growth-focused features, enhancements and experiments end-to-end.',
+              location: 'Remote — Europe',
+              link: 'https://jobs.ashbyhq.com/polar/1496592e-16ff-47e7-b11e-a993c887fc1f',
+            },
+            {
+              role: 'Senior Product Designer (On-site)',
+              description: 'Design the future platform for developers to turn their software into a business.',
               location: 'Stockholm, Sweden',
-              link: 'https://jobs.gem.com/polar/am9icG9zdDp6OvHLVgSUOIC87Ij7TiE9',
+              link: 'https://jobs.ashbyhq.com/polar/1fee39f0-897c-4bdc-8ec0-a055f5d94a6c',
+            },
+            {
+              role: 'Senior Data Engineer',
+              description: 'Design, ship and optimize our internal data warehouse to Metric APIs for developers using Polar.',
+              location: 'Remote - Europe',
+              link: 'https://jobs.ashbyhq.com/polar/0ba77853-a18b-4d38-8516-4b0ab960ea00',
             },
           ]}
         />
 
         <JobSection
-          title="Engineering"
+          title="Community"
           jobs={[
             {
-              role: 'Staff Frontend Engineer',
-              description:
-                'Lead the development of our frontend codebase and how we ship composible billing components for developers',
-              location: 'Remote — Europe',
-              link: 'https://jobs.gem.com/polar/am9icG9zdDparTigzDB_RewSqH5o5htc',
+              role: 'Head of Developer Relations',
+              description: 'Build, lead, and scale our developer-facing efforts across community, content and advocacy. You’ll be the voice of Polar.',
+              location: 'San Francisco - Remote',
+              link: 'https://jobs.ashbyhq.com/polar/d09babe6-f727-42ec-b466-cf074f468f19',
             },
             {
-              role: 'Staff Product Engineer',
-              description:
-                'Work across our entire stack to ship innovative features to enhance how developers monetize their software.',
-              location: 'Remote — Europe',
-              link: 'https://jobs.gem.com/polar/am9icG9zdDpgsiQcAqkm5Om5LnR4TJs2',
-            },
-            {
-              role: 'Senior Product Engineer',
-              description:
-                'Work across our entire stack to ship innovative features to enhance how developers monetize their software.',
-              location: 'Remote — Europe',
-              link: 'https://jobs.gem.com/polar/am9icG9zdDpzKcc8t-KY2aAP96pbMHP_',
-            },
-            {
-              role: 'Support Engineer',
-              description:
-                'Help our customers get the most out of Polar. Improve our docs, build internal tooling and automations to scale those efforts.',
-              location: 'Remote — Europe',
-              link: 'https://jobs.gem.com/polar/am9icG9zdDq-gyYKkPmJozQGu_MfvgJ_',
+              role: 'Community Manager',
+              description: 'Nurture and scale our thriving developer community around Polar.',
+              location: 'Europe - Remote',
+              link: 'https://jobs.ashbyhq.com/polar/67fa55c0-af67-4f90-a88e-db6eab4daac0',
             },
           ]}
         />
+
+        <JobSection
+          title="Customer Success"
+          jobs={[
+            {
+              role: 'Support Engineer',
+              description: 'Help provide exceptional support to developers world-wide and scale our efforts by improving docs, guides and internal tooling.',
+              location: 'Europe - Remote',
+              link: 'https://jobs.ashbyhq.com/polar/3b7b5522-3781-4a6b-b112-5ad93320192a',
+            },
+          ]}
+        />
+
+        <JobSection
+          title="Business & Operations"
+          jobs={[
+            {
+              role: 'Founder Associate (On-site)',
+              description:
+                'Support Birk on high-leverage opportunities to mundane must does. No two weeks will look the same.',
+              location: 'Stockholm, Sweden',
+              link: 'https://jobs.ashbyhq.com/polar/fd957155-cc2e-4ab1-aa58-9c1ee161721c',
+            },
+          ]}
+        />
+
       </div>
     </div>
   )

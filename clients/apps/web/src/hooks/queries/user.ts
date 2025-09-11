8d055bc4b820ba8eed9348ee204fb4f70b66f9cc
François Voron <fvoron@gmail.com>
2025-09-11 12:17:00 +0200

    clients/web: revive Personal Access Token settings

--- clients/apps/web/src/hooks/queries/user.ts
+++ clients/apps/web/src/hooks/queries/user.ts
@@ -1,5 +1,34 @@
+import { queryClient } from '@/utils/api/query'
 import { api } from '@/utils/client'
-import { useMutation } from '@tanstack/react-query'
+import { unwrap } from '@polar-sh/client'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { defaultRetry } from './retry'
+
+export const usePersonalAccessTokens = () =>
+  useQuery({
+    queryKey: ['personalAccessTokens'],
+    queryFn: () => unwrap(api.GET('/v1/personal_access_tokens/')),
+    retry: defaultRetry,
+  })
+
+export const useDeletePersonalAccessToken = () =>
+  useMutation({
+    mutationFn: (variables: { id: string }) => {
+      return api.DELETE('/v1/personal_access_tokens/{id}', {
+        params: {
+          path: {
+            id: variables.id,
+          },
+        },
+      })
+    },
+    onSuccess: (result, _variables, _ctx) => {
+      if (result.error) {
+        return
+      }
+      queryClient.invalidateQueries({ queryKey: ['personalAccessTokens'] })
+    },
+  })
 
 export const useCreateIdentityVerification = () =>
   useMutation({

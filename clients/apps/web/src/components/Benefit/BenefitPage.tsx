5a403b40b40b2184ad49abdf25fb2a47b2bb6395
Pieter Beulque <pieterbeulque@gmail.com>
2025-09-19 10:42:01 +0200

    Add shared component for benefit grant status

--- clients/apps/web/src/components/Benefit/BenefitPage.tsx
+++ clients/apps/web/src/components/Benefit/BenefitPage.tsx
@@ -11,15 +11,9 @@ import Avatar from '@polar-sh/ui/components/atoms/Avatar'
 import Button from '@polar-sh/ui/components/atoms/Button'
 import { DataTable } from '@polar-sh/ui/components/atoms/DataTable'
 import FormattedDateTime from '@polar-sh/ui/components/atoms/FormattedDateTime'
-import { Status } from '@polar-sh/ui/components/atoms/Status'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@polar-sh/ui/components/ui/tooltip'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { twMerge } from 'tailwind-merge'
+import { BenefitGrantStatus } from './BenefitGrantStatus'
 
 export interface BenefitPageProps {
   benefit: schemas['Benefit']
@@ -120,45 +114,9 @@ export const BenefitPage = ({ benefit, organization }: BenefitPageProps) => {
           {
             accessorKey: 'status',
             header: 'Status',
-            cell: ({ row: { original: grant } }) => {
-              const isRevoked = grant.revoked_at !== null
-              const isGranted = grant.is_granted
-              const hasError = grant.error !== null
-
-              const status = hasError
-                ? 'Error'
-                : isRevoked
-                  ? 'Revoked'
-                  : isGranted
-                    ? 'Granted'
-                    : 'Pending'
-
-              const statusDescription = {
-                Revoked: 'The customer does not have access to this benefit',
-                Granted: 'The customer has access to this benefit',
-                Pending: 'The benefit grant is currently being processed',
-                Error: grant.error?.message ?? 'An unknown error occurred',
-              }
-
-              const statusClassNames = {
-                Revoked: 'bg-red-100 text-red-500 dark:bg-red-950',
-                Granted: 'bg-emerald-200 text-emerald-500 dark:bg-emerald-950',
-                Pending: 'bg-yellow-100 text-yellow-500 dark:bg-yellow-950',
-                Error: 'bg-red-100 text-red-500 dark:bg-red-950',
-              }
-
-              return (
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Status
-                      className={twMerge('w-fit', statusClassNames[status])}
-                      status={status}
-                    />
-                  </TooltipTrigger>
-                  <TooltipContent>{statusDescription[status]}</TooltipContent>
-                </Tooltip>
-              )
-            },
+            cell: ({ row: { original: grant } }) => (
+              <BenefitGrantStatus grant={grant} />
+            ),
           },
           {
             accessorKey: 'created_at',

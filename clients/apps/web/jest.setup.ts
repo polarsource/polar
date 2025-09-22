5243ded0ebb4ab6b72eb881f2bd87bb9e7219943
Pieter Beulque <pieterbeulque@gmail.com>
2025-09-22 09:28:16 +0200

    Remove tests

--- clients/apps/web/jest.setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-
-import { TextDecoder, TextEncoder } from 'util'
-
-jest.mock('shiki', () => ({}))
-jest.mock('shiki/bundle/full', () => ({}))
-
-Object.assign(global, { TextDecoder, TextEncoder })

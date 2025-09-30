59165dfe80c115495aafd41358c4298dcedbc5df
Emil Widlund <hello@emilwidlund.com>
2025-09-30 11:22:12 +0200

    checkout: fix ugly checkout layout issues

--- clients/packages/checkout/src/components/AmountLabel.tsx
+++ clients/packages/checkout/src/components/AmountLabel.tsx
@@ -42,7 +42,9 @@ const AmountLabel: React.FC<AmountLabelProps> = ({
   return (
     <div className="flex flex-row items-baseline gap-x-1">
       {formatCurrencyNumber(amount, currency, minimumFractionDigits)}
-      <span className="text-[max(12px,0.5em)]">{intervalDisplay}</span>
+      {intervalDisplay ? (
+        <span className="text-[max(12px,0.5em)]">{intervalDisplay}</span>
+      ) : null}
     </div>
   )
 }

f627f05fbe7b718a1c736e1e475728d626eaf645
Pieter Beulque <pieterbeulque@gmail.com>
2025-10-03 08:03:14 +0200

    Do not fetch logged in user for docs (#7138)
    * Do not fetch logged in user for docs
    
    * Exclude more paths

--- clients/apps/web/src/middleware.ts
+++ clients/apps/web/src/middleware.ts
@@ -68,10 +68,11 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - api (API routes)
      * - ingest (Posthog)
+     * - docs, _mintlify, mintlify-assets (Mintlify)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!api|ingest|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!api|ingest|docs|_mintlify|mintlify-assets|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 }

e03f0f41c97a52e66de1c95815d7a7f3b200de3c
François Voron <fvoron@gmail.com>
2025-10-08 14:42:04 +0200

    e2e: fix ci

--- e2e/tests/test_sample.py
+++ e2e/tests/test_sample.py
@@ -1,6 +1,21 @@
+from typing import Any
+
+import pytest
 from playwright.sync_api import Page, expect
+
+
+@pytest.fixture
+def browser_context_args() -> dict[str, Any]:
+    return {
+        "base_url": "http://127.0.0.1:3000",
+        "screen": {
+            "width": 1280,
+            "height": 720,
+        },
+    }
 
 
 def test_landing(page: Page):
     page.goto("/")
+    page.wait_for_selector("h1.leading-tight\\!", state="visible")
     expect(page.get_by_text("Monetize your software")).to_be_visible()

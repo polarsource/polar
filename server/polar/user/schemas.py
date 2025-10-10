794a6e0b095025d1d69e88ed28ca987803ca670a
François Voron <fvoron@gmail.com>
2025-10-10 14:35:33 +0200

    server/user: add email_hash to authenticate Plain chat widget

--- server/polar/user/schemas.py
+++ server/polar/user/schemas.py
@@ -1,10 +1,13 @@
+import hashlib
+import hmac
 import uuid
 from typing import Annotated, Literal
 
 from fastapi import Depends
-from pydantic import UUID4, EmailStr
+from pydantic import UUID4, EmailStr, computed_field
 
 from polar.auth.scope import Scope
+from polar.config import settings
 from polar.kit.schemas import Schema, TimestampedSchema, UUID4ToStr
 from polar.models.user import IdentityVerificationStatus, OAuthPlatform
 
@@ -29,6 +32,17 @@ class UserRead(UserBase, TimestampedSchema):
     identity_verified: bool
     identity_verification_status: IdentityVerificationStatus
     oauth_accounts: list[OAuthAccountRead]
+
+    @computed_field
+    def email_hash(self) -> str | None:
+        if settings.PLAIN_CHAT_SECRET is None:
+            return None
+        message = hmac.new(
+            settings.PLAIN_CHAT_SECRET.encode("utf-8"),
+            self.email.encode("utf-8"),
+            hashlib.sha256,
+        )
+        return message.hexdigest()
 
 
 class UserIdentityVerification(Schema):

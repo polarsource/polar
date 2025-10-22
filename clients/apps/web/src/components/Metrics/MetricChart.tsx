d8829c5647328ed8230d838fcc81c3d2cbb5bf95
Pieter Beulque <pieterbeulque@gmail.com>
2025-10-22 14:31:22 +0200

    Slight corner radius to reduce harshness

--- clients/apps/web/src/components/Metrics/MetricChart.tsx
+++ clients/apps/web/src/components/Metrics/MetricChart.tsx
@@ -226,9 +226,9 @@ const MetricChart = ({
           {yAxis}
           {tooltip}
           {previousData && (
-            <Bar dataKey="previous" fill="var(--color-previous)" radius={0} />
+            <Bar dataKey="previous" fill="var(--color-previous)" radius={1} />
           )}
-          <Bar dataKey="current" fill="var(--color-current)" radius={0} />
+          <Bar dataKey="current" fill="var(--color-current)" radius={1} />
         </BarChart>
       )
     }

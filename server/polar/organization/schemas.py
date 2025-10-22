82c2fc1a413505bcbafc911836cf54996bac54b4
Emil Widlund <hello@emilwidlund.com>
2025-10-22 15:30:26 +0200

    revops: add revops feature flag

--- server/polar/organization/schemas.py
+++ server/polar/organization/schemas.py
@@ -65,6 +65,9 @@ class OrganizationFeatureSettings(Schema):
     seat_based_pricing_enabled: bool = Field(
         False, description="If this organization has seat-based pricing enabled"
     )
+    revops_enabled: bool = Field(
+        False, description="If this organization has RevOps enabled"
+    )
 
 
 class OrganizationSubscribePromoteSettings(Schema):

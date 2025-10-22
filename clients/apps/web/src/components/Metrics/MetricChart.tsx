a21badbdd55107a6919efea675774949c7597d3d
Pieter Beulque <pieterbeulque@gmail.com>
2025-10-22 12:24:50 +0200

    Add bar charts

--- clients/apps/web/src/components/Metrics/MetricChart.tsx
+++ clients/apps/web/src/components/Metrics/MetricChart.tsx
@@ -2,6 +2,8 @@ import { ParsedMetricPeriod } from '@/hooks/queries'
 import { getFormattedMetricValue, getTimestampFormatter } from '@/utils/metrics'
 import { schemas } from '@polar-sh/client'
 import {
+  Bar,
+  BarChart,
   CartesianGrid,
   ChartContainer,
   ChartTooltip,
@@ -27,6 +29,7 @@ interface MetricChartProps {
   onDataIndexHover?: (index: number | undefined) => void
   simple?: boolean
   showYAxis?: boolean
+  chartType?: 'line' | 'bar'
 }
 
 const MetricChart = ({
@@ -40,6 +43,7 @@ const MetricChart = ({
   onDataIndexHover,
   simple = false,
   showYAxis = false,
+  chartType = 'line',
 }: MetricChartProps & {
   ref?: React.RefObject<HTMLDivElement>
 }) => {
@@ -129,71 +133,104 @@ const MetricChart = ({
     [metric],
   )
 
-  return (
-    <ChartContainer
-      ref={ref}
-      style={{ height: _height, width: _width || '100%' }}
-      config={config}
-    >
-      <LineChart
-        accessibilityLayer
-        data={mergedData}
-        margin={{
-          left: 24,
-          right: 24,
-          top: 24,
-        }}
-        onMouseMove={(state) => {
-          if (onDataIndexHover) {
-            const index = state.activeTooltipIndex
-            onDataIndexHover(typeof index === 'number' ? index : undefined)
-          }
-        }}
-        onMouseLeave={() => {
-          if (onDataIndexHover) {
-            onDataIndexHover(undefined)
-          }
-        }}
-      >
-        {simple ? undefined : (
-          <CartesianGrid
-            horizontal={false}
-            vertical={true}
-            stroke={isDark ? '#222225' : '#ccc'}
-            strokeDasharray="6 6"
+  const chartContent = useMemo(() => {
+    const commonProps = {
+      accessibilityLayer: true,
+      data: mergedData,
+      margin: {
+        left: 24,
+        right: 24,
+        top: 24,
+      },
+      onMouseMove: (state: any) => {
+        if (onDataIndexHover) {
+          const index = state.activeTooltipIndex
+          onDataIndexHover(typeof index === 'number' ? index : undefined)
+        }
+      },
+      onMouseLeave: () => {
+        if (onDataIndexHover) {
+          onDataIndexHover(undefined)
+        }
+      },
+    }
+
+    const grid = simple ? undefined : (
+      <CartesianGrid
+        horizontal={false}
+        vertical={true}
+        stroke={isDark ? '#222225' : '#ccc'}
+        strokeDasharray="6 6"
+      />
+    )
+
+    const xAxis = (
+      <XAxis
+        dataKey="timestamp"
+        tickLine={false}
+        axisLine={false}
+        tickMargin={8}
+        interval="equidistantPreserveStart"
+        ticks={ticks}
+        tickFormatter={timestampFormatter}
+      />
+    )
+
+    const yAxis = showYAxis ? (
+      <YAxis
+        tickLine={false}
+        axisLine={false}
+        allowDecimals={hasDecimalValues}
+        domain={[0, (max) => Math.ceil(max / 3) * 3]} // This will make sure we can always render 3 horizontal grid lines
+        tickMargin={4}
+        width={4}
+      />
+    ) : undefined
+
+    const tooltip = (
+      <ChartTooltip<number, string>
+        cursor={true}
+        content={(props) => (
+          <ChartTooltipContent
+            {...props}
+            className="text-black dark:text-white"
+            indicator="dot"
+            labelKey="metric"
+            formatter={formatter}
           />
         )}
-        <XAxis
-          dataKey="timestamp"
-          tickLine={false}
-          axisLine={false}
-          tickMargin={8}
-          interval="equidistantPreserveStart"
-          ticks={ticks}
-          tickFormatter={timestampFormatter}
-        />
-        {showYAxis && (
-          <YAxis
-            tickLine={false}
-            axisLine={false}
-            allowDecimals={hasDecimalValues}
-            domain={[0, (max) => Math.ceil(max / 3) * 3]} // This will make sure we can always render 3 horizontal grid lines
-            tickMargin={4}
-            width={4}
-          />
-        )}
-        <ChartTooltip<number, string>
-          cursor={true}
-          content={(props) => (
-            <ChartTooltipContent
-              {...props}
-              className="text-black dark:text-white"
-              indicator="dot"
-              labelKey="metric"
-              formatter={formatter}
+      />
+    )
+
+    if (chartType === 'bar') {
+      return (
+        <BarChart {...commonProps}>
+          {grid}
+          {xAxis}
+          {yAxis}
+          {tooltip}
+          {previousData && (
+            <Bar
+              dataKey="previous"
+              fill="var(--color-previous)"
+              radius={4}
             />
           )}
-        />
+          <Bar
+            dataKey="current"
+            fill="var(--color-current)"
+            radius={4}
+          />
+        </BarChart>
+      )
+    }
+
+    return (
+      <LineChart {...commonProps}>
+        {grid}
+        {xAxis}
+        {yAxis}
+        {tooltip}
         {previousData && (
           <Line
             dataKey="previous"
@@ -211,6 +248,16 @@ const MetricChart = ({
           strokeWidth={1.5}
         />
       </LineChart>
+    )
+  }, [chartType, mergedData, simple, isDark, ticks, timestampFormatter, formatter, previousData, onDataIndexHover, showYAxis, hasDecimalValues])
+
+  return (
+    <ChartContainer
+      ref={ref}
+      style={{ height: _height, width: _width || '100%' }}
+      config={config}
+    >
+      {chartContent}
     </ChartContainer>
   )
 }

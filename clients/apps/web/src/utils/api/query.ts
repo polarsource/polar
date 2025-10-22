a01e2d1a47ede49e72a1c86333f5bd63a7598c7a
Pieter Beulque <pieterbeulque@gmail.com>
2025-10-22 10:49:26 +0200

    Remove gcTime since we do not persist

--- clients/apps/web/src/utils/api/query.ts
+++ clients/apps/web/src/utils/api/query.ts
@@ -9,9 +9,6 @@ function makeQueryClient() {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
         staleTime: 60 * 1000,
-        // gcTime needs to be _higher_ than maxAge in any persisted clients
-        // see https://tanstack.com/query/v5/docs/react/plugins/persistQueryClient
-        gcTime: 1000 * 60 * 60, // 1 hour
       },
     },
   })

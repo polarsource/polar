9f76ec8bcf4a55ca1ce7a3182702613063d80226
Emil Widlund <hello@emilwidlund.com>
2025-10-22 09:51:54 +0200

    events: add proper scroll to event explorer sidebar

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/usage-billing/events/ClientPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/usage-billing/events/ClientPage.tsx
@@ -31,7 +31,8 @@ import {
   parseAsStringLiteral,
   useQueryState,
 } from 'nuqs'
-import React, { useCallback, useMemo } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 const PAGE_SIZE = 300
 
@@ -107,6 +108,21 @@ const ClientPage: React.FC<ClientPageProps> = ({ organization }) => {
     [setStartDate, setEndDate],
   )
 
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  const handleScroll = useCallback(
+    (event: React.UIEvent<HTMLDivElement>) => {
+      if (event.currentTarget.scrollTop > 0 && !hasScrolled) {
+        setHasScrolled(true)
+        console.log('hasScrolled')
+      } else if (event.currentTarget.scrollTop === 0 && hasScrolled) {
+        setHasScrolled(false)
+        console.log('has not scrolled')
+      }
+    },
+    [hasScrolled],
+  )
+
   const dateRange = {
     from: startDate,
     to: endDate,
@@ -118,8 +134,8 @@ const ClientPage: React.FC<ClientPageProps> = ({ organization }) => {
       contextViewPlacement="left"
       contextViewClassName="w-full lg:max-w-[320px] xl:max-w-[320px] h-full overflow-y-hidden"
       contextView={
-        <div className="flex h-full flex-col gap-y-4 px-4 py-4">
-          <div className="flex flex-row items-center justify-between gap-6">
+        <div className="flex h-full flex-col gap-y-4">
+          <div className="flex flex-row items-center justify-between gap-6 px-4 pt-4">
             <div>Events</div>
             <div className="flex flex-row items-center gap-4">
               <Button
@@ -131,72 +147,85 @@ const ClientPage: React.FC<ClientPageProps> = ({ organization }) => {
               </Button>
             </div>
           </div>
-          <div className="flex flex-row items-center gap-3">
-            <Input
-              placeholder="Search Events"
-              value={query ?? undefined}
-              onChange={(e) => setQuery(e.target.value)}
-              preSlot={<Search fontSize="small" />}
-              disabled
-            />
-          </div>
-          <div className="flex h-full grow flex-col gap-y-6">
-            <div className="flex flex-col gap-y-2">
-              <h3 className="text-sm">Timeline</h3>
-              <DateRangePicker
-                date={dateRange}
-                onDateChange={onDateRangeChange}
+          <div
+            className={twMerge(
+              'flex flex-col gap-y-6 overflow-y-auto px-4 pb-4 pt-2',
+              hasScrolled && 'dark:border-polar-700 border-t border-gray-200',
+            )}
+            onScroll={handleScroll}
+          >
+            <div className="flex flex-row items-center gap-3">
+              <Input
+                placeholder="Search Events"
+                value={query ?? undefined}
+                onChange={(e) => setQuery(e.target.value)}
+                preSlot={<Search fontSize="small" />}
+                disabled
               />
             </div>
-            <div className="flex flex-col gap-y-2">
-              <h3 className="text-sm">Sorting</h3>
-              <Select
-                value={sorting}
-                onValueChange={(value) =>
-                  setSorting(value as '-timestamp' | 'timestamp')
-                }
-              >
-                <SelectTrigger>
-                  <SelectValue placeholder="Sort by" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="-timestamp">Newest</SelectItem>
-                  <SelectItem value="timestamp">Oldest</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            {eventNames.length > 0 && (
+            <div className="flex h-full grow flex-col gap-y-6">
               <div className="flex flex-col gap-y-2">
-                <h3 className="text-sm">Event</h3>
-                <List size="small" className="rounded-xl">
-                  {eventNames.map((eventName) => (
-                    <ListItem
-                      key={eventName.name}
-                      size="small"
-                      className="justify-between px-3 font-mono text-sm"
-                      inactiveClassName="text-gray-500 dark:text-polar-500"
-                      selected={selectedEventNames?.includes(eventName.name)}
-                      onSelect={() =>
-                        setSelectedEventNames((prev) =>
-                          prev && prev.includes(eventName.name)
-                            ? prev.filter((name) => name !== eventName.name)
-                            : ([...(prev ?? []), eventName.name] as string[]),
-                        )
-                      }
-                    >
-                      <span className="w-full truncate">{eventName.name}</span>
-                      <span className="text-xxs dark:text-polar-500 font-mono text-gray-500">
-                        {Number(eventName.occurrences).toLocaleString('en-US', {
-                          style: 'decimal',
-                          compactDisplay: 'short',
-                          notation: 'compact',
-                        })}
-                      </span>
-                    </ListItem>
-                  ))}
-                </List>
+                <h3 className="text-sm">Timeline</h3>
+                <DateRangePicker
+                  date={dateRange}
+                  onDateChange={onDateRangeChange}
+                />
               </div>
-            )}
+              <div className="flex flex-col gap-y-2">
+                <h3 className="text-sm">Sorting</h3>
+                <Select
+                  value={sorting}
+                  onValueChange={(value) =>
+                    setSorting(value as '-timestamp' | 'timestamp')
+                  }
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Sort by" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="-timestamp">Newest</SelectItem>
+                    <SelectItem value="timestamp">Oldest</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              {eventNames.length > 0 && (
+                <div className="flex flex-col gap-y-2">
+                  <h3 className="text-sm">Event</h3>
+                  <List size="small" className="rounded-xl">
+                    {eventNames.map((eventName) => (
+                      <ListItem
+                        key={eventName.name}
+                        size="small"
+                        className="justify-between px-3 font-mono text-sm"
+                        inactiveClassName="text-gray-500 dark:text-polar-500"
+                        selected={selectedEventNames?.includes(eventName.name)}
+                        onSelect={() =>
+                          setSelectedEventNames((prev) =>
+                            prev && prev.includes(eventName.name)
+                              ? prev.filter((name) => name !== eventName.name)
+                              : ([...(prev ?? []), eventName.name] as string[]),
+                          )
+                        }
+                      >
+                        <span className="w-full truncate">
+                          {eventName.name}
+                        </span>
+                        <span className="text-xxs dark:text-polar-500 font-mono text-gray-500">
+                          {Number(eventName.occurrences).toLocaleString(
+                            'en-US',
+                            {
+                              style: 'decimal',
+                              compactDisplay: 'short',
+                              notation: 'compact',
+                            },
+                          )}
+                        </span>
+                      </ListItem>
+                    ))}
+                  </List>
+                </div>
+              )}
+            </div>
           </div>
           <Modal
             isShown={isEventCreationGuideShown}
@@ -211,7 +240,7 @@ const ClientPage: React.FC<ClientPageProps> = ({ organization }) => {
     >
       <div className="flex h-full flex-col gap-y-4">
         {events?.items.length === 0 ? (
-          <div className="dark:border-polar-700 flex min-h-96 w-full flex-col items-center justify-center gap-4 rounded-4xl border border-gray-200 p-24">
+          <div className="dark:border-polar-700 rounded-4xl flex min-h-96 w-full flex-col items-center justify-center gap-4 border border-gray-200 p-24">
             <h1 className="text-2xl font-normal">No Events Found</h1>
             <p className="dark:text-polar-500 text-gray-500">
               There are no events matching your current filters

895a0cdd4d7398a92a12208e4b4a7e7150751f8d
Emil Widlund <hello@emilwidlund.com>
2025-10-26 21:51:24 +0100

    event: remove private fields from ingested events

--- clients/apps/web/src/components/Events/EventCard/UserEventCard.tsx
+++ clients/apps/web/src/components/Events/EventCard/UserEventCard.tsx
@@ -1,11 +1,35 @@
 import { schemas } from '@polar-sh/client'
+import { useMemo } from 'react'
 import { EventCardBase } from './EventCardBase'
 
+const useMetadata = (event: schemas['Event']) => {
+  return useMemo(() => {
+    const metadata = { ...event.metadata }
+    const metadataWithoutPrivateFields = Object.entries(metadata).reduce(
+      (acc, [key, value]) => {
+        if (key.startsWith('_')) return acc
+        return { ...acc, [key]: value }
+      },
+      {},
+    )
+
+    return Object.keys(metadataWithoutPrivateFields).length > 0
+      ? metadataWithoutPrivateFields
+      : undefined
+  }, [event])
+}
+
 export const UserEventCard = ({ event }: { event: schemas['Event'] }) => {
+  const metadataToRender = useMetadata(event)
+
+  if (!metadataToRender) {
+    return null
+  }
+
   return (
     <EventCardBase>
       <pre className="flex p-2 select-text">
-        {JSON.stringify(event.metadata, null, 2)}
+        {JSON.stringify(metadataToRender, null, 2)}
       </pre>
     </EventCardBase>
   )

ca8a6986b1c7b29363d8f51f47b45b24c4dfd138
Jesper Bränn <jesper@polar.sh>
2025-11-03 14:33:59 +0100

    feat(events): Add children_count to Events

--- server/polar/models/event.py
+++ server/polar/models/event.py
@@ -12,10 +12,13 @@ from sqlalchemy import (
     Uuid,
     and_,
     case,
+    column,
     exists,
     extract,
+    func,
     or_,
     select,
+    table,
 )
 from sqlalchemy import (
     cast as sql_cast,
@@ -163,6 +166,17 @@ class Event(Model, MetadataMixin):
             else_=external_customer_id,
         )
     )
+
+    @declared_attr
+    def child_count(cls) -> Mapped[int]:
+        child_events = table("events", column("parent_id")).alias("child_events")
+        return column_property(
+            select(func.count())
+            .select_from(child_events)
+            .where(child_events.c.parent_id == cls.id)
+            .correlate_except(child_events)
+            .scalar_subquery()
+        )
 
     organization_id: Mapped[UUID] = mapped_column(
         Uuid,

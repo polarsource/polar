75bcf3be47bc1363a0d68a77dc0899a6f6e3516e
Emil Widlund <hello@emilwidlund.com>
2025-11-11 10:38:40 +0100

    careers: adjust career page

--- clients/apps/web/src/components/Vision/JobSection.tsx
+++ clients/apps/web/src/components/Vision/JobSection.tsx
@@ -8,7 +8,7 @@ export interface JobSectionProps {
 export const JobSection = ({ title, jobs }: JobSectionProps) => {
   return (
     <section className="flex flex-col gap-6 lg:gap-8">
-      <h4>{title}</h4>
+      <h4 className="text-xl">{title}</h4>
       <div className="flex flex-col gap-2">
         {jobs.map((job) => (
           <JobLink key={job.link} {...job} />

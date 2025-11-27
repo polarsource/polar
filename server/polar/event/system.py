53c3b174c548d186ffe580dbd157e4a7d874eae5
Jesper Bränn <jesper@polar.sh>
2025-11-27 09:55:06 +0100

    events/system: Add more information to order.paid
    We want to be able to look at an order.paid event and see all information
    about that order without having to go via the orders table.

--- server/polar/event/system.py
+++ server/polar/event/system.py
@@ -223,7 +223,15 @@ class OrderPaidMetadata(TypedDict):
     order_id: str
     amount: int
     currency: str
-    backfilled: NotRequired[bool]
+    net_amount: int
+    tax_amount: int
+    applied_balance_amount: int
+    discount_amount: int
+    discount_id: NotRequired[str]
+    platform_fee: int
+    subscription_id: NotRequired[str]
+    recurring_interval: NotRequired[str]
+    recurring_interval_count: NotRequired[int]
 
 
 class OrderPaidEvent(Event):
@@ -237,7 +245,6 @@ class OrderRefundedMetadata(TypedDict):
     order_id: str
     refunded_amount: int
     currency: str
-    backfilled: NotRequired[bool]
 
 
 class OrderRefundedEvent(Event):

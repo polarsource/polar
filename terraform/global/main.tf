57e8527008316e61aa673d0cd8bcc9864afb60d5
Jesper Bränn <jesper@polar.sh>
2025-12-01 15:07:17 +0100

    tf: Add global variable set
    For variables that we want to configure once and hold
    across multiple environments.

---
+++ terraform/global/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+provider "render" {
+}
+
+resource "tfe_variable_set" "global" {
+  name         = "Global Settings"
+  description  = "For variables that are used in multiple or all environments"
+  organization = "polar-sh"
+  global       = true
+}
+
+resource "tfe_variable" "apple_client_id" {
+  key         = "apple_client_id"
+  category    = "terraform"
+  description = "Apple Client ID"
+  sensitive   = true
+}
+
+resource "tfe_variable" "apple_team_id" {
+  key         = "apple_team_id"
+  category    = "terraform"
+  description = "Apple Team ID"
+  sensitive   = true
+}
+
+resource "tfe_variable" "apple_key_id" {
+  key         = "apple_key_id"
+  category    = "terraform"
+  description = "Apple Key ID"
+  sensitive   = true
+}
+
+resource "tfe_variable" "apple_key_value" {
+  key         = "apple_key_value"
+  category    = "terraform"
+  description = "Apple Key Value"
+  sensitive   = true
+}

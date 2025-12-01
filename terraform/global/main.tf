b828d55628b0d9ba0588b93debed7e0d3b813d0d
Jesper Bränn <jesper@polar.sh>
2025-12-01 15:16:23 +0100

    tf: Add missing variable set id

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -13,29 +13,33 @@ resource "tfe_variable_set" "global" {
 }
 
 resource "tfe_variable" "apple_client_id" {
-  key         = "apple_client_id"
-  category    = "terraform"
-  description = "Apple Client ID"
-  sensitive   = true
+  key             = "apple_client_id"
+  category        = "terraform"
+  description     = "Apple Client ID"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.global.id
 }
 
 resource "tfe_variable" "apple_team_id" {
-  key         = "apple_team_id"
-  category    = "terraform"
-  description = "Apple Team ID"
-  sensitive   = true
+  key             = "apple_team_id"
+  category        = "terraform"
+  description     = "Apple Team ID"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.global.id
 }
 
 resource "tfe_variable" "apple_key_id" {
-  key         = "apple_key_id"
-  category    = "terraform"
-  description = "Apple Key ID"
-  sensitive   = true
+  key             = "apple_key_id"
+  category        = "terraform"
+  description     = "Apple Key ID"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.global.id
 }
 
 resource "tfe_variable" "apple_key_value" {
-  key         = "apple_key_value"
-  category    = "terraform"
-  description = "Apple Key Value"
-  sensitive   = true
+  key             = "apple_key_value"
+  category        = "terraform"
+  description     = "Apple Key Value"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.global.id
 }

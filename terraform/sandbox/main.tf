eee6631404df8003526f86005398d3e3691613e4
Jesper Bränn <jesper@polar.sh>
2025-12-02 11:10:58 +0100

    tf: Remove non-existant sandbox imports

--- terraform/sandbox/main.tf
+++ terraform/sandbox/main.tf
@@ -34,41 +34,7 @@ import {
 }
 
 import {
-  to = module.s3_buckets.aws_s3_bucket.public_assets
-  id = "polar-sandbox-public-assets"
-}
-
-import {
-  to = module.s3_buckets.aws_s3_bucket.public_files
-  id = "polar-sandbox-public-files"
-}
-
-import {
   to = module.s3_buckets.aws_s3_bucket_cors_configuration.files
   id = "polar-sandbox-files"
 }
 
-import {
-  to = module.s3_buckets.aws_s3_bucket_cors_configuration.public_files
-  id = "polar-sandbox-public-files"
-}
-
-import {
-  to = module.s3_buckets.aws_s3_bucket_policy.public_assets
-  id = "polar-sandbox-public-assets"
-}
-
-import {
-  to = module.s3_buckets.aws_s3_bucket_policy.public_files
-  id = "polar-sandbox-public-files"
-}
-
-import {
-  to = module.s3_buckets.aws_s3_bucket_public_access_block.public_assets
-  id = "polar-sandbox-public-assets"
-}
-
-import {
-  to = module.s3_buckets.aws_s3_bucket_public_access_block.public_files
-  id = "polar-sandbox-public-files"
-}

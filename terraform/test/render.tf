05c9082ab4f29996d3e792479eac98340e6f3b78
Jesper Bränn <jesper@polar.sh>
2025-12-02 16:44:03 +0100

    tf: worker correct image tag

--- terraform/test/render.tf
+++ terraform/test/render.tf
@@ -117,7 +117,7 @@ module "test" {
   workers = {
     worker = {
       start_command      = "uv run dramatiq -p 2 -t 4 -f polar.worker.scheduler:start polar.worker.run"
-      tag                = "test"
+      tag                = "latest"
       dramatiq_prom_port = "10000"
     }
   }

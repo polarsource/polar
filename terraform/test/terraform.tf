7e7e3926e72ed8b93848d96261368751ff7018a6
Jesper Bränn <jesper@polar.sh>
2025-12-02 09:29:15 +0100

    tf: Set up test environment

---
+++ terraform/test/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "polar-sh"
+    workspaces {
+      name = "polar"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+
+    render = {
+      source  = "render-oss/render"
+      version = "1.8.0"
+    }
+  }
+
+  required_version = ">= 1.2"
+}

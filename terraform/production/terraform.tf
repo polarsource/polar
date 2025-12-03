b7733395f003c323f4f4e938b4bd897b548bac59
Jesper Bränn <jesper@polar.sh>
2025-12-03 13:01:39 +0100

    tf/cloudflare: Map up in sandbox and prod as well

--- terraform/production/terraform.tf
+++ terraform/production/terraform.tf
@@ -16,6 +16,11 @@ terraform {
       source  = "render-oss/render"
       version = "1.8.0"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.13"
+    }
   }
 
   required_version = ">= 1.2"

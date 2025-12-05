18b29ae58440b6a476b2b4cd774200471b74740a
Petru Rares Sincraian <psincraian@outlook.com>
2025-12-05 12:38:57 +0100

    feat/web: enable sentry tracing (#8321)

--- clients/apps/web/sentry.edge.config.ts
+++ clients/apps/web/sentry.edge.config.ts
@@ -11,7 +11,7 @@ Sentry.init({
   environment: CONFIG.ENVIRONMENT,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,

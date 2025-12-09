408a342108f306797c62029e15e1a9011b297487
François Voron <fvoron@gmail.com>
2025-12-09 08:30:59 +0100

    server/worker: disable Sentry (OOM?)

--- server/polar/worker/scheduler.py
+++ server/polar/worker/scheduler.py
@@ -7,11 +7,9 @@ from polar import tasks
 from polar.customer_meter.scheduler import CustomerMeterJobStore
 from polar.logfire import configure_logfire
 from polar.logging import configure as configure_logging
-from polar.sentry import configure_sentry
 from polar.subscription.scheduler import SubscriptionJobStore
 from polar.worker import scheduler_middleware
 
-configure_sentry()
 configure_logfire("worker")
 configure_logging(logfire=True)
 

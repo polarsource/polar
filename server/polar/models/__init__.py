3c0d7b194650d61f0d89dc291bcf921ac40b9feb
Jesper Bränn <brann.jesper@gmail.com>
2025-12-18 12:33:18 +0100

    Revert "events: Add materialsed events metrics view"
    This reverts commit a67fee3094592a5dc7576eb1722175cc263caa8b.

--- server/polar/models/__init__.py
+++ server/polar/models/__init__.py
@@ -23,7 +23,6 @@ from .downloadable import Downloadable
 from .email_verification import EmailVerification
 from .event import Event, EventClosure
 from .event_hyper import EventHyper
-from .event_metrics import EventMetricsHourly
 from .event_type import EventType
 from .external_event import ExternalEvent
 from .file import File
@@ -108,7 +107,6 @@ __all__ = [
     "Event",
     "EventClosure",
     "EventHyper",
-    "EventMetricsHourly",
     "EventType",
     "ExternalEvent",
     "File",

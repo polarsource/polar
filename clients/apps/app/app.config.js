92e88f1db53668f0f7157f9109883833fbc49ff2
Emil Widlund <hello@emilwidlund.com>
2025-12-19 15:20:21 +0100

    app: bump polar app version

--- clients/apps/app/app.config.js
+++ clients/apps/app/app.config.js
@@ -44,7 +44,7 @@ module.exports = {
   expo: {
     name: 'Polar',
     slug: 'Polar',
-    version: '1.1.0',
+    version: '1.2.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'polar',

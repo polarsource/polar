ec8d040595662d71f91b4b52267aac3bde814992
Emil Widlund <hello@emilwidlund.com>
2025-12-25 22:47:07 +0100

    merge alembic heads

---
+++ server/migrations/versions/2025-12-25-2246_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 0a8d663cc43d
+Revises: 400b90a4d953, 916e176efd47
+Create Date: 2025-12-25 22:46:56.000877
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# Polar Custom Imports
+
+# revision identifiers, used by Alembic.
+revision = "0a8d663cc43d"
+down_revision = ("400b90a4d953", "916e176efd47")
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

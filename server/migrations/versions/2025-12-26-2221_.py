13c7874fb924b04e30b4fded1b10777d44ef0b47
Jesper Bränn <brann.jesper@gmail.com>
2026-01-05 19:10:25 +0100

    Revert "adjust migrations"
    This reverts commit a0220721040dada24fbae512c52c9028532e887e.

---
+++ server/migrations/versions/2025-12-26-2221_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 99e264e9af83
+Revises: 24bb42b493d7, 8c4a2b3d5e6f
+Create Date: 2025-12-26 22:21:12.037933
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# Polar Custom Imports
+
+# revision identifiers, used by Alembic.
+revision = "99e264e9af83"
+down_revision = ("24bb42b493d7", "8c4a2b3d5e6f")
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

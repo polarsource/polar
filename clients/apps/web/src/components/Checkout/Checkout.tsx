88cccb39e62ed8be8d0af9b5f771bf287c596d6e
Sebastian Ekström <seb.ekstrom@gmail.com>
2026-01-08 13:39:50 +0100

    Remove merchant avatar experiment (#8811)

--- clients/apps/web/src/components/Checkout/Checkout.tsx
+++ clients/apps/web/src/components/Checkout/Checkout.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import type { ExperimentVariant } from '@/experiments'
 import { useCheckoutConfirmedRedirect } from '@/hooks/checkout'
 import { usePostHog } from '@/hooks/posthog'
 import { useOrganizationPaymentStatus } from '@/hooks/queries/org'
@@ -36,14 +35,9 @@ import CheckoutProductInfo from './CheckoutProductInfo'
 export interface CheckoutProps {
   embed?: boolean
   theme?: 'light' | 'dark'
-  merchantAvatarVariant?: ExperimentVariant<'checkout_merchant_avatar_experiment'>
 }
 
-const Checkout = ({
-  embed: _embed,
-  theme: _theme,
-  merchantAvatarVariant = 'control',
-}: CheckoutProps) => {
+const Checkout = ({ embed: _embed, theme: _theme }: CheckoutProps) => {
   const { client } = useCheckout()
   const {
     checkout,
@@ -247,7 +241,6 @@ const Checkout = ({
             <CheckoutProductInfo
               organization={checkout.organization}
               product={checkout.product}
-              merchantAvatarVariant={merchantAvatarVariant}
             />
             <CheckoutProductSwitcher
               checkout={checkout}

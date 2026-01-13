a1c2f59714c9c3acb0fd6e04c680a0469d61d034
Pieter Beulque <pieterbeulque@gmail.com>
2026-01-14 00:23:18 +0100

    Reapply "Avoid queue pile-ups on processing customers (#8893)"
    This reverts commit 0aaa6a0c91cd8653f2103fd78c8a1dabe15ed7c9.

--- server/polar/customer_meter/service.py
+++ server/polar/customer_meter/service.py
@@ -121,6 +121,7 @@ class CustomerMeterService:
 
         customer_repository = CustomerRepository.from_session(session)
         await customer_repository.set_meters_updated_at((customer,))
+        await customer_repository.clear_meters_processing(customer, meters_dirtied_at)
 
     async def update_customer_meter(
         self,

c7940ff4d978f259067a1eb7ecb81c279220e0e3
Jesper Bränn <jesper@polar.sh>
2026-01-14 13:13:25 +0100

    events: Write to tinybird if enabled
    - On event creation (for system events)
    - On event ingestion (for user events)

--- server/polar/event/service.py
+++ server/polar/event/service.py
@@ -24,9 +24,11 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import contains_eager
 
 from polar.auth.models import AuthSubject, is_organization, is_user
+from polar.config import settings
 from polar.customer_meter.repository import CustomerMeterRepository
 from polar.event_type.repository import EventTypeRepository
 from polar.exceptions import PolarError, PolarRequestValidationError, ValidationError
+from polar.integrations.tinybird.service import ingest_events
 from polar.kit.metadata import MetadataQuery, apply_metadata_clause
 from polar.kit.pagination import PaginationParams, paginate
 from polar.kit.sorting import Sorting
@@ -637,6 +639,9 @@ class EventService:
                 )
                 await self._create_meter_events(session, inserted_events)
 
+        if settings.TINYBIRD_EVENTS_WRITE:
+            await ingest_events(list(inserted_events))
+
         with logfire.span("enqueue_events", event_count=len(event_ids)):
             enqueue_events(*event_ids)
 
@@ -649,6 +654,9 @@ class EventService:
         event = await repository.create(event, flush=True)
         # Temporarily
         await self._create_meter_events(session, [event])
+
+        if settings.TINYBIRD_EVENTS_WRITE:
+            await ingest_events([event])
 
         enqueue_events(event.id)
 

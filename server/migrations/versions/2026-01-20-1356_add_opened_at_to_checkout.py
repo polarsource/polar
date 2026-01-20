2a5f2286c0b74fd146570060560359b2625f64e9
sebastianekstrom <seb.ekstrom@gmail.com>
2026-01-20 13:57:14 +0100

    Add checkout migration for opened_at

---
+++ server/migrations/versions/2026-01-20-1356_add_opened_at_to_checkout.py
@@ -0,0 +1,30 @@
+"""add analytics_metadata to checkout
+
+Revision ID: 69ef1626e8db
+Revises: 6145195fc43a
+Create Date: 2026-01-20 13:56:05.860812
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects.postgresql import JSONB
+
+# Polar Custom Imports
+
+# revision identifiers, used by Alembic.
+revision = "69ef1626e8db"
+down_revision = "6145195fc43a"
+branch_labels: tuple[str] | None = None
+depends_on: tuple[str] | None = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "checkouts",
+        sa.Column("analytics_metadata", JSONB, nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("checkouts", "analytics_metadata")

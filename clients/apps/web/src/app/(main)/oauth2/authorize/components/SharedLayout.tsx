08479981a677b22d57969d581f8444a5802aea14
Pieter Beulque <pieterbeulque@gmail.com>
2026-01-22 20:03:56 +0100

    Revamp authorization flow a bit

---
+++ clients/apps/web/src/app/(main)/oauth2/authorize/components/SharedLayout.tsx
@@ -0,0 +1,40 @@
+import LogoType from '@/components/Brand/LogoType'
+import AddOutlined from '@mui/icons-material/AddOutlined'
+import { schemas } from '@polar-sh/client'
+
+export default function SharedLayout({
+  client,
+  introduction,
+  children,
+}: {
+  client?: schemas['AuthorizeResponseOrganization']['client']
+  introduction?: string | React.ReactNode
+  children?: React.ReactNode
+}) {
+  return (
+    <div className="dark:bg-polar-950 flex flex-col gap-12 bg-white pt-16 md:items-center md:p-16">
+      <div className="flex w-96 flex-col items-center gap-6">
+        <div className="flex flex-row items-center gap-2">
+          <LogoType className="h-10" />
+          {client?.logo_uri && (
+            <>
+              <AddOutlined className="h-5" />
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={client.logo_uri}
+                className="h-10"
+                alt={client.client_name ?? client.client_id}
+              />
+            </>
+          )}
+        </div>
+        {introduction && (
+          <div className="dark:text-polar-400 w-full text-center text-lg text-gray-600">
+            {introduction}
+          </div>
+        )}
+      </div>
+      {children && <div className="flex w-lg flex-col gap-6">{children}</div>}
+    </div>
+  )
+}

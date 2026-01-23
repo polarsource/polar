4c3c576edd9bf25503579892b553b9ee3fdec105
Pieter Beulque <pieterbeulque@gmail.com>
2026-01-23 12:04:18 +0100

    Remove unused scopes

--- clients/packages/client/src/v1.ts
+++ clients/packages/client/src/v1.ts
@@ -5891,7 +5891,6 @@ export interface components {
        *       "metrics:read": "Read metrics",
        *       "webhooks:read": "Read webhooks",
        *       "webhooks:write": "Create or modify webhooks",
-       *       "external_organizations:read": "Read external organizations",
        *       "license_keys:read": "Read license keys",
        *       "license_keys:write": "Modify license keys",
        *       "customer_portal:read": "Read your orders, subscriptions and benefits",
@@ -5977,7 +5976,6 @@ export interface components {
        *       "metrics:read": "Read metrics",
        *       "webhooks:read": "Read webhooks",
        *       "webhooks:write": "Create or modify webhooks",
-       *       "external_organizations:read": "Read external organizations",
        *       "license_keys:read": "Read license keys",
        *       "license_keys:write": "Modify license keys",
        *       "customer_portal:read": "Read your orders, subscriptions and benefits",
@@ -6065,13 +6063,8 @@ export interface components {
       | 'metrics:read'
       | 'webhooks:read'
       | 'webhooks:write'
-      | 'external_organizations:read'
       | 'license_keys:read'
       | 'license_keys:write'
-      | 'repositories:read'
-      | 'repositories:write'
-      | 'issues:read'
-      | 'issues:write'
       | 'customer_portal:read'
       | 'customer_portal:write'
       | 'notifications:read'
@@ -19680,7 +19673,7 @@ export interface components {
       response_types: 'code'[]
       /**
        * Scope
-       * @default openid profile email user:read user:write organizations:read organizations:write custom_fields:read custom_fields:write discounts:read discounts:write checkout_links:read checkout_links:write checkouts:read checkouts:write transactions:read transactions:write payouts:read payouts:write products:read products:write benefits:read benefits:write events:read events:write meters:read meters:write files:read files:write subscriptions:read subscriptions:write customers:read customers:write members:read members:write wallets:read wallets:write disputes:read customer_meters:read customer_sessions:write member_sessions:write customer_seats:read customer_seats:write orders:read orders:write refunds:read refunds:write payments:read metrics:read webhooks:read webhooks:write external_organizations:read license_keys:read license_keys:write repositories:read repositories:write issues:read issues:write customer_portal:read customer_portal:write notifications:read notifications:write notification_recipients:read notification_recipients:write organization_access_tokens:read organization_access_tokens:write
+       * @default openid profile email user:read user:write organizations:read organizations:write custom_fields:read custom_fields:write discounts:read discounts:write checkout_links:read checkout_links:write checkouts:read checkouts:write transactions:read transactions:write payouts:read payouts:write products:read products:write benefits:read benefits:write events:read events:write meters:read meters:write files:read files:write subscriptions:read subscriptions:write customers:read customers:write members:read members:write wallets:read wallets:write disputes:read customer_meters:read customer_sessions:write member_sessions:write customer_seats:read customer_seats:write orders:read orders:write refunds:read refunds:write payments:read metrics:read webhooks:read webhooks:write license_keys:read license_keys:write customer_portal:read customer_portal:write notifications:read notifications:write notification_recipients:read notification_recipients:write organization_access_tokens:read organization_access_tokens:write
        */
       scope: string
       /** Client Name */
@@ -19745,7 +19738,7 @@ export interface components {
       response_types: 'code'[]
       /**
        * Scope
-       * @default openid profile email user:read user:write organizations:read organizations:write custom_fields:read custom_fields:write discounts:read discounts:write checkout_links:read checkout_links:write checkouts:read checkouts:write transactions:read transactions:write payouts:read payouts:write products:read products:write benefits:read benefits:write events:read events:write meters:read meters:write files:read files:write subscriptions:read subscriptions:write customers:read customers:write members:read members:write wallets:read wallets:write disputes:read customer_meters:read customer_sessions:write member_sessions:write customer_seats:read customer_seats:write orders:read orders:write refunds:read refunds:write payments:read metrics:read webhooks:read webhooks:write external_organizations:read license_keys:read license_keys:write repositories:read repositories:write issues:read issues:write customer_portal:read customer_portal:write notifications:read notifications:write notification_recipients:read notification_recipients:write organization_access_tokens:read organization_access_tokens:write
+       * @default openid profile email user:read user:write organizations:read organizations:write custom_fields:read custom_fields:write discounts:read discounts:write checkout_links:read checkout_links:write checkouts:read checkouts:write transactions:read transactions:write payouts:read payouts:write products:read products:write benefits:read benefits:write events:read events:write meters:read meters:write files:read files:write subscriptions:read subscriptions:write customers:read customers:write members:read members:write wallets:read wallets:write disputes:read customer_meters:read customer_sessions:write member_sessions:write customer_seats:read customer_seats:write orders:read orders:write refunds:read refunds:write payments:read metrics:read webhooks:read webhooks:write license_keys:read license_keys:write customer_portal:read customer_portal:write notifications:read notifications:write notification_recipients:read notification_recipients:write organization_access_tokens:read organization_access_tokens:write
        */
       scope: string
       /** Client Name */
@@ -19791,7 +19784,7 @@ export interface components {
       response_types: 'code'[]
       /**
        * Scope
-       * @default openid profile email user:read user:write organizations:read organizations:write custom_fields:read custom_fields:write discounts:read discounts:write checkout_links:read checkout_links:write checkouts:read checkouts:write transactions:read transactions:write payouts:read payouts:write products:read products:write benefits:read benefits:write events:read events:write meters:read meters:write files:read files:write subscriptions:read subscriptions:write customers:read customers:write members:read members:write wallets:read wallets:write disputes:read customer_meters:read customer_sessions:write member_sessions:write customer_seats:read customer_seats:write orders:read orders:write refunds:read refunds:write payments:read metrics:read webhooks:read webhooks:write external_organizations:read license_keys:read license_keys:write repositories:read repositories:write issues:read issues:write customer_portal:read customer_portal:write notifications:read notifications:write notification_recipients:read notification_recipients:write organization_access_tokens:read organization_access_tokens:write
+       * @default openid profile email user:read user:write organizations:read organizations:write custom_fields:read custom_fields:write discounts:read discounts:write checkout_links:read checkout_links:write checkouts:read checkouts:write transactions:read transactions:write payouts:read payouts:write products:read products:write benefits:read benefits:write events:read events:write meters:read meters:write files:read files:write subscriptions:read subscriptions:write customers:read customers:write members:read members:write wallets:read wallets:write disputes:read customer_meters:read customer_sessions:write member_sessions:write customer_seats:read customer_seats:write orders:read orders:write refunds:read refunds:write payments:read metrics:read webhooks:read webhooks:write license_keys:read license_keys:write customer_portal:read customer_portal:write notifications:read notifications:write notification_recipients:read notification_recipients:write organization_access_tokens:read organization_access_tokens:write
        */
       scope: string
       /** Client Name */
@@ -23136,13 +23129,8 @@ export interface components {
       | 'metrics:read'
       | 'webhooks:read'
       | 'webhooks:write'
-      | 'external_organizations:read'
       | 'license_keys:read'
       | 'license_keys:write'
-      | 'repositories:read'
-      | 'repositories:write'
-      | 'issues:read'
-      | 'issues:write'
       | 'customer_portal:read'
       | 'customer_portal:write'
       | 'notifications:read'
@@ -42157,13 +42145,8 @@ export const availableScopeValues: ReadonlyArray<
   'metrics:read',
   'webhooks:read',
   'webhooks:write',
-  'external_organizations:read',
   'license_keys:read',
   'license_keys:write',
-  'repositories:read',
-  'repositories:write',
-  'issues:read',
-  'issues:write',
   'customer_portal:read',
   'customer_portal:write',
   'notifications:read',
@@ -43484,13 +43467,8 @@ export const scopeValues: ReadonlyArray<
   'metrics:read',
   'webhooks:read',
   'webhooks:write',
-  'external_organizations:read',
   'license_keys:read',
   'license_keys:write',
-  'repositories:read',
-  'repositories:write',
-  'issues:read',
-  'issues:write',
   'customer_portal:read',
   'customer_portal:write',
   'notifications:read',

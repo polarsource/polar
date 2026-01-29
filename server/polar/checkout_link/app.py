a02fe16fe531f053ade8a7aa4c88d10e98b9c38b
Pieter Beulque <pieterbeulque@gmail.com>
2026-01-29 10:17:36 +0100

    Clean up buy.polar.sh (#9233)
    * Remove redirects from `vercel.json`
    
    * Redirect root & non-matching paths to website
    
    * Clean up FastAPI app init

--- server/polar/checkout_link/app.py
+++ server/polar/checkout_link/app.py
@@ -1,6 +1,26 @@
-from polar.routing import APIRouter
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from polar.config import settings
+from polar.exceptions import PolarError
 
 from .endpoints import redirect
 
-router = APIRouter()
-router.get("/{client_secret}")(redirect)
+
+async def redirect_to_frontend(request: Request, exc: Exception) -> RedirectResponse:
+    """Redirect all errors to frontend base URL."""
+    return RedirectResponse(settings.FRONTEND_BASE_URL, status_code=302)
+
+
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+    exception_handlers={
+        StarletteHTTPException: redirect_to_frontend,
+        PolarError: redirect_to_frontend,
+    },
+)
+
+app.get("/{client_secret}")(redirect)

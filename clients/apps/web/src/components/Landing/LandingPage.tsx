14e8e1af264896ed08727a8f78ac156be7c9d92e
Emil Widlund <hello@emilwidlund.com>
2026-01-29 11:31:52 +0100

    testimonials

--- clients/apps/web/src/components/Landing/LandingPage.tsx
+++ clients/apps/web/src/components/Landing/LandingPage.tsx
@@ -5,15 +5,13 @@ import { MerchantOfRecord } from '@/components/Landing/MOR'
 import { Testimonials } from '@/components/Landing/Testimonials'
 import useIsMobile from '@/utils/mobile'
 import { Stream } from '@cloudflare/stream-react'
-import Avatar from '@polar-sh/ui/components/atoms/Avatar'
 import Button from '@polar-sh/ui/components/atoms/Button'
-import Image from 'next/image'
 import Link from 'next/link'
 import GetStartedButton from '../Auth/GetStartedButton'
 import { Adapters } from './Adapters'
 import { Checkout } from './Checkout'
-import { Events } from './Events'
 import Features from './Features'
+import { Logotypes } from './Logotypes'
 import { Pricing } from './Pricing'
 import { Section } from './Section'
 import { Usage } from './Usage'
@@ -52,6 +50,7 @@ export const PageContent = () => {
           </Link>
         </Hero>
         <Features />
+        <Logotypes />
         {isMobile ? null : (
           <div className="dark:border-polar-700 relative aspect-video w-full overflow-hidden rounded-xl border border-gray-200 md:rounded-3xl">
             <Stream
@@ -63,39 +62,8 @@ export const PageContent = () => {
             />
           </div>
         )}
-        <Events />
         <Adapters />
         <Usage />
-        <Link
-          href="https://x.com/rauchg/status/1909810055622672851"
-          className="flex flex-col items-center gap-y-12 text-center transition-opacity hover:opacity-80"
-          target="_blank"
-        >
-          <div className="flex flex-col items-center gap-y-2">
-            <h2 className="text-6xl">”</h2>
-            <h2 className="text-2xl leading-relaxed! md:text-4xl">
-              The speed at which Polar is executing on the financial
-              infrastructure primitives the new world needs is very impressive
-            </h2>
-          </div>
-          <div className="flex flex-col items-center gap-y-4">
-            <Avatar
-              name="Guillermo Rauch"
-              avatar_url="/assets/landing/testamonials/rauch.jpg"
-              className="h-16 w-16"
-              width={64}
-              height={64}
-              loading="lazy"
-              CustomImageComponent={Image}
-            />
-            <div className="flex flex-col">
-              <span className="">Guillermo Rauch</span>
-              <span className="dark:text-polar-500 text-gray-500">
-                CEO & Founder of Vercel
-              </span>
-            </div>
-          </div>
-        </Link>
       </Section>
       <Section className="flex flex-col gap-y-24">
         <Checkout />

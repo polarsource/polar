a2d6adb291bdda12dc56dc0c89f9d1923d3f5788
Petru Rares Sincraian <psincraian@outlook.com>
2026-02-03 17:16:33 +0100

    feat: add OAuth accounts support to Member model (#9341)

--- server/polar/models/member.py
+++ server/polar/models/member.py
@@ -1,11 +1,14 @@
+import dataclasses
 from enum import StrEnum
+from typing import Any
 from uuid import UUID
 
 from sqlalchemy import ForeignKey, String, UniqueConstraint, Uuid
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, declared_attr, mapped_column, relationship
 
 from polar.kit.db.models import RecordModel
-from polar.models.customer import Customer
+from polar.models.customer import Customer, CustomerOAuthAccount, CustomerOAuthPlatform
 
 
 class MemberRole(StrEnum):
@@ -53,6 +56,42 @@ class Member(RecordModel):
         String, nullable=False, default=MemberRole.member
     )
 
+    _oauth_accounts: Mapped[dict[str, dict[str, Any]]] = mapped_column(
+        "oauth_accounts", JSONB, nullable=False, default=dict
+    )
+
     @declared_attr
     def customer(cls) -> Mapped["Customer"]:
         return relationship("Customer", lazy="raise", back_populates="members")
+
+    def get_oauth_account(
+        self, account_id: str, platform: CustomerOAuthPlatform
+    ) -> CustomerOAuthAccount | None:
+        oauth_account_data = self._oauth_accounts.get(
+            platform.get_account_key(account_id)
+        )
+        if oauth_account_data is None:
+            return None
+
+        return CustomerOAuthAccount(**oauth_account_data)
+
+    def set_oauth_account(
+        self, oauth_account: CustomerOAuthAccount, platform: CustomerOAuthPlatform
+    ) -> None:
+        account_key = platform.get_account_key(oauth_account.account_id)
+        self._oauth_accounts = {
+            **self._oauth_accounts,
+            account_key: dataclasses.asdict(oauth_account),
+        }
+
+    def remove_oauth_account(
+        self, account_id: str, platform: CustomerOAuthPlatform
+    ) -> None:
+        account_key = platform.get_account_key(account_id)
+        self._oauth_accounts = {
+            k: v for k, v in self._oauth_accounts.items() if k != account_key
+        }
+
+    @property
+    def oauth_accounts(self) -> dict[str, Any]:
+        return self._oauth_accounts

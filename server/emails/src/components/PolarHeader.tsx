3026b5eb3aa5b5d894e299b5f2f034bae95d271e
Pieter Beulque <pieterbeulque@gmail.com>
2026-02-05 10:49:05 +0100

    Switch to black logo

--- server/emails/src/components/PolarHeader.tsx
+++ server/emails/src/components/PolarHeader.tsx
@@ -1,42 +1,18 @@
-import { Column, Img, Link, Row, Section } from '@react-email/components'
+import { Column, Img, Row, Section } from '@react-email/components'
 
 interface HeaderProps {}
 
 const Header = () => (
-  <Section className="pt-[10px]">
+  <Section className="pt-[2px]">
     <Row>
       <Column className="w-[80%]">
-        <Img
-          alt="Polar Logo"
-          height="34"
-          src="https://polar-public-assets.s3.us-east-2.amazonaws.com/emails/polar-logo-blue-combination.png"
-        />
-      </Column>
-      <Column align="right">
-        <Row align="right">
-          <Column>
-            <Link href="https://x.com/polar_sh">
-              <Img
-                alt="X"
-                className="mx-[4px]"
-                height="36"
-                src="https://polar-public-assets.s3.us-east-2.amazonaws.com/emails/x-logo.png"
-                width="36"
-              />
-            </Link>
-          </Column>
-          <Column>
-            <Link href="https://discord.gg/Pnhfz3UThd">
-              <Img
-                alt="Discord"
-                className="mx-[4px]"
-                height="36"
-                src="https://polar-public-assets.s3.us-east-2.amazonaws.com/emails/discord-logo.png"
-                width="36"
-              />
-            </Link>
-          </Column>
-        </Row>
+        <div className="relative -ml-[8px] h-[32px]">
+          <Img
+            alt="Polar Logo"
+            height="48"
+            src="https://polar-public-assets.s3.us-east-2.amazonaws.com/emails/polar-logo-black-badge.png"
+          />
+        </div>
       </Column>
     </Row>
   </Section>

b4b695253e3169c4c5324d9dec59ce649916a2c2
Pieter Beulque <pieterbeulque@gmail.com>
2026-02-05 19:26:53 +0100

    Fix button loading state spinner color

--- clients/packages/ui/src/components/atoms/Button.tsx
+++ clients/packages/ui/src/components/atoms/Button.tsx
@@ -109,7 +109,7 @@ const LoadingSpinner = (props: {
         >
           <path
             d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
-            className="text-current"
+            className="fill-current"
           ></path>
         </svg>
       </div>

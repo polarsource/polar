71faa2f64137f0271fe23ff5a6c22b4bab563280
Pieter Beulque <pieterbeulque@gmail.com>
2026-02-13 07:21:23 +0100

    Clean up index.ts

--- clients/packages/i18n/tsup.config.ts
+++ clients/packages/i18n/tsup.config.ts
@@ -2,7 +2,11 @@ import { defineConfig, Options } from 'tsup'
 
 export const options: Options[] = [
   {
-    entry: ['src/index.ts', 'src/formatters/date.ts', 'src/formatters/ordinal.ts'],
+    entry: [
+      'src/index.ts',
+      'src/formatters/date.ts',
+      'src/formatters/ordinal.ts',
+    ],
     format: ['cjs', 'esm'],
     minify: true,
     dts: true,

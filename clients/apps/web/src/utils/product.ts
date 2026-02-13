f9a9f1c8473129baa059cfa2b30daa3e625b7076
Pieter Beulque <pieterbeulque@gmail.com>
2026-02-13 16:44:04 +0100

    More cleanup

--- clients/apps/web/src/utils/product.ts
+++ clients/apps/web/src/utils/product.ts
@@ -4,7 +4,7 @@ import { notFound } from 'next/navigation'
 import { cache } from 'react'
 
 export const hasIntervals = (
-  product: schemas['ProductStorefront'] | schemas['CheckoutProduct'],
+  product: schemas['CheckoutProduct'],
 ): [boolean, boolean, boolean, boolean, boolean] => {
   const hasDayInterval = product.prices.some(
     (price) => price.type === 'recurring' && price.recurring_interval === 'day',
@@ -37,7 +37,7 @@ export const isLegacyRecurringPrice = (
 ): price is schemas['LegacyRecurringProductPrice'] => 'legacy' in price
 
 export const hasLegacyRecurringPrices = (
-  product: schemas['ProductStorefront'] | schemas['CheckoutProduct'],
+  product: schemas['CheckoutProduct'],
 ): product is schemas['Product'] & {
   prices: schemas['LegacyRecurringProductPrice'][]
 } => product.prices.some(isLegacyRecurringPrice)

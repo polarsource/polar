9a83ff9d5cf381fb0453bab53c86309f9bfb69bb
Emil Widlund <hello@emilwidlund.com>
2026-02-15 23:09:31 +0000

    polsih cover

--- clients/apps/web/src/components/Brand/CoverSection.tsx
+++ clients/apps/web/src/components/Brand/CoverSection.tsx
@@ -1,24 +1,24 @@
-import ArrowForward from '@mui/icons-material/ArrowForward'
+import ArrowDownward from '@mui/icons-material/ArrowDownward'
 import { PhyllotaxisSunflower } from './PhyllotaxisSunflower'
 
 export function CoverSection() {
   return (
     <div className="flex h-full w-full flex-col justify-between p-16">
       <div className="flex items-start justify-between">
-        <span className="text-sm font-medium">Polar Software Inc</span>
-        <span className="text-sm font-medium">2025</span>
+        <span className="font-medium">Polar Software Inc</span>
+        <span className="font-medium">{new Date().getFullYear()}</span>
       </div>
       <div className="flex flex-1 items-center justify-center">
         <PhyllotaxisSunflower size={400} />
       </div>
       <div className="flex items-end justify-between">
-        <h1 className="text-5xl tracking-tight">
+        <h1 className="text-7xl tracking-tight">
           Brand
           <br />
           Guidelines
         </h1>
-        <span className="text-sm font-medium">
-          <ArrowForward />
+        <span className="font-medium">
+          <ArrowDownward />
         </span>
       </div>
     </div>

9b105e469b34c2d578a2b7278949dfbe4b5b2c0f
Emil Widlund <hello@emilwidlund.com>
2026-02-16 22:30:15 +0100

    branding

--- clients/apps/web/src/components/Brand/MissionSection.tsx
+++ clients/apps/web/src/components/Brand/MissionSection.tsx
@@ -8,7 +8,7 @@ export function MissionSection() {
     >
       <div className="dark:bg-polar-900 flex h-full w-full flex-col items-center justify-center gap-8 bg-gray-100">
         <h2 className="w-full max-w-5xl text-center text-7xl leading-tight font-light tracking-tight text-pretty">
-          From prompt to payment in a single platform
+          From prompt to billing in a single platform
         </h2>
       </div>
     </SectionLayout>

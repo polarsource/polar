e96fc369d397319133bcc135f4eefcb99ed6b8b1
Pieter Beulque <pieterbeulque@gmail.com>
2026-02-16 13:04:25 +0100

    Remove `clsx`

--- clients/packages/ui/src/components/ui/chart.tsx
+++ clients/packages/ui/src/components/ui/chart.tsx
@@ -212,13 +212,11 @@ const ChartTooltipContent = <TValue extends ValueType, TName extends NameType>({
                       <div
                         className={cn(
                           'shrink-0 rounded-[2px] border-(--color-border) bg-(--color-bg)',
-                          {
-                            'h-2.5 w-2.5': indicator === 'dot',
-                            'w-1': indicator === 'line',
-                            'w-0 border-[1.5px] border-dashed bg-transparent':
-                              indicator === 'dashed',
-                            'my-0.5': nestLabel && indicator === 'dashed',
-                          },
+                          indicator === 'dot' && 'h-2.5 w-2.5',
+                          indicator === 'line' && 'w-1',
+                          indicator === 'dashed' &&
+                            'w-0 border-[1.5px] border-dashed bg-transparent',
+                          nestLabel && indicator === 'dashed' && 'my-0.5',
                         )}
                         style={
                           {

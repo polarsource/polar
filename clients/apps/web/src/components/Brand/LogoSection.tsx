c6fe21e82ad8866d58c7d71a8ffd2469a78aa2e4
Emil Widlund <hello@emilwidlund.com>
2026-02-17 23:12:36 +0100

    fix mission tile

--- clients/apps/web/src/components/Brand/LogoSection.tsx
+++ clients/apps/web/src/components/Brand/LogoSection.tsx
@@ -5,14 +5,14 @@ export function LogoSection() {
   return (
     <SectionLayout label="02 / Logo" className="flex-1">
       <div className="grid h-full w-full flex-1 grid-cols-2 grid-rows-2 gap-8">
-        <div className="bg-polar-900 flex h-full items-center justify-center">
+        <div className="bg-polar-900 flex aspect-square h-full items-center justify-center">
           <PolarLogotype
             logoVariant="logotype"
             logoClassName="text-white"
             size={300}
           />
         </div>
-        <div className="bg-polar-900 flex h-full items-center justify-center">
+        <div className="bg-polar-900 flex aspect-square h-full items-center justify-center">
           <PolarLogotype
             logoVariant="icon"
             logoClassName="text-white"

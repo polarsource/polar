61ba0331582bf13275a3de09ab89f6a21ec24e3f
Pieter Beulque <pieterbeulque@gmail.com>
2026-02-17 07:24:10 +0100

    Improve internal server error page

--- clients/apps/web/src/app/error.tsx
+++ clients/apps/web/src/app/error.tsx
@@ -1,29 +1,22 @@
 'use client'
 
-import Button from '@polar-sh/ui/components/atoms/Button'
+import InternalServerError from '@/components/Shared/InternalServerError'
+import * as Sentry from '@sentry/nextjs'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
+import { useEffect } from 'react'
 
 export default function Error({ error }: { error: Error }) {
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
   return (
-    <html>
-      <body className="bg-blue-100">
-        <div className="flex grow flex-col items-center justify-center space-y-4 p-16">
-          <h2 className="text-xl">Something went wrong!</h2>
-
-          <Button
-            fullWidth={false}
-            onClick={() => {
-              window.location.href = '/'
-            }}
-          >
-            <span>Go back to Polar</span>
-          </Button>
-
-          <p className="pt-24 text-gray-400"></p>
-
-          <pre className="text-sm whitespace-break-spaces text-gray-400">
-            Error digest: {'digest' in error ? <>{error.digest}</> : 'oh oh'}
-          </pre>
-        </div>
+    <html className={`antialiased ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body>
+        <InternalServerError
+          digest={'digest' in error ? (error.digest as string) : undefined}
+        />
       </body>
     </html>
   )

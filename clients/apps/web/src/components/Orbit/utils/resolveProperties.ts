9c772a54c05d3c79f80f817b8d99ee53e7b39287
Emil Widlund <hello@emilwidlund.com>
2026-02-21 16:00:57 +0100

    fix box

--- clients/apps/web/src/components/Orbit/utils/resolveProperties.ts
+++ clients/apps/web/src/components/Orbit/utils/resolveProperties.ts
@@ -1,60 +1,47 @@
-import type { CSSProperties } from 'react'
 import type { ThemeSpec, TokenProps } from './createBox'
 
 export function resolveProperties<T extends ThemeSpec>(
   theme: T,
   props: TokenProps<T>,
-): CSSProperties {
-  const resolved: CSSProperties = {}
+): string {
+  const classes: string[] = []
 
   const c = theme.colors
-  const sp = (k: keyof T['spacing']): string | number => theme.spacing[k as string]
-  const r = (k: keyof T['radii']): string | number => theme.radii[k as string]
+  const sp = (k: keyof T['spacing']) => theme.spacing[k as string | number]
+  const r = (k: keyof T['radii']) => theme.radii[k as string]
 
   // ── Colors
-  if (props.backgroundColor !== undefined) resolved.backgroundColor = c[props.backgroundColor as string]
-  if (props.color !== undefined) resolved.color = c[props.color as string]
-  if (props.borderColor !== undefined) resolved.borderColor = c[props.borderColor as string]
+  if (props.backgroundColor !== undefined) classes.push(c[props.backgroundColor as string].background)
+  if (props.color !== undefined) classes.push(c[props.color as string].text)
+  if (props.borderColor !== undefined) classes.push(c[props.borderColor as string].border)
 
   // ── Spacing
-  if (props.padding !== undefined) resolved.padding = sp(props.padding)
-  if (props.paddingX !== undefined) {
-    resolved.paddingLeft = sp(props.paddingX)
-    resolved.paddingRight = sp(props.paddingX)
-  }
-  if (props.paddingY !== undefined) {
-    resolved.paddingTop = sp(props.paddingY)
-    resolved.paddingBottom = sp(props.paddingY)
-  }
-  if (props.paddingTop !== undefined) resolved.paddingTop = sp(props.paddingTop)
-  if (props.paddingRight !== undefined) resolved.paddingRight = sp(props.paddingRight)
-  if (props.paddingBottom !== undefined) resolved.paddingBottom = sp(props.paddingBottom)
-  if (props.paddingLeft !== undefined) resolved.paddingLeft = sp(props.paddingLeft)
+  if (props.padding !== undefined) classes.push(sp(props.padding).padding)
+  if (props.paddingX !== undefined) classes.push(sp(props.paddingX).paddingX)
+  if (props.paddingY !== undefined) classes.push(sp(props.paddingY).paddingY)
+  if (props.paddingTop !== undefined) classes.push(sp(props.paddingTop).paddingTop)
+  if (props.paddingRight !== undefined) classes.push(sp(props.paddingRight).paddingRight)
+  if (props.paddingBottom !== undefined) classes.push(sp(props.paddingBottom).paddingBottom)
+  if (props.paddingLeft !== undefined) classes.push(sp(props.paddingLeft).paddingLeft)
 
-  if (props.margin !== undefined) resolved.margin = sp(props.margin)
-  if (props.marginX !== undefined) {
-    resolved.marginLeft = sp(props.marginX)
-    resolved.marginRight = sp(props.marginX)
-  }
-  if (props.marginY !== undefined) {
-    resolved.marginTop = sp(props.marginY)
-    resolved.marginBottom = sp(props.marginY)
-  }
-  if (props.marginTop !== undefined) resolved.marginTop = sp(props.marginTop)
-  if (props.marginRight !== undefined) resolved.marginRight = sp(props.marginRight)
-  if (props.marginBottom !== undefined) resolved.marginBottom = sp(props.marginBottom)
-  if (props.marginLeft !== undefined) resolved.marginLeft = sp(props.marginLeft)
+  if (props.margin !== undefined) classes.push(sp(props.margin).margin)
+  if (props.marginX !== undefined) classes.push(sp(props.marginX).marginX)
+  if (props.marginY !== undefined) classes.push(sp(props.marginY).marginY)
+  if (props.marginTop !== undefined) classes.push(sp(props.marginTop).marginTop)
+  if (props.marginRight !== undefined) classes.push(sp(props.marginRight).marginRight)
+  if (props.marginBottom !== undefined) classes.push(sp(props.marginBottom).marginBottom)
+  if (props.marginLeft !== undefined) classes.push(sp(props.marginLeft).marginLeft)
 
-  if (props.gap !== undefined) resolved.gap = sp(props.gap)
-  if (props.rowGap !== undefined) resolved.rowGap = sp(props.rowGap)
-  if (props.columnGap !== undefined) resolved.columnGap = sp(props.columnGap)
+  if (props.gap !== undefined) classes.push(sp(props.gap).gap)
+  if (props.rowGap !== undefined) classes.push(sp(props.rowGap).rowGap)
+  if (props.columnGap !== undefined) classes.push(sp(props.columnGap).columnGap)
 
   // ── Radii
-  if (props.borderRadius !== undefined) resolved.borderRadius = r(props.borderRadius)
-  if (props.borderTopLeftRadius !== undefined) resolved.borderTopLeftRadius = r(props.borderTopLeftRadius)
-  if (props.borderTopRightRadius !== undefined) resolved.borderTopRightRadius = r(props.borderTopRightRadius)
-  if (props.borderBottomLeftRadius !== undefined) resolved.borderBottomLeftRadius = r(props.borderBottomLeftRadius)
-  if (props.borderBottomRightRadius !== undefined) resolved.borderBottomRightRadius = r(props.borderBottomRightRadius)
+  if (props.borderRadius !== undefined) classes.push(r(props.borderRadius).all)
+  if (props.borderTopLeftRadius !== undefined) classes.push(r(props.borderTopLeftRadius).tl)
+  if (props.borderTopRightRadius !== undefined) classes.push(r(props.borderTopRightRadius).tr)
+  if (props.borderBottomLeftRadius !== undefined) classes.push(r(props.borderBottomLeftRadius).bl)
+  if (props.borderBottomRightRadius !== undefined) classes.push(r(props.borderBottomRightRadius).br)
 
-  return resolved
+  return classes.join(' ')
 }

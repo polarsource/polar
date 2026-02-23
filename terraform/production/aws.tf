e03d0ed93c2b93b02c3e890ebb14bd7456b51adf
Jesper Bränn <jesper@polar.sh>
2026-02-23 15:21:34 +0100

    terraform/cloudfront: Add Cloudfront infront of public S3
    - To give us a nicer looking URL
    - To be able to cache and operate on the S3 data

--- terraform/production/aws.tf
+++ terraform/production/aws.tf
@@ -118,6 +118,32 @@ module "s3_buckets" {
 }
 
 
+# =============================================================================
+# CloudFront Distribution (Public Assets)
+# =============================================================================
+
+module "cloudfront_public_assets" {
+  source = "../modules/cloudfront_distribution"
+  providers = {
+    aws           = aws
+    aws.us_east_1 = aws.us_east_1
+  }
+
+  name                           = "polar-public-assets"
+  domain                         = "assets.polar.sh"
+  cloudflare_zone_id             = "22bcd1b07ec25452aab472486bc8df94"
+  s3_bucket_id                   = module.s3_buckets.public_assets_bucket_id
+  s3_bucket_regional_domain_name = module.s3_buckets.public_assets_bucket_regional_domain_name
+  s3_bucket_arn                  = module.s3_buckets.public_assets_bucket_arn
+
+  lambda_function_associations = [
+    # {
+    #   event_type = "origin-request"
+    #   lambda_arn = module.image_resizer.qualified_arn
+    # },
+  ]
+}
+
 resource "aws_s3_bucket" "backups" {
   bucket = "polar-sh-backups"
 }

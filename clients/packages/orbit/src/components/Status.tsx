7ee76890d8fe549cfb7007e244e92483a686b8a1
Emil Widlund <hello@emilwidlund.com>
2026-02-23 09:54:53 +0100

    fix tailwind scanner

--- clients/packages/orbit/src/components/Status.tsx
+++ clients/packages/orbit/src/components/Status.tsx
@@ -5,11 +5,10 @@ const variantClasses = {
   neutral:
     'bg-neutral-100 text-neutral-500 dark:bg-polar-800 dark:text-polar-500',
   success:
-    'bg-emerald-100 text-emerald-500 dark:bg-emerald-950/50 dark:text-emerald-500',
-  warning:
-    'bg-amber-100 text-amber-500 dark:bg-amber-950/50 dark:text-amber-500',
-  error: 'bg-red-100 text-red-500 dark:bg-red-950/50 dark:text-red-500',
-  info: 'bg-blue-100 text-blue-500 dark:bg-blue-950/50 dark:text-blue-500',
+    'bg-emerald-100 text-emerald-500 dark:bg-emerald-950 dark:text-emerald-500',
+  warning: 'bg-amber-100 text-amber-500 dark:bg-amber-950 dark:text-amber-500',
+  error: 'bg-red-100 text-red-500 dark:bg-red-950 dark:text-red-500',
+  info: 'bg-blue-100 text-blue-500 dark:bg-blue-950 dark:text-blue-500',
 }
 
 const sizeClasses = {

95e7c428f6ac66eb472e4de20c3e92ae52a7c655
Emil Widlund <hello@emilwidlund.com>
2026-02-24 15:32:29 +0100

    prep markdown files

--- clients/apps/web/src/app/(main)/(website)/(landing)/(mdx)/legal/layout.tsx
+++ clients/apps/web/src/app/(main)/(website)/(landing)/(mdx)/legal/layout.tsx
@@ -10,8 +10,8 @@ export default function Layout({
   return (
     <main className="mx-auto flex w-full max-w-6xl flex-col px-2 md:px-0">
       {/* Content Card */}
-      <div className="dark:md:bg-polar-900 dark:border-polar-700 flex flex-col gap-y-8 rounded-lg border-gray-200 shadow-xs md:gap-y-12 md:border md:bg-white md:p-24 md:px-16">
-        <div className="flex flex-col gap-y-12">{children}</div>
+      <div className="dark:md:bg-polar-900 dark:border-polar-700 flex flex-col gap-y-8 rounded-lg border-gray-200 shadow-xs md:gap-y-12 md:border md:bg-white md:p-24">
+        <div className="flex flex-col text-lg">{children}</div>
       </div>
     </main>
   )

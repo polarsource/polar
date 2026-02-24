78d3690cdd84dc65e2d198069b84e97fd6755685
Emil Widlund <hello@emilwidlund.com>
2026-02-24 15:44:03 +0100

    remove experimental company pages for now

--- clients/apps/web/src/app/(main)/experimental/company/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react'
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="min-h-screen bg-white dark:bg-polar-950">{children}</div>
-  )
-}

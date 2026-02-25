9005fc240d0f690f5de6f298f51a3248d952c3a7
Jesper Bränn <jesper@polar.sh>
2026-02-25 14:45:24 +0100

    tf: Adjust bucket and domain for public files
    User uploaded

--- terraform/production/aws.tf
+++ terraform/production/aws.tf
@@ -169,8 +169,8 @@ module "cloudfront_public_assets" {
     aws.us_east_1 = aws.us_east_1
   }
 
-  name                           = "polar-public-assets"
-  domain                         = "assets.polar.sh"
+  name                           = "polar-public-files"
+  domain                         = "uploads.polar.sh"
   cloudflare_zone_id             = "22bcd1b07ec25452aab472486bc8df94"
   s3_bucket_id                   = module.s3_buckets.public_assets_bucket_id
   s3_bucket_regional_domain_name = module.s3_buckets.public_assets_bucket_regional_domain_name

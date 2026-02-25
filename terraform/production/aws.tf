0219e586e554f7781699c7ce0a3e5b14d837693a
Jesper Bränn <jesper@polar.sh>
2026-02-25 18:03:13 +0100

    ci: Give GitHub user access to GetObject
    Required for lambda deployment

--- terraform/production/aws.tf
+++ terraform/production/aws.tf
@@ -307,6 +307,8 @@ resource "aws_iam_policy" "lambda_artifacts_upload" {
       {
         Effect = "Allow"
         Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
           "s3:PutObject"
         ]
         Resource = "${aws_s3_bucket.lambda_artifacts.arn}/*"

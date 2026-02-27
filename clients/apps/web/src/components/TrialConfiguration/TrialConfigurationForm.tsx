4e7615ce6258c2f5d9c67f45353eaefe7f90fd38
Emil Widlund <hello@emilwidlund.com>
2026-02-27 23:44:46 +0100

    polish dashboard

--- clients/apps/web/src/components/TrialConfiguration/TrialConfigurationForm.tsx
+++ clients/apps/web/src/components/TrialConfiguration/TrialConfigurationForm.tsx
@@ -78,80 +78,75 @@ export const TrialConfigurationForm = ({
   return (
     <label
       className={twMerge(
-        'w-full cursor-pointer rounded-2xl border p-4 transition-colors',
-        trialEnabled
-          ? 'dark:bg-polar-800 bg-gray-50'
-          : 'dark:border-polar-700 dark:hover:border-polar-700 dark:text-polar-500 dark:hover:bg-polar-700 dark:bg-polar-900 border-gray-100 text-gray-500 hover:border-gray-200',
+        'dark:border-polar-700 dark:bg-polar-900 w-full rounded-2xl border border-gray-100 px-6 py-4',
       )}
       htmlFor="trial-enable"
     >
-      <div className="flex flex-row items-center gap-x-6">
+      <div className="flex flex-col gap-4">
         <FormItem className="flex-1">
-          <div className="flex h-10 flex-row items-center justify-start space-y-0 space-x-2 whitespace-nowrap">
+          <div className="flex h-10 flex-row items-center justify-between gap-x-4 whitespace-nowrap">
+            <FormLabel htmlFor="trial-enable">Free trial period</FormLabel>
             <Switch
               id="trial-enable"
               checked={trialEnabled}
               onCheckedChange={onTrialToggle}
             />
-            <FormLabel htmlFor="trial-enable">Free trial period</FormLabel>
           </div>
         </FormItem>
         {trialEnabled && (
-          <>
-            <div className="flex w-full flex-col gap-2 lg:flex-row">
-              <FormField
-                control={control}
-                name="trial_interval_count"
-                rules={{
-                  min: 1,
-                  max: 1000,
-                }}
-                render={({ field }) => (
-                  <FormItem className="w-full space-y-0 lg:w-1/3">
+          <div className="flex w-full flex-col gap-2 lg:flex-row">
+            <FormField
+              control={control}
+              name="trial_interval_count"
+              rules={{
+                min: 1,
+                max: 1000,
+              }}
+              render={({ field }) => (
+                <FormItem className="w-full space-y-0 lg:w-1/3">
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min={1}
+                      max={1000}
+                      step={1}
+                      {...field}
+                      value={field.value || ''}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="trial_interval"
+              render={({ field }) => {
+                return (
+                  <FormItem className="w-full space-y-0 lg:w-2/3">
                     <FormControl>
-                      <Input
-                        type="number"
-                        min={1}
-                        max={1000}
-                        step={1}
-                        {...field}
+                      <Select
+                        onValueChange={field.onChange}
                         value={field.value || ''}
-                      />
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select an interval" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {enums.trialIntervalValues.map((value) => (
+                            <SelectItem key={value} value={value}>
+                              {intervalDisplayNames[value]}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
-                )}
-              />
-              <FormField
-                control={control}
-                name="trial_interval"
-                render={({ field }) => {
-                  return (
-                    <FormItem className="w-full space-y-0 lg:w-2/3">
-                      <FormControl>
-                        <Select
-                          onValueChange={field.onChange}
-                          value={field.value || ''}
-                        >
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select an interval" />
-                          </SelectTrigger>
-                          <SelectContent>
-                            {enums.trialIntervalValues.map((value) => (
-                              <SelectItem key={value} value={value}>
-                                {intervalDisplayNames[value]}
-                              </SelectItem>
-                            ))}
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )
-                }}
-              />
-            </div>
-          </>
+                )
+              }}
+            />
+          </div>
         )}
       </div>
       {bottomText && trialEnabled && (

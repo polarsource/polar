c3121a5c4b18064443b835afcd75f1dd4cde30cf
Emil Widlund <hello@emilwidlund.com>
2026-02-27 10:02:07 +0100

    adjust font files

--- clients/apps/web/src/app/layout.tsx
+++ clients/apps/web/src/app/layout.tsx
@@ -87,7 +87,7 @@ const inter = localFont({
     { path: '../fonts/Inter-Light.woff2', weight: '300' },
     { path: '../fonts/Inter-Regular.woff2', weight: '400' },
     { path: '../fonts/Inter-Medium.woff2', weight: '500' },
-    { path: '../fonts/Inter-Semibold.woff2', weight: '600' },
+    { path: '../fonts/Inter-SemiBold.woff2', weight: '600' },
   ],
   display: 'swap',
   variable: '--sans',
@@ -96,9 +96,9 @@ const inter = localFont({
 const interDisplay = localFont({
   src: [
     { path: '../fonts/InterDisplay-Light.woff2', weight: '300' },
-    { path: '../fonts/Inter-Regular.woff2', weight: '400' },
-    { path: '../fonts/Inter-Medium.woff2', weight: '500' },
-    { path: '../fonts/Inter-Semibold.woff2', weight: '600' },
+    { path: '../fonts/InterDisplay-Regular.woff2', weight: '400' },
+    { path: '../fonts/InterDisplay-Medium.woff2', weight: '500' },
+    { path: '../fonts/InterDisplay-SemiBold.woff2', weight: '600' },
   ],
   display: 'swap',
   variable: '--display',

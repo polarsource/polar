2c734bf191b3d86eaa2e6195dc31fd04a8e60977
Emil Widlund <hello@emilwidlund.com>
2026-02-28 12:51:55 +0100

    formatter

--- clients/apps/web/src/components/Privacy/CookiePreferencesButton.tsx
+++ clients/apps/web/src/components/Privacy/CookiePreferencesButton.tsx
@@ -12,7 +12,7 @@ export const CookiePreferencesButton = () => {
       <button
         type="button"
         onClick={show}
-        className="dark:text-white dark:hover:text-polar-100 flex flex-row items-center gap-x-1 text-black transition-colors hover:text-gray-500"
+        className="dark:hover:text-polar-100 flex flex-row items-center gap-x-1 text-black transition-colors hover:text-gray-500 dark:text-white"
       >
         Cookie Preferences
       </button>

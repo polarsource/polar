77f4c884da95a2964998e08d203d608765837593
Emil Widlund <hello@emilwidlund.com>
2026-03-01 00:56:59 +0100

    run formatter

--- clients/apps/web/src/components/Widgets/WidgetContainer.tsx
+++ clients/apps/web/src/components/Widgets/WidgetContainer.tsx
@@ -14,12 +14,19 @@ export const WidgetContainer = ({
   children,
 }: PropsWithChildren<WidgetContainerProps>) => {
   return (
-    <div className={twMerge('flex max-h-96 flex-col gap-6 overflow-hidden', className)}>
+    <div
+      className={twMerge(
+        'flex max-h-96 flex-col gap-6 overflow-hidden',
+        className,
+      )}
+    >
       <div className="flex shrink-0 items-center justify-between px-6 pt-6">
         <h3 className="text-lg">{title}</h3>
         {action && <div>{action}</div>}
       </div>
-      <div className="flex flex-1 flex-col overflow-y-auto px-6">{children}</div>
+      <div className="flex flex-1 flex-col overflow-y-auto px-6">
+        {children}
+      </div>
     </div>
   )
 }

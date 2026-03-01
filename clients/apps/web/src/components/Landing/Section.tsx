34da27d11e174e61850b44e2cdf2dc643dee5c19
Emil Widlund <hello@emilwidlund.com>
2026-03-02 00:50:53 +0100

    landing

--- clients/apps/web/src/components/Landing/Section.tsx
+++ clients/apps/web/src/components/Landing/Section.tsx
@@ -17,13 +17,13 @@ export const Section = ({
     <div
       id={id}
       className={twMerge(
-        'relative flex flex-col md:items-center',
+        'dark:border-polar-700 relative flex flex-col border-b border-gray-200 md:items-center',
         wrapperClassName,
       )}
     >
       <div
         className={twMerge(
-          'flex w-full flex-col py-12 md:max-w-3xl md:px-0 md:py-16 xl:max-w-6xl',
+          'flex w-full flex-col py-12 md:max-w-3xl md:px-0 md:py-16 xl:max-w-7xl',
           className,
         )}
       >

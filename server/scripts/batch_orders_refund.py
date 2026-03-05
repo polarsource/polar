8d4e4ffd2c5ba36446437a8d19a0ea721dc3583d
François Voron <fvoron@gmail.com>
2026-03-05 11:29:55 +0100

    scripts: add batch scripts to cancel subscriptions and refund orders

---
+++ server/scripts/batch_orders_refund.py
@@ -0,0 +1,118 @@
+import asyncio
+import logging.config
+from functools import wraps
+from typing import Any
+
+import dramatiq
+import structlog
+import typer
+from pydantic import UUID4
+from rich.progress import Progress
+from sqlalchemy import func, select
+
+from polar import tasks  # noqa: F401
+from polar.kit.db.postgres import create_async_sessionmaker
+from polar.models import Customer, Order
+from polar.models.refund import RefundReason
+from polar.order.repository import OrderRepository
+from polar.organization.repository import OrganizationRepository
+from polar.postgres import create_async_engine
+from polar.redis import create_redis
+from polar.refund.schemas import RefundCreate
+from polar.refund.service import refund as refund_service
+from polar.worker import JobQueueManager
+
+cli = typer.Typer()
+
+
+def drop_all(*args: Any, **kwargs: Any) -> Any:
+    raise structlog.DropEvent
+
+
+structlog.configure(processors=[drop_all])
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": True,
+    }
+)
+
+
+def typer_async(f):  # type: ignore
+    # From https://github.com/tiangolo/typer/issues/85
+    @wraps(f)
+    def wrapper(*args, **kwargs):  # type: ignore
+        return asyncio.run(f(*args, **kwargs))
+
+    return wrapper
+
+
+@cli.command()
+@typer_async
+async def batch_orders_refund(
+    organization_id: UUID4 = typer.Argument(
+        ..., help="Organization ID to cancel subscriptions for"
+    ),
+) -> None:
+    """Refund all paid orders for the specified organization ID."""
+    engine = create_async_engine("script")
+    sessionmaker = create_async_sessionmaker(engine)
+    redis = create_redis("app")
+
+    async with JobQueueManager.open(dramatiq.get_broker(), redis) as job_queue_manager:
+        async with sessionmaker() as session:
+            organization_repository = OrganizationRepository.from_session(session)
+            organization = await organization_repository.get_by_id(organization_id)
+
+            if organization is None:
+                typer.echo(
+                    f"Error: Organization with ID {organization} not found", err=True
+                )
+                raise typer.Exit(1)
+
+            with Progress() as progress:
+                order_repository = OrderRepository.from_session(session)
+                order_statement = (
+                    select(Order)
+                    .join(Order.customer)
+                    .where(
+                        Customer.organization_id == organization_id,
+                        Order.paid.is_(True),
+                    )
+                    .options(*order_repository.get_eager_options())
+                )
+
+                total_statement = order_statement.with_only_columns(
+                    func.count()
+                ).order_by(None)
+                total_result = await session.execute(total_statement)
+                total = total_result.scalar_one()
+
+                task = progress.add_task("[cyan]Refunding orders...", total=total)
+
+                orders = await order_repository.get_all(order_statement)
+                for order in orders:
+                    if not order.refunded and order.refundable_amount > 0:
+                        await refund_service.create(
+                            session,
+                            order,
+                            RefundCreate(
+                                order_id=order.id,
+                                reason=RefundReason.other,
+                                amount=order.refundable_amount,
+                                comment=None,
+                                revoke_benefits=False,
+                            ),
+                        )
+                        # Commit and flush now since Stripe refund is now processed
+                        await session.commit()
+                        await job_queue_manager.flush(dramatiq.get_broker(), redis)
+                    progress.advance(task)
+                progress.remove_task(task)
+
+            await session.commit()
+            await job_queue_manager.flush(dramatiq.get_broker(), redis)
+
+
+if __name__ == "__main__":
+    cli()

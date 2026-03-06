b6095ddbd2e5aa82584d01314b58baad03cef0e2
Emil Widlund <hello@emilwidlund.com>
2026-03-06 11:26:18 +0100

    update billing diagram

--- clients/apps/web/src/components/Landing/Upsell.tsx
+++ clients/apps/web/src/components/Landing/Upsell.tsx
@@ -1,11 +1,14 @@
+import GetStartedButton from '../Auth/GetStartedButton'
+
 export const Upsell = () => {
   return (
-    <div className="flex flex-col items-center gap-y-8 text-center">
+    <div className="flex flex-col items-center gap-y-12 text-center">
       <h1 className="font-display text-7xl leading-tight!">
         Modern billing.
         <br />
         Built for the AI era.
       </h1>
+      <GetStartedButton />
     </div>
   )
 }

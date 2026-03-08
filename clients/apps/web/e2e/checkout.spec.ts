7373f861cd517dcb4a23d8bb7fd1fa9dca0765e7
sebastianekstrom <seb.ekstrom@gmail.com>
2026-03-08 12:27:53 +0100

    Tweak E2E

--- clients/apps/web/e2e/checkout.spec.ts
+++ clients/apps/web/e2e/checkout.spec.ts
@@ -1,20 +1,19 @@
-import { expect, test } from '@playwright/test'
-// import { CheckoutPage } from './pages/checkout.page'
+import { test } from '@playwright/test'
+import { CheckoutPage } from './pages/checkout.page'
 
 test.describe('Checkout', () => {
   const checkoutLink = process.env.E2E_CHECKOUT_LINK_SUBSCRIPTION ?? ''
 
   test('Subscription with discount', async ({ page }) => {
-    expect(1 + 1).toBe(2)
     // if (!checkoutLink) {
     //   test.skip()
     //   return
     // }
 
-    // const checkout = new CheckoutPage(page)
-    // await checkout.goto(checkoutLink)
-    // await checkout.selectCountry('Sweden')
-    // await checkout.applyDiscountCode('Free')
-    // await checkout.subscribe()
+    const checkout = new CheckoutPage(page)
+    await checkout.goto(checkoutLink)
+    await checkout.selectCountry('Sweden')
+    await checkout.applyDiscountCode('Free')
+    await checkout.subscribe()
   })
 })

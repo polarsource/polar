f09daff80dc2a56206a72b50013776f5aef2240c
Jesper Bränn <jesper@polar.sh>
2026-03-08 10:10:42 +0100

    tailscale: Add DNS forwarding

--- infra/tailscale/run-tailscale.sh
+++ infra/tailscale/run-tailscale.sh
@@ -11,4 +11,7 @@ export ALL_PROXY=socks5://localhost:1055/
 tailscale_ip=$(/render/tailscale ip)
 echo "Tailscale is up at IP ${tailscale_ip}"
 
+dnsmasq --no-daemon --listen-address=0.0.0.0 --port=53 --no-resolv --server=169.254.20.10 --server=10.208.0.10 &
+echo "DNS forwarder running on ${tailscale_ip}:53"
+
 wait ${PID}

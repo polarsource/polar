518d85be828b8c2ac9af6d57a19e6fbbc5e1e7d8
Emil Widlund <hello@emilwidlund.com>
2026-03-10 19:19:59 +0100

    sphere

--- clients/apps/web/src/components/Brand/ExperimentsSection.tsx
+++ clients/apps/web/src/components/Brand/ExperimentsSection.tsx
@@ -3,6 +3,7 @@
 import { SectionLayout } from './SectionLayout'
 import { ShaderCanvas } from './ShaderCanvas'
 import { MESH_GLSL } from './shaders/geometry/mesh'
+import { rawEffect } from './shaders/pass/raw'
 import { pixelEffect } from './shaders/pass/pixel'
 
 const meshPixel = pixelEffect({
@@ -13,15 +14,29 @@ const meshPixel = pixelEffect({
   darkColorA: '#0a0a0a',
 })
 
+const orbEffect = rawEffect()
+
 export function ExperimentsSection() {
   return (
     <SectionLayout label="Experiments">
-      <div className="w-full">
+      <div className="flex w-full flex-col gap-y-12">
+        {/* Pixelated mesh */}
         <ShaderCanvas
           geometry={MESH_GLSL}
           effect={meshPixel}
           className="aspect-[2/1] w-full"
         />
+
+        {/* Orb */}
+        <div className="flex items-center justify-center bg-white py-24">
+          <div className="h-48 w-48 overflow-hidden rounded-full shadow-2xl">
+            <ShaderCanvas
+              geometry={MESH_GLSL}
+              effect={orbEffect}
+              className="h-full w-full"
+            />
+          </div>
+        </div>
       </div>
     </SectionLayout>
   )

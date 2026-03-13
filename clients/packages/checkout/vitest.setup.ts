f39cdab545ab11d86352bd5dbd675e1f251c26e8
sebastianekstrom <seb.ekstrom@gmail.com>
2026-03-13 16:05:41 +0100

    Add TS/ESLint to checkout

--- clients/packages/checkout/vitest.setup.ts
+++ clients/packages/checkout/vitest.setup.ts
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom/vitest'
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
 import { afterEach, expect } from 'vitest'

7cb1315bb0632d3aec615907640aa628d7a53441
Emil Widlund <hello@emilwidlund.com>
2026-03-13 14:10:40 +0100

    add more images

--- clients/apps/web/src/components/Layout/Public/ArticleLayout.tsx
+++ clients/apps/web/src/components/Layout/Public/ArticleLayout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 export function ArticleLayout({ children }: PropsWithChildren) {
   return (
     <div className="dark:bg-polar-950 min-h-screen bg-white text-gray-900 dark:text-white">
-      <main className="prose-li:text-lg prose-p:text-lg mx-auto w-full max-w-2xl px-6 py-12 [&_img]:my-16 [&_img]:rounded-lg [&_img]:border-none [&_img]:shadow-none md:[&_img]:-mx-48 md:[&_img]:w-[calc(100%+24rem)] md:[&_img]:max-w-none">
+      <main className="prose-li:text-lg prose-p:text-lg mx-auto w-full max-w-2xl py-12 [&_img]:my-16 [&_img]:rounded-lg [&_img]:border-none [&_img]:shadow-none md:[&_img]:-mx-32 md:[&_img]:w-[calc(100%+16rem)] md:[&_img]:max-w-none">
         {children}
       </main>
     </div>

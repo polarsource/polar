1a6c12aca580082774795d75965da98d49a82e37
Sebastian Ekström <seb.ekstrom@gmail.com>
2026-03-19 16:55:31 +0100

    Fix remaining ESLint warnings (#10418)

--- clients/apps/web/src/components/Notifications/NotificationsPopover.tsx
+++ clients/apps/web/src/components/Notifications/NotificationsPopover.tsx
@@ -13,7 +13,7 @@ import {
   PopoverTrigger,
 } from '@polar-sh/ui/components/ui/popover'
 import Link from 'next/link'
-import { useEffect, useRef, useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import Icon from '../Icons/Icon'
 
@@ -21,7 +21,6 @@ type NotificationSchema = schemas['NotificationsList']['notifications'][number]
 
 export const NotificationsPopover = () => {
   const [show, setShow] = useState(false)
-  const [showBadge, setShowBadge] = useState(false)
 
   const notifs = useNotifications()
   const markRead = useNotificationsMarkRead()
@@ -61,7 +60,7 @@ export const NotificationsPopover = () => {
     setShow(false)
   })
 
-  useEffect(() => {
+  const showBadge = useMemo(() => {
     const haveNotifications =
       notifs.data && notifs.data.notifications.length > 0
     const noReadNotifications =
@@ -70,13 +69,11 @@ export const NotificationsPopover = () => {
       haveNotifications &&
       notifs.data.last_read_notification_id !== notifs.data.notifications[0].id
 
-    const showBadge = !!(
+    return !!(
       haveNotifications &&
       (noReadNotifications || lastNotificationIsUnread)
     )
-
-    setShowBadge(showBadge)
-  }, [notifs, notifs.data])
+  }, [notifs.data])
 
   return (
     <Popover>

d559022d514b9449a2fc9deeac0091173f5c1e5e
François Voron <fvoron@gmail.com>
2026-03-20 14:33:47 +0100

    server/order: add tests for refund in inclusive tax behavior

--- server/tests/models/test_order.py
+++ server/tests/models/test_order.py
@@ -1,5 +1,6 @@
 import pytest
 
+from polar.enums import TaxBehavior
 from polar.models import Customer, Product
 from tests.fixtures.database import SaveFixture
 from tests.fixtures.random_objects import create_order
@@ -7,7 +8,8 @@ from tests.fixtures.random_objects import create_order
 
 @pytest.mark.parametrize(
     (
-        "amount",
+        "tax_behavior",
+        "subtotal_amount",
         "tax_amount",
         "applied_balance_amount",
         "total_refund_amount",
@@ -16,28 +18,71 @@ from tests.fixtures.random_objects import create_order
     ),
     [
         pytest.param(
-            1000, 250, 1000, 1250, 1000, 250, id="refund subtotal amount with tax"
+            TaxBehavior.exclusive,
+            1000,
+            250,
+            1000,
+            1250,
+            1000,
+            250,
+            id="exclusive refund subtotal amount with tax",
         ),
         pytest.param(
+            TaxBehavior.exclusive,
             1000,
             250,
             1000,
             2250,
             2000,
             250,
-            id="refund subtotal + balance with tax",
+            id="exclusive refund subtotal + balance with tax",
         ),
         pytest.param(
-            1000, 250, -500, 750, 500, 250, id="full refund with negative balance"
+            TaxBehavior.exclusive,
+            1000,
+            250,
+            -500,
+            750,
+            500,
+            250,
+            id="exclusive full refund with negative balance",
         ),
         pytest.param(
-            1000, 250, 0, 1300, 1040, 260, id="refund exceeding original amount"
+            TaxBehavior.exclusive,
+            1000,
+            250,
+            0,
+            1300,
+            1040,
+            260,
+            id="exclusive refund exceeding original amount",
+        ),
+        pytest.param(
+            TaxBehavior.inclusive,
+            1000,
+            200,
+            0,
+            1000,
+            800,
+            200,
+            id="inclusive full amount",
+        ),
+        pytest.param(
+            TaxBehavior.inclusive,
+            1000,
+            200,
+            0,
+            500,
+            400,
+            100,
+            id="inclusive half amount",
         ),
     ],
 )
 @pytest.mark.asyncio
 async def test_calculate_refunded_tax_from_total(
-    amount: int,
+    tax_behavior: TaxBehavior,
+    subtotal_amount: int,
     tax_amount: int,
     applied_balance_amount: int,
     total_refund_amount: int,
@@ -51,12 +96,16 @@ async def test_calculate_refunded_tax_from_total(
         save_fixture,
         product=product,
         customer=customer,
-        subtotal_amount=amount,
+        subtotal_amount=subtotal_amount,
         tax_amount=tax_amount,
         applied_balance_amount=applied_balance_amount,
+        tax_behavior=tax_behavior,
     )
     assert order.refunded_amount == 0
     assert order.refunded_tax_amount == 0
+    assert order.net_amount == subtotal_amount - (
+        tax_amount if tax_behavior == TaxBehavior.inclusive else 0
+    )
 
     refund_amount, refund_tax_amount = order.calculate_refunded_tax_from_total(
         total_refund_amount
@@ -68,21 +117,47 @@ async def test_calculate_refunded_tax_from_total(
 
 @pytest.mark.parametrize(
     (
-        "amount",
+        "tax_behavior",
+        "subtotal_amount",
         "tax_amount",
         "applied_balance_amount",
         "refund_amount",
         "expected_refund_tax_amount",
     ),
     [
-        pytest.param(1000, 250, 0, 1000, 250, id="full amount"),
-        pytest.param(1000, 250, 1000, 2000, 250, id="full amount with applied balance"),
-        pytest.param(1000, 250, 1000, 500, 125, id="sub amount with applied balance"),
+        pytest.param(
+            TaxBehavior.exclusive, 1000, 250, 0, 1000, 250, id="exclusive full amount"
+        ),
+        pytest.param(
+            TaxBehavior.exclusive,
+            1000,
+            250,
+            1000,
+            2000,
+            250,
+            id="exclusive full amount with applied balance",
+        ),
+        pytest.param(
+            TaxBehavior.exclusive,
+            1000,
+            250,
+            1000,
+            500,
+            125,
+            id="exclusive sub amount with applied balance",
+        ),
+        pytest.param(
+            TaxBehavior.inclusive, 1000, 200, 0, 800, 200, id="inclusive full amount"
+        ),
+        pytest.param(
+            TaxBehavior.inclusive, 1000, 200, 0, 400, 100, id="inclusive half amount"
+        ),
     ],
 )
 @pytest.mark.asyncio
 async def test_calculate_refunded_tax_from_subtotal(
-    amount: int,
+    tax_behavior: TaxBehavior,
+    subtotal_amount: int,
     tax_amount: int,
     applied_balance_amount: int,
     refund_amount: int,
@@ -95,9 +170,10 @@ async def test_calculate_refunded_tax_from_subtotal(
         save_fixture,
         product=product,
         customer=customer,
-        subtotal_amount=amount,
+        subtotal_amount=subtotal_amount,
         tax_amount=tax_amount,
         applied_balance_amount=applied_balance_amount,
+        tax_behavior=tax_behavior,
     )
     assert order.refunded_amount == 0
     assert order.refunded_tax_amount == 0

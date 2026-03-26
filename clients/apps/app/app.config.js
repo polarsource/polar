03f2604dbc3d906a0e1dc84a04932cd46a7df418
Sebastian Ekström <seb.ekstrom@gmail.com>
2026-03-26 16:13:20 +0100

    Bump app version to 1.3.2 (#10611)

--- clients/apps/app/app.config.js
+++ clients/apps/app/app.config.js
@@ -44,7 +44,7 @@ module.exports = {
   expo: {
     name: 'Polar',
     slug: 'Polar',
-    version: '1.3.1',
+    version: '1.3.2',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'polar',

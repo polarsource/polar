aa8ad59465774ec9cdb4dd2bb5961f4ca7f2c1df
Jesper Bränn <jesper@polar.sh>
2026-03-27 13:01:10 +0100

    polar: Add Polar integration
    This is the first step in implementing and using Polar within the Polar application,
    for us to be able to track customers connected to a Polar organization.

---
+++ server/polar/integrations/polar/tasks.py
@@ -0,0 +1,49 @@
+from polar.worker import TaskPriority, actor
+
+from .client import client as polar_self_client
+
+
+@actor(actor_name="polar_self.create_customer", priority=TaskPriority.LOW)
+async def create_customer(
+    external_id: str, email: str, name: str, organization_id: str
+) -> None:
+    await polar_self_client.create_customer(
+        external_id=external_id,
+        email=email,
+        name=name,
+        organization_id=organization_id,
+    )
+
+
+@actor(actor_name="polar_self.create_free_subscription", priority=TaskPriority.LOW)
+async def create_free_subscription(external_customer_id: str, product_id: str) -> None:
+    await polar_self_client.create_free_subscription(
+        external_customer_id=external_customer_id,
+        product_id=product_id,
+    )
+
+
+@actor(actor_name="polar_self.add_member", priority=TaskPriority.LOW)
+async def add_member(customer_id: str, email: str, name: str, external_id: str) -> None:
+    await polar_self_client.add_member(
+        customer_id=customer_id,
+        email=email,
+        name=name,
+        external_id=external_id,
+    )
+
+
+@actor(actor_name="polar_self.remove_member", priority=TaskPriority.LOW)
+async def remove_member(member_id: str) -> None:
+    await polar_self_client.remove_member(member_id=member_id)
+
+
+@actor(actor_name="polar_self.track_event_ingestion", priority=TaskPriority.LOW)
+async def track_event_ingestion(
+    external_customer_id: str, count: int, organization_id: str
+) -> None:
+    await polar_self_client.track_event_ingestion(
+        external_customer_id=external_customer_id,
+        count=count,
+        organization_id=organization_id,
+    )

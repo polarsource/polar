76ef169d54a3f3b93a9e27d75600e8cb9a772d0a
Jesper Bränn <jesper@polar.sh>
2026-04-01 08:57:20 +0200

    polar: Make Polar client initialization lazy

--- server/polar/integrations/polar/tasks.py
+++ server/polar/integrations/polar/tasks.py
@@ -1,13 +1,13 @@
 from polar.worker import TaskPriority, actor
 
-from .client import client as polar_self_client
+from .client import get_client
 
 
 @actor(actor_name="polar_self.create_customer", priority=TaskPriority.LOW)
 async def create_customer(
     external_id: str, email: str, name: str, organization_id: str
 ) -> None:
-    await polar_self_client.create_customer(
+    await get_client().create_customer(
         external_id=external_id,
         email=email,
         name=name,
@@ -17,7 +17,7 @@ async def create_customer(
 
 @actor(actor_name="polar_self.create_free_subscription", priority=TaskPriority.LOW)
 async def create_free_subscription(external_customer_id: str, product_id: str) -> None:
-    await polar_self_client.create_free_subscription(
+    await get_client().create_free_subscription(
         external_customer_id=external_customer_id,
         product_id=product_id,
     )
@@ -25,7 +25,7 @@ async def create_free_subscription(external_customer_id: str, product_id: str) -
 
 @actor(actor_name="polar_self.add_member", priority=TaskPriority.LOW)
 async def add_member(customer_id: str, email: str, name: str, external_id: str) -> None:
-    await polar_self_client.add_member(
+    await get_client().add_member(
         customer_id=customer_id,
         email=email,
         name=name,
@@ -35,14 +35,14 @@ async def add_member(customer_id: str, email: str, name: str, external_id: str) 
 
 @actor(actor_name="polar_self.remove_member", priority=TaskPriority.LOW)
 async def remove_member(member_id: str) -> None:
-    await polar_self_client.remove_member(member_id=member_id)
+    await get_client().remove_member(member_id=member_id)
 
 
 @actor(actor_name="polar_self.track_event_ingestion", priority=TaskPriority.LOW)
 async def track_event_ingestion(
     external_customer_id: str, count: int, organization_id: str
 ) -> None:
-    await polar_self_client.track_event_ingestion(
+    await get_client().track_event_ingestion(
         external_customer_id=external_customer_id,
         count=count,
         organization_id=organization_id,

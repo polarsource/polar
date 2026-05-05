331cfab07cb77af7c31fb6e2f3c0c76049bad7f8
Emil Widlund <hello@emilwidlund.com>
2026-05-05 14:11:19 +0200

    format

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/settings/billing/BillingPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/settings/billing/BillingPage.tsx
@@ -1,23 +1,30 @@
-"use client";
+'use client'
 
-import { DashboardBody } from "@/components/Layout/DashboardLayout";
-import { BillingOrdersTable } from "@/components/Settings/Billing/BillingOrdersTable";
-import { BillingSubscriptionCard } from "@/components/Settings/Billing/BillingSubscriptionCard";
-import { MOCK_ORDERS, getPlanById } from "@/components/Settings/Billing/mockData";
-import { useBillingSubscription } from "@/components/Settings/Billing/useBillingStore";
-import { Section, SectionDescription } from "@/components/Settings/Section";
-import { schemas } from "@polar-sh/client";
-import { Box } from "@polar-sh/orbit/Box";
-import { useRouter } from "next/navigation";
+import { DashboardBody } from '@/components/Layout/DashboardLayout'
+import { BillingOrdersTable } from '@/components/Settings/Billing/BillingOrdersTable'
+import { BillingSubscriptionCard } from '@/components/Settings/Billing/BillingSubscriptionCard'
+import {
+  MOCK_ORDERS,
+  getPlanById,
+} from '@/components/Settings/Billing/mockData'
+import { useBillingSubscription } from '@/components/Settings/Billing/useBillingStore'
+import { Section, SectionDescription } from '@/components/Settings/Section'
+import { schemas } from '@polar-sh/client'
+import { Box } from '@polar-sh/orbit/Box'
+import { useRouter } from 'next/navigation'
 
-export default function BillingPage({ organization }: { organization: schemas["Organization"] }) {
-  const router = useRouter();
-  const subscription = useBillingSubscription();
-  const plan = getPlanById(subscription.planId);
+export default function BillingPage({
+  organization,
+}: {
+  organization: schemas['Organization']
+}) {
+  const router = useRouter()
+  const subscription = useBillingSubscription()
+  const plan = getPlanById(subscription.planId)
 
   const onChangePlan = () => {
-    router.push(`/dashboard/${organization.slug}/settings/billing/change-plan`);
-  };
+    router.push(`/dashboard/${organization.slug}/settings/billing/change-plan`)
+  }
 
   return (
     <DashboardBody wrapperClassName="max-w-(--breakpoint-md)!" title="Billing">
@@ -39,5 +46,5 @@ export default function BillingPage({ organization }: { organization: schemas["O
         </Section>
       </Box>
     </DashboardBody>
-  );
+  )
 }

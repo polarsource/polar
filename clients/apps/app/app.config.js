14500b2e5ce4d74fd3160c1e7914578a8926ebd0
sebastianekstrom <seb.ekstrom@gmail.com>
2026-05-06 14:31:37 +0200

    Bump app version to 1.4.0

--- clients/apps/app/app.config.js
+++ clients/apps/app/app.config.js
@@ -44,7 +44,7 @@ module.exports = {
   expo: {
     name: 'Polar',
     slug: 'Polar',
-    version: '1.3.4',
+    version: '1.4.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'polar',

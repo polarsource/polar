b3fa70374d8cc2a906d60de631da640510c96444
Pieter Beulque <pieterbeulque@gmail.com>
2026-05-18 14:50:20 +0200

    Add `denied` banner to avoid silently denying users (#11698)

---
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/(home)/OrganizationStatusBanner.tsx
@@ -0,0 +1,31 @@
+import { useOrganizationPaymentStatus } from '@/hooks/queries'
+import { CONFIG } from '@/utils/config'
+import { schemas } from '@polar-sh/client'
+import { DeniedBanner } from './DeniedBanner'
+import { TestModeBanner } from './TestModeBanner'
+
+interface OrganizationStatusBannerProps {
+  organization: schemas['Organization']
+}
+
+export const OrganizationStatusBanner = ({
+  organization,
+}: OrganizationStatusBannerProps) => {
+  const { data: paymentStatus, isLoading } = useOrganizationPaymentStatus(
+    organization.id,
+  )
+
+  if (isLoading || CONFIG.IS_SANDBOX) {
+    return null
+  }
+
+  if (paymentStatus?.organization_status === 'denied') {
+    return <DeniedBanner organization={organization} />
+  }
+
+  if (paymentStatus?.organization_status === 'created') {
+    return <TestModeBanner organization={organization} />
+  }
+
+  return null
+}

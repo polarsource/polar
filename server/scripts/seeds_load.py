9e3b48e58a36145c311f53da0d3f5121af7edb8f
Pieter Beulque <pieterbeulque@gmail.com>
2026-05-19 12:28:56 +0200

    Register extra webhook events in seed

--- server/scripts/seeds_load.py
+++ server/scripts/seeds_load.py
@@ -2147,6 +2147,8 @@ WEBHOOK_EVENTS: list[WebhookEventType] = [
     WebhookEventType.benefit_grant_created,
     WebhookEventType.benefit_grant_updated,
     WebhookEventType.benefit_grant_revoked,
+    WebhookEventType.subscription_revoked,
+    WebhookEventType.order_created,
 ]
 SERVER_ENV_PATH = Path(__file__).resolve().parent.parent / ".env"
 WEBHOOK_SECRET_ENV_KEY = "POLAR_POLAR_WEBHOOK_SECRET"

b878a3f256579a95bb9c5f1a1edbd7168c004281
Joel Bonander <joel@polar.sh>
2026-05-22 09:17:04 +0200

    Manually modify migration which partly ran in production and fully elsewhere

--- server/migrations/versions/2026-05-20-1505_drop_accounts_admin_id.py
+++ server/migrations/versions/2026-05-20-1505_drop_accounts_admin_id.py
@@ -19,8 +19,8 @@ depends_on: tuple[str] | None = None
 
 
 def upgrade() -> None:
-    op.execute("SET LOCAL lock_timeout = '2s'")
-    op.drop_constraint(op.f("accounts_admin_id_fkey"), "accounts", type_="foreignkey")
+    op.execute("SET LOCAL lock_timeout = '10s'")
+    # op.drop_constraint(op.f("accounts_admin_id_fkey"), "accounts", type_="foreignkey")
     op.drop_column("accounts", "admin_id")
 
 

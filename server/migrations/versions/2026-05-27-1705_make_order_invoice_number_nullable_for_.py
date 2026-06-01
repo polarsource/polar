f6567a2a8eea7e9aef0b65e1ced22d8aa47ed89c
Joel Bonander <joel@polar.sh>
2026-06-01 16:41:32 +0200

    orders: re-chain invoice_number migration onto latest main

--- server/migrations/versions/2026-05-27-1705_make_order_invoice_number_nullable_for_.py
+++ server/migrations/versions/2026-05-27-1705_make_order_invoice_number_nullable_for_.py
@@ -1,7 +1,7 @@
 """Make order.invoice_number nullable for draft orders
 
 Revision ID: d2a49dc19a62
-Revises: 56294184ba8f
+Revises: 64d92ea66fa5
 Create Date: 2026-05-27 17:05:07.479671
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "d2a49dc19a62"
-down_revision = "56294184ba8f"
+down_revision = "64d92ea66fa5"
 branch_labels: tuple[str] | None = None
 depends_on: tuple[str] | None = None
 

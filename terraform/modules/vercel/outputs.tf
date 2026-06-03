cca816b2fb8d1fd82d3b58f2f3ad92915ecc9fad
Jesper Bränn <jesper@polar.sh>
2026-06-03 12:59:18 +0200

    tf/vercel: Map up Vercel frontends in terraform
    This makes it easier to make changes in Vercel the same way we make
    changes in other parts of our infrastructure.
    
    As well as spinning up/down the test environment

---
+++ terraform/modules/vercel/outputs.tf
@@ -0,0 +1,14 @@
+output "project_id" {
+  description = "The ID of the Vercel project"
+  value       = vercel_project.this.id
+}
+
+output "project_name" {
+  description = "The name of the Vercel project"
+  value       = vercel_project.this.name
+}
+
+output "domains" {
+  description = "Custom domains attached to the project"
+  value       = [for domain in vercel_project_domain.this : domain.domain]
+}
---
+++ terraform/modules/vercel/terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 5.3"
+    }
+  }
+
+  required_version = ">= 1.2"
+}

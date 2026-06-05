0a9ea464c28c7c0b2aec8f25d0da1d03d3c4f52a
Emil Widlund <hello@emilwidlund.com>
2026-06-05 12:14:15 +0200

    remove detail tooltip (#12203)
    * remove detail tooltip
    
    * remove unused imports

--- clients/apps/web/src/components/Shared/DetailRow.tsx
+++ clients/apps/web/src/components/Shared/DetailRow.tsx
@@ -1,8 +1,3 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@polar-sh/ui/components/ui/tooltip'
 import { twMerge } from 'tailwind-merge'
 
 export interface DetailRowProps {
@@ -21,30 +16,25 @@ export const DetailRow = ({
   action,
 }: DetailRowProps) => {
   return (
-    <Tooltip>
-      <div className="flex flex-col text-sm md:flex-row md:items-baseline md:justify-between md:gap-4">
-        <TooltipTrigger asChild>
-          <h3
-            className={twMerge(
-              'dark:text-polar-500 flex-1 text-gray-500 truncate',
-              labelClassName,
-            )}
-          >
-            {label}
-          </h3>
-        </TooltipTrigger>
-        <TooltipContent>{label}</TooltipContent>
-        <span
-          className={twMerge(
-            'dark:md:hover:bg-polar-800 group flex flex-1 flex-row flex-nowrap items-center justify-between gap-x-2 rounded-md transition-colors duration-75 md:px-2.5 md:py-1 hover:md:bg-gray-100',
-            value ? '' : 'dark:text-polar-500 text-gray-500',
-            valueClassName,
-          )}
-        >
-          {value ?? '—'}
-          <span className="opacity-0 group-hover:opacity-100">{action}</span>
-        </span>
-      </div>
-    </Tooltip>
+    <div className="flex flex-col text-sm md:flex-row md:items-baseline md:justify-between md:gap-4">
+      <h3
+        className={twMerge(
+          'dark:text-polar-500 flex-1 text-gray-500 truncate',
+          labelClassName,
+        )}
+      >
+        {label}
+      </h3>
+      <span
+        className={twMerge(
+          'dark:md:hover:bg-polar-800 group flex flex-1 flex-row flex-nowrap items-center justify-between gap-x-2 rounded-md transition-colors duration-75 md:px-2.5 md:py-1 hover:md:bg-gray-100',
+          value ? '' : 'dark:text-polar-500 text-gray-500',
+          valueClassName,
+        )}
+      >
+        {value ?? '—'}
+        <span className="opacity-0 group-hover:opacity-100">{action}</span>
+      </span>
+    </div>
   )
 }

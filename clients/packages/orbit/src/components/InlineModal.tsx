b277556cd3badba99e00126ce7d6553250ff2841
Emil Widlund <hello@emilwidlund.com>
2026-06-12 12:00:25 +0200

    fix imports

--- clients/packages/orbit/src/components/InlineModal.tsx
+++ clients/packages/orbit/src/components/InlineModal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { AnimatePresence, motion } from 'motion/react'
 import React, {
   FunctionComponent,
--- clients/packages/orbit/src/components/Modal.tsx
+++ clients/packages/orbit/src/components/Modal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { motion } from 'motion/react'
 import React, {
   FunctionComponent,

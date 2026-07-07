aaf41213fb55c3798821eb79e33e629f8be422a7
Jesper Bränn <jesper@polar.sh>
2026-07-07 12:13:35 +0200

    tf: Setup Security AWS account to be allowed to TF apply

--- terraform/organization/security_account.tf
+++ terraform/organization/security_account.tf
@@ -1,5 +1,12 @@
+locals {
+  security_account = {
+    name = "security"
+    id   = "297245370456"
+  }
+}
+
 resource "aws_organizations_account" "security" {
-  name              = "security"
+  name              = local.security_account.name
   email             = var.security_account_email
   parent_id         = aws_organizations_organizational_unit.security.id
   close_on_deletion = false

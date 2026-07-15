653240834bd78cebf990daedd53c05ae6684e73d
François Voron <fvoron@gmail.com>
2026-07-15 10:24:09 +0200

    sdk[release]: 1.0.0-alpha.8 (#13171)

--- sdk/python/polar/__init__.py
+++ sdk/python/polar/__init__.py
@@ -1,6 +1,6 @@
 from polar.base import PolarClientError, PolarError, PolarNetworkError, PolarServerError
 
-__version__ = "1.0.0a7"
+__version__ = "1.0.0a8"
 __all__ = [
     "PolarError",
     "PolarNetworkError",

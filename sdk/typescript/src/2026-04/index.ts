fc9c538844e147b87577350aca4ca8eaa26936f3
François Voron <fvoron@gmail.com>
2026-07-16 10:14:40 +0200

    sdk/typescript: export individual functions (#13201)
    * sdk/typescript: export individual functions
    
    * sdk/typescript: align README examples

--- sdk/typescript/src/2026-04/index.ts
+++ sdk/typescript/src/2026-04/index.ts
@@ -1,4 +1,4 @@
-export { createPolar } from "./client";
-export type { Environment, Polar, PolarOptions } from "./client";
+export { createPolar, createPolarCore } from "./client";
+export type { Environment, Polar, PolarCore, PolarOptions } from "./client";
 export * as models from "./models";
 export * as errors from "./errors";

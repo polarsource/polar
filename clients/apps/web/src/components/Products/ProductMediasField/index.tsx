7ab2b095a0de88374cb4b484179e9ed629a92f0e
Villiam Strandh <villiam.strandh@outlook.com>
2026-07-16 09:11:53 +0200

    clean up product form on mobile (#13191)
    * fix new product page padding on mobile and adjust benefitrow
    
    * fix product checkout page padding
    
    * fix product metadata layout on mobile
    
    * fix trunacting issues

--- clients/apps/web/src/components/Products/ProductMediasField/index.tsx
+++ clients/apps/web/src/components/Products/ProductMediasField/index.tsx
@@ -82,7 +82,7 @@ const ProductMediasField = ({
   })
   return (
     <>
-      <div className="grid grid-cols-2 gap-3 [&>div>*]:aspect-video">
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 [&>div>*]:aspect-video">
         <FileList files={files} setFiles={setFiles} removeFile={removeFile} />
         <div {...getRootProps()}>
           <DropzoneView isDragActive={isDragActive}>

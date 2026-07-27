1c79cf40bebb54ed0f13689469c060694a94f454
François Voron <francois@polar.sh>
2026-07-27 09:55:43 +0200

    sdk[release]: 1.0.0-alpha.16 (#13374)

--- sdk/python/polar/v2026_04/literals.py
+++ sdk/python/polar/v2026_04/literals.py
@@ -786,6 +786,7 @@ PaymentTrigger: typing.TypeAlias = typing.Literal[
 Permission: typing.TypeAlias = typing.Literal[
     "pull", "triage", "push", "maintain", "admin"
 ]
+"""The permission level to grant. Read more about roles and their permissions on [GitHub documentation](https://docs.github.com/en/organizations/managing-user-access-to-your-organizations-repositories/managing-repository-roles/repository-roles-for-an-organization#permissions-for-each-role)."""
 PresentmentCurrency: typing.TypeAlias = typing.Literal[
     "aed",
     "all",

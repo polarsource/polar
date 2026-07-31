a75e033e1061993345679b060431e09ce066dcaf
Maxime BV <maxime@polar.sh>
2026-07-31 11:55:59 +0200

    feat(organization): enforce dispute_settings not null

--- server/polar/models/organization.py
+++ server/polar/models/organization.py
@@ -674,8 +674,8 @@ class Organization(RateLimitGroupMixin, RecordModel):
         JSONB, nullable=False, default=_default_checkout_settings
     )
 
-    dispute_settings: Mapped[OrganizationDisputeSettings | None] = mapped_column(
-        JSONB, nullable=True, default=_default_dispute_settings
+    dispute_settings: Mapped[OrganizationDisputeSettings] = mapped_column(
+        JSONB, nullable=False, default=_default_dispute_settings
     )
 
     embed_hosts: Mapped[list[str]] = mapped_column(JSONB, nullable=False, default=list)
@@ -903,6 +903,14 @@ class Organization(RateLimitGroupMixin, RecordModel):
     @property
     def checkout_require_3ds(self) -> bool:
         return self.checkout_settings.get("require_3ds", False)
+
+    @property
+    def dispute_auto_accept_below_amount(self) -> int | None:
+        return self.dispute_settings["auto_accept_below_amount"]
+
+    @property
+    def dispute_auto_accept_currency(self) -> str | None:
+        return self.dispute_settings["auto_accept_currency"]
 
     @declared_attr
     def all_products(cls) -> Mapped[list["Product"]]:

e056dd8fed94e38ad84ae20466ee243d792aaab3
Villiam Strandh <villiam.strandh@outlook.com>
2026-08-03 15:24:01 +0200

    refactor(dashboard): use nuqs for checkouts table filters (#13499)
    * fix amount column size
    
    * tabular dates in tables
    
    * use nuqs for table filters
    
    * fix date column sizing issue

--- clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/SalesPage.tsx
+++ clients/apps/web/src/app/(main)/dashboard/[organization]/(header)/sales/SalesPage.tsx
@@ -94,7 +94,7 @@ const ClientPage: React.FC<ClientPageProps> = ({ organization }) => {
     {
       accessorKey: 'created_at',
       enableSorting: true,
-      size: 120,
+      size: 160,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Date" />
       ),

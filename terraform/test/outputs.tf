deb2a52686d916f794e2b512ea9c9c0e87e58455
Jesper Bränn <jesper@polar.sh>
2026-08-11 15:50:24 +0200

    tf/redis: Use AWS Redis in test and sandbox

--- terraform/test/outputs.tf
+++ terraform/test/outputs.tf
@@ -2,3 +2,8 @@ output "lambda_worker_ecr_repository_url" {
   description = "ECR repository URL for the test Lambda worker image."
   value       = try(module.lambda_worker_ecr[0].repository_url, null)
 }
+
+output "redis_endpoint_service_name" {
+  description = "VPC endpoint service name for the worker Redis. Provide to Render when creating the private link."
+  value       = try(module.redis_private_link[0].service_name, null)
+}

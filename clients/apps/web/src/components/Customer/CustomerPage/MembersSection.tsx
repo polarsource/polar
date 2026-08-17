ad277be56d59b2b6007dfa423742067f842feac4
Ábris Simon <abris96@gmail.com>
2026-08-17 07:43:07 +0000

    use Truncated component (#13778)

--- clients/apps/web/src/components/Customer/CustomerPage/MembersSection.tsx
+++ clients/apps/web/src/components/Customer/CustomerPage/MembersSection.tsx
@@ -14,6 +14,7 @@ import {
   Status,
   type StatusColor,
   Text,
+  Truncated,
 } from '@polar-sh/orbit'
 import { Box } from '@polar-sh/orbit/Box'
 import FormattedDateTime from '@polar-sh/ui/components/atoms/FormattedDateTime'
@@ -176,7 +177,9 @@ export const MembersSection = ({
             header: 'External ID',
             accessorKey: 'external_id',
             cell: ({ row: { original } }) => (
-              <Text>{original.external_id ?? '—'}</Text>
+              <Truncated>
+                <Text>{original.external_id ?? '—'}</Text>
+              </Truncated>
             ),
           },
           {

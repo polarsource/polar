659b85bf6dc895f91c318d9001f0d91a4c58bdad
François Voron <francois@polar.sh>
2026-08-18 16:10:14 +0000

    tf: disable preview deployments on Vercel (#13824)

--- terraform/modules/vercel/main.tf
+++ terraform/modules/vercel/main.tf
@@ -14,11 +14,12 @@ resource "vercel_project" "this" {
   install_command = var.install_command
   ignore_command  = var.ignore_command
 
-  build_machine_type         = var.build_machine_type
-  resource_config            = var.resource_config
-  enable_preview_feedback    = var.enable_preview_feedback
-  enable_production_feedback = var.enable_production_feedback
-  git_provider_options       = var.git_provider_options
+  build_machine_type           = var.build_machine_type
+  resource_config              = var.resource_config
+  preview_deployments_disabled = var.preview_deployments_disabled
+  enable_preview_feedback      = var.enable_preview_feedback
+  enable_production_feedback   = var.enable_production_feedback
+  git_provider_options         = var.git_provider_options
 
   automatically_expose_system_environment_variables = true
 

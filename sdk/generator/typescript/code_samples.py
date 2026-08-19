bd27f3c17860dafed0141f842458e3700536f52e
François Voron <me@fvoron.com>
2026-08-19 13:33:24 +0200

    docs: fix code samples language property

--- sdk/generator/typescript/code_samples.py
+++ sdk/generator/typescript/code_samples.py
@@ -20,7 +20,7 @@ TEMPLATE = ENVIRONMENT.get_template("code_sample.ts.jinja")
 
 
 def language_label() -> str:
-    return "TypeScript (New SDK)"
+    return "typescript"
 
 
 def render_code_sample(sample: OperationSample) -> str:

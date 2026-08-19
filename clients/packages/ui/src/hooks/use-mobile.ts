e7ccb11ca4ec2dd044d3077961d978ad394c48c6
Abris Simon <abris96@gmail.com>
2026-08-19 08:11:05 +0200

    update to oxlint with react compiler

--- clients/packages/ui/src/hooks/use-mobile.ts
+++ clients/packages/ui/src/hooks/use-mobile.ts
@@ -2,18 +2,17 @@ import * as React from 'react'
 
 const MOBILE_BREAKPOINT = 768
 
+const mobileQuery = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+
+const subscribe = (onChange: () => void) => {
+  const mediaQuery = window.matchMedia(mobileQuery)
+  mediaQuery.addEventListener('change', onChange)
+  return () => mediaQuery.removeEventListener('change', onChange)
+}
+
+const getSnapshot = () => window.matchMedia(mobileQuery).matches
+const getServerSnapshot = () => false
+
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener('change', onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener('change', onChange)
-  }, [])
-
-  return !!isMobile
+  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
 }

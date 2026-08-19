c3181d9e4aa74a1b89c7506878af4c4c37cfbc2c
François Voron <francois@polar.sh>
2026-08-19 11:41:58 +0000

    docs: fix code samples language property (#13863)

--- sdk/generator/python/code_samples.py
+++ sdk/generator/python/code_samples.py
@@ -20,7 +20,7 @@ TEMPLATE = ENVIRONMENT.get_template("code_sample.py.jinja")
 
 
 def language_label() -> str:
-    return "Python (New SDK)"
+    return "python"
 
 
 def render_code_sample(sample: OperationSample) -> str:

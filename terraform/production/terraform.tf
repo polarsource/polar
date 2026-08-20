a5bdbd80ab7bab4a5323db5ba5c64cd0bc896913
Jesper Bränn <jesper@polar.sh>
2026-08-20 09:00:51 +0000

    tf: Upgrade providers (#13899)

--- terraform/production/terraform.tf
+++ terraform/production/terraform.tf
@@ -9,12 +9,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92"
+      version = "~> 6.61"
     }
 
     render = {
       source  = "render-oss/render"
-      version = "1.8.0"
+      version = "1.9.1"
     }
 
     cloudflare = {

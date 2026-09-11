24ab9cd642bce2e4a52df6fbd1185210605697bb
François Voron <francois@polar.sh>
2026-09-11 08:19:31 +0000

    server/payout: tweak unpaid transactions batching queries (#14333)

--- server/polar/transaction/repository.py
+++ server/polar/transaction/repository.py
@@ -1,18 +1,8 @@
 from collections.abc import Sequence
-from typing import cast
+from itertools import batched
 from uuid import UUID
 
-from sqlalchemy import (
-    ColumnClause,
-    CursorResult,
-    Select,
-    and_,
-    column,
-    func,
-    or_,
-    select,
-    update,
-)
+from sqlalchemy import Select, and_, func, or_, select, update
 from sqlalchemy.orm import selectinload
 
 from polar.kit.repository import (
@@ -35,12 +25,8 @@ class TransactionRepository(
     async def set_unpaid_transactions_payout(
         self, account: UUID, payout_transaction_id: UUID
     ) -> None:
-        batch_size = 500
-        transaction_ctid: ColumnClause[tuple[int, int]] = column("ctid")
-        transaction_ctid.table = Transaction.__table__
-        batch = (
-            select(transaction_ctid.label("ctid"))
-            .select_from(Transaction)
+        statement = (
+            select(Transaction.id)
             .join(Account, Account.id == Transaction.account_id)
             .where(
                 ~Transaction.is_deleted,
@@ -65,22 +51,18 @@ class TransactionRepository(
                 ),
             )
             .order_by(Transaction.created_at, Transaction.id)
-            .limit(batch_size)
-            .with_for_update(of=Transaction)
-            .cte("batch")
+            # No key changes: allow concurrent foreign-key references.
+            .with_for_update(of=Transaction, key_share=True)
         )
-        statement = (
-            update(Transaction)
-            .where(transaction_ctid == batch.c.ctid)
-            .values(payout_transaction_id=payout_transaction_id)
-            .execution_options(synchronize_session=False)
-        )
-        while True:
-            result = cast(
-                CursorResult[Transaction], await self.session.execute(statement)
+        transaction_ids = (await self.session.scalars(statement)).all()
+        for batch in batched(transaction_ids, 500):
+            update_statement = (
+                update(Transaction)
+                .where(Transaction.id.in_(batch))
+                .values(payout_transaction_id=payout_transaction_id)
+                .execution_options(synchronize_session=False)
             )
-            if result.rowcount < batch_size:
-                break
+            await self.session.execute(update_statement)
 
     async def get_all_paid_transactions_by_payout(
         self, payout_transaction_id: UUID

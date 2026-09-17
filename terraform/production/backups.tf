ee7cc02a8868558c938813bd278e295ecf13a5d5
Jesper Bränn <jesper@polar.sh>
2026-09-17 09:35:16 +0200

    backups: Handle no export for date more clearly
    No log to state it is difficult to debug

--- terraform/production/backups.tf
+++ terraform/production/backups.tf
@@ -9,7 +9,11 @@ locals {
     echo "Copying Render export for $DATE"
     URL=$(curl -fsS "https://api.render.com/v1/postgres/$RENDER_DATABASE_ID/export" \
       -H "Authorization: Bearer $RENDER_API_KEY" \
-      | jq -er --arg d "$DATE" '[.[] | select(.createdAt | startswith($d))][0].url')
+      | jq -r --arg d "$DATE" '[.[] | select(.createdAt | startswith($d))][0].url // empty')
+    if [ -z "$URL" ]; then
+      echo "Error: No export for $DATE found" >&2
+      exit 1
+    fi
     curl -fsS "$URL" | aws s3 cp - "s3://$BUCKET_NAME/$${DATE//-/}/$${DATE//-/}_backup.tar.gz" \
       --expected-size 200000000000 --checksum-algorithm SHA256
   EOT

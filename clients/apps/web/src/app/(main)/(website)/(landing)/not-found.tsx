9045ec24649c134d711b67c8765eb39aaf45b833
Emil Widlund <hello@emilwidlund.com>
2026-09-18 10:45:21 +0200

    implement new 404

---
+++ clients/apps/web/src/app/(main)/(website)/(landing)/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFound } from '@/components/Landing/NotFound'
+
+export default function LandingNotFound() {
+  return <NotFound />
+}
--- clients/apps/web/src/app/not-found.tsx
+++ clients/apps/web/src/app/not-found.tsx
@@ -1,5 +1,20 @@
-import PageNotFound from '@/components/Shared/PageNotFound'
+'use client'
 
-export default function NotFound() {
-  return <PageNotFound />
+import LandingLayout from '@/components/Landing/LandingLayout'
+import { NotFound } from '@/components/Landing/NotFound'
+import { ThemeProvider } from 'next-themes'
+
+export default function RootNotFound() {
+  return (
+    <ThemeProvider
+      defaultTheme="system"
+      enableSystem
+      attribute="class"
+      storageKey="polar-not-found-theme"
+    >
+      <LandingLayout>
+        <NotFound />
+      </LandingLayout>
+    </ThemeProvider>
+  )
 }

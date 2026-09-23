1617dde45302432ce454bd338e4fc9604ece5044
Maxime BV <maxime@polar.sh>
2026-09-23 12:16:17 +0200

    terraform: tag every AWS resource as terraform-managed

--- terraform/identity/main.tf
+++ terraform/identity/main.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      ManagedBy = "terraform"
+    }
+  }
 }
--- terraform/sandbox/main.tf
+++ terraform/sandbox/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   region = "us-east-2"
+
+  default_tags {
+    tags = {
+      ManagedBy = "terraform"
+    }
+  }
 }
 
 provider "render" {
